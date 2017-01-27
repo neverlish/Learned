b1de657ee57b5cb490e5a0fe1f2dc66fc89aa156
Jinho Hyeon <neverlish@gmail.com>
2017-01-27 09:04:56 +0900

    AsyncStorage 연동

--- react-native/egghead_todo/app.js
+++ react-native/egghead_todo/app.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Platform, ListView, Keyboard} from 'react-native';
+import {View, Text, StyleSheet, Platform, ListView, Keyboard, AsyncStorage} from 'react-native';
 import Header from './header';
 import Footer from './footer';
 import Row from './row';
@@ -32,12 +32,24 @@ class App extends Component {
     this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
   }
 
+  componentWillMount() {
+    AsyncStorage.getItem('items').then((json) => {
+      try {
+        const items = JSON.parse(json);
+        this.setSource(items, items);
+      } catch(e) {
+        
+      }
+    })
+  }
+
   setSource(items, itemsDataSource, otherState={}) {
     this.setState({
       items,
       dataSource: this.state.dataSource.cloneWithRows(itemsDataSource),
       ... otherState
     })
+    AsyncStorage.setItem('items', JSON.stringify(items));
   }
 
   handleClearComplete() {
