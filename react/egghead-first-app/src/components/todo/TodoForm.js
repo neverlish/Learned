97009c8ff86a2b3c93c7f3044c5c5b880594dac1
Jinho Hyeon <neverlish@gmail.com>
2017-02-06 21:59:57 +0900

    [react] egghead-first-app : TodoForm 컴포넌트 분리

---
+++ react/egghead-first-app/src/components/todo/TodoForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const TodoForm = (props) => (
+  <form>
+    <input
+      type='text'
+      onChange={props.handleInputChange}
+      value={props.currentTodo}/>
+  </form>
+)
