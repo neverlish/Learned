fa8463191b620aa81aee27a7d5c7c3fc2c7386b4
Jinho Hyeon <neverlish@gmail.com>
2017-02-07 01:16:44 +0900

    [react] egghead-first-app : filterTodos 구현

--- react/egghead-first-app/src/App.js
+++ react/egghead-first-app/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {TodoForm, TodoList, Footer} from './components/todo';
-import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo} from './lib/todoHelpers';
+import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todoHelpers';
 import {pipe, partial} from './lib/utils';
 
 class App extends Component {
@@ -13,6 +13,10 @@ class App extends Component {
       {id: 3, name: 'Ship It!', isComplete: false}
     ],
     currentTodo: ''
+  }
+
+  static contextTypes = {
+    route: React.PropTypes.string
   }
 
   handleRemove = (id, evt) => {
@@ -58,6 +62,7 @@ class App extends Component {
 
   render() {
     const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
+    const displayTodos = filterTodos(this.state.todos, this.context.route)
     return (
       <div className="App">
         <div className="App-header">
@@ -72,7 +77,7 @@ class App extends Component {
             handleSubmit={submitHandler}/>
           <TodoList
             handleToggle={this.handleToggle}
-            todos={this.state.todos}
+            todos={displayTodos}
             handleRemove={this.handleRemove}/>
           <Footer/>
         </div>
