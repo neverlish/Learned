3e51c0ba20bf07397a2caed0057d2f3df8a3ba28
Jinho Hyeon <neverlish@gmail.com>
2017-02-06 22:37:26 +0900

    [react] egghead-first-app : 목록 추가 시 빈 문자열 방지

--- react/egghead-first-app/src/App.js
+++ react/egghead-first-app/src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     }
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEmptySubmit = this.handleEmptySubmit.bind(this);
   }
 
   handleSubmit(evt) {
@@ -30,7 +31,15 @@ class App extends Component {
     const updatedTodos = addTodo(this.state.todos, newTodo)
     this.setState({
       todos: updatedTodos,
-      currentTodo: ''
+      currentTodo: '',
+      errorMessage: ''
+    })
+  }
+
+  handleEmptySubmit(evt) {
+    evt.preventDefault()
+    this.setState({
+      errorMessage: 'Please supply a todo name'
     })
   }
 
@@ -41,6 +50,7 @@ class App extends Component {
   }
 
   render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
     return (
       <div className="App">
         <div className="App-header">
@@ -48,10 +58,11 @@ class App extends Component {
           <h2>React Todos</h2>
         </div>
         <div className='Todo-App'>
+          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
           <TodoForm
             handleInputChange={this.handleInputChange}
             currentTodo={this.state.currentTodo}
-            handleSubmit={this.handleSubmit}/>
+            handleSubmit={submitHandler}/>
           <TodoList todos={this.state.todos}/>
         </div>
       </div>
