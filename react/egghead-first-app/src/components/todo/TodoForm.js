3b4ebd99d6545d7f25672a0f56cf8d27e50c9b8c
Jinho Hyeon <neverlish@gmail.com>
2017-02-06 22:14:14 +0900

    [react] egghead-first-app : PropTypes 적용

--- react/egghead-first-app/src/components/todo/TodoForm.js
+++ react/egghead-first-app/src/components/todo/TodoForm.js
@@ -8,3 +8,8 @@ export const TodoForm = (props) => (
       value={props.currentTodo}/>
   </form>
 )
+
+TodoForm.propTypes = {
+  currentTodo: React.PropTypes.string.isRequired,
+  handleInputChange: React.PropTypes.func.isRequired
+}
