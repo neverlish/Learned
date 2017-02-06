e91fa44e0e2dfad4cb95c33c406d2c9513e705aa
Jinho Hyeon <neverlish@gmail.com>
2017-02-06 23:14:47 +0900

    [react] egghead-first-app : todo 함수들 적용, partial 함수 추가

--- react/egghead-first-app/src/components/todo/TodoItem.js
+++ react/egghead-first-app/src/components/todo/TodoItem.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {partial} from '../../lib/utils'
 
 export const TodoItem = (props) => {
+  const handleToggle = partial(props.handleToggle, props.id)
   return (
     <li key={props.id}>
-      <input type='checkbox' checked={props.isComplete}/>{props.name}
+      <input type='checkbox'
+        onChange={handleToggle}
+        checked={props.isComplete}/>
+      {props.name}
     </li>
   )
 }
