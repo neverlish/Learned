4543a4fe1a76fe3ea5be1d2145b6c991caf953b3
Jinho Hyeon <neverlish@gmail.com>
2017-02-10 23:02:35 +0900

    @Input을 활용한 데이터 공유

--- angular2/egghead-getstarted/src/app/app.component.ts
+++ angular2/egghead-getstarted/src/app/app.component.ts
@@ -5,9 +5,12 @@ import { Component, Inject } from '@angular/core';
   template: `
     <div>
       <ul>
-        <li *ngFor="let message of mail.messages">
-          {{message}}
-        </li>
+        <app-simple-form
+          *ngFor="let message of mail.messages"
+          [message]="message"
+        >
+
+        </app-simple-form>
       </ul>
     </div>
   `
--- angular2/egghead-getstarted/src/app/simple-form/simple-form.component.ts
+++ angular2/egghead-getstarted/src/app/simple-form/simple-form.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
   template: `
     <div>
+      {{message}}
       <input #myInput type='text'>
       <button (click)="onClick($event, myInput.value)">Click me!</button>
     </div>
@@ -11,6 +12,8 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
+
+  @Input() message;
 
   onClick(event, value) {
     console.log(event);
