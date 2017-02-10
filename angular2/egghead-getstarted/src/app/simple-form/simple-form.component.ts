082a38f9db5e525d0d7789ef62e25f975bb0f644
Jinho Hyeon <neverlish@gmail.com>
2017-02-10 22:46:57 +0900

    Events와 Ref로 요소 관리

--- angular2/egghead-getstarted/src/app/simple-form/simple-form.component.ts
+++ angular2/egghead-getstarted/src/app/simple-form/simple-form.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   template: `
-    <p>
-      simple-form Works!
-    </p>
+    <div>
+      <input #myInput type='text'>
+      <button (click)="onClick(myInput.value)">Click me!</button>
+    </div>
   `,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
+
+  onClick(value) {
+    console.log(value);
+  }
 
   constructor() { }
 
