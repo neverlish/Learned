08f669f60dbe305a5312a11289203702b0547eee
Jinho Hyeon <neverlish@gmail.com>
2017-02-10 23:07:32 +0900

    ngModel를 활용한 양방향 데이터 동기화

--- angular2/egghead-getstarted/src/app/simple-form/simple-form.component.ts
+++ angular2/egghead-getstarted/src/app/simple-form/simple-form.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit, Input } from '@angular/core';
   template: `
     <div>
       {{message}}
-      <input #myInput type='text'>
+      <input #myInput type='text' [(ngModel)]="message">
       <button (click)="onClick($event, myInput.value)">Click me!</button>
     </div>
   `,
@@ -20,7 +20,9 @@ export class SimpleFormComponent implements OnInit {
     console.log(value);
   }
 
-  constructor() { }
+  constructor() {
+    setInterval(() => this.message = Math.random().toString(), 1000)
+  }
 
   ngOnInit() {
   }
