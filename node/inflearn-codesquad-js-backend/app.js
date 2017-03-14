29cc409227cfc5df1b21bb02cf31f41502487f3d
Jinho Hyeon <neverlish@gmail.com>
2017-03-15 04:51:45 +0900

    MySQL 연동 구현

--- node/inflearn-codesquad-js-backend/app.js
+++ node/inflearn-codesquad-js-backend/app.js
@@ -36,7 +36,17 @@ app.post('/email_post', function(req, res) {
 });
 
 app.post('/ajax_send_email', function(req, res) {
-	console.log(req.body.email);
-	var responseData = {'result': 'ok', 'email': req.body.email};
-	res.json(responseData);
+	var email = req.body.email;
+	var responseData = {};
+	var query = connection.query('select name from user where email="' + email + '"', function(err, rows) {
+		if(err) throw err;
+		if(rows[0]) {
+			responseData.result = 'ok';
+			responseData.name = rows[0].name;
+		} else {
+			responseData.result = 'none';
+			responseData.name = '';
+		}
+		res.json(responseData);
+	})
 })
