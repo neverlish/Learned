7957e4e64b5c20b8818dae0ecc96d4a219f13519
Jinho Hyeon <neverlish@gmail.com>
2017-03-15 02:50:50 +0900

    static 디렉토리 설정

--- node/inflearn-codesquad-js-backend/app.js
+++ node/inflearn-codesquad-js-backend/app.js
@@ -5,6 +5,12 @@ app.listen(3000, function() {
 	console.log('Start! express server on port 3000');
 });
 
+app.use(express.static('public'))
+
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + "/public/main.html")
+});
+
+app.get('/main', function(req, res) {
+	res.sendFile(__dirname + '/public/main.html')
 })
