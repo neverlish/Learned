0d7102e6f45889061323b320b1ec9fbde9e9b002
Jinho Hyeon <neverlish@gmail.com>
2017-03-15 03:27:32 +0900

    POST 요청처리

--- node/inflearn-codesquad-js-backend/app.js
+++ node/inflearn-codesquad-js-backend/app.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 
 app.listen(3000, function() {
 	console.log('Start! express server on port 3000');
 });
 
 app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + "/public/main.html")
@@ -14,3 +17,8 @@ app.get('/', function(req, res) {
 app.get('/main', function(req, res) {
 	res.sendFile(__dirname + '/public/main.html')
 })
+
+app.post('/email_post', function(req, res) {
+	console.log(req.body.email)
+	res.send('<h1>welcome ' + req.body.email + '</h1>')
+});
