b3749fca9fed41e5e1c3b891b09bea8143b53d45
Jinho Hyeon <neverlish@gmail.com>
2017-03-14 21:24:22 +0900

    URL Routing 처리

--- node/inflearn-codesquad-js-backend/app.js
+++ node/inflearn-codesquad-js-backend/app.js
@@ -4,3 +4,7 @@ var app = express()
 app.listen(3000, function() {
 	console.log('Start! express server on port 3000');
 });
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + "/public/main.html")
+})
