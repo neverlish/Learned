2e52ed13973bfa122c9e052a8b44f6a1102a973c
Jinho Hyeon <neverlish@gmail.com>
2017-03-15 21:23:58 +0900

    Intercept Route Changes with React Router v4 Prompt Component

--- react/egghead-react-router-v4/src/App.js
+++ react/egghead-react-router-v4/src/App.js
@@ -3,41 +3,37 @@ import {
   BrowserRouter as Router,
   Route,
   Link,
-  Switch,
-  Redirect
+  Prompt
 } from 'react-router-dom'
 
 import './App.css'
 
-const loggedin = false
+const Home = () => (<h1>Home</h1>)
 
-const Links = () =>
-  <nav>
-    <Link to='/'>Home</Link>
-    <Link to='/old/123'>Old</Link>
-    <Link to='/new/456'>New</Link>
-    <Link to='/protected'>Protected</Link>
-  </nav>
+class Form extends React.Component {
+  state = {dirty: false}
+  setDirty = () => this.setState({dirty: true})
+  render() {
+    return (
+      <div>
+        <h1>Form</h1>
+        <input type='text' onInput={this.setDirty} />
+        <Prompt
+          when={this.state.dirty}
+          message='Data will be lost!'
+        />
+      </div>
+    )
+  }
+}
 
 const App = (props) => (
   <Router>
     <div>
-      <Links />
-      <Switch>
-        <Route exact path='/' render={() => (<h1>Home</h1>)}/>
-        <Route
-          path='/new/:str'
-          render={({match}) => (<h1>New: {match.params.str}</h1>)}
-        />
-        <Route path='/old/:str' render={({match}) => (
-          <Redirect to={`/new/${match.params.str}`} />
-        )}/>
-        <Route path='/protected' render={() => (
-          loggedin
-          ? <h1>Welcome to the protected page</h1>
-          : <Redirect to='/new/Login' />
-        )} />
-      </Switch>
+      <Link to='/'>Home</Link>
+      <Link to='/form'>Form</Link>
+      <Route exact path='/' component={Home} />
+      <Route path='/form' component={Form} />
     </div>
   </Router>
 )
