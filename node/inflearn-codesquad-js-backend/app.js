db881fb975264ecdec97cb2d36d31501df776505
Jinho Hyeon <neverlish@gmail.com>
2017-03-15 19:13:35 +0900

    Routing 모듈화 2 (DB연결부분)

--- node/inflearn-codesquad-js-backend/app.js
+++ node/inflearn-codesquad-js-backend/app.js
@@ -1,18 +1,8 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
-var mysql = require('mysql')
 var main = require('./router/main')
-
-var connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: 'asasas',
-	database: 'jsman'
-})
-
-connection.connect()
+var email = require ('./router/email')
 
 app.listen(3000, function() {
 	console.log('Start! express server on port 3000');
@@ -24,28 +14,8 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
 
 app.use('/main',main)
+app.use('/email',email)
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + "/public/main.html")
 });
-
-app.post('/email_post', function(req, res) {
-	console.log(req.body.email)
-	res.render('email.ejs', {'email': req.body.email})
-});
-
-app.post('/ajax_send_email', function(req, res) {
-	var email = req.body.email;
-	var responseData = {};
-	var query = connection.query('select name from user where email="' + email + '"', function(err, rows) {
-		if(err) throw err;
-		if(rows[0]) {
-			responseData.result = 'ok';
-			responseData.name = rows[0].name;
-		} else {
-			responseData.result = 'none';
-			responseData.name = '';
-		}
-		res.json(responseData);
-	})
-})
