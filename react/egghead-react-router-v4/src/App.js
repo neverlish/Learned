71a89bed01d4de3a5f8e01ef6d7e5ddaaa9af815
Jinho Hyeon <neverlish@gmail.com>
2017-03-15 20:46:29 +0900

    Parse Query Parameters in React Router v4

--- react/egghead-react-router-v4/src/App.js
+++ react/egghead-react-router-v4/src/App.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Link
 } from 'react-router-dom'
 
 import './App.css'
 
-const App = () => (
+const Links = () => (
+  <nav>
+    <Link to='/?id=123'>Inline</Link>
+    <Link to={{pathname: '/', search: 'id=456'}}>Object</Link>
+  </nav>
+)
+
+const App = (props) => (
   <Router>
     <div>
-      <Route path='/:a(\d{2}-\d{2}-\d{4}):b(\.[a-z]+)' render={({match}) => (
-        <h1>
-          paramA: {match.params.a}<br/>
-          paramB: {match.params.b}
-        </h1>
+      <Links />
+      <Route path='/' render={({match, location}) => (
+        <div>
+          <p>root</p>
+          <p>{JSON.stringify(match)}</p>
+          <p>{JSON.stringify(location)}</p>
+          <p>{new URLSearchParams(location.search).get('id')}</p>
+        </div>
       )} />
     </div>
   </Router>
