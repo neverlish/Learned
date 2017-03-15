942629e45982fc0535a1085d11f0db4be79038c3
Jinho Hyeon <neverlish@gmail.com>
2017-03-15 20:51:40 +0900

    Conditionally Render a React Router v4 Route with the Switch Component

--- react/egghead-react-router-v4/src/App.js
+++ react/egghead-react-router-v4/src/App.js
@@ -25,7 +25,8 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' render={() => <h1>Home</h1>} />
             <Route path='/about' render={() => <h1>About</h1>} />
-            <Route render={() => <h1>Page not found</h1>} />
+            <Route path='/contact' render={() => <h1>Contact</h1>} />
+            <Route path='/:itemid' render={({match}) => <h1>Item: {match.params.itemid}</h1>} />
           </Switch>
         </div>
       </Router>
