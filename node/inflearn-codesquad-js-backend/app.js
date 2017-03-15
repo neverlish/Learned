005f7ec050ed8945139ec2d043115b827ff40cfe
Jinho Hyeon <neverlish@gmail.com>
2017-03-15 19:25:39 +0900

    Routing 리팩토링

--- node/inflearn-codesquad-js-backend/app.js
+++ node/inflearn-codesquad-js-backend/app.js
@@ -1,8 +1,7 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
-var main = require('./router/main')
-var email = require ('./router/email')
+var router = require('./router/index')
 
 app.listen(3000, function() {
 	console.log('Start! express server on port 3000');
@@ -13,9 +12,4 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
 
-app.use('/main',main)
-app.use('/email',email)
-
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + "/public/main.html")
-});
+app.use(router)
--- node/inflearn-codesquad-js-backend/router/main/main.js
+++ node/inflearn-codesquad-js-backend/router/main/main.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var path = require('path')
 
 router.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, '../public/main.html'))
+	res.sendFile(path.join(__dirname, '../../public/main.html'))
 })
 
 module.exports = router;
