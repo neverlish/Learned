80f36eee1e3d6b13cf0b84555573aa56c1574526
Jinho Hyeon <neverlish@gmail.com>
2017-03-15 19:58:36 +0900

    Create Basic Routes with the React Router v4 BrowserRouter

--- react/egghead-react-router-v4/src/App.js
+++ react/egghead-react-router-v4/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 
-console.log(BrowserRouter)
+const Home = (props) => {
+  console.log(props)
+  return <h1>Home</h1>
+}
 
-const App = () => <h1>Hello World</h1>
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path='/' component={Home}/>
+      {/*<Route path='/about' render={() => <h1>About</h1>}/>*/}
+      <Route
+        path='/about'
+        children={({match}) => match && <h1>About</h1>}/>
+    </div>
+  </Router>
+)
 
 export default App
