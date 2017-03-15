0adcfcb43484994422adfac8ff0e38793b8bdea3
Jinho Hyeon <neverlish@gmail.com>
2017-03-15 20:04:29 +0900

    Use the React Router v4 Link Component for Navigation Between Routes

--- react/egghead-react-router-v4/src/App.js
+++ react/egghead-react-router-v4/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-const Home = (props) => {
-  console.log(props)
-  return <h1>Home</h1>
-}
+const Links = () => (
+  <nav>
+    <Link to='/'>Home</Link>
+    <Link to={{pathname: '/about'}}>About</Link>
+    <Link replace to='/contact'>Contact</Link>
+  </nav>
+)
 
 const App = () => (
   <Router>
     <div>
-      <Route exact path='/' component={Home}/>
-      {/*<Route path='/about' render={() => <h1>About</h1>}/>*/}
-      <Route
-        path='/about'
-        children={({match}) => match && <h1>About</h1>}/>
+      <Links />
+      <Route exact path='/' render={() => <h1>Home</h1>}/>
+      <Route path='/about' render={() => <h1>About</h1>}/>
+      <Route path='/contact' render={() => <h1>Contact</h1>}/>
     </div>
   </Router>
 )
