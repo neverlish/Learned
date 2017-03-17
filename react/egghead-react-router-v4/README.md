cc7cba2b9239ee1beda01a22a98f9a14391c198f
Jinho Hyeon <neverlish@gmail.com>
2017-03-17 23:01:29 +0900

    [react] Egghead : Add routing to React apps using React Router v4

---
+++ react/egghead-react-router-v4/README.md
@@ -0,0 +1,3 @@
+Lecture : https://egghead.io/courses/add-routing-to-react-apps-using-react-router-v4
+
+Pull Request : https://github.com/neverlish/neverlish/pull/8
---
+++ react/egghead-react-router-v4/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {
+  BrowserRouter,
+  HashRouter,
+  MemoryRouter,
+  StaticRouter,
+  NativeRouter,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import './App.css'
+
+const LinksRoutes = () => (
+  <div>
+    <Link to='/'>Home</Link>
+    <Link to='/about'>About</Link>
+    <Route exact path='/' render={() => <h1>Home</h1>} />
+    <Route path='/about' render={() => <h1>About</h1>} />
+  </div>
+)
+
+const forceRefresh = () => {
+  console.log(new Date())
+  return false
+}
+
+const BrowserRouterApp = () => (
+  <BrowserRouter forceRefresh={forceRefresh()}>
+    <LinksRoutes/>
+  </BrowserRouter>
+)
+
+const HashRouterApp = () => (
+  <HashRouter hashType="hashbang">
+    <LinksRoutes/>
+  </HashRouter>
+)
+
+const MemoryRouterApp = () => (
+  <MemoryRouter
+    initialEntries={['/', '/about']}
+    initialIndex={1}
+  >
+    <LinksRoutes />
+  </MemoryRouter>
+)
+
+const StaticRouterApp = () => (
+  <StaticRouter location="/about" context={{}}>
+    <LinksRoutes/>
+  </StaticRouter>
+)
+
+export default StaticRouterApp
