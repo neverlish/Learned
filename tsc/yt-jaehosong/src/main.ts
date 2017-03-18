56a24f54eb9f65cf16543faef8c8000548ad7b1e
Jinho Hyeon <neverlish@gmail.com>
2017-03-18 10:21:27 +0900

    튜플 Tuple

--- tsc/yt-jaehosong/src/main.ts
+++ tsc/yt-jaehosong/src/main.ts
@@ -1,17 +1,11 @@
-let a: number = 1;
-let b: string = 'banana';
-let c: boolean = true;
+let a: [number, string];
+a[0] = 1234;
+a[1] = 'abc';
 
-a = 'apple';
+a[0] = true;
+// 숫자가 들어갈 자리에 boolean 을 넣어서 에러
 
-console.log(a)
-// apple을 출력. tsc에 에러가 있어도 컴파일을 함.
-
-let arr: number[] = [1,2,3];
-let arrstr: string[] = ['a','b','c'];
-let arrBool: boolean[] = [true, false];
-
-// number[] = Array<number>. number[] 보다는 Array<number> 사용. generic 때문.
-
-let arrNum: Array<number> = [1,2,3];
-
+a[2] = 123;
+a[2] = 'www.ionickorea.com'
+a[2] = true;
+// 3번째 항목에 들어갈 값이 정해지지 않았음. 이런 때에는 선언된 유형인 number나 string만 가능.
