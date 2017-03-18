0026a90ca5ca68882991ba99c46f862341c0c0ae
Jinho Hyeon <neverlish@gmail.com>
2017-03-18 10:15:39 +0900

    변수 선언

--- tsc/yt-jaehosong/src/main.ts
+++ tsc/yt-jaehosong/src/main.ts
@@ -1,7 +1,23 @@
-console.log('hello typescript');
-console.log('How are you?');
+var a = 1;
+let b = 2;
 
-let a = 10;
-let b = 20;
-console.log(a+b);
+console.log(a);
+console.log(b);
 
+if (true) {
+  let c = 3;
+}
+
+console.log(c);
+
+const d = 4;
+console.log(d);
+
+d = 5;
+console.log(d);
+
+/*
+var 는 전체 영역에서 사용 가능한 변수
+let은 해당 영역 또는 블럭, 함수 내에서만 사용 가능한 변수
+const는 변경이 불가한 값을 지정하는 변수
+*/
