54e28a08b9776d6e78f7747113b518813195091a
Jinho Hyeon <neverlish@gmail.com>
2017-03-19 12:20:59 +0900

    Event binding

--- ionic/yt-jaehosong/src/pages/home/home.ts
+++ ionic/yt-jaehosong/src/pages/home/home.ts
@@ -6,19 +6,8 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  urlFall: string = 'assets/fall.png';
-  urlImages: Array<string> = [
-    'assets/fall.png',
-    'assets/fall.jpg'
-  ]
+  messages: Array<string> = [];
   constructor(public navCtrl: NavController) {
-    // setTimeout(() =>
-    //   this.urlFall = 'assets/fall.jpg'
-    // , 1000);
-    let index = 0;
-    setInterval(() => {
-      index = index == 0 ? 1 : 0;
-      this.urlFall = this.urlImages[index];
-    }, 1000);
+
   }
 }
