048d21c370cf52eb81087d8e9b36e50a1b16f342
Jinho Hyeon <neverlish@gmail.com>
2017-03-19 13:08:51 +0900

    [ionic]  YouTube : Jaeho Song

---
+++ ionic/yt-jaehosong/README.md
@@ -0,0 +1,3 @@
+Lecture: https://www.youtube.com/watch?v=_O0uX4e3dIs
+
+Pull Request: https://github.com/neverlish/neverlish/pull/11
---
+++ ionic/yt-jaehosong/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ChildCat } from '../components/child-cat/child-cat';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ChildCat
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
