45ab8f41645c73f386379b1383be576f6ed011db
Jinho Hyeon <neverlish@gmail.com>
2017-03-19 12:35:27 +0900

    ViewChild

--- ionic/yt-jaehosong/src/pages/home/home.ts
+++ ionic/yt-jaehosong/src/pages/home/home.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Content } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  messages: Array<string> = [];
-  constructor(public navCtrl: NavController) {
-
+  y: number = 0;
+  onClickScroll(myContent) {
+    myContent.scrollTo(0, this.y+= 80, 100);
   }
+  // @ViewChild (Content) content: Content;
+  // // ViewChild는 템플릿이 만들어지기 전에 참조 가능
+  // onClickScroll() {
+  //   this.content.scrollTo(0, this.y+= 80, 100);
+  // }
 }
