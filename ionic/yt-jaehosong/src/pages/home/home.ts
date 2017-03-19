c8f2ce8b0ca76f65ee2658d66827c43619d8bc5c
Jinho Hyeon <neverlish@gmail.com>
2017-03-19 12:11:50 +0900

    ngIf

--- ionic/yt-jaehosong/src/pages/home/home.ts
+++ ionic/yt-jaehosong/src/pages/home/home.ts
@@ -6,7 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  show: boolean = true;
   constructor(public navCtrl: NavController) {
 
   }
