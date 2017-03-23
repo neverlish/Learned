03a678e09e91efe4df810c79734cff060096ea7d
Jinho Hyeon <neverlish@gmail.com>
2017-03-23 22:35:22 +0900

    Using Type Inference in TypeScript

---
+++ tsc/egghead-use-types-effectively/02.ts
@@ -0,0 +1,7 @@
+let userName;
+userName = ["Silver", "Surfer"];
+
+let userId = (a: string, b: number): string => a + b;
+
+let target = document.getElementById('target');
+// target.onclick = (event: HTMLButtonElement) => event.button;
