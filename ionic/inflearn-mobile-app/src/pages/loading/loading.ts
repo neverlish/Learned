5f098be34cfc056716c1a589abef28ea6adf5aa4
Jinho Hyeon <neverlish@gmail.com>
2017-03-26 21:50:26 +0900

    Loading - Promise

--- ionic/inflearn-mobile-app/src/pages/loading/loading.ts
+++ ionic/inflearn-mobile-app/src/pages/loading/loading.ts
@@ -10,13 +10,47 @@ export class LoadingPage {
 
   }
 
-  presentLoading() {
+  getPromise() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(() => {
+        resolve('Success');
+      }, 2000);
+    });
+  }
+
+  presentLoadingCircles() {
     let loader = this.loadingCtrl.create({
       spinner: 'bubbles',
       content: 'Please wait...',
       duration: 3000,
     });
     loader.present();
-    loader.dismiss();
+    this.getPromise().then(
+      (data) => {
+        console.log(data);
+        loader.dismiss();
+      },
+      (error) => {
+
+      }
+    )
+  }
+
+  presentLoadingBubbles() {
+    let loader = this.loadingCtrl.create({
+      spinner: 'circles',
+      content: 'Please wait...',
+      duration: 3000,
+    });
+    loader.present();
+  }
+
+  presentLoadingCrescent() {
+    let loader = this.loadingCtrl.create({
+      spinner: 'crescent',
+      content: 'Please wait...',
+      duration: 3000,
+    });
+    loader.present();
   }
 }
