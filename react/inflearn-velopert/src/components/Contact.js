8457b510ea6101279cd2ec8f7bd50a387d92e4bb
Jinho Hyeon <neverlish@gmail.com>
2017-03-27 21:13:36 +0900

    Contact 데이터 삭제/수정 기능 구현 | 컴포넌트 응용

--- react/inflearn-velopert/src/components/Contact.js
+++ react/inflearn-velopert/src/components/Contact.js
@@ -66,6 +66,10 @@ export default class Contact extends React.Component {
   }
 
   handleRemove() {
+    if (this.state.selectedKey < 0) {
+      return;
+    }
+
     this.setState({
       contactData: update(this.state.contactData,
         { $splice: [[this.state.selectedKey,1]] }
@@ -104,6 +108,8 @@ export default class Contact extends React.Component {
         <ContactDetails
           isSelected={this.state.selectedKey != -1}
           contact={this.state.contactData[this.state.selectedKey]}
+          onRemove={this.handleRemove}
+          onEdit={this.handleEdit}
         />
         <ContactCreate
           onCreate={this.handleCreate}
--- react/inflearn-velopert/src/components/ContactDetails.js
+++ react/inflearn-velopert/src/components/ContactDetails.js
@@ -1,6 +1,42 @@
 import React from 'react';
 
 export default class ContactDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEdit: false,
+      name: '',
+      phone: ''
+    };
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+  }
+
+  handleToggle() {
+    if (!this.state.isEdit) {
+      this.setState({
+        name: this.props.contact.name,
+        phone: this.props.contact.phone
+      });
+    } else {
+      this.handleEdit();
+    }
+    this.setState({
+      isEdit: !this.state.isEdit
+    });
+  }
+
+  handleChange(e) {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
+
+  handleEdit() {
+    this.props.onEdit(this.state.name, this.state.phone)
+  }
+
   render() {
     const details = (
       <div>
@@ -8,11 +44,44 @@ export default class ContactDetails extends React.Component {
         <p>{this.props.contact.phone}</p>
       </div>
     );
+
+    const edit = (
+      <div>
+        <p>
+          <input
+            type='text'
+            name='name'
+            placeholder='name'
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </p>
+        <p>
+          <input
+            type='text'
+            name='phone'
+            placeholder='phone'
+            value={this.state.phone}
+            onChange={this.handleChange}
+          />
+        </p>
+      </div>
+    )
+
     const blank = (<div>Not selected</div>);
+
+    const view = this.state.isEdit ? edit : details;
+
     return (
       <div>
         <h2>Details</h2>
-        {this.props.isSelected ? details : blank}
+        {this.props.isSelected ? view : blank}
+        <p>
+          <button onClick={this.handleToggle}>
+            {this.state.isEdit ? 'OK' : 'Edit'}
+          </button>
+          <button onClick={this.props.onRemove}>Remove</button>
+        </p>
       </div>
     )
   }
@@ -22,5 +91,13 @@ ContactDetails.defaultProps = {
   contact: {
     name: '',
     phone: ''
-  }
+  },
+  onRemove: () => { console.log('onRemove not defined'); },
+  onEdit: () => { console.log('onEdit not defined'); }
+}
+
+ContactDetails.propTypes = {
+  contact: React.PropTypes.object,
+  onRemove: React.PropTypes.func,
+  onEdit: React.PropTypes.func
 }
