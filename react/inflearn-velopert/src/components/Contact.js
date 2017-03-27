f7076dfcce6b712fd9a0cb38bba13b8a069e8993
Jinho Hyeon <neverlish@gmail.com>
2017-03-27 20:59:36 +0900

    Contact 데이터 추가기능 구현 | 컴포넌트 응용

--- react/inflearn-velopert/src/components/Contact.js
+++ react/inflearn-velopert/src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
 import ContactDetails from './ContactDetails'
+import ContactCreate from './ContactCreate';
 import update from 'react-addons-update';
 
 export default class Contact extends React.Component {
@@ -103,6 +104,9 @@ export default class Contact extends React.Component {
         <ContactDetails
           isSelected={this.state.selectedKey != -1}
           contact={this.state.contactData[this.state.selectedKey]}
+        />
+        <ContactCreate
+          onCreate={this.handleCreate}
         />
       </div>
     );
