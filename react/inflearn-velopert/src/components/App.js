9d60b15fad0150428c6c3a48f0190a5d41792887
Jinho Hyeon <neverlish@gmail.com>
2017-03-27 20:15:00 +0900

    Contact 검색기능 구현 | input, sort, filter

--- react/inflearn-velopert/src/components/App.js
+++ react/inflearn-velopert/src/components/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
+import Contact from './Contact';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: ''
-    }
-  }
   render() {
     return (
-      <div>
-        <button onClick={() => {this.setState({name: 'velopert'})}}>Click</button>
-        <h1>Hello asd{this.state.name}</h1>
-      </div>
+      <Contact/>
     )
   }
 }
