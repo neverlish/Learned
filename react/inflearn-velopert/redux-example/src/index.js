7f39313a0f9cf97d7873b5f6da04973fda80d9f6
Jinho Hyeon <neverlish@gmail.com>
2017-03-28 10:06:38 +0900

    Redux: Store

--- react/inflearn-velopert/redux-example/src/index.js
+++ react/inflearn-velopert/redux-example/src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+
+const store = createStore(reducers);
+
+// store 테스트용 코드
+// import * as actions from './actions';
+// console.log(store.getState());
+// const unsubscribe = store.subscribe(() => console.log(store.getState()));
+// store.dispatch(actions.increment());
+// store.dispatch(actions.increment());
+// store.dispatch(actions.decrement());
+// store.dispatch(actions.setColor([200,200,200]));
+//
+// unsubscribe();
+// store.dispatch(actions.setColor([210,210,210]));
+
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
