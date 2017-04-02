b23177c8b0c60b20577a66d6d9035b940f964071
Jinho Hyeon <neverlish@gmail.com>
2017-04-02 12:54:21 +0900

    05 - Model and Model Fields

--- django/askdjango_django_basic/basic/dojo/models.py
+++ django/askdjango_django_basic/basic/dojo/models.py
@@ -1,3 +1,16 @@
+import re
 from django.db import models
+from django.forms import ValidationError
+from django.utils import timezone
 
-# Create your models here.
+def lnglat_validator(value):
+  if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+    raise ValidationError('Invalid LngLat Type')
+
+class Post(models.Model):
+  title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')
+  content = models.TextField()
+  tags = models.CharField(max_length=100, blank=True)
+  lnglat = models.CharField(max_length=50, blank=True, validators=[lnglat_validator], help_text='경도, 위도 포맷으로 입력')
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
