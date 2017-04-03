597832640838c264879854f22b1d711f4ec514ba
Jinho Hyeon <neverlish@gmail.com>
2017-04-03 20:37:41 +0900

    08 - Admin

--- django/askdjango_django_basic/basic/dojo/admin.py
+++ django/askdjango_django_basic/basic/dojo/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ['id', 'title', 'content_size', 'status', 'created_at', 'updated_at']
+  actions = ['make_draft','make_published']
+
+  def content_size(self, post):
+    return mark_safe('<strong>{}글자</strong>'.format(len(post.content)))
+  content_size.short_description = '글자수'
+
+  def make_draft(self, request, queryset):
+    updated_count = queryset.update(status='d') #QuerySet.update
+    self.message_user(request, '{}건의 포스팅을 Draft 상태로 변경'.format(updated_count)) #django message framework 활용
+  make_draft.short_description = '지정 포스팅을 Draft 상태로 변경합니다.'
+
+  def make_published(self, request, queryset):
+      updated_count = queryset.update(status='p') #QuerySet.update
+      self.message_user(request, '{}건의 포스팅을 Published 상태로 변경'.format(updated_count)) #django message framework 활용
+  make_published.short_description = '지정 포스팅을 Published 상태로 변경합니다.'
+# admin.site.register(Post, PostAdmin)
--- django/askdjango_django_basic/basic/dojo/models.py
+++ django/askdjango_django_basic/basic/dojo/models.py
@@ -8,10 +8,16 @@ def lnglat_validator(value):
     raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
+  STATUS_CHOICES = (
+    ('d', 'Draft'),
+    ('p', 'Published'),
+    ('w', 'Withdrawn'),
+  )
   author = models.CharField(max_length=20)
   title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')
   content = models.TextField()
   tags = models.CharField(max_length=100, blank=True)
   lnglat = models.CharField(max_length=50, blank=True, validators=[lnglat_validator], help_text='경도, 위도 포맷으로 입력')
+  status = models.CharField(max_length=1, choices=STATUS_CHOICES)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
