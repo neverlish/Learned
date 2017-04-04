5b72f6900cae1f4dee351a9f795cdbb34bd4ab41
Jinho Hyeon <neverlish@gmail.com>
2017-04-04 18:26:38 +0900

    Express - Application

--- node/inflearn-tdd/index.js
+++ node/inflearn-tdd/index.js
@@ -1,23 +1,6 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-  } else if (req.url === '/users') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('User list');
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found');
-  }
+app.listen(3000, function() {
+  console.log('Server is running');
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
-})
