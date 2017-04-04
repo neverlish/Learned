1a4f4495fa18eac1198b55459fb80caabb737c95
Jinho Hyeon <neverlish@gmail.com>
2017-04-04 18:15:03 +0900

    Hello World

--- node/inflearn-tdd/index.js
+++ node/inflearn-tdd/index.js
@@ -1,9 +1,23 @@
-const fs = require('fs');
+const http = require('http');
 
-const data = fs.readFileSync('./data.txt', 'utf8');
-console.log(data);
+const hostname = '127.0.0.1';
+const port = 3000;
 
-fs.readFile('./data.txt', 'utf8', function(err, data) {
-  if(err) throw err;
-  console.log(data);
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+  } else if (req.url === '/users') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('User list');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 })
