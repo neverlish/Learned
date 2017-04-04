ac280dcbbabaa8d1728825c074b1757742f8e6b0
Jinho Hyeon <neverlish@gmail.com>
2017-04-04 18:26:48 +0900

    Express - Middleware

--- node/inflearn-tdd/index.js
+++ node/inflearn-tdd/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+function logger(req, res, next) {
+  console.log('i am logger');
+  next();
+}
+
+app.use(logger);
+
 app.listen(3000, function() {
   console.log('Server is running');
 });
