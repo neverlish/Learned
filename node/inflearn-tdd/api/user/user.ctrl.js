bbf9df3120dcd3dea4a862d33bd28d664a6cd27c
Jinho Hyeon <neverlish@gmail.com>
2017-04-06 00:45:42 +0900

    코드 리팩토링 - 컨트롤러 함수로 분리

--- node/inflearn-tdd/api/user/user.ctrl.js
+++ node/inflearn-tdd/api/user/user.ctrl.js
@@ -1,2 +1,59 @@
 // api 로직
+var users = [
+  {id: 1, name: 'alice'},
+  {id: 2, name: 'bek'},
+  {id: 3, name: 'cris'}
+];
 
+const index = function (req, res) {
+  req.query.limit = req.query.limit || 10;
+  const limit = parseInt(req.query.limit, 10);
+  if (Number.isNaN(limit)) {
+    return res.status(400).end();
+  }
+  res.json(users.slice(0, limit));
+}
+
+const show = function(req, res) {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+  const user = users.filter((user) => user.id === id)[0];
+  if (!user) return res.status(404).end();
+  res.json(user);
+}
+
+const destroy = (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+  users = users.filter(user => user.id !== id);
+  res.status(204).end();
+}
+
+const create = (req, res) => {
+  const name = req.body.name;
+  if (!name) return res.status(400).end();
+  const isConflict = users.filter(user => user.name === name).length
+  if (isConflict) return res.status(409).end();
+  const id = Date.now();
+  const user = {id, name};
+  users.push(user);
+  res.status(201).json(user);
+}
+
+const update = (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+
+  const name = req.body.name;
+  if (!name) return res.status(400).end();
+  const isConflict = users.filter(user => user.name === name).length
+  if (isConflict) return res.status(409).end();
+
+  const user = users.filter(user => user.id === id)[0];
+  if (!user) return res.status(404).end();
+
+  user.name = name;
+  res.json(user);
+}
+
+module.exports = {index, show, destroy, create, update};
