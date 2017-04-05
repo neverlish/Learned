ecce4950a0784ab4941fb9a296b3d740a1d51e96
Jinho Hyeon <neverlish@gmail.com>
2017-04-06 01:40:38 +0900

    코드 리팩토링 - 테스트 환경 개선

---
+++ node/inflearn-tdd/bin/www.js
@@ -0,0 +1,5 @@
+const app = require('../');
+
+app.listen(3000, () => {
+  console.log('Server is running ion port 3000!');
+})
--- node/inflearn-tdd/index.js
+++ node/inflearn-tdd/index.js
@@ -4,14 +4,12 @@ const morgan = require('morgan');
 var bodyParser = require('body-parser');
 var user = require('./api/user')
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/users', user)
-
-app.listen(3000, function() {
-  console.log('Server is running ion port 3000!');
-});
 
 module.exports = app;
