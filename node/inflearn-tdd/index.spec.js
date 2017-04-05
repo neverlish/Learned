1be7e0e4c41a4c8b542eb8bdccc3763f7f9f31ab
Jinho Hyeon <neverlish@gmail.com>
2017-04-05 18:25:48 +0900

    TDD로 하는 API 서버 개발 - 사용자 목록 조회 API 테스트 코드 만들기 1

--- node/inflearn-tdd/index.spec.js
+++ node/inflearn-tdd/index.spec.js
@@ -1,13 +1,16 @@
-const app = require('./index');
 const request = require('supertest');
+const should = require('should');
+const app = require('./index');
 
 describe('GET /users는', () => {
-  it('...', (done) => {
-    request(app)
-      .get('/users')
-      .end((err, res) => {
-        console.log(res.body);
-        done();
-      })
+  describe('성공시', () => {
+    it('유저 객체를 담은 배열로 응답한다 ', (done) => {
+      request(app)
+        .get('/users')
+        .end((err, res) => {
+          res.body.should.be.instanceOf(Array);
+          done();
+        });
+    });
   })
 })
