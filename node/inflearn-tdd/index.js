315ad97977601c9c5d1da0d6ef8ab8e7698e32b8
Jinho Hyeon <neverlish@gmail.com>
2017-04-05 18:31:50 +0900

    TDD로 하는 API 서버 개발 - 사용자 목록 조회 API 테스트 코드 만들기 2

--- node/inflearn-tdd/index.js
+++ node/inflearn-tdd/index.js
@@ -10,7 +10,12 @@ var users = [
 app.use(morgan('dev'));
 
 app.get('/users', function (req, res) {
-  res.json(users);
+  req.query.limit = req.query.limit || 10;
+  const limit = parseInt(req.query.limit, 10);
+  if (Number.isNaN(limit)) {
+    return res.status(400).end();
+  }
+  res.json(users.slice(0, limit));
 });
 
 app.listen(3000, function() {
--- node/inflearn-tdd/index.spec.js
+++ node/inflearn-tdd/index.spec.js
@@ -12,5 +12,23 @@ describe('GET /users는', () => {
           done();
         });
     });
+
+    it('최대 limit 갯수만큼 응답한다', (done) => {
+      request(app)
+        .get('/users?limit=2')
+        .end((err, res) => {
+          res.body.should.have.lengthOf(2);
+          done();
+        })
+    })
+  })
+
+  describe('실패시', () => {
+    it('limit이 숫자형이 아니면 400을 응답한다', (done) => {
+      request(app)
+        .get('/users?limit=two')
+        .expect(400)
+        .end(done);
+    })
   })
 })
