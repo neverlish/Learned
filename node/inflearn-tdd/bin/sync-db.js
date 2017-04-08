fa419adf4b386efe27feb99693a868ffa6922a1b
Jinho Hyeon <neverlish@gmail.com>
2017-04-09 00:48:10 +0900

    데이터베이스 - 환경설정

--- node/inflearn-tdd/bin/sync-db.js
+++ node/inflearn-tdd/bin/sync-db.js
@@ -1,5 +1,8 @@
 const models = require('../models');
 
 module.exports = () => {
-  return models.sequelize.sync({force: true});
+  const optiosn = {
+    force: process.env.NODE_ENV === 'test' ? true : false
+  }
+  return models.sequelize.sync(options);
 }
