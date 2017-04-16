9b110fc94e879dd7ae9db9c6a9da79e7635ef7df
Jinho Hyeon <neverlish@gmail.com>
2017-04-16 19:02:54 +0900

    4 - Write a simple form

--- django/official_tutorial_110/polls/views.py
+++ django/official_tutorial_110/polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -13,7 +14,19 @@ def detail(request, question_id):
   return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-  return HttpResponse("You're looking at question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-  return HttpResponse("You're voting on question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/detail.html', {
+      'question': question,
+      'error_message': "You didn't select a choice.",
+    })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
