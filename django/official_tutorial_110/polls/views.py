5295c8f00313bb80e72a58a55990c9ad9c637640
Jinho Hyeon <neverlish@gmail.com>
2017-04-16 12:50:07 +0900

    3 - Raise a 404 error

--- django/official_tutorial_110/polls/views.py
+++ django/official_tutorial_110/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -9,7 +9,8 @@ def index(request):
   return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-  return HttpResponse("You're looking at question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
   return HttpResponse("You're looking at question %s." % question_id)
