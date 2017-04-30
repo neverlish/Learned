8349537846bbad9c11674826678803e613bdcdad
Jinho Hyeon <neverlish@gmail.com>
2017-04-30 16:05:50 +0900

    23 - 장고 폼 유효성 검사

--- django/askdjango_django_basic/basic/blog/models.py
+++ django/askdjango_django_basic/basic/blog/models.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 def min_length_3_validator(value):
@@ -11,3 +12,17 @@ class Post(models.Model):
   ip = models.CharField(max_length=15)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+
+class GameUser(models.Model):
+  server_name = models.CharField(max_length=10,
+                                 choices = (
+                                   ('A', 'A서버'),
+                                   ('B', 'B서버'),
+                                   ('C', 'C서버'),
+                                 ))
+  username = models.CharField(max_length=20, validators=[MinLengthValidator(3)])
+
+  class Meta:
+    unique_together  = [
+      ('server_name', 'username'),
+    ]
--- django/askdjango_django_basic/basic/shop/admin.py
+++ django/askdjango_django_basic/basic/shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, GameUser
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(GameUser)
