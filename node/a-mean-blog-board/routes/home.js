496b0ed236dad192295fcdf4ac890827ae581851
Jinho Hyeon <neverlish@gmail.com>
2017-05-01 23:10:48 +0900

    Login 기능 추가

--- node/a-mean-blog-board/routes/home.js
+++ node/a-mean-blog-board/routes/home.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('../config/passport');
 
 router.get('/', function(req, res) {
   res.render('home/welcome');
@@ -8,5 +9,48 @@ router.get('/', function(req, res) {
 router.get('/about', function(req, res) {
   res.render('home/about');
 });
+
+// login
+router.get('/login', function(req, res) {
+  var username = req.flash('username')[0];
+  var errors = req.flash('errors')[0] || {};
+  res.render('home/login', {
+    username: username,
+    errors: errors
+  });
+});
+
+// post login
+router.post('/login',
+  function(req, res, next) {
+    var errors = {};
+    var isValid = true;
+    if (!req.body.username) {
+      isValid = false;
+      errors.username = 'Username is required!';
+    }
+    if (!req.body.password) {
+      isValid = false;
+      errors.password = 'Password is required!';
+    }
+
+    if (isValid) {
+      next();
+    } else {
+      req.flash('errors', erros);
+      res.redirect('/login');
+    }
+  },
+  passport.authenticate('local-login', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  })
+);
+
+// logout
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+})
 
 module.exports = router;
