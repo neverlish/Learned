6816ea0d655159a15e96692a248d9990ac0279ae
Jinho Hyeon <neverlish@gmail.com>
2017-05-03 11:08:33 +0900

    27 - Media Files - 유저가 업로드한 파일은 어떻게 관리될까요?

--- django/askdjango_django_basic/basic/dojo/models.py
+++ django/askdjango_django_basic/basic/dojo/models.py
@@ -18,6 +18,7 @@ class Post(models.Model):
   # author = models.CharField(max_length=20)
   title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')
   content = models.TextField()
+  photo = models.ImageField(blank=True, upload_to='blog/post/%Y/%m/%d')
   tags = models.CharField(max_length=100, blank=True)
   lnglat = models.CharField(max_length=50, blank=True, validators=[lnglat_validator], help_text='경도, 위도 포맷으로 입력')
   status = models.CharField(max_length=1, choices=STATUS_CHOICES)
