d17a2edbdd64ce7b8437b4f92b8c770740b215d3
Jinho Hyeon <neverlish@gmail.com>
2017-05-03 09:12:07 +0900

    26 - Static Fies - CSS/JavaScript 파일을 어떻게 관리해야 할까요?

--- django/askdjango_django_basic/basic/askdjango_django_basic/settings.py
+++ django/askdjango_django_basic/basic/askdjango_django_basic/settings.py
@@ -130,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askdjango_django_basic', 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 INTERNAL_IPS = ['127.0.0.1']
 
