bb89eb078b5cb50ae8b108bc19fbaf64444cbe61
Jinho Hyeon <neverlish@gmail.com>
2017-05-06 16:52:44 +0900

    33 - 사용자 인증 구현하기 - 회원가입, 로그인, 프로필

--- django/askdjango_django_basic/basic/shop/views.py
+++ django/askdjango_django_basic/basic/shop/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
+def signup(request):
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect(settings.LOGIN_URL)
+  else:
+    form = UserCreationForm()
+  return render(request, 'accounts/signup_form.html', {
+    'form': form
+  })
+
+@login_required
 def profile(request):
   return render(request, 'accounts/profile.html')
