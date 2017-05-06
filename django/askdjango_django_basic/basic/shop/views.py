df72e16516a6d940a4e5833e66d0117e15f88069
Jinho Hyeon <neverlish@gmail.com>
2017-05-06 17:26:17 +0900

    34 - 사용자 인증 커스텀 하기

--- django/askdjango_django_basic/basic/shop/views.py
+++ django/askdjango_django_basic/basic/shop/views.py
@@ -1,16 +1,16 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
 
 def signup(request):
   if request.method == 'POST':
-    form = UserCreationForm(request.POST)
+    form = SignupForm(request.POST)
     if form.is_valid():
       form.save()
       return redirect(settings.LOGIN_URL)
   else:
-    form = UserCreationForm()
+    form = SignupForm()
   return render(request, 'accounts/signup_form.html', {
     'form': form
   })
