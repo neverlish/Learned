bfaeb6f8573c85813c400c41c4328e0fa8b2588c
Jinho Hyeon <neverlish@gmail.com>
2017-05-18 20:18:40 +0900

    7 - Separating the Javascript - part 2

--- js/udemy-build-calculator/script.js
+++ js/udemy-build-calculator/script.js
@@ -1,3 +1,11 @@
 function calculate() {
   document.calc.output.value=eval(document.calc.output.value);
 }
+
+function erase() {
+  document.calc.output.value='';
+}
+
+function entries(s) {
+  document.calc.output.value+=s;
+}
