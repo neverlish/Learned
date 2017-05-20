77819f06c93ad24c509c522adcf6f8de49e1a741
Jinho Hyeon <neverlish@gmail.com>
2017-05-21 01:36:09 +0900

    Build your first page component with Ionic 2

--- ionic/egghead-building-apps-with-ionic2/src/pages/home/home.ts
+++ ionic/egghead-building-apps-with-ionic2/src/pages/home/home.ts
@@ -6,7 +6,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  public person = {
+    "gender": "male",
+    "name": {
+      "title": "mr",
+      "first": "konrad",
+      "last": "mann"
+    },
+    "email": "konrad.mann@example.com",
+    "phone": "0815-5579155",
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/men/36.jpg",
+      "medium": "https://ramdomuser.me/api/portraits/med/men/36.jpg",
+      "thumbnail": "https://ramdomuser.me/api/portraits/thumb/men/36.jpg"
+    }
+  };
   constructor(public navCtrl: NavController) {
 
   }
