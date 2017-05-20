042dd75525a1a5289b585f95e208571c6b983f6d
Jinho Hyeon <neverlish@gmail.com>
2017-05-21 01:51:00 +0900

    Use Sliding Gestures to Reorder an Ionic 2 List

--- ionic/egghead-building-apps-with-ionic2/src/pages/home/home.ts
+++ ionic/egghead-building-apps-with-ionic2/src/pages/home/home.ts
@@ -6,23 +6,43 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public person = {
-    "gender": "male",
-    "name": {
-      "title": "mr",
-      "first": "konrad",
-      "last": "mann"
+  public people = [
+    {
+      "gender": "male",
+      "name": {
+        "title": "mr",
+        "first": "konrad",
+        "last": "mann"
+      },
+      "email": "konrad.mann@example.com",
+      "phone": "0815-5579155",
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/36.jpg",
+        "medium": "https://ramdomuser.me/api/portraits/med/men/36.jpg",
+        "thumbnail": "https://ramdomuser.me/api/portraits/thumb/men/36.jpg"
+      }
     },
-    "email": "konrad.mann@example.com",
-    "phone": "0815-5579155",
-    "picture": {
-      "large": "https://randomuser.me/api/portraits/men/36.jpg",
-      "medium": "https://ramdomuser.me/api/portraits/med/men/36.jpg",
-      "thumbnail": "https://ramdomuser.me/api/portraits/thumb/men/36.jpg"
+    {
+      "gender": "female",
+      "name": {
+        "title": "mrs",
+        "first": "hazel",
+        "last": "anderson"
+      },
+      "email": "hazel.anderson@example.com",
+      "phone": "(065)-994-1587",
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/women/1.jpg",
+        "medium": "https://ramdomuser.me/api/portraits/med/women/1.jpg",
+        "thumbnail": "https://ramdomuser.me/api/portraits/thumb/women/1.jpg"
+      }
     }
-  };
+  ];
+  public shouldReorder = false;
   constructor(public navCtrl: NavController) {
 
   }
-
+  toggleReorder() {
+    this.shouldReorder = !this.shouldReorder;
+  }
 }
