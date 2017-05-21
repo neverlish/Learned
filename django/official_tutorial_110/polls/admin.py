e40f52805afab42edafa1109dff3f896bdb03ba8
Jinho Hyeon <neverlish@gmail.com>
2017-05-21 12:34:35 +0900

    7 - Customize the admin change list

--- django/official_tutorial_110/polls/admin.py
+++ django/official_tutorial_110/polls/admin.py
@@ -12,6 +12,9 @@ class QuestionAdmin(admin.ModelAdmin):
     ('Date information', {'fields': ['pub_date']}),
   ]
   inlines = [ChoiceInline]
+  list_display = ('question_text', 'pub_date', 'was_published_recently')
+  list_filter = ['pub_date']
+  search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
 
