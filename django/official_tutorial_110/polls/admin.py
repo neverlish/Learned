a6a81d32c60a632941e32e3c41e573f8b5871bfc
Jinho Hyeon <neverlish@gmail.com>
2017-05-21 12:33:01 +0900

    7 - Customize the admin form

--- django/official_tutorial_110/polls/admin.py
+++ django/official_tutorial_110/polls/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+  fieldsets = [
+    (None, {'fields': ['question_text']}),
+    ('Date information', {'fields': ['pub_date']}),
+  ]
+
+admin.site.register(Question, QuestionAdmin)
