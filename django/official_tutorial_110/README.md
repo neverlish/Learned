41cdf7ab51422423767aa6650dc11ef8b208b11f
Jinho Hyeon <neverlish@gmail.com>
2017-05-21 12:48:16 +0900

    [django] 공식 홈페이지 튜토리얼

---
+++ django/official_tutorial_110/README.md
@@ -0,0 +1,5 @@
+Address:
+  KOR: http://django-document-korean.readthedocs.io/ko/master/intro/tutorial01.html
+  ENG: https://docs.djangoproject.com/en/1.10/intro/tutorial01/
+
+Pull Request: https://github.com/neverlish/neverlish/pull/19
---
+++ django/official_tutorial_110/polls/models.py
@@ -0,0 +1,29 @@
+import datetime
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
+
+@python_2_unicode_compatible
+class Question(models.Model):
+  question_text = models.CharField(max_length=200)
+  pub_date = models.DateTimeField('date published')
+
+  def __str__(self):
+    return self.question_text
+
+  def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
+  was_published_recently.admin_order_field = 'pub_date'
+  was_published_recently.boolean = True
+  was_published_recently.short_description = 'Published recently?'
+
+@python_2_unicode_compatible
+class Choice(models.Model):
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
+  choice_text = models.CharField(max_length=200)
+  votes = models.IntegerField(default=0)
+
+  def __str__(self):
+    return self.choice_text
