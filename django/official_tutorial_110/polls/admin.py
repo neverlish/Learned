c988f2635eba41c26054049c01547d951a0aed0a
Jinho Hyeon <neverlish@gmail.com>
2017-05-21 12:34:11 +0900

    7 - Adding related objects

--- django/official_tutorial_110/polls/admin.py
+++ django/official_tutorial_110/polls/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
   fieldsets = [
     (None, {'fields': ['question_text']}),
     ('Date information', {'fields': ['pub_date']}),
   ]
+  inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
