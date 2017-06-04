f17ada3bca4fd55029e8ba1a6b97514b46572b0f
Jinho Hyeon <neverlish@gmail.com>
2017-06-04 10:22:54 +0900

    05 - The Anatomy of an Angular 2+ App
    ng g component crib-listing

--- angular2/udemy-getting-started-with-angular2/src/app/app.module.ts
+++ angular2/udemy-getting-started-with-angular2/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CribListingComponent } from './crib-listing/crib-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CribListingComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular2/udemy-getting-started-with-angular2/src/app/crib-listing/crib-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crib-listing',
+  templateUrl: './crib-listing.component.html',
+  styleUrls: ['./crib-listing.component.css']
+})
+export class CribListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
