237cb1611217e1beefe9ba5791d67821dbf8762e
Jinho Hyeon <neverlish@gmail.com>
2017-06-16 00:24:05 +0900

    3 자바스크립트 데이터 타입과 연산자 - 1 자바스크립트 기본 타입

---
+++ js/hanbit-inside-javascript/3/02.js
@@ -0,0 +1,5 @@
+// 자바스크립트 나눗셈 연산
+var num = 5/2;
+
+console.log(num); // 출력값 2.5
+console.log(Math.floor(num)); // 출력값 2
---
+++ js/hanbit-inside-javascript/3/03.js
@@ -0,0 +1,9 @@
+// 자바스크립트 문자열 예제
+
+ // str 문자열 생성
+ var str = 'test';
+ console.log(str[0], str[1], str[2], str[3]); // (출력값) t e s t
+
+ // 문자열의 첫 글자를 대문자로 변경?
+ str[0] = 'T';
+ console.log(str); // (출력값) test
