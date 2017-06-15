3cb02e08625a2f714a14ee858eb1fe39c8399251
Jinho Hyeon <neverlish@gmail.com>
2017-06-16 01:15:00 +0900

    3 - 7 연산자

---
+++ js/hanbit-inside-javascript/3/28.js
@@ -0,0 +1,10 @@
+// + 연산자 예제
+var add1 = 1 + 2;
+var add2 = 'my ' + 'string';
+var add3 = 1 + 'string';
+var add4 = 'string' + 2;
+
+console.log(add1); // (출력값) 3
+console.log(add2); // (출력값) my string
+console.log(add3); // (출력값) 1string
+console.log(add4); // (출력값) string2
---
+++ js/hanbit-inside-javascript/3/30.js
@@ -0,0 +1,11 @@
+// !! 연산자 활용을 통한 불린값 변환
+console.log(!!0); // (출력값) false
+console.log(!!1); // (출력값) true
+console.log(!!'string'); // (출력값) true 
+console.log(!!''); // (출력값) false
+console.log(!!true); // (출력값) true
+console.log(!!false); // (출력값) false
+console.log(!!null); // (출력값) false
+console.log(!!undefined); // (출력값) false
+console.log(!!{}); // (출력값) true
+console.log(!![1,2,3]); // (출력값) true
