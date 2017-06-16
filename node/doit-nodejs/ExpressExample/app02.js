6bd8d820a0242b8a03cf362d0c4d80f8467b5cdd
Jinho Hyeon <neverlish@gmail.com>
2017-06-16 12:35:19 +0900

    05 웹 서버 만들기 - 2 익스프레스로 웹 서버 만들기

---
+++ node/doit-nodejs/ExpressExample/app02.js
@@ -0,0 +1,16 @@
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.use(function(req, res, next) {
+  console.log('첫 번째 미들웨어에서 요청을 처리함.');
+
+  res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
+  res.end('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+});
+
+http.createServer(app).listen(3000, function() {
+  console.log('Express 서버가 3000번 포트에서 시작됨.');
+})
