ab662eba688a03347e0e8bae2b69cd31360cbb19
Jinho Hyeon <neverlish@gmail.com>
2017-06-18 15:31:26 +0900

    02 Core Concepts - 07 Controllers and the MVC Pattern

--- node/wesbos-learnnode/routes/index.js
+++ node/wesbos-learnnode/routes/index.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  const wes = { name: 'Wes', age: 100, cool: true };
-  // res.send('Hey! It works!');
-  // res.json(wes);
-  // res.send(req.query.name);
-  // res.json(req.query);
-  res.render('hello', {
-    name: 'wes',
-    dog: req.query.dog,
-    title: 'I love food'
-  });
-});
+router.get('/', storeController.homePage);
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
