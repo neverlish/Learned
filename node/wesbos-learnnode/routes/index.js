a44fea94976d391da74bead8667997a5ad86c231
Jinho Hyeon <neverlish@gmail.com>
2017-06-18 14:30:35 +0900

    02 Core Concepts - 04 Routing

--- node/wesbos-learnnode/routes/index.js
+++ node/wesbos-learnnode/routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = { name: 'Wes', age: 100, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(wes);
+  // res.send(req.query.name);
+  res.json(req.query);
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+})
 
 module.exports = router;
