d1c5c14df4a40c0eb06d00642fa04367b7b8e583
Jinho Hyeon <neverlish@gmail.com>
2017-06-18 14:27:08 +0900

    01 Introduction and Setup - 02 Setting up Mongo DB

--- node/wesbos-learnnode/start.js
+++ node/wesbos-learnnode/start.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 
-// Make sure we are running node 7.6+
-const [major, minor] = process.versions.node.split('.').map(parseFloat);
-if (major < 7 || (major === 7 && minor <= 5)) {
-  console.log('🛑 🌮 🐶 💪 💩\nHey You! \n\t ya you! \n\t\tBuster! \n\tYou\'re on an older version of node that doesn\'t support the latest and greatest things we are learning (Async + Await)! Please go to nodejs.org and download version 7.6 or greater. 👌\n ');
-  process.exit();
-}
-
 // import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
 // Connect to our Database and handle an bad connections
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.MLAB_LEARNNODE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
