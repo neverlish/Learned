0e17447b898043f03581f857dab0fb3dd880ccca
Jinho Hyeon <neverlish@gmail.com>
2017-06-19 18:39:11 +0900

    04 Control Flow - 13 Querying our Database for Stores

--- node/wesbos-learnnode/controllers/storeController.js
+++ node/wesbos-learnnode/controllers/storeController.js
@@ -14,3 +14,10 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`/stores/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find();
+  console.log(stores);
+  res.render('stores', { title: 'Stores', stores: stores });
+}
