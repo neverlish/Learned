a5b12db9dc187206e866d155001940b15f4be2db
Jinho Hyeon <neverlish@gmail.com>
2017-06-19 14:21:16 +0900

    04 Control Flow - 12 Flash Messages

--- node/wesbos-learnnode/controllers/storeController.js
+++ node/wesbos-learnnode/controllers/storeController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 }
 
@@ -11,7 +10,7 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/stores/${store.slug}`);
 };
