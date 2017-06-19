9bfb54233afd383a2a192b8b40e9d9c857485e7f
Jinho Hyeon <neverlish@gmail.com>
2017-06-19 11:53:31 +0900

    03 Database Storage - 10 Saving Stores and using Mixins

--- node/wesbos-learnnode/controllers/storeController.js
+++ node/wesbos-learnnode/controllers/storeController.js
@@ -2,3 +2,11 @@ exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
 }
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
+};
--- node/wesbos-learnnode/routes/index.js
+++ node/wesbos-learnnode/routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
