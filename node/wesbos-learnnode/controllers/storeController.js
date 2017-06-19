2674fea88560c4d38d7b4e05b96b6879589793ac
Jinho Hyeon <neverlish@gmail.com>
2017-06-19 13:55:53 +0900

    04 Control Flow - 11 Using Async Await

--- node/wesbos-learnnode/controllers/storeController.js
+++ node/wesbos-learnnode/controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
@@ -7,6 +10,8 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  res.redirect('/');
 };
