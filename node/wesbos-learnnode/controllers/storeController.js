64db9e0caf97ab6fb70d0a23e3bac05b367fd47c
Jinho Hyeon <neverlish@gmail.com>
2017-06-20 17:37:58 +0900

    04 Control Flow - 14 Creating an Editing Flow for Stores

--- node/wesbos-learnnode/controllers/storeController.js
+++ node/wesbos-learnnode/controllers/storeController.js
@@ -21,3 +21,23 @@ exports.getStores = async (req, res) => {
   console.log(stores);
   res.render('stores', { title: 'Stores', stores: stores });
 }
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. confirm they are the owner of the store
+  // TODO
+  // 3. Render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store: store });
+};
+
+exports.updateStore = async (req, res) => {
+  // find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+  // Redirect them the store and tell them it worked
+}
