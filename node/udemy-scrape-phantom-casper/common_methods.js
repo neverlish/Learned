fe8652b5aba4773873b46d15610f5a338d16f96a
Jinho Hyeon <neverlish@gmail.com>
2017-06-20 17:36:17 +0900

    3 Getting Started with CasperJS - 08 Setting Up a Project

---
+++ node/udemy-scrape-phantom-casper/common_methods.js
@@ -0,0 +1,13 @@
+var casper = require('casper').create();
+
+casper.start('http://en.wikipedia.org', function() {
+  this.echo(this.getTitle());
+});
+
+casper.then(function() {
+  this.echo(this.getCurrentUrl());
+})
+
+casper.run(function() {
+  this.echo('Done').exit();
+});
