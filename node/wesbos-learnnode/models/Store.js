562a415307ccbca78c710b7f94bfee469ff2c2e0
Jinho Hyeon <neverlish@gmail.com>
2017-06-22 11:46:31 +0900

    07 Custom Queries and Hooks - 20 Using Pre-Save hooks to make Unique Slugs

--- node/wesbos-learnnode/models/Store.js
+++ node/wesbos-learnnode/models/Store.js
@@ -35,12 +35,18 @@ const storeSchema = new mongoose.Schema({
   photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return;
   }
   this.slug = slug(this.name);
+  // find other store that have a slug of wes, wes-1, wes-2
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
   next();
   // TODO make more resilient so slugs are unique
 });
