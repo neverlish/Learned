238cbee760b4ddceb50c2d2f2343fd7ed21da3c9
Jinho Hyeon <neverlish@gmail.com>
2017-06-24 22:21:11 +0900

    10 Javascript Working with Arrays

--- js/udemy-learn-js-for-beginners/app.js
+++ js/udemy-learn-js-for-beginners/app.js
@@ -62,3 +62,21 @@ d += myFun(6); // adding 60
 console.log(d);
 d += myFun(); // adding 30
 console.log(d);
+
+
+var myArray = ['one', 'two', 3, 4, 'five', 'six', 7, 8, 9];
+console.log(myArray.sort());
+console.log(myArray.reverse());
+myArray.push('ten');
+console.log(myArray);
+myArray.pop();
+console.log(myArray);
+var a = myArray.shift();
+console.log(a);
+console.log(myArray);
+myArray.unshift('ten');
+console.log(myArray);
+myArray.splice(0, 2);
+console.log(myArray);
+myArray.splice(3, 0, 'added 1', 'added 2');
+console.log(myArray);
