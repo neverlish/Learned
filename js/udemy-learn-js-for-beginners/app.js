39ada95332a5b2e3467388c67b15b3cca575c723
Jinho Hyeon <neverlish@gmail.com>
2017-06-24 20:06:12 +0900

    03 Introduction to Javascript Variables

---
+++ js/udemy-learn-js-for-beginners/app.js
@@ -0,0 +1,12 @@
+var firstName = 'laurence'; // String
+var age = 10; // Integer
+var boo = false; // Boolean
+console.log(age);
+age = age + 10;
+console.log(age);
+
+console.log(lastName);
+var lastName;
+console.log(lastName);
+lastName = 'Javascript';
+console.log(lastName);
