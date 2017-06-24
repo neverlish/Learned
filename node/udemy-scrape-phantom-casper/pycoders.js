075a9c8e746532b91a4d0e861cd71da97e73d3bd
Jinho Hyeon <neverlish@gmail.com>
2017-06-25 01:22:45 +0900

    5 Scraping Javacsript R endered Web Pages - 16 Scraping JS-Rendered - Part 3

--- node/udemy-scrape-phantom-casper/pycoders.js
+++ node/udemy-scrape-phantom-casper/pycoders.js
@@ -20,6 +20,11 @@ var link = [];
 var title = [];
 var date = [];
 
+function generateTable() {
+  var data;
+  return data = "<tr>\n <td>" + date.join('\n') + '</td>\n <td>' + title.join('\n') + '</td>\n <td>' + link.join('\n') + '</td>\n </tr>';
+}
+
 function getLink() {
   var link = $('.campaign a');
   return _.map(link, function(e) {
@@ -30,14 +35,14 @@ function getLink() {
 function getTitle() {
   var title = $('.campaign a');
   return _.map(title, function(e) {
-    return e.innerHTML;
+    return e.innerHTML.replace(/\:.*$/g,'');
   });
 };
 
 function getDate() {
   var date = $('.campaign');
   return _.map(date, function(e) {
-    return e.innerText;
+    return e.innerText.replace(/\-.*$/g,'');
   });
 };
 
@@ -58,9 +63,18 @@ casper.then(function() {
 });
 
 casper.run(function() {
-  this.echo(link.length + ' links found: ');
-  // this.echo(' - ' + link.join('\n - '));
-  // this.echo(' - ' + title.join('\n - '));
-  this.echo(' - ' + date.join('\n - ')).exit();
+  var html;
+  html = '<table><tr><td>Date</td><td>Title</td><td>Link</td></tr>';
+  html = html + generateTable();
+  html += '</table>';
+  fs.write('data.html', html, 'w');
+  this.echo('\n Execution terminated').exit();
 });
+
+// casper.run(function() {
+//   this.echo(link.length + ' links found: ');
+//   // this.echo(' - ' + link.join('\n - '));
+//   this.echo(' - ' + title.join('\n - '));
+//   this.echo(' - ' + date.join('\n - ')).exit();
+// });
 
