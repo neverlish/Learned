2e5ee8f171328c04be9b93a423311672a79d8c14
Jinho Hyeon <neverlish@gmail.com>
2017-06-24 20:17:26 +0900

    04 Variables and Operators in Javascript

--- js/udemy-learn-js-for-beginners/app.js
+++ js/udemy-learn-js-for-beginners/app.js
@@ -1,12 +1,31 @@
 var firstName = 'laurence'; // String
 var age = 10; // Integer
+var age2 = 5;
+var courseName = 'Javascript'; // String
+var fiv = '5';
 var boo = false; // Boolean
+
+console.log(age + fiv);
+
+/*
+console.log(firstName);
+firstName += courseName;
+console.log(firstName);
+
 console.log(age);
 age = age + 10;
 console.log(age);
-
-console.log(lastName);
-var lastName;
-console.log(lastName);
-lastName = 'Javascript';
-console.log(lastName);
+age++;
+console.log(age);
+age--;
+console.log(age);
+age = age + age2;
+console.log(age);
+console.log(age-20);
+console.log(age*4);
+console.log(age/5);
+age += 10;
+console.log(age);
+age -= 10;
+console.log(age);
+*/
