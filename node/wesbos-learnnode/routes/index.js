313fdc7b5dbd0fbe980cac6b35dc0de8153194ef
Jinho Hyeon <neverlish@gmail.com>
2017-06-28 11:43:25 +0900

    08 User Accounts and Authentication - 23 Creating User Accounts

--- node/wesbos-learnnode/routes/index.js
+++ node/wesbos-learnnode/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -22,5 +23,12 @@ router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+// 1. Validate the registration data
+// 2. register the user
+// 3. we need to log them in
+router.post('/register', userController.validateRegister);
 
 module.exports = router;
