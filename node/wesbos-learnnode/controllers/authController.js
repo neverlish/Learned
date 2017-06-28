6f1335981d0c5b1c2fbbea2f36d85da9f0a2dafb
Jinho Hyeon <neverlish@gmail.com>
2017-06-28 13:21:08 +0900

    08 User Accounts and Authentication - 25 Virtual Fields, Login:Logout middleware and Protecting Routes

--- node/wesbos-learnnode/controllers/authController.js
+++ node/wesbos-learnnode/controllers/authController.js
@@ -6,3 +6,19 @@ exports.login = passport.authenticate('local', {
   successRedirect: '/',
   successFlash: 'You are now logged in!'
 });
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'You are now logged out! 👏');
+  res.redirect('/');
+}
+
+exports.isLoggedIn = (req, res, next) => {
+  // first check if the user is authenticated
+  if (req.isAuthenticated()) {
+    next(); // carry on! They are logged in!
+    return;
+  }
+  req.flash('error', 'Oops you must be logged in to do that!');
+  res.redirect('/login');
+};
--- node/wesbos-learnnode/routes/index.js
+++ node/wesbos-learnnode/routes/index.js
@@ -8,7 +8,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 router.post('/add', 
   storeController.upload, 
   catchErrors(storeController.resize), 
@@ -26,6 +26,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 // 1. Validate the registration data
 // 2. register the user
@@ -35,5 +36,7 @@ router.post('/register',
   userController.register,
   authController.login
 );
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
