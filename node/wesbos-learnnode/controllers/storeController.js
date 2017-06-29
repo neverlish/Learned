f3980dbdbde0e729ab2c68a42bd97eb6201f4ed7
Jinho Hyeon <neverlish@gmail.com>
2017-06-29 20:39:07 +0900

    09 Advanced-Email and Permission - 29 Locking down our application with User Permissions

--- node/wesbos-learnnode/controllers/storeController.js
+++ node/wesbos-learnnode/controllers/storeController.js
@@ -43,6 +43,7 @@ exports.resize = async (req, res, next) => {
 };
 
 exports.createStore = async (req, res) => {
+  req.body.author = req.user._id;
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`/stores/${store.slug}`);
@@ -55,11 +56,17 @@ exports.getStores = async (req, res) => {
   res.render('stores', { title: 'Stores', stores: stores });
 }
 
+const confirmOwner = (store, user) => {
+  if(!store.author.equals(user._id)) {
+    throw Error('You must own a store in order to edit it!');
+  }
+};
+
 exports.editStore = async (req, res) => {
   // 1. Find the store given the ID
   const store = await Store.findOne({ _id: req.params.id });
   // 2. confirm they are the owner of the store
-  // TODO
+  confirmOwner(store, req.user);
   // 3. Render out the edit form so the user can update their store
   res.render('editStore', { title: `Edit ${store.name}`, store: store });
 };
@@ -78,7 +85,7 @@ exports.updateStore = async (req, res) => {
 }
 
 exports.getStoreBySlug = async (req, res, next) => {
-  const store = await Store.findOne({ slug: req.params.slug });
+  const store = await Store.findOne({ slug: req.params.slug }).populate('author');
   if (!store) return next();
   res.render('store', { store, title: store.name });
 }
--- node/wesbos-learnnode/models/Store.js
+++ node/wesbos-learnnode/models/Store.js
@@ -32,7 +32,12 @@ const storeSchema = new mongoose.Schema({
       required: 'You must supply an address!'
     }
   },
-  photo: String
+  photo: String,
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: 'You must supply an author'
+  }
 });
 
 storeSchema.pre('save', async function(next) {
