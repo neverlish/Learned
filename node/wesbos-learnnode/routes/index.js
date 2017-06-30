d1888d627fece20c967f07f0753cf5a44988f02e
Jinho Hyeon <neverlish@gmail.com>
2017-06-30 16:14:43 +0900

    11 Ajax REST API 2 - 33 Creating a Geospatial Ajax Endpoint

--- node/wesbos-learnnode/routes/index.js
+++ node/wesbos-learnnode/routes/index.js
@@ -52,5 +52,6 @@ router.post('/account/reset/:token',
   API
 */
 router.get('/api/search', catchErrors(storeController.searchStores));
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
 
 module.exports = router;
