6bc26f516de8d5e947063860ef3e846d765e9f8e
Jinho Hyeon <neverlish@gmail.com>
2017-07-04 11:35:33 +0900

    13 Advanced Relationships + Aggregations - 37 Adding a Reviews Data Model

--- node/wesbos-learnnode/routes/index.js
+++ node/wesbos-learnnode/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -49,6 +50,10 @@ router.post('/account/reset/:token',
 );
 router.get('/map', storeController.mapPage);
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+router.post('/reviews/:id', 
+  authController.isLoggedIn, 
+  catchErrors(reviewController.addReview)
+);
 
 /*
   API
