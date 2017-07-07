873523e464b23b313d8457bd766e53eda2afd2d0
Jinho Hyeon <neverlish@gmail.com>
2017-07-07 11:08:31 +0900

    2 Getting Started

---
+++ tsc/udemy-learn-basic-in-refreshing-way/2.js
@@ -0,0 +1,6 @@
+"use strict";
+// tsc hero.ts
+var hello = function (name) {
+    console.log('hello ' + name);
+};
+hello('hero');
---
+++ tsc/udemy-learn-basic-in-refreshing-way/2.ts
@@ -0,0 +1,7 @@
+// tsc hero.ts
+
+const hello = (name: string) => {
+  console.log('hello ' + name);
+}
+
+hello('hero');
