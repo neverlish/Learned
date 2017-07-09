51dc70aef1758e93961c08acb44b470a92e5ec8f
Jinho Hyeon <neverlish@gmail.com>
2017-07-09 09:07:46 +0900

    3 User Login App - 14 User Registration - Part 2

--- node/udemy-projects-in-nodejs/loginApp/controllers/user.controller.js
+++ node/udemy-projects-in-nodejs/loginApp/controllers/user.controller.js
@@ -1,5 +1,18 @@
 var User = require('../models/User.model');
 
 exports.register = function(req, res) {
+  var newUser = new User();
   
-}
+  newUser.username = req.body.username;
+  newUser.email = req.body.email;
+  newUser.password = req.body.password;
+
+  newUser.save(function(err, user) {
+    if(err) {
+      console.log(err.message);
+    } else {
+      console.log(user);
+      res.redirect('/signup.html');
+    }
+  });
+};
