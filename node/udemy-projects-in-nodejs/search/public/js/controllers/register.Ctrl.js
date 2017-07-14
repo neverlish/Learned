baa92a706352aff855a4792f504bc3ceb5932cdb
Jinho Hyeon <neverlish@gmail.com>
2017-07-14 10:27:58 +0900

    5 Search Engine App - 34 Angular Registration Setup

--- node/udemy-projects-in-nodejs/search/public/js/controllers/register.Ctrl.js
+++ node/udemy-projects-in-nodejs/search/public/js/controllers/register.Ctrl.js
@@ -5,9 +5,40 @@
     .module('app')
     .controller('RegisterCtrl', RegisterCtrl);
   
-  RegisterCtrl.$inject = [];
+  RegisterCtrl.$inject = ['$scope', '$state', '$alert', 'Auth'];
 
-  function RegisterCtrl() {
-    
+  function RegisterCtrl($scope, $state, $alert, Auth) {
+    $scope.error = false;
+
+    $scope.register = function(form) {
+      var user = {
+        username: $scope.username,
+        password: $scope.password,
+        password2: $scope.password2,
+        email: $scope.email
+      }
+
+      Auth
+        .register(user)
+        // .success(function() { // deprecated
+        .then(function() {
+          console.log('User registered successfully');
+          $state.go('login');
+          $alert({
+            title: 'Congratulations',
+            content: 'Your account has been created. ' + 'You may now login',
+            placement: 'top-right',
+            container: '.alertContainer',
+            type: 'success',
+            duration: 6
+          })
+        })
+        // .error(function() { // deprecated
+        .catch(function() {
+          $scope.error = true;
+          $scope.errorMessage = 'Something went wrong'
+        });
+        
+    }
   }
 })();
---
+++ node/udemy-projects-in-nodejs/search/public/js/services/auth.srvc.js
@@ -0,0 +1,49 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .factory('Auth', Auth);
+
+  Auth.$inject = ['$http'];
+
+  function Auth($http) {
+    var loggedIn = false;
+    var currentUser = {};
+
+    return {
+      register: register,
+      login: login,
+      isLoggedIn: isLoggedIn,
+      getUserStatus: getUserStatus
+    }
+
+    function isLoggedIn() {
+      if (loggedIn) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function getUserStatus() {
+      return loggedIn;
+    }
+    
+    function register(user) {
+      return $http.post('/users/register', user);
+    }
+
+    function login(user) {
+      return $http
+        .post('/users/login', user)
+        .success(function(data) {
+          loggedIn = true;
+        })
+        .error(function(data) {
+          console.log('error occured');
+          loggedIn = false;
+        });
+    }
+  }
+})();
