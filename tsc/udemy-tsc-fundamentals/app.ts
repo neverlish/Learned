ca5a7648cbff4d2d739de15ccb7c83f3b1af00c5
Jinho Hyeon <neverlish@gmail.com>
2017-07-24 17:12:34 +0900

    04 Working with Enums and Arrays

--- tsc/udemy-tsc-fundamentals/app.ts
+++ tsc/udemy-tsc-fundamentals/app.ts
@@ -1,6 +1,20 @@
-console.log('Hello ExecuteAutomation');
+enum LanguagesKnown { English, Hindi, Tamil }
 
-let a: boolean = true
-let b: string = 'Karthik'
+let student = {
+  Name: 'Karthik',
+  Age: 30,
+  Phone: 2342342,
+  Language: LanguagesKnown[LanguagesKnown.Tamil]
+}
 
-var k: Number = 123421321
+let studentsList = [
+  {Name: 'Prashanth', Age: 26, Phone: 23423423, Language: LanguagesKnown[LanguagesKnown.English]},
+  {Name: 'Shree', Age: 27, Phone: 2454545, Language: LanguagesKnown[LanguagesKnown.Hindi]}
+]
+
+studentsList.push(student)
+
+for (var index = 0; index < studentsList.length; index++) {
+  var element = studentsList[index];
+  console.log('Age:' + element.Age + ' with Name ' + element.Name + ' has Phone:' + element.Phone + ' knows language:' + element.Language)
+}
