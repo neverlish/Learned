fcab26a70799d30e86f7ad91e1646220cf9c5125
Jinho Hyeon <neverlish@gmail.com>
2017-07-24 17:34:08 +0900

    05 Working with Functions (Basic)

--- tsc/udemy-tsc-fundamentals/app.ts
+++ tsc/udemy-tsc-fundamentals/app.ts
@@ -14,7 +14,31 @@ let studentsList = [
 
 studentsList.push(student)
 
-for (var index = 0; index < studentsList.length; index++) {
-  var element = studentsList[index];
-  console.log('Age:' + element.Age + ' with Name ' + element.Name + ' has Phone:' + element.Phone + ' knows language:' + element.Language)
+// for (var index = 0; index < studentsList.length; index++) {
+//   var element = studentsList[index];
+//   console.log('Age:' + element.Age + ' with Name ' + element.Name + ' has Phone:' + element.Phone + ' knows language:' + element.Language)
+// }
+
+function GetStudentsList(students: any[]) {
+  students.forEach(element => {
+    console.log('Age:' + element.Age + ' with Name ' + element.Name + ' has Phone:' + element.Phone + ' knows language:' + element.Language)
+  });
 }
+
+GetStudentsList(studentsList)
+
+// Rest Parameters
+function GetNumbers(...nums: number[]) {
+  nums.forEach(element => {
+    console.log('Number: ' + element);
+  });
+}
+
+GetNumbers(1,2,3,4,5,6)
+
+// Default Parameters
+function GetInfo(info: string = 'Happy') {
+  console.log(info);
+}
+
+GetInfo('Very Happy');
