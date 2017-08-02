01732141c1cfdd3f2c4b45e75e1de8fe56a10a1e
Jinho Hyeon <neverlish@gmail.com>
2017-08-02 10:12:48 +0900

    07 익스프레스 프로젝트를 모듈화하기 - 4 UI 라이브러리로 웹 문서 예쁘게 꾸미기

---
+++ node/doit-nodejs/DefaultExample/app2.js
@@ -0,0 +1,53 @@
+//===== 모듈 불러들이기 =====//
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var config = require('./config');
+var database = require('./database/database');
+var route_loader = require('./routes/route_loader');
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var expressErrorHandler = require('express-error-handler');
+
+//===== Express 서버 객체 만들기 =====//
+var app = express();
+
+
+//===== 서버 변수 설정 및 static으로 public 폴더 설정  =====//
+console.log('config.server_port : %d', config.server_port);
+app.set('port', process.env.PORT || 3000);
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//===== body-parser, cookie-parser, express-session 사용 설정 =====//
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cookieParser());
+app.use(expressSession({
+	secret:'my key',
+	resave:true,
+	saveUninitialized:true
+}));
+
+//라우팅 정보를 읽어들여 라우팅 설정
+route_loader.init(app);
+
+//===== 404 에러 페이지 처리 =====//
+var errorHandler = expressErrorHandler({
+ static: {
+   '404': './public/404.html'
+ }
+});
+
+app.use( expressErrorHandler.httpError(404) );
+app.use( errorHandler );
+
+
+//===== 서버 시작 =====//
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
+
+  database.init(app, config);
+});
