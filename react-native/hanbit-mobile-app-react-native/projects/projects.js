8c8ab5e214dcc5dde36ce4812b41b18aa145c8df
Jinho Hyeon <neverlish@gmail.com>
2017-08-06 20:31:12 +0900

    05 스타일 - 3 위치 잡기 및 레이아웃 디자인하기 - 1 Flexbox를 이용한 레이아웃

--- react-native/hanbit-mobile-app-react-native/projects/projects.js
+++ react-native/hanbit-mobile-app-react-native/projects/projects.js
@@ -4,5 +4,6 @@ import SimpleList from './components/bestsellers/SimpleList'
 import BookList from './components/bestsellers/BookList'
 import BookListV2 from './components/bestsellers/BookListV2'
 import Newsflash from './components/crossplatform/Newsflash'
+import FlexDemo from './components/styles/FlexDemo'
 
-export default Newsflash;
+export default FlexDemo;
