f9a56e7838d3db36d0471a614301b34e53dfff2e
Jinho Hyeon <neverlish@gmail.com>
2017-08-11 18:11:56 +0900

    11 JSON-RPC 서버 만들기 - 1 JSON-RPC를 웹 서버에 적용하기 - echo 함수의 오류 테스트하기

--- node/doit-nodejs/JsonRpcExample/handlers/handler_info.js
+++ node/doit-nodejs/JsonRpcExample/handlers/handler_info.js
@@ -1,7 +1,8 @@
 console.log('handler_info 파일 로딩됨.');
 
 var handler_info = [
-  {file: './echo', method: 'echo'}
+  {file: './echo', method: 'echo'},
+  {file: './echo_error', method: 'echo_error'}
 ];
 
 module.exports = handler_info;
