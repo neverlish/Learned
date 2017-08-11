9bf56dbf3c9b7744d933814a0bf0af7bd31a82f1
Jinho Hyeon <neverlish@gmail.com>
2017-08-12 01:00:28 +0900

    11 JSON-RPC 서버 만들기 - 2 계산기 모듈 추가하여 실행하기

--- node/doit-nodejs/JsonRpcExample/handlers/handler_info.js
+++ node/doit-nodejs/JsonRpcExample/handlers/handler_info.js
@@ -2,7 +2,8 @@ console.log('handler_info 파일 로딩됨.');
 
 var handler_info = [
   {file: './echo', method: 'echo'},
-  {file: './echo_error', method: 'echo_error'}
+  {file: './echo_error', method: 'echo_error'},
+  {file: './add', method: 'add'}
 ];
 
 module.exports = handler_info;
