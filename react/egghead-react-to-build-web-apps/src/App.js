1873f20705fef19d237729e3886b7b81a320fba6
Jinho Hyeon <neverlish@gmail.com>
2017-08-13 22:06:52 +0900

    15 Compose React Component Behavior with Higher Order Components

--- react/egghead-react-to-build-web-apps/src/App.js
+++ react/egghead-react-to-build-web-apps/src/App.js
@@ -1,35 +1,54 @@
 import React from 'react';
 
-class App extends React.Component {
+const HOC = (InnerComponent) => class extends React.Component {
   constructor() {
     super();
-    this.state = {items: []}
+    this.state = {count: 0}
+  }
+  update() {
+    this.setState({count: this.state.count + 1})
   }
   componentWillMount() {
-    fetch('http://swapi.co/api/people/?format=json')
-      .then(response => response.json())
-      .then(({results: items}) => this.setState({items}))
-  }
-  filter(e) {
-    this.setState({filter: e.target.value})
+    console.log('will mount')
   }
   render() {
-    let items = this.state.items;
-    if (this.state.filter) {
-      items = items.filter(item => 
-        item.name.toLowerCase().includes(this.state.filter.toLowerCase()))
-    }
+    return (
+      <InnerComponent
+        {...this.props}
+        {...this.state}
+        update={this.update.bind(this)}
+      />
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
     return (
       <div>
-        <input type='text' onChange={this.filter.bind(this)}/>
-        {items.map(item => 
-          // <h4 key={item.name}>{item.name}</h4>)}
-          <Person key={item.name} person={item}/>)}
+        <Button>button</Button>
+        <hr/>
+        <LabelHOC>label</LabelHOC>
       </div>
     )
   }
 }
 
-const Person = (props) => <h4>{props.person.name}</h4>
+const Button = HOC((props) => 
+  <button onClick={props.update}>{props.children} - {props.count}</button>
+)
+
+class Label extends React.Component {
+  componentWillMount() {
+    console.log('label will mount')
+  }
+  render() {
+    return (
+      <label onMouseOver={this.props.update}>{this.props.children} - {this.props.count}</label>
+    )
+  }
+}
+
+const LabelHOC = HOC(Label)
 
 export default App
