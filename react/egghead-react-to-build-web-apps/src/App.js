d7422a2929f16282a374dcf19263caaddc09fbd7
Jinho Hyeon <neverlish@gmail.com>
2017-08-13 23:07:38 +0900

    18 Understand React.Children Utilities

--- react/egghead-react-to-build-web-apps/src/App.js
+++ react/egghead-react-to-build-web-apps/src/App.js
@@ -1,42 +1,26 @@
 import React from 'react';
-import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '/* add your jsx here */',
-      output: ''
-    }
-  }
-  update(e) {
-    let code = e.target.value;
-    try {
-      this.setState({
-        output: window.Babel
-          .transform(code, {presets: ['es2015', 'react']})
-          .code
-          , err: ''
-      })
-    }
-    catch(err) {
-      this.setState({err: err.message})
-    }
-  }
   render() {
     return (
-      <div>
-        <header>{this.state.err}</header>
-        <div className='container'>
-          <textarea
-            onChange={this.update.bind(this)}
-            defaultValue={this.state.input}/>
-          <pre>
-            {this.state.output}
-          </pre>
-        </div>
-      </div>
+      <Parent>
+        <div className='childA'></div>
+        <div className='childB'></div>
+      </Parent>
     )
+  }
+}
+
+class Parent extends React.Component {
+  render() {
+    // let items = this.props.children.map(child => child) // children 이 하나 일때는 에러가 뜸
+    // let items = React.Children.map(this.props.children, child=>child)
+    // let items = React.Children.toArray(this.props.children)
+    let items = React.Children
+      .forEach(this.props.children, child => console.log(child.props.className))
+    // let items = React.Children.only(this.props.children) // children 이 2개일 때 에러가 뜸
+    console.log(items)
+    return null
   }
 }
 
