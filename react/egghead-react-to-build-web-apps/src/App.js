26c9437b02957d8cfc0d37171810846a0e53a83e
Jinho Hyeon <neverlish@gmail.com>
2017-08-13 23:30:27 +0900

    20 Write More Reusable React Components with Composable APIs

--- react/egghead-react-to-build-web-apps/src/App.js
+++ react/egghead-react-to-build-web-apps/src/App.js
@@ -1,39 +1,84 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    }
+    this.update = this.update.bind(this)
+  }
+  update(e) {
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value,
+    })
+  }
   render() {
     return (
-      <Buttons>
-        <button value='A'>A</button>
-        <button value='B'>B</button>
-        <button value='C'>C</button>
-      </Buttons>
+      <div>
+        <NumInput
+          ref='red'
+          min={0}
+          max={255}
+          step={1}
+          val={+this.state.red}
+          type='number'
+          label='Red'
+          update={this.update}/>
+        <br/>
+        <NumInput ref='green' update={this.update}/>
+        {this.state.green}
+        <br/>
+        <NumInput ref='blue' update={this.update}/>
+        {this.state.blue}
+        <br/>
+      </div>
     )
   }
 }
 
-class Buttons extends React.Component {
-  constructor() {
-    super();
-    this.state = {selected: 'None'}
-  }
-  selectItem(selected) {
-    this.setState({selected})
-  }
+class NumInput extends React.Component {
   render() {
-    // let items = this.props.children;
-    let fn = child =>
-      React.cloneElement(child, {
-        onClick: this.selectItem.bind(this, child.props.value)
-      })
-    let items = React.Children.map(this.props.children, fn);
+    let label = this.props.label !== '' ? 
+      <label>{this.props.label} - {this.props.val}</label> : ''
     return (
       <div>
-        <h2>You have selected: {this.state.selected}</h2>
-        {items}
+        <input
+          ref='inp'
+          type={this.props.type}
+          min={this.props.min}
+          max={this.props.max}
+          step={this.props.step}
+          defaultValue={this.props.val}
+          onChange={this.props.update}/>
+          {label}
       </div>
     )
   }
+}
+
+NumInput.propTypes = {
+  min: React.PropTypes.number,
+  max: React.PropTypes.number,
+  step: React.PropTypes.number,
+  val: React.PropTypes.number,
+  label: React.PropTypes.string,
+  update: React.PropTypes.func.isRequired,
+  type: React.PropTypes.oneOf(['number' ,'range'])
+}
+
+NumInput.defaultProps = {
+  min: 0,
+  max: 0,
+  step: 1,
+  val: 0,
+  label: '',
+  type: 'range'
 }
 
 export default App
