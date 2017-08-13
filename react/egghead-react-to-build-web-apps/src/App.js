b8f632c337364576645805cbb224cdd4e81f22f6
Jinho Hyeon <neverlish@gmail.com>
2017-08-13 21:27:29 +0900

    13 Control React Component Updates When New Props Are Received

--- react/egghead-react-to-build-web-apps/src/App.js
+++ react/egghead-react-to-build-web-apps/src/App.js
@@ -4,48 +4,31 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {val: 0}
-    this.update = this.update.bind(this)
+    this.state = {increasing: false}
   }
   update() {
-    this.setState({val: this.state.val + 1})
-  }
-  componentWillMount() {
-    console.log('componentWillMount');
-    this.setState({m: 2})
-  }
-  render() {
-    console.log('render');
-    return <button onClick={this.update}>
-      {this.state.val * this.state.m}
-    </button>
-  }
-  componentDidMount() {
-    console.log('componentDidMount');
-    this.inc = setInterval(this.update, 500)
-  }
-  componentWillUnmount() {
-    console.log('componentWillUnmount');
-    clearInterval(this.inc)
-  }
-}
-
-class Wrapper extends React.Component {
-  mount() {
-    ReactDOM.render(<App/>, document.getElementById('a'))
-  }
-  unmount() {
-    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.mount.bind(this)}>Mount</button>
-        <button onClick={this.unmount.bind(this)}>Unmount</button>
-        <div id='a'></div>
-      </div>
+    ReactDOM.render(
+      <App val={this.props.val+1}/>, 
+      document.getElementById('root')
     )
   }
+  componentWillReceiveProps(nextProps) {
+    this.setState({increasing: nextProps.val > this.props.val})
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.val % 5 === 0;
+  }
+  render() {
+    console.log(this.state.increasing)
+    return <button onClick={this.update.bind(this)}>
+      {this.props.val}
+    </button>
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(`prevProps: ${prevProps.val}`)
+  }
 }
 
-export default Wrapper
+App.defaultProps = {val: 0}
+
+export default App
