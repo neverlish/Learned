a81d902036e9f818e4e0db9b583d21398b89e50d
Jinho Hyeon <neverlish@gmail.com>
2017-08-13 17:53:17 +0900

    03 Display Output in React with a Component's render Method

--- react/egghead-react-to-build-web-apps/src/App.js
+++ react/egghead-react-to-build-web-apps/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-// class App extends React.Component {
-//   render() {
-//     return <h1>Hello World</h1>
-//     // return React.createElement('h1', null, 'Hello World')
-//   }
-// }
-
-const App = () => <h1>Hello stateless</h1>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>Bold</b>
+      </div>
+    )
+  }
+}
 
 export default App
