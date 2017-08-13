48ea074cc7dccb5405ff29ce362ddfea1bd38e5a
Jinho Hyeon <neverlish@gmail.com>
2017-08-13 21:14:56 +0900

    10 Use React ref to Get a Reference to Specific Components

--- react/egghead-react-to-build-web-apps/src/App.js
+++ react/egghead-react-to-build-web-apps/src/App.js
@@ -1,32 +1,41 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {currentEvent: '---'}
-    this.update = this.update.bind(this)
+    this.state = {a: ''}
   }
   update(e) {
-    this.setState({currentEvent: e.type})
+    this.setState({
+      // a: this.a.value, 
+      // a: ReactDOM.findDOMNode(this.a).value,
+      a: this.a.refs.input.value,
+      b: this.refs.b.value
+    })
   }
   render() {
     return (
       <div>
-        <textarea
-          cols='30'
-          rows='10'
-          onKeyPress={this.update}
-          onCopy={this.update}
-          onCut={this.update}
-          onPaste={this.update}
-          onFocus={this.update}
-          onBlur={this.update}
-          onDoubleClick={this.update}
-          onTouchStart={this.update}
-          onTouchEnd={this.update}/>
-        <h1>{this.state.currentEvent}</h1>
+        <Input
+          // ref={node => this.a = node}
+          ref={component => this.a = component}
+          update={this.update.bind(this)}/>
+        {this.state.a}
+        <hr/>
+        <input
+          ref='b'
+          type='text'
+          onChange={this.update.bind(this)}/>
+        {this.state.b}
       </div>
     )
+  }
+}
+
+class Input extends React.Component {
+  render() {
+    return <input ref='input' type='text' onChange={this.props.update}/>
   }
 }
 
