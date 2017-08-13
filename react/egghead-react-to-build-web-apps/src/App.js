3ba333aa7dc375bedf8f92db830195ee22322077
Jinho Hyeon <neverlish@gmail.com>
2017-08-13 21:22:44 +0900

    12 Manage React Component State with Lifecycle Methods

--- react/egghead-react-to-build-web-apps/src/App.js
+++ react/egghead-react-to-build-web-apps/src/App.js
@@ -12,16 +12,21 @@ class App extends React.Component {
   }
   componentWillMount() {
     console.log('componentWillMount');
+    this.setState({m: 2})
   }
   render() {
     console.log('render');
-    return <button onClick={this.update}>{this.state.val}</button>
+    return <button onClick={this.update}>
+      {this.state.val * this.state.m}
+    </button>
   }
   componentDidMount() {
     console.log('componentDidMount');
+    this.inc = setInterval(this.update, 500)
   }
   componentWillUnmount() {
     console.log('componentWillUnmount');
+    clearInterval(this.inc)
   }
 }
 
