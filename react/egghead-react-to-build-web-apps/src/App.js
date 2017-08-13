6b39269bbfa465cf79a37be9b0c11f044c923bc7
Jinho Hyeon <neverlish@gmail.com>
2017-08-13 17:51:38 +0900

    02 Write a "Hello World" React Component

--- react/egghead-react-to-build-web-apps/src/App.js
+++ react/egghead-react-to-build-web-apps/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App = () => <h1>Hello</h1>
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello World</h1>
+//     // return React.createElement('h1', null, 'Hello World')
+//   }
+// }
 
-export default App;
+const App = () => <h1>Hello stateless</h1>
+
+export default App
