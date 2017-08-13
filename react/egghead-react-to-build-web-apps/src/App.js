dc6c488ba675538fe719e05d3ca9021422971e1a
Jinho Hyeon <neverlish@gmail.com>
2017-08-13 20:27:16 +0900

    05 Manage React Component State with setState

--- react/egghead-react-to-build-web-apps/src/App.js
+++ react/egghead-react-to-build-web-apps/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
-  render() {
-    let txt = this.props.txt
-    return <h1>{txt}</h1>
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    }
   }
-}
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'this is the default txt'
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
+  render() {
+    return (
+      <div>
+        <input type='text' onChange={this.update.bind(this)}/>
+        <h1>{this.state.txt} - {this.state.cat}</h1>
+      </div>
+    )
+  }
 }
 
 export default App
--- react/egghead-react-to-build-web-apps/src/index.js
+++ react/egghead-react-to-build-web-apps/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App cat={5}/>, 
+  <App/>, 
   document.getElementById('root')
 );
