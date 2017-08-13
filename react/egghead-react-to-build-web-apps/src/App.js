d62f492f5064fd720ef5c7a6d7d1294d5b8954b5
Jinho Hyeon <neverlish@gmail.com>
2017-08-13 22:57:42 +0900

    16 Build a JSX Live Compiler as a React Component

--- react/egghead-react-to-build-web-apps/src/App.js
+++ react/egghead-react-to-build-web-apps/src/App.js
@@ -1,54 +1,43 @@
 import React from 'react';
-
-const HOC = (InnerComponent) => class extends React.Component {
-  constructor() {
-    super();
-    this.state = {count: 0}
-  }
-  update() {
-    this.setState({count: this.state.count + 1})
-  }
-  componentWillMount() {
-    console.log('will mount')
-  }
-  render() {
-    return (
-      <InnerComponent
-        {...this.props}
-        {...this.state}
-        update={this.update.bind(this)}
-      />
-    )
-  }
-}
+import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '/* add your jsx here */',
+      output: ''
+    }
+  }
+  update(e) {
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: window.Babel
+          .transform(code, {presets: ['es2015', 'react']})
+          .code
+          , err: ''
+      })
+    }
+    catch(err) {
+      this.setState({err: err.message})
+    }
+  }
   render() {
     return (
       <div>
-        <Button>button</Button>
-        <hr/>
-        <LabelHOC>label</LabelHOC>
+        <header>{this.state.err}</header>
+        <div className='container'>
+          <textarea
+            onChange={this.update.bind(this)}
+            defaultValue={this.state.input}/>
+          <pre>
+            {this.state.output}
+          </pre>
+        </div>
       </div>
     )
   }
 }
-
-const Button = HOC((props) => 
-  <button onClick={props.update}>{props.children} - {props.count}</button>
-)
-
-class Label extends React.Component {
-  componentWillMount() {
-    console.log('label will mount')
-  }
-  render() {
-    return (
-      <label onMouseOver={this.props.update}>{this.props.children} - {this.props.count}</label>
-    )
-  }
-}
-
-const LabelHOC = HOC(Label)
 
 export default App
