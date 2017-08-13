707423b4a0c34e0786fae2be950d2e9c7f034cb4
Jinho Hyeon <neverlish@gmail.com>
2017-08-14 02:53:57 +0900

    04 Define Functional Components in Preact

--- react/egghead-up-and-running-with-preact/src/components/App.js
+++ react/egghead-up-and-running-with-preact/src/components/App.js
@@ -1,9 +1,21 @@
 import { h } from 'preact';
+import User from './User';
+
+const users = [
+  {
+    image: 'https://avatars1.githubusercontent.com/u/1643522?v=4&s=460',
+    name: 'Shane Osbourne'
+  },
+  {
+    image: 'https://avatars1.githubusercontent.com/u/170270?v=4&s=460',
+    name: 'Sindre Sorhus'
+  }
+]
 
 export function App () {
   return (
     <div class='app'>
-      <h1>Hello world!</h1>
+      {users.map (user => <User image={user.image} name={user.name} key={user.name}/>)}
     </div>
   )
 }
