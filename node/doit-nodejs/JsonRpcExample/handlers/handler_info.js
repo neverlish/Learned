6bf40cb81f0594136b99c6df3d2d2cf30b307c6d
Jinho Hyeon <neverlish@gmail.com>
2017-08-13 23:57:12 +0900

    11 JSON-RPC 서버 만들기 - 4 데이터 부분을 암호화하기

--- node/doit-nodejs/JsonRpcExample/handlers/handler_info.js
+++ node/doit-nodejs/JsonRpcExample/handlers/handler_info.js
@@ -4,7 +4,8 @@ var handler_info = [
   {file: './echo', method: 'echo'},
   {file: './echo_error', method: 'echo_error'},
   {file: './add', method: 'add'},
-  {file: './listuser', method: 'listuser'}
+  {file: './listuser', method: 'listuser'},
+  {file: './echo_encrypted', method: 'echo_encrypted'}
 ];
 
 module.exports = handler_info;
