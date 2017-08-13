24bbb9a42d1900a792eaf875c6d921e96ae01c97
Jinho Hyeon <neverlish@gmail.com>
2017-08-13 17:58:30 +0900

    04 Set Properties on React Components

--- react/egghead-react-to-build-web-apps/src/App.js
+++ react/egghead-react-to-build-web-apps/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-        <b>Bold</b>
-      </div>
-    )
+    let txt = this.props.txt
+    return <h1>{txt}</h1>
   }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'this is the default txt'
 }
 
 export default App
