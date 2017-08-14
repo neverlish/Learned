f1365fb099190275e86cb2be398e4ba86440528a
Jinho Hyeon <neverlish@gmail.com>
2017-08-14 09:05:31 +0900

    12 Use Redux Dev-Tools with Preact

--- react/egghead-up-and-running-with-preact/src/index.js
+++ react/egghead-up-and-running-with-preact/src/index.js
@@ -1,16 +1,29 @@
 import {h, render} from 'preact';
 import { Provider } from 'preact-redux';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
+const initialState = {
+  loading: true, 
+  user: null
+}
 
 import reducer from './reducer';
-const store = createStore(
-  reducer, 
-  {loading: true, user: null}, 
-  applyMiddleware(thunk)
-);
 
 import App from './components/App';
+
+  const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const store = createStore(
+  reducer, 
+  initialState,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 
 render(
