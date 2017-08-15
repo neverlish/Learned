0d8a7ec210f304024898ff7f8fce591f3d2a7ed8
Jinho Hyeon <neverlish@gmail.com>
2017-08-15 12:06:46 +0900

    07 - 2 - 2 Video 컴포넌트 사용하기

--- react-native/hanbit-mobile-app-react-native/Depends/Depends.js
+++ react-native/hanbit-mobile-app-react-native/Depends/Depends.js
@@ -6,6 +6,7 @@ import {
 } from 'react-native';
 
 var _ = require('lodash');
+import Video from 'react-native-video';
 
 export default class Depends extends Component {
   constructor() {
@@ -15,9 +16,14 @@ export default class Depends extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
+        <Video
+          source={require('./warbler.mp4')} // URL이나 로컬 파일 지정 가능.
+          rate={1.0} // 0은 일시 정지, 1은 보통 속도.
+          muted={false} // 오디오를 음소거
+          pause={false} // 재생을 일시 정지
+          resizeMode='cover' // 영상의 가로/세로 비율을 유지함녀서 화면을 가득 채움
+          repeat={true} // 무한 반복
+          style={styles.backgroundVideo} />
       </View>
     );
   }
@@ -29,6 +35,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  backgroundVideo: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
   }
 });
 
