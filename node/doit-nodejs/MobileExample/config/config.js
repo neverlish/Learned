24622b6ba3ee9cc48a5136feff6366c6463fb143
Jinho Hyeon <neverlish@gmail.com>
2017-08-17 01:15:50 +0900

    13 모바일 서버 만들기 - 2 모바일 단말 관리 기능 만들기 - 데이터베이스 스키마 추가하기

--- node/doit-nodejs/MobileExample/config/config.js
+++ node/doit-nodejs/MobileExample/config/config.js
@@ -2,7 +2,8 @@ module.exports = {
   server_port: 3000,
   db_url: 'mongodb://localhost:27017/shopping',
   db_schemas: [
-    {file: './user_schema', collection: 'users6', schemaName: 'UserSchema', modelName: 'UserModel'}
+    {file: './user_schema', collection: 'users6', schemaName: 'UserSchema', modelName: 'UserModel'},
+    {file: './device_schema', collection: 'devices', schemaName: 'DeviceSchema', modelName: 'DeviceModel'}
   ],
   route_info: [
     {file: './user', path: '/process/listuser', method: 'listuser', type: 'post'}
