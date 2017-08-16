3254646883eb347e013b4fed61751e001b6391cb
Jinho Hyeon <neverlish@gmail.com>
2017-08-17 04:07:20 +0900

    13 모바일 서버 만들기 - 2 모바일 단말 관리 기능 만들기 - 단말 정보 추가를 요청하는 앱 만들기

--- node/doit-nodejs/MobileExample/Shopping/app/src/main/java/org/androidtown/shopping/MainActivity.java
+++ node/doit-nodejs/MobileExample/Shopping/app/src/main/java/org/androidtown/shopping/MainActivity.java
@@ -1,9 +1,16 @@
 package org.androidtown.shopping;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText editText;
     TextView textView;
+    EditText editText2;
 
     Handler handler = new Handler();
 
@@ -30,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         editText = (EditText) findViewById(R.id.editText);
         textView = (TextView) findViewById(R.id.textView);
+        editText2 = (EditText) findViewById(R.id.editText2);
     }
 
     public void onButton1Clicked(View v) {
@@ -65,6 +74,48 @@ public class MainActivity extends AppCompatActivity {
         println("웹서버에 요청함 : " + url);
     }
 
+    public void onButton2Clicked(View v) {
+        String url = editText2.getText().toString();
+
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            println("onResponse() 호출됨 : " + response);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String>  params = new HashMap<>();
+
+                DeviceInfo device = getDeviceInfo();
+                params.put("mobile", device.getMobile());
+                params.put("osVersion", device.getOsVersion());
+                params.put("model", device.getModel());
+                params.put("display", device.getDisplay());
+                params.put("manufacturer", device.getManufacturer());
+                params.put("macAddress", device.getMacAddress());
+
+                return params;
+            }
+        };
+
+        Volley.newRequestQueue(this).add(request);
+        println("웹서버에 요청함 : " + url);
+
+    }
+
     public void println(final String data) {
         handler.post(new Runnable() {
             @Override
@@ -72,5 +123,52 @@ public class MainActivity extends AppCompatActivity {
                 textView.append(data + '\n');
             }
         });
+    }
+
+    public DeviceInfo getDeviceInfo() {
+        DeviceInfo device = null;
+
+        // 1. mobile
+        String mobile = null;
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        if ( telephonyManager.getLine1Number() != null ) {
+            mobile = telephonyManager.getLine1Number();
+        }
+
+        // 2. osVersion
+        String osVersion = Build.VERSION.RELEASE;
+
+        // 3. model
+        String model = Build.MODEL;
+
+        // 4. display
+        String display = getDisplay(this);
+
+        // 5. manufacturer
+        String manufacturer = Build.MANUFACTURER;
+
+        // 6. macAddress
+        String macAddress = getMacAddress(this);
+
+        device = new DeviceInfo(mobile, osVersion, model, display, manufacturer, macAddress);
+
+        return device;
+    }
+
+    private static String getDisplay(Context context) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        int deviceWidth = displayMetrics.widthPixels;
+        int deviceHeight = displayMetrics.heightPixels;
+
+        return deviceWidth + "x" + deviceHeight;
+    }
+
+    private static String getMacAddress(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo info = wifiManager.getConnectionInfo();
+
+        return info.getMacAddress();
     }
 }
