c5cce711a86ed970678bb6c6a3962db896a20df2
Jinho Hyeon <neverlish@gmail.com>
2017-08-27 10:06:46 +0900

    3 - 12 Understading Props

--- react/udemy-build-real-world-js-apps/hello-world/src/index.js
+++ react/udemy-build-real-world-js-apps/hello-world/src/index.js
@@ -2,23 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import App from './sources/App11';
+import App from './sources/App12';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  // <App header='Props from the Header...' content='Props from the Content...'/>, 
+  <App />,
+  document.getElementById('root')
+);
 registerServiceWorker();
-
-// function tick() {
-//   const element = (
-//     <div>
-//       <h1>Hello World!</h1>
-//       <h2>The current time is: {new Date().toLocaleTimeString()}</h2>
-//     </div>
-//   );
-//   ReactDOM.render(
-//     element, 
-//     document.getElementById('root')
-//   );
-// }
-
-// setInterval(tick, 1000);
