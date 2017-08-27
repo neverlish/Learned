166e09bab996ad20742f77c6336c34342ec964d1
Jinho Hyeon <neverlish@gmail.com>
2017-08-28 00:15:11 +0900

    15 클라우드에 올리기 - 5 배포에 필요한 기능 알아보기 - 레디스 사용하기

--- node/doit-nodejs/DeplayExample/app.js
+++ node/doit-nodejs/DeplayExample/app.js
@@ -9,6 +9,9 @@ var express = require('express')
 
 var bodyParser = require('body-parser');
 
+var redis = require('redis');
+var store = redis.createClient();
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -18,19 +21,47 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/process/login', function(req, res) {
-  console.log('/process/login 처리함.');
+app.post('/process/setname', function(req, res) {
+  console.log('/process/setname 처리함.');
 
   var paramId = req.param('id');
-  var paramPassword = req.param('password');
+  var paramName = req.param('name');
 
-  res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-  res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>');
-  res.write('<div><p>Param id : ' + paramId + '</p></div>');
-  res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
-  res.write("<br><br><a href='/public/login2.html'>로그인 페이지로 돌아가기</a>");
+  store.hset('user', paramId, paramName, redis.print);
+  console.log('redis에 사용자를 등록햇습니다. : ' + paramId + ' -> ' + paramName);
+
+  res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
+  res.write('<h1>서버에서 응답한 결과입니다.</h1>');
+  res.write('<div><p>redis에 사용자를 등록했습니다. : ' + paramId + ' -> ' + paramName + '</p></div>');
+  res.write("<br><br><a href='/public/setname.html>처음으로 돌아가기</a>");
   res.end();
 });
+
+app.post('/process/getname', function(req, res) {
+  console.log('/process/getname 처리함.');
+
+  var paramId = req.param('id');
+
+  store.hget('user', paramId, function(err, username) {
+    if(err) {throw err;}
+
+    if (username) {
+      console.log('redis에서 사용자를 찾았습니다. : ' + paramId + ' -> ' + username);
+      
+      res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+      res.write('<h1>서버에서 응답한 결과입니다.</h1>');
+      res.write('<div><p>redis에서 사용자를 찾았습니다. : ' + paramId + ' -> ' + username + '</p></div>');
+      res.write("<br><br><a href='/public/getname.html'>처음으로 돌아가기</a>");
+      res.end();
+    } else {
+      res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
+      res.write('<h1>서버에서 응답한 결과입니다.</h1>');
+      res.write('<div><p>redis에 사용자를 찾지 못했습니다. : ' + paramId + '</p></div>');
+      res.write("<br><br><a href='/public/getname.html>처음으로 돌아가기</a>");
+      res.end();
+    }
+  })
+})
 
 app.all('*', function(req, res) {
   res.send(404, '<h1>ERROR - 페이지를 찾을 수 없습니다.</h1>')
