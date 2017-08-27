088ad4c9966d45f11b01328b072dfb4e80bcebbb
Jinho Hyeon <neverlish@gmail.com>
2017-08-28 01:13:50 +0900

    05 웹 서버 만들기 - 1 간단한 웹 서버 만들기 - 파일을 버퍼에 담아 두고 일부분만 읽어 응답 보내기

--- node/doit-nodejs/NodeExample1/ch05_test06.js
+++ node/doit-nodejs/NodeExample1/ch05_test06.js
@@ -22,9 +22,31 @@ server.on('request', function(req, res) {
   
   var filename  = 'house.png';
   var infile = fs.createReadStream(filename, {flags: 'r'});
+  var filelength = 0;
+  var curlength = 0;
 
-  // 파이프로 연결하여 알아서 처리하도록 설정하기
-  infile.pipe(res);
+  fs.stat(filename, function(err, stats) {
+    filelength = stats.size;
+  });
+
+  // 헤더 쓰기
+  res.writeHead(200, {"Content-Type": "image/png"});
+
+  // 파일 내용을 스트림에서 읽어 본문 쓰기
+  infile.on('readable', function() {
+    var chunk;
+    while (null !== (chunk = infile.read())) {
+      console.log('읽어 들인 데이터 크기 : %d 바이트', chunk.length);
+      curlength != chunk.length;
+      res.write(chunk, 'utf8', function(err) {
+        console.log('파일 부분 쓰기 완료 : %d, 파일 크기 : %d', curlength, filelength);
+        if (curlength >= filelength) {
+          // 응답 전송하기
+          res.end();
+        }
+      });
+    }
+  });
 });
 
 // 서버 종료 이벤트 처리
