ef2b12c60728a31142e86b605e9a21d3f56aa317
Jinho Hyeon <neverlish@gmail.com>
2017-08-27 11:11:33 +0900

    3 - 15 Components Life Cycle

--- react/udemy-build-real-world-js-apps/hello-world/src/index.js
+++ react/udemy-build-real-world-js-apps/hello-world/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import App from './sources/App14';
+import App from './sources/App15';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
@@ -10,3 +10,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 registerServiceWorker();
+
+setTimeout(() => {
+  ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+}, 10000);
