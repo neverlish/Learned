a969dadb16a53c792c717a1b52303967e8428819
Jinho Hyeon <neverlish@gmail.com>
2017-08-28 01:00:48 +0900

    04 노드의 기본 기능 알아보기 - 1 주소 문자열과 요청 파라미터 다루기 - 주소 문자열을 URL 객체로 변환하기

---
+++ node/doit-nodejs/NodeExample1/ch04_test01.js
@@ -0,0 +1,10 @@
+var url = require('url');
+
+// 주소 문자열을 URL 객체로 만들기
+var curURL = url.parse('https://m.search.naver.com/search.naver?query=steve+jobs&where=m&sm=mtp_hty');
+
+// URL 객체를 주소 문자열로 만들기
+var curStr = url.format(curURL);
+
+console.log('주소 문자열 : %s', curStr);
+console.dir(curURL);
