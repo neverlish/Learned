6bf4a07050ba954a8f89b0bb6aa91f372d238b3e
Jinho Hyeon <neverlish@gmail.com>
2017-08-28 00:57:34 +0900

    03 - 2 - splice() 메소드로 배열 요소 여러 개를 한꺼번에 추가하거나 삭제하기

--- node/doit-nodejs/NodeExample1/ch03_test13.js
+++ node/doit-nodejs/NodeExample1/ch03_test13.js
@@ -4,3 +4,11 @@ console.log('delete 키워드로 배열 요소 삭제 전 배열 요소의 수 :
 delete Users[1];
 console.log('delete 키워드로 배열 요소 삭제 후');
 console.dir(Users);
+
+Users.splice(1, 0, {name: '애프터스쿨', age: 25});
+console.log('splice()로 요소를 인덱스 1에 추가한 후');
+console.dir(Users);
+
+Users.splice(2, 1);
+console.log('splice()로 인덱스 2의 요소를 1개 삭제한 후');
+console.dir(Users);
