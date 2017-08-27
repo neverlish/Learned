2591a54f314c94a758394dacdaa66a70f2f32425
Jinho Hyeon <neverlish@gmail.com>
2017-08-28 00:53:59 +0900

    03 - 1 - 자바스크립트 함수 만들어보기

---
+++ node/doit-nodejs/NodeExample1/ch03_test03.js
@@ -0,0 +1,7 @@
+function add(a,b) {
+  return a+b;
+}
+
+var result = add(10, 10);
+
+console.log('더하기 (10, 10) : %d', result);
---
+++ node/doit-nodejs/NodeExample1/ch03_test07.js
@@ -0,0 +1,9 @@
+var Person = {
+  age: 20,
+  name: '소녀시대',
+  add: function(a,b) {
+    return a+b;
+  }
+}
+
+console.log('더하기 : %d', Person.add(10,10));
