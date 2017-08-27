75c1d8a7a5663e9c979006b9eebecce967f62a50
Jinho Hyeon <neverlish@gmail.com>
2017-08-27 11:49:24 +0900

    3 - 17 Forms in React - Part 2

--- react/udemy-build-real-world-js-apps/hello-world/src/sources/App16.js
+++ react/udemy-build-real-world-js-apps/hello-world/src/sources/App16.js
@@ -1,4 +1,4 @@
-// 03 The Basics - 16 Forms in React - Part 1
+// 03 The Basics - 17 Forms in React - Part 2
 
 import React, { Component } from 'react';
 import logo from '../logo.svg';
@@ -52,6 +52,8 @@ class Content extends Component {
         <p>In this lecture, we will go over Forms in React</p>
         <MyInputComponent inputValue={this.state.myInputValue} myInputChanged={this.myInputChanged}/>
         <h4>{this.state.myInputValue}</h4>
+        <EssayComponent />
+        <FlavorForm />
       </div>
     );
   }
@@ -66,5 +68,73 @@ class MyInputComponent extends Component {
     )
   }
 }
+
+class EssayComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Once upon a time...'   
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
+  handleSubmit(e) {
+    alert(this.state.value);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <textarea value={this.state.value} onChange={this.handleChange}/>
+        <h4>{this.state.value}</h4>
+        <button type='submit'>Submit</button>
+      </form>
+    )
+  }
+}
+
+class FlavorForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'grapefruit'   
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
+  handleSubmit(e) {
+    alert('The Flavor you selected is : ' + this.state.value.toUpperCase());
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select onChange={this.handleChange}>
+            <option value='grapefruit'>Grape Fruit</option>
+            <option value='apple'>Apple</option>
+            <option value='chocolate'>Chocolate</option>
+            <option value='banana'>Banana</option>
+          </select>
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
+    )
+  }
+}
+
 
 export default App;
