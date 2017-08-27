a0096737ebd081e2a15714807ba5f57ba25f8de6
Jinho Hyeon <neverlish@gmail.com>
2017-08-27 21:35:39 +0900

    4 - 24 Moving the options to the child component!

--- react/udemy-build-real-world-js-apps/game/src/components/Quiz.js
+++ react/udemy-build-real-world-js-apps/game/src/components/Quiz.js
@@ -1,17 +1,39 @@
 import React, {Component} from 'react';
+import QuizOptions from './QuizOptions';
 
 class Quiz extends Component {
+  constructor(props) {
+    super(props);
+
+    let riddle = {
+      resultsArray: [8, 9, 10, 11],
+      field1: 5,
+      field2: 5,
+      answer: 10
+    };
+
+    this.state = {riddle};
+
+    this.renderOptions = this.renderOptions.bind(this);
+  }
+
+  renderOptions() {
+    return (
+      <div className='options'>
+        <QuizOptions />
+        <QuizOptions />
+        <QuizOptions />
+        <QuizOptions />
+      </div>
+    );
+  }
   render() {
     return (
       <div className='quiz'>
         <div className='quiz-content'>
           <p className='question'>What is the sum of <span className='text-info'>5</span> and <span className='text-info'>5</span>?</p>
-          <div className='options'>
-            <div className='fields'><div className='field-block'>10</div></div>
-            <div className='fields'><div className='field-block'>10</div></div>
-            <div className='fields'><div className='field-block'>10</div></div>
-            <div className='fields'><div className='field-block'>10</div></div>
-          </div>
+          {this.renderOptions()}
+          
         </div>
         <div className='play-again'>
           <a className='button'>Play Again</a>
---
+++ react/udemy-build-real-world-js-apps/game/src/components/QuizOptions.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class QuizOptions extends Component {
+  render() {
+    return (
+      <div className='fields'><div className='field-block'>10</div></div>
+    );
+  }
+}
+
+export default QuizOptions;
