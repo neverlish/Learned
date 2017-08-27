f3f12a6a8dc3492271e7cd6b19864f2f3502888f
Jinho Hyeon <neverlish@gmail.com>
2017-08-27 21:57:43 +0900

    4 - 26 Make the 2 numbers random!

--- react/udemy-build-real-world-js-apps/game/src/components/Quiz.js
+++ react/udemy-build-real-world-js-apps/game/src/components/Quiz.js
@@ -5,16 +5,32 @@ class Quiz extends Component {
   constructor(props) {
     super(props);
 
-    let riddle = {
-      resultsArray: [8, 9, 10, 11],
-      field1: 5,
-      field2: 5,
-      answer: 10
-    };
+    let riddle = this.playGame();
 
     this.state = {riddle};
 
     this.renderOptions = this.renderOptions.bind(this);
+  }
+
+  randomNumber(min, max) {
+    return Math.floor(Math.random() * (max-min+1)) + min;
+  }
+
+  playGame() {
+    let field1 = this.randomNumber(20, 50);
+    let field2 = this.randomNumber(20, 50);
+    let result = field1 + field2;
+
+    let riddle = {
+      resultsArray: [8, 9, 10, 11],
+      field1: field1,
+      field2: field2,
+      answer: result
+    };
+
+    console.log(riddle)
+
+    return riddle;
   }
 
   renderOptions() {
@@ -30,7 +46,7 @@ class Quiz extends Component {
     return (
       <div className='quiz'>
         <div className='quiz-content'>
-          <p className='question'>What is the sum of <span className='text-info'>5</span> and <span className='text-info'>5</span>?</p>
+          <p className='question'>What is the sum of <span className='text-info'>{this.state.riddle.field1}</span> and <span className='text-info'>{this.state.riddle.field2}</span>?</p>
           {this.renderOptions()}
           
         </div>
