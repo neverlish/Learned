8fdad0dc813fc509974f6b0e0a4c18091ca81e26
Jinho Hyeon <neverlish@gmail.com>
2017-08-28 01:01:04 +0900

    04 - 1 - 요청 파라미터 확인하기

--- node/doit-nodejs/NodeExample1/ch04_test01.js
+++ node/doit-nodejs/NodeExample1/ch04_test01.js
@@ -8,3 +8,10 @@ var curStr = url.format(curURL);
 
 console.log('주소 문자열 : %s', curStr);
 console.dir(curURL);
+
+// 요청 파라미터 구분하기
+var querystring = require('querystring');
+var param = querystring.parse(curURL.query);
+
+console.log('요청 파라미터 중 query의 값 : %s', param.query);
+console.log('원본 요청 파라미터 : %s', querystring.stringify(param));
