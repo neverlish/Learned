89d72f0a4386d71084dd3636366c2277316b2bb7
Jinho Hyeon <neverlish@gmail.com>
2017-08-28 19:18:58 +0900

    5 - 33 Making content load from a JSON file

--- react/udemy-build-real-world-js-apps/easy-abc/src/components/EasyABC.js
+++ react/udemy-build-real-world-js-apps/easy-abc/src/components/EasyABC.js
@@ -1,24 +1,44 @@
 import React, {Component} from 'react';
+import alphabets from '../alphabets.json';
 
 class EasyABC extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      alphabets: alphabets,
+      currentPosition: 0
+    }
+  }
   render() {
     return (
       <div className='game'>
         <div className='option'>
           <div className='fields'>
             <div className='field-block'>
-              A
+              {this.state.alphabets[this.state.currentPosition].letter}
             </div>
           </div>
           <div className='buttons'>
-            <a href='#' className='button prev'>Previous</a>
-            <a href='#' className='button sound'>Play Sound</a>
-            <a href='#' className='button next'>Next</a>
+            <a className='button prev'>Previous</a>
+            <a className='button sound'>Play Sound Again</a>
+            <a className='button next'>Next</a>
           </div>
           <div className='fields'>
             <div className='field-block'>
-              <div className='left-field'>A</div>
-              <div className='right-field'>A</div>
+              <div className='left-field'>
+                <div className='placeholder-span hide'>Click Next to view Image</div>
+                <img
+                  className='letter-image'
+                  alt={this.state.alphabets[this.state.currentPosition].word}
+                  src={this.state.alphabets[this.state.currentPosition].image}/>
+              </div>
+              <div className='right-field'>
+                <div className='placeholder-span hide'>Click Next to view Spelling</div>
+                <div className='word'>
+                  {this.state.alphabets[this.state.currentPosition].word.toUpperCase()}
+                </div>
+              </div>
             </div>
           </div>
         </div>
