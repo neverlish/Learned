46827077a52ae67cefb569977faa814a7d66a014
Jinho Hyeon <neverlish@gmail.com>
2017-08-31 14:07:44 +0900

    7 - 46 Let's setup Bootstrap for our app! Yes, Twitter Bootstrap

--- react/udemy-build-real-world-js-apps/love-to-eat/src/App.js
+++ react/udemy-build-real-world-js-apps/love-to-eat/src/App.js
@@ -5,14 +5,26 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='container'>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">Love To Eat</a>
+            </div>
+            <div id="navbar" className="navbar-collapse collapse">
+              <ul className="nav navbar-nav">
+                <li className="active"><a href="#">Home</a></li>
+                <li><a href="#">Submit a Recipe</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
