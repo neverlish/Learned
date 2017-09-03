b8b1f6bc74d9ef51a45eec845239e5a81ac1e392
Jinho Hyeon <neverlish@gmail.com>
2017-09-03 19:51:02 +0900

    15 - 4-2 bloglist 추가

--- js/inflearn-modern-js-es6/15-miniproject/src/main.js
+++ js/inflearn-modern-js-es6/15-miniproject/src/main.js
@@ -5,22 +5,25 @@ class Blog {
   }
 
   setInitData(dataURL) {
-    this.getData(dataURL);
+    this.getData(dataURL, this.insertPosts);
   }
 
-  getData(dataURL) {
+  getData(dataURL, fn) {
     const oReq = new XMLHttpRequest();
 
     oReq.addEventListener('load', () => {
       const list = JSON.parse(JSON.parse(oReq.responseText).body);
-      
-      list.forEach((v) => {
-        console.log(v.title);
-      });
+      fn(list);
     });
-
     oReq.open('GET', dataURL);
     oReq.send();
+  }
+
+  insertPosts(list) {
+    const ul = document.querySelector('.blogList > ul');
+    list.forEach((v) => {
+      ul.innerHTML += `<li><a href=${v.link}>${v.title}</a></li>`;
+    })
   }
 }
 
