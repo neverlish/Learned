bffdc56125a781145ecfa569606c5f557167681f
Jinho Hyeon <neverlish@gmail.com>
2017-09-03 19:46:19 +0900

    15 - 4-1 XHR 통신

--- js/inflearn-modern-js-es6/15-miniproject/src/main.js
+++ js/inflearn-modern-js-es6/15-miniproject/src/main.js
@@ -1,6 +1,26 @@
 class Blog {
   constructor() {
-    console.log('Blog is started!');
+    const dataURL = 'https://tlhm20eugk.execute-api.ap-northeast-2.amazonaws.com/prod/lambda_get_blog_info';
+    this.setInitData(dataURL);
+  }
+
+  setInitData(dataURL) {
+    this.getData(dataURL);
+  }
+
+  getData(dataURL) {
+    const oReq = new XMLHttpRequest();
+
+    oReq.addEventListener('load', () => {
+      const list = JSON.parse(JSON.parse(oReq.responseText).body);
+      
+      list.forEach((v) => {
+        console.log(v.title);
+      });
+    });
+
+    oReq.open('GET', dataURL);
+    oReq.send();
   }
 }
 
