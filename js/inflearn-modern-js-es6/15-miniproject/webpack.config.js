b8d42fdc43d03f90a4fd3334a7203ef8960ebc16
Jinho Hyeon <neverlish@gmail.com>
2017-09-03 17:54:34 +0900

    15 미니 프로젝트 - 1 nodeJS 기반 환경구성과 webpack

---
+++ js/inflearn-modern-js-es6/15-miniproject/webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      
+    }]
+  }
+}
