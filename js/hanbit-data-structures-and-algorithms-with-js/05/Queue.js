e0a00399c17dc99cc9900a3d3ab1813d5434a5af
Jinho Hyeon <neverlish@gmail.com>
2017-09-13 23:53:22 +0900

    05 큐 - 2 배열 기반의 Queue 클래스 구현

---
+++ js/hanbit-data-structures-and-algorithms-with-js/05/Queue.js
@@ -0,0 +1,43 @@
+function Queue() {
+	this.dataStore = [];
+	this.enqueue = enqueue;
+	this.dequeue = dequeue;
+	this.front = front;
+	this.back = back;
+	this.toString = toString;
+	this.empty = empty;
+}
+
+function enqueue(element) {
+	this.dataStore.push(element);
+}
+
+function dequeue(element) {
+	return this.dataStore.shift();
+}
+
+function front() {
+	return this.dataStore[0];
+}
+
+function back() {
+	return this.dataStore[this.dataStore.length - 1];
+}
+
+function toString() {
+	var retStr = '';
+	for (var i = 0; i < this.dataStore.length; i++) {
+		retStr += this.dataStore[i] + '\n';
+	}
+	return retStr;
+}
+
+function empty() {
+	if (this.dataStore.length == 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+module.exports.Queue = Queue;
