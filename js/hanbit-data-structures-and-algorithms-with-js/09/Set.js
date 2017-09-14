5e285106d12590ce3fcc9c6632f0630e9aaa6155
Jinho Hyeon <neverlish@gmail.com>
2017-09-14 19:08:08 +0900

    09 - 3 집합의 추가 동작

--- js/hanbit-data-structures-and-algorithms-with-js/09/Set.js
+++ js/hanbit-data-structures-and-algorithms-with-js/09/Set.js
@@ -2,11 +2,12 @@ function Set() {
   this.dataStore = [];
   this.add = add;
   this.remove = remove;
-  // this.size = size;
-  // this.union = union;
-  // this.intersect = intersect;
-  // this.subset = subset;
-  // this.difference = difference;
+  this.size = size;
+  this.contains = contains
+  this.union = union;
+  this.intersect = intersect;
+  this.subset = subset;
+  this.difference = difference;
   this.show = show;  
 }
 
@@ -31,6 +32,65 @@ function remove(data) {
 
 function show() {
   return this.dataStore;
+}
+
+function contains(data) {
+  if (this.dataStore.indexOf(data) > -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function union(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.dataStore.length; ++i) {
+    tempSet.add(this.dataStore[i]);
+  }
+  for (var i = 0; i < set.dataStore.length; ++i) {
+    if (!tempSet.contains(set.dataStore[i])) {
+      tempSet.dataStore.push(set.dataStore[i]);
+    }
+  }
+
+  return tempSet;
+}
+
+function intersect(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.dataStore.length; ++i) {
+    if (set.contains(this.dataStore[i])) {
+      tempSet.add(this.dataStore[i]);
+    }
+  }
+  return tempSet;
+}
+
+function subset(set) {
+  if (this.size() > set.size()) {
+    return false;
+  } else {
+    for (var i = 0; i < this.dataStore.length; i++) {
+      if (!set.contains(this.dataStore[i])) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function size() {
+  return this.dataStore.length;
+}
+
+function difference(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.dataStore.length; ++i) {
+    if (!set.contains(this.dataStore[i])) {
+      tempSet.add(this.dataStore[i]);
+    }
+  }
+  return tempSet;
 }
 
 module.exports.Set = Set;
