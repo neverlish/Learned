4dea5656454e2e15262073903734c21056b05807
Jinho Hyeon <neverlish@gmail.com>
2017-09-15 15:14:24 +0900

    12 - 2 기본 정렬 알고리즘

--- js/hanbit-data-structures-and-algorithms-with-js/12/CArray.js
+++ js/hanbit-data-structures-and-algorithms-with-js/12/CArray.js
@@ -7,6 +7,9 @@ function CArray(numElements) {
   this.clear = clear;
   this.setData = setData;
   this.swap = swap;
+  this.bubbleSort = bubbleSort;
+  this.selectionSort = selectionSort;
+  this.insertionSort = insertionSort;
 }
 
 function setData() {
@@ -39,7 +42,46 @@ function toString() {
 function swap(arr, index1, index2) {
   var temp = arr[index1];
   arr[index1] = arr[index2];
-  arr[index2] = swap;
+  arr[index2] = temp;
+}
+
+function bubbleSort() {
+  var numElements = this.dataStore.length;
+  var temp;
+  for (var outer = numElements; outer >= 2; --outer) {
+    for (var inner = 0; inner <= outer-1; ++inner) {
+      if (this.dataStore[inner] > this.dataStore[inner+1]) {
+        swap(this.dataStore, inner, inner+1);
+      }
+    }
+    console.log(this.toString());
+  }
+}
+
+function selectionSort() {
+  var min, temp;
+  for (var outer = 0; outer <= this.dataStore.length-2; ++outer) {
+    min = outer;
+    for (var inner = outer + 1; inner <= this.dataStore.length-1; ++inner) {
+      if (this.dataStore[inner] < this.dataStore[min]) {
+        min = inner;
+      }
+    }
+    swap(this.dataStore, outer, min);
+  }
+}
+
+function insertionSort() {
+  var temp, inner;
+  for (var outer = 1; outer <= this.dataStore.length - 1; ++outer) {
+    temp = this.dataStore[outer];
+    inner = outer;
+    while (inner > 0 && (this.dataStore[inner - 1] >= temp)) {
+      this.dataStore[inner] = this.dataStore[inner - 1];
+      --inner;
+    }
+    this.dataStore[inner] = temp;
+  }
 }
 
 module.exports.CArray = CArray;
