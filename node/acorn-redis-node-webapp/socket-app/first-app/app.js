fd64c901cdedcd5b4ebbf4104f367e2bf25b7108
Jinho Hyeon <neverlish@gmail.com>
2017-09-28 22:09:27 +0900

    02 - 03 메시지 브로드캐스팅

--- node/acorn-redis-node-webapp/socket-app/first-app/app.js
+++ node/acorn-redis-node-webapp/socket-app/first-app/app.js
@@ -2,11 +2,11 @@ var io = require('socket.io').listen(4000);
 
 io.sockets.on('connection', function(socket) {
   socket.on('join', function(data) {
-    io.sockets.emit('userJoined', data);
+    socket.broadcast.emit('userJoined', data);
     socket.username = data.username;
   });
   socket.on('ping', function(data, done) {
-    io.sockets.emit('ping', {username: socket.username});
+    socket.broadcast.emit('ping', {username: socket.username});
     done('ack');
   });
 });
