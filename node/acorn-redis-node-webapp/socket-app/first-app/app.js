4ccd75ab37bee3ffeb0f273d459b2247568b24ab
Jinho Hyeon <neverlish@gmail.com>
2017-09-28 21:54:14 +0900

    02 - 02 핑퐁 - 상호 작용 만들기

--- node/acorn-redis-node-webapp/socket-app/first-app/app.js
+++ node/acorn-redis-node-webapp/socket-app/first-app/app.js
@@ -1,9 +1,11 @@
 var io = require('socket.io').listen(4000);
 
 io.sockets.on('connection', function(socket) {
-  socket.emit('ping');
-
-  socket.on('pong', function(data) {
-    console.log('pong');
+  socket.on('join', function(data) {
+    io.sockets.emit('userJoined', data);
+    socket.username = data.username;
+  });
+  socket.on('ping', function(data) {
+    io.sockets.emit('ping', {username: socket.username});
   });
 });
