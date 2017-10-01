b1ea707081a5a1fd97d0b6b72f7e826938594835
Jinho Hyeon <neverlish@gmail.com>
2017-10-02 00:04:09 +0900

    02 - 07 - 당신은 누구인가

--- node/acorn-redis-node-webapp/NodeChat/socket.io/index.js
+++ node/acorn-redis-node-webapp/NodeChat/socket.io/index.js
@@ -1,12 +1,42 @@
 var io = require('socket.io');
 var config = require('../config');
+var cookie = require('cookie');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var ConnectRedis = require('connect-redis')(expressSession);
+var redisSession = new ConnectRedis({
+  host: config.redisHost,
+  port: config.redisPort
+})
+
+var socketAuth = function socketAuth (socket, next) {
+  var handshakeData = socket.request;
+  var parsedCookie = cookie.parse(handshakeData.headers.cookie);
+  var sid = cookieParser.signedCookie(parsedCookie['connect.sid'], config.secret);
+  
+  if (parsedCookie['connect.sid'] === sid)
+    return next(new Error('Nothing Defined'));
+  
+  redisSession.get(sid, function(err, session) {
+    if (session.isAuthenticated) {
+      socket.user = session.user;
+      socket.sid = sid;
+      return next(); 
+    } else {
+      return next(new Error('Not Authenticated'));
+    }
+  })
+}
 
 var socketConnection = function socketConnection(socket) {
   socket.emit('message', {message: 'Hey!'});
+  socket.emit('message', socket.user);
 };
 
 exports.startIo = function startIo (server) {
   io = io.listen(server);
   var packtchat = io.of('/packtchat');
+
+  packtchat.use(socketAuth);
   packtchat.on('connection', socketConnection);
 }
