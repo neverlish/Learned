a9faf0646aa059a3ccb764e21f6083118b3b2952
Jinho Hyeon <neverlish@gmail.com>
2017-10-03 10:24:15 +0900

    07 - 04 백본 모델 - 모델 생성

--- node/acorn-redis-node-webapp/NodeChat/static/js/models.js
+++ node/acorn-redis-node-webapp/NodeChat/static/js/models.js
@@ -15,3 +15,43 @@ var SocketListener = function SocketListener(noun, collection, socket) {
   };
   return {destroy: destroy};
 };
+
+var SocketSync = function SocketListener(method, model, options) {
+  var socket = Backbone.socket;
+  var create = function create(model, options, noun) {
+    socket.emit('Add' + noun, model);
+  };
+  var read = function read(model, options, noun) {
+    socket.emit('Get' + noun, options);
+  };
+  switch (method) {
+    case 'create':
+      create(model, options, this.noun);
+      break;
+    case 'read':
+      read(model, options, this.noun);
+      break;
+  }
+}
+
+var User = Backbone.Model.extend({
+  image: function(size) {
+    switch (this.get('type')) {
+      case 'local':
+        return this.gravatar(size)
+        break;
+      case 'facebook':
+        return this.facebook(size);
+        break;
+      case 'google':
+        return this.gravatar(size);
+        break;
+    }
+  },
+  gravatar: function gravatar(size){
+    return 'http://gravatar.com/avatar/' + md5(this.get('id')) + '?d=retro&s=' + size; 
+  },
+  facebook: function facebook(size) {
+    return 'http://graph.facebook.com/' + this.get('id') + '/picture/?height=' + size;
+  }
+});
