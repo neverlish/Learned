42861d6b4dfa42df28584cba42463010046533b0
Jinho Hyeon <neverlish@gmail.com>
2017-10-06 15:25:52 +0900

    08 - 03 그런트를 사용한 폴더 비우기

--- node/acorn-redis-node-webapp/NodeChat/Gruntfile.js
+++ node/acorn-redis-node-webapp/NodeChat/Gruntfile.js
@@ -12,6 +12,11 @@ module.exports = function(grunt) {
           'js_src/src/chat.js': 'js_src/src/chat.pre.js'
         }
       }
+    },
+    clean: {
+      dist: {
+        src: ['static/js/*.js']
+      }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -22,6 +27,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // 기본 태스크
-  grunt.registerTask('default', ['nodeunit', 'preprocess']);
+  grunt.registerTask('default', ['nodeunit', 'preprocess', 'clean']);
   grunt.registerTask('prep', ['nodeunit', 'preprocess']);
 }
