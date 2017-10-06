53a1db85295b77087afdc3ca7de0a229bc8ad187
Jinho Hyeon <neverlish@gmail.com>
2017-10-06 22:25:50 +0900

    01 - 모델 제작

--- python/acorn-restful-python-web-service/Django01/gamesapi/games/models.py
+++ python/acorn-restful-python-web-service/Django01/gamesapi/games/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200, blank=True, default='')
+    release_date = models.DateTimeField()
+    game_category = models.CharField(max_length=200, blank=True, default='')
+    played = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('name',)
