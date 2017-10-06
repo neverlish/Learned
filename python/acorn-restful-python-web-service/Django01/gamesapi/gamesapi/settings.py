c5c957c61270275f91a05aaa47ca98c8a051e85b
Jinho Hyeon <neverlish@gmail.com>
2017-10-07 02:43:54 +0900

    03 - 페이지 나누기 클래스 사용자 정의

--- python/acorn-restful-python-web-service/Django01/gamesapi/gamesapi/settings.py
+++ python/acorn-restful-python-web-service/Django01/gamesapi/gamesapi/settings.py
@@ -124,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'games.pagination.LimitOffsetPaginationWithMaxLimit',
+    'PAGE_SIZE': 5
+}
