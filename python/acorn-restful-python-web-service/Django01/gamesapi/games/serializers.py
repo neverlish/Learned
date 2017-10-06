b4794661e844016765b8be548363aa9e0177cc84
Jinho Hyeon <neverlish@gmail.com>
2017-10-07 01:41:41 +0900

    02 - 관계 및 하이퍼링크를 사용해 직렬화 및 역직렬화 관리하기

--- python/acorn-restful-python-web-service/Django01/gamesapi/games/serializers.py
+++ python/acorn-restful-python-web-service/Django01/gamesapi/games/serializers.py
@@ -1,11 +1,74 @@
 from rest_framework import serializers
-from games.models import Game
+from games.models import GameCategory, Game, Player, PlayerScore
 
-class GameSerializer(serializers.ModelSerializer):
+class GameCategorySerializer(serializers.HyperlinkedModelSerializer):
+    games = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='game-detail')
+
+    class Meta:
+        model = GameCategory
+        fields = (
+            'url',
+            'pk',
+            'name',
+            'games')
+
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    # We want to display the game category's name instead of the id
+    game_category = serializers.SlugRelatedField(queryset=GameCategory.objects.all(), slug_field='name')
+
     class Meta:
         model = Game
-        fields = ('id',
-                  'name',
-                  'release_date',
-                  'game_category',
-                  'played')
+        fields = (
+            'url',
+            'game_category',
+            'name',
+            'release_date',
+            'played')
+
+class ScoreSerializer(serializers.HyperlinkedModelSerializer):
+    # We want to display all the details for the game
+    game = GameSerializer()
+
+    # We don't include the player because it will be nested in the player
+    class Meta:
+        model = PlayerScore
+        fields = (
+            'url',
+            'pk',
+            'score',
+            'score_date',
+            'game')
+
+class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+    scores = ScoreSerializer(many=True, read_only=True)
+    gender = serializers.ChoiceField(choices=Player.GENDER_CHOICES)
+    gender_description = serializers.CharField(
+        source='get_gender_display',
+        read_only=True)
+
+    class Meta:
+        model = Player
+        fields = (
+            'url',
+            'name',
+            'gender',
+            'gender_description',
+            'scores')
+
+class PlayerScoreSerializer(serializers.ModelSerializer):
+    player = serializers.SlugRelatedField(queryset=Player.objects.all(), slug_field='name')
+    # We want to display the game's name instead of the id
+    game = serializers.SlugRelatedField(queryset=Game.objects.all(), slug_field='name')
+
+    class Meta:
+        model = PlayerScore
+        fields = (
+            'url',
+            'pk',
+            'score',
+            'score_date',
+            'player',
+            'game')
