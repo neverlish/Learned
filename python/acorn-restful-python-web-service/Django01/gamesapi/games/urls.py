83a510b33f6849704f8d42b0ecabee0a5dec4ce7
Jinho Hyeon <neverlish@gmail.com>
2017-10-07 02:19:59 +0900

    02 - API에 대한 엔드포인트를 사용한 작업

--- python/acorn-restful-python-web-service/Django01/gamesapi/games/urls.py
+++ python/acorn-restful-python-web-service/Django01/gamesapi/games/urls.py
@@ -2,6 +2,13 @@ from django.conf.urls import url
 from games import views
 
 urlpatterns = [
-    url(r'^games/$', views.game_list),
-    url(r'^games/(?P<pk>[0-9]+)/$', views.game_detail),
+    url(r'^game-categories/$', views.GameCategoryList.as_view(), name=views.GameCategoryList.name),
+    url(r'^game-categories/(?P<pk>[0-9]+)/$', views.GameCategoryDetail.as_view(), name=views.GameCategoryDetail.name),
+    url(r'^games/$', views.GameList.as_view(), name=views.GameList.name),
+    url(r'^games/(?P<pk>[0-9]+)/$', views.GameDetail.as_view(), name=views.GameDetail.name),
+    url(r'^players/$', views.PlayerList.as_view(), name=views.PlayerList.name),
+    url(r'^players/(?P<pk>[0-9]+)/$', views.PlayerDetail.as_view(), name=views.PlayerDetail.name),
+    url(r'^player-scores/$', views.PlayerScoreList.as_view(), name=views.PlayerScoreList.name),
+    url(r'^player-scores/(?P<pk>[0-9]+)/$', views.PlayerScoreDetail.as_view(), name=views.PlayerScoreDetail.name),
+    url(r'^$', views.ApiRoot.as_view(), name=views.ApiRoot.name),
 ]
--- python/acorn-restful-python-web-service/Django01/gamesapi/games/views.py
+++ python/acorn-restful-python-web-service/Django01/gamesapi/games/views.py
@@ -43,3 +43,13 @@ class PlayerScoreDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = PlayerScore.objects.all()
     serializer_class = PlayerScoreSerializer
     name = 'playerscore-detail'
+
+class ApiRoot(generics.GenericAPIView):
+    name = 'api-root'
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'players': reverse(PlayerList.name, request=request),
+            'game-categories': reverse(GameCategoryList.name, request=request),
+            'games': reverse(GameList.name, request=request),
+            'scores': reverse(PlayerScoreList.name, request=request)
+        })
