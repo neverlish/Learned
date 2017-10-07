d33ad9088fed64a3c410da5df7a110d9cfc45de4
Jinho Hyeon <neverlish@gmail.com>
2017-10-07 20:18:11 +0900

    03 - 인증, 권한, 스로틀링에 대한 이해

--- python/acorn-restful-python-web-service/Django01/gamesapi/gamesapi/settings.py
+++ python/acorn-restful-python-web-service/Django01/gamesapi/gamesapi/settings.py
@@ -127,5 +127,9 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'games.pagination.LimitOffsetPaginationWithMaxLimit',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
