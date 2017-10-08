214e74bf3d652c80e083b4ccdd4ce89cdfab1177
Jinho Hyeon <neverlish@gmail.com>
2017-10-08 19:50:13 +0900

    07 - 사용자를 처리할 자원 클래스 작성

--- python/acorn-restful-python-web-service/Flask01/api/views.py
+++ python/acorn-restful-python-web-service/Flask01/api/views.py
@@ -9,6 +9,7 @@ from helpers import PaginationHelper
 api_bp = Blueprint('api', __name__)
 category_schema = CategorySchema()
 message_schema = MessageSchema()
+user_schema = UserSchema()
 api = Api(api_bp)
 auth = HTTPBasicAuth()
 
@@ -22,6 +23,52 @@ def verify_user_password(name, password):
 
 class AuthRequiredResource(Resource):
     method_decorators = [auth.login_required]
+
+class UserResource(AuthRequiredResource):
+    def get(self, id):
+        user = User.query.get_or_404(id)
+        result = user_schema.dump(user).data
+        return result
+
+class UserListResource(Resource):
+    @auth.login_required
+    def get(self):
+        pagination_helper = PaginationHelper(
+            request,
+            query = User.query,
+            resource_for_url = 'api.userlistresource',
+            key_name = 'results',
+            schema = user_schema)
+        result = pagination_helper.paginate_query()
+        return result
+
+    def post(self):
+        request_dict = request.get_json()
+        if not request_dict:
+            response = {'user': 'No input data provided'}
+            return response, status.HTTP_400_BAD_REQUEST
+        errors = user_schema.validate(request_dict)
+        if errors:
+            return errors, status.HTTP_400_BAD_REQUEST
+        name = request_dict['name']
+        existing_user = User.query.filter_by(name=name).first()
+        if existing_user is not None:
+            response = {'user': 'An user with the same name already exists'}
+            return response, status.HTTP_400_BAD_REQUEST
+        try:
+            user = User(name=name)
+            error_message, password_ok = user.check_password_strength_and_hash_if_ok(request_dict['password'])
+            if password_ok:
+                user.add(user)
+                query = User.query.get(user.id)
+                result = user_schema.dump(query).data
+                return result, status.HTTP_201_CREATED
+            else:
+                return {'error': error_message}, status.HTTP_400_BAD_REQUEST
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            resp = {'error': str(e)}
+            return resp, status.HTTP_400_BAD_REQUEST
 
 class MessageResource(AuthRequiredResource):
     def get(self, id):
@@ -185,7 +232,8 @@ class CategoryListResource(AuthRequiredResource):
             resp = jsonify({'error': str(e)})
             return resp, status.HTTP_400_BAD_REQUEST
 
-
+api.add_resource(UserListResource, '/users/')
+api.add_resource(UserResource, '/users/<int:id>')
 api.add_resource(CategoryListResource, '/categories/')
 api.add_resource(CategoryResource, '/categories/<int:id>')
 api.add_resource(MessageListResource, '/messages/')
