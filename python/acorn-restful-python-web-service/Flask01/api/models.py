dd4f2c2dd9f5b8c9775e18360bec60ea1bf5d12c
Jinho Hyeon <neverlish@gmail.com>
2017-10-08 14:54:24 +0900

    06 - 스키마를 사용해 모델을 유호화, 직렬화, 역직렬화하기

--- python/acorn-restful-python-web-service/Flask01/api/models.py
+++ python/acorn-restful-python-web-service/Flask01/api/models.py
@@ -38,3 +38,33 @@ class Category(db.Model, AddUpdateDelete):
 
     def __init__(self, name):
         self.name = name
+
+class CategorySchema(ma.Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True, validate=validate.Length(3))
+    url = ma.URLFor('api.categoryresource', id='<id>', _external=True)
+    messages = fields.Nested('MessageSchema', many=True, exclude=('category',))
+
+class MessageSchema(ma.Schema):
+    id = fields.Integer(dump_only=True)
+    message = fields.String(required=True, validate=validate.Length(1))
+    duration = fields.Integer()
+    creation_date = fields.DateTime()
+    category = fields.Nested(CategorySchema, only=['id', 'url', 'name'], required=True)
+    printed_times = fields.Integer()
+    printed_once = fields.Boolean()
+    url = ma.URLFor('api.messageresource', id='<id>', _external=True)
+
+    @pre_load
+    def process_category(self, data):
+        category = data.get('category')
+        if category:
+            if isinstance(category, dict):
+                category_name = category.get('name')
+            else:
+                category_name = category
+            category_dict = dict(name=category_name)
+        else:
+            category_dict = {}
+        data['category'] = category_dict
+        return data
