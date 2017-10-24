f044c87409d9d86587c307f94037a1a53cd8c80b
Jinho Hyeon <neverlish@gmail.com>
2017-10-24 23:40:25 +0900

    05 품질보증 - 06 페이지 테스트

--- node/hanbit-webdev-with-node-express/meadowlark.js
+++ node/hanbit-webdev-with-node-express/meadowlark.js
@@ -11,12 +11,20 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
 app.get('/', function(req, res) {
 	res.render('home');
 });
 
 app.get('/about', function(req, res) {
-	res.render('about', {fortune: fortune.getFortune()});
+	res.render('about', {
+		fortune: fortune.getFortune(), 
+		pageTestScript: '/qa/tests-about.js'
+	});
 });
 
 // 404 폴백 핸들러 (미들웨어)
