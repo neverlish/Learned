df436cbfa6ec7fa6f562d5c3a49f5ef1665b1f4d
Jinho Hyeon <neverlish@gmail.com>
2017-10-25 00:57:37 +0900

    05 - 11 그런트를 통한 자동화

---
+++ node/hanbit-webdev-with-node-express/Gruntfile.js
@@ -0,0 +1,29 @@
+// 프로젝트 디렉토리에서 grunt 실행
+
+module.exports = function(grunt) {
+	// 플러그인 로딩
+	[
+		'grunt-cafe-mocha',
+		'grunt-contrib-jshint',
+		'grunt-exec',
+	].forEach(function(task) {
+		grunt.loadNpmTasks(task);
+	});
+
+	// 플러그인 설정
+	grunt.initConfig({
+		cafemocha: {
+			all: { src: 'qa/tests-*.js', options: { ui: 'tdd' }, }
+		},
+		jshint: {
+			app: ['meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js'],
+			qa: ['Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js'], 
+		},
+		exec: {
+			linkchecker: {cmd: 'linkchecker http://localhost:3000'}
+		},
+	});
+
+	// 작업 등록
+	grunt.registerTask('default', ['cafemocha', 'jshint', 'exec']);
+}
