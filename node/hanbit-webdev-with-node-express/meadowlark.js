bdd760b2e245056d50c90d30c9ae42a62403ef31
Jinho Hyeon <neverlish@gmail.com>
2017-10-24 23:13:38 +0900

    04 모양새 갖추기 - 06 노드 모듈

--- node/hanbit-webdev-with-node-express/meadowlark.js
+++ node/hanbit-webdev-with-node-express/meadowlark.js
@@ -1,3 +1,4 @@
+var fortune = require('./lib/fortune.js');
 var express = require('express');
 var app = express();
 
@@ -10,21 +11,12 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
-
 app.get('/', function(req, res) {
 	res.render('home');
 });
 
 app.get('/about', function(req, res) {
-	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-	res.render('about', {fortune: randomFortune});
+	res.render('about', {fortune: fortune.getFortune()});
 });
 
 // 404 폴백 핸들러 (미들웨어)
