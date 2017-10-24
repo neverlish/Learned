40174f8d18519eb4c758fcc2a1deb4fe747001bd
Jinho Hyeon <neverlish@gmail.com>
2017-10-24 23:09:34 +0900

    03 - 03 - 3 동적 콘텐츠 뷰

--- node/hanbit-webdev-with-node-express/meadowlark.js
+++ node/hanbit-webdev-with-node-express/meadowlark.js
@@ -10,12 +10,21 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+var fortunes = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple.",
+];
+
 app.get('/', function(req, res) {
 	res.render('home');
 });
 
 app.get('/about', function(req, res) {
-	res.render('about');
+	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', {fortune: randomFortune});
 });
 
 // 404 폴백 핸들러 (미들웨어)
