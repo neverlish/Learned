4467b3dedfd1c3b69706dd302911ba1958dad78f
Jinho Hyeon <neverlish@gmail.com>
2017-10-24 23:07:49 +0900

    03 - 03 - 1 뷰와 레이아웃

--- node/hanbit-webdev-with-node-express/meadowlark.js
+++ node/hanbit-webdev-with-node-express/meadowlark.js
@@ -1,31 +1,32 @@
 var express = require('express');
 var app = express();
 
+// 핸들바 뷰 엔진 설정
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
-	res.render('text/plain');
-	res.send('Meadowlark Travel');
+	res.render('home');
 });
 
 app.get('/about', function(req, res) {
-	res.render('text/plain');
-	res.send('About Meadowlark Travel');
+	res.render('about');
 });
 
 // 404 폴백 핸들러 (미들웨어)
 app.use(function(req, res) {
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found')
+	res.render('404');
 });
 
 // 500 에러 핸들러 (미들웨어)
 app.use(function(err, req, res, next) {
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error')
+	res.render('500');
 });
 
 app.listen(app.get('port'), function() { 
