586742771b9777e22cf48946cbb04eb0bc66f608
Jinho Hyeon <neverlish@gmail.com>
2017-10-30 01:15:52 +0900

    18 - 03 - 8 인증 제공자 추가

--- node/hanbit-webdev-with-node-express/credentials.js
+++ node/hanbit-webdev-with-node-express/credentials.js
@@ -18,6 +18,12 @@ module.exports = {
 				appId: process.env.FB_DEV_APP_ID,
 				appSecret: process.env.FB_DEV_APP_SECRET_CODE,
 			}
+		},
+		google: {
+			development: {
+				clientID: process.env.GOOGLE_DEV_CLIENT_ID,
+				clientSecret: process.env.GOOGLE_DEV_CLIENT_SECRET_CODE,
+			}
 		}
 	}
 };
--- node/hanbit-webdev-with-node-express/lib/auth.js
+++ node/hanbit-webdev-with-node-express/lib/auth.js
@@ -1,6 +1,7 @@
 var User = require('../models/user.js'),
     passport = require('passport'),
-    FacebookStrategy = require('passport-facebook').Strategy;
+    FacebookStrategy = require('passport-facebook').Strategy,
+    GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 passport.serializeUser(function(user, done) {
   done(null, user._id);
@@ -46,7 +47,29 @@ module.exports = function(app, options) {
             done(null, user);
           });
         });
-      }))
+      }));
+
+      passport.use(new GoogleStrategy({
+        clientID: config.google[env].clientID,
+        clientSecret: config.google[env].clientSecret,
+        callbackURL: (options.baseUrl || '') + '/auth/google/callback',
+      }, function(accessToken, refreshToken, profile, done) {
+        var authId = 'google:' + profile.id;
+        User.findOne({ authId: authId }, function(err, user) {
+          if(err) return done(err, null);
+          if(user) return done(null, user);
+          user = new User({
+            authId: authId,
+            name: profile.displayName,
+            created: Date.now(),
+            role: 'customer',
+          });
+          user.save(function(err) {
+            if(err) return done(err, null);
+            done(null, user);
+          });
+        });
+      }));
 
       app.use(passport.initialize());
       app.use(passport.session());
@@ -67,7 +90,22 @@ module.exports = function(app, options) {
           if(redirect) delete req.session.authRedirect;
           res.redirect(303, redirect || options.successRedirect);
         }
-      )
+      );
+      // 구글 라우트 등록
+      app.get('/auth/google', function(req, res, next) {
+        if(req.query.redirect) req.session.authRedirect = req.query.redirect;
+        passport.authenticate('google', {scope: 'profile'})(req, res, next);
+      });
+      app.get(
+        '/auth/google/callback', 
+        passport.authenticate('google', {failureRedirect: options.failureRedirect}),
+        function(req, res) {
+          // 인증이 성공해야 여기 도달합니다.
+          var redirect = req.session.authRedirect;
+          if(redirect) delete req.session.authRedirect;
+          res.redirect(303, redirect || options.successRedirect);
+        }
+      );
     },
   }
 }
