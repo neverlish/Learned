d8fc7e7ea9608bde020f41c2515aec1f770e5729
Jinho Hyeon <neverlish@gmail.com>
2017-10-29 11:20:17 +0900

    13 - 03 데이터베이스 지속성 - 3 몽구스

--- node/hanbit-webdev-with-node-express/credentials.js
+++ node/hanbit-webdev-with-node-express/credentials.js
@@ -3,5 +3,13 @@ module.exports = {
 	gmail: {
 		user: process.env.GOOGLE_USER,
 		password: process.env.GOOGLE_PASSWORD,
+	},
+	mongo: {
+		development: {
+			connectionString: process.env.MLAB
+		},
+		production: {
+			connectionString: process.env.MLAB
+		}
 	}
 };
