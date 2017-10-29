35c2623278b7743218efafaa1e6dfbf2a8717671
Jinho Hyeon <neverlish@gmail.com>
2017-10-29 11:50:20 +0900

    13 - 03 - 6 초기 데이터 만들기

--- node/hanbit-webdev-with-node-express/meadowlark.js
+++ node/hanbit-webdev-with-node-express/meadowlark.js
@@ -169,6 +169,54 @@ switch(app.get('env')) {
 
 var Vacation = require('./models/vacation.js');
 
+Vacation.find(function(err, vacations) {
+	if (vacations.length) return;
+
+	new Vacation({
+		name: 'Hood River Day Trip',
+		slug: 'hood-river-day-trip',
+		category: 'Day Trip',
+		sku: 'HR199',
+		description: 'Spend a day sailing on the Columbia and enjoying craft beers in Hood River!',
+		priceInCents: 9995,
+		tags: ['day trip', 'hood river', 'sailing', 'windsurfing', 'breweries'],
+		inSeason: true,
+		maximumGuests: 16,
+		available: true,
+		packagesSold: 0,
+	}).save();
+
+	new Vacation({
+		name: 'Oregon Coast Getaway',
+		slug: 'oregon-coast-getaway',
+		category: 'Weekend Getaway',
+		sku: 'OC39',
+		description: 'Enjoy the ocean air and quaint coastal towns!',
+		priceInCents: 269995,
+		tags: ['weekend getaway', 'oregon coast', 'beachcombing'],
+		inSeason: false,
+		maximumGuests: 8,
+		available: true,
+		packagesSold: 0,
+	}).save();
+
+	new Vacation({
+		name: 'Rock Climbing in Bend',
+		slug: 'rock-climbing-in-bend',
+		category: 'Adventure',
+		sku: 'B99',
+		description: 'Experience the thrill of climbing in the hight desert',
+		priceInCents: 289995,
+		tags: ['weekend getaway', 'bend', 'high desert', 'rock climbing'],
+		inSeason: true,
+		requiresWaiver: true,
+		maximumGuests: 4,
+		available: false,
+		packagesSold: 0,
+		notes: 'The tour guide is currently recovering from a skiiing accident',
+	}).save();
+})
+
 app.use(function(req, res, next) {
 	// 플래시 메시지가 있다면 콘텍스트에 전달한 다음 지웁니다.
 	res.locals.flash = req.session.flash;
