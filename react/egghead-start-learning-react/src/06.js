838c74ede105833428b8e8f9a6c33fe551111892
Jinho Hyeon <neverlish@gmail.com>
2017-11-11 16:38:17 +0900

    06 Use React Components as Children for Other Components

---
+++ react/egghead-start-learning-react/src/06.js
@@ -0,0 +1,27 @@
+// 06 Use React Components as Children for Other Components
+
+import React from 'react';
+ 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state txt'
+    }
+  }  
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
+
+  render() {
+    return <div>
+      <h1>{this.state.txt}</h1>
+      <Widget update={this.update.bind(this)}/>
+    </div>
+  }
+}
+
+const Widget = (props) => 
+  <input type='text' onChange={props.update}/>
+
+export default App
--- react/egghead-start-learning-react/src/index.js
+++ react/egghead-start-learning-react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './05';
+import App from './06';
 
 ReactDOM.render(
   <App/>, 
