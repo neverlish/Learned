15869bbe9663485dd55080b3231a6fdb59b26a5d
Jinho Hyeon <neverlish@gmail.com>
2017-11-11 16:46:14 +0900

    14 Use map to Create React Components from Arrays of Data

---
+++ react/egghead-start-learning-react/src/14.js
@@ -0,0 +1,36 @@
+// 14 Use map to Create React Components from Arrays of Data
+
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {items: []}
+  }
+  componentWillMount() {
+    fetch('https://swapi.co/api/people/?format=json')
+      .then(response => response.json())
+      .then(({results: items}) => this.setState({items}))
+  }
+  filter(e) {
+    this.setState({filter: e.target.value})
+  }
+  render() {
+    let items = this.state.items;
+    if (this.state.filter) {
+      items = items.filter(item => 
+        item.name.toLowerCase().includes(this.state.filter.toLowerCase()))
+    }
+    return (
+      <div>
+        <input type='text' onChange={this.filter.bind(this)}/>
+        {items.map(item => 
+          <Person key={item.name} person={item}/>)}
+      </div>
+    )
+  }
+}
+
+const Person = (props) => <h4>{props.person.name}</h4>
+
+export default App
--- react/egghead-start-learning-react/src/index.js
+++ react/egghead-start-learning-react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './13';
+import App from './14';
 
 ReactDOM.render(
   <App/>, 
