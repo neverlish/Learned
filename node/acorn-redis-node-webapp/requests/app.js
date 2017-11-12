35dd3fda518cd34b890237d4c81fbb1af882803a
Jinho Hyeon <neverlish@gmail.com>
2017-11-12 14:04:31 +0900

    10 - 02 디버깅 - 프론트엔드 디버깅

--- node/acorn-redis-node-webapp/requests/app.js
+++ node/acorn-redis-node-webapp/requests/app.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/static'));
+
 app.get('/', function(req, res) {
   res.set('Important-Header', 'text-value');
-  res.send('hello world');
+  res.send('hello world<script src="/debug.js"></script>');
 });
 
 app.post('/', function(req, res) {
   res.send('<h1>POSTED</h1>');
+  // in browser console
+  /*
+    var xhr = new XMLHttpRequest();
+    var data = new FormData();
+    data.append('test', 'test-value');
+    xhr.open('POST','/',true);
+    xhr.send(data);
+  */
 });
 
 app.listen(3000);
