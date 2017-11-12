d20f4b7dbfa82ed2e052283c5c8e23f44473fb05
Jinho Hyeon <neverlish@gmail.com>
2017-11-12 23:36:34 +0900

    2 - Model 클래스 만들기

--- django/inflearn-codesquad-webservice/app1/lotto/models.py
+++ django/inflearn-codesquad-webservice/app1/lotto/models.py
@@ -1,5 +1,25 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+import random
 
 # Create your models here.
+
+class GuessNumbers(models.Model):
+  name = models.CharField(max_length = 24)
+  lottos = models.CharField(max_length = 255, default = '[1,2,3,4,5,6]')
+  text = models.CharField(max_length = 255)
+  num_lotto = models.IntegerField(default = 5)
+  update_date = models.DateTimeField()
+
+  def generate(self):
+    self.lottos = ''
+    origin = list(range(1, 46))
+    for _ in range(0, self.num_lotto):
+      random.shuffle(origin)
+      guess = origin[:6]
+      guess.sort()
+      self.lottos += str(guess) + '\n'
+    self.update_date = timezone.now()
+    self.save()
