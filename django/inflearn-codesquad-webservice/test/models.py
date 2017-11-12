5d8def804227b7486395feda3f560a6c10aee2b2
Jinho Hyeon <neverlish@gmail.com>
2017-11-12 22:43:46 +0900

    1 - 오브젝트 실습2

--- django/inflearn-codesquad-webservice/test/models.py
+++ django/inflearn-codesquad-webservice/test/models.py
@@ -3,8 +3,27 @@ class Text:
     self.text = str
 
   def __str__(self):
-    return 'Object: ' + self.text
+    return 'Text: ' + self.text
+
+class User:
+  numUsers = 0
+  def __init__(self, name):
+    self.numArticle = 0
+    self.name = name
+    self.articles = []
+    User.numUsers += 1
+
+  def write(self, text):
+    self.articles.append(text)
+    self.numArticle += 1
+
+  def __str__(self):
+    return 'User : %s %s' % (self.name, self.articles)
 
 t = Text('This is some text.')
-print(t)
-print(t.text)
+t2 = Text('This is some text2')
+user = User('Honux')
+user.write(t2)
+user.write(t2)
+print(t) # Text: This is some text.
+print(user, user.numArticle) # # User : Honux [<__main__.Text object at 0x10d9ae6a0>, <__main__.Text object at 0x10d9ae6a0>] 2
