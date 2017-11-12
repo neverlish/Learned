8d8e274a49fd6e8398b71974a0125e2a6f7a1d51
Jinho Hyeon <neverlish@gmail.com>
2017-11-12 14:08:01 +0900

    10 - 03 애플리케이션 CPU 프로파일링

--- node/acorn-redis-node-webapp/requests/app.js
+++ node/acorn-redis-node-webapp/requests/app.js
@@ -1,9 +1,18 @@
+var agent = require('webkit-devtools-agent');
 var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/static'));
 
+function CPUWaster(){
+  var j;
+  for(var i=0; i < 10000000; i++){
+    j = Math.sqrt(Math.pow(i, i));
+  };
+}
+
 app.get('/', function(req, res) {
+  CPUWaster();
   res.set('Important-Header', 'text-value');
   res.send('hello world<script src="/debug.js"></script>');
 });
