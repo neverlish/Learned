60ea6a52df4689605974cc478251be453ba9ed93
Jinho Hyeon <neverlish@gmail.com>
2017-11-18 03:32:43 +0900

    3 - POST 처리

--- django/inflearn-codesquad-webservice/app2/polls/views.py
+++ django/inflearn-codesquad-webservice/app2/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
 
@@ -18,4 +18,15 @@ def results(request, question_id):
   return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-  return HttpResponse("You're voting on question %s." % question_id)
+  question = get_object_or_404(Question, pk = question_id)
+  try:
+    selected_choice = question.choice_set.get(pk = request.POST['choice'])
+  except:
+    return render(request, 'polls/detail.html', {
+      'question': question,
+      'error_message': "You didn't select a choice."
+    })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return redirect('polls:results', question_id = question.id)
