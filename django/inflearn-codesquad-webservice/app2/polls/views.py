6b5ac83453fd68d44ac82b2be7e6024ecf180417
Jinho Hyeon <neverlish@gmail.com>
2017-11-18 00:10:52 +0900

    3 - template 연동 1

--- django/inflearn-codesquad-webservice/app2/polls/views.py
+++ django/inflearn-codesquad-webservice/app2/polls/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-  return HttpResponse('Hello, CodeSquad')
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  context = {'latest_question_list': latest_question_list}
+  return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
   return HttpResponse("You're looking at question %s." % question_id)
