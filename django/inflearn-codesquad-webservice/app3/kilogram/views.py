ee8aca93faafc2bdbc17fa42951897fe26fd92aa
Jinho Hyeon <neverlish@gmail.com>
2017-11-25 10:48:05 +0900

    4 SNS 이미지 앱 구현하기 - 킬로그램 앱 초기화

---
+++ django/inflearn-codesquad-webservice/app3/kilogram/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+
+class IndexView(TemplateView):
+  template_name = 'kilogram/index.html'
