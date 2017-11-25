09fd6a5fba74844514dbf4efc6c9aa66893ee554
Jinho Hyeon <neverlish@gmail.com>
2017-11-25 20:33:11 +0900

    4 - photo form 만들기 / login 템플릿 수정

--- django/inflearn-codesquad-webservice/app3/kilogram/forms.py
+++ django/inflearn-codesquad-webservice/app3/kilogram/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from .models import Photo
 
 class CreateUserForm(UserCreationForm):
   email = forms.EmailField(required = True)
@@ -15,3 +16,11 @@ class CreateUserForm(UserCreationForm):
     if commit:
       user.save()
     return user
+
+class UploadForm(forms.ModelForm):
+  comment = forms.CharField(max_length = 255)
+
+  class Meta:
+    model = Photo
+    exclude = ('thumbnail_image', 'owner')
+    
