801efd7e4682b38eff8dba9a019d99285ed9665c
Jinho Hyeon <neverlish@gmail.com>
2017-11-25 09:18:53 +0900

    3 - view 및 템플릿 테스트하기

--- django/inflearn-codesquad-webservice/app2/polls/views.py
+++ django/inflearn-codesquad-webservice/app2/polls/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
   template_name = 'polls/index.html'
   context_object_name = 'latest_question_list'
 
   def get_queryset(self):
-    return Question.objects.order_by('-pub_date')[:5]
+    return Question.objects.filter(
+      pub_date__lte = timezone.now()
+    ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
   model = Question
