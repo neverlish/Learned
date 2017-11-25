73fb27df952ce4f535cc3104852b66ace7483a88
Jinho Hyeon <neverlish@gmail.com>
2017-11-25 17:42:53 +0900

    4 - 사용자 등록 구현

--- django/inflearn-codesquad-webservice/app3/kilogram/views.py
+++ django/inflearn-codesquad-webservice/app3/kilogram/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+from .forms import CreateUserForm
+from django.core.urlresolvers import reverse_lazy
 
 # Create your views here.
 
 class IndexView(TemplateView):
   template_name = 'kilogram/index.html'
+
+class CreateUserView(CreateView):
+  template_name = 'registration/signup.html'
+  form_class = CreateUserForm
+  success_url = reverse_lazy('create_user_done')
+
+class RegisteredView(TemplateView):
+  template_name = 'registration/signup_done.html'
