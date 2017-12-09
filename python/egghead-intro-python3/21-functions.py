e78f2bbec13a7c41ae7a61d6a133888672613266
Jinho Hyeon <neverlish@gmail.com>
2017-12-09 15:59:51 +0900

    21 Create Reusable Components with Functions in Python

---
+++ python/egghead-intro-python3/21-functions.py
@@ -0,0 +1,22 @@
+# 21 Create Reusable Components with Functions in Python
+
+def say_name(name):
+  """
+  Returns name passed in as a parameter
+  """
+  return name
+
+foo = say_name('will')
+print(foo)
+
+def add(num1=1, num2=2):
+  return num1 + num2
+
+num1 = add()
+print(num1)
+
+def madlibs(name, noun='shoes', adjective='red'):
+  return '{0} has {1} {2}'.format(name, adjective, noun)
+
+madlib = madlibs('Will', adjective='black', noun='boots')
+print(madlib)
