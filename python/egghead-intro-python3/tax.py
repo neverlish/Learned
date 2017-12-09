f01380e21410047a2c8e32f0de1fcfb747c07acd
Jinho Hyeon <neverlish@gmail.com>
2017-12-09 16:14:19 +0900

    22 Reuse Code in Multiple Projects with Python Modules

---
+++ python/egghead-intro-python3/tax.py
@@ -0,0 +1,13 @@
+# 22 Reuse Code in Multiple Projects with Python Modules
+
+def total(n):
+  tax_rate = .07
+  return n * tax_rate + n
+
+def tax_amount(n):
+  tax_rate = .07
+  return n * tax_rate
+
+if __name__ == '__main__':
+  import sys
+  print(total(int(sys.argv[1])))
