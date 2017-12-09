a6ed649450b157e720dc100cf28f6efe7608865a
Jinho Hyeon <neverlish@gmail.com>
2017-12-09 17:17:55 +0900

    25 Handle Exceptions to prevent crashes in Python

---
+++ python/egghead-intro-python3/25-errors.py
@@ -0,0 +1,12 @@
+# 25 Handle Exceptions to prevent crashes in Python
+
+import sys
+
+# print(int(sys.argv[1]) / int(sys.argv[2]))
+
+try:
+  print(int(sys.argv[1]) / int(sys.argv[2]))
+except ValueError as e:
+  print('You must enter a valid number')
+except ZeroDivisionError as e:
+  print("You can't divide by zero")
