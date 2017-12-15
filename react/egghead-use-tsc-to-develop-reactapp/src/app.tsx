97dad92683640e57e927a4a9c8f832d550dadeb5
Jinho Hyeon <neverlish@gmail.com>
2017-12-16 08:21:59 +0900

    4 Create stateless React components using TypeScript

--- react/egghead-use-tsc-to-develop-reactapp/src/app.tsx
+++ react/egghead-use-tsc-to-develop-reactapp/src/app.tsx
@@ -1,10 +1,32 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-type AppProps = {message: string}
-const App: React.SFC<AppProps> = ({message}) => <div>{message}</div>;
+type AppProps = { 
+  message: string,
+};
+type AppState =  { 
+  count: number,
+};
+class App extends React.Component <AppProps, AppState>{
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+  render() {
+    return (
+      <div onClick={this.increment}>{this.props.message} {this.state.count}</div>
+    )
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+}
 
 ReactDOM.render(
-  <App message='Hello world again!'/>, 
+  <App message='Hello again'/>, 
   document.getElementById('root')
 );
