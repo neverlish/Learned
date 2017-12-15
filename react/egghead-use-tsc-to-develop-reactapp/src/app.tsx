6992e921d609f5b1a9f39432e04220697c37d8e9
Jinho Hyeon <neverlish@gmail.com>
2017-12-16 08:37:15 +0900

    [react] Egghead: Use TypeScript to develop React Applications

---
+++ react/egghead-use-tsc-to-develop-reactapp/src/app.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+type AppProps = { 
+  message: string,
+};
+type AppState =  { 
+  count: number,
+};
+class App extends React.Component <AppProps, AppState>{
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+  render() {
+    return (
+      <div onClick={this.increment}>{this.props.message} {this.state.count}</div>
+    )
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+}
+
+ReactDOM.render(
+  <App message='Hello again'/>, 
+  document.getElementById('root')
+);
