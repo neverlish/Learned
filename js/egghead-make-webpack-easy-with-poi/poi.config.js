94047f21030862b57ca928b21036e06d100c81c0
Jinho Hyeon <neverlish@gmail.com>
2017-12-16 09:25:01 +0900

    03 Configure Poi for Advanced Template Control

---
+++ js/egghead-make-webpack-easy-with-poi/poi.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  html: {
+    title: "My Amazing App",
+    description: "my description",
+    template: './index.ejs',
+    course: 'Poi Lessons',
+    contributors: ["Mindy", "Joh"]
+  }
+}
