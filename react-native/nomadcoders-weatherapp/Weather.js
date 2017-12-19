c37c3216da04e0ed27b88e09321b19148c65c2fc
Jinho Hyeon <neverlish@gmail.com>
2017-12-19 23:42:37 +0900

    3-3 Working with Icons

--- react-native/nomadcoders-weatherapp/Weather.js
+++ react-native/nomadcoders-weatherapp/Weather.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {LinearGradient} from 'expo';
+import {Ionicons} from '@expo/vector-icons';
 
 export default class Weather extends Component {
   render() {
     return (
-      <LinearGradient colors={['#00C6FB', '#005BEA', 'red']} style={styles.container}>
+      <LinearGradient colors={['#00C6FB', '#005BEA']} style={styles.container}>
         <View style={styles.upper}>
-          <Text>Icon here!</Text>
+          <Ionicons color='white' size={144} name='ios-rainy'/>
           <Text style={styles.temp}>35°</Text>
         </View>
         <View style={styles.lower}>
@@ -26,10 +27,11 @@ const styles = StyleSheet.create({
   upper: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
   },
   temp: {
-    fontSize: 38,
+    fontSize: 48,
     backgroundColor: 'transparent',
     color: 'white',
     marginTop: 10
