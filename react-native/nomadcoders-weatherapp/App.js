f69a67ecc628e362c9131e9612311068dddd3410
Jinho Hyeon <neverlish@gmail.com>
2017-12-20 02:38:13 +0900

    4-2 Hooking the component to the weather data

--- react-native/nomadcoders-weatherapp/App.js
+++ react-native/nomadcoders-weatherapp/App.js
@@ -35,12 +35,12 @@ export default class App extends React.Component {
     })
   }
   render() {
-    const { isLoaded, error } = this.state;
+    const { isLoaded, error, temperature, name } = this.state;
     return (
       <View style={styles.container}>
       <StatusBar hidden={true}/>
         {isLoaded ? (
-          <Weather/> 
+          <Weather weatherName={name} temp={Math.floor(temperature - 273.15)}/> 
         ) : (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the fucking weather</Text>
--- react-native/nomadcoders-weatherapp/Weather.js
+++ react-native/nomadcoders-weatherapp/Weather.js
@@ -2,23 +2,74 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {LinearGradient} from 'expo';
 import {Ionicons} from '@expo/vector-icons';
+import PropTypes from 'prop-types';
 
-export default class Weather extends Component {
-  render() {
-    return (
-      <LinearGradient colors={['#00C6FB', '#005BEA']} style={styles.container}>
-        <View style={styles.upper}>
-          <Ionicons color='white' size={144} name='ios-rainy'/>
-          <Text style={styles.temp}>35°</Text>
-        </View>
-        <View style={styles.lower}>
-          <Text style={styles.title}>Raining like a MF</Text>
-          <Text style={styles.subtitle}>For more info look outside</Text>
-        </View>
-      </LinearGradient>
-    );
+const weatherCases = {
+  Rain: {
+    colors: ['#00C6FB', '#005BEA'],
+    title: 'Raining like a MF',
+    subtitle: 'For more info look outside',
+    icon: 'ios-rainy'
+  },
+  Clear: {
+    colors: ['#FEF253', '#FF7300'],
+    title: 'Sunny as fuck',
+    subtitle: 'Go get your ass burnt',
+    icon: 'ios-sunny'
+  },
+  Thunderstorm: {
+    colors: ['#00ECBC', '#007ADF'],
+    title: 'Thunderstorm in the house',
+    subtitle: 'Actually, outside of the house',
+    icon: 'ios-thunderstorm'
+  },
+  Clouds: {
+    colors: ['#D7D2CC', '#304352'],
+    title: 'Clouds',
+    subtitle: 'I know, fucking boring',
+    icon: 'ios-cloudy'
+  },
+  Snow: {
+    colors: ['#7DE2FC', '#89B6E5'],
+    title: 'Cold as balls',
+    subtitle: 'Do you want to build a snowman? Fuck no.',
+    icon: 'ios-snow'
+  },
+  Drizzle: {
+    colors: ['#89F7FE', '#66A6FF'],
+    title: 'Drizzle',
+    subtitle: 'Is like rain, but gay',
+    icon: 'ios-rainy-outline'
+  },
+  Haze: {
+    colors: ['#89F7FE', '#66A6FF'],
+    title: 'Haze',
+    subtitle: 'Is like rain, but gay',
+    icon: 'ios-rainy-outline'
   }
 }
+
+function Weather({weatherName, temp}) {
+  console.log(weatherName)
+  return (
+    <LinearGradient colors={weatherCases[weatherName].colors} style={styles.container}>
+      <View style={styles.upper}>
+        <Ionicons color='white' size={144} name={weatherCases[weatherName].icon}/>
+        <Text style={styles.temp}>{temp}°</Text>
+      </View>
+      <View style={styles.lower}>
+        <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
+        <Text style={styles.subtitle}>{weatherCases[weatherName].subtitle}</Text>
+      </View>
+    </LinearGradient>
+  )
+}
+
+Weather.propTypes = {
+  temp: PropTypes.number
+}
+
+export default Weather;
 
 const styles = StyleSheet.create({
   container: {
