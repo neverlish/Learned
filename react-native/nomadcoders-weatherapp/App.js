320ec4093052177b89e8deb4de12e7775e51afb0
Jinho Hyeon <neverlish@gmail.com>
2017-12-20 02:28:57 +0900

    5-1 Changing the icons and testing on iOS

--- react-native/nomadcoders-weatherapp/App.js
+++ react-native/nomadcoders-weatherapp/App.js
@@ -40,7 +40,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
       <StatusBar hidden={true}/>
         {isLoaded ? (
-          <Weather weatherName={name} temp={Math.floor(temperature - 273.15)}/> 
+          <Weather weatherName={name} temp={Math.ceil(temperature - 273.15)}/> 
         ) : (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the fucking weather</Text>
--- react-native/nomadcoders-weatherapp/Weather.js
+++ react-native/nomadcoders-weatherapp/Weather.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {LinearGradient} from 'expo';
-import {Ionicons} from '@expo/vector-icons';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
 import PropTypes from 'prop-types';
 
 const weatherCases = {
@@ -9,43 +9,49 @@ const weatherCases = {
     colors: ['#00C6FB', '#005BEA'],
     title: 'Raining like a MF',
     subtitle: 'For more info look outside',
-    icon: 'ios-rainy'
+    icon: 'weather-rainy'
   },
   Clear: {
     colors: ['#FEF253', '#FF7300'],
     title: 'Sunny as fuck',
     subtitle: 'Go get your ass burnt',
-    icon: 'ios-sunny'
+    icon: 'weather-sunny'
   },
   Thunderstorm: {
     colors: ['#00ECBC', '#007ADF'],
     title: 'Thunderstorm in the house',
     subtitle: 'Actually, outside of the house',
-    icon: 'ios-thunderstorm'
+    icon: 'weather-lightning'
   },
   Clouds: {
     colors: ['#D7D2CC', '#304352'],
     title: 'Clouds',
     subtitle: 'I know, fucking boring',
-    icon: 'ios-cloudy'
+    icon: 'weather-cloudy'
   },
   Snow: {
     colors: ['#7DE2FC', '#89B6E5'],
     title: 'Cold as balls',
     subtitle: 'Do you want to build a snowman? Fuck no.',
-    icon: 'ios-snow'
+    icon: 'weather-snowy'
   },
   Drizzle: {
     colors: ['#89F7FE', '#66A6FF'],
     title: 'Drizzle',
     subtitle: 'Is like rain, but gay',
-    icon: 'ios-rainy-outline'
+    icon: 'weather-hail'
   },
   Haze: {
     colors: ['#89F7FE', '#66A6FF'],
     title: 'Haze',
-    subtitle: 'Is like rain, but gay',
-    icon: 'ios-rainy-outline'
+    subtitle: "Don't know what that",
+    icon: 'weather-windy'
+  },
+  Mist: {
+    colors: ['#D7D2CC', '#304352'],
+    title: 'Mist!',
+    subtitle: "It's like you have no glasses on.",
+    icon: 'weather-fog'
   }
 }
 
@@ -54,7 +60,7 @@ function Weather({weatherName, temp}) {
   return (
     <LinearGradient colors={weatherCases[weatherName].colors} style={styles.container}>
       <View style={styles.upper}>
-        <Ionicons color='white' size={144} name={weatherCases[weatherName].icon}/>
+        <MaterialCommunityIcons color='white' size={144} name={weatherCases[weatherName].icon}/>
         <Text style={styles.temp}>{temp}°</Text>
       </View>
       <View style={styles.lower}>
