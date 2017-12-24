89d0b3d07e793452acccff4fe0444b29f7c60d10
Jinho Hyeon <neverlish@gmail.com>
2017-12-24 23:53:23 +0900

    5-1 Smart vs Dumb Components

--- react/nomadcoders-react-webservice/src/Movie.js
+++ react/nomadcoders-react-webservice/src/Movie.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Movie extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string
-  }
-
-  render() {
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster}/>
-        <h1>{this.props.title}</h1>
+function Movie({title, poster}) {
+  return (
+    <div>
+        <MoviePoster poster={poster}/>
+        <h1>{title}</h1>
       </div>
-    );
-  }
+  )
 }
 
-class MoviePoster extends Component {
-  render() {
-    return (
-      <img src={this.props.poster} />
-    )
-  }
+function MoviePoster({poster}) {
+  return (
+    <img src={poster} alt='Movie Poster'/>
+  )
+}
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string
+}
+
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
 }
 
 export default Movie;
