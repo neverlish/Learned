0755eae37eacb48a604e03fb7cf37daab2c1c5b1
Jinho Hyeon <neverlish@gmail.com>
2017-12-25 00:25:16 +0900

    6-1 Ajax in React

--- react/nomadcoders-react-webservice/src/App.js
+++ react/nomadcoders-react-webservice/src/App.js
@@ -3,36 +3,10 @@ import './App.css';
 import Movie from './Movie';
 
 class App extends Component {
-  state = {
-  }
+  state = {}
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: 'Matrix',
-            poster: 'https://cdn.empireonline.com/jpg/70/0/0/640/480/aspectfit/0/0/0/0/0/0/c/articles/58c8989331e02c7e3baf82ee/neo-matrix-keanu-reeves.jpg'
-          },
-          {
-            title: 'Full Metal Jacket',
-            poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BNzc2ZThkOGItZGY5YS00MDYwLTkyOTAtNDRmZWIwMGRhYTc0L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR79,0,630,1200_AL_.jpg'
-          },
-          {
-            title: 'Oldboy',
-            poster: 'https://i.ytimg.com/vi/2HkjrJ6IK5E/hqdefault.jpg'
-          },
-          {
-            title: 'Star Wars',
-            poster: 'https://heroichollywood.b-cdn.net/wp-content/uploads/2017/05/Star-Wars-saga-1-6.png?x42694'
-          },
-          {
-            title: 'Trainspotting',
-            poster: 'https://upload.wikimedia.org/wikipedia/en/7/71/Trainspotting_ver2.jpg'
-          }
-        ]
-      })
-    }, 5000)
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
   }
 
   _renderMovies = () => {
