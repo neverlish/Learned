4785bd7399e8eea648b0633deb19061d2f26a02a
Jinho Hyeon <neverlish@gmail.com>
2017-12-25 02:12:44 +0900

    7-1 Updating Component

--- react/nomadcoders-react-webservice/src/App.js
+++ react/nomadcoders-react-webservice/src/App.js
@@ -11,7 +11,14 @@ class App extends Component {
 
   _renderMovies = () => {
     const movies = this.state.movies.map(movie => {
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
+      console.log(movie)
+      return <Movie
+        title={movie.title}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+      />
     })
     return movies
   }
