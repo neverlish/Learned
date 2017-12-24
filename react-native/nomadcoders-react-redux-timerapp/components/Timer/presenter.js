a528a478ed928db8405e25ad4bc8a2de1acda1db
Jinho Hyeon <neverlish@gmail.com>
2017-12-24 13:39:36 +0900

    15 Turning seconds into minutes

--- react-native/nomadcoders-react-redux-timerapp/components/Timer/presenter.js
+++ react-native/nomadcoders-react-redux-timerapp/components/Timer/presenter.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, StatusBar } from 'react-native';
 import Button from '../Button';
 
+function formatTime(time) {
+  let minutes = Math.floor(time / 60);
+  time -= minutes * 60;
+  let seconds = parseInt(time % 60, 10);
+  return `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+}
+
 class Timer extends Component {
   componentWillReceiveProps(nextProps) {
     const currentProps = this.props;
@@ -18,7 +25,6 @@ class Timer extends Component {
   }
 
   render() {
-    console.log(this.props)
     const { 
       isPlaying,
       elapsedTime,
@@ -31,7 +37,9 @@ class Timer extends Component {
       <View style={styles.container}>
         <StatusBar barStyle={"light-content"} />
         <View style={styles.upper}>
-          <Text style={styles.time}>25:00</Text>
+          <Text style={styles.time}>
+            {formatTime(timerDuration - elapsedTime)}
+          </Text>
         </View>
         <View style={styles.lower}>
           {!isPlaying && <Button iconName='play-circle' onPress={startTimer}/>}
