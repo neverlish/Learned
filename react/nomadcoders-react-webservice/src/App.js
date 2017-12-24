02e10f5d239a941acc9a62a20d06459a96f1e70f
Jinho Hyeon <neverlish@gmail.com>
2017-12-25 01:48:28 +0900

    6-2 Promises

--- react/nomadcoders-react-webservice/src/App.js
+++ react/nomadcoders-react-webservice/src/App.js
@@ -6,17 +6,28 @@ class App extends Component {
   state = {}
 
   componentDidMount() {
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count')
-    .then(potato => potato.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))  
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
     })
     return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count')
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))  
   }
 
   render() {
