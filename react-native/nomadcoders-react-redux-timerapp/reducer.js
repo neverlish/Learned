490c2f9319fea6afc8e76e03d4ca9cfb8832737e
Jinho Hyeon <neverlish@gmail.com>
2017-12-24 10:33:17 +0900

    10 Creating the tomato reducer

---
+++ react-native/nomadcoders-react-redux-timerapp/reducer.js
@@ -0,0 +1,89 @@
+// Import
+
+// Actions
+
+const START_TIMER = 'START_TIMER';
+const RESTART_TIMER = 'RESTART_TIMER';
+const ADD_SECOND = 'ADD_SECOND';
+
+// Action Creators
+
+function startTimer() {
+  return {
+    type: START_TIMER
+  }
+}
+
+function restartTimer() {
+  return {
+    type: RESTART_TIMER
+  }
+}
+
+function addSecond() {
+  return {
+    type: ADD_SECOND
+  }
+}
+
+// Reducers
+
+const TIMER_DURATION = 1500;
+
+const initialState = {
+  isPlaying: true
+}
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case START_TIMER:
+      return applyStartTimer(state);
+    case RESTART_TIMER:
+      return applyRestartTimer(state);
+    case ADD_SECOND:
+      return applyAddSecond(state);
+  }
+}
+
+// Reducer Functions
+
+function applyStartTimer(state) {
+  return {
+    ...state,
+    isPlaying: true
+  };
+}
+
+function applyRestartTimer(state) {
+  return {
+    ...state,
+    isPlaying: false,
+    elapsedTime: 0
+  }
+}
+
+function applyAddSecond(state) {
+  if (state.elapsedTime < TIMER_DURATION) {
+    return {
+      ...state,
+      elapsedTime: state.elapsedTime + 1
+    }
+  } else {
+    return {
+      ...state,
+      isPlaying: false
+    }
+  }
+}
+
+// Export Action Creators
+
+const actionCreators = {
+  startTimer,
+  restartTimer,
+  addSecond
+}
+
+// Export Reducer
+
+export default reducer;
