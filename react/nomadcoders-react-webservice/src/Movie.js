8363aec64d2a3681057ba36e27004712d1bfdf4c
Jinho Hyeon <neverlish@gmail.com>
2017-12-24 16:06:48 +0900

    2-2 Dataflow with Props

--- react/nomadcoders-react-webservice/src/Movie.js
+++ react/nomadcoders-react-webservice/src/Movie.js
@@ -4,8 +4,8 @@ class Movie extends Component {
   render() {
     return (
       <div>
-        <MoviePoster />
-        <h1>hello this is a movie</h1>
+        <MoviePoster poster={this.props.poster}/>
+        <h1>{this.props.title}</h1>
       </div>
     );
   }
@@ -14,7 +14,7 @@ class Movie extends Component {
 class MoviePoster extends Component {
   render() {
     return (
-      <img src='https://is2-ssl.mzstatic.com/image/thumb/Music1/v4/ac/08/3d/ac083d70-0149-0a0a-4b90-7647203edbe0/UMG_cvrart_00602547178466_01_RGB72_1500x1500_14UMGIM51157.jpg/1200x630bb.jpg' />
+      <img src={this.props.poster} />
     )
   }
 }
