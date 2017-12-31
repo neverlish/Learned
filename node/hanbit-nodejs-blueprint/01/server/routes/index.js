26fdb6eecaef256bdf400075517571f3cdb6fe63
Jinho Hyeon <neverlish@gmail.com>
2017-12-31 21:28:27 +0900

    01 - 06 로그인, 가입, 프로필 템플릿 추가하기

--- node/hanbit-nodejs-blueprint/01/server/routes/index.js
+++ node/hanbit-nodejs-blueprint/01/server/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express from server folder' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login Page', message: req.flash('loginMessage') });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Signup Page', message: req.flash('signupMessage') });
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { title: 'Profile Page', user: req.user, avatar: gravatar.url(req.user.email, {s: '100', r: 'x', d: 'retro' }, true) });
+});
+
 module.exports = router;
