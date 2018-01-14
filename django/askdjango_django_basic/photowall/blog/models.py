447844738809c3e776991d31c1ef6c3f2927557b
Jinho Hyeon <neverlish@gmail.com>
2018-01-14 20:44:29 +0900

    39 - PhotoWall 프로젝트 #02 - 구글맵

--- django/askdjango_django_basic/photowall/blog/models.py
+++ django/askdjango_django_basic/photowall/blog/models.py
@@ -1,12 +1,31 @@
+import re
 from django.db import models
+from django import forms
+
+def lnglat_validator(value):
+  if not re.match(r'^[+-]?\d+\.?\d*,[+-]?\d+\.?\d*$', value):
+    raise forms.ValidationError('경도/위도를 입력해주세요.')
 
 class Post(models.Model):
   title = models.CharField(max_length = 100)
   content = models.TextField()
   photo = models.ImageField()
+  lnglat = models.CharField(max_length = 40, blank = True, validators = [lnglat_validator])
   is_public = models.BooleanField(default = False)
   created_at = models.DateTimeField(auto_now_add = True)
   updated_at = models.DateTimeField(auto_now = True)
 
   def __str__(self):
     return self.title
+
+  @property
+  def lng(self):
+    if self.lnglat:
+      return self.lnglat.split(',')[0]
+    return None
+
+  @property
+  def lat(self):
+    if self.lnglat:
+      return self.lnglat.split(',')[1]
+    return None
