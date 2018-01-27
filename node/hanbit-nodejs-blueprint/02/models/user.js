5e4dbb406a8a2d65a580d45368d362f9f0124c34
Jinho Hyeon <neverlish@gmail.com>
2018-01-27 19:17:38 +0900

    02 - 11 Band 모델과 User 모델의 관계 생성하기

--- node/hanbit-nodejs-blueprint/02/models/user.js
+++ node/hanbit-nodejs-blueprint/02/models/user.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        User.hasMany(models.Band);
       }
     }
   });
