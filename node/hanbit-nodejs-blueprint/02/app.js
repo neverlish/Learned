45aedf0897f0cb9eed1ef73d9ca1470d258a23a9
Jinho Hyeon <neverlish@gmail.com>
2018-01-27 18:28:41 +0900

    02 - 03 스위그 템플릿 엔진 설치하기

--- node/hanbit-nodejs-blueprint/02/app.js
+++ node/hanbit-nodejs-blueprint/02/app.js
@@ -4,15 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var swig = require('swig');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var swig = new swig.Swig();
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
