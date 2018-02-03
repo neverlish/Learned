a8244d9a5d8a348ef7dd72a7d87fe492dd8fefc1
Jinho Hyeon <neverlish@gmail.com>
2018-02-03 18:58:06 +0900

    03 Coding the UI part One

--- react-native/nomadcoders-todoapp/App.js
+++ react-native/nomadcoders-todoapp/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform
+} from 'react-native';
+
+const { height, width } = Dimensions.get('window');
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle='light-content' />
+        <Text style={styles.title}>Kawai Todo</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={'New To Do'} />
+        </View>
       </View>
     );
   }
@@ -14,8 +28,38 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    color: 'white',
+    fontSize: 30,
+    marginTop: 40,
+    fontWeight: "200",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: 'white',
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgb(50, 50, 50)',
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 1
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  },
+  input: {
+
+  }
 });
