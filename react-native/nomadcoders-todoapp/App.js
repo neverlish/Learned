ff6f99fb8950b7e9165af63aa5eb94d4a2093e83
Jinho Hyeon <neverlish@gmail.com>
2018-02-04 10:44:44 +0900

    10 Saving To Dos part Two

--- react-native/nomadcoders-todoapp/App.js
+++ react-native/nomadcoders-todoapp/App.js
@@ -17,13 +17,14 @@ const { height, width } = Dimensions.get('window');
 export default class App extends React.Component {
   state = {
     newToDo: '',
-    loadedToDos: false
+    loadedToDos: false,
+    toDos: {}
   }
   componentDidMount = () => {
     this._loadToDos();
   }
   render() {
-    const { newToDo, loadedToDos } = this.state;
+    const { newToDo, loadedToDos, toDos } = this.state;
     if (!loadedToDos) {
       return <AppLoading />;
     }
@@ -43,7 +44,7 @@ export default class App extends React.Component {
             onSubmitEditing={this._addToDo}
           />
           <ScrollView contentContainerStyle={styles.toDos}>
-            <ToDo text={"Hello I'm a To Do"}/>
+            {Object.values(toDos).map(toDo => <ToDo key={toDo.id} {...toDo} deleteToDo={this._deleteTodo} />)}
           </ScrollView>
         </View>
       </View>
@@ -83,6 +84,17 @@ export default class App extends React.Component {
         return { ... newState };
       });
     }
+  }
+  _deleteTodo = (id) => {
+    this.setState(prevState => {
+      const toDos = prevState.toDos;
+      delete toDos[id];
+      const newState = {
+        ...prevState,
+        ...toDos,
+      };
+      return { ...newState };
+    })
   }
 }
 
--- react-native/nomadcoders-todoapp/ToDo.js
+++ react-native/nomadcoders-todoapp/ToDo.js
@@ -7,18 +7,28 @@ import {
   Dimensions,
   TextInput
 } from 'react-native';
+import PropTypes from 'prop-types';
 
 const { width, height } = Dimensions.get('window');
 
 export default class ToDo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isEditing: false, toDoValue: props.text };
+  }
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    isCompleted: PropTypes.bool.isRequired,
+    deleteToDo: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired
+  }
   state = {
     isEditing: false,
-    isCompleted: false,
     toDoValue: ''
   }
   render() {
     const { isCompleted, isEditing, toDoValue } = this.state;
-    const { text } = this.props;
+    const { text, id, deleteToDo } = this.props;
     return (
       <View style={styles.container}>
         <View style={styles.column}>
@@ -69,7 +79,7 @@ export default class ToDo extends Component {
                 <Text style={styles.actionText}>✏️</Text>
               </View>
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity onPressOut={() => deleteToDo(id) }>
               <View style={styles.actionContainer}>
                 <Text style={styles.actionText}>❌</Text>
               </View>
@@ -87,8 +97,7 @@ export default class ToDo extends Component {
     })
   }
   _startEditing = () => {
-    const { text } = this.props;
-    this.setState({ isEditing: true, toDoValue: text })
+    this.setState({ isEditing: true })
   }
   _finishEditing = () => {
     this.setState({
@@ -137,8 +146,7 @@ const styles = StyleSheet.create({
   column: {
     flexDirection: 'row',
     alignItems: 'center',
-    width: width / 2,
-    justifyContent: 'space-between'
+    width: width / 2
   },
   actions: {
     flexDirection: 'row'
