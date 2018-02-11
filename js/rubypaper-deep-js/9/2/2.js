98415f69a22844408a23342e9cfc2da6ae60b288
Jinho Hyeon <neverlish@gmail.com>
2018-02-11 10:58:54 +0900

    9 - 2 ECMAScript 2016

---
+++ js/rubypaper-deep-js/9/2/2.js
@@ -0,0 +1,7 @@
+// 9 자바스크립트 표준 - 2 ECMAScript 2016 - 2 지수 연산자
+
+var squareRootOfTwo = 2 ** 0.5; // 1.4142135623730951
+var cubeTwo = 2 ** 3; // 8
+
+var squareRootOfTwoES6 = Math.pow(2, 0.5);
+var cubeTwoES6 = Math.pow(2, 3);
