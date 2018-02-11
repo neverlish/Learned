cf292c4df8f8133b4de10e3c0ebbbd261722ff64
Jinho Hyeon <neverlish@gmail.com>
2018-02-11 23:46:48 +0900

    05 - 10 지오로케이션 애플리케이션 확인하기
    db.locations.ensureIndex({'coordinates': '2dsphere'})

--- node/hanbit-nodejs-blueprint/05/app/controllers/locations.js
+++ node/hanbit-nodejs-blueprint/05/app/controllers/locations.js
@@ -46,3 +46,42 @@ router.post('/locations', function(req, res, next) {
     });
   });
 });
+
+router.post('/nearme', function(req, res, next) {
+  // 최대 표시할 레코드의 수 설정
+  var limit = req.body.limit || 10;
+  // 기본 최장 거리는 10km
+  var maxDistance = req.body.distance || 10;
+  // coords 객체 설정
+  // coords Object = [ <longitude>, <latitude> ]
+  var coords = [];
+  // 배열 생성
+  coords[0] = req.body.longitude;
+  coords[1] = req.body.latitude;
+
+  // 위치 찾기
+  Location.find({
+    'coordinates': {
+      $near: {
+        $geometry: {
+          type: 'Point',
+          coordinates: coords
+        },
+        // 반지름
+        $maxDistance: maxDistance * 1609.34
+      }
+    }
+  }).limit(limit).exec(function(err, stores) {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    console.log(123123, stores);
+    // res.status(200).json(stores)
+    res.render('locations', {
+      title: 'Locations',
+      location: stores,
+      lat: -23.54312,
+      long: -46.642748
+    });
+  });
+});
