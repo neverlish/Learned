25d7158d0082eb61c3b557f9322c01cc48850c5c
Jinho Hyeon <neverlish@gmail.com>
2018-02-11 23:30:31 +0900

    05 - 06 모델 생성

---
+++ node/hanbit-nodejs-blueprint/05/app/models/location.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var LocationSchema = new Schema({
+  title: String,
+  coordinates: {
+    type: [Number],
+    index: '2dsphere'
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.model('Location', LocationSchema);
