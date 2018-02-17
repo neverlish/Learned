3da8453df51c91f5ce5d98c58ae4d4373935479b
Jinho Hyeon <neverlish@gmail.com>
2018-02-18 01:21:29 +0900

    3 - 북마크 앱 따라하기 (4)

--- electron/inflearn-electron-desktopapp/bookmark/index.js
+++ electron/inflearn-electron-desktopapp/bookmark/index.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, ipcMain, dialog} = require('electron');
+const {app, BrowserWindow, ipcMain, dialog, Tray, Menu, clipboard} = require('electron');
 const request = require('superagent');
 const getTitle = require('get-title');
 const fs = require('fs');
@@ -7,37 +7,100 @@ const path = require('path');
 const DATA_PATH = path.join(__dirname, './data.json');
 const data = JSON.parse(fs.readFileSync(DATA_PATH).toString());
 
+let win = null;
+let tray = null;
+
+const template = [
+  {
+    label: 'Open',
+    click: () => {
+      win.show();
+    }
+  },
+  {
+    label: 'Save',
+    submenu: [
+      {
+        label: 'Home',
+        click: () => {
+          const item = {
+            type: 'home',
+            url: clipboard.readText()
+          };
+          save(item);
+        }
+      },
+      {
+        label: 'Github',
+        click: () => {
+          const item = {
+            type: 'github',
+            url: clipboard.readText()
+          };
+          save(item);
+        }
+      }
+    ]
+  },
+  {
+    type: 'separator'
+  },
+  {
+    label: 'Quit',
+    click: () => {
+      app.quit();
+    }
+  },
+]
+
+const context = [
+  {
+    label: app.getName(),
+    submenu: [
+      {role: 'paste'},
+      {type: 'separator'},
+      {
+        label: 'Quit',
+        click: () => {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
 app.on('ready', () => {
-  const win = new BrowserWindow({
+  Menu.setApplicationMenu(Menu.buildFromTemplate(context));
+  const menu = Menu.buildFromTemplate(template);
+  tray = new Tray(path.join(__dirname, './icon.png'));
+  tray.setContextMenu(menu);
+  tray.on('right-click', () => {
+    toggle();
+  });
+  const bounds = tray.getBounds();
+  win = new BrowserWindow({
     width: 400,
     height: 400,
+    x: bounds.x + (bounds.width / 2) - 200,
+    y: bounds.y + (bounds.height / 2) + 10,
     acceptFirstMouse: true,
-    titleBarStyle: 'hidden',
-    show: false
+    show: false,
+    resizable: false,
+    movable: false,
+    frame: false
   });
 
   win.loadURL(`file://${__dirname}/index.html`);
   win.once('ready-to-show', () => {
-    win.show();
     win.webContents.send('update', data);
   });
 
+  win.on('blur', () => {
+    win.hide();
+  });
+
   ipcMain.on('paste', (event, item) => {
-    if (item.url.indexOf('http://') > -1 || item.url.indexOf('https://') > -1) {
-      const type = item.type;
-      const url = item.url;
-      request.get(url)
-        .end((err, response) => {
-          const contents = response.res.text;
-          getTitle(contents).then(title => {
-            data.push({type, url, title});
-            fs.writeFileSync(DATA_PATH, JSON.stringify(data));
-            win.webContents.send('update', data);
-          });
-        })
-    } else {
-      dialog.showErrorBox('경고', '유효한 url이 아닙니다.');
-    }
+    save(item);
   })
 
   ipcMain.on('remove', (event, removeId) => {
@@ -47,3 +110,28 @@ app.on('ready', () => {
   });
 });
 
+function toggle() {
+  if (win.isVisible()) {
+    win.hide();
+  } else {
+    win.show();
+  }
+}
+
+function save(item) {
+  if (item.url.indexOf('http://') > -1 || item.url.indexOf('https://') > -1) {
+    const type = item.type;
+    const url = item.url;
+    request.get(url)
+      .end((err, response) => {
+        const contents = response.res.text;
+        getTitle(contents).then(title => {
+          data.push({type, url, title});
+          fs.writeFileSync(DATA_PATH, JSON.stringify(data));
+          win.webContents.send('update', data);
+        });
+      })
+  } else {
+    dialog.showErrorBox('경고', '유효한 url이 아닙니다.');
+  }
+}
