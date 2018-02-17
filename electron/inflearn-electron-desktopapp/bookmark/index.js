daa8b3ac9a8da770432a1a3f5e6938b7b841dc55
Jinho Hyeon <neverlish@gmail.com>
2018-02-18 00:37:25 +0900

    3 - 북마크 앱 따라하기 (2)

--- electron/inflearn-electron-desktopapp/bookmark/index.js
+++ electron/inflearn-electron-desktopapp/bookmark/index.js
@@ -1,13 +1,36 @@
 const {app, BrowserWindow} = require('electron');
 
+const data = [
+  {
+    type: 'home',
+    url: 'https://github.com',
+    title: '타이틀'
+  },
+  {
+    type: 'github',
+    url: 'https://github.com',
+    title: '타이틀'
+  },
+  {
+    type: 'home',
+    url: 'https://github.com',
+    title: '타이틀'
+  },
+];
+
 app.on('ready', () => {
   const win = new BrowserWindow({
     width: 400,
     height: 400,
     acceptFirstMouse: true,
-    titleBarStyle: 'hidden'
+    titleBarStyle: 'hidden',
+    show: false
   });
 
   win.loadURL(`file://${__dirname}/index.html`);
+  win.once('ready-to-show', () => {
+    win.show();
+    win.webContents.send('update', data);
+  });
 });
 
