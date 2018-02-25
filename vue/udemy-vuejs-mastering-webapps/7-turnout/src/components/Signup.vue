1d0b34eedc0f3ccbd8fdff19984035a9e1b952e4
Jinho Hyeon <neverlish@gmail.com>
2018-02-25 22:14:14 +0900

    7 - 61 Developing the Sign Up Component

--- vue/udemy-vuejs-mastering-webapps/7-turnout/src/components/Signup.vue
+++ vue/udemy-vuejs-mastering-webapps/7-turnout/src/components/Signup.vue
@@ -1,6 +1,6 @@
 <template>
   <div class='form-inline'>
-    <h3>Sign in</h3>
+    <h3>Sign Up</h3>
     <div class='form-group'>
       <input
         type='text'
@@ -14,10 +14,10 @@
         class='form-control'
         v-model='password'
       />
-      <button class='btn btn-primary' @click='signIn'>Sign In</button>
+      <button class='btn btn-primary' @click='signUp'>Sign Up</button>
     </div>
     <br />
-    <router-link to='/signup'>Not a user? Sign up</router-link>
+    <router-link to='/signin'>Already a user? Sign in</router-link>
     <br />
     <p>{{ error.message }}</p>
   </div>
@@ -37,8 +37,8 @@ export default {
     }
   },
   methods: {
-    signIn() {
-      firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+    signUp() {
+      firebaseApp.auth().createUserWithEmailAndPassword(this.email, this.password)
         .catch(error => {
           this.error = error
         })
--- vue/udemy-vuejs-mastering-webapps/7-turnout/src/main.js
+++ vue/udemy-vuejs-mastering-webapps/7-turnout/src/main.js
@@ -9,12 +9,14 @@ Vue.use(VueRouter)
 
 import Dashboard from './components/Dashboard.vue'
 import Signin from './components/Signin.vue'
+import Signup from './components/Signup.vue'
 
 const router = new VueRouter({
   mode: 'history',
   routes: [
     { path: '/dashboard', component: Dashboard },
-    { path: '/signin', component: Signin }
+    { path: '/signin', component: Signin },
+    { path: '/signup', component: Signup }
   ]
 })
 
