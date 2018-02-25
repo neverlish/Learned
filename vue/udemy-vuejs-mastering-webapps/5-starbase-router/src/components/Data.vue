51f3c8d8aa6c0fe1a8d43265fbc24ef81f8f550e
Jinho Hyeon <neverlish@gmail.com>
2018-02-25 19:04:32 +0900

    5 - 38 Developing the Data Component

--- vue/udemy-vuejs-mastering-webapps/5-starbase-router/src/components/Data.vue
+++ vue/udemy-vuejs-mastering-webapps/5-starbase-router/src/components/Data.vue
@@ -1,6 +1,6 @@
 <template>
   <div class=''>
-    Type: {{ type }}
+    {{ items }}
   </div>
 </template>
 
@@ -8,16 +8,31 @@
 export default {
   data() {
     return {
-      type: this.$route.params.type
+      type: this.$route.params.type,
+      items: []
     }
   },
   watch: {
-    '$route': 'change'
+    '$route': 'fetchItems'
   },
   methods: {
-    change() {
+    fetchItems() {
+      this.items = []
       this.type = this.$route.params.type
+      let initial_ids = [1, 13, 14]
+
+      for (let i in initial_ids) {
+        let id = initial_ids[i]
+        fetch(`https://swapi.co/api/${this.type}/${id}`, {
+          method: 'GET'
+        })
+          .then(response => response.json())
+          .then(json => this.items.push(json))
+      }
     }
+  },
+  created() {
+    this.fetchItems()
   }
 }
 </script>
