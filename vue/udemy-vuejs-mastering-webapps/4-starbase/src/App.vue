c8924ec36d3214efa8f56f67179c94ce5a2a047a
Jinho Hyeon <neverlish@gmail.com>
2018-02-25 17:50:12 +0900

    4 - 27 Introducing the API

--- vue/udemy-vuejs-mastering-webapps/4-starbase/src/App.vue
+++ vue/udemy-vuejs-mastering-webapps/4-starbase/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id='app'>
     <h3>{{ title }}</h3>
+    <button class='btn btn-primary' @click='fetchCharacter'>Test Fetch Character</button>
   </div>
 </template>
 
@@ -10,6 +11,15 @@ export default {
   data() {
     return {
       title: 'Generate Your Team'
+    }
+  },
+  methods: {
+    fetchCharacter() {
+      fetch('https://swapi.co/api/people', {
+        method: 'GET'
+      })
+        .then(response => response.json())
+        .then(json => console.log(json))
     }
   }
 }
