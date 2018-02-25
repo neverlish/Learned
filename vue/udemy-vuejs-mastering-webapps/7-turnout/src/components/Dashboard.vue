18e9f1af9bf8230c46e2eeea7745c3afe1385981
Jinho Hyeon <neverlish@gmail.com>
2018-02-25 22:42:32 +0900

    7 - 66 Creating the EventItem Component

--- vue/udemy-vuejs-mastering-webapps/7-turnout/src/components/Dashboard.vue
+++ vue/udemy-vuejs-mastering-webapps/7-turnout/src/components/Dashboard.vue
@@ -5,13 +5,20 @@
     <hr>
     <AddEvent />
     <hr>
-    {{ $store.state.events }}
+    <div class='col-md-12'>
+      <EventItem
+        v-for='(event_item, index) in this.$store.state.events'
+        :event='event_item'
+        :key='index'
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { firebaseApp, eventsRef } from '../firebaseApp'
 import AddEvent from './AddEvent.vue'
+import EventItem from './EventItem.vue'
 
 export default {
   methods: {
@@ -21,7 +28,8 @@ export default {
     }
   },
   components: {
-    AddEvent
+    AddEvent,
+    EventItem
   },
   mounted() {
     eventsRef.on('value', snap => {
