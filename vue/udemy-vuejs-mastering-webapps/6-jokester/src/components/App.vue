40024bccaa3a62974213fb587247935409d8adb6
Jinho Hyeon <neverlish@gmail.com>
2018-02-25 19:33:32 +0900

    6 - 45 Creating the Store

--- vue/udemy-vuejs-mastering-webapps/6-jokester/src/components/App.vue
+++ vue/udemy-vuejs-mastering-webapps/6-jokester/src/components/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id='app'>
     <h3>Got jokes?</h3>
+    {{ $store.state }}
   </div>
 </template>
---
+++ vue/udemy-vuejs-mastering-webapps/6-jokester/src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+  jokes: []
+}
+
+export default new Vuex.Store({
+  state
+})
