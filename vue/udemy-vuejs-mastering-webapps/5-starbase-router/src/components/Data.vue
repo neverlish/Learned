08c66162e6c09a29e6e9622b97a86ec18c82c78e
Jinho Hyeon <neverlish@gmail.com>
2018-02-25 18:58:27 +0900

    5 - 37 Router-Linking Different Pages

--- vue/udemy-vuejs-mastering-webapps/5-starbase-router/src/components/Data.vue
+++ vue/udemy-vuejs-mastering-webapps/5-starbase-router/src/components/Data.vue
@@ -10,6 +10,14 @@ export default {
     return {
       type: this.$route.params.type
     }
+  },
+  watch: {
+    '$route': 'change'
+  },
+  methods: {
+    change() {
+      this.type = this.$route.params.type
+    }
   }
 }
 </script>
