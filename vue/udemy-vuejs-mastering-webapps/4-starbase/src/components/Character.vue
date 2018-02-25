28263f61b1f684a855831502416891642a42b25c
Jinho Hyeon <neverlish@gmail.com>
2018-02-25 18:24:26 +0900

    4 - 30 Structuring the Characters

--- vue/udemy-vuejs-mastering-webapps/4-starbase/src/components/Character.vue
+++ vue/udemy-vuejs-mastering-webapps/4-starbase/src/components/Character.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class='' @click='switchCharacter'>
-    {{ character }}
+  <div class='col-md-4' @click='switchCharacter'>
+    <div class='character-card'>
+      <div class='card-block'>
+        <h4 class='card-title'>{{ character.name }}</h4>
+        <p class='card-text'>Height: {{ character.height }}cm</p>
+        <p class='card-text'>Mass: {{ character.mass }}kg</p>
+        <p class='card-text'>Hair Color: {{ character.hair_color }}</p>
+        <p class='card-text'>Eye Color: {{ character.eye_color }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
