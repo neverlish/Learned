22ec173d13bc5f547dd18bb4cc8f148ab3ee4545
Jinho Hyeon <neverlish@gmail.com>
2018-02-25 21:36:54 +0900

    7 - 58 Signing In

--- vue/udemy-vuejs-mastering-webapps/7-turnout/src/components/Signin.vue
+++ vue/udemy-vuejs-mastering-webapps/7-turnout/src/components/Signin.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class=''>
-    Sign In Component
+  <div class='form-inline'>
+    <h3>Sign in</h3>
+    <div class='form-group'>
+      <input
+        type='text'
+        placeholder='email'
+        class='form-control'
+        v-model='email'
+      />
+      <input
+        type='password'
+        placeholder='password'
+        class='form-control'
+        v-model='password'
+      />
+      <button class='btn btn-primary' @click='signIn'>Sign In</button>
+    </div>
+    <br />
+    <p>{{ error.message }}</p>
+    <br />
   </div>
 </template>
+
+<script>
+import { firebaseApp } from '../firebaseApp'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: {
+        message: ''
+      }
+    }
+  },
+  methods: {
+    signIn() {
+      firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+        .catch(error => {
+          this.error = error
+        })
+    }
+  }
+}
+</script>
