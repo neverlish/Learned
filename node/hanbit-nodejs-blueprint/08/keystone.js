9006658bbc3a2dc33e1da962b351f06eb425cc1a
Jinho Hyeon <neverlish@gmail.com>
2018-03-07 22:07:28 +0900

    08 키스톤 CMS로 블로그 만들기 - 03 기초 애플리케이션 생성하기
    npm i -g generator-keystone@0.3.16
    
    yo keystone
    swig / sass / Blog:Yes / ImageGallery:Yes / ContactForm:Yes / UserAdmin / neverlish@gmail.com / 121212 / gulp / create new dir:No

---
+++ node/hanbit-nodejs-blueprint/08/keystone.js
@@ -0,0 +1,52 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').load();
+
+// Require keystone
+var keystone = require('keystone');
+var swig = require('swig');
+
+// Disable swig's bulit-in template caching, express handles it
+swig.setDefaults({ cache: process.env.NODE_ENV === 'development' ? false : 'memory' });
+
+keystone.init({
+
+	'name': '08',
+	'brand': '08',
+
+	'sass': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'swig',
+
+	'custom engine': swig.renderFile,
+
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'UserAdmin',
+
+});
+
+keystone.import('models');
+
+keystone.set('locals', {
+	_: require('underscore'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
+});
+
+keystone.set('routes', require('./routes'));
+
+
+
+keystone.set('nav', {
+	posts: ['posts', 'post-categories'],
+	galleries: 'galleries',
+	enquiries: 'enquiries',
+	userAdmins: 'user-admins',
+});
+
+keystone.start();
