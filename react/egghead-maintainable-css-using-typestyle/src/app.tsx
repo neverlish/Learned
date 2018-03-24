f44eaae9c820714d5a756dd4db4813b73348044d
Jinho Hyeon <neverlish@gmail.com>
2018-03-25 01:05:07 +0900

    11 Render HTML/CSS server-side using TypeStyle

--- react/egghead-maintainable-css-using-typestyle/src/app.tsx
+++ react/egghead-maintainable-css-using-typestyle/src/app.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as tylestyle from 'typestyle';
 
-import App from './09';
+import App from './11';
 
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
 )
+
+tylestyle.setStylesTarget(document.getElementById('styles-target'))
