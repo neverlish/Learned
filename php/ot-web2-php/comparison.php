4b04dda44b896835c35a658c0aea9e39481bf9a2
Jinho Hyeon <neverlish@gmail.com>
2018-04-07 14:34:27 +0900

    PHP의 Boolean과 비교연산자

---
+++ php/ot-web2-php/comparison.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Comparison Operators &amp; Boolean data type </h1>
+    <h2>1==1</h2>
+    <?php
+      var_dump(1==1); // bool(true)
+    ?>
+    <h2>1>1</h2>
+    <?php
+      var_dump(1>1); // bool(false)
+    ?>
+    <h2>1>=1</h2>
+    <?php
+      var_dump(1>=1); // bool(true)
+    ?>
+  </body>
+</html>
