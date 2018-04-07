1d853ffd231a36efcc97fddd2954b8471cbebb9e
Jinho Hyeon <neverlish@gmail.com>
2018-04-07 21:02:46 +0900

    PHP의 반복문과 배열의 활용

--- php/ot-web2-php/index.php
+++ php/ot-web2-php/index.php
@@ -5,11 +5,20 @@
     <title></title>
   </head>
   <body>
-    <h1>WEB</h1>
+    <h1><a href='index.php'>WEB</a></h1>
     <ol>
-      <li><a href="index.php?id=HTML">HTML</a></li>
-      <li><a href="index.php?id=CSS">CSS</a></li>
-      <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+      <?php
+        $list = scandir('./data');
+        $i = 0;
+        while ($i < count($list)) {
+          if ($list[$i] != '.') {
+            if ($list[$i] != '..') {
+              echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+            }
+          }
+          $i = $i + 1;
+        }
+      ?>
     </ol>
     <h2>
       <?php
