3d5eb0eb270cdb0c279a2e3880eb3a12aef52846
Jinho Hyeon <neverlish@gmail.com>
2018-04-08 00:10:55 +0900

    PHP에서 글생성 기능 구현하기

--- php/ot-web2-php/create.php
+++ php/ot-web2-php/create.php
@@ -7,14 +7,6 @@
     }
   }
 
-  function print_description() {
-    if (isset($_GET['id'])) {
-      echo file_get_contents('data/'.$_GET['id']);
-    } else {
-      echo 'Hello, PHP';
-    }
-  }
-
   function print_list() {
     $list = scandir('./data');
     $i = 0;
@@ -46,13 +38,16 @@
       ?>
     </ol>
     <a href='create.php'>create</a>
-    <h2>
-      <?php
-        print_title();
-      ?>
-    </h2>
-    <?php
-      print_description();
-    ?>
+    <form action='create_process.php' method='post'>
+      <p>
+        <input type='text' name='title' placeholder='Title'>
+      </p>
+      <p>
+        <textarea name='description' placeholder='Description'></textarea>
+      </p>
+      <p>
+        <input type='submit'>
+      </p>
+    </form>
   </body>
 </html>
