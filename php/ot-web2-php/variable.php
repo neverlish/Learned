0ff90eab4680df8c10d613a35838d3ce8051a9f1
Jinho Hyeon <neverlish@gmail.com>
2018-04-07 13:43:26 +0900

    PHP의 변수

---
+++ php/ot-web2-php/variable.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<body>
+  <h1>Variable</h1>
+  <?php
+    $name = "leezche";
+    echo "Lorem ipsum dolor sit amet, consectetur ".$name." adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ".$name." laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore egoing eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. by ".$name;
+  ?>
+</body>
+</html>
