916e778528e6b3feb9b9a64414b2be605efb28c7
Jinho Hyeon <neverlish@gmail.com>
2018-04-07 14:40:54 +0900

    PHP의 조건문의 형식

---
+++ php/ot-web2-php/conditional.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Conditional</h1>
+    <h2>if</h2>
+    <?php
+      echo '1<br>';
+      if (false) {
+        echo '2-1<br>';
+      } else {
+        echo '2-2<br>';
+      }
+      echo '3<br>';
+    ?>
+  </body>
+</html>
