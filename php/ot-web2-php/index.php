45030ac33a222be6f7f57cb4e68df5550823bfee
Jinho Hyeon <neverlish@gmail.com>
2018-04-07 23:26:59 +0900

    PHP 함수의 활용

--- php/ot-web2-php/index.php
+++ php/ot-web2-php/index.php
@@ -1,40 +1,57 @@
+<?php
+  function print_title() {
+    if (isset($_GET['id'])) {
+      echo $_GET['id'];  
+    } else {
+      echo 'Welcome';
+    }
+  }
+
+  function print_description() {
+    if (isset($_GET['id'])) {
+      echo file_get_contents('data/'.$_GET['id']);
+    } else {
+      echo 'Hello, PHP';
+    }
+  }
+
+  function print_list() {
+    $list = scandir('./data');
+    $i = 0;
+    while ($i < count($list)) {
+      if ($list[$i] != '.') {
+        if ($list[$i] != '..') {
+          echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+        }
+      }
+      $i = $i + 1;
+    }
+  }
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>
+      <?php
+        print_title();
+      ?>
+    </title>
   </head>
   <body>
     <h1><a href='index.php'>WEB</a></h1>
     <ol>
       <?php
-        $list = scandir('./data');
-        $i = 0;
-        while ($i < count($list)) {
-          if ($list[$i] != '.') {
-            if ($list[$i] != '..') {
-              echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
-            }
-          }
-          $i = $i + 1;
-        }
+        print_list();
       ?>
     </ol>
     <h2>
       <?php
-        if (isset($_GET['id'])) {
-          echo $_GET['id'];  
-        } else {
-          echo 'Welcome';
-        }
+        print_title();
       ?>
     </h2>
     <?php
-      if (isset($_GET['id'])) {
-        echo file_get_contents('data/'.$_GET['id']);
-      } else {
-        echo 'Hello, PHP';
-      }
+      print_description();
     ?>
   </body>
 </html>
