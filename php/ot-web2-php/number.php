53c21b39b79530da732c4c07bed1294ed9b84ff7
Jinho Hyeon <neverlish@gmail.com>
2018-04-07 13:30:21 +0900

    PHP의 데이터 타입 (숫자와 문자)

---
+++ php/ot-web2-php/number.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<body>
+  <h1>Number & Arithmetic Operator</h1>
+  <h2>1+1</h2>
+  <?php
+    echo 1+1;
+  ?>
+  <h2>2-1</h2>
+  <?php
+    echo 2-1;
+  ?>
+  <h2>2*2</h2>
+  <?php
+    echo 2*2;
+  ?>
+  <h2>4/2</h2>
+  <?php
+    echo 4/2;
+  ?>
+</body>
+</html>
---
+++ php/ot-web2-php/string.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<body>
+  <h1>String & String Operator</h1>
+  <?php
+    echo "Hello \"w\"orld";
+  ?>
+  <h2>concetenation operator</h2>
+  <?php
+    echo "Hello "."world";
+  ?>
+  <h2>String length function</h2>
+  <?php
+    echo strlen("Hello world");
+  ?>
+</body>
+</html>
