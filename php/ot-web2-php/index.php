180a0687d76f141603f55ed6e26cc60c90fd6a08
Jinho Hyeon <neverlish@gmail.com>
2018-04-07 14:24:07 +0900

    PHP의 URL 파라미터

--- php/ot-web2-php/index.php
+++ php/ot-web2-php/index.php
@@ -1,8 +1,21 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
   <body>
-    <?php
-      echo date('Y-m-d H:i:s');
-    ?>
+    <h1>WEB</h1>
+    <ol>
+      <li><a href="index.php?id=HTML">HTML</a></li>
+      <li><a href="index.php?id=CSS">CSS</a></li>
+      <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>
+      <?php
+        echo $_GET['id'];
+      ?>
+    </h2>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   </body>
 </html>
