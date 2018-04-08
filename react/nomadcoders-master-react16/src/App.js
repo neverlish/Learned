d4dc416a09276deffe52ab68ac263fbf22fa8c06
Jinho Hyeon <neverlish@gmail.com>
2018-04-08 17:45:38 +0900

    4 Error Boundaries

--- react/nomadcoders-master-react16/src/App.js
+++ react/nomadcoders-master-react16/src/App.js
@@ -1,6 +1,23 @@
 import React, { Component, Fragment } from 'react';
 import { createPortal } from 'react-dom';
 
+class ErrorMaker extends Component {
+  state = {
+    friends: ['jisu', 'flynn', 'daal', 'kneeplayer']
+  }
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      })
+    }, 2000);
+  }
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
+
 class Portals extends Component {
   render() {
     return createPortal(<Message />, document.getElementById('touchme'));
@@ -15,12 +32,26 @@ class ReturnTypes extends Component {
   }
 }
 
+const ErrorFallback = () => ' Sorry something went wrong'
+
 class App extends Component {
+  state = {
+    hasError: false
+  }
+  componentDidCatch = (error, info) => {
+    this.setState({
+      hasError: true
+    })
+  }
   render() {
-    return (<Fragment>
-      <ReturnTypes />
-      <Portals />
-    </Fragment>);
+    const { hasError } = this.state;
+    return (
+      <Fragment>
+        <ReturnTypes />
+        <Portals />
+        {hasError ? <ErrorFallback /> : <ErrorMaker />}
+      </Fragment>
+    );
   }
 }
 
