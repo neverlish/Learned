f8274d8ddfa13a962839152d76dbf27a1ce95e98
Jinho Hyeon <neverlish@gmail.com>
2018-04-08 16:54:55 +0900

    2 Return Types Strings and Fragments

--- react/nomadcoders-master-react16/src/App.js
+++ react/nomadcoders-master-react16/src/App.js
@@ -1,8 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+class ReturnTypes extends Component {
+  render() {
+    // return (
+    //   <Fragment>
+    //     <header />
+    //     <div />
+    //     <footer />
+    //   </Fragment>
+    // )
+    return 'hello';
+  }
+}
 
 class App extends Component {
   render() {
-    return <div className="App"/>;
+    return (<Fragment>
+      <ReturnTypes />
+    </Fragment>);
   }
 }
 
