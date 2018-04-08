937d751ceed5b8ab368bea535f657ef14488e57d
Jinho Hyeon <neverlish@gmail.com>
2018-04-08 13:19:51 +0900

    활용 - 글읽기

--- php/ot-web3-php-mysql/create.php
+++ php/ot-web3-php-mysql/create.php
@@ -1,12 +1,40 @@
+<?php
+  $conn = mysqli_connect(
+    'localhost',
+    'root',
+    'asasas',
+    'opentutorials');
+
+  $sql = "SELECT * FROM topic";
+  $result = mysqli_query($conn, $sql);
+  $list = '';
+  while ($row = mysqli_fetch_array($result)) {
+    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+
+  $article = array(
+    'title' => 'Welcome',
+    'description' => 'Hello, Web'
+  );
+
+  if (isset($_GET['id'])) {
+    $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+    $article['title'] = $row['title'];
+    $article['description'] = $row['description'];
+  }
+?>
 <!doctype html>
 <html>
 <head>
   <meta charset='utf-8'>
+  <title>WEB</title>
 </head>
 <body>
-  <h1>Web</h1>
+  <h1><a href='index.php'>WEB</a></h1>
   <ol>
-    <li>HTML</li>
+    <?= $list ?>
   </ol>
   <a href='create.php'>create</a>
   <form action='process_create.php' method='POST'>
