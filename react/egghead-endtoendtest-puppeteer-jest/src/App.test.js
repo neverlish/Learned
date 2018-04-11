039c9c3cfc67015ba6f267a447eb8b548a333dda
Jinho Hyeon <neverlish@gmail.com>
2018-04-11 20:54:21 +0900

    2 Setup and Install Dependencies for Testing with Google's Puppeteer

--- react/egghead-endtoendtest-puppeteer-jest/src/App.test.js
+++ react/egghead-endtoendtest-puppeteer-jest/src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+const puppeteer = require('puppeteer')
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const isDebugging = () => {
+  const debugging_mode = {
+    headless: false,
+    slowMo: 250,
+    devtools: true,
+    // executablePath: string,
+    // timeout: number,
+    // ignoreHTTPSErrors: bool
+  }
+  return process.env.NODE_ENV === 'debug' ? debugging_mode : {}
+}
+
+describe('on page load', () => {
+  test('h1 does correctly', async() => {
+    let browser = await puppeteer.launch(isDebugging())
+    let page = await browser.newPage()
+
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400
+      },
+      userAgent: ''
+    })
+  })
+})
