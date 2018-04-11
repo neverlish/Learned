ae27d94deca2b528e8de7cc940ea47f9c786fb54
Jinho Hyeon <neverlish@gmail.com>
2018-04-11 21:05:16 +0900

    3 Setup and Install Dependencies for Testing with Google's Puppeteer

--- react/egghead-endtoendtest-puppeteer-jest/src/App.js
+++ react/egghead-endtoendtest-puppeteer-jest/src/App.js
@@ -9,6 +9,14 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <nav data-testid='navbar' className='navbar' role='navigation'>
+            <ul>
+              <li data-testid="navBarLi" className='nav-li'><a href="#">Home</a></li>
+              <li data-testid="navBarLi" className='nav-li'><a href="#">About</a></li>
+              <li data-testid="navBarLi" className='nav-li'><a href="#">Skills</a></li>
+              <li data-testid="navBarLi" className='nav-li'><a href="#">Works</a></li>
+            </ul>
+          </nav>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- react/egghead-endtoendtest-puppeteer-jest/src/App.test.js
+++ react/egghead-endtoendtest-puppeteer-jest/src/App.test.js
@@ -4,25 +4,39 @@ const isDebugging = () => {
   const debugging_mode = {
     headless: false,
     slowMo: 250,
-    devtools: true,
-    // executablePath: string,
-    // timeout: number,
-    // ignoreHTTPSErrors: bool
+    devtools: true
   }
   return process.env.NODE_ENV === 'debug' ? debugging_mode : {}
 }
 
+let browser
+let page
+
+beforeAll(async() => {
+  browser = await puppeteer.launch(isDebugging())
+  page = await browser.newPage()
+  await page.goto('http://localhost:3000')
+  page.setViewport({ width: 500, height: 2400 })
+})
+
 describe('on page load', () => {
   test('h1 does correctly', async() => {
-    let browser = await puppeteer.launch(isDebugging())
-    let page = await browser.newPage()
+    const html = await page.$eval('.App-title', e => e.innerHTML)
+    
+    expect(html).toBe('Welcome to React')
+  }, 16000)
 
-    page.emulate({
-      viewport: {
-        width: 500,
-        height: 2400
-      },
-      userAgent: ''
-    })
+  test('nav loads correctly', async() => {
+    const navbar = await page.$eval('[data-testid="navbar"]', el => el ? true : false)
+    const listItems = await page.$$('[data-testid="navBarLi"]')
+
+    expect(navbar).toBe(true)
+    expect(listItems.length).toBe(4)
   })
+})
+
+afterAll(() => {
+  if (isDebugging) {
+    browser.close()
+  }
 })
