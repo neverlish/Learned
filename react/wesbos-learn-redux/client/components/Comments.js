93407dbec333633bfa12f8c87390ab044d53e514
Jinho Hyeon <neverlish@gmail.com>
2018-04-15 00:23:19 +0900

    16 Redux Reducer Composition

--- react/wesbos-learn-redux/client/components/Comments.js
+++ react/wesbos-learn-redux/client/components/Comments.js
@@ -7,7 +7,7 @@ const Comments = React.createClass({
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className='remove-comment'>&times;</button>
+          <button className='remove-comment' onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
         </p>
       </div>
     )
@@ -18,6 +18,7 @@ const Comments = React.createClass({
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   },
   render() {
     return (
