9aaa8b97c6da72e26f30ab2e27f247f4ee9e64d7
Jinho Hyeon <neverlish@gmail.com>
2018-04-15 00:54:54 +0900

    18 Hot Reloading Redux Reducers with webpack

--- react/wesbos-learn-redux/client/store.js
+++ react/wesbos-learn-redux/client/store.js
@@ -18,4 +18,11 @@ const store = createStore(rootReducer, defaultState);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
+if (module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
 export default store;
