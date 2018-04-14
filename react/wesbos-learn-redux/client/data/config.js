7f23294577d3d05e2ab68a35062396c51c3ed1dd
Jinho Hyeon <neverlish@gmail.com>
2018-04-15 00:49:01 +0900

    17 Error Tracking and Insights with Sentry

--- react/wesbos-learn-redux/client/data/config.js
+++ react/wesbos-learn-redux/client/data/config.js
@@ -1,7 +1,7 @@
 import Raven from 'raven-js';
 
-const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
-const sentry_app = '61499';
+const sentry_key = '621df2ccd6154fe7817901257a648306';
+const sentry_app = '1189316';
 export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
 
 export function logException(ex, context) {
--- react/wesbos-learn-redux/client/reduxstagram.js
+++ react/wesbos-learn-redux/client/reduxstagram.js
@@ -14,6 +14,23 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
+import Raven from 'raven-js';
+import { sentry_url, logException } from './data/config';
+
+Raven.config(sentry_url,{
+  tags: {
+    git_commit: 'asdasdasd',
+    userLevel: 'editor'
+  }
+}).install();
+
+// logException(new Error('download failed!'), {
+//   email: 'neverlish@gmail.com'
+// });
+
+// Raven.captureMessage('Something bad happened');
+// Raven.showReportDialog();
+
 const router = (
   <Provider store={store}>
     <Router history={history}>
