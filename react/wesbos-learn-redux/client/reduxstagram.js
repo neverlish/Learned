4495029739d37eb6ba0ad78c70f50706dc21f3a1
Jinho Hyeon <neverlish@gmail.com>
2018-04-14 22:16:30 +0900

    08 Integrating our Store with React Router

--- react/wesbos-learn-redux/client/reduxstagram.js
+++ react/wesbos-learn-redux/client/reduxstagram.js
@@ -11,14 +11,18 @@ import PhotoGrid from './components/PhotoGrid';
 
 // Import react router deps
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path='/' component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path='/view/:postId' component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path='/' component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path='/view/:postId' component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'));
