a28aeaeda4c5e27ed159c8db45773c3702cff030
Jinho Hyeon <neverlish@gmail.com>
2018-04-14 20:44:24 +0900

    04 Setting up React Router

--- react/wesbos-learn-redux/client/reduxstagram.js
+++ react/wesbos-learn-redux/client/reduxstagram.js
@@ -6,5 +6,19 @@ import css from './styles/style.styl';
 
 // Import Components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main/>, document.getElementById('root'));
+// Import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path='/view/:postId' component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
