0d563dd3ead09a153e801c626fb535e404cc7678
Jinho Hyeon <neverlish@gmail.com>
2018-04-14 23:05:35 +0900

    11 Displaying Redux State inside our Components

--- react/wesbos-learn-redux/client/components/PhotoGrid.js
+++ react/wesbos-learn-redux/client/components/PhotoGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className='photo-grid'>
-        I'm the photo grid
+        {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
       </div>
     )
   }
