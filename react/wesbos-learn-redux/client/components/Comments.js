a169df700d8458676aaa706c2f0b08c26896cce8
Jinho Hyeon <neverlish@gmail.com>
2018-04-14 23:56:23 +0900

    13 Displaying the Single Photo Component

---
+++ react/wesbos-learn-redux/client/components/Comments.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  render() {
+    return (
+      <div className='comment'>
+        I'm the comments!!
+      </div>
+    )
+  }
+});
+
+export default Comments;
--- react/wesbos-learn-redux/client/components/Single.js
+++ react/wesbos-learn-redux/client/components/Single.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+    
     return (
       <div className='single-photo'>
-        I'm the single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     )
   }
