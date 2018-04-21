3d6434c9d149b7c4afdd1efeecabe77b06f1c495
Jinho Hyeon <neverlish@gmail.com>
2018-04-21 13:17:17 +0900

    05 The Gulp Task

--- node/youtube-simonswiss-jade-pug-tips/gulpfile.js
+++ node/youtube-simonswiss-jade-pug-tips/gulpfile.js
@@ -1,2 +1,11 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+
+// command 'gulp jade'
+gulp.task('jade', function() {
+  gulp.src('./src/*.jade')
+  .pipe(jade({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./dist'))
+})
