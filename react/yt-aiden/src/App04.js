5c586adfb050cad6983af158acb8a8af18fa48ba
Jinho Hyeon <neverlish@gmail.com>
2018-04-21 11:08:34 +0900

    04 React component 만들기

---
+++ react/yt-aiden/src/App04.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <h1 className='title'>Hello World</h1>
+  }
+}
+
+// const App = () => <h1>hello stateless</h1>
+
+export default App;
--- react/yt-aiden/src/index.js
+++ react/yt-aiden/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App03';
+import App from './App04';
 
 ReactDOM.render(
-  <App baby={3} />, 
+  <App />, 
   document.getElementById('root')
 );
