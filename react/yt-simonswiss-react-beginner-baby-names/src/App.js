c97e7019aa5069f724abb6d0a6d4043b78010e96
Jinho Hyeon <neverlish@gmail.com>
2018-04-21 09:34:14 +0900

    03 Our First Component Output

--- react/yt-simonswiss-react-beginner-baby-names/src/App.js
+++ react/yt-simonswiss-react-beginner-baby-names/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ul>
+        <li className='boy'>Name 1</li>
+        <li className='girl'>Name 1</li>
+        <li className='boy'>Name 2</li>
+        <li className='girl'>Name 2</li>
+        <li className='boy'>Name 3</li>
+        <li className='girl'>Name 3</li>
+      </ul>
     );
   }
 }
