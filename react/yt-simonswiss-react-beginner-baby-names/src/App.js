ea9017fe94a2623ed065371f5b5c6741bdf0116f
Jinho Hyeon <neverlish@gmail.com>
2018-04-21 10:01:40 +0900

    07 Stateless Components

--- react/yt-simonswiss-react-beginner-baby-names/src/App.js
+++ react/yt-simonswiss-react-beginner-baby-names/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import NamesList from './components/NamesList';
+import Credit from './components/Credit';
 
 class App extends Component {
   render() {
-    
+
     return (
       <div>
         <NamesList data={this.props.data} />
+        <Credit />
       </div>
     )
   }
