269c0de95d9003ac61dd1ef7c86778f83b43e379
Jinho Hyeon <neverlish@gmail.com>
2018-04-21 12:54:39 +0900

    14 React에서 반복하기

---
+++ react/yt-aiden/src/App14.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {items: []}
+  }
+
+  componentWillMount() {
+    fetch('https://swapi.co/api/people/?format=json')
+      .then( response => response.json() )
+      .then( ({results: items}) => this.setState({items}))
+  }
+
+  filter(e) {
+    this.setState({filter: e.target.value})
+  }
+
+  render() {
+    let items = this.state.items
+    if (this.state.filter) {
+      items = items.filter( item => item.name.toLowerCase().includes(this.state.filter.toLowerCase()) )
+    }
+    return (
+      <div>
+        <input type='text' onChange={this.filter.bind(this)} />
+        {items.map(item => <Person key={item.name} person={item} />)}    
+      </div>
+    )
+  }
+}
+
+const Person = (props) => <h4>{props.person.name}</h4>
+
+export default App;
--- react/yt-aiden/src/index.js
+++ react/yt-aiden/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App13';
+import App from './App14';
 
 ReactDOM.render(
   <App />, 
