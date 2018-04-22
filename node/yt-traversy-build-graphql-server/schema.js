e175c8c4ad5ef6b987dcf81971977cc53c30d594
Jinho Hyeon <neverlish@gmail.com>
2018-04-22 16:16:49 +0900

    2 Server & Express GraphQL Setup

---
+++ node/yt-traversy-build-graphql-server/schema.js
@@ -0,0 +1,12 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = require('graphql');
+
+module.exports = new GraphQLSchema({
+
+});
