d1468602ffb09d7aee0e165254e2d1d8f4cb5dbe
Jinho Hyeon <neverlish@gmail.com>
2018-04-22 17:13:21 +0900

    2 Mongoose Model Setup

--- node/yt-tutsfusion-graphql-nodejs-mongodb/index.js
+++ node/yt-tutsfusion-graphql-nodejs-mongodb/index.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
   res.send('Hello world, this is graphql api.');
 });
 
+mongoose.connect('mongodb://neverlish:asasas@ds153869.mlab.com:53869/graphql-api')
+const db = mongoose.connection;
+
+db.on('error', () => console.log('Failed to connect to database'))
+  .once('open', () => console.log('Connected to DB.'))
+
 // GraphQL API endpoint
 
 app.use('/graphql', graphqlHTTP(() => ({
---
+++ node/yt-tutsfusion-graphql-nodejs-mongodb/models/user.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  }
+}, { collection: 'user', timestamps: true });
+
+export default mongoose.model('user', userSchema);
