99b2c67d33227f9d68c3c22966d1b045ce31fa13
Jinho Hyeon <neverlish@gmail.com>
2018-04-22 16:46:22 +0900

    5 Mutations

--- node/yt-traversy-build-graphql-server/schema.js
+++ node/yt-traversy-build-graphql-server/schema.js
@@ -59,6 +59,53 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+// Mutations
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addCustomer: {
+      type: CustomerType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve(parentValue, args) {
+        return axios.post('http://localhost:3000/customers', {
+          name: args.name,
+          email: args.email,
+          age: args.age
+        })
+        .then(res => res.data);
+      }
+    },
+    deleteCustomer: {
+      type: CustomerType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, args) {
+        return axios.delete('http://localhost:3000/customers/' + args.id)
+        .then(res => res.data);
+      }
+    },
+    editCustomer: {
+      type: CustomerType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parentValue, args) {
+        return axios.patch('http://localhost:3000/customers/' + args.id, args)
+        .then(res => res.data);
+      }
+    }
+  }
+});
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 });
