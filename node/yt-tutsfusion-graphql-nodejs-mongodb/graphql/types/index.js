c0d19abf262c33b416fe0b3591c339ff38fbc4f3
Jinho Hyeon <neverlish@gmail.com>
2018-04-22 17:19:06 +0900

    3 GraphQL Types

---
+++ node/yt-tutsfusion-graphql-nodejs-mongodb/graphql/types/index.js
@@ -0,0 +1,9 @@
+import { userType, userInputType } from './user';
+import { postType, postInputType  } from './post';
+
+export default {
+  userType,
+  userInputType,
+  postType,
+  postInputType
+}
---
+++ node/yt-tutsfusion-graphql-nodejs-mongodb/graphql/types/user.js
@@ -0,0 +1,48 @@
+import {
+  GraphQLObjectType,
+  GraphQLInputObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLID,
+  GraphQLList
+} from 'graphql';
+
+import PostModel from '../../models/post';
+import { postType } from './post';
+
+export const userType = new GraphQLObjectType({
+  name: 'User',
+  description: 'User api',
+  fields: () => ({
+    _id: {
+      type: new GraphQLNonNull(GraphQLID)
+    },
+    email: {
+      type: GraphQLString
+    },
+    name: {
+      type: GraphQLString
+    },
+    posts: {
+      type: new GraphQLList(postType),
+      resolve(user) {
+        const { _id } = user;
+        return PostModel.find({ uid: _id }).exec();
+      }
+    }
+  })
+});
+
+export const userInputType = new GraphQLInputObjectType({
+  name: 'UserInput',
+  description: 'Insert User',
+  fields: () => ({
+    email: {
+      type: GraphQLString
+    },
+    name: {
+      type: GraphQLString
+    }
+  })
+})
