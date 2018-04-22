4acbba63f996c6d1a11ecb1783473744b438b5c5
Jinho Hyeon <neverlish@gmail.com>
2018-04-22 19:13:00 +0900

    3 GraphQL Queries - 09 Apollo Server

--- node/udemy-intro-graphql-apollo/server.js
+++ node/udemy-intro-graphql-apollo/server.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import bodyParser from 'body-parser';
 const server = express();
+
+server.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}));
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({}));
 
 console.log('changing the file');
 
