6f57e91b170b331d7eeda0921eb4a808287fe6f8
Jinho Hyeon <neverlish@gmail.com>
2018-04-22 16:36:14 +0900

    4 JSON Server & Axios

--- node/yt-traversy-build-graphql-server/schema.js
+++ node/yt-traversy-build-graphql-server/schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -7,12 +8,14 @@ const {
   GraphQLNonNull
 } = require('graphql');
 
+/*
 // Hardcoded data
 const customers = [
   { id: '1', name: 'John Doe', email: 'jdoe@gmail.com', age: 35 },
   { id: '2', name: 'Steve Smith', email: 'steve@gmail.com', age: 25 },
   { id: '3', name: 'Sara Williams', email: 'sara@gmail.com', age: 32 },
 ];
+*/
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
@@ -35,17 +38,22 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString }
       },
       resolve(parentValue, args) {
+        /*
         for (let i = 0; i < customers.length; i++) {
           if (customers[i].id === args.id) {
             return customers[i];
           }
         }
+        */
+        return axios.get('http://localhost:3000/customers/' + args.id)
+          .then(res => res.data);
       }
     },
     customers: {
       type: new GraphQLList(CustomerType),
       resolve(parentValue, args) {
-        return customers;
+        return axios.get('http://localhost:3000/customers/')
+          .then(res => res.data);
       }
     }
   }
