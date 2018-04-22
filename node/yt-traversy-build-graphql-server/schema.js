b064788bab5f3db73dc48fcf5dc0f3be02241e1c
Jinho Hyeon <neverlish@gmail.com>
2018-04-22 16:28:31 +0900

    3 RootQuery & Fetching Data

--- node/yt-traversy-build-graphql-server/schema.js
+++ node/yt-traversy-build-graphql-server/schema.js
@@ -7,6 +7,50 @@ const {
   GraphQLNonNull
 } = require('graphql');
 
-module.exports = new GraphQLSchema({
+// Hardcoded data
+const customers = [
+  { id: '1', name: 'John Doe', email: 'jdoe@gmail.com', age: 35 },
+  { id: '2', name: 'Steve Smith', email: 'steve@gmail.com', age: 25 },
+  { id: '3', name: 'Sara Williams', email: 'sara@gmail.com', age: 32 },
+];
 
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id === args.id) {
+            return customers[i];
+          }
+        }
+      }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args) {
+        return customers;
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
