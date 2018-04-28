09d887a63f941f1d50b847184e39cae1ba946259
Jinho Hyeon <neverlish@gmail.com>
2018-04-28 15:39:44 +0900

    03 데이터 타입

---
+++ swift/yt-yagom-intro-swift/03-data_type.playground/Contents.swift
@@ -0,0 +1,39 @@
+// Swift의 기본 데이터 타입
+// Bool, Int, Uint, Float, Double, Character, String
+
+// Bool
+var someBool: Bool = true
+someBool = false
+//someBool = 0
+//someBool = 1
+
+// Int
+var someInt: Int = -100
+//someInt = 100.1
+
+// UInt
+var someUInt: UInt = 100
+//someUInt = -100
+//someUInt = someInt
+
+// Float
+var someFloat: Float = 3.14
+someFloat = 3
+
+// Double
+var someDouble: Double = 3.14
+someDouble = 3
+//someDouble = someFloat
+
+// Character
+var someCharacter: Character = "🇰🇷"
+someCharacter = "😊"
+someCharacter = "가"
+someCharacter = "A"
+//someCharacter = "하하하"
+
+// String
+var someString: String = "하하하 😁"
+someString = someString + "웃으면 복이 와요"
+
+//someString = someCharacter
