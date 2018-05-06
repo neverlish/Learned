181261e028473da14a598677ef792356d3d8523b
Jinho Hyeon <neverlish@gmail.com>
2018-05-07 01:37:27 +0900

    08 ES6 Setup

---
+++ react/yt-codewithtim-reduxmadeeasy/src/index.js
@@ -0,0 +1,53 @@
+import { createStore } from 'redux';
+
+// REDUCER 
+function counterReducer(state = {count: 0} , action) {
+  var nextState = {
+    count: state.count
+  }
+
+  switch (action.type) {
+    case 'ADD':
+      nextState.count = state.count + 1
+      return nextState
+      break
+    case 'MINUS':
+      nextState.count = state.count - 1
+      return nextState
+      break
+    case 'RESET':
+      nextState.count = 0
+      return nextState
+      break
+    default:
+      return state
+  }
+}
+
+// STORE
+const store = createStore(counterReducer)
+const counterEl = document.getElementById('counter')
+
+function render() {
+  var state = store.getState()
+  counterEl.innerHTML = state.count.toString()
+}
+
+render()
+store.subscribe(render)
+
+// ACTIONS
+document.getElementById('add')
+  .addEventListener('click', () => {
+    store.dispatch({ type: 'ADD' })
+  })
+
+document.getElementById('minus')
+  .addEventListener('click', () => {
+    store.dispatch({ type: 'MINUS' })
+  })
+
+document.getElementById('reset')
+  .addEventListener('click', () => {
+    store.dispatch({ type: 'RESET' })
+  })
