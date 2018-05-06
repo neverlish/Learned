a8481ae851ecf0ea992606bd14472e2f2d6e7946
Jinho Hyeon <neverlish@gmail.com>
2018-05-07 03:56:58 +0900

    17 React Router

--- react/yt-codewithtim-reduxmadeeasy/src/components/users.js
+++ react/yt-codewithtim-reduxmadeeasy/src/components/users.js
@@ -2,6 +2,30 @@ import React from 'react'
 import Button from './button'
 import UserItem from './userItem'
 
+class Link extends React.Component {
+  constructor(props) {
+    super(props)
+    this.changeUrl = this.changeUrl.bind(this)
+  }
+
+  changeUrl() {
+    window.location.replace(this.props.newUrl)
+  }
+
+  render() {
+    return (
+      <div>
+        <Button
+          onClick={this.changeUrl}
+          text={`Go to ${this.props.newUrl}`}
+          className='btn btn-blue'
+        />
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
 class Users extends React.Component {
   constructor(props) {
     super(props)
@@ -16,6 +40,11 @@ class Users extends React.Component {
           text='Fetch Users'
           className='btn btn-blue'
         />
+        <Link newUrl='http://www.codewithtim.com'>
+          <p>Go to codewithtim.com</p>
+          <p>Go to google.com</p>
+          <p>Go to udemy.com</p>
+        </Link>
         <div id='users'>
           {data.users.map((user, i) => {
             return <UserItem
