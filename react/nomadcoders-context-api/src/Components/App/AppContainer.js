8a0769436ea2d429050a8e607d1183be3849621a
Jinho Hyeon <neverlish@gmail.com>
2018-05-06 22:35:31 +0900

    4 Consuming the Store

--- react/nomadcoders-context-api/src/Components/App/AppContainer.js
+++ react/nomadcoders-context-api/src/Components/App/AppContainer.js
@@ -3,9 +3,19 @@ import AppPresenter from "./AppPresenter";
 import Store from "store";
 
 class AppContainer extends Component {
+  state = {
+    message: 'Hello'
+  }
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        message: 'Bye'
+      })
+    }, 2000)
+  }
   render() {
     return (
-      <Store.Provider>
+      <Store.Provider value={this.state}>
         <AppPresenter />
       </Store.Provider>
     );
--- react/nomadcoders-context-api/src/Components/Notification/NotificationPresenter.js
+++ react/nomadcoders-context-api/src/Components/Notification/NotificationPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import Flex, { FlexItem } from "styled-flex-component";
 import FontAwesome from "react-fontawesome";
+import Store from 'store';
 
 const Notification = styled.div`
   background-color: white;
@@ -54,7 +55,9 @@ const Button = styled.button`
 const NotificationPresenter = ({ id, text, seen }) => (
   <Notification seen={seen}>
     <Flex alignCenter justifyBetween>
-      <Title>{text}</Title>
+      <Title>
+        <Store.Consumer>{store => store.message}</Store.Consumer>
+      </Title>
       <FlexItem>
         <Fragment>
           <Button success seen={seen} onClick={() => {}}>
