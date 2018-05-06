133c93ecae468ee6276026a049c6b80a235da095
Jinho Hyeon <neverlish@gmail.com>
2018-05-07 03:47:32 +0900

    16 User List Component

--- react/yt-codewithtim-reduxmadeeasy/src/components/users.js
+++ react/yt-codewithtim-reduxmadeeasy/src/components/users.js
@@ -1,20 +1,29 @@
 import React from 'react'
+import Button from './button'
+import UserItem from './userItem'
 
 class Users extends React.Component {
   constructor(props) {
     super(props)
-    this.componentDidMount = this.componentDidMount.bind(this)
-  }
-
-  componentDidMount() {
-    console.log(this.props)
-    this.props.fetchUsers()
   }
 
   render() {
+    const { data, fetchUsers } = this.props
     return (
-      <div>
-        hello
+      <div className='container'>
+        <Button 
+          onClick={fetchUsers}
+          text='Fetch Users'
+          className='btn btn-blue'
+        />
+        <div id='users'>
+          {data.users.map((user, i) => {
+            return <UserItem
+              user={user}
+              key={i} 
+            />
+          })}
+        </div>
       </div>
     )
   }
