73d823ff49ce9f6342e54e9077041262d30f9187
Jinho Hyeon <neverlish@gmail.com>
2018-05-06 23:03:36 +0900

    7 Counting Unseen Notifications

--- react/nomadcoders-context-api/src/Components/Header/HeaderPresenter.js
+++ react/nomadcoders-context-api/src/Components/Header/HeaderPresenter.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Flex, { FlexItem } from "styled-flex-component";
 import FontAwesome from "react-fontawesome";
+import Store from 'store';
 
 const Header = styled.header`
   height: 100px;
@@ -43,6 +44,16 @@ const Number = styled.span`
   top: -10px;
 `;
 
+const getUnseen = (notifications) => {
+  let unseen = [];
+  Object.keys(notifications).map(key => {
+    if (!notifications[key].seen) {
+      return unseen.push(notifications[key]);
+    }
+  })
+  return unseen.length;
+}
+
 const HeaderPresenter = () => (
   <Header>
     <Flex full justifyBetween alignCenter>
@@ -59,7 +70,11 @@ const HeaderPresenter = () => (
           </HeaderIcon>
           <HeaderIcon>
             <FontAwesome name="bell" />
-            <Number>10</Number>
+            <Number>
+              <Store.Consumer>
+                {store => getUnseen(store.notifications)}
+              </Store.Consumer>
+            </Number>
           </HeaderIcon>
         </Flex>
       </FlexItem>
