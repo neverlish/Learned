b06270496ad630d933d758639b247244e55e6dfe
Jinho Hyeon <neverlish@gmail.com>
2018-05-07 04:30:54 +0900

    19 Implementing React Router Part 1

--- react/yt-codewithtim-reduxmadeeasy/src/index.js
+++ react/yt-codewithtim-reduxmadeeasy/src/index.js
@@ -3,10 +3,15 @@ import { render } from 'react-dom'
 import Users from './containers/users'
 import { Provider } from 'react-redux'
 import UsersStore from './store'
+import App from './components/app'
+import { Router, Route, hashHistory } from 'react-router'
 
 render(
   <Provider store={UsersStore}>
-    <Users />
+    <Router history={hashHistory}>
+      <Route path='/users' component={Users} />
+      <Route path='/test' component={Users} />
+    </Router>
   </Provider>,
   document.getElementById('app')
 )
