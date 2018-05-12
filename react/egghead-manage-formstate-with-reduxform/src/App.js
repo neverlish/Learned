314da2b0410b3639b5a3aab8784fc54e108d959c
Jinho Hyeon <neverlish@gmail.com>
2018-05-12 23:00:42 +0900

    05 Set an Initial Value of a Form Field using redux-form

--- react/egghead-manage-formstate-with-reduxform/src/App.js
+++ react/egghead-manage-formstate-with-reduxform/src/App.js
@@ -5,8 +5,16 @@ class RegisterFormContainer extends Component {
   submit = values => {
     window.alert(JSON.stringify(values), null, 4)
   }
+
+  getInitialValues() {
+    return {
+      preference: 'spaces',
+      newsletter: true
+    }
+  }
+
   render() {
-    return <RegisterForm onSubmit={this.submit} />
+    return <RegisterForm onSubmit={this.submit} initialValues={this.getInitialValues()} />
   }
 }
 
