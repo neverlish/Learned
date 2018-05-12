986a245423c73cc8f91e6a5c444f1fed471af02d
Jinho Hyeon <neverlish@gmail.com>
2018-05-12 23:39:01 +0900

    11 Use Asynchronous Validation after Submitting a Form in redux-form

--- react/egghead-manage-formstate-with-reduxform/src/App.js
+++ react/egghead-manage-formstate-with-reduxform/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import RegisterForm from './components/RegisterForm';
+import { SubmissionError } from 'redux-form';
 
 class RegisterFormContainer extends Component {
   submit = values => {
-    window.alert(JSON.stringify(values), null, 4)
+    if (['kent', 'andy', 'john', 'joel'].includes(values.username)) {
+      throw new SubmissionError({
+        username: 'Username already taken'
+      })
+    } else {
+      window.alert(JSON.stringify(values), null, 4)
+    }
   }
 
   getInitialValues() {
