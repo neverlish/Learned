c6a45e9baa32863c2734eb8d46351d1019ea8c71
Jinho Hyeon <neverlish@gmail.com>
2018-05-13 17:13:25 +0900

    12 Lazy load Images using Intersection Observer API

--- vue/egghead-offline-pwa-in-vue/src/components/Images.vue
+++ vue/egghead-offline-pwa-in-vue/src/components/Images.vue
@@ -3,11 +3,10 @@
     <v-layout row wrap>
       <v-flex xs12 sm6 md4 lg3 class="message-list" v-for="image in images" :key="image.id">
         <v-card flat tile :to="`/images/${image.id}`">
-          <v-card-media
+          <lazy-image
             :src="imageUrl(image)"
-            height="150px"
           >
-          </v-card-media>
+          </lazy-image>
         </v-card>
       </v-flex>
     </v-layout>
@@ -16,18 +15,22 @@
 
 
 <script>
+import LazyImage from './LazyImage'
 export default {
-  data: () => ({ images: [], limit: 20 }),
-  created() {
-    fetch('https://picsum.photos/list')
-      .then(data => data.json())
-      .then(images => (this.images = images.slice(0, this.limit)))
-  },
-  methods: {
-    imageUrl(image) {
-      const imageId = image.post_url.split('/').pop()
-      return `https://source.unsplash.com/${imageId}`
-    }
-  }
+	data: () => ({ images: [], limit: 20 }),
+	created() {
+		fetch('https://picsum.photos/list')
+			.then(data => data.json())
+			.then(images => (this.images = images.slice(0, this.limit)))
+	},
+	methods: {
+		imageUrl(image) {
+			const imageId = image.post_url.split('/').pop()
+			return `https://source.unsplash.com/${imageId}`
+		},
+	},
+	components: {
+		LazyImage,
+	},
 }
 </script>
