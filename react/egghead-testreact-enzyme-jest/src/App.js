79f454c4aa831a9bf848994bfd82fc237b3c2947
Jinho Hyeon <neverlish@gmail.com>
2018-05-14 21:20:57 +0900

    04 Shallow Render a React Component with Enzyme

--- react/egghead-testreact-enzyme-jest/src/App.js
+++ react/egghead-testreact-enzyme-jest/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Test = () => <div>Testing</div>
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Test />
       </div>
     );
   }
--- react/egghead-testreact-enzyme-jest/src/App.test.js
+++ react/egghead-testreact-enzyme-jest/src/App.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { configure } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() })
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App />', () => {
+  it('should render App', () => {
+    const wrapper = shallow(<App />, {context: {}, disableLifecycleMethods: bool})
+    console.log(wrapper.debug())
+  })
+})
