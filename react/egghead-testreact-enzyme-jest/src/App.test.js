1ca7c0324be411d3effe984c29a6e4f170384c84
Jinho Hyeon <neverlish@gmail.com>
2018-05-14 21:47:55 +0900

    09 Fully Render React Components with Enzyme

--- react/egghead-testreact-enzyme-jest/src/App.test.js
+++ react/egghead-testreact-enzyme-jest/src/App.test.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App, { Link } from './App';
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json'
 
 configure({ adapter: new Adapter() })
 
-describe('<App />', () => {
-  const wrapper = shallow(<App />)
+describe('<App /> shallow rendering', () => {
   it('h1 contains correct text', () => {
+    const wrapper = shallow(<App />)
     expect(wrapper.find('h1').text()).toBe('Welcome to React')
   })
   it('matches the snapshop', () => {
     const tree = shallow(<App />)
     expect(toJson(tree)).toMatchSnapshot()
+  })
+})
+
+describe('<App /> mount rendering', () => {
+  it('h1 contains correct text', () => {
+    const wrapper = mount(<App />)
+    expect(wrapper.find('h1').text()).toBe('Welcome to React')
+    wrapper.unmount()
+  })
+  it('matches the snapshop', () => {
+    const tree = mount(<App />)
+    expect(toJson(tree)).toMatchSnapshot()
+    tree.unmount()
   })
 })
 
