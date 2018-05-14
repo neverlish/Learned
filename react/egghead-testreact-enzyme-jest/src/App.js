e62c23f692fd4c0dd780813ca284ffe8688e9764
Jinho Hyeon <neverlish@gmail.com>
2018-05-14 21:33:05 +0900

    06 Understand the Different Accepted Selectors in Enzyme

--- react/egghead-testreact-enzyme-jest/src/App.js
+++ react/egghead-testreact-enzyme-jest/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Title = ({text}) => <div>{text}</div>
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Title text='Some Title' />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- react/egghead-testreact-enzyme-jest/src/App.test.js
+++ react/egghead-testreact-enzyme-jest/src/App.test.js
@@ -9,6 +9,6 @@ configure({ adapter: new Adapter() })
 describe('<App />', () => {
   const wrapper = shallow(<App />)
   it('h1 contains correct text', () => {
-    expect(wrapper.find('h1').text()).toBe('Welcome to React')
+    expect(wrapper.find({alt: 'logo'}).text()).toBe('Welcome to React')
   })
 })
