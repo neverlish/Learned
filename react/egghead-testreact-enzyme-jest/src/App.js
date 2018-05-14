dcde77dc20040773478275684f7764844e05f1c5
Jinho Hyeon <neverlish@gmail.com>
2018-05-14 21:37:30 +0900

    07 Test Enzyme Rendered Components with Jest Snapshots

--- react/egghead-testreact-enzyme-jest/src/App.js
+++ react/egghead-testreact-enzyme-jest/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const Title = ({text}) => <div>{text}</div>
-
 class App extends Component {
   render() {
     return (
@@ -12,7 +10,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Title text='Some Title' />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- react/egghead-testreact-enzyme-jest/src/App.test.js
+++ react/egghead-testreact-enzyme-jest/src/App.test.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json'
 
 configure({ adapter: new Adapter() })
 
 describe('<App />', () => {
   const wrapper = shallow(<App />)
   it('h1 contains correct text', () => {
-    expect(wrapper.find({alt: 'logo'}).text()).toBe('Welcome to React')
+    expect(wrapper.find('h1').text()).toBe('Welcome to React')
+  })
+  it('matches the snapshop', () => {
+    const tree = shallow(<App />)
+    expect(toJson(tree)).toMatchSnapshot()
   })
 })
