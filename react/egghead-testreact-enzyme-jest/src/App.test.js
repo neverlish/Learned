1f1e3b080722ae9ef8b30817621ed94570a43e83
Jinho Hyeon <neverlish@gmail.com>
2018-05-14 21:18:21 +0900

    03 Test Against Different React Releases with Enzyme

--- react/egghead-testreact-enzyme-jest/src/App.test.js
+++ react/egghead-testreact-enzyme-jest/src/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() })
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
