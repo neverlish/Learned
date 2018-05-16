e0e0a7c3ad608bb935b5c66704c93ac8a42a71ed
Jinho Hyeon <neverlish@gmail.com>
2018-05-17 02:22:48 +0900

    08 Creating a Block part Two

--- tsc/nomadcoders-blockchain-with-ts/src/index.ts
+++ tsc/nomadcoders-blockchain-with-ts/src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJS from 'crypto-js';
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string, 
+    timestamp: number, 
+    data: string
+  ): string => 
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString()
   
   constructor(
     index: number,
@@ -23,6 +33,12 @@ class Block {
 const genesisBlock: Block = new Block(0, '202020202020202', '', 'Hello', 123456);
 
 let blockchain: [Block] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 console.log(blockchain)
 
