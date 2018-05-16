ac5060d016744b9605c39d7399569046e989766f
Jinho Hyeon <neverlish@gmail.com>
2018-05-17 02:45:36 +0900

    12 Conclusions

--- tsc/nomadcoders-blockchain-with-ts/src/index.ts
+++ tsc/nomadcoders-blockchain-with-ts/src/index.ts
@@ -64,6 +64,7 @@ const createNewBlock = (data: string): Block => {
     data, 
     nextTimeStamp
   );
+  addBlock(newBlock);
   return newBlock;
 };
 
@@ -88,5 +89,11 @@ const addBlock = (candidateBlock: Block): void => {
     blockchain.push(candidateBlock);
   }
 }
+
+createNewBlock('second block');
+createNewBlock('third block');
+createNewBlock('fourth block');
+
+console.log(blockchain);
 
 export {};
