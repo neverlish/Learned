dc455804299680f09b2c7329acb00e1aac670429
Jinho Hyeon <neverlish@gmail.com>
2018-05-17 02:43:10 +0900

    11 Validating Block Structure part Two

--- tsc/nomadcoders-blockchain-with-ts/src/index.ts
+++ tsc/nomadcoders-blockchain-with-ts/src/index.ts
@@ -67,6 +67,8 @@ const createNewBlock = (data: string): Block => {
   return newBlock;
 };
 
+const getHashforBlock = (aBlock: Block): string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
 const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
   if (!Block.validateStructure(candidateBlock)) {
     return false;
@@ -74,6 +76,16 @@ const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
     return false;
   } else if (previousBlock.hash !== candidateBlock.previousHash) {
     return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
   }
 }
 
