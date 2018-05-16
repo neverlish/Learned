ba3133f2510b552ce280a809cfbdcfe4a1f9d9bc
Jinho Hyeon <neverlish@gmail.com>
2018-05-17 02:02:55 +0900

    06 Classes on Typescript part One

--- tsc/nomadcoders-blockchain-with-ts/src/index.ts
+++ tsc/nomadcoders-blockchain-with-ts/src/index.ts
@@ -1,19 +1,21 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: 'nicolas',
-  age: 22,
-  gender: 'male'
-}
+const lynn = new Human('Lynn', 18, 'female');
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
