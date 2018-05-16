12f499a7cad1eb96715b9ad901cc9ae9868b7ec7
Jinho Hyeon <neverlish@gmail.com>
2018-05-17 01:44:35 +0900

    05 Interfaces on Typescript

--- tsc/nomadcoders-blockchain-with-ts/src/index.ts
+++ tsc/nomadcoders-blockchain-with-ts/src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
 }
 
-console.log(sayHi('Nicolas', 444, 'male'));
+const person = {
+  name: 'nicolas',
+  age: 22,
+  gender: 'male'
+}
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+}
+
+console.log(sayHi(person));
 
 export {};
