a522b867c17dcefeb9aa65438bb138f04100aa42
Jinho Hyeon <neverlish@gmail.com>
2018-05-16 21:36:45 +0900

    16 Map Vue.js Components to Remote Data Streams with RxJS

--- vue/egghead-async-vueapp-with-rxjs/src/App.vue
+++ vue/egghead-async-vueapp-with-rxjs/src/App.vue
@@ -3,7 +3,7 @@
     {{activeTab$}}
     <b-tabs v-model='activeTab'>
       <b-tab-item
-        v-for='person of people'
+        v-for='person of people$'
         :key='person.id'
         :label='person.name'></b-tab-item>
     </b-tabs>
@@ -21,26 +21,31 @@ import {Observable} from 'rxjs'
 export default {
   data() {
     return {
-      activeTab: 0,
-      people: [
-        { name: 'Luke', id: 1 },
-        { name: 'Darth', id: 4 },
-        { name: 'Leia', id: 5 },
-        { name: 'Yoda', id: 20 },
-      ]
+      activeTab: 0
     }
   },
   domStreams: ['click$', 'imageError$'],
   subscriptions() {
-    const activeTab$ = this.$watchAsObservable(
-      'activeTab',
-      { immediate: true }
-    ).pluck('newValue')
     const createLoader = url => Observable.from(
       this.$http.get(url)
     ).pluck('data')
 
-    const luke$ = activeTab$.map(tabId => this.people[tabId].id)
+    const people$ = createLoader(
+      `https://starwars.egghead.training/people`
+    ).map(people => people.slice(0, 7))
+
+    const activeTab$ = this.$watchAsObservable(
+      'activeTab',
+      { immediate: true }
+    ).pluck('newValue')
+    
+
+    const luke$ = activeTab$
+      // .map(tabId => this.people[tabId].id)
+      .combineLatest(
+        people$, 
+        (tabId, people) => people[tabId].id
+      )
       .map(id => `https://starwars.egghead.training/people/${id}`)
       .exhaustMap(createLoader)
       // .catch(err => Observable.of({name: 'Failed ... :('}))
@@ -70,7 +75,8 @@ export default {
       image$,
       disabled$,
       buttonText$,
-      activeTab$
+      activeTab$,
+      people$
     }
   }
 }
