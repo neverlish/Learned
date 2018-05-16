69c29285f4742fa0efba45efc6144c8167227707
Jinho Hyeon <neverlish@gmail.com>
2018-05-16 20:44:47 +0900

    06 Split a Request into Data Stream and an Image Stream with RxJS and Vue.js

--- vue/egghead-async-vueapp-with-rxjs/src/App.vue
+++ vue/egghead-async-vueapp-with-rxjs/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <section class='section'>
     <button class='button' v-stream:click='click$'>click</button>
-    <h1 class='title'>{{luke$}}</h1>
+    <h1 class='title'>{{name$}}</h1>
+    <img :src='image$' alt=''/>
   </section>
 </template>
 
@@ -11,15 +12,18 @@ import {Observable} from 'rxjs'
 export default {
   domStreams: ['click$'],
   subscriptions() {
-    const people$ = Observable.from(
+    const person$ = Observable.from(
       this.$http.get(
         'https://starwars.egghead.training/people/1'
       )
-    ).pluck('data', 'name')
-    const luke$ = this.click$.switchMap(() => people$)
+    ).pluck('data')
+    const luke$ = this.click$.switchMap(() => person$)
+    
+    const name$ = luke$.pluck('name')
+    const image$ = luke$.pluck('image').map(image => `https://starwars.egghead.training/${image}`)
     return {
-      luke$,
-      people$
+      name$,
+      image$
     }
   }
 }
