374a0537ee19ccb7e3317501ad6328dde61c4acd
Jinho Hyeon <neverlish@gmail.com>
2018-05-17 18:11:58 +0900

    13 User Sign Up: Uniqueness Validation (Part 1)

--- react/yt-remzolothky-reactredux/server/routes/users.js
+++ react/yt-remzolothky-reactredux/server/routes/users.js
@@ -1,26 +1,65 @@
 import express from 'express';
-import validateInput from '../shared/validations/signup';
+import commonValidations from '../shared/validations/signup';
 import bcrypt from 'bcrypt';
+import Promise from 'bluebird';
+import isEmpty from 'lodash/isEmpty';
 
 import User from '../models/user';
 
 let router = express.Router();
 
+function validateInput(data, otherValidations) {
+  let { errors } = otherValidations(data);
+
+  return User.query({
+    where: { email: data.email },
+    orWhere: { username: data.username }
+  }).fetch().then(user => {
+    if (user) {
+      if (user.get('username') === data.username) {
+        errors.username = 'There is user with such username';
+      }
+      if (user.get('email') === data.email) {
+        errors.email = 'There is user with such email';
+      }
+    }
+
+    return {
+      errors, 
+      isValid: isEmpty(errors)
+    }
+  })
+
+  // return Promise.all([
+  //   User.where({ email: data.email }).fetch().then(user => {
+  //     if (user) { errors.email = 'There is user with such email'; }
+  //   }),
+  //   User.where({ username: data.username }).fetch().then(user => {
+  //     if (user) { errors.username = 'There is user with such username'; }
+  //   })
+  // ]).then(() => {
+  //   return {
+  //     errors, 
+  //     isValid: isEmpty(errors)
+  //   }
+  // })
+}
+
 router.post('/', (req, res) => {
-  const { errors, isValid } = validateInput(req.body);
-
-  if (isValid) {
-    const { username, password, timezone, email } = req.body;
-    const password_digest = bcrypt.hashSync(password, 10);
-
-    User.forge({
-      username, timezone, email, password_digest
-    }, { hasTimestamps: true }).save()
-      .then(user => res.json({ success: true }))
-      .catch(err => res.status(500).json({ error: err }))
-  } else {
-    res.status(400).json(errors);
-  }
+  validateInput(req.body, commonValidations).then(({ errors, isValid }) => {
+    if (isValid) {
+      const { username, password, timezone, email } = req.body;
+      const password_digest = bcrypt.hashSync(password, 10);
+  
+      User.forge({
+        username, timezone, email, password_digest
+      }, { hasTimestamps: true }).save()
+        .then(user => res.json({ success: true }))
+        .catch(err => res.status(500).json({ error: err }))
+    } else {
+      res.status(400).json(errors);
+    }
+  });
 });
 
 export default router;
