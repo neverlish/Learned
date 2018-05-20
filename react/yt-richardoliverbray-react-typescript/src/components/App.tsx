67eecc823e85d92660f2dbf6c20469797105f559
Jinho Hyeon <neverlish@gmail.com>
2018-05-20 11:45:19 +0900

    6 Interface Nesting

--- react/yt-richardoliverbray-react-typescript/src/components/App.tsx
+++ react/yt-richardoliverbray-react-typescript/src/components/App.tsx
@@ -16,15 +16,29 @@ export class App extends React.Component<{}, IState> {
       currentTask: '',
       tasks: [
         ...this.state.tasks, 
-        this.state.currentTask
+        {
+          id: this._timeInMilliseconds(),
+          value: this.state.currentTask,
+          completed: false
+        }
       ]
     })
   }
 
+  public deleteTask(id: number): void {
+    const filteredTasks: Array<ITask> = this.state.tasks.filter(
+      (task: ITask) => task.id !== id
+    );
+    this.setState({ tasks: filteredTasks });
+  }
+
   public renderTasks(): JSX.Element[] {
-    return this.state.tasks.map((task: string, index: number) => {
+    return this.state.tasks.map((task: ITask, index: number) => {
       return (
-        <div key={index}>{task}</div>
+        <div key={task.id}>
+          <span>{task.value}</span>
+          <button onClick={() => this.deleteTask(task.id)}>Delete</button>
+        </div>
       )
     });
   }
@@ -47,9 +61,20 @@ export class App extends React.Component<{}, IState> {
       </div>
     );
   }
+
+  private _timeInMilliseconds(): number {
+    const date: Date = new Date();
+    return date.getTime();
+  }
 }
 
 interface IState {
   currentTask: string;
-  tasks: Array<string>;
+  tasks: Array<ITask>;
+}
+
+interface ITask {
+  id: number;
+  value: string;
+  completed: boolean;
 }
