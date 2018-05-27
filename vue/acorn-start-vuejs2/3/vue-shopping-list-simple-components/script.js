22a8deb15fba39a53d455b75008379c04de9d016
Jinho Hyeon <neverlish@gmail.com>
2018-05-28 00:22:46 +0900

    3 - 2 간단한 컴포넌트를 사용해 쇼핑 목록 재작성 - 모든 컴포넌트들을 위한 템플릿 정의하기

---
+++ vue/acorn-start-vuejs2/3/vue-shopping-list-simple-components/script.js
@@ -0,0 +1,12 @@
+var data = {
+  items: [{ text: 'Bananas', checked: true }, { text: 'Apples', checked: false }],
+  title: 'My Shopping List'
+};
+
+new Vue({
+  el: '#app',
+  data: data,
+  methods: {
+    
+  }
+});
