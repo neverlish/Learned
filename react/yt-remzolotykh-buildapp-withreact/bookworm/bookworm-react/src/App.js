30d241392ee429887c99d78af707e93c599cf6d5
Jinho Hyeon <neverlish@gmail.com>
2018-06-06 23:10:39 +0900

    [Bookworm-Client] 04 User signs up

--- react/yt-remzolotykh-buildapp-withreact/bookworm/bookworm-react/src/App.js
+++ react/yt-remzolotykh-buildapp-withreact/bookworm/bookworm-react/src/App.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { Route } from 'react-router-dom'
 import HomePage from './components/pages/HomePage'
 import LoginPage from './components/pages/LoginPage'
+import SignupPage from './components/pages/SignupPage'
 import DashboardPage from './components/pages/DashboardPage'
 import UserRoute from './components/routes/UserRoute'
 import GuestRoute from './components/routes/GuestRoute'
@@ -11,6 +12,7 @@ const App = ({ location }) => (
   <div className='ui container'>
     <Route location={location} path='/' exact component={HomePage} />
     <GuestRoute location={location} path='/login' exact component={LoginPage} />
+    <GuestRoute location={location} path='/signup' exact component={SignupPage} />
     <UserRoute location={location} path='/dashboard' exact component={DashboardPage} />
   </div>
 )
--- react/yt-remzolotykh-buildapp-withreact/bookworm/bookworm-react/src/api.js
+++ react/yt-remzolotykh-buildapp-withreact/bookworm/bookworm-react/src/api.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 export default {
   user: {
-    login: (credentials) => 
-      axios.post('/api/auth', { credentials }).then(res => res.data.user)
+    login: credentials => 
+      axios.post('/api/auth', { credentials }).then(res => res.data.user),
+    signup: user => 
+      axios.post('/api/users', { user }).then(res => res.data.user)
   }
 }
