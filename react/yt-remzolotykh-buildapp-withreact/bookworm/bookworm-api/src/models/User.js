09af00445f254e438fcd1cba13f0683282b4476d
Jinho Hyeon <neverlish@gmail.com>
2018-06-07 01:14:09 +0900

    [Bookworm-Server] 05 User email confirmation

--- react/yt-remzolotykh-buildapp-withreact/bookworm/bookworm-api/src/models/User.js
+++ react/yt-remzolotykh-buildapp-withreact/bookworm/bookworm-api/src/models/User.js
@@ -7,7 +7,8 @@ import uniqueValidator from 'mongoose-unique-validator'
 const schema = new mongoose.Schema({
   email: { type: String, required: true, lowercase: true, index: true, unique: true },
   passwordHash: { type: String, required: true },
-  confirmed: { type: Boolean, default: false }
+  confirmed: { type: Boolean, default: false },
+  confirmationToken: { type: String, default: '' }
 }, { timestamps: true })
 
 schema.methods.isValidPassword = function isValidPassword(password) {
@@ -18,9 +19,18 @@ schema.methods.setPassword = function setPassword(password) {
   this.passwordHash = bcrypt.hashSync(password, 10)
 }
 
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+  this.confirmationToken = this.generateJWT()
+}
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+  return `${process.env.HOST}/confirmation/${this.confirmationToken}`
+}
+
 schema.methods.generateJWT = function generateJWT () {
   return jwt.sign({
-    email: this.email
+    email: this.email,
+    confirmed: this.confirmed
   }, process.env.JWT_SECRET)
 }
 
