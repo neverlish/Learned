4c2e118b5b835bbef7b41cb0a99bb912a5925960
Jinho Hyeon <neverlish@gmail.com>
2018-06-08 11:42:41 +0900

    [Bookworm-Client] 08 Include token into authorization header

--- react/yt-remzolotykh-buildapp-withreact/bookworm/bookworm-react/src/index.js
+++ react/yt-remzolotykh-buildapp-withreact/bookworm/bookworm-react/src/index.js
@@ -11,6 +11,7 @@ import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 import rootReducer from './rootReducer'
 import { userLoggedIn } from "./actions/auth";
+import setAuthorizationHeader from './utils/setAuthorizationHeader'
 
 const store = createStore(
   rootReducer, 
@@ -20,6 +21,7 @@ const store = createStore(
 if (localStorage.bookwormJWT) {
   const payload = decode(localStorage.bookwormJWT)
   const user = { token: localStorage.bookwormJWT, email: payload.email, confirmed: payload.confirmed }
+  setAuthorizationHeader(localStorage.bookwormJWT)
   store.dispatch(userLoggedIn(user))
 }
 
