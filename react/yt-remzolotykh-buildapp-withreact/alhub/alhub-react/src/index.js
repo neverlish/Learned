29c9bb05a347cf62f0a61abc27543b56dbd24b22
Jinho Hyeon <neverlish@gmail.com>
2018-06-09 20:54:23 +0900

    [Alhub-Client] 11 Simple i18n

--- react/yt-remzolotykh-buildapp-withreact/alhub/alhub-react/src/index.js
+++ react/yt-remzolotykh-buildapp-withreact/alhub/alhub-react/src/index.js
@@ -6,11 +6,18 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { addLocaleData } from 'react-intl'
+import en from 'react-intl/locale-data/en'
+import ko from 'react-intl/locale-data/ko'
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import rootReducer from "./rootReducer";
 import { userFetched, fetchCurrentUser } from './actions/users'
 import setAuthorizationHeader from "./utils/setAuthorizationHeader";
+import { localSet } from './actions/locale'
+
+addLocaleData(en)
+addLocaleData(ko)
 
 const store = createStore(
   rootReducer,
@@ -22,6 +29,10 @@ if (localStorage.bookwormJWT) {
   store.dispatch(fetchCurrentUser());
 } else {
   store.dispatch(userFetched({}))
+}
+
+if (localStorage.alhubLang) {
+  store.dispatch(localSet(localStorage.alhubLang))
 }
 
 ReactDOM.render(
---
+++ react/yt-remzolotykh-buildapp-withreact/alhub/alhub-react/src/messages.js
@@ -0,0 +1,8 @@
+export default {
+  en: {
+    'nav.dashboard': 'Dashboard'
+  },
+  ko: {
+    'nav.dashboard': '대시보드'
+  }
+}
