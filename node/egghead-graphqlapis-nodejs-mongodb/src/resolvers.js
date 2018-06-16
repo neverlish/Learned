7088ecf1c2bf222e6764899879ab3aff599dc9c3
Jinho Hyeon <neverlish@gmail.com>
2018-06-16 12:29:34 +0900

    07 Create GraphQL Query to Fetch All Records from Array

--- node/egghead-graphqlapis-nodejs-mongodb/src/resolvers.js
+++ node/egghead-graphqlapis-nodejs-mongodb/src/resolvers.js
@@ -1,7 +1,17 @@
+const products = [{
+  _id: '12',
+  name: 'GraphQL course',
+  qty: 1
+}, {
+  _id: '13',
+  name: 'Nodejs course',
+  qty: 1
+}]
+
 export const resolvers = {
   Query: {
-    hello(root, {msg}, context, info) {
-      return msg;
+    allProduct() {
+      return products
     }
   }
 }
