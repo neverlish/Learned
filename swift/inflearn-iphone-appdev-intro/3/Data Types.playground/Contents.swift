37e33f11884b436efaacaa6ebc0301adaca16506
Jinho Hyeon <neverlish@gmail.com>
2018-06-16 22:02:00 +0900

    3 - 자료형

---
+++ swift/inflearn-iphone-appdev-intro/3/Data Types.playground/Contents.swift
@@ -0,0 +1,9 @@
+var message: String = "Hello~"
+
+var isLogin: Bool = false
+isLogin = true
+
+var day: Int = 27
+
+var weight: Double = 65.5234583495839485903495834058340985093485038454
+var height: Float = 175.5439573497589347593479573498573498759834759345
