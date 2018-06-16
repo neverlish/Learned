a3914012ed30b9a83a500fd222137c78c1f7f39d
Jinho Hyeon <neverlish@gmail.com>
2018-06-16 21:48:52 +0900

    3 - 프로그래밍 핵심 개념

--- swift/inflearn-iphone-appdev-intro/3/Variable.playground/Contents.swift
+++ swift/inflearn-iphone-appdev-intro/3/Variable.playground/Contents.swift
@@ -7,3 +7,20 @@ radius = 5.456
 radius * radius * pi
 
 // pi = 3.14 // cannot assign to value
+
+func circleArea(r: Double) -> Double {
+    return r * r * 3.1415926535
+}
+
+circleArea(r: 3.123)
+circleArea(r: 5.456)
+circleArea(r: 8.97)
+
+class Circle {
+    var radius = 3.123
+    let pi = 3.1415926535
+    
+    func circleArea(r: Double) -> Double {
+        return r * r * 3.1415926535
+    }
+}
