06ae245a609ed6bf94a26c6652c488cc54c197c0
Jinho Hyeon <neverlish@gmail.com>
2018-06-16 12:39:31 +0900

    12 Fetch All Records from MongoDB with GraphQL Queries

--- node/egghead-graphqlapis-nodejs-mongodb/src/resolvers.js
+++ node/egghead-graphqlapis-nodejs-mongodb/src/resolvers.js
@@ -1,19 +1,10 @@
 import Product from './models/product'
 
-const products = [{
-  _id: '12',
-  name: 'GraphQL course',
-  qty: 1
-}, {
-  _id: '13',
-  name: 'Nodejs course',
-  qty: 1
-}]
 
 export const resolvers = {
   Query: {
-    allProduct() {
-      return products
+    async allProduct() {
+      return await Product.find()
     }
   },
   Mutation: {
