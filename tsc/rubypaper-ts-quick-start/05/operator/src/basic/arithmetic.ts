12e99dbaf91e47b92589136c406ade0d0faa2081
Jinho Hyeon <neverlish@gmail.com>
2018-06-24 15:32:33 +0900

    05 연산자 - 1 기본 연산자

---
+++ tsc/rubypaper-ts-quick-start/05/operator/src/basic/arithmetic.ts
@@ -0,0 +1,10 @@
+// 05-1-1 산술 연산자 - 산술 연산자
+
+// 산술 연산자 --- #1
+
+console.log(10 + 2, 10 - 2, 10 * 2, 10 / 2, 10 % 2); // 12 8 20 5 0
+console.log(10 ** 3); // 1000
+
+// 타입이 다른 피연산자 간의 더하기 연산 --- #2
+console.log(1 + "happy"); // 1happy
+console.log(false + "happy"); // falsehappy
