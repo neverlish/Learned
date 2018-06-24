12ac32c547cc009a1b4554caa5a390148c8da768
Jinho Hyeon <neverlish@gmail.com>
2018-06-24 10:02:23 +0900

    02 - 3 타입스크립트의 개발 도구

---
+++ tsc/rubypaper-ts-quick-start/02/taskrunner/test-tsnode-tslint.ts
@@ -0,0 +1,5 @@
+// 02-3-5 타입스크립트의 확장 기능 - TSLint - 린트 검사를 수행할 파일
+
+// var hello = "hello typescript"; // Forbidden 'var' keyword, use 'let' or 'const' instead
+let hello = "hello typescript";
+console.log(hello);
