de3b81427ec2dd7f5b7ef91c90157ab0070b1a4d
Jinho Hyeon <neverlish@gmail.com>
2018-06-28 21:26:16 +0900

    09 Creating first Mutation

--- node/nomadcoders-graphql-nodejs-movieapi/graphql/db.js
+++ node/nomadcoders-graphql-nodejs-movieapi/graphql/db.js
@@ -27,3 +27,13 @@ export const getById = id => {
   const filteredMovies = movies.filter(movie => id === movie.id)
   return filteredMovies[0]
 }
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name, 
+    score
+  }
+  movies.push(newMovie)
+  return newMovie
+}
--- node/nomadcoders-graphql-nodejs-movieapi/graphql/resolvers.js
+++ node/nomadcoders-graphql-nodejs-movieapi/graphql/resolvers.js
@@ -1,4 +1,4 @@
-import { getMovies, getById } from './db'
+import { getMovies, getById, addMovie } from './db'
 
 const resolvers = {
   Query: {
@@ -6,7 +6,7 @@ const resolvers = {
     movie: (_, { id }) => getById(id)
   },
   Mutation: {
-    addMovie: (_, {name, score}) => 
+    addMovie: (_, {name, score}) => addMovie(name, score)
   }
 }
 
