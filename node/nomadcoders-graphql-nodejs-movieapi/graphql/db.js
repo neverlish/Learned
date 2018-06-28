fb04d95a03a84194289b25648e45e97557c33d46
Jinho Hyeon <neverlish@gmail.com>
2018-06-28 21:20:52 +0900

    08 Defining Mutations

--- node/nomadcoders-graphql-nodejs-movieapi/graphql/db.js
+++ node/nomadcoders-graphql-nodejs-movieapi/graphql/db.js
@@ -1,49 +1,29 @@
-export const people = [
+let movies = [
   {
-    id: "0",
-    name: "Nicolas",
-    age: 18,
-    gender: "female"
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1
   },
   {
-    id: "1",
-    name: "Jisu",
-    age: 18,
-    gender: "female"
+    id: 1,
+    name: "Avengers - The new one",
+    score: 8
   },
   {
-    id: "2",
-    name: "Japan Guy",
-    age: 18,
-    gender: "male"
+    id: 2,
+    name: "The Godfather I",
+    score: 99
   },
   {
-    id: "3",
-    name: "Daal",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: "4",
-    name: "JD",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: "5",
-    name: "moondaddi",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: "6",
-    name: "flynn",
-    age: 18,
-    gender: "male"
+    id: 3,
+    name: "Logan",
+    score: 2
   }
 ]
 
+export const getMovies = () => movies
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => String(id) === person.id)
-  return filteredPeople[0]
+  const filteredMovies = movies.filter(movie => id === movie.id)
+  return filteredMovies[0]
 }
