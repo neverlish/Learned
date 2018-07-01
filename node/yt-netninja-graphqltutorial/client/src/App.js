86f5653e37b5ba63b51ba3544c9aaa2e5c4d8f7b
Jinho Hyeon <neverlish@gmail.com>
2018-07-01 18:14:38 +0900

    25 Apollo Client Setup

--- node/yt-netninja-graphqltutorial/client/src/App.js
+++ node/yt-netninja-graphqltutorial/client/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 import BookList from './components/BookList';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
 
 class App extends Component {
   render() {
     return (
-      <div id='main'>
-        <h1>Ninja's Reading List</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id='main'>
+          <h1>Ninja's Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
