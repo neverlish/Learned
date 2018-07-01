b004b08e8f9d1c6f3f97eb56df17bb5b5036e819
Jinho Hyeon <neverlish@gmail.com>
2018-07-01 15:02:02 +0900

    06 Setting up GraphQL

--- node/yt-netninja-graphqltutorial/server/app.js
+++ node/yt-netninja-graphqltutorial/server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  
+}));
 
 app.listen(4000, () => {
   console.log('now listening for requests on port 4000');
