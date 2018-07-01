da9ee5e1d7deab0f5122394180683a47d665dc28
Jinho Hyeon <neverlish@gmail.com>
2018-07-01 18:22:59 +0900

    26 Making Queries from React

--- node/yt-netninja-graphqltutorial/server/app.js
+++ node/yt-netninja-graphqltutorial/server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(process.env.MLAB);
 mongoose.connection.once('open', () => {
