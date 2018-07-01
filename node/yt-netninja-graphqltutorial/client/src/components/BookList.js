f24ea65680c97b871c0e6ec39ce793c30c76b002
Jinho Hyeon <neverlish@gmail.com>
2018-07-01 20:02:41 +0900

    35 Making a Single Query

--- node/yt-netninja-graphqltutorial/client/src/components/BookList.js
+++ node/yt-netninja-graphqltutorial/client/src/components/BookList.js
@@ -4,6 +4,13 @@ import { getBooksQuery } from '../queries/queries';
 import BookDetails from './BookDetails'
 
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    };
+  }
+
   displayBooks() {
     var data = this.props.data;
     if (data.loading) {
@@ -11,7 +18,7 @@ class BookList extends Component {
     } else {
       return data.books.map(book => {
         return (
-          <li key={book.id}>{ book.name }</li>
+          <li key={book.id} onClick={ (e) => {this.setState({ selected: book.id })} }>{ book.name }</li>
         );
       })
     }
@@ -23,7 +30,7 @@ class BookList extends Component {
         <ul id='book-list'>
           { this.displayBooks() }
         </ul>
-        <BookDetails />
+        <BookDetails bookId={ this.state.selected }/>
       </div>
     );
   }
