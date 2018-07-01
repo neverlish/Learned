ee57ee85882c4a97f25ee7d29503b6b027e31fad
Jinho Hyeon <neverlish@gmail.com>
2018-07-01 19:45:01 +0900

    33 Re-fetching Queries

--- node/yt-netninja-graphqltutorial/client/src/components/AddBook.js
+++ node/yt-netninja-graphqltutorial/client/src/components/AddBook.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { graphql, compose } from 'react-apollo';
-import { getAuthorsQuery, addBookMutation } from '../queries/queries';
+import { getAuthorsQuery, getBooksQuery, addBookMutation } from '../queries/queries';
 
 class AddBook extends Component {
   constructor(props) {
@@ -30,7 +30,8 @@ class AddBook extends Component {
         name: this.state.name,
         genre: this.state.genre,
         authorId: this.state.authorId
-      }
+      },
+      refetchQueries: [{ query: getBooksQuery }]
     });
   }
 
