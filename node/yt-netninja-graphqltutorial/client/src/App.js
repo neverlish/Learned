809483b3d46d5931d54ac7a83f87e15276fcfb32
Jinho Hyeon <neverlish@gmail.com>
2018-07-01 18:58:04 +0900

    28 Add Book Component

--- node/yt-netninja-graphqltutorial/client/src/App.js
+++ node/yt-netninja-graphqltutorial/client/src/App.js
@@ -3,6 +3,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
@@ -15,6 +16,7 @@ class App extends Component {
         <div id='main'>
           <h1>Ninja's Reading List</h1>
           <BookList />
+          <AddBook />
         </div>
       </ApolloProvider>
     );
