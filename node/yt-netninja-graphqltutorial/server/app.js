d3171b1bf68f38fe1a5987fc291aa45b7bf1c64c
Jinho Hyeon <neverlish@gmail.com>
2018-07-01 16:39:17 +0900

    16 Connecting to mLab

--- node/yt-netninja-graphqltutorial/server/app.js
+++ node/yt-netninja-graphqltutorial/server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.MLAB);
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
