c406b056fcce9b5a6e93db7d3ddfca5b2e4ccf40
Jinho Hyeon <neverlish@gmail.com>
2018-07-07 13:00:07 +0900

    1 - 11 Creating a Virtual Environment on NodeJS

--- fullstack/nomadcoders-uber-clone/server/src/ormConfig.ts
+++ fullstack/nomadcoders-uber-clone/server/src/ormConfig.ts
@@ -6,10 +6,10 @@ const defaultConnectionOptions: ConnectionOptions = {
   synchronize: true,
   logging: true,
   entities: ['entities/**/*.*'],
-  host: process.env.DB_ENDPOINT || 'localhost',
+  host: process.env.DB_ENDPOINT,
   port: 5432,
-  username: process.env.DB_USERNAME || 'jinhohyeon',
-  password: process.env.DB_PASSWORD || ''
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
 };
 
 export default defaultConnectionOptions;
