02480f16efe7d3338d449dd5109e8b4a1ca5694c
Jinho Hyeon <neverlish@gmail.com>
2018-07-07 12:18:42 +0900

    1 - 07 API and Schema Structue part Two

--- fullstack/nomadcoders-uber-clone/server/src/schema.ts
+++ fullstack/nomadcoders-uber-clone/server/src/schema.ts
@@ -1,4 +1,22 @@
 import { GraphQLSchema } from 'graphql';
 import { makeExecutableSchema } from 'graphql-tools';
-import { fileLoader } from 'merge-graphql-schemas';
+import { fileLoader, mergeResolvers, mergeTypes } from 'merge-graphql-schemas';
 import path from 'path';
+
+const allTypes: GraphQLSchema[] = fileLoader(
+  path.join(__dirname, "./api/**/*.graphql")
+);
+
+const allResvolers: string[] = fileLoader(
+  path.join(__dirname, "./api/**/*.resolvers.*")
+);
+
+const mergedTypes = mergeTypes(allTypes);
+const mergedResolvers = mergeResolvers(allResvolers);
+
+const schema = makeExecutableSchema({
+  typeDefs: mergedTypes,
+  resolvers: mergedResolvers
+});
+
+export default schema;
