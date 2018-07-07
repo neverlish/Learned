5450ba6101c9d1f95b2b7070852fff55d62cd7b2
Jinho Hyeon <neverlish@gmail.com>
2018-07-08 00:50:46 +0900

    1 - 12 User Entity GraphQL Type

--- fullstack/nomadcoders-uber-clone/server/src/api/Hello/sayBye/sayBye.resolvers.ts
+++
@@ -1,7 +0,0 @@
-const resolvers = {
-  Query: {
-    sayBye: () => "Hey hello how are ya"
-  }
-}
-
-export default resolvers;
--- fullstack/nomadcoders-uber-clone/server/src/types/graph.d.ts
+++ fullstack/nomadcoders-uber-clone/server/src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello(name: String!): SayHelloResponse!\n}\n\ntype SayHelloResponse {\n  text: String!\n  error: Boolean!\n}\n"];
+export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello(name: String!): SayHelloResponse!\n}\n\ntype SayHelloResponse {\n  text: String!\n  error: Boolean!\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  facebookID: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  createdAt: String!\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -13,4 +13,27 @@ export interface SayHelloQueryArgs {
 export interface SayHelloResponse {
   text: string;
   error: boolean;
+}
+
+export interface User {
+  id: number;
+  email: string | null;
+  verifiedEmail: boolean;
+  firstName: string;
+  lastName: string;
+  facebookID: string;
+  age: number | null;
+  password: string | null;
+  phoneNumber: string | null;
+  verifiedPhoneNumber: boolean;
+  profilePhoto: string | null;
+  createdAt: string;
+  updatedAt: string | null;
+  fullName: string | null;
+  isDriving: boolean;
+  isRiding: boolean;
+  isTaken: boolean;
+  lastLng: number | null;
+  lastLat: number | null;
+  lastOrientation: number | null;
 }
