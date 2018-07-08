51de266789ab580975bc2fcf5f52d70524c2833d
Jinho Hyeon <neverlish@gmail.com>
2018-07-08 16:07:09 +0900

    1 - 19 Creating the Verification Key

--- fullstack/nomadcoders-uber-clone/server/src/entities/Verification.ts
+++ fullstack/nomadcoders-uber-clone/server/src/entities/Verification.ts
@@ -1,11 +1,14 @@
-import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { BaseEntity, BeforeInsert, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 import { verificationTarget } from '../types/types';
+
+const PHONE = "PHONE";
+const EMAIL = "EMAIL";
 
 @Entity()
 class Verification extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
 
-  @Column({ type: 'text', enum: ["PHONE", "EMAIL"] })
+  @Column({ type: 'text', enum: [PHONE, EMAIL] })
   target: verificationTarget;
   
   @Column({ type: 'text' })
@@ -20,6 +23,17 @@ class Verification extends BaseEntity {
   @CreateDateColumn() createdAt: string;
 
   @UpdateDateColumn() updatedAt: string;
+
+  @BeforeInsert()
+  createKey(): void {
+    if (this.target === PHONE) {
+      this.key = Math.floor(Math.random() * 100000).toString()
+    } else if (this.target === EMAIL) {
+      this.key = Math.random()
+        .toString(36)
+        .substr(2)
+    }
+  }
 }
 
 export default Verification;
