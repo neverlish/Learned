44421f9e6c61a1b33a407bfddb79f9ae84198237
Jinho Hyeon <neverlish@gmail.com>
2018-07-10 18:04:14 +0900

    5 Getting data from the GraphQL API part Two

--- react/nomadcoders-apollo-graphql-webapp/src/Home.js
+++ react/nomadcoders-apollo-graphql-webapp/src/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import Query from 'react-apollo';
+import {Query} from 'react-apollo';
 import {HOME_PAGE} from './queries';
 
-const Home = () => <Query query={HOME_PAGE}>Home</Query>
+const Home = () =>(
+  <Query query={HOME_PAGE}>
+    {({ loading, data, error }) => {
+      if (loading) return 'loading';
+      if (error) return 'something happened';
+      return data.movies.map(movie => (
+        <h2 key={movie.id}>
+          {movie.title} / {movie.rating}
+        </h2>
+      ));
+    }}
+  </Query>
+);
 
 export default Home
--- react/nomadcoders-apollo-graphql-webapp/src/queries.js
+++ react/nomadcoders-apollo-graphql-webapp/src/queries.js
@@ -1,11 +1,12 @@
 import gql from 'graphql-tag';
 
 export const HOME_PAGE = gql`
-  query {
+  {
     movies(limit: 50, rating: 7) {
       id
       title
       genres
+      rating
     }
   }
 `;
