27a3b8e3f4104670121db8832c5b13ac5206861c
Jinho Hyeon <neverlish@gmail.com>
2018-07-10 17:04:26 +0900

    2 Setting up an Apollo Client

--- react/nomadcoders-apollo-graphql-webapp/src/App.js
+++ react/nomadcoders-apollo-graphql-webapp/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import client from './apolloClient';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-      </div>
-    );
+    return <ApolloProvider client={client}>
+      <div className="App"/>
+    </ApolloProvider>
   }
 }
 
