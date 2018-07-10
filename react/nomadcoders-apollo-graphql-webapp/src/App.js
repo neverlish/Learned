5a89d3d3fdca4cf0fc816cc2d4f4dd896d07a2b4
Jinho Hyeon <neverlish@gmail.com>
2018-07-10 17:11:36 +0900

    3 Setting up React Router

--- react/nomadcoders-apollo-graphql-webapp/src/App.js
+++ react/nomadcoders-apollo-graphql-webapp/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import client from './apolloClient';
+import Home from './Home';
+import Detail from './Detail';
 
 class App extends Component {
   render() {
     return <ApolloProvider client={client}>
-      <div className="App"/>
+      <Router>
+        <React.Fragment>
+          <Route exact={true} path={'/'} component={Home} />
+          <Route path={'/details/:movieId'} component={Detail} />
+        </React.Fragment>
+      </Router>
     </ApolloProvider>
   }
 }
---
+++ react/nomadcoders-apollo-graphql-webapp/src/Detail.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Detail = () => 'Detail'
+
+export default Detail
