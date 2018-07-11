3ea89cb248d6fb657236c4a9733457d3b4eec980
Jinho Hyeon <neverlish@gmail.com>
2018-07-11 20:10:45 +0900

    7 Creating a Query with variables

--- react/nomadcoders-apollo-graphql-webapp/src/Detail.js
+++ react/nomadcoders-apollo-graphql-webapp/src/Detail.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import {withRouter} from 'react-router-dom';
 import {Query} from 'react-apollo';
+import {MOVIE_DETAILS} from './queries';
 
 const Detail = ({
   match: {
     params: { movieId }
   }
-}) => {
-  console.log(movieId);
-  return <div>Detail</div>;
-}
+}) => <Query query={MOVIE_DETAILS} variables={{ movieId }}>{({ loading, error, data }) => {
+  if (loading) return 'loading';
+  if (error) return 'error';
+  return (
+    <React.Fragment>
+      {data.movie.title}
+      {data.movie.medium_cover_image}
+      {data.movie.rating}
+      {data.movie.description_intro}
+    </React.Fragment>
+  )
+}}</Query>
 
-export default withRouter(Detail)
+
+export default Detail
--- react/nomadcoders-apollo-graphql-webapp/src/queries.js
+++ react/nomadcoders-apollo-graphql-webapp/src/queries.js
@@ -11,3 +11,21 @@ export const HOME_PAGE = gql`
     }
   }
 `;
+
+export const MOVIE_DETAILS = gql`
+  query getMovieDetails($movieId: Int!) {
+    movie(id: $movieId) {
+      medium_cover_image
+      title
+      rating
+      description_intro
+      language
+      genres
+    }
+    suggestions(id: $movieId) {
+      medium_cover_image
+      title
+      rating
+    }
+  }
+`;
