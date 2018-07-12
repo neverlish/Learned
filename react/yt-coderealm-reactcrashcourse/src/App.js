863d22907a9ea3796863a6311cf874726617f7b1
Jinho Hyeon <neverlish@gmail.com>
2018-07-12 20:53:29 +0900

    03 Create-React-App

---
+++ react/yt-coderealm-reactcrashcourse/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Welcome to the React Crash Course!</h1>
+    )
+  }
+}
+
+export default App
