e949024e45a0759511e06b78784c115244e7d719
Jinho Hyeon <neverlish@gmail.com>
2018-07-12 21:17:18 +0900

    05 State Management

--- react/yt-coderealm-reactcrashcourse/src/components/Countdown.js
+++ react/yt-coderealm-reactcrashcourse/src/components/Countdown.js
@@ -1,17 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
+import moment from 'moment'
 
-const Countdown = (props) =>
-<section className='hero is-success is-fullheight'>
-  <div className='hero-body'>
-    <div className='container'>
-      <h1 className='title'>
-        Full Height title
-      </h1>
-      <h2 className='subtitle'>
-        Full Height subtitle
-      </h2>
-    </div>
-  </div>
-</section>
+export default class Countdown extends Component {
+  constructor(props) {
+    super(props)
 
-export default Countdown
+    this.state = {
+      duration: this.getRemainingTime()
+    }
+  }
+
+  getRemainingTime() {
+    let now = moment(),
+        newYear = moment({year: now.year() + 1}),
+        diff = newYear.diff(now)
+
+    return moment.duration(diff)
+  }
+
+  render() {
+    const { duration } = this.state
+
+    return <section className='hero is-info is-bold is-fullheight has-text-centered'>
+      <div className='hero-body'>
+        <div className='container'>
+          <h1 className='title'>
+            New Year is Coming Up!
+          </h1>
+          <div className='section'>
+            <nav className='level'>
+              <div className='level-item has-text-centered'>
+                <div>
+                  <p className='heading'>Days</p>
+                  <p className='title'>{Math.floor(duration.asDays())}</p>
+                </div>
+              </div>
+              <div className='level-item has-text-centered'>
+                <div>
+                  <p className='heading'>Hours</p>
+                  <p className='title'>{duration.hours().toString().padStart(2, 0)}</p>
+                </div>
+              </div>
+              <div className='level-item has-text-centered'>
+                <div>
+                  <p className='heading'>Minutes</p>
+                  <p className='title'>{duration.minutes().toString().padStart(2, 0)}</p>
+                </div>
+              </div>
+              <div className='level-item has-text-centered'>
+                <div>
+                  <p className='heading'>Seconds</p>
+                  <p className='title'>{duration.seconds().toString().padStart(2, 0)}</p>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </section>
+  }
+}
