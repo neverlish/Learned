cc790e6d97b4243803b5421123dffcb9483f7e13
Jinho Hyeon <neverlish@gmail.com>
2018-07-12 22:28:01 +0900

    11 Listening to Events

--- react/yt-coderealm-reactcrashcourse/src/components/Countdown/Datepicker.js
+++ react/yt-coderealm-reactcrashcourse/src/components/Countdown/Datepicker.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Datepicker = (props) =>
-  <form>
-    <div className='field is-grouped is-grouped-centered' style={{marginBottom: 40}}>
-      <p className='control'>
-        <input className='input is-medium is-rounded' type='text' placeholder='Type a date...' />
-      </p>
-      <p className='control'>
-        <button className='button is-light is-outlined is-medium is-rounded'>
-          Reset
-        </button>
-      </p>
-    </div>
-  </form>
+class Datepicker extends Component {
+  state = {
+    date: '2018-01-21'
+  }
+
+  handleDateChange = (e) => {
+    this.setState({
+      date: e.target.value
+    })
+  }
+
+  handleDateSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state.date)
+  }
+
+  render() {
+    const { date } = this.state
+
+    return <form onSubmit={this.handleDateSubmit}>
+      <div className='field is-grouped is-grouped-centered' style={{marginBottom: 40}}>
+        <p className='control'>
+          <input className='input is-medium is-rounded' type='text' value={date} onChange={this.handleDateChange} placeholder='Type a date...' />
+        </p>
+        <p className='control'>
+          <button type='submit' className='button is-light is-outlined is-medium is-rounded'>
+            Reset
+          </button>
+        </p>
+      </div>
+    </form>
+  }
+}
+  
 
 export default Datepicker
