a9c76e16b3b31c07957161e434f15d32fbfa12a1
Jinho Hyeon <neverlish@gmail.com>
2018-07-12 21:22:03 +0900

    06 Component Lifecycle

--- react/yt-coderealm-reactcrashcourse/src/components/Countdown.js
+++ react/yt-coderealm-reactcrashcourse/src/components/Countdown.js
@@ -10,6 +10,18 @@ export default class Countdown extends Component {
     }
   }
 
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        duration: this.getRemainingTime()
+      })
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
   getRemainingTime() {
     let now = moment(),
         newYear = moment({year: now.year() + 1}),
