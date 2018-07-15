849187c9fd767101b77e0fec86d365cea144f288
Jinho Hyeon <neverlish@gmail.com>
2018-07-16 00:48:22 +0900

    06 Forms, Inputs & Styling

--- react/yt-coderealm-react-materialui/src/Components/Exercises/Dialogs/Create.js
+++ react/yt-coderealm-react-materialui/src/Components/Exercises/Dialogs/Create.js
@@ -1,16 +1,31 @@
 import React, { Component, Fragment } from 'react'
-import { Dialog, Button } from 'material-ui'
+import { Dialog, Button, TextField, Select } from 'material-ui'
 import {
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle
 } from 'material-ui/Dialog'
+import { FormControl } from 'material-ui/Form'
+import { InputLabel } from 'material-ui/Input'
+import { MenuItem } from 'material-ui/Menu'
+import { withStyles } from 'material-ui/styles'
 import { Add } from 'material-ui-icons'
 
-export default class extends Component {
+const styles = theme => ({
+  FormControl: {
+    width: 500
+  }
+})
+
+export default withStyles(styles)(class extends Component {
   state = {
-    open: false
+    open: false,
+    exercise: {
+      title: '',
+      description: '',
+      muscles: ''
+    }
   }
 
   handleToggle = () => {
@@ -19,8 +34,37 @@ export default class extends Component {
     })
   }
 
+  handleChange = name => ({ target: { value }}) => {
+    this.setState({
+      exercise: {
+        ...this.state.exercise,
+        [name]: value
+      }
+    })
+  }
+
+  handleSubmit = () => {
+    const { exercise } = this.state
+
+    this.props.onCreate({
+      ...exercise,
+      id: exercise.title.toLocaleLowerCase().replace(/ /g, '-')
+    })
+
+    this.setState({
+      open: false,
+      exercise: {
+        title: '',
+        description: '',
+        muscles: ''
+      }
+    })
+  }
+
   render() {
-    const { open } = this.state
+    const { open, exercise: { title, description, muscles } } = this.state,
+          { classes, muscles: categories } = this.props
+
     return <Fragment>
       <Button variant='fab' onClick={this.handleToggle} mini>
         <Add />
@@ -39,10 +83,47 @@ export default class extends Component {
           </DialogContentText>
 
           <form>
+            <TextField
+              label='Title'
+              value={title}
+              onChange={this.handleChange('title')}
+              margin='normal'
+              className={classes.FormControl}
+            />
+            <br />
+            <FormControl className={classes.FormControl}>
+              <InputLabel htmlFor='muscles'>
+                Muscles
+              </InputLabel>
+              <Select
+                value={muscles}
+                onChange={this.handleChange('muscles')}
+              >
+                {categories.map(category => 
+                  <MenuItem value={category} key={category}>
+                    {category}
+                  </MenuItem>
+                )}>
+              </Select>
+            </FormControl>
+            <br />
+            <TextField
+              multiline
+              rows='4'
+              label='Description'
+              value={description}
+              onChange={this.handleChange('description')}
+              margin='normal'
+              className={classes.FormControl}
+            />
           </form>
 
           <DialogActions>
-            <Button color='primary' variant='raised'>
+            <Button
+              color='primary'
+              variant='raised'
+              onClick={this.handleSubmit}
+            >
               Create
             </Button>
           </DialogActions>
@@ -50,5 +131,4 @@ export default class extends Component {
       </Dialog>
     </Fragment>
   }
-}
-  
+})
--- react/yt-coderealm-react-materialui/src/Components/Layouts/Header.js
+++ react/yt-coderealm-react-materialui/src/Components/Layouts/Header.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { AppBar, Toolbar, Typography } from 'material-ui'
 import CreateDialog from '../Exercises/Dialogs/Create'
 
-export default props => 
+export default ({ muscles, onExerciseCreate }) => 
   <AppBar position='static'>
     <Toolbar>
       <Typography variant='headline' color='inherit' style={{ flex: 1 }}>
         Exercise Database
       </Typography>
 
-      <CreateDialog />
+      <CreateDialog
+        muscles={muscles}
+        onCreate={onExerciseCreate}
+      />
     </Toolbar>
   </AppBar>
 
