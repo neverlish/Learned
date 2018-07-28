53e0dca1c00435cf4b9be482ae552c4cb8d585f7
Jinho Hyeon <neverlish@gmail.com>
2018-07-29 00:04:58 +0900

    1 - 50 Sending Confirmation Email part Two

--- fullstack/nomadcoders-uber-clone/server/src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
+++ fullstack/nomadcoders-uber-clone/server/src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -2,6 +2,7 @@ import { Resolvers } from '../../../types/resolvers';
 import { EmailSignUpMutationArgs, EmailSignUpResponse } from '../../../types/graph';
 import User from '../../../entities/User';
 import createJWT from '../../../utils/createJWT';
+import Verification from '../../../entities/Verification';
 
 const resolvers: Resolvers = {
   Mutation: {
@@ -20,6 +21,12 @@ const resolvers: Resolvers = {
           };
         } else {
           const newUser = await User.create({ ...args }).save();
+          if (newUser.email) {
+            const emailVerification = await Verification.create({
+              payload: newUser.email,
+              target: 'EMAIL'
+            });
+          }
           const token = createJWT(newUser.id);
           return {
             ok: true,
--- fullstack/nomadcoders-uber-clone/server/src/utils/sendEmail.ts
+++ fullstack/nomadcoders-uber-clone/server/src/utils/sendEmail.ts
@@ -1,6 +1,22 @@
-import MailGun from 'mailgun-js';
+import Mailgun from 'mailgun-js';
 
-const mailGunClient = new MailGun({
+const mailGunClient = new Mailgun({
   apiKey: process.env.MAILGUN_API_KEY || '',
   domain: 'sandbox13164822276b4686948a2564312ec05f.mailgun.org'
 });
+
+const sendEmail = (subject: string, html: string) => {
+  const emailData = {
+    from: 'neverlish@gmail.com',
+    to: 'neverlish@gmail.com',
+    subject,
+    html
+  };
+  return mailGunClient.messages().send(emailData)
+};
+
+export const sendVerificationEmail = (fullName: string, key: string) => {
+  const emailSubject = `Hello! ${fullName}, please verify your email`;
+  const emailBody = `Verify your email by clicking <a href="http://nuber.com/verification/${key}/">here</a>`
+  return sendEmail(emailSubject, emailBody);
+}
