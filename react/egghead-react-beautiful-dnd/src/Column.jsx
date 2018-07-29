ea7d16d1b35c18a3c7bc85b2d8e930b4c6e9244c
Jinho Hyeon <neverlish@gmail.com>
2018-07-29 09:28:17 +0900

    07 Customise the Appearance of an App during a Drag using react-beautiful-dnd snapshot Values

--- react/egghead-react-beautiful-dnd/src/Column.jsx
+++ react/egghead-react-beautiful-dnd/src/Column.jsx
@@ -13,6 +13,8 @@ const Title = styled.h3`
 `;
 const TaskList = styled.div`
   padding: 8px;
+  transition: background-color 0.2s ease;
+  background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
 `;
 
 export default class Column extends React.Component {
@@ -21,10 +23,11 @@ export default class Column extends React.Component {
       <Container>
         <Title>{this.props.column.title}</Title>
         <Droppable droppableId={this.props.column.id}>
-          {provided => (
+          {(provided, snapshot) => (
             <TaskList 
               innerRef={provided.innerRef}
               {...provided.droppableProps}
+              isDraggingOver={snapshot.isDraggingOver}
             >
               {this.props.tasks.map((task, index) => (
                 <Task key={task.id} task={task} index={index}/>
