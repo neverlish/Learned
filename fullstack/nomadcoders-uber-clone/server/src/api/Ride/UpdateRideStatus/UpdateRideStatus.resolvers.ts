8db18c4f1c26321a1b332dee2fe979cc7fa2ce6f
Jinho Hyeon <neverlish@gmail.com>
2018-07-30 01:12:10 +0900

    1 - 77 UpdateRideStatus Resolver part Two

--- fullstack/nomadcoders-uber-clone/server/src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
+++ fullstack/nomadcoders-uber-clone/server/src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -15,25 +15,51 @@ const resolvers: Resolvers = {
         const user: User = req.user;
         if (user.isDriving) {
           try {
-            const ride = await Ride.findOne({
-              id: args.rideId, 
-              status: 'REQUESTING'
-            });
+            let ride: Ride | undefined;
+            if (args.status === 'ACCEPTED') {
+              ride = await Ride.findOne({
+                id: args.rideId, 
+                status: 'REQUESTING'
+              });
+              if (ride) {
+                ride.driver = user;
+                user.isTaken = true;
+                user.save();
+              }
+            } else {
+              ride = await Ride.findOne({
+                id: args.rideId,
+                driver: user
+              });
+            }
             if (ride) {
               ride.status = args.status;
               ride.save();
+              return {
+                ok: true,
+                error: null,
+                rideId: ride.id
+              };
             } else {
               return {
                 ok: false,
-                error: 'Cant update ride'
+                error: 'Cant update ride',
+                rideId: null
               };
             }
           } catch (error) {
             return {
               ok: false,
-              error: error.message
+              error: error.message,
+              rideId: null
             };
           }
+        } else {
+          return {
+            ok: false,
+            error: 'You are not driving',
+            rideId: null
+          };
         }
       }
     )
