360ef016e5c1d085dcd0518643000ab626b41484
Jinho Hyeon <neverlish@gmail.com>
2018-07-30 01:06:19 +0900

    1 - 76 UpdateRideStatus Resolver part One

---
+++ fullstack/nomadcoders-uber-clone/server/src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -0,0 +1,43 @@
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import { UpdateRideStatusMutationArgs, UpdateRideStatusResponse } from "../../../types/graph";
+import User from "../../../entities/User";
+import Ride from "../../../entities/Ride";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    UpdateRideStatus: privateResolver(
+      async(
+        _,
+        args: UpdateRideStatusMutationArgs,
+        { req }
+      ): Promise<UpdateRideStatusResponse> => {
+        const user: User = req.user;
+        if (user.isDriving) {
+          try {
+            const ride = await Ride.findOne({
+              id: args.rideId, 
+              status: 'REQUESTING'
+            });
+            if (ride) {
+              ride.status = args.status;
+              ride.save();
+            } else {
+              return {
+                ok: false,
+                error: 'Cant update ride'
+              };
+            }
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message
+            };
+          }
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
