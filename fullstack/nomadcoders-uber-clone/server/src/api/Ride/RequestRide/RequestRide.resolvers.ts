8e0f05dc8371deee4eabe7decdf25b1c58e79585
Jinho Hyeon <neverlish@gmail.com>
2018-07-29 22:19:19 +0900

    1 - 72 RequestRide Resolver

---
+++ fullstack/nomadcoders-uber-clone/server/src/api/Ride/RequestRide/RequestRide.resolvers.ts
@@ -0,0 +1,35 @@
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import { RequestRideMutationArgs, RequestRideResponse } from "../../../types/graph";
+import User from "../../../entities/User";
+import Ride from "../../../entities/Ride";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    RequestRide: privateResolver(
+      async (
+        _,
+        args: RequestRideMutationArgs,
+        { req }
+      ): Promise<RequestRideResponse> => {
+        const user: User = req.user;
+        try {
+          const ride = await Ride.create({ ...args, passenger: user }).save();
+          return {
+            ok: true,
+            error: null,
+            ride
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            ride: null
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
--- fullstack/nomadcoders-uber-clone/server/src/entities/Ride.ts
+++ fullstack/nomadcoders-uber-clone/server/src/entities/Ride.ts
@@ -6,7 +6,11 @@ import User from './User';
 class Ride extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
 
-  @Column({ type: "text", enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTING", "ONROUTE"] })
+  @Column({
+    type: "text", 
+    enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTING", "ONROUTE"],
+    default: 'ACCEPTED'
+  })
   status: rideStatus;
   
   @Column({ type: "text" })
@@ -39,7 +43,7 @@ class Ride extends BaseEntity {
   @ManyToOne(type => User, user => user.ridesAsPassenger)
   passenger: User;
 
-  @ManyToOne(type => User, user => user.ridesAsDriver)
+  @ManyToOne(type => User, user => user.ridesAsDriver, { nullable: true })
   driver: User;
 
   @CreateDateColumn() createdAt: string;
