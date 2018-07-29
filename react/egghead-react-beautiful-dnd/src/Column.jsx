a58859e9e585c99ec02569870aebabe520f4b302
Jinho Hyeon <neverlish@gmail.com>
2018-07-29 12:03:27 +0900

    13 Optimize Performance in react-beautiful-dnd with shouldComponentUpdate and PureComponent

--- react/egghead-react-beautiful-dnd/src/Column.jsx
+++ react/egghead-react-beautiful-dnd/src/Column.jsx
@@ -23,6 +23,20 @@ const TaskList = styled.div`
   min-height: 100px;
 `;
 
+class InnerList extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.tasks === this.props.tasks) {
+      return false;
+    }
+    return true;
+  }
+  render() {
+    return this.props.tasks.map((task, index) => (
+      <Task key={task.id} task={task} index={index} />
+    ))
+  }
+}
+
 export default class Column extends React.Component {
   render() {
     return (
@@ -40,9 +54,7 @@ export default class Column extends React.Component {
                   {...provided.droppableProps}
                   isDraggingOver={snapshot.isDraggingOver}
                 >
-                  {this.props.tasks.map((task, index) => (
-                    <Task key={task.id} task={task} index={index}/>
-                  ))}
+                  <InnerList tasks={this.props.tasks} />
                   {provided.placeholder}
                 </TaskList>
               )}
--- react/egghead-react-beautiful-dnd/src/index.js
+++ react/egghead-react-beautiful-dnd/src/index.js
@@ -10,6 +10,14 @@ const Container = styled.div`
   display: flex;
 `;
 
+class InnerList extends React.PureComponent {
+  render() {
+    const { column, taskMap, index } = this.props;
+    const tasks = column.taskIds.map(taskId => taskMap[taskId]);
+    return <Column column={column} tasks={tasks} index={index} />;
+  }
+}
+
 class App extends React.Component {
   state = initialData;
 
@@ -88,8 +96,7 @@ class App extends React.Component {
       },
     };
     this.setState(newState);
-
-  };  
+  };
 
   render() {
     return (
@@ -102,9 +109,12 @@ class App extends React.Component {
             >
               {this.state.columnOrder.map((columnId, index) => {
                 const column = this.state.columns[columnId];
-                const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
-                return <Column key={column.id} column={column} tasks={tasks} index={index} />;
+                return <InnerList
+                  key={column.id}
+                  column={column}
+                  taskMap={this.state.tasks}
+                  index={index} />;
               })}
               {provided.placeholder}
             </Container>
