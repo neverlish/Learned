80081f5f1b8d7d4c6b8530fb6c6c3b216b1406d1
Jinho Hyeon <neverlish@gmail.com>
2018-08-04 10:10:06 +0900

    3 - 4 - AddItemComponent

---
+++ vue/acorn-start-vuejs2/shopping-list/src/components/AddItemComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class='input-group'>
+      <input type='text' class='input form-control' placeholder='add shopping lilst item'>
+      <span class='input-group-btn'>
+        <button class='btn btn-default' type='button'>Add!</button>
+      </span>
+    </div>
+  </div>
+</template>
