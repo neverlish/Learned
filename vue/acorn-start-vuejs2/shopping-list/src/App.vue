6ed43d450d31d751e3594e987b16b82e25da232f
Jinho Hyeon <neverlish@gmail.com>
2018-08-04 10:08:06 +0900

    3 - 4 단일 파일 컴포넌트를 사용해 쇼핑 목록 애플리케이션 재작성

--- vue/acorn-start-vuejs2/shopping-list/src/App.vue
+++ vue/acorn-start-vuejs2/shopping-list/src/App.vue
@@ -1,11 +1,40 @@
 <template>
-  <div id="app">
-
+  <div id="app" class='container'>
+    <h2>{{ title }}</h2>
+    <add-item-component></add-item-component>
+    <items-component></items-component>
+    <div class='footer'>
+      <hr />
+      <change-title-component></change-title-component>
+    </div>
   </div>
 </template>
 
 <script>
+import AddItemComponent from './components/AddItemComponent'
+import ItemsComponent from './components/ItemsComponent'
+import ChangeTitleComponent from './components/ChangeTitleComponent'
 
+export default {
+  components: {
+    AddItemComponent,
+    ItemsComponent,
+    ChangeTitleComponent
+  },
+  data () {
+    return {
+      items: [ { text: 'Bananas', checked: true }, { text: 'Apples', checked: false } ]
+    }
+  },
+  methods: {
+    addItem (text) {
+      this.items.push({
+        text: text,
+        checked: false
+      })
+    }
+  }
+}
 </script>
 
 <style>
