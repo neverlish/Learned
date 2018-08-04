04038e847962c95703349fa25246665f9a1e1578
Jinho Hyeon <neverlish@gmail.com>
2018-08-04 10:28:38 +0900

    3 - 5 단일 파일 컴포넌트로 포모도로 애플리케이션 재작성

--- vue/acorn-start-vuejs2/pomodoro/src/App.vue
+++ vue/acorn-start-vuejs2/pomodoro/src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <div id="app">
-
+  <div id="app" class='container'>
+    <h2>
+      <span>Pomodoro</span>
+      <controls-component></controls-component>
+    </h2>
+    <state-title-component></state-title-component>
+    <countdown-component></countdown-component>
+    <kittens-component></kittens-component>
   </div>
 </template>
 
 <script>
+import ControlsComponent from './components/ControlsComponent'
+import StateTitleComponent from './components/StateTitleComponent'
+import CountdownComponent from './components/CountdownComponent'
+import KittensComponent from './components/KittensComponent'
+
+export default {
+  components: {
+    ControlsComponent,
+    StateTitleComponent,
+    CountdownComponent,
+    KittensComponent
+  }
+}
 
 </script>
 
