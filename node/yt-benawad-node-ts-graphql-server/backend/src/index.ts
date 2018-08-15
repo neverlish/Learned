e4abd84e0217484222671e05536a453860d98aac
Jinho Hyeon <neverlish@gmail.com>
2018-08-15 16:16:58 +0900

    03 CRUD in Typeorm

--- node/yt-benawad-node-ts-graphql-server/backend/src/index.ts
+++ node/yt-benawad-node-ts-graphql-server/backend/src/index.ts
@@ -1,17 +1,71 @@
 import 'reflect-metadata';
 import { GraphQLServer } from 'graphql-yoga'
-import { createConnection } from 'typeorm'
+import { createConnection, getConnection } from 'typeorm'
+import { ResolverMap } from './types/ResolverType'
+import { User } from './entity/User';
 
 const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
+  type User {
+		id: Int!
+		firstName: String!
+		lastName: String!
+		age: Int!
+		email: String!
+	}
+	
+	type Query {
+		hello(name: String): String!
+		user(id: Int!): User!
+		users: [User!]!
+	}
+
+	type Mutation {
+		createUser(firstName: String!, lastName: String!, age: Int!, email: String!): User!
+		updateUser(id: Int!, firstName: String, lastName: String, age: Int, email: String): Boolean
+		deleteUser(id: Int!): Boolean
+	}
 `
 
-const resolvers = {
+const resolvers: ResolverMap = {
   Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
-  },
+		hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
+		user: (_, { id }) => User.findOneById(id),
+		users: () => User.find(),
+	},
+	Mutation: {
+		createUser: (_, args) => User.create(args).save(),
+		updateUser: async (_, { id, ...args }) => {
+			try {
+				await User.updateById(id, args)
+			} catch (err) {
+				console.log(err)
+				return false
+			}
+
+			return true
+		},
+		deleteUser: async (_, { id }) => {
+			try {
+				await User.removeById(id)
+				// const deleteQuery =  getConnection()
+				// 	.createQueryBuilder()
+				// 	.delete()
+				// 	.from(User)
+				// 	.where("id = :id", { id })
+				
+				// 	if (id === 3) {
+				// 		deleteQuery.andWhere('email = :email', { email: 'neverlish@gmail.com' })
+				// 	}
+
+				// 	await deleteQuery.execute()
+			} catch (err) {
+				console.log(err)
+				return false
+			}
+
+			return true
+		}
+	},
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers })
