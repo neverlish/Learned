86c1348c071467b4c00a5a20c90269d28ba7ef05
Jinho Hyeon <neverlish@gmail.com>
2018-08-18 19:33:38 +0900

    03 - 2 다른 AWS 자원에 접근하기

--- serverless/acorn-serverless-webapp/03/hello-serverless/handler.js
+++ serverless/acorn-serverless-webapp/03/hello-serverless/handler.js
@@ -28,3 +28,31 @@ module.exports.catNames = (event, context, callback) => {
 
   callback(null, response);
 };
+
+module.exports.testPermissions = (event, context, callback) => {
+  const AWS = require('aws-sdk');
+  const s3 = new AWS.S3();
+  const bucket = 'jinho01';
+  const key = 'log2.txt';
+  const write = {
+    Bucket: bucket,
+    Key: key,
+    Body: 'Test'
+  };
+
+  s3.putObject(write, (err, data) => {
+    if (err) return callback(err);
+
+    const read = { Bucket: bucket, Key: key };
+    s3.getObject(read, (err, data) => {
+      if (err) return callback(err);
+
+      const response = {
+        statusCode: 200,
+        body: data.Body.toString()
+      };
+
+      callback(null, response);
+    })
+  })
+}
