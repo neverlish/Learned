e6d1423021f4b424cdfa10158bbac2185bea66eb
Jinho Hyeon <neverlish@gmail.com>
2018-08-18 20:22:55 +0900

    03 - 2 - 이벤트

--- serverless/acorn-serverless-webapp/03/hello-serverless/handler.js
+++ serverless/acorn-serverless-webapp/03/hello-serverless/handler.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const sns = new AWS.SNS();
+
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
@@ -30,8 +34,6 @@ module.exports.catNames = (event, context, callback) => {
 };
 
 module.exports.testPermissions = (event, context, callback) => {
-  const AWS = require('aws-sdk');
-  const s3 = new AWS.S3();
   const bucket = 'jinho01';
   const key = 'log2.txt';
   const write = {
@@ -56,3 +58,40 @@ module.exports.testPermissions = (event, context, callback) => {
     })
   })
 }
+
+module.exports.processLog = (event, context, callback) => {
+  const bucketName = event.Records[0].s3.bucket.name;
+  const objectKey = event.Records[0].s3.object.key;
+  const s3Params = { 
+    Bucket: bucketName, 
+    Key: objectKey 
+  };
+
+  s3.getObject(s3Params, (err, data) => {
+    if (err) throw err;
+
+    // check if file have errors to report
+    const fileContent = data.Body.toString();   
+    if (fileContent.indexOf('error') !== -1) {         
+      const msg = `file ${objectKey} has errors`;
+      const snsParams = { 
+        Message: msg, 
+        TopicArn: 'arn:aws:sns:ap-northeast-2:367416660003:email-alerts'
+      };
+
+      sns.publish(snsParams, callback);
+    }
+  });
+};
+
+module.exports.processTask = (event, context, callback) => {
+
+  const time = new Date().toUTCString();
+  const msg = `Lambda triggered on ${time}`;
+  const snsParams = { 
+    Message: msg, 
+    TopicArn: 'arn:aws:sns:ap-northeast-2:367416660003:email-alerts' // use you account number and SNS topic
+  };
+
+  sns.publish(snsParams, callback);
+};
