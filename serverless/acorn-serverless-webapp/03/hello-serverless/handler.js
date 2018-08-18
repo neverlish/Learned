b2f48f432fbc619e77fb8af7d9bac6a6950ed2a4
Jinho Hyeon <neverlish@gmail.com>
2018-08-18 19:26:14 +0900

    03 - 2 서버리스 프레임워크 더 알아보기 - npm 패키지 사용

--- serverless/acorn-serverless-webapp/03/hello-serverless/handler.js
+++ serverless/acorn-serverless-webapp/03/hello-serverless/handler.js
@@ -15,3 +15,16 @@ module.exports.hello = async (event, context) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.catNames = (event, context, callback) => {
+  const catNames = require('cat-names');
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: catNames.random()
+    })
+  };
+
+  callback(null, response);
+};
