842daf1bda4293d6e25934db8e08341d45253d10
Jinho Hyeon <neverlish@gmail.com>
2018-08-18 19:14:01 +0900

    03 - 1 - CORS

--- serverless/acorn-serverless-webapp/03/hello-serverless/handler.js
+++ serverless/acorn-serverless-webapp/03/hello-serverless/handler.js
@@ -3,6 +3,9 @@
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://www.example.com'
+    },
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
