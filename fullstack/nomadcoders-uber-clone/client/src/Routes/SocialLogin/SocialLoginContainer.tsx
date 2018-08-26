781cf3b091c18251fbce6434a4476047bac11933
Jinho Hyeon <neverlish@gmail.com>
2018-08-26 13:47:10 +0900

    2 - 29 SocialLogin with Apollo part Three

--- fullstack/nomadcoders-uber-clone/client/src/Routes/SocialLogin/SocialLoginContainer.tsx
+++ fullstack/nomadcoders-uber-clone/client/src/Routes/SocialLogin/SocialLoginContainer.tsx
@@ -1,7 +1,8 @@
 
 import React from "react";
-import { Mutation } from "react-apollo";
+import { Mutation, MutationFn } from "react-apollo";
 import { RouteComponentProps } from "react-router-dom";
+import { toast } from "react-toastify";
 import { facebookConnect, facebookConnectVariables } from "../../types/api";
 import SocialLoginPresenter from "./SocialLoginPresenter";
 import { FACEBOOK_CONNECT } from "./SocialLoginQueries";
@@ -21,14 +22,42 @@ interface IState {
 interface IProps extends RouteComponentProps<any> {}
 
 class SocialLoginContainer extends React.Component<IProps, IState> {
+  public state = {
+    email: '',
+    fbId: '',
+    firstName: '',
+    lastName: ''    
+  };
+
+  public facebookMutation: MutationFn;
+
   public render() {
     return (
       <LoginMutation mutation={FACEBOOK_CONNECT}>
-        {() => (
-          <SocialLoginPresenter />
-        )}
+        {(facebookMutation, { loading }) => {
+          this.facebookMutation = facebookMutation;
+          return (<SocialLoginPresenter loginCallback={this.loginCallback}/>)
+        }}
       </LoginMutation>
     );
+  }
+
+  public loginCallback = response => {
+    const { name, first_name, last_name, email, id, accessToken } = response;
+    if (accessToken) {
+      toast.success(`Welcome ${name}!`);
+      this.facebookMutation({
+        variables: {
+          email,
+          fbId: id,
+          firstName: first_name,
+          lastName: last_name
+        }
+      })
+    } else {
+      toast.error("Could not log you in 😔");
+    }
+  
   }
 }
 
