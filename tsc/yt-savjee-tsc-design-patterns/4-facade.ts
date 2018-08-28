97c88285ca10e7b6e4d6d7a18315d7d4fb0b811f
Jinho Hyeon <neverlish@gmail.com>
2018-08-28 16:12:54 +0900

    4 Facade

---
+++ tsc/yt-savjee-tsc-design-patterns/4-facade.ts
@@ -0,0 +1,109 @@
+class BlurayPlayer {
+  on() {
+    console.log('Blueray player turning on...');
+  }
+
+  turnOff() {
+    console.log('Bluray turning off...');
+  }
+
+  play() {
+    console.log('Playing bluray disc...');
+  }
+}
+
+class Amplifier {
+  on() {
+    console.log('Amp is turning on..');
+  }
+
+  turnOff() {
+    console.log('Amplifier turning off...');
+  }
+
+  setSource(source: string) {
+    console.log('Setting source to ' + source);
+  }
+
+  setVolume(volumeLevel: number) {
+    console.log('Setting volue to ' + volumeLevel);
+  }
+}
+
+class Lights {
+  dim() {
+    console.log('Lights are dimming..');
+  }
+}
+
+class TV {
+  turnOn() {
+    console.log('TV turning on..');
+  }
+
+  turnOff() {
+    console.log('TV turning off..');
+  }
+}
+
+class PopcornMaker {
+  turnOn() {
+    console.log('Popcorn maker turning on..');
+  }
+
+  turnOff() {
+    console.log('Popcorn maker turning off..');
+  }
+
+  pop() {
+    console.log('Popping corn!');
+  }
+}
+
+class HomeTheaterFacede {
+  private bluray: BlurayPlayer;
+  private amp: Amplifier;
+  private lights: Lights;
+  private tv: TV;
+  private popcornMaker: PopcornMaker;
+
+  constructor(amp: Amplifier, bluray: BlurayPlayer, lights: Lights, tv: TV, popcornMaker: PopcornMaker) {
+    this.amp = amp;
+    this.bluray = bluray;
+    this.lights = lights;
+    this.tv = tv;
+    this.popcornMaker = popcornMaker;
+  }
+
+  public watchMovie() {
+    this.popcornMaker.turnOn();
+    this.popcornMaker.pop();
+
+    this.lights.dim();
+
+    this.tv.turnOn();
+
+    this.amp.on();
+    this.amp.setSource('bluray');
+    this.amp.setVolume(11);
+
+    this.bluray.on();
+    this.bluray.play();
+  }
+
+  public endMovie() {
+    this.popcornMaker.turnOff();
+    this.amp.turnOff();
+    this.tv.turnOff();
+    this.bluray.turnOff();
+  }
+}
+
+let bluray = new BlurayPlayer();
+let amp = new Amplifier();
+let lights = new Lights();
+let tv = new TV();
+let popcornMaker = new PopcornMaker();
+
+let hometheater = new HomeTheaterFacede(amp, bluray, lights, tv, popcornMaker);
+hometheater.watchMovie();
