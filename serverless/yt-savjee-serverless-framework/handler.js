8a93285be4364678dc12fcce3a41ef1da34280b5
Jinho Hyeon <neverlish@gmail.com>
2018-08-29 00:07:02 +0900

    09 Working with input

--- serverless/yt-savjee-serverless-framework/handler.js
+++ serverless/yt-savjee-serverless-framework/handler.js
@@ -1,6 +1,27 @@
 'use strict';
 
 module.exports.hello = async (event, context) => {
+  if (event.queryStringParameters && event.queryStringParameters.name) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Hello ' + event.queryStringParameters.name + ', nice to meet you!',
+      }),  
+    }
+  }
+
+  if (event.httpMethod === 'POST' && event.body) {
+    let json = JSON.parse(event.body);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Hi I have received a JSON object for you!',
+        object: json
+      }),  
+    }
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({
