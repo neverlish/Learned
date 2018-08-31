eac13e3851d7b864dbabdd9ad14268154f67b36c
Jinho Hyeon <neverlish@gmail.com>
2018-08-31 20:44:30 +0900

    02 JSX - 4 JSX 문법 - 1 감싸인 요소

--- react/gilbut-react-technique/hello-react/src/App.js
+++ react/gilbut-react-technique/hello-react/src/App.js
@@ -5,14 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>리액트 안녕!</h1>        
+        <h2>당신은 어썸한가요?</h2>
       </div>
     );
   }
