4b82b02d025feb18ae97d6d282fc336259bf01da
Jinho Hyeon <neverlish@gmail.com>
2018-08-31 20:52:59 +0900

    03 - 1 - 2 컴포넌트 초기 코드 작성

---
+++ react/gilbut-react-technique/hello-react/src/MyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        나의 새롭고 멋진 컴포넌트
+      </div>
+    )
+  }
+}
+
+export default MyComponent;
