876477e273a89942e5d10c70e76b825041b9ec2e
Jinho Hyeon <neverlish@gmail.com>
2018-08-31 21:03:07 +0900

    03 - 3 - 4 state 값 업데이트: setState()

--- react/gilbut-react-technique/hello-react/src/MyComponent.js
+++ react/gilbut-react-technique/hello-react/src/MyComponent.js
@@ -24,6 +24,12 @@ class MyComponent extends Component {
         <p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p>
         <p>저는 {this.props.age}살 입니다.</p>
         <p>숫자: {this.state.number}</p>
+        <button onClick={() => {
+            this.setState({
+              number: this.state.number + 1
+            })
+          }
+        }>더하기</button>
       </div>
     )
   }
