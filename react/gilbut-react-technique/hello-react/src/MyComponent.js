785cd3147eec555034593fb7857cc66e3e156088
Jinho Hyeon <neverlish@gmail.com>
2018-08-31 21:02:00 +0900

    03 - 3 - 3 JSX 내부에서 state 렌더링

--- react/gilbut-react-technique/hello-react/src/MyComponent.js
+++ react/gilbut-react-technique/hello-react/src/MyComponent.js
@@ -23,6 +23,7 @@ class MyComponent extends Component {
       <div>
         <p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p>
         <p>저는 {this.props.age}살 입니다.</p>
+        <p>숫자: {this.state.number}</p>
       </div>
     )
   }
