dce7f01607fcf5a6697cca735eff197338ccdb2b
Jinho Hyeon <neverlish@gmail.com>
2018-08-31 20:44:57 +0900

    02 - 4 - 2 자바스크립트 표현

--- react/gilbut-react-technique/hello-react/src/App.js
+++ react/gilbut-react-technique/hello-react/src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const text = '당신은 어썸한가요?';
     return (
       <div>
         <h1>리액트 안녕!</h1>        
-        <h2>당신은 어썸한가요?</h2>
+        <h2>{text}</h2>
       </div>
     );
   }
