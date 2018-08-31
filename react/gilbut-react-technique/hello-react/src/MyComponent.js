36409ab802d23e85e8eac2888fe628a16f5c16f3
Jinho Hyeon <neverlish@gmail.com>
2018-08-31 20:57:20 +0900

    03 - 2 - 4 props 검증: propTypes

--- react/gilbut-react-technique/hello-react/src/MyComponent.js
+++ react/gilbut-react-technique/hello-react/src/MyComponent.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
   static defaultProps = {
     name: '기본 이름'
   }
+
+  static propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired
+  }
   
   render() {
     return (
       <div>
-        안녕하세요, 제 이름은 {this.props.name} 입니다.
+        <p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p>
+        <p>저는 {this.props.age}살 입니다.</p>
       </div>
     )
   }
