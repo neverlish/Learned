faa881b7d292110d6f01514dd04844b7a58a47de
Jinho Hyeon <neverlish@gmail.com>
2018-08-31 20:45:33 +0900

    02 - 4 - 3 if 문 대신 조건부 연산자

--- react/gilbut-react-technique/hello-react/src/App.js
+++ react/gilbut-react-technique/hello-react/src/App.js
@@ -5,10 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     const text = '당신은 어썸한가요?';
+    const condition = true;
+
     return (
       <div>
         <h1>리액트 안녕!</h1>        
         <h2>{text}</h2>
+        {
+          condition ? '참' : '거짓'
+        }
       </div>
     );
   }
