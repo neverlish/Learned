a9b4ddc48d2977def9bd8714d6248d2445609b7d
Jinho Hyeon <neverlish@gmail.com>
2018-08-31 20:47:08 +0900

    02 - 4 - 4 &&를 사용한 조건부 렌더링

--- react/gilbut-react-technique/hello-react/src/App.js
+++ react/gilbut-react-technique/hello-react/src/App.js
@@ -11,9 +11,7 @@ class App extends Component {
       <div>
         <h1>리액트 안녕!</h1>        
         <h2>{text}</h2>
-        {
-          condition ? '참' : '거짓'
-        }
+        { condition && '보여주세요' }
       </div>
     );
   }
