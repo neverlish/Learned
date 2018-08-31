a9d27b207332a72223c474a917b47a26319611d2
Jinho Hyeon <neverlish@gmail.com>
2018-08-31 20:47:43 +0900

    02 - 4 - 5 인라인 스타일링

--- react/gilbut-react-technique/hello-react/src/App.js
+++ react/gilbut-react-technique/hello-react/src/App.js
@@ -6,12 +6,22 @@ class App extends Component {
   render() {
     const text = '당신은 어썸한가요?';
     const condition = true;
+    const style = {
+      backgroundColor: 'gray',
+      border: '1px solid black',
+      height: Math.round(Math.random() * 300) + 50,
+      width: Math.round(Math.random() * 300) + 50,
+      WebkitTransition: 'all',
+      MozTransition: 'all',
+      msTransition: 'all'
+    };
 
     return (
       <div>
         <h1>리액트 안녕!</h1>        
         <h2>{text}</h2>
         { condition && '보여주세요' }
+        <div style={style}></div>
       </div>
     );
   }
