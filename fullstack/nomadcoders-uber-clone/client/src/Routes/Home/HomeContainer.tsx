30ce1a7e5e3c135ca0e316b1ccb574b5718c8f21
Jinho Hyeon <neverlish@gmail.com>
2018-09-01 17:45:12 +0900

    2 - 55 HomeScreen Moving with the User

--- fullstack/nomadcoders-uber-clone/client/src/Routes/Home/HomeContainer.tsx
+++ fullstack/nomadcoders-uber-clone/client/src/Routes/Home/HomeContainer.tsx
@@ -35,7 +35,7 @@ class HomeContainer extends React.Component<IProps, IState> {
   }
 
   public componentDidMount() {
-    navigator.geolocation.watchPosition(
+    navigator.geolocation.getCurrentPosition(
       this.handleGeoSuccess,
       this.handleGeoError
     );
@@ -113,7 +113,9 @@ class HomeContainer extends React.Component<IProps, IState> {
   }
 
   public handleGeoWatchSuccess = (position: Position) => {
-    return;
+    const { coords: { latitude, longitude } } = position;
+    this.userMarker.setPosition({ lat: latitude, lng: longitude });
+    this.map.panTo({ lat: latitude, lng: longitude });
   }
 
   public handleGeoWatchError = () => {
