71607b6f563bf9e34d026d372e71aa690baa835c
Jinho Hyeon <neverlish@gmail.com>
2018-09-03 00:57:36 +0900

    2 - 65 Drawing Nearby Drivers part Three

--- fullstack/nomadcoders-uber-clone/client/src/Routes/Home/HomeContainer.tsx
+++ fullstack/nomadcoders-uber-clone/client/src/Routes/Home/HomeContainer.tsx
@@ -276,22 +276,39 @@ class HomeContainer extends React.Component<IProps, IState> {
       } = data;
       if (ok && drivers) {
         for (const driver of drivers) {
-          console.log(driver);
           if (driver && driver.lastLat && driver.lastLng) {
-            const markerOptions: google.maps.MarkerOptions = {
-              icon: {
-                path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-                scale: 5
-              },
-              position: {
-                lat: driver.lastLat,
-                lng: driver.lastLng
+            const existingDriver: 
+              | google.maps.Marker 
+              | undefined 
+              = this.drivers.find(
+              (driverMarker: google.maps.Marker) => {
+                const markerID = driverMarker.get('ID');
+                return markerID === driver.id;
               }
-            };
-            const newMarker: google.maps.Marker = new google.maps.Marker(markerOptions);
-            newMarker.set('ID', driver.id);
-            newMarker.setMap(this.map);
-            this.drivers.push(newMarker);
+            );
+
+            if (existingDriver) {
+              existingDriver.setPosition({
+                lat:driver.lastLat, 
+                lng: driver.lastLng
+              });
+              existingDriver.setMap(this.map);
+            } else {
+              const markerOptions: google.maps.MarkerOptions = {
+                icon: {
+                  path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                  scale: 5
+                },
+                position: {
+                  lat: driver.lastLat,
+                  lng: driver.lastLng
+                }
+              };
+              const newMarker: google.maps.Marker = new google.maps.Marker(markerOptions);
+              newMarker.set('ID', driver.id);
+              newMarker.setMap(this.map);
+              this.drivers.push(newMarker);
+            }
           }
         }
         this.drivers = [];
