d3f463a3cbdea6134308febe0b25e8b605e1e8b1
Jinho Hyeon <neverlish@gmail.com>
2018-09-03 00:01:35 +0900

    2 - 62 NearbyDrivers Query part Two

--- fullstack/nomadcoders-uber-clone/client/src/Routes/Home/HomeContainer.tsx
+++ fullstack/nomadcoders-uber-clone/client/src/Routes/Home/HomeContainer.tsx
@@ -64,23 +64,38 @@ class HomeContainer extends React.Component<IProps, IState> {
     const { isMenuOpen, toAddress, price } = this.state;
     return (
       <ProfileQuery query={USER_PROFILE}>
-        {({ data, loading }) => (
-          <NearbyQueries query={GET_NEARBY_DRIVERS}>
-            {() => (
-              <HomePresenter
-                loading={loading}
-                isMenuOpen={isMenuOpen}
-                price={price}
-                toggleMenu={this.toggleMenu}
-                mapRef={this.mapRef}
-                toAddress={toAddress}
-                onInputChange={this.onInputChange}
-                onAddressSubmit={this.onAddressSubmit}
-                data={data}
-              />
-            )}
-          </NearbyQueries>
-        )}
+        {({ data, loading }) => {
+          if (data && data.GetMyProfile) {
+            const { GetMyProfile: { user = null } = {} } = data;
+            if (user) {
+              return (
+                <NearbyQueries
+                  query={GET_NEARBY_DRIVERS}
+                  skip={user.isDriving}
+                  onCompleted={this.handleNearbyDrivers}
+                >
+                  {() => (
+                    <HomePresenter
+                      loading={loading}
+                      isMenuOpen={isMenuOpen}
+                      price={price}
+                      toggleMenu={this.toggleMenu}
+                      mapRef={this.mapRef}
+                      toAddress={toAddress}
+                      onInputChange={this.onInputChange}
+                      onAddressSubmit={this.onAddressSubmit}
+                      data={data}
+                    />
+                  )}
+                </NearbyQueries>
+              )
+            } else {
+              return null;
+            }
+          } else {
+            return 'Loading';
+          }
+        }}
       </ProfileQuery>
     );
   }
@@ -251,6 +266,17 @@ class HomeContainer extends React.Component<IProps, IState> {
       });
     }
   };
+
+  public handleNearbyDrivers = (data: {} | getDrivers) => {
+    if ('GetNearbyDrivers' in data) {
+      const { 
+        GetNearbyDrivers: { drivers, ok } 
+      } = data;
+      if (ok && drivers) {
+        console.log(drivers);
+      }
+    }
+  }
 }
 
 export default graphql<any, reportMovement>(
