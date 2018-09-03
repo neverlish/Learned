9356b55586cea92d5c3fafa05698a48510874509
Jinho Hyeon <neverlish@gmail.com>
2018-09-03 22:32:42 +0900

    2 - 71 Apollo Subscriptions part Two

--- fullstack/nomadcoders-uber-clone/client/src/Routes/Home/HomeContainer.tsx
+++ fullstack/nomadcoders-uber-clone/client/src/Routes/Home/HomeContainer.tsx
@@ -131,9 +131,22 @@ class HomeContainer extends React.Component<IProps, IState> {
                     {({ subscribeToMore, data: nearbyRide }) => {
                       const rideSubscriptionOptions: SubscribeToMoreOptions = {
                         document: SUBSCRIBE_NEARBY_RIDES,
-                        updateQuery: this.handleSubscriptionUpdate
+                        updateQuery: (prev, { subscriptionData }) => {
+                          if (!subscriptionData.data) {
+                            return prev;
+                          }
+                          const newObject = Object.assign({}, prev, {
+                            GetNearbyRide: {
+                              ...prev.GetNearbyRide,
+                              ride: subscriptionData.data.NearbyRideSubscription
+                            }
+                          });
+                          return newObject;
+                        }
                       };
-                      subscribeToMore(rideSubscriptionOptions);
+                      if (isDriving) {
+                        subscribeToMore(rideSubscriptionOptions);
+                      }
                       return <AcceptRide mutation={ACCEPT_RIDE}>
                         {(acceptRideFn) => (
                           <HomePresenter
@@ -412,10 +425,6 @@ class HomeContainer extends React.Component<IProps, IState> {
         });
       }
     }
-  }
-
-  public handleSubscriptionUpdate = (data) => {
-    console.log(data);
   }
 }
 
