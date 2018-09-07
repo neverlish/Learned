d3a9396c0f02f68b0fcb315e74f214a0494d669b
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 03:46:06 +0900

    2 - 78 Chat Screen Mutation

--- fullstack/nomadcoders-uber-clone/client/src/Routes/Chat/ChatPresenter.tsx
+++ fullstack/nomadcoders-uber-clone/client/src/Routes/Chat/ChatPresenter.tsx
@@ -1,40 +1,73 @@
 import React from 'react';
+import Form from '../../Components/Form';
 import Header from '../../Components/Header';
+import Input from '../../Components/Input';
 import Message from '../../Components/Message';
 import styled from '../../typed-components';
 import { getChat, userProfile } from '../../types/api';
 
 const Container = styled.div``;
 
+const Chat = styled.div`
+  height: 80vh;
+  overflow: scroll;
+  padding: 0 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const InputCont = styled.div`
+  padding: 0 20px;
+`;
+
 interface IProps {
   data?: getChat;
   userData?: userProfile;
   loading: boolean;
+  messageText: string;
+  onSubmit: () => void;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const ChatPresenter: React.SFC<IProps> = ({
   loading,
   userData: { GetMyProfile : { user = null } = {} } = {},
-  data: { GetChat: { chat = null } = {} } = {}
+  data: { GetChat: { chat = null } = {} } = {},
+  messageText,
+  onInputChange,
+  onSubmit
 }) => (
   <Container>
     <Header title={'Chat'} />
     {!loading && 
       chat && user && (
         <React.Fragment>
-          {chat.messages &&
-            chat.messages.map(message => {
-              if (message) {
-                return (
-                  <Message
-                    key={message.id}
-                    text={message.text}
-                    mine={user.id === message.userId}
-                  />
-                );
-              }
-              return null;
-            })}
+          <Chat>
+            {chat.messages &&
+              chat.messages.map(message => {
+                if (message) {
+                  return (
+                    <Message
+                      key={message.id}
+                      text={message.text}
+                      mine={user.id === message.userId}
+                    />
+                  );
+                }
+                return null;
+              })}
+          </Chat>
+          <InputCont>
+            <Form submitFn={onSubmit}>
+              <Input
+                value={messageText}
+                placeholder={'Type your message'}
+                onChange={onInputChange}
+                name={'message'}
+              />
+            </Form>
+          </InputCont>
         </React.Fragment>
       )}
   </Container>
