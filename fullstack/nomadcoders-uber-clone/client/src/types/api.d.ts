493ed463bf5f4dd569a13dab7cee5b80ebdcbd2c
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 04:02:35 +0900

    2 - 79 Chat Screen Subscription

--- fullstack/nomadcoders-uber-clone/client/src/types/api.d.ts
+++ fullstack/nomadcoders-uber-clone/client/src/types/api.d.ts
@@ -132,6 +132,24 @@ export interface sendMessageVariables {
 // This file was automatically generated and should not be edited.
 
 // ====================================================
+// GraphQL subscription operation: messageSubscription
+// ====================================================
+
+export interface messageSubscription_MessageSubscription {
+  __typename: "Message";
+  id: number;
+  text: string;
+  userId: number | null;
+}
+
+export interface messageSubscription {
+  MessageSubscription: messageSubscription_MessageSubscription | null;
+}
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
 // GraphQL mutation operation: updateProfile
 // ====================================================
 
