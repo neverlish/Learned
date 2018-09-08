366b4e918af453e9681544f5e0b38400b40c22dd
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 19:58:54 +0900

    06 - 3 key - 1 key 설정

--- react/gilbut-react-technique/hello-react/src/IterationSample.js
+++ react/gilbut-react-technique/hello-react/src/IterationSample.js
@@ -4,7 +4,7 @@ class IterationSample extends Component {
   render() {
     const names = ['눈사람', '얼음', '눈', '바람'];
     const nameList = names.map(
-      (name) => (<li>{name}</li>)
+      (name, index ) => (<li key={index}>{name}</li>)
     );
 
     return (
