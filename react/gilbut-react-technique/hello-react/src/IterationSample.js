e39dd4f368efc18ac8253eab91859f8f963c67a8
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 19:57:02 +0900

    06 컴포넌트 반복 - 2 데이터 배열을 컴포넌트 배열로 map하기 - 1 예제 컴포넌트 생성

---
+++ react/gilbut-react-technique/hello-react/src/IterationSample.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class IterationSample extends Component {
+  render() {
+    const names = ['눈사람', '얼음', '눈', '바람'];
+    const nameList = names.map(
+      (name) => (<li>{name}</li>)
+    );
+
+    return (
+      <ul>
+        {nameList}
+      </ul>
+    )
+  }
+}
+
+export default IterationSample;
