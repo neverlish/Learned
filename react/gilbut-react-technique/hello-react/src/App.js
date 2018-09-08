f67df2971c4c7a8195eae051e012b674d8cda209
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 19:57:57 +0900

    06 - 2 - 2 App 컴포넌트에서 예제 컴포넌트 렌더링

--- react/gilbut-react-technique/hello-react/src/App.js
+++ react/gilbut-react-technique/hello-react/src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <ScrollBox ref={(ref) => this.scrollBox = ref}/>
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
+      <IterationSample />
     );
   }
 }
