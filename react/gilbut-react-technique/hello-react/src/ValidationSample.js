7cad885aac34b6293b618ba0079e7a0d834fc840
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 13:52:57 +0900

    05 - 2 ref 사용 - 2 적용

--- react/gilbut-react-technique/hello-react/src/ValidationSample.js
+++ react/gilbut-react-technique/hello-react/src/ValidationSample.js
@@ -19,6 +19,7 @@ class ValidationSample extends Component {
       clicked: true,
       validated: this.state.password === '0000'
     });
+    this.input.focus();
   }
 
   render() {
@@ -29,6 +30,7 @@ class ValidationSample extends Component {
           value={this.state.password}
           onChange={this.handleChange}
           className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''}
+          ref={(ref) => this.input = ref}
         />
         <button onClick={this.handleButtonClick}>검증하기</button>
       </div>
