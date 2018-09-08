ab22f33436fae12358b355b16d2abb783fdfe997
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 13:33:59 +0900

    04 - 2 - 2 onChange 이벤트 핸들링

--- react/gilbut-react-technique/hello-react/src/EventPractice.js
+++ react/gilbut-react-technique/hello-react/src/EventPractice.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+  state = {
+    message: ''
+  }
+
   render() {
     return (
       <div>
         <h1>이벤트 연습</h1>
+        <input
+          type='text'
+          name='message'
+          placeholder='아무거나 입력해보세요'
+          value={this.state.message}
+          onChange={
+            (e) => {
+              this.setState({
+                message: e.target.value
+              })
+            }
+          }
+        />
+        <button onClick={
+          () => {
+            alert(this.state.message);
+            this.setState({
+              message: ''
+            });
+          }
+        }>확인</button>
       </div>
     );
   }
