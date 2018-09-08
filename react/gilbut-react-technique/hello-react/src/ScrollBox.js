393c1a57c02dadf787b273cc7292b6f3bb9b8880
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 13:57:51 +0900

    05 - 3 - 3 컴포넌트에 메서드 생성

--- react/gilbut-react-technique/hello-react/src/ScrollBox.js
+++ react/gilbut-react-technique/hello-react/src/ScrollBox.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class ScrollBox extends Component {
+
+  scrollToBottom = () => {
+    const { scrollHeight, clientHeight } = this.box;
+    this.box.scrollTop = scrollHeight - clientHeight;
+  }
+
   render() {
     const style = {
       border: '1px solid black',
