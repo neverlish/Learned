fbc465576253630f9891b28e931e932119048dc8
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 11:27:53 +0900

    3 - 02 Deploying to Now part Two

--- fullstack/nomadcoders-uber-clone/client/src/apollo.ts
+++ fullstack/nomadcoders-uber-clone/client/src/apollo.ts
@@ -8,6 +8,8 @@ import { WebSocketLink } from "apollo-link-ws";
 import { getMainDefinition } from "apollo-utilities";
 import { toast } from "react-toastify";
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const getToken = () => {
   const token = localStorage.getItem("jwt");
   if (token) {
@@ -29,7 +31,9 @@ const authMiddleware = new ApolloLink((operation: Operation, forward: any) => {
 });
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000/graphql"
+  uri: isDev
+    ? "http://localhost:4000/graphql"
+    : "https://nuberserver-neverlish.now.sh/graphql"
 });
 
 const wsLink = new WebSocketLink({
@@ -39,7 +43,9 @@ const wsLink = new WebSocketLink({
     },
     reconnect: true
   },
-  uri: "ws://localhost:4000/subscription"
+  uri: isDev 
+    ? "ws://localhost:4000/subscription"
+    : "ws://nuberserver-neverlish.now.sh/subscription"
 });
 
 const combinedLinks = split(
--- fullstack/nomadcoders-uber-clone/server/description.md
+++ fullstack/nomadcoders-uber-clone/server/description.md
@@ -5,3 +5,18 @@
 psql
 # CREATE DATABASE nuber;
 ```
+
+### 3 - 2 Deploying to Now part Two
+
+- Create Database
+  - AWS RDS -> Create Database
+    - Engine Options -> Postgres
+      - Only enable options eligible for RDS Free Usage Tier
+    - Input Settings
+    - Data Options -> Input Database Name
+    - Create
+- Change Database
+  - Security Group -> Inbound
+  - Inbound Edit -> Source: Anywhere
+- Alias Now.sh
+  - `$ now alias https://nuber-server-qgvjrifnzz.now.sh nuberserver-neverlish`
