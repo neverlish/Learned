54b22493d375837d5d3ff157878a0be821f4806c
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 20:37:48 +0900

    07 - 3 - 2 App 컴포넌트에서 예제 컴포넌트 사용

--- react/gilbut-react-technique/hello-react/src/App.js
+++ react/gilbut-react-technique/hello-react/src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
-      <IterationSample />
-    );
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    )
   }
 }
 
