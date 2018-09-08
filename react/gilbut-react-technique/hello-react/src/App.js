d93b93dd015b7c9adc221f020a2dcb4bc3ee8fca
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 13:31:05 +0900

    04 이벤트 핸들링 - 2 예제로 이벤트 핸들링 익히기 - 1 컴포넌트 생성 및 불러오기

--- react/gilbut-react-technique/hello-react/src/App.js
+++ react/gilbut-react-technique/hello-react/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import MyComponent from './MyComponent';
+import EventPractice from './EventPractice';
 
 class App extends Component {
   render() {
     return (
-      <MyComponent name="React" age={4} />
+      <EventPractice />
     );
   }
 }
---
+++ react/gilbut-react-technique/hello-react/src/EventPractice.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EventPractice extends Component {
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
