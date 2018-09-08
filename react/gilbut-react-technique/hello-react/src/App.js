42e3a569db0c7c7660e824954251fb79764557be
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 13:52:49 +0900

    05 - 1 - 2 App 컴포넌트에서 예제 컴포넌트 렌더링

--- react/gilbut-react-technique/hello-react/src/App.js
+++ react/gilbut-react-technique/hello-react/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
 
 class App extends Component {
   render() {
     return (
-      <EventPractice />
+      <ValidationSample />
     );
   }
 }
