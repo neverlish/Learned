7529a3576a025b5525ead3e595083ee5d728197a
Jinho Hyeon <neverlish@gmail.com>
2018-09-08 13:56:05 +0900

    05 - 3 컴포넌트에 ref 달기 - 2 컴포넌트 초기 설정

--- react/gilbut-react-technique/hello-react/src/App.js
+++ react/gilbut-react-technique/hello-react/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
   render() {
     return (
-      <ValidationSample />
+      <div>
+        <ScrollBox />
+      </div>
     );
   }
 }
