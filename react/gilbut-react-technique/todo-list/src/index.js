b43d707f993b6a6d5a2bd60c20807638cb5b32d1
Jinho Hyeon <neverlish@gmail.com>
2018-09-09 23:52:35 +0900

    10 - 1 - 2 CSS Module 및 Sass 적용

--- react/gilbut-react-technique/todo-list/src/index.js
+++ react/gilbut-react-technique/todo-list/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './styles/main.scss';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
