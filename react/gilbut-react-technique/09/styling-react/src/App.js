c3f9afe80406b471c20f0c8cb8d3767d0ca2785d
Jinho Hyeon <neverlish@gmail.com>
2018-09-09 22:46:51 +0900

    09 - 2 - 2 Sass 사용

--- react/gilbut-react-technique/09/styling-react/src/App.js
+++ react/gilbut-react-technique/09/styling-react/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classNames from 'classnames/bind';
-import styles from './App.css';
+import styles from './App.scss';
 
 const cx = classNames.bind(styles);
 
@@ -11,7 +11,7 @@ class App extends Component {
       <div className={cx('box', {
         blue: isBlue
       })}>
-        
+        <div className={cx('box-inside')}/>
       </div>
     );
   }
