a3b361f088022bbb76e9a5cf583d4a7c6f7bf162
Jinho Hyeon <neverlish@gmail.com>
2018-09-09 23:38:10 +0900

    09 - 3 styled-components

--- react/gilbut-react-technique/09/styling-react/src/App.js
+++ react/gilbut-react-technique/09/styling-react/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Button from './components/Button';
+import StyledButton from './components/StyledButton';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Button>버튼</Button>
+        <StyledButton big>버튼</StyledButton>
       </div>
     );
   }
