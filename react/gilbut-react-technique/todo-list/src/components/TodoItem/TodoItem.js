81c0845372bec5bff05c1a7a7bf7b9bbe6ed8c9b
Jinho Hyeon <neverlish@gmail.com>
2018-09-10 00:12:38 +0900

    10 - 2 - 4 TodoItem 컴포넌트 생성

---
+++ react/gilbut-react-technique/todo-list/src/components/TodoItem/TodoItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import styles from './TodoItem.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+class TodoItem extends Component {
+  render() {
+    const { done, children, onToggle, onRemove } = this.props;
+
+    return (
+      <div className={cx('todo-item')} onClick={onToggle}>
+        <input className={cx('tick')} type='checkbox' checked={done} readOnly />
+        <div className={cx('text', { done })}>{children}</div>
+        <div className={cx('delete')} onClick={onRemove}>[지우기]</div>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
