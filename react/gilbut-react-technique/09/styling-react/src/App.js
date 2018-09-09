3886f668fb77f0762f90a1be40cacfcf2d4d1d7c
Jinho Hyeon <neverlish@gmail.com>
2018-09-09 22:14:49 +0900

    09 - 1 - 2 CSS Module 사용

--- react/gilbut-react-technique/09/styling-react/src/App.js
+++ react/gilbut-react-technique/09/styling-react/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classNames from 'classnames/bind';
+import styles from './App.css';
+
+const cx = classNames.bind(styles);
 
 class App extends Component {
   render() {
+    const isBlue = true;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={cx('box', {
+        blue: isBlue
+      })}>
+        
       </div>
     );
   }
