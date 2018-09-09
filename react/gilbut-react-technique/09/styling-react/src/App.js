fc96d7d4e8c56a8f117b9f1119d8e9e73b600f5b
Jinho Hyeon <neverlish@gmail.com>
2018-09-09 23:22:13 +0900

    09 - 2 - 5 버튼 생성

--- react/gilbut-react-technique/09/styling-react/src/App.js
+++ react/gilbut-react-technique/09/styling-react/src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import classNames from 'classnames/bind';
-import styles from './App.scss';
-
-const cx = classNames.bind(styles);
+import Button from './components/Button';
 
 class App extends Component {
   render() {
-    const isBlue = true;
     return (
-      <div className={cx('box', {
-        blue: isBlue
-      })}>
-        <div className={cx('box-inside')}/>
+      <div>
+        <Button>버튼</Button>
       </div>
     );
   }
