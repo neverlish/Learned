872f1e4405b97ae489b711fe98dc2e420a1dec4e
Jinho Hyeon <neverlish@gmail.com>
2018-09-17 23:20:20 +0900

    04 - 2 - 2 초기 상태 설정하기

--- react/gilbut-react-quickly/04/clock/jsx/clock.jsx
+++ react/gilbut-react-quickly/04/clock/jsx/clock.jsx
@@ -1,4 +1,10 @@
 class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentTime: (new Date()).toLocaleString('en')
+    }
+  }
   render() {
     return <div>{this.state.currentTime}</div>
   }
