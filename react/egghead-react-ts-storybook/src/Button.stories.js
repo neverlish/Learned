1205fbfdeeecd39b9f535f0a9342fdbd1676fd1f
Jinho Hyeon <neverlish@gmail.com>
2018-09-19 14:24:38 +0900

    5 Display the JSX of a React Storybook Story with the JSX Addon

--- react/egghead-react-ts-storybook/src/Button.stories.js
+++ react/egghead-react-ts-storybook/src/Button.stories.js
@@ -3,6 +3,6 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Button } from './Button'
 
-storiesOf('Button', module).add('with background', () => (
-  <Button bg='palegoldenrod'>Hello world</Button>
-))
+storiesOf('Button', module)
+  .addWithJSX('with background', () => (<Button bg='palegoldenrod'>Hello world</Button>))
+  .addWithJSX('with background 2', () => (<Button bg='green'>Hello world</Button>))
