572b6f36b2b828f45d440896d18b4e14a430e01f
Jinho Hyeon <neverlish@gmail.com>
2018-09-19 14:40:06 +0900

    7 Create Interactive Storybook Stories with the Knobs Decorator

--- react/egghead-react-ts-storybook/src/Button.stories.js
+++ react/egghead-react-ts-storybook/src/Button.stories.js
@@ -3,6 +3,7 @@ import { wInfo } from './utils'
 
 import { storiesOf } from '@storybook/react'
 import { Button } from './Button'
+import { color } from '@storybook/addon-knobs/react'
 
 storiesOf('Button', module)
   .addWithJSX(
@@ -15,4 +16,6 @@ storiesOf('Button', module)
       ~~~
     `)(() => (<Button bg='palegoldenrod'>Hello world</Button>))
   )
-  .addWithJSX('with background 2', () => (<Button bg='green'>Hello world</Button>))
+  .addWithJSX('with background 2', () => (
+    <Button bg={color('bg', 'green', 'group1')}>Hello world</Button>)
+  )
