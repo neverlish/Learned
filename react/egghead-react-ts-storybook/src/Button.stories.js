b58456b41bd94dc420638366fc32d85a8d03fa27
Jinho Hyeon <neverlish@gmail.com>
2018-09-19 14:35:27 +0900

    6 Apply Style to a React StoryBook with the Info addon to Turn Stories into Documentation

--- react/egghead-react-ts-storybook/src/Button.stories.js
+++ react/egghead-react-ts-storybook/src/Button.stories.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { wInfo } from './utils'
 
 import { storiesOf } from '@storybook/react'
 import { Button } from './Button'
 
 storiesOf('Button', module)
-  .addWithJSX('with background', () => (<Button bg='palegoldenrod'>Hello world</Button>))
+  .addWithJSX(
+    'with background', 
+    wInfo(`
+      description of the component
+
+      ~~~js
+      <Button>Click here</Button>
+      ~~~
+    `)(() => (<Button bg='palegoldenrod'>Hello world</Button>))
+  )
   .addWithJSX('with background 2', () => (<Button bg='green'>Hello world</Button>))
