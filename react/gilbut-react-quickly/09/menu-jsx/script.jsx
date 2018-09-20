610d51a0752ab42cf60308d1c766bfb36d73a9dd
Jinho Hyeon <neverlish@gmail.com>
2018-09-21 03:05:15 +0900

    09 - 3 - 2 Link 컴포넌트 리팩토링

--- react/gilbut-react-quickly/09/menu-jsx/script.jsx
+++ react/gilbut-react-quickly/09/menu-jsx/script.jsx
@@ -18,7 +18,13 @@ class Menu extends React.Component {
 
 class Link extends React.Component {
   render() {
-    
+    const url = '/' + this.props.label.toLowerCase().trim().replace(' ', '-')
+    return <div>
+      <a href={url}>
+        {this.props.label}
+      </a>
+      <br />
+    </div>
   }
 }
 
