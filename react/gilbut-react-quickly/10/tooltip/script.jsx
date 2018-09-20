5288f739454a6aa153f7183db438647c9c30048f
Jinho Hyeon <neverlish@gmail.com>
2018-09-21 03:21:48 +0900

    10 - 2 - 2 render() 메서드

--- react/gilbut-react-quickly/10/tooltip/script.jsx
+++ react/gilbut-react-quickly/10/tooltip/script.jsx
@@ -15,7 +15,30 @@ class Tooltip extends React.Component {
   }
 
   render() {
+    const style = {
+      zIndex: (this.state.opacity) ? 1000 : -1000,
+      opacity: +this.state.opacity,
+      top: (this.state.top || 0) + 20,
+      left: (this.state.left || 0) - 30
+    }
 
+    return (
+      <div style={{ display: 'inline' }}>
+        <span style={{ color: 'blue' }}
+          onMouseEnter={this.toggle}
+          onMouseOut={this.toggle}>
+          {this.props.children}
+        </span>
+        <div className='tooltip bottom'
+          style={style}
+          role='tooltip'>
+          <div className='tooltip-arrow' />
+          <div className='tooltip-inner'>
+            {this.props.text}
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
