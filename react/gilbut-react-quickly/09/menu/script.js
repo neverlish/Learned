a0b857ea4b76b0f1187cda985e9237aa1993340a
Jinho Hyeon <neverlish@gmail.com>
2018-09-21 02:57:03 +0900

    09 - 2 - 1 Menu 컴포넌트

--- react/gilbut-react-quickly/09/menu/script.js
+++ react/gilbut-react-quickly/09/menu/script.js
@@ -1,4 +1,26 @@
-class Menu extends React.Component {}
+class Menu extends React.Component {
+  render() {
+    let menus = [
+      'Home',
+      'About',
+      'Services',
+      'Portfolio',
+      'Contact us'
+    ]
+
+    return React.createElement(
+      'div', 
+      null,
+      menus.map((v, i) => {
+        return React.createElement(
+          'div',
+          { key: i },
+          React.createElement(Link, { label: v })
+        )
+      })
+    )
+  }
+}
 
 class Link extends React.Component {}
 
