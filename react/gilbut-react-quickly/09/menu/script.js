3a51e916f6a4573fdd335c1402a3b5c6f1cd949f
Jinho Hyeon <neverlish@gmail.com>
2018-09-21 02:56:06 +0900

    09 - 2 JSX를 사용하지 않고 만드는 Menu 컴포넌트

---
+++ react/gilbut-react-quickly/09/menu/script.js
@@ -0,0 +1,11 @@
+class Menu extends React.Component {}
+
+class Link extends React.Component {}
+
+ReactDOM.render(
+  React.createElement(
+    Menu,
+    null
+  ),
+  document.getElementById('menu')
+)
