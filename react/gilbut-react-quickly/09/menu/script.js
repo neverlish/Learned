1987da199988ec31c6e17cc86828773dd1ab9107
Jinho Hyeon <neverlish@gmail.com>
2018-09-21 02:59:20 +0900

    09 - 2 - 2 Link 컴포넌트

--- react/gilbut-react-quickly/09/menu/script.js
+++ react/gilbut-react-quickly/09/menu/script.js
@@ -22,7 +22,21 @@ class Menu extends React.Component {
   }
 }
 
-class Link extends React.Component {}
+class Link extends React.Component {
+  render() {
+    const url = '/' + this.props.label.toLowerCase().trim().replace(' ', '-')
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'a',
+        { href: url },
+        this.props.label
+      ),
+      React.createElement('br')
+    )
+  }
+}
 
 ReactDOM.render(
   React.createElement(
