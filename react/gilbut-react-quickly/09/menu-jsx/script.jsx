5e74d88bb3fb844117968ca91f2d35c89497d4d0
Jinho Hyeon <neverlish@gmail.com>
2018-09-21 03:03:41 +0900

    09 - 3 - 1 Menu 컴포넌트 리팩토링

--- react/gilbut-react-quickly/09/menu-jsx/script.jsx
+++ react/gilbut-react-quickly/09/menu-jsx/script.jsx
@@ -1,6 +1,18 @@
 class Menu extends React.Component {
   render() {
+    let menus = [
+      'Home',
+      'About',
+      'Services',
+      'Portfolio',
+      'Contact us'
+    ]
     
+    return <div>
+      {menus.map((v, i) => {
+        return <div key={i}><Link label={v} /></div>
+      })}
+    </div>
   }
 }
 
