74b14e21e44b823bead1d8c31eda3d9cdaef33bd
Jinho Hyeon <neverlish@gmail.com>
2018-09-21 17:27:15 +0900

    11 - 3 TimerWrapper 컴포넌트

--- react/gilbut-react-quickly/11/timer/timer.jsx
+++ react/gilbut-react-quickly/11/timer/timer.jsx
@@ -1,20 +1,41 @@
 class TimerWrapper extends React.Component {
   constructor(props) {
-
+    super(props)
+    this.state = { timeLeft: null, timer: null }
+    this.startTimer = this.startTimer.bind(this)
   }
 
   startTimer(timeLeft) {
-
+    clearInterval(this.state.timer)
+    let timer = setInterval(() => {
+      var timeLeft = this.state.timeLeft - 1
+      console.log('2: Inside of setInterval')
+      if (timeLeft == 0) clearInterval(timer)
+      this.setState({ timeLeft: timeLeft })
+    }, 1000)
+    console.log('1: After setInterval')
+    return this.setState({ timeLeft: timeLeft, timer: timer })
   }
 
   render() {
-
+    return (
+      <div className='row-fluid'>
+        <h2>Timer</h2>
+        <div className='btn-group' role='group'>
+          <Button time='5' startTimer={this.startTimer} />
+          <Button time='10' startTimer={this.startTimer} />
+          <Button time='15' startTimer={this.startTimer} />
+        </div>
+        <Timer timeLeft={this.state.timeLeft} />
+        <audio id='end-of-time' src='flute_c_long_01.wav' preload='auto'></audio>
+      </div>
+    )
   }
 }
 
 class Timer extends React.Component {
   render() {
-    
+
   }
 }
 
