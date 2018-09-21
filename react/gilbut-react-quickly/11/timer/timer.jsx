1a03a7cdb3462c7ae5b7b763f586c4ea0e676e06
Jinho Hyeon <neverlish@gmail.com>
2018-09-21 17:15:05 +0900

    11 - 2 앱 아키텍처

---
+++ react/gilbut-react-quickly/11/timer/timer.jsx
@@ -0,0 +1,34 @@
+class TimerWrapper extends React.Component {
+  constructor(props) {
+
+  }
+
+  startTimer(timeLeft) {
+
+  }
+
+  render() {
+
+  }
+}
+
+class Timer extends React.Component {
+  render() {
+    
+  }
+}
+
+class Button extends React.Component {
+  startTimer(event) {
+
+  }
+
+  render() {
+
+  }
+}
+
+ReactDOM.render(
+  <TimerWrapper />,
+  document.getElementById('timer-app')
+)
