100bb9277dec3737df3924cba53e20763bd3919e
Jinho Hyeon <neverlish@gmail.com>
2018-09-22 21:58:53 +0900

    20 - 2 - 2 서버에서 React 렌더링하기

--- react/gilbut-react-quickly/20/autocomplete/index.js
+++ react/gilbut-react-quickly/20/autocomplete/index.js
@@ -54,4 +54,25 @@ mongodb.MongoClient.connect(url, function(err, db) {
       return res.json(result.ops[0])
     })
   })
+
+  app.get('/', function(req, res, next) {
+    var url = 'http://localhost:3000/rooms'
+    req.rooms.find({}, { sort: {_id: -1 } }).toArray(function(err, rooms) {
+      if (err) return next(err)
+      res.render('index', {
+        autocomplete: ReactDOMServer.renderToString(Autocomplete({
+          options: rooms,
+          url: url
+        })),
+        data: `<script type="text/javascript">
+                window.__autocomplete_data = {
+                  rooms: ${JSON.stringify(rooms, null, 2)},
+                  url: "${url}"
+                }
+              </script>`
+      })
+    })
+  })
+
+  app.listen(port)
 })
