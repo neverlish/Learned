d06fa810a0ab9870ec13dcfe6e9c9f4675abca47
Jinho Hyeon <neverlish@gmail.com>
2018-09-22 11:24:09 +0900

    14 - 3 - 6 영화 목록 리듀서

---
+++ react/gilbut-react-quickly/14/redux-netflix/src/modules/movies.js
@@ -0,0 +1,30 @@
+const { handleActions } = require('redux-actions')
+
+const FETCH_MOVIES = 'movies/FETCH_MOVIES'
+const FETCH_MOVIE = 'movies/FETCH_MOVIE'
+
+const initialState = {
+  movies: [],
+  movie: {}
+}
+
+module.exports = {
+  fetchMoviesActionCreator: (movies) => ({
+    type: FETCH_MOVIES,
+    movies
+  }),
+  fetchMovieActionCreator: (index) => ({
+    type: FETCH_MOVIE,
+    index
+  }),
+  reducer: handleActions({
+    [FETCH_MOVIES]: (state, action) => ({
+      ...state,
+      all: action.movies
+    }),
+    [FETCH_MOVIE]: (state, action) => ({
+      ...state,
+      current: state.all[action.index - 1]
+    })
+  }, initialState)
+}
