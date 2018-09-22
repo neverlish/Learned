660f97e55fce526d0575681d1da418049b95721c
Jinho Hyeon <neverlish@gmail.com>
2018-09-22 21:35:22 +0900

    19 - 4 Password 컴포넌트 구현하기 - 1 테스트 작성

---
+++ react/gilbut-react-quickly/19/password/__tests__/password.test.js
@@ -0,0 +1,34 @@
+describe('비밀번호', function() {
+  it('Generate(생성) 버튼을 누르면 변경된다', (done) => {
+    const TestUtils = require('react-dom/test-utils')
+    const React = require('react')
+    const ReactDOM = require('react-dom')
+    const Password = require('../jsx/password.jsx')
+    const PasswordGenerate = require('../jsx/password-generate.jsx')
+    const PasswordInfo = require('../jsx/password-info.jsx')
+    const PasswordInput = require('../jsx/password-input.jsx')
+    const PasswordVisibility = require('../jsx/password-visibility.jsx')
+
+    const fD = ReactDOM.findDOMNode
+
+    let password = TestUtils.renderIntoDocument(<Password
+      upperCase={true}
+      lowerCase={true}
+      special={true}
+      number={true}
+      over6={true}
+      />
+    )
+
+    let rules = TestUtils.scryRenderedDOMComponentsWithTag(password, 'li')
+    expect(rules.length).toBe(5)
+    expect(fD(rules[0]).textContent).toEqual('Must have at least one upper-case character')
+
+    let generateButton = TestUtils.findRenderedDOMComponentWithClass(password, 'generate-btn')
+    expect(fD(rules[1]).firstChild.nodeName.toLowerCase()).toBe('#text')
+    TestUtils.Simulate.click(fD(generateButton))
+    expect(fD(rules[1]).firstChild.nodeName.toLowerCase()).toBe('strike')
+
+    done()
+  })
+})
