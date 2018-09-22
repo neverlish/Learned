9819c0ce400a0f20860d73972221ef8d25cbf23f
Jinho Hyeon <neverlish@gmail.com>
2018-09-22 10:24:50 +0900

    13 - 3 - 3 URL 매개변수와 데이터 전달

--- react/gilbut-react-quickly/13/router/jsx/post.jsx
+++ react/gilbut-react-quickly/13/router/jsx/post.jsx
@@ -1,8 +1,12 @@
 const React = require('react')
 
 module.exports = function Product(props) {
+  let post = props.route.posts.find(element=>element.slug === props.params.id)
   return (
     <div>
+      <h3>{post.title}</h3>
+      <p>{post.text}</p>
+      <p><a href={post.link} target="_blank">Continue reading...</a></p>
     </div>
   )
 }
