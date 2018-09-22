8fea8c43d8dcf01af939c2698c0dd9a0d38a1318
Jinho Hyeon <neverlish@gmail.com>
2018-09-22 10:23:00 +0900

    13 - 3 - 2 프로그래밍적으로 페이지 이동하기

--- react/gilbut-react-quickly/13/router/jsx/contact.jsx
+++ react/gilbut-react-quickly/13/router/jsx/contact.jsx
@@ -1,6 +1,7 @@
 const React = require('react')
 
 module.exports = function Contact(props) {
+  setTimeout(()=>{props.router.push('about')}, 1000)
   return <div>
     <h3>Contact Us</h3>
     <input type="text" placeholder="your email" className="form-control"></input>
