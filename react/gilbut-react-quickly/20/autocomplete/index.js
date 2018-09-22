bbe56349a2becb2b68088178c52c41b206487def
Jinho Hyeon <neverlish@gmail.com>
2018-09-22 21:57:08 +0900

    20 - 2 - 1 RESTful API 정의하기

--- react/gilbut-react-quickly/20/autocomplete/index.js
+++ react/gilbut-react-quickly/20/autocomplete/index.js
@@ -18,3 +18,40 @@ require('babel-register')({
 
 const Autocomplete  = React.createFactory(require('./src/autocomplete.jsx')),
       port = 3000
+
+mongodb.MongoClient.connect(url, function(err, db) {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+
+  app.use(compression())
+  app.use(logger('dev'))
+  app.use(errorHandler())
+  app.use(bodyParser.json())
+  app.use(validator())
+  app.use(express.static('public'))
+  app.engine('handlebars', exphbs())
+  app.set('view engine', 'handlebars')
+
+  app.use(function(req, res, next){
+    req.rooms = db.collection('rooms')
+    return next()
+  })
+
+  app.get('/rooms', function(req, res, next) {
+    req.rooms.find({}, {sort: {_id: -1}}).toArray(function(err, docs){
+      if (err) return next(err)
+      return res.json(docs)
+    })
+  })
+  app.post('/rooms', function(req, res, next){
+    req.checkBody('name', 'Invalid name in body').notEmpty()
+    var errors = req.validationErrors()
+    if (errors) return next(errors)
+    req.rooms.insert(req.body, function (err, result) {
+      if (err) return next(err)
+      return res.json(result.ops[0])
+    })
+  })
+})
