b3cbdc909c908bc8157bdf3037ac21341c827b86
Jinho Hyeon <neverlish@gmail.com>
2018-09-23 11:07:09 +0900

    07 부분 적용 함수 - 1 단위 테스트

---
+++ js/gilbut-js-pattern-test/07/PFA/ThirdPartyRestaurantApiAspects_tests.js
@@ -0,0 +1,24 @@
+// getRestaurantsNearConference의 단위 테스트
+describe('ThirdParty.restaurantApi() 애스팩트', function () {
+  var api = ThirdParty.restaurantApi();
+
+  describe('getRestaurantsNearConference(cuisine)', function() {
+    var returnFromUnderlyingFunction = '아무거',
+        cuisine = '중화요리';
+
+    beforeEach(function() {
+      spyOn(api, 'getRestaurantsWithinRadius')
+        .and.returnValue(returnFromUnderlyingFunction);
+    });
+
+    it('올바른 인자로 getRestaurantsWithinRadius를 호출한다', function() {
+      api.getRestaurantsNearConference(cuisine);
+      expect(api.getRestaurantsWithinRadius).toHaveBeenCalledWith('울산 남구 신정로 20번길 988', 2.0, cuisine);
+    });
+
+    it('getRestaurantsWithinRadius로부터 받은 값을 반환한다', function() {
+      var ret = api.getRestaurantsNearConference(cuisine);
+      expect(ret).toBe(returnFromUnderlyingFunction);
+    });
+  });
+});
