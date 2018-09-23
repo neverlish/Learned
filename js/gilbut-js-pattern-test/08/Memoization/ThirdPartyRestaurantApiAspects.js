5f0a852b8cad78cd68ec703abf4c779cb38f8d20
Jinho Hyeon <neverlish@gmail.com>
2018-09-23 16:25:08 +0900

    08 - 2 - 2 returnValueCache 애스팩트를 restaurantApi에 적용하기

--- js/gilbut-js-pattern-test/08/Memoization/ThirdPartyRestaurantApiAspects.js
+++ js/gilbut-js-pattern-test/08/Memoization/ThirdPartyRestaurantApiAspects.js
@@ -1,13 +1,35 @@
-// getRestaurantsNearConference
+// getRestaurantsWithinRadius를 returnValueCache 애스팩트로 장식
 
-// ThirdParty.restaurantApi()에 getRestaurantnsNearConference 멤버를 추가한다.
+// getRestaurantsWithinRadius에 메모이제이션 패턴 적용
 
 Aop.around(
   // 반환값을 수정해야 할 함수
   'restaurantApi',
 
   // 반환값을 수정하는 함수
-  function addGetRestaurantnsNearConference(targetInfo) {
+  function addMemoziationToGetRestaurantsWithinRadius(targetInfo) {
+    // ThirdParty.restaurantApi()가 반환한 원본 API
+    var api = Aop.next.call(this, targetInfo);
+
+    // getRestaurantsWithinRadius 함수를 장식하여 메모이제이션 추가
+    Aop.around('getRestaurantsWithinRadius에', Aspects.returnValueCache().advice, api);
+
+    // 고친 API를 반환한다
+    return api;
+  },
+
+  // 반환값을 수정해야 할 함수의 이름공간
+  ThirdParty
+);
+
+// ThirdParty.restaurantApi()에 getRestaurantsNearConference 멤버 추가
+
+Aop.around(
+  // 반환값을 수정해야 할 함수
+  'restaurantApi',
+
+  // 반환값을 수정하는 함수
+  function addGetRestaurantsNearConference(targetInfo) {
     // ThirdParty.restaurantApi()가 반환한 원본 API
     var api = Aop.next.call(this, targetInfo);
 
@@ -19,7 +41,7 @@ Aop.around(
     // 없으면 이 함수를 추가한다
     api.getRestaurantsNearConference = api.getRestaurantsNearConference || getRestaurantsNearConference;
 
-    // 수정한 API를 반환한다
+    // 고친 API를 반환한다
     return api;
   },
 
