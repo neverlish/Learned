4c764dbad8432d1fa043618946895eab2e32dfd2
Jinho Hyeon <neverlish@gmail.com>
2018-09-24 19:26:12 +0900

    [client,server] 12 Sending, Verifying, and Refreshing JWT Tokens in Node.js

--- fullstack/yt-benawad-slackclone-graphql-react/client/src/routes/index.js
+++ fullstack/yt-benawad-slackclone-graphql-react/client/src/routes/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './Home';
+import CreateTeam from './CreateTeam';
 import Register from './Register';
 import Login from './Login';
 
@@ -11,6 +12,7 @@ export default () => (
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
+      <Route path="/create-team" exact component={CreateTeam} />
     </Switch>
   </BrowserRouter>
 );
--- fullstack/yt-benawad-slackclone-graphql-react/server/resolvers/user.js
+++ fullstack/yt-benawad-slackclone-graphql-react/server/resolvers/user.js
@@ -1,14 +1,5 @@
-import bcrypt from 'bcrypt';
-import * as _ from 'lodash';
-
 import { tryLogin } from '../auth';
-
-const formatErrors = (e, models) => {
-  if (e instanceof models.sequelize.ValidationError) {
-    return e.errors.map(x => _.pick(x, ['path', 'message']));
-  }
-  return [{ path: 'name', message: '' }];
-}
+import formatErrors from '../formatErrors';
 
 export default {
   Query: {
