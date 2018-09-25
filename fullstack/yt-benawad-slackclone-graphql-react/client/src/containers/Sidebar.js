e8a3f03c9c284cda863d9d7bf7d3ae2777f269e5
Jinho Hyeon <neverlish@gmail.com>
2018-09-25 15:18:11 +0900

    [client] 23 Sharing Data Across React Components by Lifting A GraphQL Query

--- fullstack/yt-benawad-slackclone-graphql-react/client/src/containers/Sidebar.js
+++ fullstack/yt-benawad-slackclone-graphql-react/client/src/containers/Sidebar.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { graphql } from 'react-apollo';
-import findIndex from 'lodash/findIndex';
 import decode from 'jwt-decode';
 
 import Channels from '../components/Channels';
 import Teams from '../components/Teams';
 import AddChannelModal from '../components/AddChannelModal';
-import { allTeamsQuery } from '../graphql/teams';
 
-class Sidebar extends React.Component {
+export default class Sidebar extends React.Component {
   state = {
     openAddChannelModal: false,
   };
@@ -22,15 +19,9 @@ class Sidebar extends React.Component {
   }
 
   render() {
-    const { data: { loading, allTeams }, currentTeamId } = this.props;
-    if (loading) {
-      return null;
-    }
+    const { teams, team } = this.props;
 
-    const teamIdx = currentTeamId ? findIndex(allTeams, ['id', parseInt(currentTeamId, 10)]) : 0;
-    const team = allTeams[teamIdx];
     let username = '';
-
     try {
       const token = localStorage.getItem('token');
       const { user } = decode(token);
@@ -41,13 +32,7 @@ class Sidebar extends React.Component {
     }
 
     return [
-      <Teams
-        key="team-sidebar"
-        teams={allTeams.map(t => ({
-          id: t.id,
-          letter: t.name.charAt(0).toUpperCase(),
-        }))}
-      />,
+      <Teams key="team-sidebar" teams={teams} />,
       <Channels
         key="channels-sidebar"
         teamName={team.name}
@@ -66,5 +51,3 @@ class Sidebar extends React.Component {
     ];
   }
 }
-
-export default graphql(allTeamsQuery)(Sidebar);
