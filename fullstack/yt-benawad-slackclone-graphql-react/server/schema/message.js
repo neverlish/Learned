ec47d89ad36a4c2c6b3e36294086cf4418706f85
Jinho Hyeon <neverlish@gmail.com>
2018-09-27 00:09:24 +0900

    [client,server] 31 Real Time Messaging in React with GraphQL Subscriptions

--- fullstack/yt-benawad-slackclone-graphql-react/server/schema/message.js
+++ fullstack/yt-benawad-slackclone-graphql-react/server/schema/message.js
@@ -7,6 +7,10 @@ export default `
     created_at: String!
   }
 
+  type Subscription {
+    newChannelMessage(channelId: Int!): Message!
+  }
+
   type Query {
     messages(channelId: Int!): [Message!]!
   }
