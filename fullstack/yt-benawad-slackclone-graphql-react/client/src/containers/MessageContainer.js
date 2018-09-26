a01263b2e355da2da463a40ab1bb4e8c676566af
Jinho Hyeon <neverlish@gmail.com>
2018-09-26 16:11:23 +0900

    [client,server] 29 Semantic UI React Comment Component

--- fullstack/yt-benawad-slackclone-graphql-react/client/src/containers/MessageContainer.js
+++ fullstack/yt-benawad-slackclone-graphql-react/client/src/containers/MessageContainer.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Comment } from 'semantic-ui-react';
+
 import Messages from '../components/Messages';
 
 const MessageContainer = ({ data: { loading, messages } }) => (loading
   ? null
   : (
     <Messages>
-      {JSON.stringify(messages)}
+      <Comment.Group>
+        {messages.map(m => (
+          <Comment key={`${m.id}-message`}>
+            <Comment.Content>
+              <Comment.Author as="a">{m.user.username}</Comment.Author>
+              <Comment.Metadata>
+                <div>{m.created_at}</div>
+              </Comment.Metadata>
+              <Comment.Text>{m.text}</Comment.Text>
+              <Comment.Actions>
+                <Comment.Action>Reply</Comment.Action>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+        ))}
+      </Comment.Group>
     </Messages>
   )
 );
@@ -20,7 +37,7 @@ const messagesQuery = gql`
       user {
         username
       }
-      createdAt
+      created_at
     }
   }
 `;
