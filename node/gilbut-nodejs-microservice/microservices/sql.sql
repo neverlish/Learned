3eb5633e068291ddc7f481a76b7ebe812c3e4a2a
Jinho Hyeon <neverlish@gmail.com>
2018-09-30 18:34:24 +0900

    04 - 4 - 2 회원 관리 테이블 설계

--- node/gilbut-nodejs-microservice/microservices/sql.sql
+++ node/gilbut-nodejs-microservice/microservices/sql.sql
@@ -11,3 +11,12 @@ CREATE TABLE IF NOT EXISTS `goods` (
   `description` text NOT NULL,
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 회원 관리 테이블 생성 쿼리
+CREATE TABLE IF NOT EXISTS `members` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  PRIMARY KEY(`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
