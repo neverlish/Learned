c826cc01f028f071bc69e5c0674acb8618d86eb1
Jinho Hyeon <neverlish@gmail.com>
2018-09-30 18:35:39 +0900

    04 - 4 - 3 구매 관리 테이블 설계

--- node/gilbut-nodejs-microservice/microservices/sql.sql
+++ node/gilbut-nodejs-microservice/microservices/sql.sql
@@ -20,3 +20,18 @@ CREATE TABLE IF NOT EXISTS `members` (
   PRIMARY KEY(`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 구매 관리 테이블 생성 쿼리
+CREATE TABLE IF NOT EXISTS `purchases` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userid` int NOT NULL,
+  `goodsid` int NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create user 'micro'@'%' identified by 'service';
+grant all privileges on monolithic.* to 'micro'@'%';
+flush privileges;
+
+SELECT User FROM mysql.user WHERE User='micro';
