8cfb07489ea720e5e283fdffdb2dbfde84cfe4e6
Jinho Hyeon <neverlish@gmail.com>
2018-09-30 19:01:21 +0900

    05 - 5 기능 만들기 - 1 상품 관리 기능 만들기

--- node/gilbut-nodejs-microservice/microservices/monolithic_goods.js
+++ node/gilbut-nodejs-microservice/microservices/monolithic_goods.js
@@ -1,3 +1,11 @@
+const mysql = require('mysql');
+const conn = {
+  host: 'localhost',
+  user: 'micro',
+  password: 'service',
+  database: 'monolithic'
+};
+
 exports.onRequest = function(res, method, pathname, params, cb) {
   switch (method) {
     case 'POST':
@@ -14,5 +22,87 @@ exports.onRequest = function(res, method, pathname, params, cb) {
       });
     default:
       return process.nextTick(cb, res, null);
+  }
+}
+
+function register(method, pathname, params, cb) {
+  var response = {
+    key: params.key,
+    errorcode: 0,
+    errormessage: 'success'
+  };
+
+  if (params.name == null || params.category == null || params.price == null | params.description == null) {
+    response.errorcode = 1;
+    response.errormessage = 'Invalid Parameters';
+    cb(response);
+  } else {
+    var connection = mysql.createConnection(conn);
+    connection.connect();
+    connection.query(
+      'insert into goods(name, category, price, description) values(?, ?, ?, ?)',
+      [params.name, params.category, params.price, params.description],
+      (error, results, fields) => {
+        if (error) {
+          response.errorcode = 1;
+          response.errormessage = error;
+        }
+        cb(response);
+      }
+    );
+    connection.end();
+  }
+}
+
+function inquiry(method, pathname, params, cb) {
+  var response = {
+    key: params.key,
+    errorcode: 0,
+    errormessage: 'success'
+  };
+
+  var connection = mysql.createConnection(conn);
+  connection.connect();
+  connection.query(
+    'select * from goods',
+    (error, results, fields) => {
+      if (error || results.length == 0) {
+        response.errorcode = 1;
+        response.errormessage = error ? error : 'no data';
+      } else {
+        response.results = results;
+      }
+      cb(response);
+    }
+  );
+  connection.end();
+}
+
+function unregister(method, pathname, params, cb) {
+  var response = {
+    key: params.key,
+    errorcode: 0,
+    errormessage: 'success'
+  };
+
+  if (params.id == null) {
+    response.errorcode = 1;
+    response.errormessage = 'Invalid Parameters';
+    cb(response);   
+  } else {
+    var connection = mysql.createConnection(conn);
+    connection.connect();
+    connection.query(
+      'delete from goods where id = ?',
+      [params.id],
+      (error, results, fields) => {
+        if (error) {
+          response.errorcode = 1;
+          response.errormessage = error;
+        }
+        cb(response);
+      }
+    );
+    connection.end();
   }
 }
