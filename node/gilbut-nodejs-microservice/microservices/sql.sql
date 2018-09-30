867aba625159022ceaa37a47321f7acb77263e9f
Jinho Hyeon <neverlish@gmail.com>
2018-09-30 18:32:49 +0900

    04 - 4 - 1 상품 관리 테이블 설계

--- node/gilbut-nodejs-microservice/microservices/sql.sql
+++ node/gilbut-nodejs-microservice/microservices/sql.sql
@@ -1,3 +1,13 @@
 CREATE DATABASE monolithic;
 
 use monolithic;
+
+-- 상품 관리 테이블 생성 쿼리
+CREATE TABLE IF NOT EXISTS `goods` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `category` varchar(128) NOT NULL,
+  `price` int NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
