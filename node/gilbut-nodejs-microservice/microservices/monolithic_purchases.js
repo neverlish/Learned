911a39ed8d42b8accb85193e9e18dcb59b17893a
Jinho Hyeon <neverlish@gmail.com>
2018-09-30 19:17:57 +0900

    05 - 5 - 3 구매 관리 기능 만들기

--- node/gilbut-nodejs-microservice/microservices/monolithic_purchases.js
+++ node/gilbut-nodejs-microservice/microservices/monolithic_purchases.js
@@ -1,3 +1,11 @@
+const mysql = require('mysql');
+const conn = {
+  host: 'localhost',
+  user: 'micro',
+  password: 'service',
+  database: 'monolithic'
+};
+
 exports.onRequest = function(res, method, pathname, params, cb) {
   switch (method) {
     case 'POST':
@@ -8,7 +16,71 @@ exports.onRequest = function(res, method, pathname, params, cb) {
       return inquiry(method, pathname, params, (response) => {
         process.nextTick(cb, res, response);
       });
+    case 'DELETE':
+      return unregister(method, pathname, params, (response) => {
+        process.nextTick(cb, res, response);
+      });
     default:
       return process.nextTick(cb, res, null);
+  }
+}
+
+function register(method, pathname, params, cb) {
+  var response = {
+    key: params.key,
+    errorcode: 0,
+    errormessage: 'success'
+  };
+
+  if (params.userid == null || params.goodsid == null) {
+    response.errorcode = 1;
+    response.errormessage = 'Invalid Parameters';
+    cb(response);
+  } else {
+    var connection = mysql.createConnection(conn);
+    connection.connect();
+    connection.query(
+      'insert into purchases(userid, goodsid) values(?, ?)',
+      [params.userid, params.goodsid],
+      (error, results, fields) => {
+        if (error) {
+          response.errorcode = 1;
+          response.errormessage = error;
+        }
+        cb(response);
+      }
+    );
+    connection.end();
+  }
+}
+
+function inquiry(method, pathname, params, cb) {
+  var response = {
+    key: params.key,
+    errorcode: 0,
+    errormessage: 'success'
+  };
+
+  if (params.userid == null) {
+    response.errorcode = 1;
+    response.errormessage = 'Invalid Parameters';
+    cb(response);
+  } else {
+    var connection = mysql.createConnection(conn);
+    connection.connect();
+    connection.query(
+      'select id, goodsid, date from purchases where userid = ?',
+      [params.userid],
+      (error, results, fields) => {
+        if (error || results.length == 0) {
+          response.errorcode = 1;
+          response.errormessage = error ? error : 'no data';
+        } else {
+          response.results = results;
+        }
+        cb(response);
+      }
+    );
+    connection.end();
   }
 }
