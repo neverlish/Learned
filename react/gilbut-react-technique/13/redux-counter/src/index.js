e7ec27b00a9d98805c2d5689a588cab46fc3ad26
Jinho Hyeon <neverlish@gmail.com>
2018-10-03 21:21:58 +0900

    13 - 1 - 7 스토어 생성

--- react/gilbut-react-technique/13/redux-counter/src/index.js
+++ react/gilbut-react-technique/13/redux-counter/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+
 ReactDOM.render(
   <App />, 
   document.getElementById('root')
