525b41c29656f7f206ba9036d3ad6b8353eb3e10
Jinho Hyeon <neverlish@gmail.com>
2018-10-03 21:50:28 +0900

    13 - 2 - 3 Reducers 수정

--- react/gilbut-react-technique/13/redux-counter/src/reducers/index.js
+++ react/gilbut-react-technique/13/redux-counter/src/reducers/index.js
@@ -1,10 +1,67 @@
-import number from './number';
-import color from './color';
+import * as types from '../actions/ActionTypes';
 
-import { combineReducers } from 'redux';
-const reducers = combineReducers({
-  numberData: number,
-  colorData: color
-});
+const initialState = {
+  counters: [
+    {
+      color: 'black',
+      number: 0
+    }
+  ]
+};
 
-export default reducers;
+function counter(state = initialState, action) {
+  const { counters } = state;
+  switch(action.type) {
+    case types.CREATE:
+      return {
+        counters: [
+          ...counters,
+          {
+            color: action.color,
+            number: 0
+          }
+        ]
+      };
+    case types.REMOVE:
+      return {
+        counters: counters.slice(0, counters.length - 1)
+      };
+    case types.INCREMENT:
+      return {
+        counters: [
+          ...counters.slice(0, action.index),
+          {
+            ...counters[action.index],
+            number: counters[action.index].number + 1
+          },
+          ...counters.slice(action.index + 1, counters.length)
+        ]
+      };
+    case types.DECREMENT:
+      return {
+        counters: [
+          ...counters.slice(0, action.index),
+          {
+            ...counters[action.index],
+            number: counters[action.index].number - 1
+          },
+          ...counters.slice(action.index + 1, counters.length)
+        ]
+      };
+    case types.SET_COLOR:
+      return {
+        counters: [
+          ...counters.slice(0, action.index),
+          {
+            ...counters[action.index],
+            color: action.color
+          },
+          ...counters.slice(action.index + 1, counters.length)
+        ]
+      };
+    default:
+      return state;
+  }
+}
+
+export default counter;
