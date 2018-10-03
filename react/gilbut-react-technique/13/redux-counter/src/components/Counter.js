74bf665df641745e297d12fd5e077b98853f0090
Jinho Hyeon <neverlish@gmail.com>
2018-10-03 21:56:58 +0900

    13 - 2 - 4 프리젠테이셔널 컴포넌트 생성

--- react/gilbut-react-technique/13/redux-counter/src/components/Counter.js
+++ react/gilbut-react-technique/13/redux-counter/src/components/Counter.js
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Counter.css';
 
-const Counter = ({ number, color, onIncrement, onDecrement, onSetColor }) => {
+const Counter = ({ number, color, index, onIncrement, onDecrement, onSetColor }) => {
   return (
     <div
       className="Counter"
-      onClick={onIncrement}
+      onClick={() => onIncrement(index)}
       onContextMenu={(e) => {
         e.preventDefault();
-        onDecrement();
+        onDecrement(index);
       }}
-      onDoubleClick={onSetColor}
+      onDoubleClick={() => onSetColor(index)}
       style={{
         backgroundColor: color
       }}
@@ -22,6 +22,7 @@ const Counter = ({ number, color, onIncrement, onDecrement, onSetColor }) => {
 };
 
 Counter.propTypes = {
+  index: PropTypes.number,
   number: PropTypes.number,
   color: PropTypes.string,
   onIncrement: PropTypes.func,
@@ -30,6 +31,7 @@ Counter.propTypes = {
 };
 
 Counter.defaultProps = {
+  index: 0,
   number: 0,
   color: 'black',
   onIncrement: () => console.warn('onIncrement not defined'),
