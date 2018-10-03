f779bbd5207e444e9baa7c23dce9935382440e67
Jinho Hyeon <neverlish@gmail.com>
2018-10-03 21:26:47 +0900

    13 - 1 - 9 CounterContainer 컴포넌트 생성

---
+++ react/gilbut-react-technique/13/redux-counter/src/containers/CounterContainer.js
@@ -0,0 +1,46 @@
+import Counter from '../components/Counter';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
+
+export function getRandomColor() {
+  const colors = [
+    '#495057',
+    '#f03e3e',
+    '#d6336c',
+    '#ae3ec9',
+    '#7048e8',
+    '#4263eb',
+    '#1c7cd6',
+    '#1098ad',
+    '#0ca678',
+    '#37b24d',
+    '#74b816',
+    '#f59f00',
+    '#f76707'
+  ];
+
+  const random = Math.floor(Math.random() * 13);
+
+  return colors[random];
+}
+
+const mapStateToProps = (state) => ({
+  color: state.color,
+  number: state.number
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onIncrement: () => dispatch(actions.increment()),
+  onDecrement: () => dispatch(actions.decrement()),
+  onSetColor: () => {
+    const color = getRandomColor();
+    dispatch(actions.setColor(color));
+  }
+});
+
+const CounterContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
+
+export default CounterContainer;
