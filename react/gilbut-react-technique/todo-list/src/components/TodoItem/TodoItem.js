c42cbf7141c6125c3f62a8f377b426b229212de6
Jinho Hyeon <neverlish@gmail.com>
2018-10-03 20:35:06 +0900

    11 - 2 - 2 TodoItem 컴포넌트 최적화

--- react/gilbut-react-technique/todo-list/src/components/TodoItem/TodoItem.js
+++ react/gilbut-react-technique/todo-list/src/components/TodoItem/TodoItem.js
@@ -5,6 +5,10 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 class TodoItem extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.done !== nextProps.done;
+  }
+
   render() {
     const { done, children, onToggle, onRemove } = this.props;
 
