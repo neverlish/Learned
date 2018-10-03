69229603e1de13f1c51ee163ef386b03fe06ea8a
Jinho Hyeon <neverlish@gmail.com>
2018-10-03 21:44:29 +0900

    13 - 2 - 2 Actions 수정

--- react/gilbut-react-technique/13/redux-counter/src/actions/index.js
+++ react/gilbut-react-technique/13/redux-counter/src/actions/index.js
@@ -1,14 +1,26 @@
 import * as types from './ActionTypes';
 
-export const increment = () => ({
-  type: types.INCREMENT
+export const create = (color) => ({
+  type: types.CREATE,
+  color
 });
 
-export const decrement = () => ({
-  type: types.DECREMENT
+export const remove = () => ({
+  type: types.REMOVE
 });
 
-export const setColor = (color) => ({
+export const increment = (index) => ({
+  type: types.INCREMENT,
+  index
+});
+
+export const decrement = (index) => ({
+  type: types.DECREMENT,
+  index
+});
+
+export const setColor = ({ index, color }) => ({
   type: types.SET_COLOR,
+  index,
   color
 });
