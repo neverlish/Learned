a099c6d660c1fc7b70fc13b43bfafe056cb9e71b
Jinho Hyeon <neverlish@gmail.com>
2018-10-03 20:32:26 +0900

    11 컴포넌트 리렌더링 최적화 - 1 문제점 찾기 - 2 데이터 준비

--- react/gilbut-react-technique/todo-list/src/components/App.js
+++ react/gilbut-react-technique/todo-list/src/components/App.js
@@ -3,13 +3,14 @@ import PageTemplate from './PageTemplate';
 import TodoInput from './TodoInput';
 import TodoList from './TodoList'; 
 
+const initialTodos = new Array(500).fill(0).map(
+  (foo, index) => ({ id: index, text: `일정 ${index}`, done: false })
+)
+
 class App extends Component {
   state = {
     input: '',
-    todos: [
-      { id: 0, text: '리액트 공부하기', done: true },
-      { id: 1, text: '컴포넌트 스타일링 해보기', done: false }
-    ]
+    todos: initialTodos
   }
 
   id = 1
