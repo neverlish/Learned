59b63226e4520748b8f45383c42ee45d491fbf7e
Jinho Hyeon <neverlish@gmail.com>
2018-10-08 18:26:10 +0900

    14 - 4 - 3 스토어 생성 및 설정

--- react/gilbut-react-technique/todo-list/src/index.js
+++ react/gilbut-react-technique/todo-list/src/index.js
@@ -4,5 +4,16 @@ import './styles/main.scss';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import modules from './modules';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(modules, window.devToolsExtension && window.devToolsExtension());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
