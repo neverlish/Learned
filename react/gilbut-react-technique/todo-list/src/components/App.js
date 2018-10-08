a16fffe66e599b1e612ced83f569a4d58f2dba7b
Jinho Hyeon <neverlish@gmail.com>
2018-10-08 21:20:36 +0900

    14 - 4 - 5 마무리

--- react/gilbut-react-technique/todo-list/src/components/App.js
+++ react/gilbut-react-technique/todo-list/src/components/App.js
@@ -1,89 +1,17 @@
 import React, { Component } from 'react';
 import PageTemplate from './PageTemplate';
-import TodoInput from './TodoInput';
-import TodoList from './TodoList'; 
 
-const initialTodos = new Array(500).fill(0).map(
-  (foo, index) => ({ id: index, text: `일정 ${index}`, done: false })
-)
+import TodoInputContainer from '../containers/TodoInputContainer';
+import TodoListContainer from '../containers/TodoListContainer';
 
 class App extends Component {
-  state = {
-    input: '',
-    todos: initialTodos
-  }
-
-  id = 1
-  getId = () => {
-    return ++this.id;
-  }
-
-  handleChange = (e) => {
-    const { value } = e.target;
-    this.setState({
-      input: value
-    });
-  }
-
-  handleInsert = () => {
-    const { todos, input } = this.state;
-
-    const newTodo = {
-      text: input,
-      done: false,
-      id: this.getId()
-    };
-
-    this.setState({
-      todos: [...todos, newTodo],
-      input: ''
-    });
-  }
-
-  handleToggle = (id) => {
-    const { todos } = this.state;
-    const index = todos.findIndex(todo => todo.id === id);
-
-    const toggled = {
-      ...todos[index],
-      done: !todos[index].done
-    };
-
-    this.setState({
-      todos: [
-        ...todos.slice(0, index),
-        toggled,
-        ...todos.slice(index + 1, todos.length)
-      ]
-    });
-  }
-
-  handleRemove = (id) => {
-    const { todos } = this.state;
-    const index = todos.findIndex(todo => todo.id === id);
-
-    this.setState({
-      todos: [
-        ...todos.slice(0, index),
-        ...todos.slice(index + 1, todos.length)
-      ]
-    });
-  }
-
   render() {
-    const { input, todos } = this.state;
-    const {
-      handleChange,
-      handleInsert,
-      handleToggle,
-      handleRemove
-    } = this;
     return (
       <PageTemplate>
-        <TodoInput onChange={handleChange} onInsert={handleInsert} value={input} />
-        <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove} />
+        <TodoInputContainer />
+        <TodoListContainer />
       </PageTemplate>
-    );
+    )
   }
 }
 
--- react/gilbut-react-technique/todo-list/src/components/TodoList/TodoList.js
+++ react/gilbut-react-technique/todo-list/src/components/TodoList/TodoList.js
@@ -11,11 +11,11 @@ class TodoList extends Component {
     const todoList = todos.map(
       todo => (
         <TodoItem
-          key={todo.id}
-          done={todo.done}
-          onToggle={() => onToggle(todo.id)}
-          onRemove={() => onRemove(todo.id)}>
-          {todo.text}
+          key={todo.get('id')}
+          done={todo.get('done')}
+          onToggle={() => onToggle(todo.get('id'))}
+          onRemove={() => onRemove(todo.get('id'))}>
+          {todo.get('text')}
         </TodoItem>
       )
     )
