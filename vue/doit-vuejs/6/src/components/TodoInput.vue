60d1a5280e1a80e4465eb5b618a4967f16f85bef
Jinho Hyeon <neverlish@gmail.com>
2018-10-09 01:32:45 +0900

    6 - 4 컴포넌트 내용 구현하기

--- vue/doit-vuejs/6/src/components/TodoInput.vue
+++ vue/doit-vuejs/6/src/components/TodoInput.vue
@@ -1,13 +1,61 @@
 <template>
-  <div>input</div>
+  <div class='inputBox shadow'>
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter='addTodo'>
+    <span class="addContainer" v-on:click='addTodo'>
+      <i class="addBtn fas fa-plus" area-hidden='true'></i>
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem !== '') {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(value, value);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+  input:focus {
+    outline: none;
+  }
 
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
 </style>
--- vue/doit-vuejs/6/src/components/TodoList.vue
+++ vue/doit-vuejs/6/src/components/TodoList.vue
@@ -1,13 +1,67 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for='(todoItem, index) in todoItems' class='shadow'>
+        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span class="removeBtn" type='button' @click='removeTodo(todoItem, index)'>
+          <i class="far fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  methods: {
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
 
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
 </style>
