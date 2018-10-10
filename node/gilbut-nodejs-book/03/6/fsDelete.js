58436f6254f8b183d8d69491585bcd8bba581f71
Jinho Hyeon <neverlish@gmail.com>
2018-10-10 19:07:45 +0900

    03 - 6 - 3 기타 fs 메서드

---
+++ node/gilbut-nodejs-book/03/6/fsDelete.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+fs.readdir('./folder', (err, dir) => {
+  if (err) {
+    throw err;
+  }
+  console.log('폴더 내용 확인', dir);
+  fs.unlink('./folder/newfile.js', (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('파일 삭제 성공');
+    fs.rmdir('./folder', (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log('폴더 삭제 성공');
+    });
+  });
+});
+
+/*
+폴더 내용 확인 [ 'newfile.js' ]
+파일 삭제 성공
+폴더 삭제 성공
+*/
