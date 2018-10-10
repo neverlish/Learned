586046a922a6fc1a97110681f1f9dfd91e928d80
Jinho Hyeon <neverlish@gmail.com>
2018-10-10 18:39:55 +0900

    03 - 6 - 파일 시스템 접근하기

---
+++ node/gilbut-nodejs-book/03/6/readFile.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+fs.readFile('./readme.txt', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log(data);
+  console.log(data.toString());
+});
+
+/*
+<Buffer ec a0 80 eb a5 bc 20 ec 9d bd ec 96 b4 ec a3 bc ec 84 b8 ec 9a 94 2e 0a>
+저를 읽어주세요.
+*/
