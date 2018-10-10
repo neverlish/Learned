21d5bca8a498535fe37a9476313af5c491667820
Jinho Hyeon <neverlish@gmail.com>
2018-10-11 00:47:50 +0900

    06 - 3 - 5 static

--- node/gilbut-nodejs-book/06/learn-express/app.js
+++ node/gilbut-nodejs-book/06/learn-express/app.js
@@ -19,10 +19,10 @@ app.use(function(req, res, next) {
 });
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
