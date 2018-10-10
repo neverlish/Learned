e2d2acd1a26f4c8d3ceaf9873a945ece54fe395c
Jinho Hyeon <neverlish@gmail.com>
2018-10-11 00:53:06 +0900

    06 - 3 - 7 connect-flash

--- node/gilbut-nodejs-book/06/learn-express/app.js
+++ node/gilbut-nodejs-book/06/learn-express/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -33,6 +34,7 @@ app.use(session({
     secure: false
   },
 }));
+app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- node/gilbut-nodejs-book/06/learn-express/routes/users.js
+++ node/gilbut-nodejs-book/06/learn-express/routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/flash', function(req, res) {
+  req.session.message = '세션 메시지';
+  req.flash('message', 'flash 메시지');
+  res.redirect('/users/flash/result');
+});
+
+router.get('/flash/result', function(req, res) {
+  res.send(`${req.session.message} ${req.flash('message')}`);
+});
+
 module.exports = router;
