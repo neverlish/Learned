89f57345cdc6df89b1aae9485f91d875fb67daf6
Jinho Hyeon <neverlish@gmail.com>
2018-10-11 00:47:37 +0900

    06 - 3 미들웨어 - 1 커스텀 미들웨어 만들기

--- node/gilbut-nodejs-book/06/learn-express/app.js
+++ node/gilbut-nodejs-book/06/learn-express/app.js
@@ -13,6 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(function(req, res, next) {
+  console.log(req.url, '저도 미들웨어입니다.');
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
