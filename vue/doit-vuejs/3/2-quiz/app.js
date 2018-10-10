06056319542f7f30d29c047ceafccea10bbdf679
Jinho Hyeon <neverlish@gmail.com>
2018-10-10 23:25:25 +0900

    3 - 2 연습문제

---
+++ vue/doit-vuejs/3/2-quiz/app.js
@@ -0,0 +1,20 @@
+// 실습 #1 - `todo-footer` 컴포넌트 전역 등록
+// <p>This is another child global component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+Vue.component('todo-footer', {
+  template: '<p>This is another child global component</p>'
+});
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message : 'This is a parent component'
+  },
+
+  // 실습 #2 - `todo-list` 컴포넌트 지역 등록
+  // <p>This is another child local component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+  components: {
+    'todo-list': {
+      template: '<p>This is another child local component</p>'
+    }
+  }
+});
