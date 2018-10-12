ad39dc57d408cf84d8dae2dae73bdb75d7ee82ca
Jinho Hyeon <neverlish@gmail.com>
2018-10-12 23:41:25 +0900

    07 - 6 - 3 관계 정의하기

--- node/gilbut-nodejs-book/07/learn-sequelize/models/index.js
+++ node/gilbut-nodejs-book/07/learn-sequelize/models/index.js
@@ -13,4 +13,7 @@ db.Sequelize = Sequelize;
 db.User = require('./user')(sequelize, Sequelize);
 db.Comment = require('./comment')(sequelize, Sequelize);
 
+db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' });
+db.Comment.belongsTo(db.User, { foreignKey: 'commenter', targetKey: 'id' });
+
 module.exports = db;
