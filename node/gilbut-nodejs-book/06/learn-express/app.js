d7dd529231e888b4e747ebcd06effd8d554014d7
Jinho Hyeon <neverlish@gmail.com>
2018-10-12 22:21:34 +0900

    06 - 5 템플릿 엔진 사용하기 - 2 EJS

--- node/gilbut-nodejs-book/06/learn-express/app.js
+++ node/gilbut-nodejs-book/06/learn-express/app.js
@@ -13,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(function(req, res, next) {
   console.log(req.url, '저도 미들웨어입니다.');
