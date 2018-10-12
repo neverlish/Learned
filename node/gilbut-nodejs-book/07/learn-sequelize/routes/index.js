ecb44edee9d44063eb8542f4015a2a9fb6b84759
Jinho Hyeon <neverlish@gmail.com>
2018-10-12 23:42:02 +0900

    07 - 6 - 5 쿼리 수행하기

--- node/gilbut-nodejs-book/07/learn-sequelize/routes/index.js
+++ node/gilbut-nodejs-book/07/learn-sequelize/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var User = require('../models').User;
+
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  User.findAll()
+    .then((users) => {
+      res.render('sequelize', { users });
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    });
 });
 
 module.exports = router;
--- node/gilbut-nodejs-book/07/learn-sequelize/routes/users.js
+++ node/gilbut-nodejs-book/07/learn-sequelize/routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var User = require('../models').User;
+
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.findAll()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    });
+});
+
+router.post('/', function(req, res, next) {
+  User.create({
+    name: req.body.name,
+    age: req.body.age,
+    married: req.body.married,
+  })
+    .then((result) => {
+      console.log(result);
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    });
 });
 
 module.exports = router;
