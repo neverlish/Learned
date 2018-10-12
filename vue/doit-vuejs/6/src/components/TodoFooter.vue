b9a4b852375aacd35be076446580cd5a254cd7dd
Jinho Hyeon <neverlish@gmail.com>
2018-10-12 10:05:58 +0900

    [vue] Do it! Vue.js 입문

---
+++ vue/doit-vuejs/6/src/components/TodoFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='clearAllContainer'>
+    <span class="clearAllBtn" @click='clearTodo'>Clear All</span>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clearTodo() {
+      this.$emit('clearAll');
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+  }
+
+  .clearAllBtn {
+    color: #e20303;
+    display: black;
+  }
+</style>
