4610a092b672e3de3faed3f431cc5a86f2a85d08
Jinho Hyeon <neverlish@gmail.com>
2018-10-13 16:10:41 +0900

    08 - 6 - 1 몽고디비 연결하기

--- node/gilbut-nodejs-book/08/learn-mongoose/app.js
+++ node/gilbut-nodejs-book/08/learn-mongoose/app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var connect = require('./schemas');
 
 var app = express();
+connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ node/gilbut-nodejs-book/08/learn-mongoose/schemas/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+  const connect = () => {
+    if (process.env.NODE_ENV !== 'production') {
+      mongoose.set('debug', true);
+    }
+    mongoose.connect('mongodb://NAME:PASSWORD@localhost:27017/admin', {
+      dbName: 'nodejs'
+    }, (error) => {
+      if (error) {
+        console.log('몽고디비 연결 에러', error);
+      } else {
+        console.log('몽고디비 연결 성공');
+      }
+    });
+  };
+
+  mongoose.connection.on('error', (error) => {
+    console.error('몽고디비 연결 에러', error);
+  });
+  mongoose.connection.on('disconnected', () => {
+    console.error('몽고디비 연결이 끊어졌습니다. 연결을 재시도합니다.');
+    connect();
+  });
+  require('./user');
+  require('./comment');
+}
