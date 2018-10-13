a632019e87ef526c40d707e03741704db9e7bf04
Jinho Hyeon <neverlish@gmail.com>
2018-10-14 01:32:59 +0900

    09 - 4 Multer 모듈로 이미지 업로드 구현하기

--- node/gilbut-nodejs-book/nodebird/routes/page.js
+++ node/gilbut-nodejs-book/nodebird/routes/page.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User } = require('../models');
 
 const router = express.Router();
 
@@ -16,12 +17,25 @@ router.get('/join', isNotLoggedIn, (req, res) => {
 });
 
 router.get('/', (req, res, next) => {
-  res.render('main', {
-    title: 'NodeBird',
-    twits: [],
-    user: req.user,
-    loginError: req.flash('loginError'),
-  });
+  Post.findAll({
+    include: {
+      model: User,
+      attributes: ['id', 'nick'],
+    },
+    order: [['createdAt', 'DESC']],
+  })
+    .then((posts) => {
+      res.render('main', {
+        title: 'NodeBird',
+        twits: posts,
+        user: req.user,
+        loginError: req.flash('loginError'),
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      next(error);
+    })
 });
 
 module.exports = router;
