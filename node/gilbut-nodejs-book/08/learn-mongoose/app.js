1e15cad0b3f9b66b170dadd771c81401216bcf4a
Jinho Hyeon <neverlish@gmail.com>
2018-10-13 16:21:28 +0900

    08 - 6 - 3 쿼리 수행하기

--- node/gilbut-nodejs-book/08/learn-mongoose/app.js
+++ node/gilbut-nodejs-book/08/learn-mongoose/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var commentsRouter = require('./routes/comments');
 var connect = require('./schemas');
 
 var app = express();
@@ -16,13 +17,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- node/gilbut-nodejs-book/08/learn-mongoose/routes/index.js
+++ node/gilbut-nodejs-book/08/learn-mongoose/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var User = require('../schemas/user');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  User.find({})
+    .then((users) => {
+      res.render('mongoose', { users });
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    });
 });
 
 module.exports = router;
