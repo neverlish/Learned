5e5daf3d08b7b73241d07e0f1a932995e98e2549
Jinho Hyeon <neverlish@gmail.com>
2018-10-14 22:46:44 +0900

    11 - 3 Socket.IO 사용하기

--- node/gilbut-nodejs-book/11/gif-chat/socket.js
+++ node/gilbut-nodejs-book/11/gif-chat/socket.js
@@ -1,27 +1,24 @@
-const WebSocket = require('ws');
+const SocketIO = require('socket.io');
 
 module.exports = (server) => {
-  const wss = new WebSocket.Server({ server });
+  const io = SocketIO(server, { path: '/socket.io' });
 
-  wss.on('connection', (ws, req) => {
+  io.on('connection', (socket) => {
+    const req = socket.request;
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('새로운 클라이언트 접속', ip);
-
-    ws.on('message', (message) => {
-      console.log(message);
+    console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
+    socket.on('disconnect', () => {
+      console.log('클라이언트 접속 해제', ip, socket.id);
+      clearInterval(socket.interval);
     });
-    ws.on('error', (error) => {
+    socket.on('error', (error) => {
       console.error(error);
     });
-    ws.on('close', () => {
-      console.log('클라이언트 접속 해제', ip);
-      clearInterval(ws.interval);
+    socket.on('reply', (data) => {
+      console.log(data);
     });
-    const interval = setInterval(() => {
-      if (ws.readyState === ws.OPEN) {
-        ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-      }
+    socket.interval = setInterval(() => {
+      socket.emit('news', 'Hello Socket.IO');
     }, 3000);
-    ws.interval = interval;
   });
 };
