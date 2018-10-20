352a2ce93668c9e93636db88ef961339f565e96b
Jinho Hyeon <neverlish@gmail.com>
2018-10-20 18:55:13 +0900

    15 - 2 - 2 웹 요청 처리

--- react/gilbut-react-technique/15/redux-starter-kit/src/App.js
+++ react/gilbut-react-technique/15/redux-starter-kit/src/App.js
@@ -2,18 +2,51 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
-
+import * as postActions from './modules/post';
 
 class App extends Component {
-  render() {
-    const { CounterActions, number } = this.props;
+  loadData = async () => {
+    const { PostActions, number } = this.props;
+    try {
+      const response = await PostActions.getPost(number);
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
+  componentDidMount() {
+    this.loadData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.number !== prevProps.number) {
+      this.loadData();
+    }
+  }
+
+  render() {
+    const { CounterActions, number, post, error, loading } = this.props;
     
     return (
       <div>
         <h1>{number}</h1>
-        <button onClick={CounterActions.incrementAsync}>+</button>
-        <button onClick={CounterActions.decrementAsync}>-</button>
+        {
+          loading
+            ? (<h2>로딩중... </h2>)
+            : (
+              error
+                ? (<h2>오류 발생!</h2>)
+                : (
+                  <div>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                  </div>
+                )
+            )
+        }
+        <button onClick={CounterActions.increment}>+</button>
+        <button onClick={CounterActions.decrement}>-</button>
       </div>
     );
   }
@@ -21,9 +54,13 @@ class App extends Component {
 
 export default connect(
   (state) => ({
-    number: state.counter
+    number: state.counter,
+    post: state.post.data,
+    loading: state.post.pending,
+    error: state.post.error
   }),
   (dispatch) => ({
-    CounterActions: bindActionCreators(counterActions, dispatch)
+    CounterActions: bindActionCreators(counterActions, dispatch),
+    PostActions: bindActionCreators(postActions, dispatch)
   })
 )(App);
---
+++ react/gilbut-react-technique/15/redux-starter-kit/src/modules/post.js
@@ -0,0 +1,65 @@
+import { handleActions, createAction } from 'redux-actions';
+
+import axios from 'axios';
+
+function getPostAPI(postId) {
+  return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+}
+
+const GET_POST_PENDING = 'GET_POST_PENDING';
+const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
+const GET_POST_FAILURE = 'GET_POST_FAILURE';
+
+const getPostPending = createAction(GET_POST_PENDING);
+const getPostSuccess = createAction(GET_POST_SUCCESS);
+const getPostFailure = createAction(GET_POST_FAILURE);
+
+export const getPost = (postId) => dispatch => {
+  dispatch(getPostPending());
+
+  return getPostAPI(postId).then((response) => {
+    dispatch(getPostSuccess(response));
+    return response;
+  }).catch(error => {
+    dispatch(getPostFailure(error));
+    throw(error);
+  });
+}
+
+const initialState = {
+  pending: false,
+  error: false,
+  data: {
+    title: '',
+    body: ''
+  }
+};
+
+export default handleActions({
+  [GET_POST_PENDING]: (state, action) => {
+    return {
+      ...state,
+      pending: true,
+      error: false
+    };
+  },
+  [GET_POST_SUCCESS]: (state, action) => {
+    const { title, body } = action.payload.data;
+
+    return {
+      ...state,
+      pending: false,
+      data: {
+        title,
+        body
+      }
+    };
+  },
+  [GET_POST_FAILURE]: (state, action) => {
+    return {
+      ...state,
+      pending: false,
+      error: true
+    }
+  }
+}, initialState);
