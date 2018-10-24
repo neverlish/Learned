6c7447628a90e5bd05a2e4290dc787f709c981e9
Jinho Hyeon <neverlish@gmail.com>
2018-10-24 23:32:01 +0900

    3-3 자동완성 UI 만들기 - 1 자동완성 UI - 1 제어 대상 엘리먼트 추출하기

--- rxjs/rubypayer-rxjs-quickstart/3/src/autocomplete.js
+++ rxjs/rubypayer-rxjs-quickstart/3/src/autocomplete.js
@@ -1,4 +1,7 @@
 export default class AutoComplete {
   constructor($autocomplete) {
+    this.$input = $autocomplete.querySelector('input');
+    this.$layer = $autocomplete.querySelector('.layer');
+    this.$loading = $autocomplete.querySelector('.loading');
   }
 };
