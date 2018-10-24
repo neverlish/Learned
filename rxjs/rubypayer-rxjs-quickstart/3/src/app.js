49c68eafcba44dac829731f1e6025fa956921d27
Jinho Hyeon <neverlish@gmail.com>
2018-10-25 00:32:40 +0900

    3-4 - 2 - 4 적용하기

--- rxjs/rubypayer-rxjs-quickstart/3/src/app.js
+++ rxjs/rubypayer-rxjs-quickstart/3/src/app.js
@@ -1,7 +1,15 @@
 import AutoComplete from "./autocomplete.js";
 import Map from "./map.js";
 import Sidebar from "./sidebar.js";
+import { createShare$ } from './common.js';
+
+const {
+  render$,
+  search$
+} = createShare$();
 
 const search = new AutoComplete(document.querySelector(".autocomplete"));
 const sidebar = new Sidebar(document.querySelector(".stations"));
-const map = new Map(document.querySelector(".map"));
+const map = new Map(document.querySelector(".map"), search$);
+
+render$.subscribe(v => console.log('===> render', v))
