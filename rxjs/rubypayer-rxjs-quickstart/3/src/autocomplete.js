d9a5abe87e4cebaf811f961b0d5242c7b4960aa9
Jinho Hyeon <neverlish@gmail.com>
2018-10-24 23:38:08 +0900

    3-3 - 1 - 2 사용자 입력 내용 Observable 만들기

--- rxjs/rubypayer-rxjs-quickstart/3/src/autocomplete.js
+++ rxjs/rubypayer-rxjs-quickstart/3/src/autocomplete.js
@@ -1,7 +1,20 @@
+const { fromEvent } = rxjs;
+const { debounceTime, map, distinctUntilChanged, share } = rxjs.operators;
+
 export default class AutoComplete {
   constructor($autocomplete) {
     this.$input = $autocomplete.querySelector('input');
     this.$layer = $autocomplete.querySelector('.layer');
     this.$loading = $autocomplete.querySelector('.loading');
+  }
+
+  createKeyup$() {
+    return fromEvent(this.$input, 'keyup')
+      .pipe(
+        debounceTime(300),
+        map(event => event.target.value),
+        distinctUntilChanged(),
+        share()
+      );
   }
 };
