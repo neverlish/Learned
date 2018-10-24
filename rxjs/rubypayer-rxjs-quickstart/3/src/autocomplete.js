f3d7d7d7ede00cc86078036837b5d10abb795414
Jinho Hyeon <neverlish@gmail.com>
2018-10-24 23:45:53 +0900

    3-3 - 1 - 3 검색어로 버스 번호 목록 조회하는 Observable 만들기

--- rxjs/rubypayer-rxjs-quickstart/3/src/autocomplete.js
+++ rxjs/rubypayer-rxjs-quickstart/3/src/autocomplete.js
@@ -1,11 +1,31 @@
 const { fromEvent } = rxjs;
-const { debounceTime, map, distinctUntilChanged, share } = rxjs.operators;
+const { debounceTime, map, distinctUntilChanged, share, partition, switchMap, pluck, tap, retry, finalize } = rxjs.operators;
+const { ajax } = rxjs.ajax;
 
 export default class AutoComplete {
   constructor($autocomplete) {
     this.$input = $autocomplete.querySelector('input');
     this.$layer = $autocomplete.querySelector('.layer');
     this.$loading = $autocomplete.querySelector('.loading');
+
+    let [search$, reset$] = this.createKeyup$().pipe(
+      partition(query => query.trim().length > 0)
+    );
+    
+    search$ = search$
+      .pipe(
+        tap(() => this.showLoading()),
+        switchMap(query => ajax.getJSON(`/bus/${query}`)),
+        pluck('busRouteList'),
+        retry(2),
+        tap(() => this.hideLoading()),
+        finalize(() => {
+          this.hideLoading();
+          this.$layer.style.display = 'none';
+        })
+      );
+
+    search$.subscribe(items => this.render(items));
   }
 
   createKeyup$() {
@@ -16,5 +36,26 @@ export default class AutoComplete {
         distinctUntilChanged(),
         share()
       );
+  }
+
+  showLoading() {
+    this.$loading.style.display = 'block';
+  }
+
+  hideLoading() {
+    this.$loading.style.display = 'none';
+  }
+
+  render(buses) {
+    this.$layer.innerHTML = buses.map(bus => {
+      return `<li>
+        <a href="#">
+          <strong>${bus.routeName}</strong>
+          <span>${bus.regionName}</span>
+          <div>${bus.routeTypeName}</div>
+        </a>
+      </li>`;
+    }).join('');
+    this.$layer.style.display = 'block';
   }
 };
