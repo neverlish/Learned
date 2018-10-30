e6e21bfaef3a89fa59328ac3ed7cbfe124bdd546
Jinho Hyeon <neverlish@gmail.com>
2018-10-30 22:06:25 +0900

    18 - 3 - 2 미들웨어

--- react/gilbut-react-technique/blog/blog-backend/src/index.js
+++ react/gilbut-react-technique/blog/blog-backend/src/index.js
@@ -2,6 +2,17 @@ const Koa = require('koa');
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  console.log(1);
+  await next();
+  console.log('bye');
+});
+
+app.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+
 app.use((ctx) => {
   ctx.body = 'hello world';
 });
