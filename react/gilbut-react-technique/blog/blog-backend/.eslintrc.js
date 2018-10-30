2526c83324f97850583deff0972b6cce4e8c5298
Jinho Hyeon <neverlish@gmail.com>
2018-10-30 23:07:03 +0900

    19 - 6 데이터 생성과 조회 - 1 NODE_PATH와 jsconfig.json

--- react/gilbut-react-technique/blog/blog-backend/.eslintrc.js
+++ react/gilbut-react-technique/blog/blog-backend/.eslintrc.js
@@ -1,5 +1,12 @@
+const path = require('path');
+
 module.exports = {
   "extends": "airbnb-base",
+  "settings": {
+    "import/resolver": {
+      node: { paths: [path.resolve('./src')] }
+    }
+  },
   "rules": {
     "no-unused-vars": 1,
     "comma-dangle": 0,
