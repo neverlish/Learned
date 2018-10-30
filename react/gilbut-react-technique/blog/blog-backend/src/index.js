f70b655cdacb4aef349ee4e1f41540d9c659d8c9
Jinho Hyeon <neverlish@gmail.com>
2018-10-30 22:15:04 +0900

    18 - 5 - 4 라우트 모듈화

--- react/gilbut-react-technique/blog/blog-backend/src/index.js
+++ react/gilbut-react-technique/blog/blog-backend/src/index.js
@@ -1,22 +1,12 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 
+const api = require('./api');
+
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx) => {
-  ctx.body = '홈';
-});
-
-router.get('/about/:name?', (ctx) => {
-  const { name } = ctx.params;
-  ctx.body = name ? `${name}의 소개` : '소개';
-});
-
-router.get('/posts', (ctx) => {
-  const { id } = ctx.query;
-  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
-})
+router.use('/api', api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
 
