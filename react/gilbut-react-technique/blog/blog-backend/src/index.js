04d25ed1a2c8f2ce016afe7c126519e27c9f4c38
Jinho Hyeon <neverlish@gmail.com>
2018-10-30 22:01:18 +0900

    18 - 3 Koa 기본 사용법 - 1 hello world 띄우기

--- react/gilbut-react-technique/blog/blog-backend/src/index.js
+++ react/gilbut-react-technique/blog/blog-backend/src/index.js
@@ -1,1 +1,11 @@
-const hello = 'hello';
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use((ctx) => {
+  ctx.body = 'hello world';
+});
+
+app.listen(4000, () => {
+  console.log('listening to port 4000');
+});
