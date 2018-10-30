b693f7fec78e31fa2b4811be4543944b0bef6cdf
Jinho Hyeon <neverlish@gmail.com>
2018-10-30 22:50:54 +0900

    19 - 3 - 2 mongoose로 데이터베이스 연결

--- react/gilbut-react-technique/blog/blog-backend/src/index.js
+++ react/gilbut-react-technique/blog/blog-backend/src/index.js
@@ -3,11 +3,19 @@ require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
 
 const {
   PORT: port = 4000,
   MONGO_URI: mongoURI,
 } = process.env;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI).then(() => {
+  console.log('connected to mongodb');
+}).catch((e) => {
+  console.log(e);
+});
 
 const api = require('./api');
 
