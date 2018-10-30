18a1333e555a108b36f7cb27265245ce49821cc6
Jinho Hyeon <neverlish@gmail.com>
2018-10-30 22:49:38 +0900

    19 - 3 - 1 .env 환경변수 파일 생성

--- react/gilbut-react-technique/blog/blog-backend/src/index.js
+++ react/gilbut-react-technique/blog/blog-backend/src/index.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+
+const {
+  PORT: port = 4000,
+  MONGO_URI: mongoURI,
+} = process.env;
 
 const api = require('./api');
 
@@ -13,6 +20,6 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('listening to port 4000');
+app.listen(port, () => {
+  console.log('listening to port', port);
 });
