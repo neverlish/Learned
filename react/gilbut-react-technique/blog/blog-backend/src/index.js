a6e9bcbbdc88c48644f869a39b0a081eb4d3afab
Jinho Hyeon <neverlish@gmail.com>
2018-10-30 22:13:12 +0900

    18 - 5 - 2 라우트 파라미터워 쿼리

--- react/gilbut-react-technique/blog/blog-backend/src/index.js
+++ react/gilbut-react-technique/blog/blog-backend/src/index.js
@@ -8,9 +8,15 @@ router.get('/', (ctx) => {
   ctx.body = '홈';
 });
 
-router.get('/about', (ctx) => {
-  ctx.body = '소개';
+router.get('/about/:name?', (ctx) => {
+  const { name } = ctx.params;
+  ctx.body = name ? `${name}의 소개` : '소개';
 });
+
+router.get('/posts', (ctx) => {
+  const { id } = ctx.query;
+  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
+})
 
 app.use(router.routes()).use(router.allowedMethods());
 
