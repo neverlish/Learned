1f0730afc03e929518bc3f369041abc0592a99a1
Jinho Hyeon <neverlish@gmail.com>
2018-10-30 23:09:06 +0900

    19 - 6 - 2 데이터 생성

--- react/gilbut-react-technique/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ react/gilbut-react-technique/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,101 +1,31 @@
-let postId = 1;
+const Post = require('models/post');
 
-const posts = [
-  {
-    id: 1,
-    title: '제목',
-    body: '내용'
+exports.write = async (ctx) => {
+  const { title, body, tags } = ctx.request.body;
+
+  const post = new Post({
+    title, body, tags
+  });
+
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(e, 500);
   }
-];
-
-exports.write = (ctx) => {
-  const {
-    title,
-    body
-  } = ctx.request.body;
-
-  postId += 1;
-
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
 };
 
-exports.list = (ctx) => {
-  ctx.body = posts;
+exports.list = async (ctx) => {
 };
 
-exports.read = (ctx) => {
-  const { id } = ctx.params;
-
-  const post = posts.find(p => p.id.toString() === id);
-
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.'
-    };
-    return;
-  }
-
-  ctx.body = post;
+exports.read = async (ctx) => {
 };
 
-exports.remove = (ctx) => {
-  const { id } = ctx.params;
-
-  const index = posts.findIndex(p => p.id.toString() === id);
-
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.'
-    };
-    return;
-  }
-
-  posts.splice(index, 1);
-  ctx.status = 204;
+exports.remove = async (ctx) => {
 };
 
-exports.replace = (ctx) => {
-  const { id } = ctx.params;
-
-  const index = posts.findIndex(p => p.id.toString() === id);
-
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.'
-    };
-    return;
-  }
-
-  posts[index] = {
-    id,
-    ...ctx.request.body
-  };
-
-  ctx.body = posts[index];
+exports.replace = async (ctx) => {
 };
 
-exports.update = (ctx) => {
-  const { id } = ctx.params;
-
-  const index = posts.findIndex(p => p.id.toString() === id);
-
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.'
-    };
-    return;
-  }
-
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body
-  };
-
-  ctx.body = posts[index];
-}
+exports.update = async (ctx) => {
+};
