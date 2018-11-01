04888cbcaa89976878abc1417bc839a0ea9c6aa1
Jinho Hyeon <neverlish@gmail.com>
2018-11-01 20:53:11 +0900

    19 - 9 - 3 보이는 개수 제한

--- react/gilbut-react-technique/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ react/gilbut-react-technique/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -44,7 +44,10 @@ exports.write = async (ctx) => {
 
 exports.list = async (ctx) => {
   try {
-    const posts = await Post.find().sort({ _id: -1 }).exec();
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .exec();
     ctx.body = posts;
   } catch (e) {
     ctx.throw(e, 500);
