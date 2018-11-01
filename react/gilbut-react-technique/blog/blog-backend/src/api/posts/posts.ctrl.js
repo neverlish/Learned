9c69c70b41b5bc7f617552c0118dfe205b182d30
Jinho Hyeon <neverlish@gmail.com>
2018-11-01 20:55:11 +0900

    19 - 9 - 4 페이지 기능 구현

--- react/gilbut-react-technique/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ react/gilbut-react-technique/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -43,10 +43,18 @@ exports.write = async (ctx) => {
 };
 
 exports.list = async (ctx) => {
+  const page = parseInt(ctx.query.page || 1, 10);
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
   try {
     const posts = await Post.find()
       .sort({ _id: -1 })
       .limit(10)
+      .skip((page - 1) * 10)
       .exec();
     ctx.body = posts;
   } catch (e) {
