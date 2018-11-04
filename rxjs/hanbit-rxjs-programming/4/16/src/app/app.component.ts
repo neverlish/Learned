d45110551d1d7ca276045325bd07c206a7e0b87e
Jinho Hyeon <neverlish@gmail.com>
2018-11-04 21:11:18 +0900

    16 - 3 비동기 파이프

--- rxjs/hanbit-rxjs-programming/4/16/src/app/app.component.ts
+++ rxjs/hanbit-rxjs-programming/4/16/src/app/app.component.ts
@@ -3,20 +3,9 @@ import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>interval: {{ x }}</h1>',
+  template: '<h1>interval: {{ source$ | async }}</h1>',
 })
 export class AppComponent {
   x = 0;
-  subscription = null;
   source$ = interval(1000);
-
-  ngOnInit() {
-    this.subscription = this.source$.subscribe(x => {
-      this.x = x;
-    });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
 }
