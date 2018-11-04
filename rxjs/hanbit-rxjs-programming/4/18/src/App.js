d5c3b838fc7db13dd2c76059d0e1b320de06d695
Jinho Hyeon <neverlish@gmail.com>
2018-11-04 22:21:56 +0900

    18 - 8 리액트와 리덕스 연동

--- rxjs/hanbit-rxjs-programming/4/18/src/App.js
+++ rxjs/hanbit-rxjs-programming/4/18/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from './actions';
 
 class App extends Component {
   render() {
@@ -10,4 +13,9 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+  state => state,
+  dispatch => ({
+    actions: bindActionCreators(actions, dispatch)
+  })
+)(App);
