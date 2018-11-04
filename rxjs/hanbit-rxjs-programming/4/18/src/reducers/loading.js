0eb7d0a0cc33d56a848f578b6b97e1b3533ca3d9
Jinho Hyeon <neverlish@gmail.com>
2018-11-04 22:14:02 +0900

    18 - 6 리듀서

---
+++ rxjs/hanbit-rxjs-programming/4/18/src/reducers/loading.js
@@ -0,0 +1,12 @@
+import * as ActionTypes from '../constants/ActionTypes';
+
+export default function loading(state = false, action = {}) {
+  switch (action.type) {
+    case ActionTypes.LOADING_START:
+      return true;
+    case ActionTypes.LOADING_END:
+      return false;
+    default:
+      return state;
+  }
+}
