cc101fca02f4897a12d6347df1c097ca1d910a47
Jinho Hyeon <neverlish@gmail.com>
2018-11-04 22:08:35 +0900

    18 - 4 액션 타입 상수

---
+++ rxjs/hanbit-rxjs-programming/4/18/src/constants/ActionTypes.js
@@ -0,0 +1,6 @@
+export const LOADING_START = 'LOADING_START';
+export const LOADING_END = 'LOADING_END';
+export const SET_AUTH = 'SET_AUTH';
+export const RESET_AUTH = 'RESET_AUTH';
+export const SHOW_ERROR_MESSAGE = 'SHOW_ERROR_MESSAGE';
+export const HIDE_ERROR_MESSAGE = 'HIDE_ERROR_MESSAGE';
