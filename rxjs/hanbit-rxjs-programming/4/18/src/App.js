1e6b23ada83a2108e7ecea7c60c54c98ac38bce7
Jinho Hyeon <neverlish@gmail.com>
2018-11-04 22:22:17 +0900

    18 - 8 - 1 Header 컴포넌트

--- rxjs/hanbit-rxjs-programming/4/18/src/App.js
+++ rxjs/hanbit-rxjs-programming/4/18/src/App.js
@@ -3,11 +3,13 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from './actions';
 
+import Header from './Header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Header />
       </div>
     );
   }
