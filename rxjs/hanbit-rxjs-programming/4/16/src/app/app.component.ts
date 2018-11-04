f5f41b19468f112c378e583ab88ff1fc7045efb7
Jinho Hyeon <neverlish@gmail.com>
2018-11-04 21:16:54 +0900

    16 - 4 - 2 리스트 구현

--- rxjs/hanbit-rxjs-programming/4/16/src/app/app.component.ts
+++ rxjs/hanbit-rxjs-programming/4/16/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   template: `
     <input type="text" [formControl]="name">
     <div> 입력된 내용은 {{ name$ | async }}</div>
+    <ul>
+      <li *ngFor="let user of list$ | async">
+        {{ user }}
+      </li>
+    </ul>
   `,
 })
 export class AppComponent {
   name = new FormControl();
   name$ = this.name.valueChanges;
+  list$ = of(['john', 'aiden', 'bob', 'paul', 'sam']);
 }
