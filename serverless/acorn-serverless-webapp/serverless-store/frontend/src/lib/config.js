a3082ba5af550fb846c0b59ee01a8af2a6088d85
Jinho Hyeon <neverlish@gmail.com>
2018-11-13 01:01:06 +0900

    09 서버리스 알림 처리 - 1 AWS IoT - 프로토콜

--- serverless/acorn-serverless-webapp/serverless-store/frontend/src/lib/config.js
+++ serverless/acorn-serverless-webapp/serverless-store/frontend/src/lib/config.js
@@ -14,5 +14,9 @@ export default {
     "APP_CLIENT_ID": "23te4d2t1qqbvoqe485loa84mh",
     "IDENTITY_POOL_ID": "ap-northeast-2:3ef56c6d-1a3a-4d0d-bd69-e555daef1091",
     "REGION": "ap-northeast-2"
+  },
+  "iot": {
+    "REGION": "ap-northeast-2",
+    "ENDPOINT": "a5n8ekl1og4d7-ats.iot.ap-northeast-2.amazonaws.com"
   }
 }
