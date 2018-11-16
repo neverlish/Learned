617c34a376aa0d8197241ce06040b8812df7261e
Jinho Hyeon <neverlish@gmail.com>
2018-11-17 02:42:16 +0900

    2 Querying AppSync and using CSS Grid

--- serverless/yt-benawad-appsync-amplify/src/App.tsx
+++ serverless/yt-benawad-appsync-amplify/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CreateAuctionForm } from './CreateAuctionForm';
+import { Auctions } from './Auctions';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <CreateAuctionForm />
+        <Auctions />
       </div>
     );
   }
