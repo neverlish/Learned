e3fe72fef5fa6f47da7836bbb3ef7d8c44068f61
Jinho Hyeon <neverlish@gmail.com>
2018-11-16 22:39:07 +0900

    20 - 2 - 6 버튼 생성

--- react/gilbut-react-technique/blog/blog-frontend/src/components/common/Header/Header.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/components/common/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Header.scss';
 import classNames from 'classnames/bind'
 import { Link } from 'react-router-dom';
+import Button from 'components/common/Button';
 
 const cx = classNames.bind(styles);
 
@@ -12,8 +13,7 @@ const Header = () => (
         <Link to='/'>reactblog</Link>
       </div>
       <div className={cx('right')}>
-        {/* 조건에 따라 버튼 렌더링 */}
-        오른쪽
+        <Button theme='outline' to='/editor'>새 포스트</Button>
       </div>
     </div>
   </header>
