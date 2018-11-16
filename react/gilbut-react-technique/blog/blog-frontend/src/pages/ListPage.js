674e3a6d3b9d6d2d0fc7486d44c5e4766a02646a
Jinho Hyeon <neverlish@gmail.com>
2018-11-16 22:44:53 +0900

    20 - 3 PostList 페이지 UI 구현 - 1 ListWrapper 컴포넌트

--- react/gilbut-react-technique/blog/blog-frontend/src/pages/ListPage.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/pages/ListPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
+import ListWrapper from 'components/list/ListWrapper';
 
 const ListPage = () => {
   return (
     <PageTemplate>
-      List
+      <ListWrapper>
+        리스트
+      </ListWrapper>
     </PageTemplate>
   );
 };
