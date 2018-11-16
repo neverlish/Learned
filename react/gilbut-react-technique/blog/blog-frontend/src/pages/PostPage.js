bbbc5d29bcc6598623b86ff00c9069e06b411429
Jinho Hyeon <neverlish@gmail.com>
2018-11-16 22:55:19 +0900

    20 - 4 Post 페이지 UI 구현 - 1 PostInfo 컴포넌트

--- react/gilbut-react-technique/blog/blog-frontend/src/pages/PostPage.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/pages/PostPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PageTemplate from 'components/common/PageTemplate';
+import PostInfo from 'components/post/PostInfo';
 
 const PostPage = () => {
   return (
-    <div>
-      Post
-    </div>
+    <PageTemplate>
+      <PostInfo />
+    </PageTemplate>
   );
 };
 
