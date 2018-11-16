b7281af59fb1f155a36daf7a915f8c1da563545f
Jinho Hyeon <neverlish@gmail.com>
2018-11-16 22:33:15 +0900

    20 - 2 - 4 Footer 컴포넌트 생성

--- react/gilbut-react-technique/blog/blog-frontend/src/components/common/PageTemplate/PageTemplate.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/components/common/PageTemplate/PageTemplate.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './PageTemplate.scss';
 import classNames from 'classnames/bind';
 import Header from 'components/common/Header';
+import Footer from 'components/common/Footer';
 
 const cx = classNames.bind(styles);
 
 const PageTemplate = () => (
   <div className={cx('page-template')}>
     <Header />
+    <Footer />
   </div>
 );
 
