da12b097a210a1ab2a460568662988fee9af4942
Jinho Hyeon <neverlish@gmail.com>
2018-11-17 01:05:01 +0900

    16 - 4 - 2 NavLink 컴포넌트

--- react/gilbut-react-technique/react-router-tutorial/src/components/Menu.js
+++ react/gilbut-react-technique/react-router-tutorial/src/components/Menu.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Menu = () => {
+  const activeStyle = {
+    color: 'green',
+    fontSize: '2rem'
+  };
+
   return (
     <div>
       <ul>
-        <li><Link to='/'>홈</Link></li>
-        <li><Link to='/about'>소개</Link></li>
-        <li><Link to='/about/react'>React 소개</Link></li>
+        <li><NavLink exact to='/' activeStyle={activeStyle}>홈</NavLink></li>
+        <li><NavLink exact to='/about' activeStyle={activeStyle}>소개</NavLink></li>
+        <li><NavLink to='/about/react' activeStyle={activeStyle}>React 소개</NavLink></li>
       </ul>
     </div>
   );
