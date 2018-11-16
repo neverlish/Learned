a79d76720284b4b3f85507ab40db88ba7f40166b
Jinho Hyeon <neverlish@gmail.com>
2018-11-17 01:05:52 +0900

    16 - 4 - 3 자바스크립트에서 라우팅

--- react/gilbut-react-technique/react-router-tutorial/src/pages/Home.js
+++ react/gilbut-react-technique/react-router-tutorial/src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Home = () => {
+const Home = ({ history }) => {
   return (
     <div>
       <h2>홈</h2>
+      <button onClick={() => {
+        history.push('/about/javascript');
+      }}>자바스크립트를 사용하여 이동</button>
     </div>
   );
 };
