c2fca3bee0f3218a4c6814c414215844412ab2ae
Jinho Hyeon <neverlish@gmail.com>
2018-11-16 23:50:37 +0900

    20 - 6 - 2 에디터 상태 관리

--- react/gilbut-react-technique/blog/blog-frontend/src/components/editor/EditorPane/EditorPane.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/components/editor/EditorPane/EditorPane.js
@@ -20,6 +20,7 @@ const cx = classNames.bind(styles);
 class EditorPane extends Component {
   editor = null; // 에디터 ref
   codeMirror = null; // CodeMirror 인스턴스
+  cursor = null; // 에디터의 텍스트 cursor 위치
 
   initializeEditor = () => {
     this.codeMirror = CodeMirror(this.editor, {
@@ -28,20 +29,60 @@ class EditorPane extends Component {
       lineNumbers: true, // 왼쪽에 라인 넘버 띄우기
       lineWrapping: true // 내용이 너무 길면 다음 줄에 작성
     });
+    this.codeMirror.on('change', this.handleChangeMarkdown);
   }
 
   componentDidMount() {
     this.initializeEditor();
   }
 
+  handleChange = (e) => {
+    const { onChangeInput } = this.props;
+    const { value, name } = e.target;
+    onChangeInput({ name, value });
+  }
+
+  handleChangeMarkdown = (doc) => {
+    const { onChangeInput } = this.props;
+    this.cursor = doc.getCursor(); // 텍스트 cursor 위치 저장
+    onChangeInput({
+      name: 'markdown',
+      value: doc.getValue()
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // markdown이 변경되면 에디터 값도 변경합니다. 이 과정에서 텍스트 커서의 위치가 초기화되기 때문에, 저장한 커서의 위치가 있으면 해당 위치로 설정합니다.
+    if (prevProps.markdown !== this.props.markdown) {
+      const { codeMirror, cursor } = this;
+      if (!codeMirror) return; // 인스턴스를 아직 만들지 않았을 때
+      codeMirror.setValue(this.props.markdown);
+      if (!cursor) return; // 커서가 없을 때
+      codeMirror.setCursor(cursor);
+    }
+  }
+
   render() {
+    const { handleChange } = this;
+    const { tags, title } = this.props;
     return (
       <div className={cx('editor-pane')}>
-        <input className={cx('title')} placeholder='제목을 입력하세요' name='title' />
+        <input
+          className={cx('title')}
+          placeholder='제목을 입력하세요' 
+          name='title'
+          value={title}
+          onChange={handleChange}
+        />
         <div className={cx('code-editor')} ref={ref => this.editor = ref}></div>
         <div className={cx('tags')}>
           <div className={cx('description')}>태그</div>
-          <input name='tags' placeholder='태그를 입력하세요 (쉼표로 구분)' />
+          <input
+            name='tags'
+            placeholder='태그를 입력하세요 (쉼표로 구분)'
+            value={tags}
+            onChange={handleChange}
+          />
         </div>
       </div>
     )
--- react/gilbut-react-technique/blog/blog-frontend/src/pages/EditorPage.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/pages/EditorPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import EditorTemplate from 'components/editor/EditorTemplate';
 import EditorHeader from 'components/editor/EditorHeader';
-import EditorPane from 'components/editor/EditorPane';
+import EditorPaneContainer from 'containers/editor/EditorPaneContainer';
 import PreviewPane from 'components/editor/PreviewPane';
 
 const EditorPage = () => {
   return (
     <EditorTemplate 
       header={<EditorHeader />}
-      editor={<EditorPane />}
+      editor={<EditorPaneContainer />}
       preview={<PreviewPane />}
     />
   );
