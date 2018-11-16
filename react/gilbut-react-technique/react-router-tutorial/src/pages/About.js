d39a4672cf348e90bd2caad420b3c2a7c9c32c9b
Jinho Hyeon <neverlish@gmail.com>
2018-11-17 00:58:47 +0900

    16 - 3 Route와 파라미터 - 1 기본 라우트 준비

---
+++ react/gilbut-react-technique/react-router-tutorial/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>소개</h2>
+      <p>
+        안녕하세요, 저는 리액트 라우터입니다.
+      </p>
+    </div>
+  );
+};
+
+export default About;
