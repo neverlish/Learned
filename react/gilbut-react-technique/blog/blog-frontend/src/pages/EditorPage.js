6f8d5d9c8cc657b78c045e6574382fc3b4da9d25
Jinho Hyeon <neverlish@gmail.com>
2018-11-16 23:23:00 +0900

    20 - 5 - 2 EditorHeader 컴포넌트

--- react/gilbut-react-technique/blog/blog-frontend/src/pages/EditorPage.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/pages/EditorPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import EditorTemplate from 'components/editor/EditorTemplate';
+import EditorHeader from 'components/editor/EditorHeader';
 
 const EditorPage = () => {
   return (
     <EditorTemplate 
-      header='헤더'
+      header={<EditorHeader />}
       editor='에디터'
       preview='프리뷰'
     />
