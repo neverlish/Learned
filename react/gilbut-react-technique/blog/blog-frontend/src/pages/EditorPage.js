bd315be46dfb0948ebb9f86fc1bb15ef80c60f5f
Jinho Hyeon <neverlish@gmail.com>
2018-11-16 23:08:56 +0900

    20 - 5 Editor 페이지 UI 구현 - 1 EditorTemplate 컴포넌트

--- react/gilbut-react-technique/blog/blog-frontend/src/pages/EditorPage.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/pages/EditorPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import EditorTemplate from 'components/editor/EditorTemplate';
 
 const EditorPage = () => {
   return (
-    <div>
-      Editor
-    </div>
+    <EditorTemplate 
+      header='헤더'
+      editor='에디터'
+      preview='프리뷰'
+    />
   );
 };
 
