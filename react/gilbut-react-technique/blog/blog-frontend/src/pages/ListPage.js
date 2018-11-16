4a81a8e9c602b2e74b92492df347c2cfa5342331
Jinho Hyeon <neverlish@gmail.com>
2018-11-16 22:48:32 +0900

    20 - 3 - 2 PostList 컴포넌트 생성

--- react/gilbut-react-technique/blog/blog-frontend/src/pages/ListPage.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/pages/ListPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
 import ListWrapper from 'components/list/ListWrapper';
+import PostList from 'components/list/PostList';
 
 const ListPage = () => {
   return (
     <PageTemplate>
       <ListWrapper>
-        리스트
+        <PostList />
       </ListWrapper>
     </PageTemplate>
   );
