acd582b38a7ce36fee16f3a3be605700c7fcdaab
Jinho Hyeon <neverlish@gmail.com>
2018-11-17 00:59:56 +0900

    16 - 3 - 2 라우트 설정

--- react/gilbut-react-technique/react-router-tutorial/src/App.js
+++ react/gilbut-react-technique/react-router-tutorial/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import {
+  Home,
+  About
+} from 'pages';
 
 const App = () => {
   return (
     <div>
-      리액트 라우터를 배워봅시다.
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
     </div>
   );
 };
