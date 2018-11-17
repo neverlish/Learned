545ed18af62d1a535991e48d6a19548aa7f23857
Jinho Hyeon <neverlish@gmail.com>
2018-11-17 12:07:22 +0900

    4 GraphQL Subscriptions with Apollo and AppSync

--- serverless/yt-benawad-appsync-amplify/src/Auctions.tsx
+++ serverless/yt-benawad-appsync-amplify/src/Auctions.tsx
@@ -1,3 +1,4 @@
+import { buildSubscription } from "aws-appsync";
 import * as React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
@@ -5,6 +6,8 @@ import gql from "graphql-tag";
 import { AuctionCard } from "./AuctionCard";
 import { listAuctions } from "./graphql/queries";
 import { ListAuctionsQuery, ListAuctionsQueryVariables } from "./API";
+import { OnMount } from "./components/OnMount";
+import { onCreateAuction } from "./graphql/subscriptions";
 
 export const Auctions = () => {
   return (
@@ -12,7 +15,7 @@ export const Auctions = () => {
       query={gql(listAuctions)}
       variables={{ limit: 100 }}
     >
-      {({ data, loading }) => {
+      {({ data, loading, subscribeToMore }) => {
         if (
           loading ||
           !data ||
@@ -32,6 +35,13 @@ export const Auctions = () => {
               gridGap: 10
             }}
           >
+            <OnMount 
+              onEffect={() => {
+                return subscribeToMore(
+                  buildSubscription( gql(onCreateAuction),gql(listAuctions))
+                );
+              }}
+            />
             {data.listAuctions.items.map(x => (
               <AuctionCard name={x!.name} price={x!.price} key={x!.id} />
             ))}
