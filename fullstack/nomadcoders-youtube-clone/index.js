24e9da415ee0dd01feb4f63e1c503da5e0b3f7be
Jinho Hyeon <neverlish@gmail.com>
2018-11-22 01:00:35 +0900

    02 - 05 ES6 on NodeJS using Babel

--- fullstack/nomadcoders-youtube-clone/index.js
+++ fullstack/nomadcoders-youtube-clone/index.js
@@ -1,19 +1,13 @@
-const express = require("express");
+import express from 'express';
 const app = express();
 
 const PORT = 4000;
 
-function handlerListening() {
-  console.log(`Listening on http://localhost:${PORT}`);
-}
+const handlerListening = () => console.log(`Listening on http://localhost:${PORT}`);
 
-function handleHome(req, res) {
-  res.send('Hello from home');
-}
+const handleHome = (req, res) => res.send('Hello from home');
 
-function handleProfile(req, res) {
-  res.send('You are on my profile');
-}
+const handleProfile = (req, res) => res.send('You are on my profile');
 
 app.get('/', handleHome);
 
