3fd035af3f9cdb619f02d2aebe65f0b6c86188fb
Jinho Hyeon <neverlish@gmail.com>
2018-11-22 01:11:02 +0900

    02 - 06 Express Core Middlewares

--- fullstack/nomadcoders-youtube-clone/index.js
+++ fullstack/nomadcoders-youtube-clone/index.js
@@ -9,6 +9,13 @@ const handleHome = (req, res) => res.send('Hello from home');
 
 const handleProfile = (req, res) => res.send('You are on my profile');
 
+const betweenHome = (req, res, next) => {
+  console.log('Between');
+  next();
+};
+
+app.use(betweenHome);
+
 app.get('/', handleHome);
 
 app.get('/profile', handleProfile);
