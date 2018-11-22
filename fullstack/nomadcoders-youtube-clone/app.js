99e4987f3389ff5dbc9281a36ef9da6de42921a5
Jinho Hyeon <neverlish@gmail.com>
2018-11-22 18:07:54 +0900

    02 - 09 MVC Pattern part One

--- fullstack/nomadcoders-youtube-clone/app.js
+++ fullstack/nomadcoders-youtube-clone/app.js
@@ -4,13 +4,11 @@ import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 
-import { userRouter } from './router';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const app = express();
-
-const handleHome = (req, res) => res.send('Hello from home');
-
-const handleProfile = (req, res) => res.send('You are on my profile');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -18,10 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
 
-app.get('/', handleHome);
-
-app.get('/profile', handleProfile);
-
+app.use('/', globalRouter);
 app.use('/user', userRouter);
+app.use('/video', videoRouter);
 
 export default app;
