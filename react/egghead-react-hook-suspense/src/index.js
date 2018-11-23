8a484474c9845a6ffceff0ddb8baf4f4454c82f4
Jinho Hyeon <neverlish@gmail.com>
2018-11-23 23:00:25 +0900

    02 Test React Components that use React Hooks

--- react/egghead-react-hook-suspense/src/index.js
+++ react/egghead-react-hook-suspense/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Counter from './lessons/01-use-state-hook'
+import Counter from './lessons/02-testing-hooks'
 
 ReactDOM.render(<Counter />, document.getElementById('root'))
