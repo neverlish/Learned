2124fe69cc0f949d503e10c5eb137b03d73f77e4
Jinho Hyeon <neverlish@gmail.com>
2018-11-30 02:18:06 +0900

    7 Execute Mutations to an AWS AppSync GraphQL API from a React Application

--- serverless/egghead-appsync-react/todo-app/src/App.js
+++ serverless/egghead-appsync-react/todo-app/src/App.js
@@ -4,6 +4,18 @@ import './App.css';
 
 import gql from 'graphql-tag';
 import { graphql, compose } from 'react-apollo';
+import { graphqlMutation } from 'aws-appsync-react';
+
+const CreateTodo = gql`
+  mutation($title: String!, $completed: Boolean) {
+    createTodo(input: {
+      title: $title,
+      completed: $completed
+    }) {
+      id title completed
+    }
+  }
+`
 
 const ListTodos = gql`
   query {
@@ -16,23 +28,28 @@ const ListTodos = gql`
 `;
 
 class App extends Component {
+  state = { todo: '' }
+
+  addTodo = () => {
+    if (this.state.todo === '') return;
+
+    const todo = {
+      title: this.state.todo,
+      completed: false
+    };
+    this.props.createTodo(todo);
+    this.setState({ todo: '' })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input
+          onChange={e => this.setState({ todo: e.target.value })}
+          value={this.state.todo}
+          placeholder='Todo name '
+        />
+        <button onClick={this.addTodo}>Add Todo</button>
         {
           this.props.todos.map((item, i) => (
             <p key={i}>{ item.title }</p>
@@ -44,6 +61,7 @@ class App extends Component {
 }
 
 export default compose(
+  graphqlMutation(CreateTodo, ListTodos, 'Todo'),
   graphql(ListTodos, {
     options: {
       fetchPolicy: 'cache-and-network'
