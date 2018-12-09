415b1fa0b14cca53e904b15f404890bac4cedd94
Jinho Hyeon <neverlish@gmail.com>
2018-12-10 01:22:55 +0900

    7 Refetch Data with the Apollo Query Component either Manually or on Timed Intervals

--- react/egghead-graphqldata-react-apolloclient/client/src/Recipes.js
+++ react/egghead-graphqldata-react-apolloclient/client/src/Recipes.js
@@ -33,56 +33,60 @@ export default class Recipes extends Component {
         <Query
           query={recipesQuery}
           variables={{ vegetarian: this.state.vegetarian }}
+          pollInterval={3000}
         >
-          {({ data, loading, error }) => {
+          {({ data, loading, error, refetch }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Something went wrong</p>;
 
             return (
-              <ul>
-                {data.recipes.map(({ id, title, isStarred }) => (
-                  <li key={id}>
-                    {title}
-                    <Mutation
-                      mutation={updateRecipeStarredMutation}
-                      refetchQueries={[
-                        {
-                          query: recipesQuery,
-                          variables: { vegetarian: false }
-                        },
-                        {
-                          query: recipesQuery,
-                          variables: { vegetarian: true }
-                        }
-                      ]}
-                      awaitRefetchQueries={true}
-                    >
-                      {(updateRecipeStarred, { loading, error }) => (
-                        <button
-                          onClick={() =>
-                            updateRecipeStarred({
-                              variables: {
-                                id,
-                                isStarred: !isStarred
-                              }
-                            })
+              <React.Fragment>
+                <ul>
+                  {data.recipes.map(({ id, title, isStarred }) => (
+                    <li key={id}>
+                      {title}
+                      <Mutation
+                        mutation={updateRecipeStarredMutation}
+                        refetchQueries={[
+                          {
+                            query: recipesQuery,
+                            variables: { vegetarian: false }
+                          },
+                          {
+                            query: recipesQuery,
+                            variables: { vegetarian: true }
                           }
-                          className="star-btn"
-                          style={{
-                            color: isStarred ? "orange" : "grey",
-                            animation: loading
-                              ? "inflate 0.7s ease infinite alternate"
-                              : "none"
-                          }}
+                        ]}
+                        awaitRefetchQueries={true}
+                      >
+                        {(updateRecipeStarred, { loading, error }) => (
+                          <button
+                            onClick={() =>
+                              updateRecipeStarred({
+                                variables: {
+                                  id,
+                                  isStarred: !isStarred
+                                }
+                              })
+                            }
+                            className="star-btn"
+                            style={{
+                              color: isStarred ? "orange" : "grey",
+                              animation: loading
+                                ? "inflate 0.7s ease infinite alternate"
+                                : "none"
+                            }}
 
-                        >
-                          ★ {error && "Failed to update"}
-                        </button>
-                      )}
-                    </Mutation>
-                  </li>
-                ))}
-              </ul>
+                          >
+                            ★ {error && "Failed to update"}
+                          </button>
+                        )}
+                      </Mutation>
+                    </li>
+                  ))}
+                </ul>
+                <button onClick={() => refetch()}>Refresh Recipes</button>
+              </React.Fragment>
             )
           }}
         </Query>
