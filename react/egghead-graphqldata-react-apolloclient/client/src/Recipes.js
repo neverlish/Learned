5dd5e889ae919a01f9d46a647a8359329b989b3e
Jinho Hyeon <neverlish@gmail.com>
2018-12-10 00:41:39 +0900

    4 Provide Dynamic Arguments in a Apollo Query Component with GraphQL Variables

--- react/egghead-graphqldata-react-apolloclient/client/src/Recipes.js
+++ react/egghead-graphqldata-react-apolloclient/client/src/Recipes.js
@@ -1,33 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 
 const recipesQuery = gql`
-  {
-    recipes {
+  query recipes($vegetarian: Boolean!) {
+    recipes(vegetarian: $vegetarian) {
       id
       title
     }
   }
 `
 
-export default function() {
-  return (
-    <Query
-      query={recipesQuery}
-    >
-      {({ data, loading, error }) => {
-        if (loading) return <p>Loading...</p>;
-        if (error) return <p>Something went wrong</p>;
+export default class Recipes extends Component {
+  state = {
+    vegetarian: false 
+  }
 
-        return (
-          <ul>
-            {data.recipes.map(({ id, title }) =>
-              <li key={id}>{title}</li>
-            )}
-          </ul>
-        )
-      }}
-    </Query>
-  )
+  updateVegetarian = ({ target: { checked }}) => {
+    this.setState({ vegetarian: checked });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <label>
+          <input
+            type='checkbox'
+            checked={this.state.vegetarian}
+            onChange={this.updateVegetarian}
+          />
+          <span>vegetarian</span>
+        </label>
+        <Query
+          query={recipesQuery}
+          variables={{ vegetarian: this.state.vegetarian }}
+        >
+          {({ data, loading, error }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Something went wrong</p>;
+    
+            return (
+              <ul>
+                {data.recipes.map(({ id, title }) => (
+                  <li key={id}>{title}</li>
+                ))}
+              </ul>
+            )
+          }}
+        </Query>
+      </React.Fragment>
+    )
+  }
 }
