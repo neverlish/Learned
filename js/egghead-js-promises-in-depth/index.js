10795e6e8f828da5d4b6353cf1bc01889a0c2d3d
Jinho Hyeon <neverlish@gmail.com>
2018-12-11 01:35:16 +0900

    10 Wait for Multiple JavaScript Promises to Settle with Promise.all()

--- js/egghead-js-promises-in-depth/index.js
+++ js/egghead-js-promises-in-depth/index.js
@@ -3,21 +3,29 @@ const API_URL = "https://starwars.egghead.training/";
 const output = document.getElementById("output");
 const spinner = document.getElementById("spinner");
 
-Promise.resolve($.getJSON(API_URL + "films"))
-  .then(films => {
-    output.innerText = getFilmTitles(films);
+function queryAPI(endpoint) {
+  return fetch(API_URL + endpoint).then(response => {
+    return response.ok
+      ? response.json()
+      : Promise.reject(Error('Unsuccessful response'));
+  });
+}
+
+Promise.all([
+  queryAPI('films'),
+  queryAPI('planets'),
+  queryAPI('species')
+])
+  .then(([films, planets, species]) => {
+    output.innerText =
+      `${films.length} films, ` +
+      `${planets.length} planets, ` +
+      `${species.length} species`;
   })
   .catch(error => {
     console.warn(error);
-    output.innerText = ':(';
+    output.innerText = ":(";
   })
   .finally(() => {
     spinner.remove();
   });
-
-function getFilmTitles(films) {
-  return films
-    .sort((a, b) => a.episode_id - b.episode_id)
-    .map(film => `${film.episode_id}. ${film.title}`)
-    .join("\n");
-}
