ccfc570f94146c4d63a0d0e8a03acc7a5b55847c
Jinho Hyeon <neverlish@gmail.com>
2018-12-11 14:15:57 +0900

    02 Queue Data Structure in JavaScript

---
+++ js/egghead-datastructures-algorithms-in-js/queues.js
@@ -0,0 +1,32 @@
+function createQueue() {
+  const queue = []
+
+  return {
+    enqueue(item) {
+      queue.unshift(item)
+    },
+    dequeue() {
+      return queue.pop()
+    },
+    peek() {
+      return queue[queue.length - 1]
+    },
+    get length() {
+      return queue.length
+    },
+    isEmpty() {
+      return queue.length === 0
+    }
+  }
+}
+
+const q = createQueue()
+
+q.enqueue('Make an egghead lesson')
+q.enqueue('Help others learn')
+q.enqueue('Be happy')
+
+q.dequeue()
+q.dequeue()
+q.dequeue()
+console.log(q.peek())
