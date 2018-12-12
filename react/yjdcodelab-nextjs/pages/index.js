293482830c123436a80ee3be05c28198a57dc14d
Jinho Hyeon <neverlish@gmail.com>
2018-12-13 00:21:07 +0900

    1 - NextJS 소개 및 개발 환경 구성 7

--- react/yjdcodelab-nextjs/pages/index.js
+++ react/yjdcodelab-nextjs/pages/index.js
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+
 export default () => (
-  <div>Hello, Next.js!</div>  
+  <div>
+    Hello, Next.js!
+
+    <div>
+      <Link href='/playground'>
+        <a>Go Playground</a>
+      </Link>
+    </div>
+
+    <div>
+      <a href='/playground'>Go Playground(without Link)</a>
+    </div>
+  </div>  
 );
--- react/yjdcodelab-nextjs/pages/playground.js
+++ react/yjdcodelab-nextjs/pages/playground.js
@@ -1,7 +1,16 @@
+import Link from 'next/link';
+
 export default () => (
   <div>
     <h1>Playground</h1>
 
     <p>Welcome to playground.</p>
+
+    <div>
+      <Link href='/'><a>Home</a></Link>
+    </div>
+    <div>
+      <a href='/'>Home (without Link)</a>
+    </div>
   </div>
 );
