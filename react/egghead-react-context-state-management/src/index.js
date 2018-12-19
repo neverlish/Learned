965fe9f516548e97cd1a1477b080481992e440b5
Jinho Hyeon <neverlish@gmail.com>
2018-12-19 16:20:20 +0900

    06 Use a React Context Consumer Outside of the Matching Provider

--- react/egghead-react-context-state-management/src/index.js
+++ react/egghead-react-context-state-management/src/index.js
@@ -1,30 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoginPage from './LoginPage';
-import MainPage from './MainPage';
-import { UserProvider, UserConsumer } from './UserContext';
-import { EmailProvider } from './EmailContext';
 import './index.css';
 
-function Root() {
-  return (
-    <UserConsumer>
-      {({ user }) =>
-        user ? (
-          <MainPage />
-        ) : (
-          <LoginPage />
-        )
-      }
-    </UserConsumer>
-  )
-}
+const Color = React.createContext('black');
 
-ReactDOM.render(
-  <UserProvider>
-    <EmailProvider>
-      <Root />
-    </EmailProvider>
-  </UserProvider>,
-  document.querySelector('#root')
+const FancyButton = props => (
+  <Color.Consumer>
+    {color => {
+      if (typeof color === 'undefined') {
+        throw Error(
+          'FancyButton requires a Color Provider'
+        );
+      }
+      return (
+        <button className={`fancy-btn ${color}`} {...props}>
+          Click Me
+        </button>
+      );
+    }}
+  </Color.Consumer>
 );
+
+const App = () => (
+  <div>
+    <FancyButton />
+    <Color.Provider value="red">
+      <FancyButton />
+    </Color.Provider>
+    <Color.Provider value="green">
+      <FancyButton />
+    </Color.Provider>
+    <Color.Provider value="blue">
+      <FancyButton />
+    </Color.Provider>
+  </div>
+);
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import LoginPage from './LoginPage';
+// import MainPage from './MainPage';
+// import { UserProvider, UserConsumer } from './UserContext';
+// import { EmailProvider } from './EmailContext';
+// import './index.css';
+
+// function Root() {
+//   return (
+//     <UserConsumer>
+//       {({ user }) =>
+//         user ? (
+//           <MainPage />
+//         ) : (
+//           <LoginPage />
+//         )
+//       }
+//     </UserConsumer>
+//   )
+// }
+
+// ReactDOM.render(
+//   <UserProvider>
+//     <EmailProvider>
+//       <Root />
+//     </EmailProvider>
+//   </UserProvider>,
+//   document.querySelector('#root')
+// );
