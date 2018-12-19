5b7be4446c9aaf6ccac489c7050b4ef5e2bebd3a
Jinho Hyeon <neverlish@gmail.com>
2018-12-19 23:15:31 +0900

    07 Use Types vs. Interfaces

---
+++ tsc/egghead-practical-advanced-tsc/07/interoperability.ts
@@ -0,0 +1,22 @@
+type Pet = {
+  pose(): void;
+};
+
+interface IFeline {
+  nightvision: boolean;
+}
+
+interface ICat extends IFeline, Pet {
+
+}
+
+type Cat = IFeline & Pet;
+
+class HouseCat implements IFeline, Pet {
+  pose(): void {
+    throw new Error("Method not implemented.");
+  }
+  nightvision: boolean;
+}
+
+export {};
