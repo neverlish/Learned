7f3200cb9593bb321b9c0ee434fa83055799c164
Jinho Hyeon <neverlish@gmail.com>
2018-12-20 12:21:47 +0900

    10 Use the TypeScript "unknown" type to avoid runtime errors

---
+++ tsc/egghead-practical-advanced-tsc/10/ts-unknownUseCase.ts
@@ -0,0 +1,26 @@
+interface IComment {
+  date: Date;
+  message: string;
+}
+
+interface IDataService {
+  getData(): unknown;
+}
+
+let service: IDataService;
+
+const response = service.getData();
+response.a.b.c.d;
+
+if(typeof response === "string") {
+  console.log(response.toUpperCase());
+} else if(isComment(response)) {
+  response.date;
+} else {
+  const numbers = <number[]>response;
+  numbers.indexOf(1);
+}
+
+function isComment(type: any): type is IComment {
+  return (<IComment>type).message !== undefined;
+}
