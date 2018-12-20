f4a71b899cbb66c772f1d86281865269a85b40eb
Jinho Hyeon <neverlish@gmail.com>
2018-12-20 13:48:31 +0900

    14 Deeply mark all the properties of a type as read-only in TypeScript

---
+++ tsc/egghead-practical-advanced-tsc/14/index.ts
@@ -0,0 +1,45 @@
+interface IEmail {
+  from: string;
+  to: string[];
+  body: string;
+}
+
+interface ITodo {
+  isCompleted: boolean;
+  text: string;
+  linkedEmail: IEmail;
+}
+
+interface IRootState {
+  userId: string;
+  showCompletedOnly: boolean;
+  todoTypes: string[];
+  todos: ITodo[];
+  iconGrid: string[][];
+}
+
+type DeepReadonlyObject<T> = { readonly [K in keyof T]: DeepReadonly<T[K]> };
+
+type DeepReadonly<T> = T extends (infer E)[][] ?
+  ReadonlyArray<ReadonlyArray<DeepReadonlyObject<E>>> :
+  T extends (infer E)[] ? ReadonlyArray<DeepReadonlyObject<E>> :
+  T extends object ? DeepReadonlyObject<T> :
+  T;
+
+type IReadonlyRootState = DeepReadonly<IRootState>;
+
+function rootReducer(action: any, state: IReadonlyRootState): IReadonlyRootState {
+  return state;
+}
+
+let state: IReadonlyRootState;
+
+state.showCompletedOnly = true;
+state.userId = 'newId';
+state.todoTypes = [];
+state.todoTypes[0] = 'diff type';
+state.todos[1].linkedEmail.body = "hi";
+state.todos[1].linkedEmail.to[1] = "john@gmail.com";
+
+state.todoTypes.map(todo => todo.toUpperCase());
+state.iconGrid[0].map(icon => icon);
