ee2e32f265f38579b1ba564dafe3a44c8880c712
Jinho Hyeon <neverlish@gmail.com>
2018-12-21 00:11:54 +0900

    10 Show an Error when a POST or DELETE Fails in an Offline PWA

--- react/egghead-pwa-react-cra/src/sw.js
+++ react/egghead-pwa-react-cra/src/sw.js
@@ -9,9 +9,23 @@ workbox.routing.registerRoute(
 )
 
 workbox.routing.registerRoute(
-  new RegExp('/.*\.json'),
+  new RegExp('http://.*:4567.*\.json'),
   workbox.strategies.networkFirst()
 )
+
+self.addEventListener('fetch', event => {
+  if (event.request.method === 'POST' || event.request.method === 'DELETE') {
+    event.respondWith(
+      fetch(event.request).catch(err => {
+        return new Response(
+          JSON.stringify({ error: 'This action disbaled while app is offline' }), {
+            headers: { 'Content-Type': 'application/json' }
+          }
+        )
+      })
+    )
+  }
+})
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
 
