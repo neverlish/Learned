a80d2e9922ed309700ffdb4bfd44d8f089c65b0e
Jinho Hyeon <neverlish@gmail.com>
2018-12-22 23:22:02 +0900

    18 Initialize Redux Application State Using The State ADT

--- js/egghead-redux-state-adt/data/reducers/game.js
+++ js/egghead-redux-state-adt/data/reducers/game.js
@@ -4,12 +4,19 @@ import start, {
   markCardsUnselected
 } from '../model/game'
 
+import initialize from '../model/initialize'
+
 const START_GAME = 'START_GAME'
+const RESET_GAME = 'RESET_GAME'
 const HIDE_ALL_CARDS = 'HIDE_ALL_CARDS'
 
 // startGame :: String -> Action String
 export const startGame =
   createAction(START_GAME)
+
+// resetGame :: String -> Action String
+export const resetGame =
+  createAction(RESET_GAME)
 
 // hideAllCards :: String -> Action String
 export const hideAllCards =
@@ -18,6 +25,7 @@ export const hideAllCards =
 // reducer :: Reducer
 const reducer = createReducer({
   START_GAME: start,
+  RESET_GAME: initialize,
   HIDE_ALL_CARDS: markCardsUnselected
 })
 
--- js/egghead-redux-state-adt/index.js
+++ js/egghead-redux-state-adt/index.js
@@ -1,33 +1,12 @@
 import log from './logger'
 
-import { showFeedback, selectCard } from './data/reducers/turn'
 import reducer from './data/reducers'
-import start, { markCardsUnselected } from './data/model/game'
-import { hideAllCards } from './data/reducers/game';
+import { resetGame } from './data/reducers/game'
 
-const state = {
-  colors: [ 'orange', 'green', 'blue', 'yellow' ],
-  shapes: [ 'square', 'circle', 'triangle' ],
-  cards: [],
-  isCorrect: null,
-  hint: {
-    color: 'green',
-    shape: 'triangle'
-  },
-  left: 8,
-  moves: 0,
-  rank: 4,
-  seed: 42,
-}
-
-const gameState =
-  start()
-    .chain(markCardsUnselected)
-    .execWith(state)
+import initialize, {
+  initialState
+} from './data/model/initialize'
 
 log(
-  reducer(
-    gameState,
-    hideAllCards()
-  ).cards
+  reducer(null, resetGame())
 )
