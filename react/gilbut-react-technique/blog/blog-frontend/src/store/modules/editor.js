f5b2043ec86bad6a207284a599a1ed8cfc5bc82a
Jinho Hyeon <neverlish@gmail.com>
2018-12-29 00:38:48 +0900

    21 - 1 - 4 editor 모듈에 WRITE_POST 액션 생성

--- react/gilbut-react-technique/blog/blog-frontend/src/store/modules/editor.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/store/modules/editor.js
@@ -3,26 +3,38 @@ import { createAction, handleActions } from 'redux-actions';
 import { Map } from 'immutable';
 import { pender } from 'redux-pender';
 
+import * as api from 'lib/api';
+
 // action types
 const INITIALIZE = 'editor/INITIALIZE';
 const CHANGE_INPUT = 'editor/CHANGE_INPUT';
+const WRITE_POST = 'editor/WRITE_POST';
 
 // action creators
 export const initialize = createAction(INITIALIZE);
 export const changeInput = createAction(CHANGE_INPUT);
+export const writePost = createAction(WRITE_POST, api.writePost);
 
 // initial state
 const initialState = Map({
   title: '',
   markdown: '',
-  tags: ''
+  tags: '',
+  postId: null
 });
 
 // reducer
-export default handleActions({
+export default handleActions({ 
   [INITIALIZE]: (state, action) => initialState,
   [CHANGE_INPUT]: (state, action) => {
     const { name, value } = action.payload;
     return state.set(name, value);
-  }
+  },
+  ...pender({
+    type: WRITE_POST,
+    onSuccess: (state, action) => {
+      const { _id } = action.payload.data;
+      return state.set('postId', _id);
+    }
+  })
 }, initialState);
