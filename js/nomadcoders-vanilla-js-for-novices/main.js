9d53b8a598f54742bf22b827058b396ab0be8c36
Jinho Hyeon <neverlish@gmail.com>
2018-12-30 21:07:00 +0900

    1 - 10 Organizing Data with Objects

--- js/nomadcoders-vanilla-js-for-novices/main.js
+++ js/nomadcoders-vanilla-js-for-novices/main.js
@@ -1,4 +1,19 @@
-const something = 'Something'
-const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 54, true, 'stuff', 'lalala', something]
+const nicoInfo = {
+  name: 'Nico',
+  age: 33,
+  gender: 'Male',
+  isHandsome: true,
+  favMovies: ['Along the gods', 'LOTR', 'Oldboy'],
+  favFood: [
+    { 
+      name: 'Kimchi', 
+      fatty: false 
+    }, 
+    { 
+      name: 'Cheese burger', 
+      fatty: true 
+    }
+  ]
+}
 
-console.log(daysOfWeek)
+console.log(nicoInfo.favFood[0].name)
