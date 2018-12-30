c1d77896389b74673e0777ef4ba2552fec541b69
Jinho Hyeon <neverlish@gmail.com>
2018-12-30 22:48:29 +0900

    2 - 02 JS DOM Functions

---
+++ js/nomadcoders-vanilla-js-for-novices/index.js
@@ -0,0 +1,3 @@
+const title = document.getElementById('title')
+title.innerHTML = 'Hi! From JS'
+console.log(title)
--- js/nomadcoders-vanilla-js-for-novices/main.js
+++
@@ -1,9 +0,0 @@
-const calculator = {
-  plus: function (a, b) {
-    return a + b
-  }  
-}
-
-const plus = calculator.plus(5, 5)
-
-console.log(plus)
