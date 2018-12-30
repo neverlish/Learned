0975ee781415d811cfeb6a5a9f3c272751695c8f
Jinho Hyeon <neverlish@gmail.com>
2018-12-30 23:44:37 +0900

    2 - 04 Events and event handlers

--- js/nomadcoders-vanilla-js-for-novices/index.js
+++ js/nomadcoders-vanilla-js-for-novices/index.js
@@ -1,4 +1,7 @@
 const title = document.querySelector('#title')
-title.innerHTML = 'Hi! From JS'
-title.style.color = 'red'
-document.title = 'I own you now'
+
+function handleClick(event) {
+  console.log(event)
+}
+
+window.addEventListener('click', handleClick)
