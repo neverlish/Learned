fd8470e3d63ddbd80b8870232613fd59f5a3c0df
Jinho Hyeon <neverlish@gmail.com>
2018-12-31 20:00:55 +0900

    3 - 10 Getting the Weather part Two (API)

--- js/nomadcoders-vanilla-js-for-novices/weather.js
+++ js/nomadcoders-vanilla-js-for-novices/weather.js
@@ -1,4 +1,21 @@
+const weather = document.querySelector('.js-weather')
+
+const API_KEY = '241051bf13976dd3ddf8b8d9f247255e'
 const COORDS = 'coords'
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (json) {
+      const temperature = json.main.temp
+      const place = json.name
+      weather.innerText = `${temperature} @ ${place}`
+    })
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj))
@@ -12,6 +29,7 @@ function handleGeoSuccess(position) {
     longitude
   }
   saveCoords(coordsObj)
+  getWeather(latitude, longitude)
 }
 
 function handleGeoError() {
@@ -27,7 +45,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords()
   } else {
-
+    const parsedCoords = JSON.parse(localStorage.getItem(COORDS))
+    getWeather(parsedCoords.latitude, parsedCoords.longitude)
   }
 }
 
