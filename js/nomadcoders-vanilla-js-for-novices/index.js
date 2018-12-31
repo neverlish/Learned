2bf186e192d3038a1874ab0b1e4c6afd28bcc74d
Jinho Hyeon <neverlish@gmail.com>
2018-12-31 13:11:32 +0900

    2 - 07 DOM - If else - Function practice part Two

--- js/nomadcoders-vanilla-js-for-novices/index.js
+++ js/nomadcoders-vanilla-js-for-novices/index.js
@@ -1,31 +1,18 @@
 const title = document.querySelector('#title')
 
-const BASE_COLOR = 'rgb(52, 73, 94)'
-const OTHER_COLOR = '#7f8c8d'
+const CLICKED_CLASS = 'clicked'
 
 function handleClick() {
-  const currentColor = title.style.color
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR
+  const hasClass = title.classList.contains(CLICKED_CLASS)
+  if (!hasClass) {
+    title.classList.add(CLICKED_CLASS)
   } else {
-    title.style.color = BASE_COLOR
+    title.classList.remove(CLICKED_CLASS)
   }
 }
 
 function init() {
-  title.style.color = BASE_COLOR
   title.addEventListener('click', handleClick)
 }
 
 init()
-
-function handleOffline() {
-  console.log('Bye bye')
-}
-
-function handleOnline() {
-  console.log('Welcome back')
-}
-
-window.addEventListener('offline', handleOffline)
-window.addEventListener('online', handleOnline)
