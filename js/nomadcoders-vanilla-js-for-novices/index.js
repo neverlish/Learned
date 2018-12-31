198dda0f8253538f23d6bccd5e0331f29e976a2b
Jinho Hyeon <neverlish@gmail.com>
2018-12-31 12:22:28 +0900

    2 - 06 DOM - If else - Function practice

--- js/nomadcoders-vanilla-js-for-novices/index.js
+++ js/nomadcoders-vanilla-js-for-novices/index.js
@@ -1,9 +1,31 @@
-const age = prompt('How old are you')
+const title = document.querySelector('#title')
 
-if (age > 18 && age < 21) {
-  console.log('you can drink but you should not')
-} else if (age > 21) {
-  console.log('go ahead')
-} else {
-  console.log('too young')
+const BASE_COLOR = 'rgb(52, 73, 94)'
+const OTHER_COLOR = '#7f8c8d'
+
+function handleClick() {
+  const currentColor = title.style.color
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR
+  } else {
+    title.style.color = BASE_COLOR
+  }
 }
+
+function init() {
+  title.style.color = BASE_COLOR
+  title.addEventListener('click', handleClick)
+}
+
+init()
+
+function handleOffline() {
+  console.log('Bye bye')
+}
+
+function handleOnline() {
+  console.log('Welcome back')
+}
+
+window.addEventListener('offline', handleOffline)
+window.addEventListener('online', handleOnline)
