0f83d84687ea9936b934abe73c9042852670706b
Jinho Hyeon <neverlish@gmail.com>
2018-12-31 23:46:57 +0900

    1 - 15 Fetching the Movie Details

--- react/nomadcoders-nextjs-pwa-online-shoppingmall/pages/movie.js
+++ react/nomadcoders-nextjs-pwa-online-shoppingmall/pages/movie.js
@@ -1,18 +1,27 @@
+import Axios from 'axios'
 import Head from 'next/head'
 import { withRouter } from 'next/router'
 
 const Movie = props => (
   <div>
     <Head>
-      <title>{props.title} | Nomad Store</title>
+      <title>{props.movie.title} | Nomad Store</title>
     </Head>
-    <h1>{props.title}</h1>
-    <p>lalalala</p>
+    <h1>{props.movie.title}</h1>
+    <p>{props.movie.description_intro}</p>
   </div>
 )
 
-Movie.getInitialProps = async () => {
-  return { title: 'lalalalal' }
+Movie.getInitialProps = async context => {
+  const {
+    query: { id }
+  } = context
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await Axios.get(`https://yts.am/api/v2/movie_details.json?movie_id=${id}`)  
+  return { movie }
 }
 
 export default withRouter(Movie)
