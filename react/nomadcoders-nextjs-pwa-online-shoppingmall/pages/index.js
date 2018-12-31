829ba496bc8d52ca49275a6780e8eccf7ccc34a1
Jinho Hyeon <neverlish@gmail.com>
2018-12-31 23:41:24 +0900

    1 - 14 Fetching the movie list

--- react/nomadcoders-nextjs-pwa-online-shoppingmall/pages/index.js
+++ react/nomadcoders-nextjs-pwa-online-shoppingmall/pages/index.js
@@ -1,8 +1,21 @@
+import Axios from 'axios'
 import Head from 'next/head'
 import PostLink from '../components/PostLink'
 
 export default class extends React.Component {
+  static async getInitialProps() {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await Axios.get('https://yts.am/api/v2/list_movies.json')
+    return {
+      movies
+    }
+  }
+
   render() {
+    const { movies } = this.props
     return (
       <div>
         <Head>
@@ -10,12 +23,11 @@ export default class extends React.Component {
         </Head>
         <h1>Movies:</h1>
         <ul>
-          <li>
-            <PostLink title={'Something'} id={0} />
-          </li>
-          <li>
-            <PostLink title={'Something else'} id={1} />
-          </li>
+          {movies.map(movie => (
+            <li key={movie.id}>
+              <PostLink title={movie.title} id={movie.id} />
+            </li>
+          ))}
         </ul>
       </div>
     )
