8f8993fc1c17d03eb076c524c4494ea377683f40
Jinho Hyeon <neverlish@gmail.com>
2019-01-01 19:08:36 +0900

    3 - 05 Index Queries part One

---
+++ react/nomadcoders-nextjs-pwa-online-shoppingmall/pages/index/indexPresenter.js
@@ -0,0 +1,43 @@
+import { Layout } from 'antd'
+import Head from 'next/head'
+
+import Header from '../../components/Header'
+import Button from '../../components/Button'
+
+const { Content } = Layout;
+
+export default ({ data }) => (
+  <>
+    <Head>
+      <title>Home | Nomad Store</title>
+    </Head>
+    <Header
+      centerColumn={<h4>Nomad Store</h4>}
+      rightColumn={
+        <Button href='/cart' text='Cart' btnIcon={'shopping-cart'} />
+      }
+      leftColumn={<Button href='/search' text='search' btnIcon={'search'} />}
+    />
+    <Content style={{ padding: "0 50px" }}>
+      <div
+        style={{
+          display: "grid",
+          gridGap: "10px",
+          gridTemplateColumns: "repeat(auto-fit, minmax(100px, 1fr))",
+          width: "100%"
+        }}
+      >
+        {data &&
+          data.categories &&
+          data.categories.map(category => (
+            <Button
+              key={category.id}
+              href={`/category?name=${category.name.toLowerCase()}`}
+              hrefAs={`/category/${category.name.toLowerCase()}`}
+              text={category.name}
+            />
+          ))}
+      </div>
+    </Content>
+  </>
+)
