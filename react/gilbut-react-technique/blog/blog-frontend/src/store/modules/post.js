a9b8c5f4408db643a3af1c98675ca96406560382
Jinho Hyeon <neverlish@gmail.com>
2019-01-03 09:54:37 +0900

    21 - 2 - 2 post 모듈 생성

--- react/gilbut-react-technique/blog/blog-frontend/src/store/modules/post.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/store/modules/post.js
@@ -1,16 +1,28 @@
 import { createAction, handleActions } from 'redux-actions';
 
-import { Map } from 'immutable';
+import { Map, fromJS } from 'immutable';
 import { pender } from 'redux-pender';
 
+import * as api from 'lib/api';
+
 // action types
+const GET_POST = 'post/GET_POST';
 
 // action creators
+export const getPost = createAction(GET_POST, api.getPost);
 
 // initial state
-const initialState = Map({});
+const initialState = Map({
+  post: Map({})
+});
 
 // reducer
 export default handleActions({
-
+  ...pender({
+    type: GET_POST,
+    onSuccess: (state, action) => {
+      const { data: post } = action.payload;
+      return state.set('post', fromJS(post));
+    }
+  })
 }, initialState);
