516973698865156f2d2d5b8e42b9e9731dc325e3
Jinho Hyeon <neverlish@gmail.com>
2019-01-03 09:47:49 +0900

    21 - 2 포스트 보여주기 - 1 포스트 읽기 API 함수 생성

--- react/gilbut-react-technique/blog/blog-frontend/src/lib/api.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/lib/api.js
@@ -1,3 +1,5 @@
 import axios from 'axios';
 
 export const writePost = ({ title, body, tags }) => axios.post('/api/posts', { title, body, tags });
+
+export const getPost = (id) => axios.get(`/api/posts/${id}`);
