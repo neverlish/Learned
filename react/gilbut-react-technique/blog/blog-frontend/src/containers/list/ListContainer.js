a0ddf9c8fbad08e95a986ffecfe189172cf4a14e
Jinho Hyeon <neverlish@gmail.com>
2019-01-04 19:43:19 +0900

    21 - 3 - 3 ListContainer 컴포넌트 생성

---
+++ react/gilbut-react-technique/blog/blog-frontend/src/containers/list/ListContainer.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PostList from 'components/list/PostList';
+import Pagination from 'components/list/Pagination';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as listActions from 'store/modules/list';
+
+class ListContainer extends Component {
+  getPostList = () => {
+    const { tag, page, ListActions } = this.props;
+    ListActions.getPostList({
+      page,
+      tag
+    });
+  }
+
+  componentDidMount() {
+    this.getPostList();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.page !== this.props.page || prevProps.tag !== this.props.tag) {
+      this.getPostList();
+      document.documentElement.scrollTop = 0;
+    }
+  }
+
+  render() {
+    const { loading, posts, page, lastPage, tag } = this.props;
+    if (loading) return null;
+    return (
+      <div>
+        <PostList posts={posts} />
+        <Pagination page={page} lastPage={lastPage} tag={tag} />
+      </div>
+    );
+  }
+}
+
+export default connect(
+  (state) => ({
+    lastPage: state.list.get('lastPage'),
+    posts: state.list.get('posts'),
+    loading: state.pender.pending['list/GET_POST_LIST']
+  }),
+  (dispatch) => ({
+    ListActions: bindActionCreators(listActions, dispatch)
+  })
+)(ListContainer);
--- react/gilbut-react-technique/blog/blog-frontend/src/pages/ListPage.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/pages/ListPage.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
 import ListWrapper from 'components/list/ListWrapper';
-import PostList from 'components/list/PostList';
-import Pagination from 'components/list/Pagination';
+import ListContainer from 'containers/list/ListContainer';
 
-const ListPage = () => {
+const ListPage = ({ match }) => {
+  const { page = 1, tag } = match.params;
   return (
     <PageTemplate>
       <ListWrapper>
-        <PostList />
-        <Pagination />
+        <ListContainer
+          page={parseInt(page, 10)}
+          tag={tag}
+        />
       </ListWrapper>
     </PageTemplate>
   );
