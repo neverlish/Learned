a77d41dd96b7260278fbc7e1753d7c58773e6604
Jinho Hyeon <neverlish@gmail.com>
2019-01-05 00:15:26 +0900

    22 - 2 - 5 HTML 템플릿 생성

--- react/gilbut-react-technique/blog/blog-backend/src/ssr/index.js
+++ react/gilbut-react-technique/blog/blog-backend/src/ssr/index.js
@@ -1,6 +1,33 @@
 const render = require('./render').default;
+const manifest = require('../../../blog-frontend/build/asset-manifest.json');
+
+function buildHtml(rendered) {
+  return `
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+  <meta name="theme-color" content="#000000">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="shortcut icon" href="/favicon.ico">
+  <title>React App</title>
+  <link href="/${manifest['app.css']}" rel="stylesheet">
+</head>
+
+<body>
+  <noscript>You need to enable JavaScript to run this app.</noscript>
+  <div id="root">${rendered}</div>
+  <script type="text/javascript" src="/${manifest['vendor.js']}"></script>
+  <script type="text/javascript" src="/${manifest['app.js']}"></script>
+</body>
+
+</html>
+  `;
+}
 
 module.exports = async (ctx) => {
   const rendered = render(ctx);
-  ctx.body = rendered;
+  ctx.body = buildHtml(rendered);
 };
