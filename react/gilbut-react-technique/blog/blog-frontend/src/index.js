b719619b33bd45b5b2bfbc51aeb33f0e05eb4501
Jinho Hyeon <neverlish@gmail.com>
2019-01-05 01:15:45 +0900

    22 - 2 - 9 서버사이드 렌더링과 코드 스플리팅 충돌 해결

--- react/gilbut-react-technique/blog/blog-frontend/src/index.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/index.js
@@ -2,7 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 import registerServiceWorker from './registerServiceWorker';
+import routes from './routes';
+import { matchPath } from 'react-router';
 import 'styles/base.scss';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const render = async () => {
+  if (process.env.NODE_ENV === 'development') {
+    // 개발 모드에서는 바로 렌더링을 합니다.
+    return ReactDOM.render(<Root />, document.getElementById('root'));
+  }
+
+  // 프로덕션 모드에서는 일치하는 라우트를 찾아 미리 불러온 후 렌더링을 합니다.
+  const getComponents = [];
+  const { pathname } = window.location;
+
+  routes.forEach(
+    route => {
+      const match = matchPath(pathname, route);
+      if (!match) return;
+      const { getComponent } = route.component;
+      if (!getComponent) return;
+      getComponents.push(getComponent());
+    }
+  );
+
+  // getComponents가 끝날 때까지 기다립니다.
+  await Promise.all(getComponents);
+  // render가 아닌 hydrate를 사용합니다.
+  ReactDOM.hydrate(<Root />, document.getElementById('root'));
+}
+
+render();
 // registerServiceWorker();
