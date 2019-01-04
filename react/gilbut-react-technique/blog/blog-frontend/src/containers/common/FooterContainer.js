9e8bb3bf9e6f5970939e6ce44b4ffd3309ba84af
Jinho Hyeon <neverlish@gmail.com>
2019-01-04 23:04:32 +0900

    21 - 5 - 8 FooterContainer 완성

--- react/gilbut-react-technique/blog/blog-frontend/src/containers/common/FooterContainer.js
+++ react/gilbut-react-technique/blog/blog-frontend/src/containers/common/FooterContainer.js
@@ -6,21 +6,33 @@ import * as baseActions from 'store/modules/base';
 
 class FooterContainer extends Component {
   handleLoginClick = async () => {
-    const { BaseActions } = this.props;
+    const { BaseActions, logged } = this.props;
+    if (logged) {
+      try {
+        await BaseActions.logout();
+        window.location.reload();
+      } catch (e) {
+        console.log(e);
+      }
+      return;
+    }
     BaseActions.showModal('login');
+    BaseActions.initializeLoginModal();
   }
 
   render() {
     const { handleLoginClick } = this;
+    const { logged } = this.props;
+
     return (
-      <Footer onLoginClick={handleLoginClick} />
+      <Footer onLoginClick={handleLoginClick} logged={logged} />
     );
   }
 }
 
 export default connect(
   (state) => ({
-
+    logged: state.base.get('logged')
   }),
   (dispatch) => ({
     BaseActions: bindActionCreators(baseActions, dispatch)
