4a7e382095b312e637c37637b20727f8fd18c557
Jinho Hyeon <neverlish@gmail.com>
2019-01-14 20:33:23 +0900

    12 - 1 - 5 성공 패스-스루 확인

--- js/gilbut-js-pattern-test/12/Decorator/attendeeWebApiDecorator_03_tests.js
+++ js/gilbut-js-pattern-test/12/Decorator/attendeeWebApiDecorator_03_tests.js
@@ -16,7 +16,7 @@ describe('attendeeWebApiDecorator', function () {
   });
 
   describe('post(attendee)', function () {
-    describe('원post가 성공할 경우', function() {
+    describe('원post가 성공할 경우', function () {
       it('ID가 채번된 attendee로 귀결되는 프라미스를 반환한다', function (done) {
         decoratedWebApi.post(attendeeA).then(
           function onSuccess(attendee) {
@@ -73,7 +73,28 @@ describe('attendeeWebApiDecorator', function () {
   });
 
   describe('getAll()', function () {
-    describe('원getAll이 성공할 경우')
+    describe('원getAll이 성공할 경우', function () {
+      it('미결 상태인 레코드가 하나도 없다면 처리된 전체 레코드에 대한 프라미스를 반환한다', function (done) {
+        spyOn(baseWebApi, 'getAll').and.returnValue(
+          new Promise(function(resolve, reject) {
+            setTimeout(function () {
+              resolve([attendeeA, attendeeB]);
+            }, 1);
+          })
+        );
+
+        decoratedWebApi.getAll().then(
+          function onSuccess(attendees) {
+            expect(attendees.length).toBe(2);
+            done();
+          },
+          function onFailure() {
+            expect('getAll 실패').toBe(false);
+            done();
+          }
+        );
+      });
+    });
 
     describe('원getAll이 실패할 경우', function () {
       it('원버림 프라미스를 반환한다', function (done) {
