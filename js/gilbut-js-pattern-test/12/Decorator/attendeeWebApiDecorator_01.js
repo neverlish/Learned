97ccde7a4cfb005652ab818ab3599d3777f50a83
Jinho Hyeon <neverlish@gmail.com>
2019-01-14 20:20:56 +0900

    12 - 1 - 3 아무것도 하지 않는 장식자 작성

---
+++ js/gilbut-js-pattern-test/12/Decorator/attendeeWebApiDecorator_01.js
@@ -0,0 +1,15 @@
+// attendeeWebApiDecorator 초기 코드
+
+Conference.attendeeWebApiDecorator = function(baseWebapi) {
+  'use strict';
+
+  return {
+    post: function post(attendee) {
+
+    },
+
+    getAll: function getAll() {
+      
+    }
+  }
+}
