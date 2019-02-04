3423f28fc5c670d348fcb55984c071767d122f6a
Jinho Hyeon <neverlish@gmail.com>
2019-02-04 15:54:16 +0900

    04 Authorization TypeGraphQL

--- node/yt-benawad-typegraphql/src/index.ts
+++ node/yt-benawad-typegraphql/src/index.ts
@@ -16,7 +16,10 @@ const main = async () => {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver, LoginResolver, MeResolver]
+    resolvers: [RegisterResolver, LoginResolver, MeResolver],
+    authChecker: ({ context: { req } }) => {
+      return !!req.session.userId;
+    }
   });
 
   const apolloServer = new ApolloServer({
---
+++ node/yt-benawad-typegraphql/src/modules/middleware/logger.ts
@@ -0,0 +1,8 @@
+import { MiddlewareFn } from 'type-graphql';
+import { MyContext } from '../../types/MyContext';
+
+export const logger: MiddlewareFn<MyContext> = async ({ args }, next) => {
+  console.log('args: ', args);
+
+  return next();
+}
