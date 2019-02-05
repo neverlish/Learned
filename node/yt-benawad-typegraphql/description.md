8a1df95579c5b7082e19aeeaf0e4a5838c3025b8
Jinho Hyeon <neverlish@gmail.com>
2019-02-05 21:46:36 +0900

    08 Setting up a Test Environment TypeGraphQL

---
+++ node/yt-benawad-typegraphql/description.md
@@ -0,0 +1,3 @@
+## 08 Setting up a Test Environment TypeGraphQL
+
+- `yarn ts-jest config:init`
---
+++ node/yt-benawad-typegraphql/src/modules/user/register/Register.test.ts
@@ -0,0 +1,45 @@
+import { Connection } from 'typeorm';
+
+import { testConn } from '../../../test-utils/testConn';
+import { gCall } from '../../../test-utils/gCall';
+
+let conn: Connection;
+
+beforeAll(async () => {
+  conn = await testConn();
+});
+
+afterAll(async () => {
+  await conn.close();
+});
+
+const registerMutation = `
+  mutation Register($data: RegisterInput!) {
+    register(
+      data: $data
+    ) {
+      id
+      firstName
+      email
+      name
+    }
+  }
+`
+
+describe('Register', () => {
+  it('create user', async () => {
+    console.log(
+      await gCall({
+        source: registerMutation,
+        variableValues: {
+          data: {
+            firstName: 'bob',
+            lastName: 'bob2',
+            email: 'bob@bob.com',
+            password: 'asdfasdf'
+          }
+        }
+      })
+    );
+  });
+});
