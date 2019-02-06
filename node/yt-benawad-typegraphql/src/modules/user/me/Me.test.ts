3efe10d2ff59d147f818e10045ca646c8d8ad058
Jinho Hyeon <neverlish@gmail.com>
2019-02-06 10:09:08 +0900

    09 Testing Resolvers TypeGraphQL

--- node/yt-benawad-typegraphql/src/modules/user/me/Me.test.ts
+++ node/yt-benawad-typegraphql/src/modules/user/me/Me.test.ts
@@ -15,11 +15,9 @@ afterAll(async () => {
   await conn.close();
 });
 
-const registerMutation = `
-  mutation Register($data: RegisterInput!) {
-    register(
-      data: $data
-    ) {
+const meQuery = `
+  {
+    me {
       id
       firstName
       lastName
@@ -29,37 +27,45 @@ const registerMutation = `
   }
 `
 
-describe('Register', () => {
-  it('create user', async () => {
+describe('Me', () => {
+  it('get user', async () => {
     jest.setTimeout(30000);
 
-    const user = {
+    const user = await User.create({
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
       email: faker.internet.email(),
       password: faker.internet.password()
-    };
+    }).save();
 
     const response = await gCall({
-      source: registerMutation,
-      variableValues: {
-        data: user
-      }
+      source: meQuery,
+      userId: user.id
     });
 
     expect(response).toMatchObject({
       data: {
-        register: {
+        me: {
+          id: `${user.id}`,
           firstName: user.firstName,
           lastName: user.lastName,
-          email: user.email,
+          email: user.email
         }
       }
     });
-
-    const dbUser = await User.findOne({ where: { email: user.email } });
-    expect(dbUser).toBeDefined();
-    expect(dbUser!.confirmed).toBeFalsy();
-    expect(dbUser!.firstName).toBe(user.firstName);
   });
+
+  it('return null', async () => {
+    jest.setTimeout(30000);
+
+    const response = await gCall({
+      source: meQuery
+    });
+
+    expect(response).toMatchObject({
+      data: {
+        me: null
+      }
+    });
+  })
 });
