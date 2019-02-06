b75988f8d8040e31b444e0b0b993c14411638e05
Jinho Hyeon <neverlish@gmail.com>
2019-02-06 18:37:04 +0900

    4 Manually Sign Up New Users in React with AWS Amplify Auth Class

--- serverless/egghead-react-amplify/src/App.js
+++ serverless/egghead-react-amplify/src/App.js
@@ -2,14 +2,81 @@ import React, { Component } from 'react';
 
 import { withAuthenticator } from 'aws-amplify-react';
 
+import { Auth } from 'aws-amplify'
+
 class App extends Component {
+  state = {
+    username: '',
+    password: '',
+    email: '',
+    phone_number: '',
+    authenticationCode: '',
+    step: 0
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  signUp = async () => {
+    const { username, password, email, phone_number } = this.state;
+
+    try {
+      await Auth.signUp({ username, password, attributes: { email, phone_number } });
+      console.log('successfully signed up!');
+      this.setState({ step: 1 });
+    } catch (err) {
+      console.log('error signing up: ', err);
+    }
+  }
+
+  confirmSignup = async () => {
+    const { username, authenticationCode } = this.state;
+
+    try {
+      await Auth.confirmSignUp(username, authenticationCode);
+      console.log('user successfully signed up!');
+    } catch (err) {
+      console.log('error confirming sign up: ', err);
+    }
+  }
+
   render() {
     return (
       <div className="App">
+        {
+          this.state.step === 0 && (
+            <div>
+              <input placeholder='username' onChange={this.onChange} name='username' style={styles.input} />
+              <input placeholder='password' onChange={this.onChange} name='password' type='password' style={styles.input} />
+              <input placeholder='email' onChange={this.onChange} name='email' style={styles.input} />
+              <input placeholder='phone number' onChange={this.onChange} name='phone_number' style={styles.input} />
+              <button onClick={this.signUp}>Sign Up</button>
+            </div>
+          )
+        }
 
+        {
+          this.state.step === 1 && (
+            <div>
+              <input placeholder='username' onChange={this.onChange} name='username' style={styles.input} />
+              <input placeholder='authentication code' onChange={this.onChange} name='authenticationCode' style={styles.input} />
+              <button onClick={this.confirmSignup}>Sign Up</button>
+            </div>
+          )
+        }
       </div>
     );
   }
 }
+
+const styles = {
+  input: {
+    height: 35,
+    margin: 5
+  }
+};
 
 export default withAuthenticator(App, { includeGreetings: true });
