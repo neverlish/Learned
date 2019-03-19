c11a98c92016463bd070e2c4503acb056fcb1bc9
Jinho Hyeon <neverlish@gmail.com>
2019-03-19 23:53:24 +0900

    03 GraphQL Code Generator Next.js

--- react/yt-benawad-ts-nextjs-graphql/pages/index.tsx
+++ react/yt-benawad-ts-nextjs-graphql/pages/index.tsx
@@ -1,35 +1,30 @@
-import { gql } from 'apollo-boost';
 import Link from 'next/link';
 import * as React from 'react';
-import { Mutation } from 'react-apollo';
 import Layout from '../components/Layout';
+import { LoginComponent } from '../generated/apolloComponents';
 
 const IndexPage: React.FunctionComponent = () => {
   return (
-    <Layout title="Home | Next.js + TypeScript Example">
+    <Layout title='Home | Next.js + TypeScript Example'>
       <h1>hello Next.js 👋</h1>
       <p><Link href='/about'><a>About</a></Link></p>
-      <Mutation
-        mutation={gql`
-          mutation {
-            login(email:"test@email.com",password:"1234") {
-              id
-              firstName
-              lastName
-              email
-              name
-            }
-          }
-        `}
-      >
-        {(mutate) => <button onClick={async () => {
-          const response = await mutate();
-          console.log(response);
-        }}>call login mutation</button>}
-      </Mutation>
+      <LoginComponent>
+        {(mutate) => (
+          <button
+            onClick={async () => {
+              const response = await mutate({
+                variables: { email: 'test@test.com', password: 'password' }
+              });
 
+              console.log(response);
+            }}
+          >
+            call login mutation
+          </button>
+        )}
+      </LoginComponent>
     </Layout>
-  )
-}
+  );
+};
 
 export default IndexPage;
