5f30f27fbce26737c300d45a9a526e8bfff55f56
Jinho Hyeon <neverlish@gmail.com>
2019-03-20 01:26:59 +0900

    04 Formik Register Form Next.js

---
+++ react/yt-benawad-ts-nextjs-graphql/components/fields/InputField.tsx
@@ -0,0 +1,16 @@
+import React, { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import { FieldProps } from 'formik';
+
+type InputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+
+export const InputField = ({
+  field,
+  form: _,
+  ...props
+}: FieldProps & InputProps) => {
+  return (
+    <div>
+      <input {...field} {...props} />
+    </div>
+  )
+}
