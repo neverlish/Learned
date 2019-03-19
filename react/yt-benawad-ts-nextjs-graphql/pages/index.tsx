dbb6f508175cec9622fafd4657ddeae599c7cf9c
Jinho Hyeon <neverlish@gmail.com>
2019-03-19 23:26:07 +0900

    02 Setting Up Apollo in Next.js

--- react/yt-benawad-ts-nextjs-graphql/pages/index.tsx
+++ react/yt-benawad-ts-nextjs-graphql/pages/index.tsx
@@ -1,12 +1,33 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { gql } from 'apollo-boost';
+import Link from 'next/link';
+import * as React from 'react';
+import { Mutation } from 'react-apollo';
+import Layout from '../components/Layout';
 
 const IndexPage: React.FunctionComponent = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>hello Next.js 👋</h1>
       <p><Link href='/about'><a>About</a></Link></p>
+      <Mutation
+        mutation={gql`
+          mutation {
+            login(email:"test@email.com",password:"1234") {
+              id
+              firstName
+              lastName
+              email
+              name
+            }
+          }
+        `}
+      >
+        {(mutate) => <button onClick={async () => {
+          const response = await mutate();
+          console.log(response);
+        }}>call login mutation</button>}
+      </Mutation>
+
     </Layout>
   )
 }
