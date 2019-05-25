690e173f3e9078c1e7ad9625cbc4f395582f951a
Jinho Hyeon <neverlish@gmail.com>
2019-05-26 01:23:30 +0900

    14 Convert GraphQL List Type to a Relay Connection Type

--- graphql/egghead-build-graphql-server/index.js
+++ graphql/egghead-build-graphql-server/index.js
@@ -10,12 +10,16 @@ const {
   GraphQLInt,
   GraphQLBoolean,
   GraphQLNonNull,
-  GraphQLList,
   GraphQLInputObjectType,
 } = require('graphql');
 
 const { getVideoById, getVideos, createVideo } = require('./src/data');
-const { globalIdField } = require('graphql-relay');
+const {
+  globalIdField,
+  connectionDefinitions,
+  connectionFromPromisedArray,
+  connectionArgs,
+} = require('graphql-relay');
 const { nodeInterface, nodeField } = require('./src/node');
 
 const PORT = process.env.PORT || 3000;
@@ -43,14 +47,31 @@ const videoType = new GraphQLObjectType({
 });
 exports.videoType = videoType;
 
+const { connectionType: VideoConnection } = connectionDefinitions({
+  nodeType: videoType,
+  connectionFields: () => ({
+    totalCount: {
+      type: GraphQLInt,
+      description: 'A count of the total number of objects in this connection.',
+      resolve: (conn) => {
+        return conn.edges.length;
+      },
+    },
+  }),
+});
+
 const queryType = new GraphQLObjectType({
   name: 'QueryType',
   description: 'The root query type.',
   fields: {
     node: nodeField,
     videos: {
-      type: new GraphQLList(videoType),
-      resolve: getVideos,
+      type: VideoConnection,
+      args: connectionArgs,
+      resolve: (_, args) => connectionFromPromisedArray(
+        getVideos(),
+        args,
+      ),
     },
     video: {
       type: videoType,
