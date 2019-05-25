5ff0dc799953f4c00b4ea14f69df116775e9c8bb
Jinho Hyeon <neverlish@gmail.com>
2019-05-26 01:40:21 +0900

    15 Use Relay’s Input Object Mutations

--- graphql/egghead-build-graphql-server/index.js
+++ graphql/egghead-build-graphql-server/index.js
@@ -19,6 +19,7 @@ const {
   connectionDefinitions,
   connectionFromPromisedArray,
   connectionArgs,
+  mutationWithClientMutationId,
 } = require('graphql-relay');
 const { nodeInterface, nodeField } = require('./src/node');
 
@@ -88,9 +89,9 @@ const queryType = new GraphQLObjectType({
   },
 });
 
-const videoInputType = new GraphQLInputObjectType({
-  name: 'VideoInput',
-  fields: {
+const videoMutation = mutationWithClientMutationId({
+  name: 'AddVideo',
+  inputFields: {
     title: {
       type: new GraphQLNonNull(GraphQLString),
       description: 'The title of the video.',
@@ -104,23 +105,23 @@ const videoInputType = new GraphQLInputObjectType({
       description: 'Whether or not the viewer is released.',
     },
   },
+  outputFields: {
+    video: {
+      type: videoType,
+    },
+  },
+  mutateAndGetPayload: (args) => new Promise((resolve, reject) => {
+    Promise.resolve(createVideo(args))
+      .then((video) => resolve({ video }))
+      .catch(reject);
+  }),
 });
 
 const mutationType = new GraphQLObjectType({
   name: 'Mutation',
   description: 'The root Mutation type.',
   fields: {
-    createVideo: {
-      type: videoType,
-      args: {
-        video: {
-          type: new GraphQLNonNull(videoInputType),
-        },
-      },
-      resolve: (_, args) => {
-        return createVideo(args.video);
-      },
-    },
+    createVideo: videoMutation,
   },
 })
 
