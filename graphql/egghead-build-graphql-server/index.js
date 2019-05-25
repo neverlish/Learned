f280a798d091319d1395833dbb3587e28dce8fd1
Jinho Hyeon <neverlish@gmail.com>
2019-05-26 00:28:35 +0900

    07 Use Arguments in a GraphQL Query

--- graphql/egghead-build-graphql-server/index.js
+++ graphql/egghead-build-graphql-server/index.js
@@ -11,6 +11,8 @@ const {
   GraphQLBoolean,
 } = require('graphql');
 
+const { getVideoById } = require('./src/data');
+
 const PORT = process.env.PORT || 3000;
 const server = express();
 
@@ -43,14 +45,15 @@ const queryType = new GraphQLObjectType({
   fields: {
     video: {
       type: videoType,
-      resolve: () => new Promise((resolve) => {
-        resolve({
-          id: 'a',
-          title: 'GraphQL',
-          duration: 180,
-          watched: false,
-        });
-      }),
+      args: {
+        id: {
+          type: GraphQLID,
+          description: 'The id of the video.',
+        },
+      },
+      resolve: (_, args) => {
+        return getVideoById(args.id);
+      },
     },
   },
 });
@@ -58,22 +61,6 @@ const queryType = new GraphQLObjectType({
 const schema = new GraphQLSchema({
   query: queryType,
 });
-
-const videoA = {
-  id: 'a',
-  title: 'Create a GraphQL Schema',
-  duration: 120,
-  watched: true,
-};
-
-const videoB = {
-  id: 'b',
-  title: 'Ember.js CLI',
-  duration: 240,
-  watched: false,
-};
-
-const videos = [videoA, videoB];
 
 server.use('/graphql', graphqlHTTPServer({
   schema,
