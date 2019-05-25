3c82ebaeb1cfc50936ff8fe9234e3403dbe8e9b8
Jinho Hyeon <neverlish@gmail.com>
2019-05-26 02:00:02 +0900

    03 Update Vuex State with Mutations and MapMutations in Vue.js

--- vue/egghead-nuxt-vuex/pages/index.vue
+++ vue/egghead-nuxt-vuex/pages/index.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>{{ counter }}</div>
+  <div>
+    {{ counter }}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapMutations } from "vuex";
 export default {
   computed: {
     ...mapState({
       counter: state => state.counter
     })
+  },
+  methods: {
+    ...mapMutations(["increment", "decrement"])
   }
 };
 </script>
