d42fec644e6d9ab564bfb2d743fd73e16e0c4f17
Jinho Hyeon <neverlish@gmail.com>
2019-05-26 23:19:19 +0900

    05 Render on the DOM with RxJS

--- rxjs/egghead-intro-reactive-programming/index.js
+++ rxjs/egghead-intro-reactive-programming/index.js
@@ -3,12 +3,33 @@ var requestStream = Rx.Observable.of('https://api.github.com/users');
 var responseStream = requestStream
   .flatMap(requestUrl => Rx.Observable.fromPromise(jQuery.getJSON(requestUrl)));
 
-responseStream.subscribe(response => {
-  console.log(response);
+function createSuggestionStream(responseStream) {
+  return responseStream.map(listUser =>
+    listUser[Math.floor(Math.random() * listUser.length)]
+  );
+}
+
+var suggestion1Stream = createSuggestionStream(responseStream);
+var suggestion2Stream = createSuggestionStream(responseStream);
+var suggestion3Stream = createSuggestionStream(responseStream);
+
+function renderSuggestion(userData, selector) {
+  var element = document.querySelector(selector);
+  var usernameEl = element.querySelector('.username');
+  usernameEl.href = userData.html_url;
+  usernameEl.textContent = userData.login;
+  var imgEl = element.querySelector('img');
+  imgEl.src = userData.avatar_url;
+}
+
+suggestion1Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion1');
 });
 
-// requestStream.subscribe(requestUrl => {
-//   responseStream.subscribe(response => {
-//     console.log(response);
-//   });
-// });
+suggestion2Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion2');
+});
+
+suggestion3Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion3');
+});
