de7f396864d1f60a26faeaac2ca848fcc734f40c
Jinho Hyeon <neverlish@gmail.com>
2019-06-01 21:15:35 +0900

    3 - 10 GraphQL Fragments + Product Page part One

--- react/nomadcoders-nextjs-pwa-online-shoppingmall/components/ProductCard.js
+++ react/nomadcoders-nextjs-pwa-online-shoppingmall/components/ProductCard.js
@@ -1,21 +1,19 @@
 import Link from 'next/link';
-import { Card, Col, Icon } from 'antd';
+import { Card, Icon } from 'antd';
 import { Meta } from 'antd/lib/list/Item';
 
 export default ({ id, name, subtitle, photoUrl }) => (
-  <Col style={{ marginBottom: '25px' }}>
-    <Card
-      hoverable
-      actions={[
-        <Link href={`/product?id=${id}`} as={`/product/${id}`}>
-          <a>
-            <Icon type='eye' theme='outlined' />
-          </a>
-        </Link>
-      ]}
-      cover={<img alt='example' src={photoUrl} height={'200px'} />}
-    >
-      <Meta title={name} description={subtitle} />
-    </Card>
-  </Col>
+  <div style={{ marginBottom: '25px' }}>
+    <Link href={`/product?id=${id}`} as={`/product/${id}`}>
+      <a>
+        <Card
+          hoverable
+          actions={[<Icon type='eye' theme='outlined' />]}
+          cover={<img alt='example' src={photoUrl} height={'200px'} />}
+        >
+          <Meta title={name} description={subtitle} />
+        </Card>
+      </a>
+    </Link>
+  </div>
 );
