e93e3e1874e10293c260b13f1df037f5a777d7df
Jinho Hyeon <neverlish@gmail.com>
2019-06-01 20:39:00 +0900

    3 - 09 Search Screen part Two

--- react/nomadcoders-nextjs-pwa-online-shoppingmall/pages/search/searchContainer.js
+++ react/nomadcoders-nextjs-pwa-online-shoppingmall/pages/search/searchContainer.js
@@ -1,4 +1,6 @@
-import SearchPresenter from './searchPresenter';
+import { Query } from "react-apollo";
+import SearchPresenter from "./searchPresenter";
+import { SEARCH_QUERY } from "./searchQueries";
 
 export default class extends React.Component {
   state = {
@@ -9,14 +11,22 @@ export default class extends React.Component {
   render() {
     const { searchTerm, canSearch } = this.state;
     return (
-      <SearchPresenter
-        searchTerm={searchTerm}
-        updateSearchTerm={this._updateSearchTerm}
-      />
+      <Query skip={!canSearch} query={SEARCH_QUERY} variables={{ searchTerm }}>
+        {({ data }) => (
+          <SearchPresenter
+            searchTerm={searchTerm}
+            updateSearchTerm={this._updateSearchTerm}
+            data={data}
+          />
+        )}
+      </Query>
     );
   }
 
   _updateSearchTerm = (event) => {
+    this.setState({
+      canSearch: false
+    });
     clearTimeout(this.searchTimeout);
     const {
       target: { value }
@@ -24,6 +34,9 @@ export default class extends React.Component {
     this.setState({
       searchTerm: value
     });
-    this.searchTimeout = setTimeout(() => console.log("Searching..."), 500);
+    this.searchTimeout = setTimeout(
+      () => this.setState({ canSearch: true }),
+      500
+    );
   }
 }
