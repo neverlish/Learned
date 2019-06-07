6610b37bdbb5ae5c684ba17f60219d46b5e9408c
Jinho Hyeon <neverlish@gmail.com>
2019-06-07 23:23:12 +0900

    02 - 24 Log In and User Profile Controller

--- fullstack/nomadcoders-youtube-clone/controllers/userController.js
+++ fullstack/nomadcoders-youtube-clone/controllers/userController.js
@@ -18,7 +18,12 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Log Out" });
 export const userDetail = (req, res) =>
