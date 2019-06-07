1ccd19cac2d9e4adc5919a2e54eb5e146ffb0ccd
Jinho Hyeon <neverlish@gmail.com>
2019-06-07 21:33:27 +0900

    02 - 13 Installing Pug

--- fullstack/nomadcoders-youtube-clone/app.js
+++ fullstack/nomadcoders-youtube-clone/app.js
@@ -6,12 +6,13 @@ import bodyParser from 'body-parser';
 
 import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
-import videoRouter from './routers/videoRouter'; 
+import videoRouter from './routers/videoRouter';
 
 import routes from './routes';
 
 const app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
