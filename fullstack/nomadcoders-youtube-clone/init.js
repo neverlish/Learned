ae62a942ae0e69d0130271575369fe90b642695f
Jinho Hyeon <neverlish@gmail.com>
2019-06-07 23:54:46 +0900

    03 - 02 Configuring Dot Env

--- fullstack/nomadcoders-youtube-clone/init.js
+++ fullstack/nomadcoders-youtube-clone/init.js
@@ -1,7 +1,9 @@
-import './db';
+import "./db";
 import app from './app';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handlerListening = () => console.log(`Listening on http://localhost:${PORT}`);
 
