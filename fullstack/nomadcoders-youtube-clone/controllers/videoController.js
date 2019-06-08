01359e30f7a69ad705a2d276600e30e7594c4f47
Jinho Hyeon <neverlish@gmail.com>
2019-06-08 15:59:17 +0900

    03 - 05 Home Controller Finished

--- fullstack/nomadcoders-youtube-clone/controllers/videoController.js
+++ fullstack/nomadcoders-youtube-clone/controllers/videoController.js
@@ -1,7 +1,14 @@
 import routes from "../routes";
+import Video from '../models/Video';
 
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render('home', { pageTitle: 'Home', videos: [] });
+  }
 };
 
 export const search = (req, res) => {
