7c1d2e040f591465e95607ed12c2a05e3ac0f86a
Jinho Hyeon <neverlish@gmail.com>
2019-06-09 10:04:55 +0900

    3-1 - State와 Mobx 3

--- react/yjdcodelab-nextjs/pages/index.js
+++ react/yjdcodelab-nextjs/pages/index.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
 import faker from 'faker';
+import { observable, decorate } from 'mobx';
+import { observer } from 'mobx-react';
+
+class Data {
+  name = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+  };
+  avatar = faker.image.avatar();
+  email = faker.internet.email();
+}
+
+decorate(Data, {
+  name: observable,
+  avatar: observable,
+  email: observable
+});
 
 class Index extends Component {
-  state = {
-    name: {
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-    },
-    avatar: faker.image.avatar(),
-    email: faker.internet.email()
-  };
+  data = new Data();
 
   generate = () => {
-    this.setState({
-      name: {
-        firstName: faker.name.firstName(),
-        lastName: this.state.name.lastName,
-      },
-    }, () => {
-      this.setState({ email: faker.internet.email() }, () => {
-        this.setState({ avatar: faker.image.avatar() });
-      });
-    });
+    this.data.avatar = faker.image.avatar();
+    this.data.email = faker.internet.email();
+    this.data.name.firstName = faker.name.firstName();
   }
+
   render() {
     return (
       <div>
@@ -30,17 +34,19 @@ class Index extends Component {
         <dl className='row'>
           <dt className='col-sm-3'>Avatar</dt>
           <dt className='col-sm-9'>
-            <img src={this.state.avatar} />
+            <img src={this.data.avatar} />
           </dt>
           <dt className='col-sm-3'>Name</dt>
-          <dt className='col-sm-9'>{this.state.name.firstName} {this.state.name.lastName}</dt>
+          <dt className='col-sm-9'>{this.data.name.firstName} {this.data.name.lastName}</dt>
           <dt className='col-sm-3'>Email</dt>
-          <dt className='col-sm-9'>{this.state.email}</dt>
+          <dt className='col-sm-9'>{this.data.email}</dt>
         </dl>
         <button onClick={this.generate}>Generate</button>
       </div>
     );
   }
 }
+
+Index = observer(Index);
 
 export default Index;
