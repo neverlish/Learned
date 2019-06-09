677292d0ba528ff911a7de69a70a43a2494af66a
Jinho Hyeon <neverlish@gmail.com>
2019-06-09 17:34:04 +0900

    06 - 02 Local Authentication with Passport part Two

--- fullstack/nomadcoders-youtube-clone/controllers/userController.js
+++ fullstack/nomadcoders-youtube-clone/controllers/userController.js
@@ -1,19 +1,27 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   const {
-    body: { password, password2 }
+    body: { name, email, password, password2 }
   } = req;
   if (password !== password2) {
     res.status(400);
     res.render("join", { pageTitle: "Join" });
   } else {
-    // To Do: Register User
-    // To Do: Log user in
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
+    }
     res.redirect(routes.home);
   }
 };
