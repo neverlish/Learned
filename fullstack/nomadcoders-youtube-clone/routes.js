60ac722c53a0c5a51179ee2b9924dacf59895d96
Jinho Hyeon <neverlish@gmail.com>
2019-06-09 23:48:40 +0900

    06 - 10 User Detail + Facebook Login Part One

--- fullstack/nomadcoders-youtube-clone/routes.js
+++ fullstack/nomadcoders-youtube-clone/routes.js
@@ -23,6 +23,10 @@ const DELETE_VIDEO = "/:id/delete";
 const GITHUB = "/auth/github";
 const GITHUB_CALLBACK = "/auth/github/callback";
 
+// Facebook
+const FB = "/auth/facebook";
+const FB_CALLBACK = "/auth/facebook/callback";
+
 const routes = {
   home: HOME,
   join: JOIN,
@@ -60,7 +64,9 @@ const routes = {
   },
   gitHub: GITHUB,
   github_callback: GITHUB_CALLBACK,
-  me: ME
+  me: ME,
+  facebook: FB,
+  facebookCallback: FB_CALLBACK
 };
 
 export default routes;
