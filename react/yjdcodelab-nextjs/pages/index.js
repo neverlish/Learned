43380a975156981f8dba75f57d4c9fb08f96f6a3
Jinho Hyeon <neverlish@gmail.com>
2019-06-09 12:53:24 +0900

    4-2 - Firebase Firestore 1

--- react/yjdcodelab-nextjs/pages/index.js
+++ react/yjdcodelab-nextjs/pages/index.js
@@ -23,6 +23,36 @@ class Index extends React.Component {
       });
   }
 
+  write = () => {
+    const now = new Date();
+    const feed = {
+      content: this.textarea.value,
+      author: {
+        uid: store.user.uid,
+        displayName: store.user.displayName,
+        photoURL: store.user.photoURL,
+        email: store.user.email,
+      },
+      create_at: now,
+      update_at: now,
+    };
+
+    const db = firebase.firestore();
+    const settings = { timestampsInSnapshots: true };
+    db.settings(settings);
+    db.collection('feeds')
+      .doc('path-0')
+      .set(feed)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => {
+        alert('error: ' + error.message);
+        console.log(error);
+      });
+    console.log(this.textarea.value);
+  };
+
   render() {
     return (
       <Layout>
@@ -31,7 +61,10 @@ class Index extends React.Component {
           <button className='btn btn-primary' onClick={this.login}>로그인</button>
         )}
         {store.user !== null && (
-          <div>입력폼</div>
+          <div>
+            <textarea ref={ref => this.textarea = ref} rows='10' className='form-control mb-2' />
+            <button className='btn btn-primary' onClick={this.write}>전송</button>
+          </div>
         )}
       </Layout>
     );
