e5535dbedb98161c8ef3f4b846340cb61d7d18f7
Jinho Hyeon <neverlish@gmail.com>
2019-07-02 22:10:37 +0900

    4 - 02 - 06 목록 뷰 만들기

--- django/baepeu-python-webprogramming/bookmark/bookmark/views.py
+++ django/baepeu-python-webprogramming/bookmark/bookmark/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Bookmark
+
+class BookmarkListView(ListView):
+    model = Bookmark
