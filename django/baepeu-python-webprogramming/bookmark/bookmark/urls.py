6e9f75ffb9d694df3c00ddbb91186f6e83495c71
Jinho Hyeon <neverlish@gmail.com>
2019-07-02 22:24:24 +0900

    4 - 02 - 10 북마크 확인 기능 구현

--- django/baepeu-python-webprogramming/bookmark/bookmark/urls.py
+++ django/baepeu-python-webprogramming/bookmark/bookmark/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookmarkListView, BookmarkCreateView
+from .views import *
 
 urlpatterns = [
     path('', BookmarkListView.as_view(), name='list'),
     path('add/', BookmarkCreateView.as_view(), name='add'),
+    path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),
 ]
--- django/baepeu-python-webprogramming/bookmark/bookmark/views.py
+++ django/baepeu-python-webprogramming/bookmark/bookmark/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 
 from .models import Bookmark
@@ -12,3 +13,6 @@ class BookmarkCreateView(CreateView):
     fields = ['site_name', 'url']
     success_url = reverse_lazy('list')
     template_name_suffix = '_create'
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
