05598e140d1da812b110e6d330018a099e06c7f6
Jinho Hyeon <neverlish@gmail.com>
2019-07-02 22:54:59 +0900

    4 - 04 배포하기-Pythonanywhere - 02 깃 설치 및 소스코드

--- django/baepeu-python-webprogramming/bookmark/config/settings.py
+++ django/baepeu-python-webprogramming/bookmark/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i$qfm6h(p!=5umvih&f^9lai84&20r_8r-p_7v4l^xnl%qaz_3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
