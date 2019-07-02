4c46ab49e57cfaceaf9af28a7e965469a9049b96
Jinho Hyeon <neverlish@gmail.com>
2019-07-02 22:21:36 +0900

    4 - 02 - 09 북마크 추가 기능 구현

--- django/baepeu-python-webprogramming/bookmark/bookmark/urls.py
+++ django/baepeu-python-webprogramming/bookmark/bookmark/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookmarkListView
+from .views import BookmarkListView, BookmarkCreateView
 
 urlpatterns = [
     path('', BookmarkListView.as_view(), name='list'),
+    path('add/', BookmarkCreateView.as_view(), name='add'),
 ]
--- django/baepeu-python-webprogramming/bookmark/bookmark/views.py
+++ django/baepeu-python-webprogramming/bookmark/bookmark/views.py
@@ -1,6 +1,14 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .models import Bookmark
 
 class BookmarkListView(ListView):
     model = Bookmark
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    success_url = reverse_lazy('list')
+    template_name_suffix = '_create'
