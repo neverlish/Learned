157d3aa5942d7bc037ffd94b48d16281479b86bf
Jinho Hyeon <neverlish@gmail.com>
2019-07-02 22:29:43 +0900

    4 - 02 - 12 북마크 삭제 기능 구현

--- django/baepeu-python-webprogramming/bookmark/bookmark/views.py
+++ django/baepeu-python-webprogramming/bookmark/bookmark/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 
@@ -21,3 +21,7 @@ class BookmarkUpdateView(UpdateView):
     model = Bookmark
     fields = ['site_name', 'url']
     template_name_suffix = '_update'
+
+class BookmarkDeleteView(DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('list')
