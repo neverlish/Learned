97c4fa08bb03e66df04c9b9da2aa42ada44525dc
Jinho Hyeon <neverlish@gmail.com>
2019-07-03 10:11:46 +0900

    5 - 03 - 06 뷰 만들기

--- django/baepeu-python-webprogramming/dstagram/photo/views.py
+++ django/baepeu-python-webprogramming/dstagram/photo/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.shortcuts import redirect
 
-# Create your views here.
+from .models import Photo
+
+def photo_list(request):
+    photos = Photo.objects.all()
+    return render(request, 'photo/list.html', { 'photos': photos })
+
+class PhotoUploadView(CreateView):
+    model = Photo
+    fields = ['photo', 'text']
+    template_name = 'photo/upload.html'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({ 'form': form })
+
+class PhotoDeleteView(DeleteView):
+    model = Photo
+    success_url = '/'
+    template_name = 'photo/delete.html'
+
+class PhotoUpdateView(UpdateView):
+    model = Photo
+    fields = ['photo', 'text']
+    template_name = 'photo/update.html'
