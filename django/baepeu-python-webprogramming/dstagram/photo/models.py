c60f4e85fc79de64891e8c8e770a038eccd6e45c
Jinho Hyeon <neverlish@gmail.com>
2019-07-03 09:59:13 +0900

    5 - 03 - 02 모델 만들기

--- django/baepeu-python-webprogramming/dstagram/photo/models.py
+++ django/baepeu-python-webprogramming/dstagram/photo/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Photo(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_photos')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', default='photos/no_image.png')
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated']
+
+    def __str__(self):
+        return self.author.username + " " + self.created.strftime("%Y-%m-%d %H:%M:%S")
+
+    def get_absolute_url(self):
+        return reverse('photo:photo_detail', args=[str(self.id)])
