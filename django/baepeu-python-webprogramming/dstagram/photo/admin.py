365fae4535091325e1d6f29d698df76e1e5d3000
Jinho Hyeon <neverlish@gmail.com>
2019-07-03 10:07:11 +0900

    5 - 03 - 05 관리자 페이지 커스터마이징

--- django/baepeu-python-webprogramming/dstagram/photo/admin.py
+++ django/baepeu-python-webprogramming/dstagram/photo/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Photo
 
-admin.site.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'created', 'updated']
+    raw_id_fields = ['author']
+    list_filter = ['created', 'updated', 'author']
+    search_fields = ['text', 'created']
+    ordering = ['-updated', '-created']
+
+admin.site.register(Photo, PhotoAdmin)
