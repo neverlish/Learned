9519ddfd0d956a8661f7473657a35eaa973abaeb
Jinho Hyeon <neverlish@gmail.com>
2019-07-03 10:19:05 +0900

    5 - 03 - 07 URL 연결

---
+++ django/baepeu-python-webprogramming/dstagram/photo/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.views.generic.detail import DetailView
+
+from .views import *
+from .models import Photo
+
+app_name = 'photo'
+
+urlpatterns = [
+    path('', photo_list, name='photo_list'),
+    path('detail/<int:pk>/', DetailView.as_view(model=Photo, template_name='photo/detail.html'), name='photo_detail'),
+    path('upload/', PhotoUploadView.as_view(), name='photo_upload'),
+    path('delete/<int:pk>/', PhotoDeleteView.as_view(), name='photo_delete'),
+    path('update/<int:pk>/', PhotoUpdateView.as_view(), name='photo_update'),
+]
