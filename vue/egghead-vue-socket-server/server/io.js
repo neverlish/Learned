5f3c9e45df313680c304a1f296dbeaabd6cbd31a
Jinho Hyeon <neverlish@gmail.com>
2019-07-03 23:34:04 +0900

    2 Set up a simple Socket.io client to connect to the server.

--- vue/egghead-vue-socket-server/server/io.js
+++ vue/egghead-vue-socket-server/server/io.js
@@ -3,10 +3,17 @@ const logger = require('./logger');
 
 let io = null;
 
-exports.initialize = function (server) {
-  io = sio(server);
-}
-
 exports.io = function () {
   return io;
+}
+
+exports.initialize = function (server) {
+  io = sio(server);
+  io.on('connection', (socket) => {
+    logger.debug(`A user connected with ${socket.id}`);
+
+    socket.on('disconnect', function () {
+      logger.debug(`A user disconnected with ${socket.id}`);
+    });
+  });
 }
--- vue/egghead-vue-socket-server/server/server.js
+++ vue/egghead-vue-socket-server/server/server.js
@@ -7,10 +7,11 @@ const app = express();
 const pjson = require('./package.json');
 const path = require('path');
 const port = process.env.PORT || 8500;
-
 const server = require('http').Server(app);
-const io = require('./io').initialize(server);
 const logger = require('./logger');
+
+const io = require('./io').initialize(server);
+const global_socket = require('./io').io();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,3 +20,13 @@ server.listen(port);
 logger.info(`${pjson.name} Server Started >> `);
 logger.info(`running in ${process.env.NODE_ENV}`);
 logger.info(`running on port ${port}`);
+
+setInterval(function () {
+  global_socket.emit('PULSE', heartbeat())
+}, 1000);
+
+function heartbeat() {
+  const pulse = Math.ceil(Math.random() * (160 - 60) + 60);
+  logger.debug(`Heartbeat ${pulse}`);
+  return pulse;
+}
