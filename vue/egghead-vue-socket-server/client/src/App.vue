a515b2b134839a19b98d95992a5ff630f144b6ab
Jinho Hyeon <neverlish@gmail.com>
2019-07-04 20:18:36 +0900

    3 Connect a Vue.js Component to a Socket.io server

---
+++ vue/egghead-vue-socket-server/client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-toolbar dark fixed app>
+      <v-icon>speaker_phone</v-icon>
+      <v-toolbar-title>Power Vuejs using Socket.io</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <v-layout row mb-4>
+          <v-flex>
+            <Graph></Graph>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer dark></v-footer>
+  </v-app>
+</template>
+
+<script>
+import Graph from "@/components/Graph";
+export default {
+  components: {
+    Graph
+  }
+};
+</script>
