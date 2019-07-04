3ce2b475e3a49056064f444aa18a32fa7267ee07
Jinho Hyeon <neverlish@gmail.com>
2019-07-04 22:48:54 +0900

    5 - 05 댓글 기능 구현하기 - 02 Disqus 앱 설치

--- django/baepeu-python-webprogramming/dstagram/config/settings.py
+++ django/baepeu-python-webprogramming/dstagram/config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'photo',
     'accounts',
+    'disqus',
+    'django.contrib.sites',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
+
+DISQUS_WEBSITE_SHORTNAME = 'dstagram-django'
+SITE_ID = 1
