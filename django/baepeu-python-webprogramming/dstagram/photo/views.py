b38a41b416f3461a2e3e0534318a36b73bff4fef
Jinho Hyeon <neverlish@gmail.com>
2019-07-04 22:51:53 +0900

    5 - 05 - 03 권한 제한하기

--- django/baepeu-python-webprogramming/dstagram/photo/views.py
+++ django/baepeu-python-webprogramming/dstagram/photo/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Photo
 
+@login_required
 def photo_list(request):
     photos = Photo.objects.all()
     return render(request, 'photo/list.html', { 'photos': photos })
 
-class PhotoUploadView(CreateView):
+class PhotoUploadView(LoginRequiredMixin, CreateView):
     model = Photo
     fields = ['photo', 'text']
     template_name = 'photo/upload.html'
@@ -21,12 +24,12 @@ class PhotoUploadView(CreateView):
         else:
             return self.render_to_response({ 'form': form })
 
-class PhotoDeleteView(DeleteView):
+class PhotoDeleteView(LoginRequiredMixin, DeleteView):
     model = Photo
     success_url = '/'
     template_name = 'photo/delete.html'
 
-class PhotoUpdateView(UpdateView):
+class PhotoUpdateView(LoginRequiredMixin, UpdateView):
     model = Photo
     fields = ['photo', 'text']
     template_name = 'photo/update.html'
