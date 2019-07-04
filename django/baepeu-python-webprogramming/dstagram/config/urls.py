1ae2aab33d57343d9b082452d5c9cdd30a2234ed
Jinho Hyeon <neverlish@gmail.com>
2019-07-04 22:26:01 +0900

    5 - 03 - 09 사진 표시하기

--- django/baepeu-python-webprogramming/dstagram/config/urls.py
+++ django/baepeu-python-webprogramming/dstagram/config/urls.py
@@ -5,3 +5,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('photo.urls')),
 ]
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
