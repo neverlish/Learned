46dfad73f72c89bdf19a542919eafea2f69efba3
Jinho Hyeon <neverlish@gmail.com>
2019-07-05 20:51:07 +0900

    6 - 03 데이터베이스 설정하기 - 04 프로젝트에 데이터베이스 설정하기

--- django/baepeu-python-webprogramming/onlineshop/config/settings.py
+++ django/baepeu-python-webprogramming/onlineshop/config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -73,10 +76,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onlineshop',
+        'USER': 'user',
+        'PASSWORD': os.environ["DATABASE_PASSWORD"],
+        'HOST': os.environ["DATABASE_HOST"],
+        'PORT': '3306',
     }
 }
 
