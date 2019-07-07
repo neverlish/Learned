551791d46ac73ba018f0725d3936ad4c5dc0d47b
Jinho Hyeon <neverlish@gmail.com>
2019-07-07 10:10:10 +0900

    6 - 09 - 04 뷰 만들기

--- django/baepeu-python-webprogramming/onlineshop/order/views.py
+++ django/baepeu-python-webprogramming/onlineshop/order/views.py
@@ -1,3 +1,108 @@
+from django.views.generic.base import View
+from django.http import JsonResponse
 from django.shortcuts import render
+from .models import *
+from cart.cart import Cart
+from .forms import *
 
-# Create your views here.
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.amount
+                order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'order/created.html', { 'order': order })
+    else:
+        form = OrderCreateForm()
+    return render(request, 'order/create.html', { 'cart': cart, 'form': form })
+
+def order_complete(request):
+    order_id = request.GET.get('order_id')
+    order = Order.objects.get(id=order_id)
+    return render(request, 'order/created.html', { 'order': order })
+
+class OrderCreateAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({ 'authenticated': False }, status=403)
+
+        cart = Cart(request)
+        form = OrderCreateForm(request.POST)
+
+        if form.is_valid():
+            order = form.save(commit=False)
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.amount
+
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            cart.clear()
+            data = {
+                'order_id': order.id
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
+
+class OrderCheckoutAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({ 'authenticated': False}, status=403)
+
+        order_id = request.POST.get('order_id')
+        order = Order.objects.get(id=order_id)
+        amount = request.POST.get('amount')
+
+        try:
+            merchant_order_id = OrderTransaction.objects.create_new(order=order, amount=amount)
+        except:
+            merchant_order_id = None
+
+        if merchant_order_id is not None:
+            data = {
+                'works': True,
+                'merchant_order_id': merchant_order_id
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
+
+class OrderImpAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({ 'authenticated': False}, status=403)
+
+        order_id = request.POST.get('order_id')
+        order = Order.objects.get(id=order_id)
+        merchant_id = request.POST.get('merchant_id')
+        imp_id = request.POST.get('imp_id')
+        amount = request.POST.get('amount')
+
+        try:
+            trans = OrderTransaction.objects.get(order=order, merchant_order_id=merchant_id, amount=amount)
+        except:
+            trans = None
+
+        if trans is not None:
+            trans.transaction_id = imp_id
+            trans.success = True
+            trans.save()
+            order.paid = True
+            order.save()
+
+            data = {
+                'works': True
+            }
+
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
