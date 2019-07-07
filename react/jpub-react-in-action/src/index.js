a062c393d3e10e7e5c58ffd8ebe67852721b2529
Jinho Hyeon <neverlish@gmail.com>
2019-07-07 23:36:40 +0900

    08 - 2 - 1 사용자의 로그인 처리

--- react/jpub-react-in-action/src/index.js
+++ react/jpub-react-in-action/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import firebase from 'firebase';
 
 import * as API from './shared/http';
 import { history } from './history';
@@ -9,6 +10,8 @@ import App from './app';
 import Home from './pages/Home';
 import SinglePost from './pages/Post';
 import NotFound from './pages/404';
+import Login from './pages/Login';
+import { getFirebaseToken } from './backend/auth';
 
 import './shared/crash';
 import './shared/service-worker';
@@ -21,6 +24,7 @@ export const renderApp = (state, callback = () => { }) => {
             <Route path='' component={App}>
                 <Route path='/' component={Home} />
                 <Route path='/posts/:postId' component={SinglePost} />
+                <Route path='/login' component={Login} />
                 <Route path='*' component={NotFound} />
             </Route>
         </Router>,
@@ -31,13 +35,57 @@ export const renderApp = (state, callback = () => { }) => {
 
 let state = {
     location: window.location.pathname,
+    user: {
+        authenticated: false,
+        profilePicture: null,
+        id: null,
+        name: null,
+        token: null
+    }
 };
 
 history.listen(location => {
+    const user = firebase.auth().currentUser;
     state = Object.assign({}, state, {
-        location: location.pathname
+        location: user ? location.pathname : '/login'
     });
     renderApp(state);
 });
 
-renderApp(state);
+firebase.auth().onAuthStateChanged(async user => {
+    if (!user) {
+        state = {
+            location: state.location,
+            user: {
+                authenticated: false
+            }
+        };
+        return renderApp(state, () => {
+            history.push('/login')
+        })
+    }
+    const token = await getFirebaseToken();
+    const res = await API.loadUser(user.uid);
+    let renderUser;
+    if (res.status === 404) {
+        const userPayload = {
+            name: user.displayName,
+            profilePicture: user.photoURL,
+            id: user.uid
+        };
+        renderUser = await API.createUser(userPayload).then(res => res.json());
+    } else {
+        renderUser = await res.json();
+    }
+    history.push('/');
+    state = Object.assign({}, state, {
+        user: {
+            name: renderUser.name,
+            id: renderUser.id,
+            profilePicture: renderUser.profilePicture,
+            authenticated: true
+        },
+        token
+    });
+    renderApp(state);
+});
--- react/jpub-react-in-action/src/pages/Login.js
+++ react/jpub-react-in-action/src/pages/Login.js
@@ -10,7 +10,7 @@ class Login extends Component {
     }
 
     login() {
-        loginWithGithub().then(() => {
+        loginWithGithub().then((res) => {
             history.push('/');
         });
     }
