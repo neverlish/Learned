104eba93cd1e69221b45fe393d985d3500887c17
Jinho Hyeon <neverlish@gmail.com>
2019-07-07 10:27:52 +0900

    6 - 09 - 06 템플릿 만들기

--- django/baepeu-python-webprogramming/onlineshop/config/settings.py
+++ django/baepeu-python-webprogramming/onlineshop/config/settings.py
@@ -148,6 +148,9 @@ AWS_LOCATION = 'static'
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 DEFAULT_FILE_STORAGE = 'config.asset_storage.MediaStorage'
 
