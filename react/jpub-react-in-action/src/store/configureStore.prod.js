3d1eb7af59a653d7b9ce56a4d8a3f9eb7e5e5d4a
Jinho Hyeon <neverlish@gmail.com>
2019-07-08 01:22:47 +0900

    10 - 2 - 7 충돌 보고를 위한 리덕스 미들웨어 개발하기

--- react/jpub-react-in-action/src/store/configureStore.prod.js
+++ react/jpub-react-in-action/src/store/configureStore.prod.js
@@ -2,12 +2,13 @@ import thunk from 'redux-thunk';
 import { createStore, compose, applyMiddleware } from 'redux';
 
 import rootReducer from '../reducers/root';
+import crashingReport from '../middleware/crash';
 
 let store;
 export default function configureStore(initialState) {
     if (store) {
         return store;
     }
-    store = createStore(rootReducer, initialState, compose(applyMiddleware(thunk)));
+    store = createStore(rootReducer, initialState, compose(applyMiddleware(thunk, crashingReport)));
     return store;
 }
