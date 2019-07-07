3b48acfc1f11706b8fcc4b7375a1fb13e6dc8105
Jinho Hyeon <neverlish@gmail.com>
2019-07-07 22:51:18 +0900

    08 고급 라우팅과 파이어베이스의 통합 - 1 라우터 사용하기

--- react/jpub-react-in-action/src/app.js
+++ react/jpub-react-in-action/src/app.js
@@ -1,38 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import parseLinkHeader from 'parse-link-header';
-import orderBy from 'lodash/orderBy';
 
 import ErrorMessage from './components/error/Error';
+import Nav from './components/nav/navbar';
 import Loader from './components/Loader';
-import * as API from './shared/http';
-import Ad from './components/ad/Ad';
-import NavBar from './components/nav/navbar';
-import Welcome from './components/welcome/Welcome';
-import Post from './components/post/Post';
-import CreatePost from './components/post/Create';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             error: null,
-            loading: false,
-            posts: [],
-            endpoint: `${process.env.ENDPOINT}/posts?_page=1&_sort=date&_order=DESC&_embed=comments&_expand=user&_embed=likes`
-        };
-
-        this.getPosts = this.getPosts.bind(this);
-        this.createNewPost = this.createNewPost.bind(this);
+            loading: false
+        }
     }
 
     static propTypes = {
         children: PropTypes.node
     };
-
-    componentDidMount() {
-        this.getPosts();
-    }
 
     componentDidCatch(err, info) {
         console.error(err);
@@ -40,39 +24,6 @@ class App extends Component {
         this.setState(() => ({
             error: err
         }));
-    }
-
-    getPosts() {
-        API.fetchPosts(this.state.endpoint)
-            .then(res => {
-                return res
-                    .json()
-                    .then(posts => {
-                        const links = parseLinkHeader(res.headers.get('Link'));
-                        this.setState(() => ({
-                            posts: orderBy(this.state.posts.concat(posts), 'date', 'desc'),
-                            endpoint: links.next.url
-                        }))
-                    })
-                    .catch(err => {
-                        this.setState(() => ({ error: err }));
-                    })
-            });
-    }
-
-    createNewPost(post) {
-        return API.createPost(post)
-            .then(res => res.json())
-            .then(newPost => {
-                this.setState(prevState => {
-                    return {
-                        posts: orderBy(prevState.posts.concat(newPost), 'date', 'desc')
-                    };
-                });
-            })
-            .catch(err => {
-                this.setState(() => ({ error: err }));
-            })
     }
 
     render() {
@@ -85,38 +36,13 @@ class App extends Component {
         }
         return (
             <div className='app'>
-                <NavBar />
+                <Nav user={this.props.user} />
                 {this.state.loading ? (
                     <div className='loading'>
                         <Loader />
                     </div>
                 ) : (
-                        <div className="home">
-                            <Welcome key="welcome" />
-                            <div>
-                                <CreatePost onSubmit={this.createNewPost} />
-                                {this.state.posts.length && (
-                                    <div className="posts">
-                                        {this.state.posts.map(({ id }) => {
-                                            return <Post id={id} key={id} user={this.props.user} />;
-                                        })}
-                                    </div>
-                                )}
-                                <button className="block" onClick={this.getPosts}>
-                                    Load more posts
-                            </button>
-                            </div>
-                            <div>
-                                <Ad
-                                    url="https://ifelse.io/book"
-                                    imageUrl="/static/assets/ads/ria.png"
-                                />
-                                <Ad
-                                    url="https://ifelse.io/book"
-                                    imageUrl="/static/assets/ads/orly.jpg"
-                                />
-                            </div>
-                        </div>
+                        this.props.children
                     )}
             </div>
         );
--- react/jpub-react-in-action/src/index.js
+++ react/jpub-react-in-action/src/index.js
@@ -2,11 +2,37 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import App from './app';
+import Home from './pages/Home';
+import Router from './components/router/Router';
+import Route from './components/router/Route';
+import { history } from './history';
 
 import './shared/crash';
 import './shared/service-worker';
 import './shared/vendor';
-// NOTE: this isn't ES*-compliant/possible, but works because we use Webpack as a build tool
 import './styles/styles.scss';
 
-render(<App />, document.getElementById('app'));
+export const renderApp = (state, callback = () => { }) => {
+    render(
+        <Router {...state}>
+            <Route path='' component={App}>
+                <Route path='/' component={Home} />
+            </Route>
+        </Router>,
+        document.getElementById('app'),
+        callback
+    );
+};
+
+let state = {
+    location: window.location.pathname,
+};
+
+history.listen(location => {
+    state = Object.assign({}, state, {
+        location: location.pathname
+    });
+    renderApp(state);
+});
+
+renderApp(state);
