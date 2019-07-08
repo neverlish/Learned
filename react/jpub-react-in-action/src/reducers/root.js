829a91862cf3f7c89ab2db6601cec2264a4f1025
Jinho Hyeon <neverlish@gmail.com>
2019-07-08 09:04:29 +0900

    11 - 1 - 3 스토어에서 리듀서를 조합하기

--- react/jpub-react-in-action/src/reducers/root.js
+++ react/jpub-react-in-action/src/reducers/root.js
@@ -1,3 +1,21 @@
 import { combineReducers } from 'redux';
-const rootReducer = combineReducers({});
+
+import { error } from './error';
+import { loading } from './loading';
+import { pagination } from './pagination';
+import { posts, postIds } from './posts';
+import { user } from './user';
+import { comments, commentIds } from './comments';
+
+const rootReducer = combineReducers({
+    commentIds,
+    comments,
+    error,
+    loading,
+    pagination,
+    postIds,
+    posts,
+    user
+});
+
 export default rootReducer;
