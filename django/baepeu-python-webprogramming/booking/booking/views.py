26ab19ac1ccaa539303bc5738c1b98e2c5ab8959
Jinho Hyeon <neverlish@gmail.com>
2019-07-10 00:17:01 +0900

    8 - 07 뷰 만들기

--- django/baepeu-python-webprogramming/booking/booking/views.py
+++ django/baepeu-python-webprogramming/booking/booking/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Booking
+from .serializers import BookingSerializer
+
+class BookingList(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
