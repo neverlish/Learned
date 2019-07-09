832a567574d33ed3e53ed4011163e09ce48e6e2f
Jinho Hyeon <neverlish@gmail.com>
2019-07-09 23:55:35 +0900

    7 - 04 관리자 페이지 등록하기

--- django/baepeu-python-webprogramming/vuejs/todo/admin.py
+++ django/baepeu-python-webprogramming/vuejs/todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'completed']
+
+admin.site.register(Todo, TodoAdmin)
