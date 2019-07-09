20abc4661ca351893184b5d9ae3ee73d0cb799bd
Jinho Hyeon <neverlish@gmail.com>
2019-07-10 00:36:55 +0900

    8 - 12 추가 권한 설정하기

--- django/baepeu-python-webprogramming/booking/booking/views.py
+++ django/baepeu-python-webprogramming/booking/booking/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+from .permissions import IsOwnerOrReadOnly
+
 from .models import Booking
 from .serializers import BookingSerializer
 
@@ -11,7 +13,7 @@ class BookingList(generics.ListCreateAPIView):
 
 class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
 
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
