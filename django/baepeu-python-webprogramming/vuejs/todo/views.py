a5bd5a179ba01b6dcf63e389886062291ab24e89
Jinho Hyeon <neverlish@gmail.com>
2019-07-09 23:56:36 +0900

    7 - 05 뷰 만들기

--- django/baepeu-python-webprogramming/vuejs/todo/views.py
+++ django/baepeu-python-webprogramming/vuejs/todo/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from .forms import TodoForm
+from .models import Todo
+
+def todo_fetch(request):
+    todos = Todo.objects.all()
+    todo_list = []
+    for index,todo in enumerate(todos, start=1):
+        todo_list.append({'id':index,'title':todo.title,'completed':todo.completed})
+
+    return JsonResponse(todo_list, safe=False)
+
+@csrf_exempt
+def todo_save(request):
+    if request.body:
+        data = json.loads(request.body)
+        if 'todos' in data:
+            todos = data['todos']
+            Todo.objects.all().delete()
+            for todo in todos:
+                form = TodoForm(todo)
+                if form.is_valid():
+                    form.save()
+
+    return JsonResponse({})
