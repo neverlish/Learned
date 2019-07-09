7bec5cd95a2cb247ab8ba8448773cc9579385ee3
Jinho Hyeon <neverlish@gmail.com>
2019-07-09 23:03:08 +0900

    6 - 09 - 08 관리자 페이지 커스터마이징 하기

--- django/baepeu-python-webprogramming/onlineshop/order/urls.py
+++ django/baepeu-python-webprogramming/onlineshop/order/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('checkout/', OrderCheckoutAjaxView.as_view(), name='order_checkout'),
     path('validation/', OrderImpAjaxView.as_view(), name='order_validation'),
     path('complete/', order_complete, name='order_complete'),
+    path('admin/order/<int:order_id>/', admin_order_detail, name='admin_order_detail'),
+    path('admin/order/<int:order_id>/pdf/', admin_order_pdf, name='admin_order_pdf'),
 ]
--- django/baepeu-python-webprogramming/onlineshop/order/views.py
+++ django/baepeu-python-webprogramming/onlineshop/order/views.py
@@ -1,9 +1,15 @@
+from django.conf import settings
 from django.views.generic.base import View
-from django.http import JsonResponse
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.template.loader import render_to_string
+from django.shortcuts import render, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
+
 from .models import *
 from cart.cart import Cart
 from .forms import *
+
+import weasyprint
 
 def order_create(request):
     cart = Cart(request)
@@ -106,3 +112,17 @@ class OrderImpAjaxView(View):
             return JsonResponse(data)
         else:
             return JsonResponse({}, status=401)
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'order/admin/detail.html', { 'order': order })
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('order/admin/pdf.html', {'order':order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename=order_{}.pdf'.format(order.id)
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATICFILES_DIRS[0]+'/css/pdf.css')])
+    return response
