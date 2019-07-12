816ab4dd3f053291285e53ab13e1a589578488d6
Jinho Hyeon <neverlish@gmail.com>
2019-07-13 00:51:10 +0900

    06 Display Sorted Data with useStaticQuery in Gatsby

---
+++ react/egghead-gatsby-theme-authoring/gatsby-theme-events/src/components/layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Layout = ({ children }) => (
+  <div>
+    <h1>Gatsby Events Theme</h1>
+    {children}
+  </div>
+);
+
+export default Layout;
--- react/egghead-gatsby-theme-authoring/gatsby-theme-events/src/templates/events.js
+++ react/egghead-gatsby-theme-authoring/gatsby-theme-events/src/templates/events.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Layout from '../components/layout';
+import EventList from '../components/event-list';
 
-const EventsTemplate = () => <p>TODO build the events page</p>;
+const EventsTemplate = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allEvent(sort: { fields: startDate, order: ASC}) {
+        nodes {
+          id
+          name
+          startDate
+          endDate
+          location
+          url
+          slug
+        }
+      }
+    }
+  `);
+
+  const events = data.allEvent.nodes;
+  return (
+    <Layout>
+      <EventList events={events} />
+    </Layout>
+  );
+};
 
 export default EventsTemplate;
