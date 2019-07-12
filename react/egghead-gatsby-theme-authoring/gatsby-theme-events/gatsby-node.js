8dd2f79af9b72240ade4635ad0b117966a1656a7
Jinho Hyeon <neverlish@gmail.com>
2019-07-13 00:25:57 +0900

    04 Set up to Create Data Driven Pages in Gatsby

--- react/egghead-gatsby-theme-authoring/gatsby-theme-events/gatsby-node.js
+++ react/egghead-gatsby-theme-authoring/gatsby-theme-events/gatsby-node.js
@@ -11,7 +11,40 @@ exports.onPreBootstrap = ({ reporter }) => {
 }
 
 // 2 define the event type
+exports.sourceNodes = ({ actions }) => {
+  actions.createTypes(`
+    type Event implements Node @dontInfer {
+      id: ID!
+      name: String!
+      location: String!
+      startDate: Date! @dateformat @proxy(from: "start_date")
+      endDate: Date! @dateformat @proxy(from: "end_date")
+      url: String!
+      slug: String!
+    }
+  `);
+}
 
 // 3 define resolvers for any custom fields (slug)
+exports.createResolvers = ({ createResolvers }) => {
+  const basePath = '/';
+
+  const slugify = str => {
+    const slug = str
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, '-')
+      .replace(/(^-|-$)+/g, '');
+
+    return `/${basePath}/${slug}`.replace(/\/\/+/g, '/');
+  }
+
+  createResolvers({
+    Event: {
+      slug: {
+        resolve: source => slugify(source.name)
+      }
+    }
+  });
+}
 
 // 4 query for event and create pages
