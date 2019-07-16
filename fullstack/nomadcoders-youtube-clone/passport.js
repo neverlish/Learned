501b357fb25739232c0ce1b57ba802576b49d4b9
Jinho Hyeon <neverlish@gmail.com>
2019-07-16 09:56:26 +0900

    06 - 11 Facebook Login Part Two

--- fullstack/nomadcoders-youtube-clone/passport.js
+++ fullstack/nomadcoders-youtube-clone/passport.js
@@ -25,7 +25,9 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `http://localhost:4000${routes.facebookCallback}`
+      callbackURL: `https://fast-frog-15.localtunnel.me${routes.facebookCallback}`,
+      profileFields: ['id', 'displayName', 'photos', 'email'],
+      scope: ['public_profile', 'email']
     },
     facebookLoginCallback
   )
