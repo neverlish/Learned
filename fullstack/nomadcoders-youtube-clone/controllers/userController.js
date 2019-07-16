07569d5ded6b552f422fdbea7d2de9f862001f07
Jinho Hyeon <neverlish@gmail.com>
2019-07-16 10:35:07 +0900

    07 - 01 Edit User Password

--- fullstack/nomadcoders-youtube-clone/controllers/userController.js
+++ fullstack/nomadcoders-youtube-clone/controllers/userController.js
@@ -117,7 +117,26 @@ export const userDetail = async (req, res) => {
   }
 };
 
-export const editProfile = (req, res) =>
+export const getEditProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
+
+export const postEditProfile = async (req, res) => {
+  const {
+    body: { name, email },
+    file
+  } = req;
+
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      name,
+      email,
+      avatarUrl: file ? file.path : req.user.avatarUrl
+    });
+    res.redirect(routes.me);
+  } catch (error) {
+    res.render("editProfile", { pageTitle: "Edit Profile" });
+  }
+};
+
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
