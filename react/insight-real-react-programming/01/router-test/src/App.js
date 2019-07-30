587df607eefd8466d9027a0d7c7b6aabf73f6d4a
Jinho Hyeon <neverlish@gmail.com>
2019-07-30 20:52:33 +0900

    01 - 5 - 2 react-router-dom 사용하기

--- react/insight-real-react-programming/01/router-test/src/App.js
+++ react/insight-real-react-programming/01/router-test/src/App.js
@@ -1,54 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Rooms from './Rooms';
 
 class App extends Component {
-  state = {
-    pageName: '',
-  };
-
-  componentDidMount() {
-    window.onpopstate = (event) => {
-      this.onChangePage(event.state);
-    };
-  }
-
-  onChangePage = pageName => {
-    this.setState({ pageName });
-  };
-
-  onClick1 = () => {
-    const pageName = 'page1';
-    window.history.pushState(pageName, '', '/page1');
-    this.onChangePage(pageName);
-  };
-
-  onClick2 = () => {
-    const pageName = 'page2';
-    window.history.pushState(pageName, '', '/page2');
-    this.onChangePage(pageName);
-  };
-
   render() {
-    const { pageName } = this.state;
     return (
-      <div>
-        <button onClick={this.onClick1}>page1</button>
-        <button onClick={this.onClick2}>page2</button>
-        {!pageName && <Home />}
-        {pageName === 'page1' && <Page1 />}
-        {pageName === 'page2' && <Page2 />}
-      </div>
-    );
+      <BrowserRouter>
+        <div style={{ padding: 20, border: '5px solid gray' }}>
+          <Link to='/'>홈</Link>
+          <br />
+          <Link to='/photo'>사진</Link>
+          <br />
+          <Link to='/rooms'>방 소개</Link>
+          <br />
+          <Route exact path='/' component={Home} />
+          <Route path='/photo' component={Photo} />
+          <Route path='/rooms' component={Rooms} />
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
-function Home() {
-  return <h2>여기는 홈페이지입니다. 원하는 페이지 버튼을 클릭하세요.</h2>;
+function Home({ match }) {
+  return <h2>이곳은 홈페이지입니다.</h2>;
 }
-function Page1() {
-  return <h2>여기는 Page1입니다.</h2>;
-}
-function Page2() {
-  return <h2>여기는 Page2입니다.</h2>;
+function Photo({ match }) {
+  return <h2>여기서 사진을 감상하세요.</h2>
 }
 
 export default App;
---
+++ react/insight-real-react-programming/01/router-test/src/Rooms.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+function Rooms({ match }) {
+  return (
+    <div>
+      <h2>여기는 방을 소개하는 페이지입니다.</h2>
+      <Link to={`${match.url}/blueRoom`}>파란 방입니다</Link>
+      <br />
+      <Link to={`${match.url}/greenRoom`}>초록 방입니다</Link>
+      <br />
+      <Route path={`${match.url}/:roomId`} component={Room} />
+      <Route
+        exact
+        path={match.url}
+        render={() => <h3>방을 선택해주세요.</h3>}
+      />
+    </div>
+  );
+}
+
+export default Rooms;
+
+function Room({ match }) {
+  return <h2>{`${match.params.roomId} 방을 선택하셨습니다.`}</h2>;
+}
