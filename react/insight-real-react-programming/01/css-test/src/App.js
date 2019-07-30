9e2b3fc281ca424684b4d7ef2117a2e76cc4b6e4
Jinho Hyeon <neverlish@gmail.com>
2019-07-30 19:34:42 +0900

    01 - 4 - 1 일반적인 CSS 파일로 작성하기

--- react/insight-real-react-programming/01/css-test/src/App.js
+++ react/insight-real-react-programming/01/css-test/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './Button1';
+import Box from './Box1';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button size='big' />
+      <Button size='small' />
+      <Box size='big' />
+      <Box size='small' />
     </div>
   );
 }
