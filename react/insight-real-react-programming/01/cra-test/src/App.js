f4bb34482fd73db1777f116f27f9bde6d7e95cf5
Jinho Hyeon <neverlish@gmail.com>
2019-07-30 19:14:08 +0900

    01 - 3 - 4 코드 분할하기

--- react/insight-real-react-programming/01/cra-test/src/App.js
+++ react/insight-real-react-programming/01/cra-test/src/App.js
@@ -5,11 +5,14 @@ import './App.css';
 import smallImage from './small.jpeg';
 import bigImage from './big.jpeg';
 
+import TodoList from './TodoList';
+
 function App() {
   return (
     <div className="App">
       <img src={bigImage} />
       <img src={smallImage} />
+      <TodoList />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
