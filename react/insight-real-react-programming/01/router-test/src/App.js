20d87e3d06fa731aea9eebf951202c66fb2de74e
Jinho Hyeon <neverlish@gmail.com>
2019-07-30 20:51:56 +0900

    01 - 5 - 1 브라우저 히스토리 API

--- react/insight-real-react-programming/01/router-test/src/App.js
+++ react/insight-real-react-programming/01/router-test/src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    pageName: '',
+  };
+
+  componentDidMount() {
+    window.onpopstate = (event) => {
+      this.onChangePage(event.state);
+    };
+  }
+
+  onChangePage = pageName => {
+    this.setState({ pageName });
+  };
+
+  onClick1 = () => {
+    const pageName = 'page1';
+    window.history.pushState(pageName, '', '/page1');
+    this.onChangePage(pageName);
+  };
+
+  onClick2 = () => {
+    const pageName = 'page2';
+    window.history.pushState(pageName, '', '/page2');
+    this.onChangePage(pageName);
+  };
+
+  render() {
+    const { pageName } = this.state;
+    return (
+      <div>
+        <button onClick={this.onClick1}>page1</button>
+        <button onClick={this.onClick2}>page2</button>
+        {!pageName && <Home />}
+        {pageName === 'page1' && <Page1 />}
+        {pageName === 'page2' && <Page2 />}
+      </div>
+    );
+  }
+}
+
+function Home() {
+  return <h2>여기는 홈페이지입니다. 원하는 페이지 버튼을 클릭하세요.</h2>;
+}
+function Page1() {
+  return <h2>여기는 Page1입니다.</h2>;
+}
+function Page2() {
+  return <h2>여기는 Page2입니다.</h2>;
 }
 
 export default App;
