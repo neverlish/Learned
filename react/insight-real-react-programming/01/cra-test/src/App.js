21563029304202f681670ff141522e57a0dc9e07
Jinho Hyeon <neverlish@gmail.com>
2019-07-30 19:01:46 +0900

    01 - 3 - 2 주요 명령어 알아보기

--- react/insight-real-react-programming/01/cra-test/src/App.js
+++ react/insight-real-react-programming/01/cra-test/src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import smallImage from './small.jpeg';
+import bigImage from './big.jpeg';
+
 function App() {
   return (
     <div className="App">
+      <img src={bigImage} />
+      <img src={smallImage} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
