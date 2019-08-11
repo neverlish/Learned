7f9288dae10638bc77e406156969fd55f0af2420
Jinho Hyeon <neverlish@gmail.com>
2019-08-11 13:33:17 +0900

    11 - 01 Multer Uploads to AWS S3

--- fullstack/nomadcoders-youtube-clone/middlewares.js
+++ fullstack/nomadcoders-youtube-clone/middlewares.js
@@ -1,8 +1,32 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 import routes from "./routes";
 
-const multerVideo = multer({ dest: "uploads/videos/" });
-const multerAvater = multer({ dest: "uploads/avatars/" });
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+  region: "ap-northeast-2"
+});
+
+const multerVideo = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "wetube-neverlish/video"
+  })
+});
+
+const multerAvatar = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "wetube-neverlish/avatar"
+  })
+});
+
+export const uploadVideo = multerVideo.single("videoFile");
+export const uploadAvatar = multerAvatar.single("avatar");
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
@@ -26,6 +50,3 @@ export const onlyPrivate = (req, res, next) => {
     res.redirect(routes.home);
   }
 };
-
-export const uploadVideo = multerVideo.single("videoFile");
-export const uploadAvatar = multerAvater.single("avatar");
