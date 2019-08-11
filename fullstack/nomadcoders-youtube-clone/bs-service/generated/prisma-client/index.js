0d09910542b38be00925fe9c0321a0a39fe3fa89
Jinho Hyeon <neverlish@gmail.com>
2019-08-11 19:34:39 +0900

    11 - 10 Intro to Prisma part One

---
+++ fullstack/nomadcoders-youtube-clone/bs-service/generated/prisma-client/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Post",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/jinho-hyeon-0e5e36/bs-service/dev`
+});
+exports.prisma = new exports.Prisma();
---
+++ fullstack/nomadcoders-youtube-clone/description.md
@@ -0,0 +1,5 @@
+## 11 - 10 Intro to Prisma part One
+
+- `prisma init bs-service`
+- `prisma deploy`
+- `prisma generate`
