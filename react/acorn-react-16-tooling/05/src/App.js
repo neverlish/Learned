76538a2b4ee731bbc1477871643f10315b2bcf00
Jinho Hyeon <neverlish@gmail.com>
2019-08-17 20:55:35 +0900

    05 - 2 Flow 설치 및 초기화

--- react/acorn-react-16-tooling/05/src/App.js
+++ react/acorn-react-16-tooling/05/src/App.js
@@ -1,8 +1,11 @@
+// @flow
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+type Props = {};
+
+class App extends Component<Props> {
   render() {
     return (
       <div className="App">
