f0f8709f52094d4086d4c94fb2cea2f65bb527b3
Jinho Hyeon <neverlish@gmail.com>
2019-08-17 20:00:58 +0900

    04 - 2 - 1 react-scripts를 사용해 테스트 실행하기

--- react/acorn-react-16-tooling/04/src/App.js
+++ react/acorn-react-16-tooling/04/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Repeat from './Repeat';
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Repeat times="5" value="React!" />
         </a>
       </header>
     </div>
