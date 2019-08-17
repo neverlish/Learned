8d7f7e56dde3a91295cfbfb0cec6bafb2c51cd04
Jinho Hyeon <neverlish@gmail.com>
2019-08-17 21:46:15 +0900

    05 - 4 이벤트 처리기 함수에 관한 유효성 검사

---
+++ react/acorn-react-16-tooling/05/src/validating-event-handler-functions/EventHandler.js
@@ -0,0 +1,20 @@
+// @flow
+import * as React from 'react';
+import { Component } from 'react';
+
+class EventHandler extends Component<{}> {
+  clickHandler = (e: SyntheticEvent<HTMLAnchorElement>): void => {
+    e.preventDefault();
+    console.log('clicked', e.currentTarget.href);
+  }
+
+  render() {
+    return (
+      <section>
+        <a href="#page1" onClick={this.clickHandler}>
+          First Link
+        </a>
+      </section>
+    )
+  }
+}
