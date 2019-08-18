7cd1d02847a054c84a0f6eb62d382a7b4a925fff
Jinho Hyeon <neverlish@gmail.com>
2019-08-18 10:23:47 +0900

    08 - 5 컴포넌트 성능 프로파일링 - 1 조정 작업 삭제하기

--- react/acorn-react-16-tooling/08/src/index.js
+++ react/acorn-react-16-tooling/08/src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const update = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+setInterval(update, 5000);
+update();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
