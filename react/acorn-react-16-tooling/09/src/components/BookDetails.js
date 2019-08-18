98c5c5aa2953ec54e3c3e4429465a4671cf38acf
Jinho Hyeon <neverlish@gmail.com>
2019-08-18 15:57:57 +0900

    09 - 1 - 2 Home 컴포넌트 및 상태

--- react/acorn-react-16-tooling/09/src/components/BookDetails.js
+++ react/acorn-react-16-tooling/09/src/components/BookDetails.js
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const BookDetails = () => {
-  return (
-    <div>
+import Book from './Book';
+import Loading from './Loading';
+import { fetchBook, deleteBook } from '../api';
 
-    </div>
-  );
-};
+class BookDetails extends Component {
+  componentWillMount() {
+    const {
+      match: { params: { title } },
+      fetchingBook,
+      fetchedBook
+    } = this.props;
 
-export default BookDetails;
+    fetchingBook();
+    fetchBook(title).then(book => {
+      fetchedBook(book);
+    });
+  }
+
+  onDeleteClick = () => {
+    const {
+      book: { title },
+      deletingBook,
+      deletedBook,
+      history
+    } = this.props;
+
+    deletingBook();
+    deleteBook(title).then(() => {
+      deletedBook();
+      history.push('/');
+    });
+  };
+
+  render() {
+    const { loading, book, controlsDisabled } = this.props;
+
+    return (
+      <Loading loading={loading}>
+        <Book {...book} />
+        <button
+          disabled={controlsDisabled}
+          onClick={this.onDeleteClick}
+        >
+          Delete
+        </button>
+      </Loading>
+    );
+  }
+}
+
+const mapState = state => state.bookDetails;
+const mapDispatch = dispatch => ({
+  fetchingBook() {
+    dispatch({ type: 'FETCHING_BOOK' });
+  },
+
+  fetchedBook(book) {
+    dispatch({ type: 'FETCHED_BOOK', book });
+  },
+
+  deletingBook() {
+    dispatch({ type: 'DELETING_BOOK' });
+  },
+
+  deletedBook() {
+    dispatch({ type: 'DELETED_BOOK' });
+  }
+});
+
+export default connect(mapState, mapDispatch)(BookDetails);
--- react/acorn-react-16-tooling/09/src/reducers/index.js
+++ react/acorn-react-16-tooling/09/src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import app from './app';
+import bookDetails from './bookDetails';
+import home from './home';
 
 const reducers = combineReducers({
+  bookDetails,
   app,
+  home,
 });
 
 export default reducers;
