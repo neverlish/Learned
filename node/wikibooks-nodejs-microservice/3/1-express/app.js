8969112651a591aa432cb0036bc8450a0558a79f
Jinho Hyeon <neverlish@gmail.com>
2019-09-08 15:43:29 +0900

    3 - 1 - 4 경로 매개변수 사용

--- node/wikibooks-nodejs-microservice/3/1-express/app.js
+++ node/wikibooks-nodejs-microservice/3/1-express/app.js
@@ -5,18 +5,23 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 
+app.param('image', (req, res, next, image) => {
+  if (!image.match(/\.(png|jpg)$/i)) {
+    return res.status(req.method === 'POST' ? 403 : 404).end();
+  }
+
+  req.image = image;
+
+  req.localpath = path.join(__dirname, 'uploads', req.image);
+
+  return next();
+});
+
 app.post('/uploads/:image', bodyparser.raw({
   limit: '10mb',
   type: 'image/*'
 }), (req, res) => {
-  let image = req.params.image.toLowerCase();
-
-  if (!image.match(/\.(png|jpg)$/)) {
-    return res.status(403).end();
-  }
-
-  let len = req.body.length;
-  let fd = fs.createWriteStream(path.join(__dirname, 'uploads', image), {
+  let fd = fs.createWriteStream(req.localpath, {
     flags: 'w+',
     encoding: 'binary'
   });
@@ -25,47 +30,28 @@ app.post('/uploads/:image', bodyparser.raw({
   fd.end();
 
   fd.on('close', () => {
-    res.send({ status: 'ok', size: len });
+    res.send({ status: 'ok', size: req.body.length });
   });
 });
 
 app.head('/uploads/:image', (req, res) => {
   fs.access(
-    path.join(__dirname, 'uploads', req.params.image),
+    req.localpath,
     fs.constants.R_OK,
     (err) => {
-      res.status(err ? 404 : 200);
-      res.end();
+      res.status(err ? 404 : 200).end();
     },
   );
 });
 
 app.get('/uploads/:image', (req, res) => {
-  let ext = path.extname(req.params.image);
-
-  if (!ext.match(/^\.(png|jpg)$/)) {
-    return res.status(404).end();
-  }
-
-  let fd = fs.createReadStream(path.join(__dirname, 'uploads', req.params.image));
+  let fd = fs.createReadStream(req.localpath);
 
   fd.on('error', (e) => {
-    if (e.code === 'ENOENT') {
-      res.status(404);
-
-      if (req.accepts('html')) {
-        res.setHeader('Content-Type', 'text/html');
-
-        res.write('<strong>Error:</strong> image not found');
-      }
-
-      return res.end();
-    }
-
-    res.status(500).end();
+    res.status(e.code === 'ENOENT' ? 404 : 500).end();
   });
 
-  res.setHeader('Content-Type', 'image/' + ext.substr(1));
+  res.setHeader('Content-Type', 'image/' + path.extname(req.image).substr(1));
 
   fd.pipe(res);
 });
