8f80e4e2e442ccbff64a0f3cb53d4fc2b32ec2c9
Jinho Hyeon <neverlish@gmail.com>
2019-09-14 13:46:45 +0900

    1 useState - 00 Introduction to useState

--- react/nomadcoders-react-practical-hooks/src/index.js
+++ react/nomadcoders-react-practical-hooks/src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+import App from './1/00-useState';
 
 const rootElement = document.getElementById('root');
 
