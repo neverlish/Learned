9d907462bbc5ec82b1e5aa131cbe81378abc1476
Jinho Hyeon <neverlish@gmail.com>
2019-09-14 13:35:38 +0900

    0 Introduction - 02 Workflow

---
+++ react/nomadcoders-react-practical-hooks/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<App />, rootElement);
