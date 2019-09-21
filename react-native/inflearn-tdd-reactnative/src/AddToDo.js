b12f9997148b58eb973b92b2ad5ccdbadb822ac9
Jinho Hyeon <neverlish@gmail.com>
2019-09-22 00:02:29 +0900

    3 앱 Design 및 Component 정의 - 2 첫번째 테스트 코드 작성

---
+++ react-native/inflearn-tdd-reactnative/src/AddToDo.js
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export default class AddToDo extends Component {
+  render() {
+    null;
+  }
+}
--- react-native/inflearn-tdd-reactnative/tests/App.spec.js
+++ react-native/inflearn-tdd-reactnative/tests/App.spec.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Text } from 'react-native';
 import App from '../src/App';
-
 import { shallow } from 'enzyme';
 
-describe('Jest', () => {
-  it('is it working?', () => {
-    const a = 1;
-    expect(a + 1).toBe(2);
-  });
-});
+describe('App', () => {
+  const wrapper = shallow(<App></App>);
 
-describe('Enzyme', () => {
-  it('is it working?', () => {
-    const text = 'some text';
-    const wrapper = shallow(<Text>{text}</Text>);
-    expect(wrapper.text()).toBe(text);
+  it('is Text visible?', () => {
+    expect(wrapper.find('Text').contains('Todo TDD')).toBe(true);
+  });
+
+  it('is AddToDo visible?', () => {
+    expect(wrapper.find('AddToDo')).toHaveLength(1);
+  });
+
+  it('is ToDoList visible?', () => {
+    expect(wrapper.find('ToDoList')).toHaveLength(1);
   });
 });
