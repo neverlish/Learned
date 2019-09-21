f9ce94e76e4163c5a0a17048d7cb5fbe4850b7ff
Jinho Hyeon <neverlish@gmail.com>
2019-09-21 23:26:57 +0900

    2 - 4 Enzyme 설치/설정 및 동작확인

--- react-native/inflearn-tdd-reactnative/tests/App.spec.js
+++ react-native/inflearn-tdd-reactnative/tests/App.spec.js
@@ -1,10 +1,20 @@
-import 'react-native';
 import React from 'react';
+import { Text } from 'react-native';
 import App from '../src/App';
+
+import { shallow } from 'enzyme';
 
 describe('Jest', () => {
   it('is it working?', () => {
     const a = 1;
     expect(a + 1).toBe(2);
+  });
+});
+
+describe('Enzyme', () => {
+  it('is it working?', () => {
+    const text = 'some text';
+    const wrapper = shallow(<Text>{text}</Text>);
+    expect(wrapper.text()).toBe(text);
   });
 });
