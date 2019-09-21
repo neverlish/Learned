807c30cd52c680b73366c1d513317dae8ac902d4
Jinho Hyeon <neverlish@gmail.com>
2019-09-21 23:22:00 +0900

    2 - 3 Jest 설정 및 동작확인

--- react-native/inflearn-tdd-reactnative/tests/App.spec.js
+++ react-native/inflearn-tdd-reactnative/tests/App.spec.js
@@ -1,14 +1,10 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../src/App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('Jest', () => {
+  it('is it working?', () => {
+    const a = 1;
+    expect(a + 1).toBe(2);
+  });
 });
