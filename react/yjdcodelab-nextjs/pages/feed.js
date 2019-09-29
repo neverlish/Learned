f8a9aef1074428f388f82d7a39b9b857e62291b8
Jinho Hyeon <neverlish@gmail.com>
2019-09-29 14:41:58 +0900

    5-1 - Dynamic Routes 2

--- react/yjdcodelab-nextjs/pages/feed.js
+++ react/yjdcodelab-nextjs/pages/feed.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import Layout from '../components/Layout';
+import db from '../common/db';
 
 @observer
 class Feed extends React.Component {
+  static async getInitialProps({ query }) {
+    const result = await db.collection('feeds')
+      .doc(query.feedId)
+      .get();
+    const props = {
+      feedId: query.feedId,
+      feed: result.data(),
+    };
+    return props;
+  }
+
   render() {
     return (
-      <div>feed page</div>
+      <Layout>
+        Feed Page
+
+        <div>
+          {this.props.feed.content}
+          <div>
+            {this.props.feed.author.displayName}
+          </div>
+        </div>
+      </Layout>
     );
   }
 }
