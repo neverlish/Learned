1397e6789cacad85859988ec26f06d7a4b9659c0
Jinho Hyeon <neverlish@gmail.com>
2019-11-09 21:34:49 +0900

    03 - 15 itsMe and amIFollowing Fileds part One

---
+++ fullstack/nomadcoders-instagram-clone/server/src/api/User/computed.js
@@ -0,0 +1,31 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: parent => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    amIFollowing: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        const exists = await prisma.$exists.user({
+          AND: [{ id: parentId }, { followers_some: [user.id] }]
+        });
+        if (exists) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+    itsMe: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
+  }
+};
--- fullstack/nomadcoders-instagram-clone/server/src/api/User/me/me.js
+++ fullstack/nomadcoders-instagram-clone/server/src/api/User/me/me.js
@@ -13,10 +13,5 @@ export default {
         posts
       };
     }
-  },
-  User: {
-    fullName: parent => {
-      return `${parent.firstName} ${parent.lastName}`;
-    }
   }
 };
