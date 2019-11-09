4594cf6f8c5cef90122637ca656b8f1ff64366c6
Jinho Hyeon <neverlish@gmail.com>
2019-11-09 21:46:38 +0900

    03 - 17 isLiked Computed File

--- fullstack/nomadcoders-instagram-clone/server/src/api/User/computed.js
+++ fullstack/nomadcoders-instagram-clone/server/src/api/User/computed.js
@@ -31,5 +31,26 @@ export default {
       const { id: parentId } = parent;
       return user.id === parentId;
     }
+  },
+  Post: {
+    isLiked: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id
+            }
+          },
+          {
+            post: {
+              id
+            }
+          }
+        ]
+      });
+    }
   }
 };
