1594d78f7ac4431fab4b711ea86248b7daf72557
Jinho Hyeon <neverlish@gmail.com>
2019-11-09 21:39:27 +0900

    03 - 16 itsMe and amIFollowing Fileds part Two

--- fullstack/nomadcoders-instagram-clone/server/src/api/User/computed.js
+++ fullstack/nomadcoders-instagram-clone/server/src/api/User/computed.js
@@ -5,24 +5,28 @@ export default {
     fullName: parent => {
       return `${parent.firstName} ${parent.lastName}`;
     },
-    amIFollowing: async (parent, _, { request }) => {
+    isFollowing: async (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
       try {
-        const exists = await prisma.$exists.user({
-          AND: [{ id: parentId }, { followers_some: [user.id] }]
+        return await prisma.$exists.user({
+          AND: [
+            {
+              id: user.id
+            },
+            {
+              following_some: {
+                id: parentId
+              }
+            }
+          ]
         });
-        if (exists) {
-          return true;
-        } else {
-          return false;
-        }
       } catch (error) {
         console.log(error);
         return false;
       }
     },
-    itsMe: (parent, _, { request }) => {
+    isSelf: (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
       return user.id === parentId;
