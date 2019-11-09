1fc5068a57fe8a00dbd587e6e86be2d5368deb09
Jinho Hyeon <neverlish@gmail.com>
2019-11-09 18:05:35 +0900

    03 - 07 toggleLike Resolver part One

---
+++ fullstack/nomadcoders-instagram-clone/server/src/api/Post/toggleLike/toggleLike.js
@@ -0,0 +1,47 @@
+import { prisma } from '../../../../generated/prisma-client';
+import { isAuthenticated } from '../../../middlewares';
+
+export default {
+  Mutation: {
+    toggleLike: async (_, args, { request }) => {
+      isAuthenticated(request);
+      const { postId } = args;
+      const { user } = request;
+      try {
+        const existingLike = await prisma.$exists.like({
+          AND: [
+            {
+              user: {
+                id: user.id,
+              }
+            },
+            {
+              post: {
+                id: postId,
+              },
+            },
+          ],
+        });
+        if (existingLike) {
+          // TO DO
+        } else {
+          await prisma.createLike({
+            user: {
+              connect: {
+                id: user.id
+              }
+            },
+            post: {
+              connect: {
+                id: postId
+              }
+            },
+          });
+        }
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
+};
