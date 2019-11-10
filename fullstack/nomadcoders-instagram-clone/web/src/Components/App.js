678273b3060a5195800b0d269df785945cde640e
Jinho Hyeon <neverlish@gmail.com>
2019-11-10 13:12:50 +0900

    05 - 1 Footer and Auth UI part Two

--- fullstack/nomadcoders-instagram-clone/web/src/Components/App.js
+++ fullstack/nomadcoders-instagram-clone/web/src/Components/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { gql } from 'apollo-boost';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from '../Styles/Theme';
 import Router from './Router';
 import { useQuery } from 'react-apollo-hooks';
+import Footer from './Footer';
 
 const QUERY = gql`
   {
     isLoggedIn @client
   }
-`
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%;
+`;
 
 export default () => {
   const {
@@ -18,8 +25,11 @@ export default () => {
   } = useQuery(QUERY)
   return (
     <ThemeProvider theme={Theme}>
-      <GlobalStyles />
-      <Router isLoggedIn={isLoggedIn} />
+      <Wrapper>
+        <GlobalStyles />
+        <Router isLoggedIn={isLoggedIn} />
+        <Footer />
+      </Wrapper>
     </ThemeProvider>
   )
 };
