a563605e0c98b19925ff8d68fa47c3fb1003b8aa
Jinho Hyeon <neverlish@gmail.com>
2019-11-10 12:37:56 +0900

    04 - 02 React Router

--- fullstack/nomadcoders-instagram-clone/web/src/Components/App.js
+++ fullstack/nomadcoders-instagram-clone/web/src/Components/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from '../Styles/Theme';
+import Router from './Router';
 
 export default () => (
   <ThemeProvider theme={Theme}>
     <GlobalStyles />
-    hello
+    <Router isLoggedIn={false} />
   </ThemeProvider>
 );
