f8fb5f0dd1e39f013ab26792711b7884f7c5eccb
Jinho Hyeon <neverlish@gmail.com>
2019-11-10 15:20:26 +0900

    06 - 02 Header Logic part Two

--- fullstack/nomadcoders-instagram-clone/web/src/Components/Header.js
+++ fullstack/nomadcoders-instagram-clone/web/src/Components/Header.js
@@ -72,8 +72,7 @@ const HeaderLink = styled(Link)`
 
 export default withRouter(({ history }) => {
   const search = useInput("");
-  const meQuery = useQuery(ME);
-  console.log(meQuery);
+  const { data } = useQuery(ME);
   const onSearchSubmit = e => {
     e.preventDefault();
     history.push(`/search?term=${search.value}`);
@@ -98,9 +97,15 @@ export default withRouter(({ history }) => {
           <HeaderLink to="/notifications">
             <HeartEmpty />
           </HeaderLink>
-          <HeaderLink to="/username">
-            <User />
-          </HeaderLink>
+          {!(data && data.me) ? (
+            <HeaderLink to="/#">
+              <User />
+            </HeaderLink>
+          ) : (
+              <HeaderLink to={data.me.username}>
+                <User />
+              </HeaderLink>
+            )}
         </HeaderColumn>
       </HeaderWrapper>
     </Header>
