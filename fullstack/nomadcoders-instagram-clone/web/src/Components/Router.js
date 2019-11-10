2b873783adbebcf20f9ffe2b2b9963791d13c43a
Jinho Hyeon <neverlish@gmail.com>
2019-11-10 14:54:29 +0900

    06 Frontend: Header Component - 00 Header UI

--- fullstack/nomadcoders-instagram-clone/web/src/Components/Router.js
+++ fullstack/nomadcoders-instagram-clone/web/src/Components/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Auth from '../Routes/Auth';
 import Feed from '../Routes/Feed';
 
@@ -16,11 +16,9 @@ const LoggedOutRoutes = () => (
 );
 
 const AppRouter = ({ isLoggedIn }) => (
-  <Router>
-    <Switch>
-      {isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}
-    </Switch>
-  </Router>
+  <Switch>
+    {isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}
+  </Switch>
 );
 
 AppRouter.propTypes = {
