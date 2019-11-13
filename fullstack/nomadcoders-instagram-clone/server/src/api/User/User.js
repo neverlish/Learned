35196ea1e4aa51313378916417412e55ef9ac5b3
Jinho Hyeon <neverlish@gmail.com>
2019-11-14 00:16:20 +0900

    09 Frontend: Profile - 00 Profile Screen part One

--- fullstack/nomadcoders-instagram-clone/server/src/api/User/User.js
+++ fullstack/nomadcoders-instagram-clone/server/src/api/User/User.js
@@ -8,6 +8,11 @@ export default {
     likes: ({ id }) => prisma.user({ id }).likes(),
     comments: ({ id }) => prisma.user({ id }).comments(),
     rooms: ({ id }) => prisma.user({ id }).rooms(),
+    postsCount: ({ id }) =>
+      prisma
+        .postsConnection({ where: { user: { id } } })
+        .aggregate()
+        .count(),
     followingCount: ({ id }) =>
       prisma
         .usersConnection({ where: { followers_some: { id } } })
