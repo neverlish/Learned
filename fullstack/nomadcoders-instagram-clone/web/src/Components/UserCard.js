fa34d8839c93f97322f8b68dcb12eac2a3899b94
Jinho Hyeon <neverlish@gmail.com>
2019-11-13 23:39:28 +0900

    08 - 03 Follow Button

--- fullstack/nomadcoders-instagram-clone/web/src/Components/UserCard.js
+++ fullstack/nomadcoders-instagram-clone/web/src/Components/UserCard.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import Avatar from "./Avatar";
 import FatText from "./FatText";
-import Button from "./Button";
+import FollowButton from "./FollowButton";
 import { Link } from "react-router-dom";
 
 const Card = styled.div`
@@ -23,17 +23,18 @@ const ELink = styled(Link)`
   margin-bottom: 10px;
 `;
 
-const UserCard = ({ username, isFollowing, url, isSelf }) => (
+const UserCard = ({ id, username, isFollowing, url, isSelf }) => (
   <Card>
     <EAvatar url={url} size={"md"} />
     <ELink to={`/${username}`}>
       <FatText text={username} />
     </ELink>
-    {!isSelf && <Button text={isFollowing ? "Unfollow" : "Follow"} />}
+    {!isSelf && <FollowButton id={id} isFollowing={isFollowing} />}
   </Card>
 );
 
 UserCard.propTypes = {
+  id: PropTypes.string.isRequired,
   username: PropTypes.string.isRequired,
   isFollowing: PropTypes.bool.isRequired,
   url: PropTypes.string.isRequired,
