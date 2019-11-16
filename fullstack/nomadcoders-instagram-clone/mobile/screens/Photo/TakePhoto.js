6554c9344d2efd695ea13776083c9139c942f6d3
Jinho Hyeon <neverlish@gmail.com>
2019-11-16 17:53:55 +0900

    18 - 05 Saving Photo

--- fullstack/nomadcoders-instagram-clone/mobile/screens/Photo/TakePhoto.js
+++ fullstack/nomadcoders-instagram-clone/mobile/screens/Photo/TakePhoto.js
@@ -1,24 +1,52 @@
-import { Camera } from "expo-camera";
 import { Ionicons } from "@expo/vector-icons";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 import * as Permissions from "expo-permissions";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Platform, TouchableOpacity } from "react-native";
 import styled from "styled-components";
 import Loader from "../../components/Loader";
 import constants from "../../constants";
-import { TouchableOpacity, Platform } from "react-native";
 import styles from "../../styles";
 
 const View = styled.View`
   flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Icon = styled.View``;
 
+const Button = styled.View`
+  width: 80;
+  height: 80;
+  border-radius: 40px;
+  border: 10px solid ${styles.lightGreyColor};
+`;
 
 export default ({ navigation }) => {
+  const cameraRef = useRef();
+  const [canTakePhoto, setCanTakePhoto] = useState(true);
   const [loading, setLoading] = useState(true);
   const [hasPermission, setHasPermission] = useState(false);
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.front);
+
+  const takePhoto = async () => {
+    if (!canTakePhoto) {
+      return;
+    }
+    try {
+      setCanTakePhoto(false);
+      const { uri } = await cameraRef.current.takePictureAsync({
+        quality: 1
+      });
+      const asset = await MediaLibrary.createAssetAsync(uri);
+      console.log(asset);
+    } catch (e) {
+      console.log(e);
+      setCanTakePhoto(true);
+    }
+  };
 
   const askPermission = async () => {
     try {
@@ -51,29 +79,37 @@ export default ({ navigation }) => {
       {loading ? (
         <Loader />
       ) : hasPermission ? (
-        <Camera
-          type={cameraType}
-          style={{
-            justifyContent: "flex-end",
-            padding: 15,
-            width: constants.width,
-            height: constants.height / 2
-          }}
-        >
-          <TouchableOpacity onPress={toggleType}>
-            <Icon>
-              <Ionicons
-                name={
-                  Platform.OS === "ios"
-                    ? "ios-reverse-camera"
-                    : "md-reverse-camera"
-                }
-                size={32}
-                color={"white"}
-              />
-            </Icon>
-          </TouchableOpacity>
-        </Camera>
+        <>
+          <Camera
+            ref={cameraRef}
+            type={cameraType}
+            style={{
+              justifyContent: "flex-end",
+              padding: 15,
+              width: constants.width,
+              height: constants.height / 2
+            }}
+          >
+            <TouchableOpacity onPress={toggleType}>
+              <Icon>
+                <Ionicons
+                  name={
+                    Platform.OS === "ios"
+                      ? "ios-reverse-camera"
+                      : "md-reverse-camera"
+                  }
+                  size={32}
+                  color={"white"}
+                />
+              </Icon>
+            </TouchableOpacity>
+          </Camera>
+          <View>
+            <TouchableOpacity onPress={takePhoto} disabled={!canTakePhoto}>
+              <Button />
+            </TouchableOpacity>
+          </View>
+        </>
       ) : null}
     </View>
   );
