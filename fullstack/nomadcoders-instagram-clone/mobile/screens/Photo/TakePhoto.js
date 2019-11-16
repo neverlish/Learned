07030b088c3549452c58883e2baba166e9d841bb
Jinho Hyeon <neverlish@gmail.com>
2019-11-16 17:48:33 +0900

    18 - 04 Take Photo part Two

--- fullstack/nomadcoders-instagram-clone/mobile/screens/Photo/TakePhoto.js
+++ fullstack/nomadcoders-instagram-clone/mobile/screens/Photo/TakePhoto.js
@@ -1,17 +1,25 @@
-import { Camera } from "expo";
+import { Camera } from "expo-camera";
+import { Ionicons } from "@expo/vector-icons";
 import * as Permissions from "expo-permissions";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Loader from "../../components/Loader";
 import constants from "../../constants";
+import { TouchableOpacity, Platform } from "react-native";
+import styles from "../../styles";
 
 const View = styled.View`
   flex: 1;
 `;
 
+const Icon = styled.View``;
+
+
 export default ({ navigation }) => {
   const [loading, setLoading] = useState(true);
   const [hasPermission, setHasPermission] = useState(false);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.front);
+
   const askPermission = async () => {
     try {
       const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -26,6 +34,14 @@ export default ({ navigation }) => {
     }
   };
 
+  const toggleType = () => {
+    if (cameraType === Camera.Constants.Type.front) {
+      setCameraType(Camera.Constants.Type.back);
+    } else {
+      setCameraType(Camera.Constants.Type.front);
+    }
+  };
+
   useEffect(() => {
     askPermission();
   }, [])
@@ -36,8 +52,28 @@ export default ({ navigation }) => {
         <Loader />
       ) : hasPermission ? (
         <Camera
-          style={{ width: constants.width, height: constants.height / 2 }}
-        />
+          type={cameraType}
+          style={{
+            justifyContent: "flex-end",
+            padding: 15,
+            width: constants.width,
+            height: constants.height / 2
+          }}
+        >
+          <TouchableOpacity onPress={toggleType}>
+            <Icon>
+              <Ionicons
+                name={
+                  Platform.OS === "ios"
+                    ? "ios-reverse-camera"
+                    : "md-reverse-camera"
+                }
+                size={32}
+                color={"white"}
+              />
+            </Icon>
+          </TouchableOpacity>
+        </Camera>
       ) : null}
     </View>
   );
