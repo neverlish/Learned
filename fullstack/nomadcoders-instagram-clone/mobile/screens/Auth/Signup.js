8a53a1e271a0c26302eebddea8232d6cbdd71a52
Jinho Hyeon <neverlish@gmail.com>
2019-11-16 15:20:00 +0900

    12 - 08 Facebook Login part One

--- fullstack/nomadcoders-instagram-clone/mobile/screens/Auth/Signup.js
+++ fullstack/nomadcoders-instagram-clone/mobile/screens/Auth/Signup.js
@@ -1,16 +1,25 @@
+import * as Facebook from 'expo-facebook';
 import React, { useState } from "react";
+import { useMutation } from "react-apollo-hooks";
+import { Alert, Keyboard, TouchableWithoutFeedback } from "react-native";
 import styled from "styled-components";
-import { TouchableWithoutFeedback, Keyboard, Alert } from "react-native";
 import AuthButton from "../../components/AuthButton";
 import AuthInput from "../../components/AuthInput";
 import useInput from "../../hooks/useInput";
-import { useMutation } from "react-apollo-hooks";
 import { CREATE_ACCOUNT } from "./AuthQueries";
 
 const View = styled.View`
   justify-content: center;
   align-items: center;
   flex: 1;
+`;
+
+const FBContainer = styled.View`
+  margin-top: 25px;
+  padding-top: 25px;
+  border-top-width: 1px;
+  border-color: ${props => props.theme.lightGreyColor};
+  border-style: solid;
 `;
 
 export default ({ navigation }) => {
@@ -59,6 +68,26 @@ export default ({ navigation }) => {
       setLoading(false);
     }
   };
+  const fbLogin = async () => {
+    try {
+      const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+        "329644654379394",
+        {
+          permissions: ["public_profile"]
+        }
+      );
+      if (type === "success") {
+        const response = await fetch(
+          `https://graph.facebook.com/me?access_token=${token}`
+        );
+        Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  };
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View>
@@ -86,6 +115,14 @@ export default ({ navigation }) => {
           autoCorrect={false}
         />
         <AuthButton loading={loading} onPress={handleSingup} text="Sign up" />
+        <FBContainer>
+          <AuthButton
+            bgColor={"#2D4DA7"}
+            loading={false}
+            onPress={fbLogin}
+            text="Connect Facebook"
+          />
+        </FBContainer>
       </View>
     </TouchableWithoutFeedback>
   );
