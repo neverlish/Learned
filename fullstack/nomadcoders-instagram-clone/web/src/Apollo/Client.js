0a2cab197dd39a407b411916c895cee6ea49e192
Jinho Hyeon <neverlish@gmail.com>
2019-11-17 01:55:24 +0900

    19 - 07 Deploying Prisma Server part Two + Netlify Fix

--- fullstack/nomadcoders-instagram-clone/web/src/Apollo/Client.js
+++ fullstack/nomadcoders-instagram-clone/web/src/Apollo/Client.js
@@ -2,7 +2,9 @@ import ApolloClient from 'apollo-boost';
 import { defaults, resolvers } from './LocalState';
 
 export default new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: process.env.NODE_ENV === "development"
+    ? "http://localhost:4000"
+    : "https://prismagram-neverlish.herokuapp.com",
   clientState: {
     defaults,
     resolvers,
