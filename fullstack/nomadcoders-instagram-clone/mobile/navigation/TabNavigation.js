499391ed139a9bc105da24cf2c235011577a6d74
Jinho Hyeon <neverlish@gmail.com>
2019-11-16 13:04:29 +0900

    11 - 04 Messages Navigation part One

--- fullstack/nomadcoders-instagram-clone/mobile/navigation/TabNavigation.js
+++ fullstack/nomadcoders-instagram-clone/mobile/navigation/TabNavigation.js
@@ -1,13 +1,34 @@
-import { View } from "react-native";
-import { createBottomTabNavigator } from "react-navigation";
-import Home from "../screens/Home";
-import Search from "../screens/Search";
-import Notifications from "../screens/Notifications";
-import Profile from "../screens/Profile";
+import React from 'react';
+import { View, Text, TouchableOpacity } from "react-native";
+import { createBottomTabNavigator, createStackNavigator } from "react-navigation";
+import Home from "../screens/Tabs/Home";
+import Search from "../screens/Tabs/Search";
+import Notifications from "../screens/Tabs/Notifications";
+import Profile from "../screens/Tabs/Profile";
+
+const stackFactory = (initialRoute, customConfig) => createStackNavigator({
+  InitialRoute: {
+    screen: initialRoute,
+    navigationOptions: { ...customConfig },
+  }
+});
 
 export default createBottomTabNavigator({
-  Home,
-  Search,
+  Home: {
+    screen: stackFactory(Home, {
+      title: 'Home',
+      headerRight: (
+        <TouchableOpacity>
+          <Text>Hello</Text>
+        </TouchableOpacity>
+      )
+    }),
+  },
+  Search: {
+    screen: stackFactory(Search, {
+      title: 'Search'
+    }),
+  },
   Add: {
     screen: View,
     navigationOptions: {
@@ -16,6 +37,14 @@ export default createBottomTabNavigator({
       }
     }
   },
-  Notifications,
-  Profile,
+  Notifications: {
+    screen: stackFactory(Notifications, {
+      title: "Notifications"
+    })
+  },
+  Profile: {
+    screen: stackFactory(Profile, {
+      title: "Profile"
+    })
+  },
 });
