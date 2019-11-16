72211e2b917749e7f163d5ebd4b93a400f46b1f0
Jinho Hyeon <neverlish@gmail.com>
2019-11-16 16:03:59 +0900

    14 App: Home Screen - 00 Apollo Context

--- fullstack/nomadcoders-instagram-clone/mobile/screens/Tabs/Home.js
+++ fullstack/nomadcoders-instagram-clone/mobile/screens/Tabs/Home.js
@@ -1,6 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import { gql } from "apollo-boost";
 import Loader from "../../components/Loader";
+import { useQuery } from "react-apollo-hooks";
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createdAt
+    }
+  }
+`;
 
 const View = styled.View`
   justify-content: center;
@@ -10,8 +42,8 @@ const View = styled.View`
 
 const Text = styled.Text``;
 
-export default () => (
-  <View>
-    <Loader />
-  </View>
-);
+export default () => {
+  const { loading, data } = useQuery(FEED_QUERY);
+  console.log(loading, data);
+  return <View>{loading ? <Loader /> : null}</View>
+};
