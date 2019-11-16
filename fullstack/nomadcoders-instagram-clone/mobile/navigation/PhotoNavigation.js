7d6a5b3cfb86adc68eaad691e71f1c8c915a15b8
Jinho Hyeon <neverlish@gmail.com>
2019-11-16 17:35:36 +0900

    18 - 02 Select Photo part Two

--- fullstack/nomadcoders-instagram-clone/mobile/navigation/PhotoNavigation.js
+++ fullstack/nomadcoders-instagram-clone/mobile/navigation/PhotoNavigation.js
@@ -44,7 +44,7 @@ export default createStackNavigator(
     Tabs: {
       screen: PhotoTabs,
       navigationOptions: {
-        header: null
+        title: "Choose Photo"
       }
     },
     UploadPhoto
