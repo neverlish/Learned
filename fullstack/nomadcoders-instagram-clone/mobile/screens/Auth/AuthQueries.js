fcbe5567e281c1e8f57870884c34dfcf203beadf
Jinho Hyeon <neverlish@gmail.com>
2019-11-16 13:38:45 +0900

    12 - 04 Login part Two

---
+++ fullstack/nomadcoders-instagram-clone/mobile/screens/Auth/AuthQueries.js
@@ -0,0 +1,8 @@
+
+import { gql } from "apollo-boost";
+
+export const LOG_IN = gql`
+  mutation requestSecret($email: String!) {
+    requestSecret(email: $email)
+  }
+`;
--- fullstack/nomadcoders-instagram-clone/mobile/screens/Auth/Login.js
+++ fullstack/nomadcoders-instagram-clone/mobile/screens/Auth/Login.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { TouchableWithoutFeedback, Keyboard, Alert } from "react-native";
 import AuthButton from "../../components/AuthButton";
 import AuthInput from "../../components/AuthInput";
 import useInput from "../../hooks/useInput";
+import { useMutation } from "react-apollo-hooks";
+import { LOG_IN } from "./AuthQueries";
 
 const View = styled.View`
   justify-content: center;
@@ -11,9 +13,15 @@ const View = styled.View`
   flex: 1;
 `;
 
-export default () => {
+export default ({ navigation }) => {
   const emailInput = useInput('');
-  const handleLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const [requestSecret] = useMutation(LOG_IN, {
+    variables: {
+      email: emailInput.value
+    }
+  });
+  const handleLogin = async () => {
     const { value } = emailInput;
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (value === "") {
@@ -22,6 +30,16 @@ export default () => {
       return Alert.alert("Please write an email");
     } else if (!emailRegex.test(value)) {
       return Alert.alert("That email is invalid");
+    }
+    try {
+      setLoading(true);
+      await requestSecret();
+      Alert.alert("Check your email");
+      navigation.navigate("Confirm");
+    } catch (e) {
+      Alert.alert("Can't log in now");
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -35,7 +53,7 @@ export default () => {
           onEndEditing={handleLogin}
           autoCorrect={false}
         />
-        <AuthButton onPress={() => null} text="Log In" />
+        <AuthButton loading={loading} onPress={() => null} text="Log In" />
       </View>
     </TouchableWithoutFeedback>
   );
