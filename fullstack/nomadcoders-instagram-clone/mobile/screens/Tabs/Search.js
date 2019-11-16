0d31a9da214a2ad6cab414344b45bdf2828123ca
Jinho Hyeon <neverlish@gmail.com>
2019-11-16 16:47:04 +0900

    16 - 01 Search as HeaderTitle part Two

--- fullstack/nomadcoders-instagram-clone/mobile/screens/Tabs/Search.js
+++ fullstack/nomadcoders-instagram-clone/mobile/screens/Tabs/Search.js
@@ -11,20 +11,36 @@ const View = styled.View`
 const Text = styled.Text``;
 
 export default class extends React.Component {
-  state = {
-    term: ''
-  };
   static navigationOptions = ({ navigation }) => ({
     headerTitle: (
       <SearchBar
-        value={''}
-        onChange={this.onChange}
-        onSubmit={() => { }}
+        value={navigation.getParam("term", "")}
+        onChange={navigation.getParam("onChange", () => null)}
+        onSubmit={navigation.getParam("onSubmit", () => null)}
       />
     )
   });
+  constructor(props) {
+    super(props);
+    const { navigation } = props;
+    this.state = {
+      term: ''
+    };
+    navigation.setParams({
+      term: this.state.term,
+      onChange: this.onChange,
+      onSubmit: this.onSubmit
+    });
+  }
   onChange = text => {
-    this.setState({ text });
+    const { navigation } = this.props;
+    this.setState({ term: text });
+    navigation.setParams({
+      term: text
+    });
+  };
+  onSubmit = () => {
+    console.log("Submit");
   };
   render() {
     return (
