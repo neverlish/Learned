afaff1868b64ad7f4065c509718570c46ecad4e8
Jinho Hyeon <neverlish@gmail.com>
2019-11-17 10:14:34 +0900

    21 - 03 Sending Messages and KeyboardAvoidingView

--- fullstack/nomadcoders-instagram-clone/mini-talk/mobile/Chat.tsx
+++ fullstack/nomadcoders-instagram-clone/mini-talk/mobile/Chat.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { View, Text, ScrollView, TextInput } from "react-native";
+import React, { useState, useMemo } from "react";
+import { View, Text, ScrollView, TextInput, KeyboardAvoidingView } from "react-native";
 import gql from "graphql-tag";
-import { useQuery } from "react-apollo-hooks";
+import { useQuery, useMutation } from "react-apollo-hooks";
 import withSuspense from "./withSuspense";
 
-const CHAT = gql`
+const GET_MESSAGES = gql`
   query messages {
     messages {
       id
@@ -13,20 +13,69 @@ const CHAT = gql`
   }
 `;
 
+const SEND_MESSAGE = gql`
+  mutation sendMessage($text: String!) {
+    sendMessage(text: $text) {
+      id
+      text
+    }
+  }
+`;
+
 function Chat() {
-  const { data, error } = useQuery(CHAT, { suspend: true });
-  console.log(data);
+  const [message, setMessage] = useState("");
+
+  const sendMessageMutation = useMutation(SEND_MESSAGE, {
+    variables: {
+      text: message,
+    }
+  });
+  const { data, error } = useQuery(GET_MESSAGES, { suspend: true });
+  const onChangeText = text => setMessage(text);
+  const onSubmit = async () => {
+    if (message === "") {
+      return;
+    }
+    try {
+      await sendMessageMutation();
+      setMessage("");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <ScrollView
-      contentContainerStyle={{
-        paddingVertical: 50,
-        flex: 1,
-        justifyContent: "flex-end",
-        alignItems: "center"
-      }}
-    >
-      <Text>Hello</Text>
-    </ScrollView>
+    <KeyboardAvoidingView style={{ flex: 1 }} enabled behavior='padding'>
+      <ScrollView
+        contentContainerStyle={{
+          paddingVertical: 50,
+          flex: 1,
+          justifyContent: "flex-end",
+          alignItems: "center"
+        }}
+      >
+        {data.messages.map(m => (
+          <View key={m.id} style={{ marginBottom: 10 }}>
+            <Text>{m.text}</Text>
+          </View>
+        ))}
+        <TextInput
+          placeholder="Type a message"
+          style={{
+            marginTop: 50,
+            width: "90%",
+            borderRadius: 10,
+            paddingVertical: 15,
+            paddingHorizontal: 10,
+            backgroundColor: "#f2f2f2"
+          }}
+          returnKeyType="send"
+          value={message}
+          onChangeText={onChangeText}
+          onSubmitEditing={onSubmit}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
