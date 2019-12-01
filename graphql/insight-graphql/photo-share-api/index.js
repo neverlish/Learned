4f5477229b9f3ddb1f02e23098a1817be21089af
Jinho Hyeon <neverlish@gmail.com>
2019-12-01 12:58:12 +0900

    5 - 3 apollo-server-express

--- graphql/insight-graphql/photo-share-api/index.js
+++ graphql/insight-graphql/photo-share-api/index.js
@@ -1,145 +1,20 @@
-const { ApolloServer } = require('apollo-server');
-const { GraphQLScalarType } = require('graphql');
+const { ApolloServer } = require('apollo-server-express')
+const express = require('express')
+const expressPlayground = require('graphql-playground-middleware-express').default
+const { readFileSync } = require('fs')
 
-const typeDefs = `
-  scalar DateTime
+const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
+const resolvers = require('./resolvers')
 
-  enum PhotoCategory {
-    SELFIE
-    PORTRAIT
-    ACTION
-    LANDSCAPE
-    GRAPHIC
-  }
-  type User {
-    githubLogin: ID!
-    name: String
-    avatar: String
-    postedPhotos: [Photo!]!
-    inPhotos: [Photo!]!
-  }
-  type Photo {
-    id: ID!
-    created: DateTime!
-    url: String!
-    name: String!
-    description: String
-    category: PhotoCategory!
-    postedBy: User!
-    taggedUsers: [User!]!
-  }
-  type Query {
-    totalPhotos: Int!
-    allPhotos(after: DateTime): [Photo!]!
-  }
-  input PostPhotoInput {
-    name: String!
-    category: PhotoCategory=PORTRAIT
-    description: String
-  }
-  type Mutation {
-    postPhoto(input: PostPhotoInput!): Photo!
-  }
-`
+var app = express()
 
-var _id = 0
-var users = [
-  {
-    githubLogin: 'mHattrup',
-    name: 'Mike Hattrup'
-  },
-  {
-    githubLogin: 'gPlake',
-    name: 'Glen Plake'
-  },
-  {
-    githubLogin: 'sSchmidt',
-    name: 'Scot Schmidt'
-  }
-]
+const server = new ApolloServer({ typeDefs, resolvers })
 
-var photos = [
-  {
-    id: '1',
-    name: 'Dropping the Heart Chute',
-    description: 'The heart chute is one of my favorite chutes',
-    category: 'ACTION',
-    githubUser: 'gPlake',
-    created: '3-28-1977'
-  },
-  {
-    id: '2',
-    name: 'Enjoying the sunshine',
-    category: 'SELFIE',
-    githubUser: 'sSchmidt',
-    created: '1-2-1985'
-  },
-  {
-    id: '3',
-    name: 'Gunbarrel 25',
-    description: '25 laps on gunbarrel today',
-    category: 'LANDSCAPE',
-    githubUser: 'sSchmidt',
-    created: '2018-04-15T19:09:57.308Z'
-  }
-]
+server.applyMiddleware({ app })
 
-var tags = [
-  { photoID: '1', userID: 'gPlake' },
-  { photoID: '2', userID: 'sSchmidt' },
-  { photoID: '2', userID: 'mHattrup' },
-  { photoID: '2', userID: 'gPlake' }
-]
+app.get('/', (req, res) => res.send('PhotoShare API에 오신 것을 환영합니다.'))
 
-const resolvers = {
-  Query: {
-    totalPhotos: () => photos.length,
-    allPhotos: (parent, args) => photos
-  },
-  Mutation: {
-    postPhoto(parent, args) {
-      var newPhoto = {
-        id: _id++,
-        ...args.input,
-        created: new Date()
-      }
-      photos.push(newPhoto)
-      return newPhoto
-    }
-  },
-  Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
-    postedBy: parent => {
-      return users.find(u => u.githubLogin === parent.githubUser)
-    },
-    taggedUsers: parent => tags
-      .filter(tag => tag.photoID === parent.id)
-      .map(tag => tag.userID)
-      .map(userID => users.find(u => u.githubLogin === userID))
-  },
-  User: {
-    postedPhotos: parent => {
-      return photos.filter(p => p.githubUser === parent.githubLogin)
-    },
-    inPhotos: parent => tags
-      .filter(tag => tag.userID === parent.id)
-      .map(tag => tag.photoID)
-      .map(photoID => photos.find(p => p.id === photoID))
-  },
-  DateTime: new GraphQLScalarType({
-    name: 'DateTime',
-    description: 'A valid date time value.',
-    parseValue: value => new Date(value),
-    serialize: value => new Date(value).toISOString(),
-    parseLiteral: ast => ast.value
-  })
-}
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-})
-
-server
-  .listen()
-  .then(({ url }) => console.log(`GraphQL Service runnin on ${url}`))
+app.listen({ port: 4000 }, () =>
+  console.log(`GraphQL Service running on http://localhost:4000${server.graphqlPath}`))
---
+++ graphql/insight-graphql/photo-share-api/resolvers/index.js
@@ -0,0 +1,96 @@
+const { GraphQLScalarType } = require('graphql')
+
+var _id = 0
+var users = [
+  {
+    githubLogin: 'mHattrup',
+    name: 'Mike Hattrup'
+  },
+  {
+    githubLogin: 'gPlake',
+    name: 'Glen Plake'
+  },
+  {
+    githubLogin: 'sSchmidt',
+    name: 'Scot Schmidt'
+  }
+]
+
+var photos = [
+  {
+    id: '1',
+    name: 'Dropping the Heart Chute',
+    description: 'The heart chute is one of my favorite chutes',
+    category: 'ACTION',
+    githubUser: 'gPlake',
+    created: '3-28-1977'
+  },
+  {
+    id: '2',
+    name: 'Enjoying the sunshine',
+    category: 'SELFIE',
+    githubUser: 'sSchmidt',
+    created: '1-2-1985'
+  },
+  {
+    id: '3',
+    name: 'Gunbarrel 25',
+    description: '25 laps on gunbarrel today',
+    category: 'LANDSCAPE',
+    githubUser: 'sSchmidt',
+    created: '2018-04-15T19:09:57.308Z'
+  }
+]
+
+var tags = [
+  { photoID: '1', userID: 'gPlake' },
+  { photoID: '2', userID: 'sSchmidt' },
+  { photoID: '2', userID: 'mHattrup' },
+  { photoID: '2', userID: 'gPlake' }
+]
+
+const resolvers = {
+  Query: {
+    totalPhotos: () => photos.length,
+    allPhotos: (parent, args) => photos
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      var newPhoto = {
+        id: _id++,
+        ...args.input,
+        created: new Date()
+      }
+      photos.push(newPhoto)
+      return newPhoto
+    }
+  },
+  Photo: {
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser)
+    },
+    taggedUsers: parent => tags
+      .filter(tag => tag.photoID === parent.id)
+      .map(tag => tag.userID)
+      .map(userID => users.find(u => u.githubLogin === userID))
+  },
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin)
+    },
+    inPhotos: parent => tags
+      .filter(tag => tag.userID === parent.id)
+      .map(tag => tag.photoID)
+      .map(photoID => photos.find(p => p.id === photoID))
+  },
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'A valid date time value.',
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString(),
+    parseLiteral: ast => ast.value
+  })
+}
+
+module.exports = resolvers
