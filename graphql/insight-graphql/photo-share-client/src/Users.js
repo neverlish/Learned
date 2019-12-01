2149c68d2ac2a0a02bec30f0ba246a42360780ca
Jinho Hyeon <neverlish@gmail.com>
2019-12-01 20:43:23 +0900

    6 - 3 - 4 Mutation 컴포넌트

--- graphql/insight-graphql/photo-share-client/src/Users.js
+++ graphql/insight-graphql/photo-share-client/src/Users.js
@@ -1,6 +1,17 @@
 import React from 'react'
-import { Query } from "react-apollo"
+import { Query, Mutation } from "react-apollo"
 import { ROOT_QUERY } from './App'
+import { gql } from 'apollo-boost'
+
+const ADD_FAKE_USERS_MUTATION = gql`
+  mutation addFakeUsers($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`
 
 const Users = () =>
   <Query query={ROOT_QUERY}>
@@ -18,6 +29,14 @@ const UserList = ({ count, users, refetchUsers }) =>
   <div>
     <p>{count} Users</p>
     <button onClick={() => refetchUsers()}>다시 가져오기</button>
+    <Mutation
+      mutation={ADD_FAKE_USERS_MUTATION}
+      variables={{ count: 1 }}
+      refetchQueries={[{ query: ROOT_QUERY }]}>
+      {addFakeUsers =>
+        <button onClick={addFakeUsers}>임시 사용자 추가</button>
+      }
+    </Mutation>
     <ul>
       {users.map(user =>
         <UserListItem
