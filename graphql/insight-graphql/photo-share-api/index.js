d0244768a20a2037f5d63926116edfb47809e9b1
Jinho Hyeon <neverlish@gmail.com>
2019-12-01 09:46:10 +0900

    5 - 2 - 2 타입 리졸버

--- graphql/insight-graphql/photo-share-api/index.js
+++ graphql/insight-graphql/photo-share-api/index.js
@@ -1,25 +1,41 @@
 const { ApolloServer } = require('apollo-server');
 
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
   }
   type Mutation {
-    postPhoto(name: String!, description: String): Boolean!
+    postPhoto(name: String!, description: String): Photo!
   }
 `
 
+var _id = 0
 var photos = []
 
 const resolvers = {
   Query: {
-    totalPhotos: () => photos.length
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
   },
   Mutation: {
     postPhoto(parent, args) {
-      photos.push(args)
-      return true
+      var newPhoto = {
+        id: _id++,
+        ...args
+      }
+      photos.push(newPhoto)
+      return newPhoto
     }
+  },
+  Photo: {
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`
   }
 }
 
