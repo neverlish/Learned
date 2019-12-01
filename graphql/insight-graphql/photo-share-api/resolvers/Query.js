501d8d006fe088a20997354b90ff39d6c63a0588
Jinho Hyeon <neverlish@gmail.com>
2019-12-01 22:50:57 +0900

    7 - 3 - 2 데이터 제한

--- graphql/insight-graphql/photo-share-api/resolvers/Query.js
+++ graphql/insight-graphql/photo-share-api/resolvers/Query.js
@@ -2,10 +2,14 @@ module.exports = {
   me: (parent, args, { currentUser }) => currentUser,
   totalPhotos: (parent, args, { db }) =>
     db.collection('photos').estimatedDocumentCount(),
-  allPhotos: (parent, args, { db }) =>
-    db.collection('photos')
+  allPhotos: async (parent, args, { db }) => {
+    if (args.first > 100) {
+      throw new Error('Only 100 photos can be requested at a time')
+    }
+    return await db.collection('photos')
       .find()
-      .toArray(),
+      .toArray()
+  },
   totalUsers: (parent, args, { db }) =>
     db.collection('users').estimatedDocumentCount(),
   allUsers: (parent, args, { db }) =>
