9455fa44688b5c52744bd438662e4ea127581918
Jinho Hyeon <neverlish@gmail.com>
2019-12-15 23:46:45 +0900

    06 데이터와 연산

---
+++ java/ot-java1/Data_and_operation/Datatype.java
@@ -0,0 +1,19 @@
+public class Datatype {
+	public static void main(String[] args) {
+		System.out.println(6);
+		System.out.println("six");
+		
+		System.out.println("6");
+		
+		System.out.println(6+6);
+		System.out.println("6" + "6");
+		
+		
+		System.out.println(6*6);
+//		System.out.println("6" * "6");
+		
+		System.out.println("1111".length());
+		
+//		System.out.println(1111.length());
+	}
+}
