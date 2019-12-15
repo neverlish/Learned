33ea3cbd194e0c661dcc37cd87d53df322436c08
Jinho Hyeon <neverlish@gmail.com>
2019-12-15 23:50:10 +0900

    07 숫자와 연산

---
+++ java/ot-java1/Data_and_operation/Number.java
@@ -0,0 +1,12 @@
+public class Number {
+	public static void main(String[] args) {
+		System.out.println(6 + 2);
+		System.out.println(6 - 2);
+		System.out.println(6 * 2);
+		System.out.println(6 / 2);
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(Math.PI));
+		System.out.println(Math.ceil(Math.PI));
+	}
+}
