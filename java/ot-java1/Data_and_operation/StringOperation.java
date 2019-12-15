e10154f72d12a432a2f7c6e15686722bcebeaeef
Jinho Hyeon <neverlish@gmail.com>
2019-12-15 23:58:38 +0900

    08 문자열 다루기

---
+++ java/ot-java1/Data_and_operation/StringOperation.java
@@ -0,0 +1,6 @@
+public class StringOperation {
+	public static void main(String[] args) {
+		System.out.println("Hello World".length());
+        System.out.println("Hello, [[[name]]] ... bye. ".replace("[[[name]]]", "duru"));
+	}
+}
