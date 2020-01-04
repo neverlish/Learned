ccafda72b0f5730e01b39b81b4340513c7ad4cf2
Jinho Hyeon <neverlish@gmail.com>
2020-01-05 00:58:18 +0900

    3 메소드의 기본 형식

---
+++ java/ot-java1-method/ProjectMethod/WhyMethod.java
@@ -0,0 +1,21 @@
+
+public class WhyMethod {
+
+	public static void main(String[] args) {
+        
+        // 100000000
+        printTwoTimesA();
+        // 100000000
+        printTwoTimesA();
+        // 100000000
+        printTwoTimesA();
+ 
+    }
+ 
+    public static void printTwoTimesA() {
+        System.out.println("-");
+        System.out.println("a");
+        System.out.println("a");
+    }
+
+}
