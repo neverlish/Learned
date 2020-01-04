1b8b890e6a197d01b34e4520602ff183169c1062
Jinho Hyeon <neverlish@gmail.com>
2020-01-05 00:32:58 +0900

    7 - 2 배열

---
+++ java/ot-java1-flow-control/java-flow-control/ArrayApp.java
@@ -0,0 +1,21 @@
+
+public class ArrayApp {
+
+	public static void main(String[] args) {
+		// egoing, jinhuck, youbin 
+//      String users = "egoing, jinhuck, youbin";
+        String[] users = new String[3];
+        users[0] = "egoing";
+        users[1] = "jinhuck";
+        users[2] = "youbin";
+         
+        System.out.println(users[1]);
+        System.out.println(users.length);
+         
+        int[] scores = {10, 100, 100}; // 원소, element
+        System.out.println(scores[1]);
+        System.out.println(scores.length);
+
+	}
+
+}
