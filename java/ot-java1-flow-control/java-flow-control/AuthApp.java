75615d88979ca5ac9f76b6cb5c1dbd308e342332
Jinho Hyeon <neverlish@gmail.com>
2020-01-04 23:47:26 +0900

    4 - 3 조건문 응용

--- java/ot-java1-flow-control/java-flow-control/AuthApp.java
+++ java/ot-java1-flow-control/java-flow-control/AuthApp.java
@@ -5,14 +5,16 @@ public class AuthApp {
 		String id = "egoing";
         String inputId = args[0];
          
+        String pass = "1111";
+        String inputPass = args[1];
+         
         System.out.println("Hi.");
          
-        //if(inputId == id) {
-        if(inputId.equals(id)) {
+        if(inputId.equals(id) && inputPass.equals(pass)) {
             System.out.println("Master!");
         } else {
             System.out.println("Who are you?");
-        }
+        }  
 
 	}
 
