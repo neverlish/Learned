77fa2fbe1d4a065f4f492603315203b472d46dd0
Jinho Hyeon <neverlish@gmail.com>
2020-01-05 00:41:35 +0900

    8 - 2 종합응용 2과 수업을 마치며

--- java/ot-java1-flow-control/java-flow-control/AuthApp3.java
+++ java/ot-java1-flow-control/java-flow-control/AuthApp3.java
@@ -2,13 +2,21 @@
 public class AuthApp3 {
 
 	public static void main(String[] args) {
-		String[] users = {"egoing", "jinhuck", "youbin"};
+		String[][] users = {
+                {"egoing", "1111"},
+                {"jinhuck", "2222"},
+                {"youbin", "3333"}
+        };
         String inputId = args[0];
+        String inputPass = args[1];
          
         boolean isLogined = false;
         for(int i=0; i<users.length; i++) {
-            String currentId = users[i];
-            if(currentId.equals(inputId)) {
+            String[] current = users[i];
+            if(
+                    current[0].equals(inputId) && 
+                    current[1].equals(inputPass)
+            ) {
                 isLogined = true;
                 break;
             }
