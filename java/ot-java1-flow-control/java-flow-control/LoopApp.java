6db5b0a88cc1de5d275d16019eb2b2ba70b0fb3a
Jinho Hyeon <neverlish@gmail.com>
2020-01-05 00:25:47 +0900

    7 반복문과 배열 - 1 반복문

---
+++ java/ot-java1-flow-control/java-flow-control/LoopApp.java
@@ -0,0 +1,26 @@
+
+public class LoopApp {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+        System.out.println("=== while ===");
+        int i = 0;
+        //..
+        while(i < 3) {
+            System.out.println(2);
+            System.out.println(3);
+//          i = i + 1;
+            //..
+            i++;
+        }
+        System.out.println("=== for ===");
+        for(int j=0; j < 3; j++) {
+            System.out.println(2);
+            System.out.println(3);
+        }
+         
+        System.out.println(4);
+
+	}
+
+}
