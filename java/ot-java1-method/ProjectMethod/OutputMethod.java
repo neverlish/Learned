483d5c7371af37c4eb21e9e76032cd7855262b0d
Jinho Hyeon <neverlish@gmail.com>
2020-01-05 01:08:16 +0900

    5 메소드의 출력

---
+++ java/ot-java1-method/ProjectMethod/OutputMethod.java
@@ -0,0 +1,20 @@
+public class OutputMethod {
+     
+    public static String a() {
+        // ... 
+        return "a";
+    }
+     
+    public static int one() {
+        return 1;
+        //...
+    }
+ 
+    public static void main(String[] args) {
+ 
+        System.out.println(a());
+        System.out.println(one());
+         
+    }
+ 
+}
