78810fe4a844aa337ee473afdb1b43f576c815b4
Jinho Hyeon <neverlish@gmail.com>
2020-01-04 23:25:34 +0900

    2 boolean

---
+++ java/ot-java1-flow-control/java-flow-control/BooleanApp.java
@@ -0,0 +1,19 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		System.out.println("One");
+        System.out.println(1);
+         
+        System.out.println(true);
+        System.out.println(false);
+         
+        String foo = "Hello world";
+        // String true = "Hello world"; reserved word
+         
+        System.out.println(foo.contains("world"));
+        System.out.println(foo.contains("egoing"));
+
+	}
+
+}
