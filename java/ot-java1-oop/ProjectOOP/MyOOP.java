77764c095ce941a58678439f90908436f3824240
Jinho Hyeon <neverlish@gmail.com>
2020-01-11 00:40:59 +0900

    4 - 2 형식

--- java/ot-java1-oop/ProjectOOP/MyOOP.java
+++ java/ot-java1-oop/ProjectOOP/MyOOP.java
@@ -1,19 +1,3 @@
-class Print {
-	public static String delimiter = "";
-	
-	public static void A() {
-        System.out.println(delimiter);
-        System.out.println("A");
-        System.out.println("A");
-    }
-    public static void B() {
-        System.out.println(delimiter);
-        System.out.println("B");
-        System.out.println("B");
-    }
-}
-
-
 public class MyOOP {
 
 	public static void main(String[] args) {
---
+++ java/ot-java1-oop/ProjectOOP/Print.java
@@ -0,0 +1,16 @@
+class Print {
+	public static String delimiter = "";
+	
+	public static void A() {
+        System.out.println(delimiter);
+        System.out.println("A");
+        System.out.println("A");
+    }
+    public static void B() {
+        System.out.println(delimiter);
+        System.out.println("B");
+        System.out.println("B");
+    }
+}
+
+
