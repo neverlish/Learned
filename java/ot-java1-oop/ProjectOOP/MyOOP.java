020deea7ac6eb4d1e9047a013ee4451ce0a8cd33
Jinho Hyeon <neverlish@gmail.com>
2020-01-11 01:17:02 +0900

    7 생성자와 this

--- java/ot-java1-oop/ProjectOOP/MyOOP.java
+++ java/ot-java1-oop/ProjectOOP/MyOOP.java
@@ -1,15 +1,13 @@
 public class MyOOP {
 
 	public static void main(String[] args) {
-		Print p1 = new Print();
-		p1.delimiter = "----";
+		Print p1 = new Print("----");
         p1.A();
         p1.A();
         p1.B();
         p1.B();
         
-        Print p2 = new Print();
-        p2.delimiter = "****";
+        Print p2 = new Print("****");
         p2.A();
         p2.A();
         p2.B();
--- java/ot-java1-oop/ProjectOOP/Print.java
+++ java/ot-java1-oop/ProjectOOP/Print.java
@@ -1,13 +1,17 @@
 class Print {
 	public String delimiter = "";
 	
+	public Print(String delimiter) {
+		this.delimiter = delimiter;
+	}
+	
 	public void A() {
-        System.out.println(delimiter);
+        System.out.println(this.delimiter);
         System.out.println("A");
         System.out.println("A");
     }
     public void B() {
-        System.out.println(delimiter);
+        System.out.println(this.delimiter);
         System.out.println("B");
         System.out.println("B");
     }
