1f40c4ec7789dc09a8053dbc492e29d6325e3546
Jinho Hyeon <neverlish@gmail.com>
2020-01-11 01:25:10 +0900

    8 - 2 인스턴스화

--- java/ot-java1-oop/ProjectOOP/AccountingApp.java
+++ java/ot-java1-oop/ProjectOOP/AccountingApp.java
@@ -1,20 +1,31 @@
 class Accounting{
-    public static double valueOfSupply;
+    public double valueOfSupply;
     public static double vatRate = 0.1;
-    public static double getVAT() {
+    public Accounting(double valueOfSupply) {
+        this.valueOfSupply = valueOfSupply;
+    }
+    public double getVAT() {
         return valueOfSupply * vatRate;
     }
-    public static double getTotal() {
+    public double getTotal() {
         return valueOfSupply + getVAT();
     }
 }
-
 public class AccountingApp {
     public static void main(String[] args) {
-        Accounting.valueOfSupply = 10000.0;
-        System.out.println("Value of supply : " + Accounting.valueOfSupply);
-        System.out.println("VAT : " + Accounting.getVAT());
-        System.out.println("Total : " + Accounting.getTotal());
+        Accounting a1 = new Accounting(10000.0);
+         
+        Accounting a2 = new Accounting(20000.0);
+         
+        System.out.println("Value of supply : " + a1.valueOfSupply);
+        System.out.println("Value of supply : " + a2.valueOfSupply);
+         
+        System.out.println("VAT : " + a1.getVAT());
+        System.out.println("VAT : " + a2.getVAT());
+         
+        System.out.println("Total : " + a1.getTotal());
+        System.out.println("Total : " + a2.getTotal());
+         
   
     }
 }
