759940a739b21b272b1c66d56ae82c52838bb81e
Jinho Hyeon <neverlish@gmail.com>
2020-01-11 12:54:15 +0900

    01 자바 프로그래밍 시작하기 - 3 이클립스로 첫 프로그램 만들기

---
+++ java/doit-java-intro/ch01/src/hello/HelloJava.java
@@ -0,0 +1,14 @@
+/*
+ * 여러 줄 주
+ */
+
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+		// System.out.println("Hello, Java"); 한 줄 주석 연습입니다.
+	}
+
+}
