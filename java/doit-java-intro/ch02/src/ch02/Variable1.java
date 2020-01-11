6fe14eba0df0242b36e67c6eed4e79e5b829b89c
Jinho Hyeon <neverlish@gmail.com>
2020-01-11 13:03:16 +0900

    02 변수와 자료형 - 2 변수란 무엇인가?

---
+++ java/doit-java-intro/ch02/src/ch02/Variable1.java
@@ -0,0 +1,15 @@
+// 변수 선언하여 사용하기
+
+package ch02;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		int level;
+		level = 10;
+		
+		System.out.println(level);
+
+	}
+
+}
---
+++ java/doit-java-intro/ch02/src/ch02/Variable2.java
@@ -0,0 +1,13 @@
+// 변수 초기화하기
+
+package ch02;
+
+public class Variable2 {
+
+	public static void main(String[] args) {
+		int level = 10;
+		System.out.println(level);
+
+	}
+
+}
