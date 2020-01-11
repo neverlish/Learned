1ce83750b109b169ed1a7342c054b79f647c6677
Jinho Hyeon <neverlish@gmail.com>
2020-01-11 18:29:31 +0900

    05 - 3 메서드

---
+++ java/doit-java-intro/ch05/src/classpart/FunctionTest.java
@@ -0,0 +1,20 @@
+// 함수 구현하고 호출하기 
+
+package classpart;
+
+public class FunctionTest {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		
+		int sum = add(num1, num2);
+		System.out.println(num1 + " + " + num2 + " = " + sum + "입니다");
+	}
+	
+	public static int add(int n1, int n2) {
+		int result = n1 + n2;
+		return result;
+	}
+
+}
--- java/doit-java-intro/ch05/src/classpart/Student.java
+++ java/doit-java-intro/ch05/src/classpart/Student.java
@@ -8,7 +8,11 @@ public class Student {
 	int grade;
 	String address;
 	
-	public void showStudentInfo() {
-		System.out.println(studentName + "," + address);
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName(String name) {
+		studentName = name;
 	}
 }
