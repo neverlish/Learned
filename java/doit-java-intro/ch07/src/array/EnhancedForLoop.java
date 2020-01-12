b992c3cf469033274b89a5b350638be195e775e7
Jinho Hyeon <neverlish@gmail.com>
2020-01-12 16:09:54 +0900

    07 배열과 ArrayList - 1 배열이란?

---
+++ java/doit-java-intro/ch07/src/array/EnhancedForLoop.java
@@ -0,0 +1,16 @@
+// 향상된 for 문 사용하기
+
+package array;
+
+public class EnhancedForLoop {
+
+	public static void main(String[] args) {
+		String[] strArray = {"Java", "Android", "C", "Javascript", "Python"};
+		
+		for (String lang: strArray) {
+			System.out.println(lang);
+		}
+
+	}
+
+}
