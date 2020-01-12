edbc9cbdf4d6dd9451c271f0b38fbd474c6a2ce7
Jinho Hyeon <neverlish@gmail.com>
2020-01-12 21:08:20 +0900

    08 상속과 다형성 - 1 상속이란?

---
+++ java/doit-java-intro/ch08/src/inheritance/VIPCustomer.java
@@ -0,0 +1,18 @@
+// VIPCustomer 클래스 구현하기
+
+package inheritance;
+
+public class VIPCustomer extends Customer {
+	private int agentID;
+	double saleRatio;
+	
+	public VIPCustomer() {
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		saleRatio = 0.1;
+	}
+	
+	public int getAgentID() {
+		return agentID;
+	}
+}
