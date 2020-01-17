a60827e164a14f7a9b544b7ab561b31b29b68005
Jinho Hyeon <neverlish@gmail.com>
2020-01-17 22:59:28 +0900

    09 - 2 템플릿 메서드

---
+++ java/doit-java-intro/ch09/src/template/AICar.java
@@ -0,0 +1,16 @@
+// 추상 클래스와 템플릿 메서드
+
+package template;
+
+public class AICar extends Car {
+	@Override
+	public void drive() {
+		System.out.println("자율 주행합니다");
+		System.out.println("자동차가 알아서 방향을 전환합니다.");
+	}
+	
+	@Override
+	public void stop() {
+		System.out.println("스스로 멈춥니다.");
+	}
+}
