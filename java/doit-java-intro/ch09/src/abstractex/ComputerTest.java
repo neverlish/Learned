085d008c0adb3fd7f8ec39c975d0153e2c4f6278
Jinho Hyeon <neverlish@gmail.com>
2020-01-17 22:46:51 +0900

    09 추상 클래스 - 1 추상 클래스

---
+++ java/doit-java-intro/ch09/src/abstractex/ComputerTest.java
@@ -0,0 +1,15 @@
+// 추상 클래스 테스트하기
+
+package abstractex;
+
+public class ComputerTest {
+
+	public static void main(String[] args) {
+//		Computer c1 = new Computer();
+		Computer c2 = new DeskTop();
+//		Computer c3 = new NoteBook();
+		Computer c4 = new MyNoteBook();
+
+	}
+
+}
