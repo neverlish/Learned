381502321f8dda1a0fa55835318722cb353d32ed
Jinho Hyeon <neverlish@gmail.com>
2020-01-18 12:00:05 +0900

    09 - 3 템플릿 메서드 응용하기

---
+++ java/doit-java-intro/ch09/src/gameLevel/Player.java
@@ -0,0 +1,25 @@
+// Player 클래스 구현하기
+
+package gameLevel;
+
+public class Player {
+	private PlayerLevel level;
+	
+	public Player() {
+		level = new BeginnerLevel();
+		level.showLevelMessage();
+	}
+	
+	public PlayerLevel getLevel() {
+		return level;
+	}
+	
+	public void upgradeLevel(PlayerLevel level) {
+		this.level = level;
+		level.showLevelMessage();
+	}
+	
+	public void play(int count) {
+		level.go(count);
+	}
+}
