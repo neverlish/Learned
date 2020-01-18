8358d059ac3dfdaba0799d1fe5817fb7c256602c
Jinho Hyeon <neverlish@gmail.com>
2020-01-18 12:20:58 +0900

    10 인터페이스 - 1 인터페이스란?

---
+++ java/doit-java-intro/ch10/src/interfaceex/Calc.java
@@ -0,0 +1,13 @@
+// Calc 인터페이스 만들기
+
+package interfaceex;
+
+public interface Calc {
+	double PI = 3.14;
+	int ERROR = -999999999;
+	
+	int add(int num1, int num2);
+	int substract(int num1, int num2);
+	int times(int num1, int num2);
+	int divide(int num1, int num2);
+}
---
+++ java/doit-java-intro/ch10/src/interfaceex/CalculatorTest.java
@@ -0,0 +1,17 @@
+// CompleteCalc 클래스 실행하기
+
+package interfaceex;
+
+public class CalculatorTest {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 5;
+		
+		CompleteCalc calc = new CompleteCalc();
+		System.out.println(calc.add(num1, num2));
+		System.out.println(calc.substract(num1, num2));
+		System.out.println(calc.times(num1, num2));
+		System.out.println(calc.divide(num1, num2));
+		calc.showInfo();
+	}
+}
