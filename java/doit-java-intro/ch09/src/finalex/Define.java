7fa310234cf561fa1f68a8d6e334724ab816b3a4
Jinho Hyeon <neverlish@gmail.com>
2020-01-18 12:09:23 +0900

    09 - 4 final 예약어

---
+++ java/doit-java-intro/ch09/src/finalex/Define.java
@@ -0,0 +1,12 @@
+// 여러 파일에서 공유하는 상수
+
+package finalex;
+
+public class Define {
+	public static final int MIN = 1;
+	public static final int MAX = 99999;
+	public static final int ENG = 1001;
+	public static final int MATH = 2001;
+	public static final double PI = 3.14;
+	public static final String GOOD_MORNING = "Good Morning!";
+}
