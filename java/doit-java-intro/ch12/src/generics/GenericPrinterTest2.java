ffd071fd0cd16d3774a2a291f73f132d05082ca2
Jinho Hyeon <neverlish@gmail.com>
2020-01-19 11:00:28 +0900

    12 컬렉션 프레임워크 - 1 제네릭

---
+++ java/doit-java-intro/ch12/src/generics/GenericPrinterTest2.java
@@ -0,0 +1,19 @@
+// <T extends 클래스> 사용하기
+
+package generics;
+
+public class GenericPrinterTest2 {
+
+	public static void main(String[] args) {
+		GenericPrinter<Powder> powderPrinter = new GenericPrinter<Powder>();
+		
+		powderPrinter.setMaterial(new Powder());
+		powderPrinter.printing();
+		
+		GenericPrinter<Plastic> plasticPrinter = new GenericPrinter<Plastic>();
+
+		plasticPrinter.setMaterial(new Plastic());
+		plasticPrinter.printing();
+	}
+
+}
