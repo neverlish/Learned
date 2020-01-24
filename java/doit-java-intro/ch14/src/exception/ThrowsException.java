e928467156e6012540f5066f38fe9688cae0f896
Jinho Hyeon <neverlish@gmail.com>
2020-01-24 14:59:59 +0900

    14 - 3 예외 처리 미루기

---
+++ java/doit-java-intro/ch14/src/exception/ThrowsException.java
@@ -0,0 +1,30 @@
+// throws로 예외 미루
+
+package exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ThrowsException {
+	public Class loadClass(String fileName, String className) 
+			throws FileNotFoundException, ClassNotFoundException {
+		FileInputStream fis = new FileInputStream(fileName);
+		Class c = Class.forName(className);
+		return c;
+	}
+	
+	public static void main(String[] args) {
+		ThrowsException test = new ThrowsException();
+		try {
+			test.loadClass("a.txt", "java.lang.String");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
