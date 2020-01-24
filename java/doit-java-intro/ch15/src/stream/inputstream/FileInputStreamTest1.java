6bb96c0676c49262575858bdfe727aacdcb2819c
Jinho Hyeon <neverlish@gmail.com>
2020-01-24 15:53:01 +0900

    15 - 3 바이트 단위 스트림

---
+++ java/doit-java-intro/ch15/src/stream/inputstream/FileInputStreamTest1.java
@@ -0,0 +1,33 @@
+// FileInputStream 사용하기
+
+package stream.inputstream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileInputStreamTest1 {
+
+	public static void main(String[] args) {
+		FileInputStream fis = null;
+		
+		try {
+			fis = new FileInputStream("input.txt");
+			System.out.println(fis.read());
+			System.out.println(fis.read());
+			System.out.println(fis.read());
+		} catch (IOException e) {
+			System.out.println(e);
+		} finally {
+			try {
+				fis.close();
+			} catch (IOException e) {
+				System.out.println(e);
+			} catch (NullPointerException e) {
+				System.out.println(e);
+			}
+		}
+		System.out.println("end");
+
+	}
+
+}
