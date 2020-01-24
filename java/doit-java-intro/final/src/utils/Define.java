849f654c70791d23395c06c2de276bb26a5b201e
Jinho Hyeon <neverlish@gmail.com>
2020-01-24 20:30:33 +0900

    최종: 학점 산출 프로그램 만들기 - 2 클래스 정의하기 관계도 그리기

---
+++ java/doit-java-intro/final/src/utils/Define.java
@@ -0,0 +1,11 @@
+// 상수 값 정의
+
+package utils;
+
+public class Define {
+	public static final int KOREA = 1001;
+	public static final int MATH = 2001;
+	
+	public static final int AB_TYPE = 0;
+	public static final int SAB_TYPE = 1;
+}
