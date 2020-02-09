082d3fa6b846cc27ed435466f5603ee62e52e9d3
Jinho Hyeon <neverlish@gmail.com>
2020-02-09 22:38:56 +0900

    1 - 06 If with a Twist

---
+++ go/nomadcoders-start-go/src/1_06.go
@@ -0,0 +1,16 @@
+// 1 - 06 If with a Twist
+
+package main
+
+import "fmt"
+
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(canIDrink(16))
+}
