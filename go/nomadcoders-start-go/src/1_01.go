aedef2c71b2235da58226e9f2adb8dc3281f05a6
Jinho Hyeon <neverlish@gmail.com>
2020-02-09 22:20:03 +0900

    1 - 01 Packages and Imports

---
+++ go/nomadcoders-start-go/src/1_01.go
@@ -0,0 +1,9 @@
+// 1 - 01 Packages and Imports
+
+package main
+
+import "something"
+
+func main()  {
+	something.SayHello()
+}
---
+++ go/nomadcoders-start-go/src/something/something.go
@@ -0,0 +1,11 @@
+package something
+
+import "fmt"
+
+func sayBye() {
+	fmt.Println("Bye")
+}
+
+func SayHello() {
+	fmt.Println("Hello")
+}
