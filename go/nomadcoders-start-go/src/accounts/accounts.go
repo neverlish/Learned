b837c7bf1ebf53dd29fb02257753f862b5bc996a
Jinho Hyeon <neverlish@gmail.com>
2020-02-09 23:36:55 +0900

    2 - 02 Methods part Two

--- go/nomadcoders-start-go/src/accounts/accounts.go
+++ go/nomadcoders-start-go/src/accounts/accounts.go
@@ -1,19 +1,31 @@
 package accounts
 
+import "errors"
+
 type Account struct {
 	owner string
 	balance int
 }
+
+var errNoMoney = errors.New("Can't withdraw")
 
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
 
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) {
 	a.balance += amount
 }
 
 func (a Account) Balance() int {
 	return a.balance
+}
+
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
 }
