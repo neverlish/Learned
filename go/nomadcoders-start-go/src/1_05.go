d55ea83f689c4393b50e23c84ee4ec337c0b6da0
Jinho Hyeon <neverlish@gmail.com>
2020-02-09 22:36:51 +0900

    1 - 05 for, range, ...args

---
+++ go/nomadcoders-start-go/src/1_05.go
@@ -0,0 +1,25 @@
+// 1 - 05 for, range, ...args
+
+package main
+
+import "fmt"
+
+func superAdd(numbers ...int)  int {
+	// fmt.Println(numbers)
+	// for index, number := range numbers {
+	// 	fmt.Println(index, number)
+	// }
+	// for i := 0; i < len(numbers); i++ {
+	// 	fmt.Println(numbers[i])
+	// }
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+func main() {
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
+}
