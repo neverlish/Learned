130aa92682713f7223108f20ec70d4e6760035c0
Jinho Hyeon <neverlish@gmail.com>
2020-02-13 22:43:49 +0900

    3 URL Checker & Go Routines - 00 hitURL()

---
+++ go/nomadcoders-start-go/url.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Request failed")
+
+func main() {
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+
+	return nil
+}
