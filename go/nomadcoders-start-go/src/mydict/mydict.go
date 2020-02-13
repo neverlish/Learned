e2f1ac921a59cb99ad918e1a231c3ccaf6dad4ed
Jinho Hyeon <neverlish@gmail.com>
2020-02-13 22:24:57 +0900

    2 - 05 Add Method

--- go/nomadcoders-start-go/src/mydict/mydict.go
+++ go/nomadcoders-start-go/src/mydict/mydict.go
@@ -5,6 +5,7 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word already exists")
 
 func (d Dictionary) Search(word string) (string, error) {
 	value, exist := d[word]
@@ -12,4 +13,15 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+		case errNotFound:
+			d[word] = def
+		case nil:
+			return errWordExists
+	}
+	return nil
 }
