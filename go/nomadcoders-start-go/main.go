c58991e435a5e8f719194ee87727eef57327f814
Jinho Hyeon <neverlish@gmail.com>
2020-02-14 23:41:52 +0900

    5 - 01 Setup Part Two

--- go/nomadcoders-start-go/main.go
+++ go/nomadcoders-start-go/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"strings"
+	"fmt"
+	"github.com/labstack/echo"
 	"scrapper"
 )
 
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	fmt.Println(term)
+	return nil
+}
+
 func main() {
-	scrapper.Scrape("term")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
