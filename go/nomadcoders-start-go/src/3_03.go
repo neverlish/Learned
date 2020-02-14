e0907328c94273272e3f828c571691e15e7f3ff3
Jinho Hyeon <neverlish@gmail.com>
2020-02-14 21:15:04 +0900

    3 - 03 Channels

---
+++ go/nomadcoders-start-go/src/3_03.go
@@ -0,0 +1,24 @@
+// 3 - 03 Channels
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(chan bool)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	fmt.Println(<- c)
+	fmt.Println(<- c)
+}
+
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true
+}
