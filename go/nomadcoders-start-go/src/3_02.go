f5c54518ccfef42fdc105f33be97accc72eaf879
Jinho Hyeon <neverlish@gmail.com>
2020-02-14 10:25:52 +0900

    3 - 02 Goroutines

---
+++ go/nomadcoders-start-go/src/3_02.go
@@ -0,0 +1,21 @@
+// 3 - 02 Goroutines
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go sexyCount("nico")
+	go sexyCount("flynn")
+	time.Sleep(time.Second * 5)
+}
+
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
+	}
+}
