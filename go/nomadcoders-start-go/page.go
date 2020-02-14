58a64791a2ab2cc1738232f036a30a0bf9dd8807
Jinho Hyeon <neverlish@gmail.com>
2020-02-14 23:08:00 +0900

    4 - 05 Channels Time

--- go/nomadcoders-start-go/page.go
+++ go/nomadcoders-start-go/page.go
@@ -35,28 +35,9 @@ func main() {
 	fmt.Println("Done, extracted", len(jobs))
 }
 
-func writeJobs(jobs []extractedJob) {
-	file, err := os.Create("jobs.csv")
-	checkErr(err)
-
-	w := csv.NewWriter(file)
-
-	defer w.Flush()
-
-	headers := []string{"ID", "Title", "Location", "Salary", "Summary"}
-
-	wErr := w.Write(headers)
-	checkErr(wErr)
-
-	for _, job := range jobs {
-		jobSlice := []string{job.id, job.title, job.location, job.salary, job.summary}
-		jwErr := w.Write(jobSlice)
-		checkErr(jwErr)
-	}
-}
-
 func getPage(page int) []extractedJob {
 	var jobs []extractedJob
+	c := make(chan extractedJob)
 	pageURL := baseURL + "&start=" + strconv.Itoa(page * 50)
 	fmt.Println("Requesting", pageURL)
 	res, err := http.Get(pageURL)
@@ -71,26 +52,30 @@ func getPage(page int) []extractedJob {
 	searchCards := doc.Find(".jobsearch-SerpJobCard ")
 
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		job := extractJob(card)
-		jobs = append(jobs, job)
+		go extractJob(card, c)
 	})
+
+	for i := 0; i < searchCards.Length(); i++ {
+		job := <- c
+		jobs = append(jobs, job)
+	}
 
 	return jobs
 }
 
-func extractJob(card *goquery.Selection) extractedJob {
+func extractJob(card *goquery.Selection, c chan <- extractedJob) {
 	id, _ := card.Attr("data-jk")
-		title := cleanString(card.Find(".title>a").Text())
-		location := cleanString(card.Find(".sjcl").Text())
-		salary := cleanString(card.Find(".salaryText").Text())
-		summary := cleanString(card.Find(".summary").Text())
-		return extractedJob{
-			id: id,
-			title: title,
-			location: location,
-			salary: salary,
-			summary: summary,
-		}
+	title := cleanString(card.Find(".title>a").Text())
+	location := cleanString(card.Find(".sjcl").Text())
+	salary := cleanString(card.Find(".salaryText").Text())
+	summary := cleanString(card.Find(".summary").Text())
+	c <- extractedJob{
+		id: id,
+		title: title,
+		location: location,
+		salary: salary,
+		summary: summary,
+	}
 }
 
 func cleanString(str string) string {
@@ -114,6 +99,26 @@ func getPages() int {
 	})
 
 	return pages
+}
+
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+
+	defer w.Flush()
+
+	headers := []string{"Link", "Title", "Location", "Salary", "Summary"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.salary, job.summary}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
+	}
 }
 
 func checkErr(err error) {
