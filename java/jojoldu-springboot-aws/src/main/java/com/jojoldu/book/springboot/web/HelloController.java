c5c28416d89d40087f96a5c0b7c8211f13140c56
Jinho Hyeon <neverlish@gmail.com>
2020-02-24 22:18:44 +0900

    02 - 4 Hello Controller 코드를 롬복으로 전환하기

--- java/jojoldu-springboot-aws/src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ java/jojoldu-springboot-aws/src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
