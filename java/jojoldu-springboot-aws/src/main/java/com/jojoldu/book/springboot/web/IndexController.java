0b74f6a0116d6a4caf2199b00993c731efeaba1e
Jinho Hyeon <neverlish@gmail.com>
2020-02-28 23:46:44 +0900

    04 - 3 게시글 등록 화면 만들기

--- java/jojoldu-springboot-aws/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ java/jojoldu-springboot-aws/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -9,4 +9,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postSave() {
+        return "posts-save";
+    }
 }
