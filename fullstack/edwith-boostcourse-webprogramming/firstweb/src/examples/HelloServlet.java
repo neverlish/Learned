7598c5631745961091b3cb0beb216d01a0479891
Jinho Hyeon <neverlish@gmail.com>
2020-03-01 16:18:53 +0900

    1 웹 프로그래밍 기초 - 4 개발환경 설정-BE - 6 HelloWorld 서블릿 컴파일 및 실행하기

---
+++ fullstack/edwith-boostcourse-webprogramming/firstweb/src/examples/HelloServlet.java
@@ -0,0 +1,35 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Hello servlet</h1>");
+	}
+
+}
