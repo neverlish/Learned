b4db2147c26e42ede17830d54071584725f887ba
Jinho Hyeon <neverlish@gmail.com>
2020-03-01 16:58:08 +0900

    1 - 5 - 3 Servlet 라이프 싸이클

---
+++ fullstack/edwith-boostcourse-webprogramming/firstweb/src/examples/LifecycleServlet.java
@@ -0,0 +1,58 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LifecycleServlet")
+public class LifecycleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public LifecycleServlet() {
+        System.out.println("LifecycleServlet 생성!");
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init test 호출!!");
+	}
+
+	public void destroy() {
+		System.out.println("destroy 호출!!");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+		out.println("<form method='post' action='/firstweb/LifecycleServlet'>");
+		out.println("name : <input type='text' name='name'><br>");
+		out.println("<input type='submit' value='ok'><br>");                                                 
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.println("<h1> hello " + name + "</h1>");
+		out.close();
+	}
+
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		System.out.println("service 호출!!");
+//	}
+
+	
+}
