6c6cb61ebda578702361de5fe4a04c9d828fecf9
Jinho Hyeon <neverlish@gmail.com>
2020-03-08 16:42:34 +0900

    2 - 11 WEB API-BE - 3 Web API 실습

---
+++ fullstack/edwith-boostcourse-webprogramming/webapiexam/src/main/java/kr/or/connect/webapiexam/api/RoleByIdServlet.java
@@ -0,0 +1,46 @@
+package kr.or.connect.webapiexam.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kr.or.connect.jdbcexam.dao.RoleDao;
+import kr.or.connect.jdbcexam.dto.Role;
+
+@WebServlet("/roles/*")
+public class RoleByIdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public RoleByIdServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("application/json");
+		
+		String pathInfo = request.getPathInfo();
+		String[] pathParts = pathInfo.split("/");
+		String idStr = pathParts[1];
+		int id = Integer.parseInt(idStr);
+		
+		RoleDao dao = new RoleDao();
+		
+		Role role = dao.getRole(id);
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		String json = objectMapper.writeValueAsString(role);
+
+		PrintWriter out = response.getWriter();
+		out.println(json);
+		out.close();
+	}
+
+}
