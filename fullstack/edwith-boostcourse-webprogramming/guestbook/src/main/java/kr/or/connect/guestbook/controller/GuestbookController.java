09ad23537ca00d26262da584841999841e1ed309
Jinho Hyeon <neverlish@gmail.com>
2020-03-29 19:17:34 +0900

    5 웹 앱 개발: 예약서비스 3/4 - 4 상태유지기술(Cookie & Session)-BE - 3 쿠키를 이용한 상태정보 유지하기

--- fullstack/edwith-boostcourse-webprogramming/guestbook/src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
+++ fullstack/edwith-boostcourse-webprogramming/guestbook/src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
@@ -3,11 +3,14 @@ package kr.or.connect.guestbook.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +26,47 @@ public class GuestbookController {
 	
 	@GetMapping(path="/list")
 	public String list(@RequestParam(name="start", required=false, defaultValue="0") int start,
-						ModelMap model) {
+						ModelMap model,
+//						HttpServletRequest request,
+						@CookieValue(value="count", defaultValue="1", required=true) String value,
+						HttpServletResponse response) {
+		
+//		String value = null;
+//		boolean find = false;
+//		Cookie[] cookies = request.getCookies();
+//		
+//		if (cookies != null) {
+//			for (Cookie cookie: cookies) {
+//				if ("count".equals(cookie.getName())) {
+//					find = true;
+//					value = cookie.getValue();
+//				}
+//			}
+//		}
+//		
+//		if (!find) {
+//			value = "1";
+//		} else {
+//			try {
+//				int i = Integer.parseInt(value);
+//				value = Integer.toString(++i);
+//			} catch (Exception e) {
+//				value = "1";
+//			}
+//		}
+		
+		try {
+			int i = Integer.parseInt(value);
+			value = Integer.toString(++i);
+		}catch(Exception ex){
+			value = "1";
+		}
+		
+		Cookie cookie = new Cookie("count", value);
+		cookie.setMaxAge(60 * 60 * 24 * 365);
+		cookie.setPath("/");
+		response.addCookie(cookie);
+		
 		List<Guestbook> list = guestbookService.getGuestbooks(start);
 		
 		int count = guestbookService.getCount();
@@ -41,6 +84,7 @@ public class GuestbookController {
 		model.addAttribute("list", list);
 		model.addAttribute("count", count);
 		model.addAttribute("pageStartList", pageStartList);
+		model.addAttribute("cookieCount", value); 
 		
 		return "list";
 	}
--- fullstack/edwith-boostcourse-webprogramming/guestbook/src/main/webapp/WEB-INF/views/list.jsp
+++ fullstack/edwith-boostcourse-webprogramming/guestbook/src/main/webapp/WEB-INF/views/list.jsp
@@ -9,7 +9,7 @@
 </head>
 <body>
 	<h1>방명록</h1>
-	<br> 방명록 전체 수 : ${count }
+	<br> 방명록 전체 수 : ${count }, 방문한 수: ${cookieCount}
 	<br>
 	<br>
 	
