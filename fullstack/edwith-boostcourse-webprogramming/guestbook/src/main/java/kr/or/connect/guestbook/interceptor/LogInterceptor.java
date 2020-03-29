170e512636d5ebfcd0fb89b4e4fac00ec6395a9f
Jinho Hyeon <neverlish@gmail.com>
2020-03-29 21:44:59 +0900

    6 웹 앱 개발: 예약서비스 4/4 - 2 로깅-BE - 3 slf4j를 이용한 로그남기기

--- fullstack/edwith-boostcourse-webprogramming/guestbook/src/main/java/kr/or/connect/guestbook/interceptor/LogInterceptor.java
+++ fullstack/edwith-boostcourse-webprogramming/guestbook/src/main/java/kr/or/connect/guestbook/interceptor/LogInterceptor.java
@@ -3,18 +3,24 @@ package kr.or.connect.guestbook.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LogInterceptor extends HandlerInterceptorAdapter {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-		System.out.println(handler.toString() + "가 종료되었습니다. " + modelAndView.getViewName() + "을 view로 사용합니다.");
+		logger.debug("{}가 종료되었습니다. {} 를 view로 사용합니다.", handler.toString(), modelAndView.getViewName());
+//		System.out.println(handler.toString() + "가 종료되었습니다. " + modelAndView.getViewName() + "을 view로 사용합니다.");
 	}
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-		System.out.println(handler.toString() + " 를 호출했습니다.");
+		logger.debug("{}를 호출했습니다.", handler.toString());
+//		System.out.println(handler.toString() + " 를 호출했습니다.");
 		return true;
 	}
 }
