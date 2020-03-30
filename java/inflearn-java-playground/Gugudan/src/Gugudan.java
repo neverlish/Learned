8ac91e70ef78677a9d6fe352af1a97071d281193
Jinho Hyeon <neverlish@gmail.com>
2020-03-30 09:39:47 +0900

    2 구구단 - 02 2, 3단 구현 - 계산 및 출력

---
+++ java/inflearn-java-playground/Gugudan/src/Gugudan.java
@@ -0,0 +1,26 @@
+
+public class Gugudan {
+	public static void main(String[] args) {
+		// 2단
+		System.out.println(2 * 1);
+		System.out.println(2 * 2);
+		System.out.println(2 * 3);
+		System.out.println(2 * 4);
+		System.out.println(2 * 5);
+		System.out.println(2 * 6);
+		System.out.println(2 * 7);
+		System.out.println(2 * 8);
+		System.out.println(2 * 9);
+		
+		// 3단
+		System.out.println(3 * 1);
+		System.out.println(3 * 2);
+		System.out.println(3 * 3);
+		System.out.println(3 * 4);
+		System.out.println(3 * 5);
+		System.out.println(3 * 6);
+		System.out.println(3 * 7);
+		System.out.println(3 * 8);
+		System.out.println(3 * 9);
+	}
+}
