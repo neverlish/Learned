7be49bd7b5ab5af933d95b0603ef3ad9733a095b
Jinho Hyeon <neverlish@gmail.com>
2020-03-30 10:32:03 +0900

    2 - 08 클래스 활용해 구구단 다시 구현

--- java/inflearn-java-playground/Gugudan/src/Gugudan.java
+++ java/inflearn-java-playground/Gugudan/src/Gugudan.java
@@ -13,11 +13,4 @@ public class Gugudan {
 			System.out.println(result[i]);
 		}
 	}
-	
-	public static void main(String[] args) {
-		for (int i = 2; i < 10; i++) {
-			int[] result = calculate(i);
-			print(result);
-		}
-	}
 }
