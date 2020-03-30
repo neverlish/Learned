01c734ed9b04a3f963fd219bdfba3ce55208ff24
Jinho Hyeon <neverlish@gmail.com>
2020-03-30 10:05:28 +0900

    2 - 05 8, 9단 구현 - 값 입력 및 조건문

--- java/inflearn-java-playground/Gugudan/src/Gugudan.java
+++ java/inflearn-java-playground/Gugudan/src/Gugudan.java
@@ -2,18 +2,34 @@ import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		// 6단
-		int i = 1;
+		// 8, 9단
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("사용자가 입력한 값 : " + number);
 		
-		while (i < 10) {
-			System.out.println(6 * i);
-			i = i + 1;
+		if (number < 2) {
+			System.out.println("값을 잘못 입력했습니다.");
+		} else if (number > 9) {
+			System.out.println("값을 잘못 입력했습니다.");
+		} else {
+			for (int i = 1; i < 10; i++) {
+				System.out.println(number * i);
+			}
 		}
+		
+		// 6단
+//		int i = 1;
+//		
+//		while (i < 10) {
+//			System.out.println(6 * i);
+//			i = i + 1;
+//		}
 		
 		// 7단
-		for (int j = 1; j < 10; j++) {
-			System.out.println(7 * j);
-		}
+//		for (int j = 1; j < 10; j++) {
+//			System.out.println(7 * j);
+//		}
 		
 		
 		// 2단
