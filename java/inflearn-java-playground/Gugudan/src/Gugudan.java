3e7f37fb796a8b229cca64d751d927b96a2121d4
Jinho Hyeon <neverlish@gmail.com>
2020-03-30 10:22:28 +0900

    2 - 06 배열로 구구단 다시 구현

--- java/inflearn-java-playground/Gugudan/src/Gugudan.java
+++ java/inflearn-java-playground/Gugudan/src/Gugudan.java
@@ -1,92 +1,29 @@
-import java.util.Scanner;
-
 public class Gugudan {
 	public static void main(String[] args) {
-		// 8, 9단
-		System.out.println("구구단 중 출력할 단은? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println("사용자가 입력한 값 : " + number);
-		
-		if (number < 2) {
-			System.out.println("값을 잘못 입력했습니다.");
-		} else if (number > 9) {
-			System.out.println("값을 잘못 입력했습니다.");
-		} else {
-			for (int i = 1; i < 10; i++) {
-				System.out.println(number * i);
-			}
+		int[] result = new int[9];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = 2 * (i + 1);
+		}
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
 		}
 		
-		// 6단
-//		int i = 1;
-//		
-//		while (i < 10) {
-//			System.out.println(6 * i);
-//			i = i + 1;
-//		}
+		int[] times3 = new int[9];
 		
-		// 7단
-//		for (int j = 1; j < 10; j++) {
-//			System.out.println(7 * j);
-//		}
+		for (int i = 0; i < times3.length; i++) {
+			times3[i] = 3 * (i + 1);
+		}
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(times3[i]);
+		}
 		
+		int[] times4 = new int[9];
 		
-		// 2단
-//		System.out.println(2 * 1);
-//		System.out.println(2 * 2);
-//		System.out.println(2 * 3);
-//		System.out.println(2 * 4);
-//		System.out.println(2 * 5);
-//		System.out.println(2 * 6);
-//		System.out.println(2 * 7);
-//		System.out.println(2 * 8);
-//		System.out.println(2 * 9);
-		
-		// 3단
-//		System.out.println(3 * 1);
-//		System.out.println(3 * 2);
-//		System.out.println(3 * 3);
-//		System.out.println(3 * 4);
-//		System.out.println(3 * 5);
-//		System.out.println(3 * 6);
-//		System.out.println(3 * 7);
-//		System.out.println(3 * 8);
-//		System.out.println(3 * 9);
-		
-		// 4단
-//		System.out.println("4단");
-//		int result = 4 * 1;
-//		System.out.println(result);
-//		result = 4 * 2;
-//		System.out.println(result);
-//		result = 4 * 3;
-//		System.out.println(result);
-//		result = 4 * 4;
-//		System.out.println(result);
-//		result = 4 * 5;
-//		System.out.println(result);
-//		result = 4 * 6;
-//		System.out.println(result);
-//		result = 4 * 7;
-//		System.out.println(result);
-//		result = 4 * 8;
-//		System.out.println(result);
-//		result = 4 * 9;
-//		System.out.println(result);
-//		
-//		System.out.println("구구단 중 출력할 단은? : ");
-//		Scanner scanner = new Scanner(System.in);
-//		int number = scanner.nextInt();
-//		System.out.println(number * 1);
-//		System.out.println(number * 2);
-//		System.out.println(number * 3);
-//		System.out.println(number * 4);
-//		System.out.println(number * 5);
-//		System.out.println(number * 6);
-//		System.out.println(number * 7);
-//		System.out.println(number * 8);
-//		System.out.println(number * 9);
-		
+		for (int i = 0; i < times3.length; i++) {
+			times4[i] = 4 * (i + 1);
+		}
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(times4[i]);
+		}
 	}
 }
