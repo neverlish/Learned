ee37e47d9eb383c4ae2cbe33b594db5b913b629b
Jinho Hyeon <neverlish@gmail.com>
2020-03-30 10:27:24 +0900

    2 - 07 메소드 활용해 구구단 다시 구현

--- java/inflearn-java-playground/Gugudan/src/Gugudan.java
+++ java/inflearn-java-playground/Gugudan/src/Gugudan.java
@@ -1,29 +1,23 @@
 public class Gugudan {
-	public static void main(String[] args) {
+	public static int[] calculate(int times) {
 		int[] result = new int[9];
 		for (int i = 0; i < result.length; i++) {
-			result[i] = 2 * (i + 1);
+			result[i] = times * (i + 1);
 		}
+		
+		return result;
+	}
+	
+	public static void print(int[] result) {
 		for (int i = 0; i < result.length; i++) {
 			System.out.println(result[i]);
 		}
-		
-		int[] times3 = new int[9];
-		
-		for (int i = 0; i < times3.length; i++) {
-			times3[i] = 3 * (i + 1);
-		}
-		for (int i = 0; i < result.length; i++) {
-			System.out.println(times3[i]);
-		}
-		
-		int[] times4 = new int[9];
-		
-		for (int i = 0; i < times3.length; i++) {
-			times4[i] = 4 * (i + 1);
-		}
-		for (int i = 0; i < result.length; i++) {
-			System.out.println(times4[i]);
+	}
+	
+	public static void main(String[] args) {
+		for (int i = 2; i < 10; i++) {
+			int[] result = calculate(i);
+			print(result);
 		}
 	}
 }
