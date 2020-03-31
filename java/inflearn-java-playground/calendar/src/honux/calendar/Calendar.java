bccb1b86301ab3f9311f2cdbeaf2ba2cc99cad31
Jinho Hyeon <neverlish@gmail.com>
2020-03-31 19:52:13 +0900

    3 - 03 println 사용해서 출력하기

--- java/inflearn-java-playground/calendar/src/honux/calendar/Calendar.java
+++ java/inflearn-java-playground/calendar/src/honux/calendar/Calendar.java
@@ -3,8 +3,12 @@ package honux.calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Calendar");
-
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("--------------------");
+		System.out.println("1  2  3  4  5  6  7");
+		System.out.println("8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
 	}
 
 }
