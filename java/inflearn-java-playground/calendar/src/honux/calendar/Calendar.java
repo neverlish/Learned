598ca7b1878c94de6576967fe344b4e87457af61
Jinho Hyeon <neverlish@gmail.com>
2020-04-04 20:24:46 +0900

    3 - 11 가상의 달력 찍기2

--- java/inflearn-java-playground/calendar/src/honux/calendar/Calendar.java
+++ java/inflearn-java-playground/calendar/src/honux/calendar/Calendar.java
@@ -1,12 +1,22 @@
 package honux.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+			return true;
+		else
+			return false;
+	}
+	
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}
 	}
 	
 	public void printCalendar(int year, int month) {
@@ -14,7 +24,7 @@ public class Calendar {
 		System.out.println("  SU MO TU WE TH FR SA");
 		System.out.println("--------------------");
 		
-		int maxDay = getMaxDaysOfMonth(month);
+		int maxDay = getMaxDaysOfMonth(year, month);
 		
 		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
--- java/inflearn-java-playground/calendar/src/honux/calendar/Prompt.java
+++ java/inflearn-java-playground/calendar/src/honux/calendar/Prompt.java
@@ -10,10 +10,14 @@ public class Prompt {
 		Calendar cal = new Calendar();
 		
 		int month = 1;
+		int year = 2017;
 		
 		while (true) {
+			System.out.println("년도를 입력하세요");
+			System.out.println("YEAR> ");
+			year = scanner.nextInt();
 			System.out.println("달을 입력하세요");
-			System.out.println(PROMPT);
+			System.out.println("MONTH> ");
 			month = scanner.nextInt();
 			if (month == -1) {
 				break;
@@ -21,7 +25,7 @@ public class Prompt {
 			if (month > 12) {
 				continue;
 			}
-			cal.printCalendar(2017, month);
+			cal.printCalendar(year, month);
 		}
 		
 		System.out.println("Bye~");
