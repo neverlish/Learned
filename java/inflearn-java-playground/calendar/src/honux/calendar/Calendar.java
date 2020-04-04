573bc64a5054326934f50d3e23304789c0ac0de1
Jinho Hyeon <neverlish@gmail.com>
2020-04-04 23:12:44 +0900

    3 - 12 더 정교한 달력 찍기

--- java/inflearn-java-playground/calendar/src/honux/calendar/Calendar.java
+++ java/inflearn-java-playground/calendar/src/honux/calendar/Calendar.java
@@ -19,16 +19,28 @@ public class Calendar {
 		}
 	}
 	
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("    <<%4d년%3d월>>\n", year, month);
 		System.out.println("  SU MO TU WE TH FR SA");
 		System.out.println("--------------------");
 		
-		int maxDay = getMaxDaysOfMonth(year, month);
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
 		
-		for (int i = 1; i <= maxDay; i++) {
+		int maxDay = getMaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = (count < 7) ? count : 0;
+		
+		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
+		}
+		System.out.println();
+		
+		count++;
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim) {
 				System.out.println();
 			}
 		}
--- java/inflearn-java-playground/calendar/src/honux/calendar/Prompt.java
+++ java/inflearn-java-playground/calendar/src/honux/calendar/Prompt.java
@@ -3,14 +3,25 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	private final static String PROMPT = "cal> ";
+	public int parseDay(String week) {
+		if (week == "su") return 0;
+		else if (week.equals("mo")) return 1;
+		else if (week.equals("tu")) return 2;
+		else if (week.equals("wd")) return 3;
+		else if (week.equals("th")) return 4;
+		else if (week.equals("fr")) return 5;
+		else if (week.equals("sa")) return 6;
+		else
+			return 0;
+	}
 	
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		
-		int month = 1;
+		int month = 0;
 		int year = 2017;
+		int weekday = 0;
 		
 		while (true) {
 			System.out.println("년도를 입력하세요");
@@ -19,13 +30,17 @@ public class Prompt {
 			System.out.println("달을 입력하세요");
 			System.out.println("MONTH> ");
 			month = scanner.nextInt();
+			System.out.println("첫째 날의 요일을 입력하세요(su, mo, tu, wd, th, fr, sa).");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+			
 			if (month == -1) {
 				break;
 			}
 			if (month > 12) {
 				continue;
 			}
-			cal.printCalendar(year, month);
+			cal.printCalendar(year, month, weekday);
 		}
 		
 		System.out.println("Bye~");
