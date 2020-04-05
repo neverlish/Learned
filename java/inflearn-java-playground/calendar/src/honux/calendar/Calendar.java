3f46e8914cfcf096684785b5c9e5882fb95c83bd
Jinho Hyeon <neverlish@gmail.com>
2020-04-05 21:19:46 +0900

    3 - 18 저장 기능 추가하기 풀이

--- java/inflearn-java-playground/calendar/src/honux/calendar/Calendar.java
+++ java/inflearn-java-playground/calendar/src/honux/calendar/Calendar.java
@@ -1,23 +1,54 @@
 package honux.calendar;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Calendar {
 	private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	private static final int[] LEAP_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static final String SAVE_FILE = "calendar.dat";
 	
 	private HashMap<Date, PlanItem> planMap;
 	
 	public Calendar() {
 		planMap = new HashMap<Date, PlanItem>();
+		File f = new File(SAVE_FILE);
+		if (!f.exists())
+			return;
+		try {
+			Scanner s = new Scanner(f);
+			while (s.hasNext()) {
+				String line = s.nextLine();
+				String[] words = line.split(",");
+				String date = words[0];
+				String detail = words[1].replaceAll("\"", "");
+				PlanItem p = new PlanItem(date, detail);
+				planMap.put(p.getDate(), p);
+			}
+			s.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void registerPlan(String strDate, String plan) {
 		PlanItem p = new PlanItem(strDate, plan);
 		planMap.put(p.getDate(), p);
+		
+		File f = new File(SAVE_FILE);
+		String item = p.saveString();
+		try {
+			FileWriter fw = new FileWriter(f, true);
+			fw.write(item);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public PlanItem searchPlan(String strDate) {
--- java/inflearn-java-playground/calendar/src/honux/calendar/PlanItem.java
+++ java/inflearn-java-playground/calendar/src/honux/calendar/PlanItem.java
@@ -33,4 +33,10 @@ public class PlanItem {
 	public void addPeople(String name) {
 		peoples += name + ",";
 	}
+
+	public String saveString() {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		String sdate = formatter.format(planDate);
+		return sdate + "," + "\"" + detail + "\"" + "\n";
+	}
 }
