7ec437a0d1717329c14d41fbce563402f77e1123
Jinho Hyeon <neverlish@gmail.com>
2020-04-18 13:47:53 +0900

    05 Variables and Mutability in Rust

---
+++ rust/egghead-firstprogram-with-rust/05.rs
@@ -0,0 +1,12 @@
+// 05 Variables and Mutability in Rust
+
+fn main() {
+  // let name = "Pascal";
+  let mut name = "Pascal";
+
+  println!("{}", name);
+
+  name = "Alice";
+
+  println!("{}", name);
+}
