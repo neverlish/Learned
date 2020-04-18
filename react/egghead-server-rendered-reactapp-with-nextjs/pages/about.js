83f3c8eb33be530d67a6387b8554e308d8076188
Jinho Hyeon <neverlish@gmail.com>
2020-04-18 11:54:20 +0900

    03 Implement a custom Material-UI theme on the server using a Higher-order component

--- react/egghead-server-rendered-reactapp-with-nextjs/pages/about.js
+++
@@ -1,6 +0,0 @@
-const About = ({ title = 'Hello from about route!' }) =>
-  <div>
-    <h2>{title}</h2>
-  </div>;
-
-export default About;
--- react/egghead-server-rendered-reactapp-with-nextjs/pages/index.js
+++ react/egghead-server-rendered-reactapp-with-nextjs/pages/index.js
@@ -1,6 +1,8 @@
+import withMui from '../shared/MUI/withMUI';
+
 const Index = ({ title = 'Hello from next.js' }) =>
   <div>
     <h2>{title}</h2>
   </div>;
 
-export default Index;
+export default withMui(Index);
