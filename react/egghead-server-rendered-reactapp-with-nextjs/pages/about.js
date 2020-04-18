735b398f4ef04037b5f1878cb43e2da44ba2d68d
Jinho Hyeon <neverlish@gmail.com>
2020-04-18 11:46:35 +0900

    02 Render Text within a Server-side ReactJS App with Next.js

---
+++ react/egghead-server-rendered-reactapp-with-nextjs/pages/about.js
@@ -0,0 +1,6 @@
+const About = ({ title = 'Hello from about route!' }) =>
+  <div>
+    <h2>{title}</h2>
+  </div>;
+
+export default About;
---
+++ react/egghead-server-rendered-reactapp-with-nextjs/pages/index.js
@@ -0,0 +1,6 @@
+const Index = ({ title = 'Hello from next.js' }) =>
+  <div>
+    <h2>{title}</h2>
+  </div>;
+
+export default Index;
