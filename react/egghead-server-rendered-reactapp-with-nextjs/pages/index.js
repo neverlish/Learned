b3ec4039344f0c154482b13e199dec955f32a1fc
Jinho Hyeon <neverlish@gmail.com>
2020-04-18 11:56:49 +0900

    04 Render Material-UI Components with Next.js

--- react/egghead-server-rendered-reactapp-with-nextjs/pages/index.js
+++ react/egghead-server-rendered-reactapp-with-nextjs/pages/index.js
@@ -1,7 +1,9 @@
+import Header from '../components/header';
 import withMui from '../shared/MUI/withMUI';
 
 const Index = ({ title = 'Hello from next.js' }) =>
   <div>
+    <Header />
     <h2>{title}</h2>
   </div>;
 
