2da312494bb2bfbdcdc96b74267225ab8560fe6f
Jinho Hyeon <neverlish@gmail.com>
2020-05-19 21:48:19 +0900

    1 - 2 - 1 기본 자료형과 변수 선언방법 (3) 크기 범위와 기타 자료형

---
+++ kotlin/edwith-kotlin-basic1/kotlin-part1/src/chap02/section2/minmax.kt
@@ -0,0 +1,10 @@
+package chap02.section2
+
+fun main() {
+    println("Int: ${Int.MIN_VALUE}~${Int.MAX_VALUE}")
+    println("Byte: ${Byte.MIN_VALUE}~${Byte.MAX_VALUE}")
+    println("Short: ${Short.MIN_VALUE}~${Short.MAX_VALUE}")
+    println("Long: ${Long.MIN_VALUE}~${Long.MAX_VALUE}")
+    println("Float: ${Float.MIN_VALUE}~${Float.MAX_VALUE}")
+    println("Double: ${Double.MIN_VALUE}~${Double.MAX_VALUE}")
+}
