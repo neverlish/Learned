a6e1741fee12b8fc72e78bcdb7e28d65b88e7524
Jinho Hyeon <neverlish@gmail.com>
2020-05-19 23:15:06 +0900

    1 - 2 - 4 연산자를 조합해 다양한 식 만들기 (1) 기본연산자

---
+++ kotlin/edwith-kotlin-basic1/kotlin-part1/src/chap02/section4/IncDecOp.kt
@@ -0,0 +1,11 @@
+package chap02.section4
+
+fun main() {
+    var a: Int = 10
+    var b: Int = 10
+    var result1 = ++a
+    var result2 = b++
+
+    println("result1: $result1, result2: $result2")
+    println("a: $a, b: $b")
+}
