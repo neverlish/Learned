05c2c425673e34ecec261be7124f77eb9bfb178d
Jinho Hyeon <neverlish@gmail.com>
2020-05-31 16:19:42 +0900

    1 - 08 Adding the Icons

--- react-native/nomadcoders-react-native-novice-2020/navigation/Tabs.js
+++ react-native/nomadcoders-react-native-novice-2020/navigation/Tabs.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Movies from "../screens/Movies";
-import Tv from "../screens/Tv";
-import Search from "../screens/Search";
+import React, { useLayoutEffect } from "react";
+import { Platform } from "react-native";
 import Favs from "../screens/Favs";
+import Movies from "../screens/Movies";
+import Search from "../screens/Search";
+import Tv from "../screens/Tv";
 
 const Tabs = createBottomTabNavigator();
 
@@ -20,7 +22,35 @@ export default ({ navigation, route }) => {
   }, [route]);
 
   return (
-    <Tabs.Navigator>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+          if (route.name === "Movies") {
+            iconName += "film";
+          } else if (route.name === "TV") {
+            iconName += "tv";
+          } else if (route.name === "Search") {
+            iconName += "search";
+          } else if (route.name === "Favourites") {
+            iconName += "heart";
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              color={focused ? "white" : "grey"}
+              size={26}
+            />
+          );
+        }
+      })}
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          backgroundColor: 'black',
+          borderTopColor: 'black'
+        }
+      }}>
       <Tabs.Screen name="Movies" component={Movies} />
       <Tabs.Screen name="TV" component={Tv} />
       <Tabs.Screen name="Search" component={Search} />
