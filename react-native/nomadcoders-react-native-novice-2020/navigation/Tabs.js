66c534f4d25a9f8f83c08e019e2e591854965bf1
Jinho Hyeon <neverlish@gmail.com>
2020-05-31 16:55:37 +0900

    2 - 04 TV and Discovery Data

--- react-native/nomadcoders-react-native-novice-2020/navigation/Tabs.js
+++ react-native/nomadcoders-react-native-novice-2020/navigation/Tabs.js
@@ -32,7 +32,7 @@ export default ({ navigation, route }) => {
             iconName += "tv";
           } else if (route.name === "Search") {
             iconName += "search";
-          } else if (route.name === "Favourites") {
+          } else if (route.name === "Discovery") {
             iconName += "heart";
           }
           return (
@@ -54,7 +54,7 @@ export default ({ navigation, route }) => {
       <Tabs.Screen name="Movies" component={Movies} />
       <Tabs.Screen name="TV" component={Tv} />
       <Tabs.Screen name="Search" component={Search} />
-      <Tabs.Screen name="Favourites" component={Favs} />
+      <Tabs.Screen name="Discovery" component={Favs} />
     </Tabs.Navigator>
   );
 };
