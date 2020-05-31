963f50695ce154035d18fe65e1363204b7cbe9f3
Jinho Hyeon <neverlish@gmail.com>
2020-05-31 16:51:12 +0900

    2 - 03 Bulletproof getAnything

--- react-native/nomadcoders-react-native-novice-2020/screens/Movies.js
+++ react-native/nomadcoders-react-native-novice-2020/screens/Movies.js
@@ -1,19 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { movieApi } from "../api";
 
 export default ({ navigation }) => {
+  const [movies, setMovies] = useState({
+    nowPlaying: [],
+    popular: [],
+    upcoming: [],
+    nowPlayingError: null,
+    popularError: null,
+    upcomingError: null
+  });
+
   const getData = async () => {
-    const [nowPlaying, error] = await movieApi.nowPlaying();
+    const [nowPlaying, nowPlayingError] = await movieApi.nowPlaying();
     const [popular, popularError] = await movieApi.popular();
+    const [upcoming, upcomingError] = await movieApi.upcoming();
+    setMovies({
+      nowPlaying,
+      popular,
+      upcoming,
+      nowPlayingError,
+      popularError,
+      upcomingError
+    });
   };
 
   useEffect(() => {
     getData();
   }, []);
+
   return (
     <View style={{ flex: 1, backgroundColor: "black" }}>
-      <Text>Movies</Text>
+      <Text style={{ color: "white" }}>{movies.nowPlaying?.length}</Text>
     </View>
   );
 };
