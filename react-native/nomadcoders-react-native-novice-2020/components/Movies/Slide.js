bfa615b57a9f9915f61fa72ee725f5e3ac793df5
Jinho Hyeon <neverlish@gmail.com>
2020-06-01 06:57:46 +0900

    3 - 04 Finishing the Slider

--- react-native/nomadcoders-react-native-novice-2020/components/Movies/Slide.js
+++ react-native/nomadcoders-react-native-novice-2020/components/Movies/Slide.js
@@ -2,6 +2,8 @@ import PropTypes from "prop-types";
 import React from "react";
 import styled from "styled-components/native";
 import { apiImage } from "../../api";
+import Poster from "../Poster";
+import { TouchableOpacity } from "react-native";
 
 const Container = styled.View`
   height: 100%;
@@ -16,37 +18,61 @@ const BG = styled.Image`
 `;
 
 const Content = styled.View`
+  height: 100%;
   flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
 `;
 
 const Data = styled.View`
   width: 50%;
+  align-items: flex-start;
 `;
 
 const Title = styled.Text`
   color: white;
   font-weight: bold;
-  font-size: 18px;
+  font-size: 19px;
+  margin-bottom: 10px;
 `;
 
 const Votes = styled.Text`
-  color: white;
-  opacity: 0.7;
+  color: rgb(220, 220, 220);
+  margin-bottom: 7px;
+  font-size: 12px;
 `;
 
 const Overview = styled.Text`
-  color: white;
-  opacity: 0.7;
+  color: rgb(220, 220, 220);
+  font-size: 14px;
+  font-weight: 500;
 `;
 
-const Slide = ({ id, title, backgroundImage, votes, overview }) => (
+const Button = styled.View`
+  margin-top: 10px;
+  background-color: #e74c3c;
+  padding: 10px;
+  border-radius: 3px;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+`;
+
+const Slide = ({ id, title, backgroundImage, votes, overview, poster }) => (
   <Container>
     <BG source={{ uri: apiImage(backgroundImage) }} />
     <Content>
+      <Poster url={apiImage(poster)} />
       <Data>
-        <Title>{title}</Title>
-        <Votes>{votes} / 10</Votes>
-        <Overview>{overview}</Overview>
+        <Title>{title.slice(0, 30)}</Title>
+        <Votes>⭐️ {votes} / 10</Votes>
+        <Overview>{overview.slice(0, 120)}</Overview>
+        <TouchableOpacity>
+          <Button>
+            <ButtonText>View details</ButtonText>
+          </Button>
+        </TouchableOpacity>
       </Data>
     </Content>
   </Container>
