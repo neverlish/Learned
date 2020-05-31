7ae828be8404467d1d5589ca120058867cfebd54
Jinho Hyeon <neverlish@gmail.com>
2020-05-31 16:36:52 +0900

    2 - 02 Divide and Conquer

--- react-native/nomadcoders-react-native-novice-2020/screens/Movies.js
+++ react-native/nomadcoders-react-native-novice-2020/screens/Movies.js
@@ -1,9 +1,19 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useEffect } from "react";
+import { Text, View } from "react-native";
+import { movieApi } from "../api";
 
-export default ({ navigation }) => (
-  <View style={{ flex: 1, backgroundColor: "black" }}>
-    <Text>Movies</Text>
-    <Button title="Movie" onPress={() => navigation.navigate("Detail")} />
-  </View>
-);
+export default ({ navigation }) => {
+  const getData = async () => {
+    const [nowPlaying, error] = await movieApi.nowPlaying();
+    const [popular, popularError] = await movieApi.popular();
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <View style={{ flex: 1, backgroundColor: "black" }}>
+      <Text>Movies</Text>
+    </View>
+  );
+};
