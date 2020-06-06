78f86c57d464cf4cf665ee6f7c81c84798f2ad2d
Jinho Hyeon <neverlish@gmail.com>
2020-06-06 12:40:12 +0900

    08 Deno with OAK framework (3)

--- deno/inflearn-follow-learn-deno/oak-app/server.ts
+++ deno/inflearn-follow-learn-deno/oak-app/server.ts
@@ -1,4 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 const app = new Application();
 
@@ -31,9 +32,27 @@ let books: Book[] = [
   },
 ];
 
-router.get("/", (context) => {
-  context.response.body = "Hello world, Hi!";
-});
+router
+  .get("/", (context) => {
+    context.response.body = "Hello world, Hi!";
+  })
+  .get("/books", (context) => {
+    context.response.body = books;
+  })
+  .post("/book", async (context) => {
+    const body = await context.request.body();
+
+    if (!context.request.hasBody) {
+      context.response.status = 400;
+      context.response.body = "데이터가 없습니다";
+    } else {
+      const book: Book = body.value;
+      book.id = v4.generate();
+      books.push(book);
+      context.response.status = 201;
+      context.response.body = book;
+    }
+  });
 
 console.log(`Server is listening on port 5000`);
 await app.listen({ port: 5000 });
