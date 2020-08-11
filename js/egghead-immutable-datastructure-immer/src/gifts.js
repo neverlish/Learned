5b2f1570a4f3e919672be8033543fde5d0e09c21
Jinho Hyeon <neverlish@gmail.com>
2020-08-11 22:58:36 +0900

    03 Simplify Deep State Updates using Immer produce

--- js/egghead-immutable-datastructure-immer/src/gifts.js
+++ js/egghead-immutable-datastructure-immer/src/gifts.js
@@ -1,32 +1,24 @@
+import produce from 'immer'
+
 export function addGift(state, id, description, image) {
-  return {
-    ...state,
-    gifts: [
-      ...state.gifts,
-      {
-        id,
-        description,
-        image,
-        reservedBy: undefined
-      }
-    ]
-  }
+  return produce(state, draft => {
+    draft.gifts.push({
+      id,
+      description,
+      image,
+      reservedBy: undefined
+    })
+  })
 }
 
 export function toggleReservation(state, giftId) {
-  return {
-    ...state,
-    gifts: state.gifts.map(gift => {
-      if (gift.id !== giftId) return gift
-      return {
-        ...gift,
-        reservedBy:
-          gift.reservedBy === undefined
-            ? state.currentUser.id
-            : gift.reservedBy === state.currentUser.id
-              ? undefined
-              : gift.reservedBy
-      }
-    })
-  }
+  return produce(state, draft => {
+    const gift = draft.gifts.find(gift => gift.id === giftId)
+    gift.reservedBy =
+      gift.reservedBy === undefined
+        ? state.currentUser.id
+        : gift.reservedBy === state.currentUser.id
+          ? undefined
+          : gift.reservedBy
+  })
 }
