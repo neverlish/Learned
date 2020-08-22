5d16ea2c6fa60a277723fa6406661d61e298c724
Jinho Hyeon <neverlish@gmail.com>
2020-08-22 21:51:16 +0900

    23 Connect React app to a serverless backend deployed with CDK and fix CORS issues

--- aws/egghead-build-app-aws-cdk/lambda/todoHandler.ts
+++ aws/egghead-build-app-aws-cdk/lambda/todoHandler.ts
@@ -11,6 +11,10 @@ const createResponse = (
 ) => {
   return {
     statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,GET,POST,DELETE"
+    },
     body: JSON.stringify(body, null, 2)
   };
 };
@@ -56,6 +60,10 @@ const deleteTodoItem = async (data: { id: string }) => {
 exports.handler = async function (event: AWSLambda.APIGatewayEvent) {
   try {
     const { httpMethod, body: requestBody } = event;
+
+    if (httpMethod === "OPTIONS") {
+      return createResponse('Ok');
+    }
 
     if (httpMethod === "GET") {
       const response = await getAllTodos();
