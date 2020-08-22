7aa3340a61216266ff9562abc25b4afdd160e906
Jinho Hyeon <neverlish@gmail.com>
2020-08-22 20:43:48 +0900

    18 Get all items from a DynamoDB table deployed with CDK using DocumentClient API

--- aws/egghead-build-app-aws-cdk/lib/todo-app-stack.ts
+++ aws/egghead-build-app-aws-cdk/lib/todo-app-stack.ts
@@ -1,9 +1,7 @@
-import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda';
 import * as apiGateWay from '@aws-cdk/aws-apigateway';
 import * as s3 from '@aws-cdk/aws-s3';
-import * as s3Notifications from '@aws-cdk/aws-s3-notifications';
 import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
+import * as cdk from '@aws-cdk/core';
 import { TodoBackend } from './todo-backend';
 
 export class TodoAppStack extends cdk.Stack {
@@ -12,29 +10,13 @@ export class TodoAppStack extends cdk.Stack {
 
     const todoBackend = new TodoBackend(this, 'TodoBackend');
 
-    const helloLambda = new lambda.Function(this, 'HelloLambda', {
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'hello.handler',
-      runtime: lambda.Runtime.NODEJS_12_X,
-      memorySize: 256,
-      timeout: cdk.Duration.seconds(10),
-      environment: {
-        isProduction: 'absolutely not'
-      }
-    });
-
     new apiGateWay.LambdaRestApi(this, 'Endpoint', {
-      handler: helloLambda
+      handler: todoBackend.handler,
     })
 
     const logoBucket = new s3.Bucket(this, 'LogoBucket-neverlish', {
       publicReadAccess: true
     });
-
-    logoBucket.addEventNotification(
-      s3.EventType.OBJECT_CREATED,
-      new s3Notifications.LambdaDestination(helloLambda)
-    );
 
     new s3Deployment.BucketDeployment(this, 'DeployLogo', {
       destinationBucket: logoBucket,
