90cb02e4111c1debbaa66ab3298c92f696f4d658
Jinho Hyeon <neverlish@gmail.com>
2020-09-02 07:50:35 +0900

    03 - 02 First Model Fields

--- fullstack/nomadcoders-airbnb-clone/server/users/models.py
+++ fullstack/nomadcoders-airbnb-clone/server/users/models.py
@@ -3,4 +3,18 @@ from django.db import models
 
 
 class User(AbstractUser):
-    bio = models.TextField(default="")
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
