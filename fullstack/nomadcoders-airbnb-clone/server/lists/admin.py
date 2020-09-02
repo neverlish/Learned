baa784be026430aedd725f4a4950ff205bf6a700
Jinho Hyeon <neverlish@gmail.com>
2020-09-02 16:07:32 +0900

    05 - 02 Lists Model

--- fullstack/nomadcoders-airbnb-clone/server/lists/admin.py
+++ fullstack/nomadcoders-airbnb-clone/server/lists/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.List)
+class ListAdmin(admin.ModelAdmin):
+
+    """ List Admin Definition """
+
+    pass
