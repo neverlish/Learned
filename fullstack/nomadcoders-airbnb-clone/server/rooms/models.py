7a267c75f6a9d19352ad68fa7a90611f9562c8b9
Jinho Hyeon <neverlish@gmail.com>
2020-09-02 11:36:54 +0900

    04 - 04 on_delete, Amenity, Faciliy, HouseRule Models

--- fullstack/nomadcoders-airbnb-clone/server/rooms/models.py
+++ fullstack/nomadcoders-airbnb-clone/server/rooms/models.py
@@ -22,6 +22,27 @@ class RoomType(AbstractItem):
     pass
 
 
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    pass
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    pass
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
+    pass
+
+
 class Room(core_models.TimeStampedModel):
 
     """ Room Model Definition """
@@ -40,7 +61,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
 
     def __str__(self):
         return self.name
