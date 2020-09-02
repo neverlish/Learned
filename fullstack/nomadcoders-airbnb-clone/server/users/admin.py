07adee305ef11669015cf2c314dc8c11081b85ea
Jinho Hyeon <neverlish@gmail.com>
2020-09-02 09:15:27 +0900

    03 - 04 Falling in Love with Admin Panel

--- fullstack/nomadcoders-airbnb-clone/server/users/admin.py
+++ fullstack/nomadcoders-airbnb-clone/server/users/admin.py
@@ -4,4 +4,5 @@ from . import models
 
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = ("language", "currency", "superhost")
