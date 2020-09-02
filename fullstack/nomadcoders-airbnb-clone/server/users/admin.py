b9c2ca50f0eea71e07f631842bddc9fbc2c363c8
Jinho Hyeon <neverlish@gmail.com>
2020-09-02 09:25:51 +0900

    03 - 05 UserAdmin + CustomAdmin

--- fullstack/nomadcoders-airbnb-clone/server/users/admin.py
+++ fullstack/nomadcoders-airbnb-clone/server/users/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ("username", "email", "gender", "language", "currency", "superhost")
-    list_filter = ("language", "currency", "superhost")
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
