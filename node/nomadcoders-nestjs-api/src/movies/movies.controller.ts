bc03da8cee8e7453c8fcf0cd0bbd5d8678015a5f
Jinho Hyeon <neverlish@gmail.com>
2020-09-24 02:13:59 +0900

    2 - 5 DTOs and Validation part Two

--- node/nomadcoders-nestjs-api/src/movies/movies.controller.ts
+++ node/nomadcoders-nestjs-api/src/movies/movies.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie-dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
-import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -28,7 +29,7 @@ export class MoviesController {
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: number, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData);
   }
 }
--- node/nomadcoders-nestjs-api/src/movies/movies.service.ts
+++ node/nomadcoders-nestjs-api/src/movies/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie-dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -31,7 +32,7 @@ export class MoviesService {
     });
   }
 
-  update(id: number, updateData) {
+  update(id: number, updateData: UpdateMovieDto) {
     const movie = this.getOne(id);
     this.deleteOne(id);
     this.movies.push({ ...movie, ...updateData });
