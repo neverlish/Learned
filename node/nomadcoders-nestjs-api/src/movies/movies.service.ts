9dcc2f1bd71fb035bcf4fec691f8e9830000858e
Jinho Hyeon <neverlish@gmail.com>
2020-09-24 02:05:41 +0900

    2 - 4 DTOs and Validation part One

--- node/nomadcoders-nestjs-api/src/movies/movies.service.ts
+++ node/nomadcoders-nestjs-api/src/movies/movies.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -9,7 +10,7 @@ export class MoviesService {
     return this.movies;
   }
 
-  getOne(id: string): Movie {
+  getOne(id: number): Movie {
     const movie = this.movies.find(movie => movie.id === +id);
     if (!movie) {
       throw new NotFoundException(`Movie with ID ${id} not found.`);
@@ -17,20 +18,20 @@ export class MoviesService {
     return movie;
   }
 
-  deleteOne(id: string) {
+  deleteOne(id: number) {
     this.getOne(id);
 
-    this.movies = this.movies.filter(movie => movie.id !== +id);
+    this.movies = this.movies.filter(movie => movie.id !== id);
   }
 
-  create(movieData) {
+  create(movieData: CreateMovieDto) {
     this.movies.push({
       id: this.movies.length + 1,
       ...movieData,
     });
   }
 
-  update(id: string, updateData) {
+  update(id: number, updateData) {
     const movie = this.getOne(id);
     this.deleteOne(id);
     this.movies.push({ ...movie, ...updateData });
