4ed5ea5f53947f277c6a868346171eb4cac27fe5
Jinho Hyeon <neverlish@gmail.com>
2020-12-01 00:41:06 +0900

    moved hateoas to model assembler

--- spring/springio-buildingrest-spring/src/main/java/payroll/EmployeeController.java
+++ spring/springio-buildingrest-spring/src/main/java/payroll/EmployeeController.java
@@ -13,9 +13,11 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 @RestController
 public class EmployeeController {
     private final EmployeeRepository repository;
+    private final EmployeeModelAssembler assembler;
 
-    EmployeeController(EmployeeRepository repository) {
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate root
@@ -23,10 +25,7 @@ public class EmployeeController {
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all() {
         List<EntityModel<Employee>> employees = repository.findAll().stream()
-            .map(employee -> EntityModel.of(employee,
-                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"))
-            )
+            .map(assembler::toModel)
             .collect(Collectors.toList());
 
         return CollectionModel.of(employees,
@@ -45,9 +44,7 @@ public class EmployeeController {
         Employee employee = repository.findById(id)
             .orElseThrow(() -> new EmployeeNotFoundException(id));
 
-        return EntityModel.of(employee, //
-            linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-            linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return assembler.toModel(employee);
     }
 
     @PutMapping("/employees/{id}")
