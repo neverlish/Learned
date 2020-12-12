a20639f4f7bf1f276a3de4a2abd433fc77a9359d
Jinho Hyeon <neverlish@gmail.com>
2020-12-12 22:58:07 +0900

    4 Authentication & Permissions - Adding information to our model

--- django/djangorestframework-tutorial/snippets/models.py
+++ django/djangorestframework-tutorial/snippets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
@@ -16,6 +18,22 @@ class Snippet(models.Model):
         choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES,
                              default='friendly', max_length=100)
+    owner = models.ForeignKey(
+        'auth.User', related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ['created']
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(
+            style=self.style, linenos=linenos, full=True, **options)
+        self.highlighted = highlight(self.code, lext, formatter)
+        super(Snippet, self).save(*args, **kwargs)
