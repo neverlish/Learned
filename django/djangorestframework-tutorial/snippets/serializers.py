36c03e306386c781761f5309cf2f54f8a8633d31
Jinho Hyeon <neverlish@gmail.com>
2020-12-12 23:05:04 +0900

    4 - Updating our serializer

--- django/djangorestframework-tutorial/snippets/serializers.py
+++ django/djangorestframework-tutorial/snippets/serializers.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos',
+                  'language', 'style', 'owner']
 
 
 class UserSerializer(serializers.ModelSerializer):
