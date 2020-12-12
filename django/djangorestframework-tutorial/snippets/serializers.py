04114f0f09562b858e17ee87327a54f86b121ad6
Jinho Hyeon <neverlish@gmail.com>
2020-12-12 23:45:44 +0900

    5 - Hyperlinking our API

--- django/djangorestframework-tutorial/snippets/serializers.py
+++ django/djangorestframework-tutorial/snippets/serializers.py
@@ -3,19 +3,21 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos',
+        fields = ['url', 'id', 'highlight', 'title', 'code', 'linenos',
                   'language', 'style', 'owner']
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
