98c2d4caaab4ada997778be2422aaad2a011f0b4
Jinho Hyeon <neverlish@gmail.com>
2020-12-12 23:08:30 +0900

    4 - Adding login to the Browsable API

--- django/djangorestframework-tutorial/snippets/urls.py
+++ django/djangorestframework-tutorial/snippets/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
