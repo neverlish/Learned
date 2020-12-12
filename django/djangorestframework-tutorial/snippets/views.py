9f0792b0339fd6b872046970cde1130e98423f55
Jinho Hyeon <neverlish@gmail.com>
2020-12-12 23:30:04 +0900

    5 Relationships & Hyperlinked APIs - Creating an endpoint for the root of our API

--- django/djangorestframework-tutorial/snippets/views.py
+++ django/djangorestframework-tutorial/snippets/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from django.contrib.auth.models import User
@@ -29,3 +32,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+    })
