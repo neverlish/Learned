41a22ce7734bbfa0aeec8e76e62dc48eddd13259
Jinho Hyeon <neverlish@gmail.com>
2020-12-12 23:45:53 +0900

    5 - Making sure our URL patterns are named

--- django/djangorestframework-tutorial/snippets/urls.py
+++ django/djangorestframework-tutorial/snippets/urls.py
@@ -4,12 +4,21 @@ from snippets import views
 
 urlpatterns = [
     path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('snippets/<int:pk>/highlight', views.SnippeHighlight.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-    path('api-auth/', include('rest_framework.urls')),
+    path('snippets/',
+         views.SnippetList.as_view(),
+         name='snippet-list'),
+    path('snippets/<int:pk>/',
+         views.SnippetDetail.as_view(),
+         name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/',
+         views.SnippetHighlight.as_view(),
+         name='snippet-highlight'),
+    path('users/',
+         views.UserList.as_view(),
+         name='user-list'),
+    path('users/<int:pk>/',
+         views.UserDetail.as_view(),
+         name='user-detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
