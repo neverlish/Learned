a383bd31ca229de0464dc019ab515e928403e67b
Jinho Hyeon <neverlish@gmail.com>
2021-01-01 14:58:21 +0900

    3 - 4 - 3 데이터 다루기

---
+++ data/ebs-ai-basic/3/4/02.py
@@ -0,0 +1,7 @@
+# step1 특정 폴더에 저장한 소리 파일의 제목 출력하기
+
+folder = ['discomfort', 'hungry', 'laugh', 'tired']
+
+for i in range(1, 11):
+    a = folder[0] + '/' + folder[0] + '_' + str(i) + '.wav'
+    print(a)
