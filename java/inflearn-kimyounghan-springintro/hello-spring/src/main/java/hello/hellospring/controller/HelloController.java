4a8e8e3bda463770e14b342fe0e11d4d017a354d
Jinho Hyeon <neverlish@gmail.com>
2021-01-10 14:25:04 +0900

    2 - 3 View 환경설정

---
+++ java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+}
