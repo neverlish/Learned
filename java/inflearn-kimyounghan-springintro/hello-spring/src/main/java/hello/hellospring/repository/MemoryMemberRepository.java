4608c846c7983969c3e8f1fac9999173db40c5af
Jinho Hyeon <neverlish@gmail.com>
2021-01-10 16:52:52 +0900

    5 스프링 빈과 의존관계 - 1 컴포넌트 스캔과 자동 의존관계 설정

--- java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
--- java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,13 +3,17 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
