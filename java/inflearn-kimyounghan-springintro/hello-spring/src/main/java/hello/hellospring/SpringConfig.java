70ee60df7c5ca644ae8f55668e236e64c3fb289d
Jinho Hyeon <neverlish@gmail.com>
2021-01-10 19:38:18 +0900

    7 스프링 DB 접근 기술 - 2 순수 JDBC

--- java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +25,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
