f29259fc169d5453ff562c2a96d82f8a06c9ac41
Jinho Hyeon <neverlish@gmail.com>
2021-01-10 15:47:58 +0900

    4 - 3 회원 리포지토리 테스트 케이스 작성

--- java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -31,4 +31,8 @@ public class MemoryMemberRepository implements MemberRepository {
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
