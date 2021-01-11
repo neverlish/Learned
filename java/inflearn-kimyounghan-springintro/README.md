3ea51a914ca398531dadeff90f63224be49149ee
Jinho Hyeon <neverlish@gmail.com>
2021-01-11 13:50:37 +0900

    [java] Inflearn : 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 (#210)

---
+++ java/inflearn-kimyounghan-springintro/README.md
@@ -0,0 +1,3 @@
+Lecture: https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%EC%9E%85%EB%AC%B8-%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8/
+
+Pull Request: https://github.com/neverlish/Learned/pull/210
---
+++ java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,23 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+}
