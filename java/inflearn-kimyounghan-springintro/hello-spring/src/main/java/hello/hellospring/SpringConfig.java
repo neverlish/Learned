a11b26c962b8f104a5ab898e68d954849e74e309
Jinho Hyeon <neverlish@gmail.com>
2021-01-11 10:35:32 +0900

    7 - 5 JPA

--- java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,21 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
-    private DataSource dataSource;
+    EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -26,6 +25,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public Long getId() {
