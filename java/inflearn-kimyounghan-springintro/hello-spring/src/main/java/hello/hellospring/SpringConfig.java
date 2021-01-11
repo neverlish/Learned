8e04eaa1f4e2fb6e8fcc50a36f3f112f73e7bf92
Jinho Hyeon <neverlish@gmail.com>
2021-01-11 09:53:09 +0900

    7 - 4 스프링 JdbcTemplate

--- java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ java/inflearn-kimyounghan-springintro/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
