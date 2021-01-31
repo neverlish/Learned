6a61b4ce7f14ba7a14ee0d517bf18c031fe62f70
Jinho Hyeon <neverlish@gmail.com>
2021-01-31 12:22:21 +0900

    1 객체지향의 기본 - 1 클래스와 객체 - 1 클래스와 객체의 정의

---
+++ kotlin/boostcourse-kotlin-basic-2/src/ClassTest.kt
@@ -0,0 +1,15 @@
+class Car {
+    var wheel: Int = 4
+
+    fun start() {
+        print("Engine Start!")
+    }
+}
+
+class Bird
+
+fun main() {
+    val sonata = Car()
+
+    println(sonata.wheel)
+}
