58001eb562a598a6b8daa8859612062f4a43647b
Jinho Hyeon <neverlish@gmail.com>
2021-01-31 14:26:46 +0900

    1 - 1 - 3 상속과 다형성 (2) 같지만 다른 두얼굴의 사나이! 다형성

--- kotlin/boostcourse-kotlin-basic-2/src/chap01/section3/BirdChileClasses.kt
+++ kotlin/boostcourse-kotlin-basic-2/src/chap01/section3/BirdChileClasses.kt
@@ -1,12 +1,16 @@
 package chap01.section3
 
 open class Bird(var name: String, val wing: Int, var beak: String) {
-    fun fly() {
+    open fun fly() {
         println("Fly")
     }
 }
 
 class Lark(name: String, wing: Int, beak: String) : Bird(name, wing, beak) {
+    override fun fly() {
+        println("Quick Fly")
+    }
+
     fun singHitone() {
         println("sing Hitone")
     }
@@ -19,21 +23,25 @@ class Parrot : Bird {
         this.language = language
     }
 
+    override fun fly() {
+        println("Slow Fly")
+    }
+
     fun speak() {
         println("Speak: $language")
     }
 }
 
 fun main() {
-    val lark = Lark("mylark", 2, "short")
-    val parrot = Parrot("myparrot", 2, "long", "English")
+    val lark: Bird = Lark("mylark", 2, "short")
+    val parrot: Bird = Parrot("myparrot", 2, "long", "English")
 
     println("lark - name: ${lark.name}")
-    println("parrot - name: ${parrot.name}, lang: ${parrot.language}")
+//    println("parrot - name: ${parrot.name}, lang: ${parrot.language}")
 
-    lark.singHitone()
+//    lark.singHitone()
     lark.fly()
 
-    parrot.speak()
+//    parrot.speak()
     parrot.fly()
 }
