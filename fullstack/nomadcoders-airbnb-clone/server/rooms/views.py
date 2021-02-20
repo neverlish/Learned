d5ebf06bd2bc2e01d68c3dc58f7eb94d5d42aeb1
Jinho Hyeon <neverlish@gmail.com>
2021-02-20 20:36:01 +0900

    13 SEARCHVIEW - 00 Template, Form, Url, Setup

--- fullstack/nomadcoders-airbnb-clone/server/rooms/views.py
+++ fullstack/nomadcoders-airbnb-clone/server/rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 
@@ -17,3 +18,9 @@ class RoomDetail(DetailView):
     """ RoomDetail Definition """
 
     model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
