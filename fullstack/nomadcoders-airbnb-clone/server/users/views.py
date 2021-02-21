5ffd68b1862e44502565017dfd10d67ac481701f
Jinho Hyeon <neverlish@gmail.com>
2021-02-21 13:18:07 +0900

    14 - 04 Log In and Log Out

--- fullstack/nomadcoders-airbnb-clone/server/users/views.py
+++ fullstack/nomadcoders-airbnb-clone/server/users/views.py
@@ -1,15 +1,26 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
 class LoginView(View):
     def get(self, request):
         form = forms.LoginForm(initial={"email": "itn@las.com"})
-        return render(request, "users/login.html", {'form': form})
+        return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
         return render(request, "users/login.html", {"form": form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
