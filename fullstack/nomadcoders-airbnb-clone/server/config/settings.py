44cd6d7e97f8ebb3274ef79864c37376e334bb44
Jinho Hyeon <neverlish@gmail.com>
2021-02-21 14:04:38 +0900

    16 - 02 Verifying Email part One

--- fullstack/nomadcoders-airbnb-clone/server/config/settings.py
+++ fullstack/nomadcoders-airbnb-clone/server/config/settings.py
@@ -146,3 +146,4 @@ EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
 EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "neverlish@gmail.com"
