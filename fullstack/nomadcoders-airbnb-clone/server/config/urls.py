14b55657ec71784722deeaca96fd94f091671e3d
Jinho Hyeon <neverlish@gmail.com>
2021-02-21 12:23:16 +0900

    14 USER LOG IN & LOG OUT - 0 Setting Up!

--- fullstack/nomadcoders-airbnb-clone/server/config/urls.py
+++ fullstack/nomadcoders-airbnb-clone/server/config/urls.py
@@ -6,8 +6,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
     path("rooms/", include("rooms.urls", namespace="rooms")),
+    path("users/", include("users.urls", namespace="users")),
     path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
---
+++ fullstack/nomadcoders-airbnb-clone/server/users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+app_name = "users"
+
+urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
