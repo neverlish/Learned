b4a6be54b30a0f36cfee75320c7962412f7e6924
Jinho Hyeon <neverlish@gmail.com>
2021-02-21 13:37:50 +0900

    15 - 02 ModelForms are AWESOME

--- fullstack/nomadcoders-airbnb-clone/server/users/forms.py
+++ fullstack/nomadcoders-airbnb-clone/server/users/forms.py
@@ -20,11 +20,11 @@ class LoginForm(forms.Form):
             self.add_error("email", forms.ValidationError("User does not exist"))
 
 
-class SignUpForm(forms.Form):
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
 
-    first_name = forms.CharField(max_length=80)
-    last_name = forms.CharField(max_length=80)
-    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
     password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
@@ -36,20 +36,10 @@ class SignUpForm(forms.Form):
         except models.User.DoesNotExist:
             return email
 
-    def clean_password1(self):
-        password = self.cleaned_data.get("password")
-        password1 = self.cleaned_data.get("password1")
-        if password != password1:
-            raise forms.ValidationError("Password confirmation does not match")
-        else:
-            return password
-
-    def save(self):
-        first_name = self.cleaned_data.get("first_name")
-        last_name = self.cleaned_data.get("last_name")
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
-        user = models.User.objects.create_user(email, email, password)
-        user.first_name = first_name
-        user.last_name = last_name
+        user.username = email
+        user.set_password(password)
         user.save()
