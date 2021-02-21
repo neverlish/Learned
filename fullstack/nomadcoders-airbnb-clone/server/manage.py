752fddc377c9f04447b7fffbb6c68768e4c00790
Jinho Hyeon <neverlish@gmail.com>
2021-02-21 13:53:33 +0900

    16 - 01 Dotenv

--- fullstack/nomadcoders-airbnb-clone/server/manage.py
+++ fullstack/nomadcoders-airbnb-clone/server/manage.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+import dotenv
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
@@ -18,4 +19,5 @@ def main():
 
 
 if __name__ == "__main__":
+    dotenv.read_dotenv()
     main()
--- fullstack/nomadcoders-airbnb-clone/server/users/views.py
+++ fullstack/nomadcoders-airbnb-clone/server/users/views.py
@@ -40,4 +40,5 @@ class SignUpView(FormView):
         user = authenticate(self.request, username=email, password=password)
         if user is not None:
             login(self.request, user)
+        user.verify_email()
         return super().form_valid(form)
