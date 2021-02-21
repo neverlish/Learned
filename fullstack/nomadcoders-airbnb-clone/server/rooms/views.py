ce63b670eb3e4da17379173fc5c84bf1bf260481
Jinho Hyeon <neverlish@gmail.com>
2021-02-21 11:27:30 +0900

    13 - 06 Filtering Like a Boss part Two

--- fullstack/nomadcoders-airbnb-clone/server/rooms/views.py
+++ fullstack/nomadcoders-airbnb-clone/server/rooms/views.py
@@ -31,8 +31,8 @@ def search(request):
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
     s_amenities = request.GET.get("amenities")
     s_facilities = request.GET.get("facilities")
 
@@ -48,7 +48,7 @@ def search(request):
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
     }
 
     room_types = models.RoomType.objects.all()
@@ -70,7 +70,36 @@ def search(request):
     filter_args["country"] = country
 
     if room_type != 0:
-        filter_args["room_type__pk"] = room_type
+        filter_args["room_type__pk"] = 
+        
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            filter_args["amenities__pk"] = int(s_amenity)
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            filter_args["facilities__pk"] = int(s_facility)
 
     rooms = models.Room.objects.filter(**filter_args)
 
