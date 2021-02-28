fbe97fd316f322adb70075ef29dacc76c55ce163
Jinho Hyeon <neverlish@gmail.com>
2021-02-28 16:54:33 +0900

    18 - 03 Closing Thoughts

--- fullstack/nomadcoders-airbnb-clone/server/users/forms.py
+++ fullstack/nomadcoders-airbnb-clone/server/users/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
 from . import models
 
 
@@ -20,26 +22,5 @@ class LoginForm(forms.Form):
             self.add_error("email", forms.ValidationError("User does not exist"))
 
 
-class SignUpForm(forms.ModelForm):
-    class Meta:
-        model = models.User
-        fields = ("first_name", "last_name", "email")
-
-    password = forms.CharField(widget=forms.PasswordInput)
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(email=email)
-            raise forms.ValidationError("User already exists with that email")
-        except models.User.DoesNotExist:
-            return email
-
-    def save(self, *args, **kwargs):
-        user = super().save(commit=False)
-        email = self.cleaned_data.get("email")
-        password = self.cleaned_data.get("password")
-        user.username = email
-        user.set_password(password)
-        user.save()
+class SignUpForm(UserCreationForm):
+    username = forms.EmailField(label="Email")
