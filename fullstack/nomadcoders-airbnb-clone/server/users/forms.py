ad84d2ff132eddce57cc30847d3f428504264687
Jinho Hyeon <neverlish@gmail.com>
2021-02-28 17:05:40 +0900

    19 INTRO TO TAILWINDCSS - 01 Setting Up TailwindCSS with Gulp

--- fullstack/nomadcoders-airbnb-clone/server/users/forms.py
+++ fullstack/nomadcoders-airbnb-clone/server/users/forms.py
@@ -23,4 +23,25 @@ class LoginForm(forms.Form):
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.EmailField(label="Email")
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()
