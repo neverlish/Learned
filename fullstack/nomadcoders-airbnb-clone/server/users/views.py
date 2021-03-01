154dd75b5cbee1d57971e5def48156ae58879738
Jinho Hyeon <neverlish@gmail.com>
2021-03-01 11:44:27 +0900

    21 - 06 Update Profile

--- fullstack/nomadcoders-airbnb-clone/server/users/views.py
+++ fullstack/nomadcoders-airbnb-clone/server/users/views.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from django.views.generic import FormView, DetailView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse
 from django.shortcuts import render, redirect, reverse
@@ -193,3 +193,21 @@ class UserProfileView(DetailView):
 
     model = models.User
     context_object_name = "user_obj"
+
+class UpdateProfileView(UpdateView):
+
+    model = models.User
+    template_name = "users/update-profile.html"
+    fields = (
+        "first_name",
+        "last_name",
+        "avatar",
+        "gender",
+        "bio",
+        "birthdate",
+        "language",
+        "currency",
+    )
+
+    def get_object(self, queryset=None):
+        return self.request.user
