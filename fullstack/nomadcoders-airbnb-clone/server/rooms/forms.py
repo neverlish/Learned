d7bf1e26e0290fd7952673359223c1b08eacbeaf
Jinho Hyeon <neverlish@gmail.com>
2021-03-01 13:06:51 +0900

    23 - 08 Creating Room part One

--- fullstack/nomadcoders-airbnb-clone/server/rooms/forms.py
+++ fullstack/nomadcoders-airbnb-clone/server/rooms/forms.py
@@ -40,3 +40,31 @@ class CreatePhotoForm(forms.ModelForm):
 
         photo.room = room
         photo.save()
+
+
+class CreateRoomForm(forms.ModelForm):
+    class Meta:
+        model = models.Room
+        fields = (
+            "name",
+            "description",
+            "country",
+            "city",
+            "price",
+            "address",
+            "guests",
+            "beds",
+            "bedrooms",
+            "baths",
+            "check_in",
+            "check_out",
+            "instant_book",
+            "room_type",
+            "amenities",
+            "facilities",
+            "house_rules",
+        )
+
+    def save(self, *args, **kwargs):
+        room = super().save(commit=False)
+        return room
