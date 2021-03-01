8830b14bbcdb830e8c91643d6546399d107a1e31
Jinho Hyeon <neverlish@gmail.com>
2021-03-01 12:58:45 +0900

    23 - 03 Delete Photo

--- fullstack/nomadcoders-airbnb-clone/server/config/settings.py
+++ fullstack/nomadcoders-airbnb-clone/server/config/settings.py
@@ -149,3 +149,8 @@ EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
 EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
 EMAIL_FROM = "neverlish@gmail.com"
+
+
+# Auth
+
+LOGIN_URL = "/users/login/"
