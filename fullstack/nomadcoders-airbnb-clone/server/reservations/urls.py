a1a6261d07711f680e7d9169d3d8b200b66656fe
Jinho Hyeon <neverlish@gmail.com>
2021-03-06 23:02:56 +0900

    24 - 09 Create Reservation

---
+++ fullstack/nomadcoders-airbnb-clone/server/reservations/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = "reservations"
+
+urlpatterns = [
+    path(
+        "create/<int:room>/<int:year>-<int:month>-<int:day>",
+        views.create,
+        name="create",
+    ),
+    path("<int:pk>", views.ReservationDetailView.as_view(), name="detail"),
+]
