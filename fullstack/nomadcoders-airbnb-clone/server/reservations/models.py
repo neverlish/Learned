d7399c22145f017de5e9b1c74e6ee972a8c86b0b
Jinho Hyeon <neverlish@gmail.com>
2021-03-06 23:30:41 +0900

    24 - 14 Reviewing Rooms part One

--- fullstack/nomadcoders-airbnb-clone/server/reservations/models.py
+++ fullstack/nomadcoders-airbnb-clone/server/reservations/models.py
@@ -55,7 +55,11 @@ class Reservation(core_models.TimeStampedModel):
 
     def is_finished(self):
         now = timezone.now().date()
-        return now > self.check_out
+        is_finished = now > self.check_out
+        if is_finished:
+            BookedDay.objects.filter(reservation=self).delete()
+        return is_finished
+
 
     is_finished.boolean = True
 
