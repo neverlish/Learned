407e49b8ace86350addef72bcabfd0fd65c96f69
Jinho Hyeon <neverlish@gmail.com>
2021-03-13 19:40:13 +0900

    3 - 2 새로운 할인 정책 적용과 문제점

--- spring/inflearn-spring-coreprinciple-basic/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring/inflearn-spring-coreprinciple-basic/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +8,7 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
