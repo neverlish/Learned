d6a56863a2eff3d850779f2fda51716dfc6070c9
Jinho Hyeon <neverlish@gmail.com>
2021-03-13 14:13:46 +0900

    26 - 10 Conclusions

--- fullstack/nomadcoders-airbnb-clone/server/config/settings.py
+++ fullstack/nomadcoders-airbnb-clone/server/config/settings.py
@@ -180,7 +180,6 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
 if not DEBUG:
 
-    # Sentry
     DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
     STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
@@ -188,7 +187,7 @@ if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = "airbnb-clone-nomadcoders"
     AWS_AUTO_CREATE_BUCKET = True
     AWS_BUCKET_ACL = "public-read"
-
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
     
