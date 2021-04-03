1057c230a2a5062a2e77b8bb39f4068ef6a28662
Jinho Hyeon <neverlish@gmail.com>
2021-04-03 20:44:02 +0900

    3 - 5 새로운 구조와 할인 정책 적용

--- spring/inflearn-spring-coreprinciple-basic/src/main/java/hello/core/AppConfig.java
+++ spring/inflearn-spring-coreprinciple-basic/src/main/java/hello/core/AppConfig.java
@@ -3,6 +3,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -24,6 +25,7 @@ public class AppConfig {
     }
 
     private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
