88daf90cf2bd20fc050b2aa87416693d33529f1b
Jinho Hyeon <neverlish@gmail.com>
2021-04-04 00:34:59 +0900

    5 - 2 싱글톤 패턴

--- spring/inflearn-spring-coreprinciple-basic/src/test/java/hello/core/singleton/SingletonTest.java
+++ spring/inflearn-spring-coreprinciple-basic/src/test/java/hello/core/singleton/SingletonTest.java
@@ -22,4 +22,16 @@ public class SingletonTest {
 
         assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
 }
