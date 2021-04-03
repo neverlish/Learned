1f8b21b723536c40eff534c4bc75bf5b2892c6b5
Jinho Hyeon <neverlish@gmail.com>
2021-04-04 00:22:52 +0900

    5 싱글톤 컨테이너 - 1 웹 애플리케이션과 싱글톤

---
+++ spring/inflearn-spring-coreprinciple-basic/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,25 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
