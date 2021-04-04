7bb1640061ee2ee46ea102a6d8c380e3cf72f4a4
Jinho Hyeon <neverlish@gmail.com>
2021-04-04 23:49:16 +0900

    5 - 6 @Configuration과 바이트코드 조작의 마법

--- spring/inflearn-spring-coreprinciple-basic/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ spring/inflearn-spring-coreprinciple-basic/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -28,4 +28,13 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
 }
