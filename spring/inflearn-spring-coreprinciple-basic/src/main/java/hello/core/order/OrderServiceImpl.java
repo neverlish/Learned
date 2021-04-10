a3bc0a9b789f88611e398f7e0b0ee3d71f2d7b8b
Jinho Hyeon <neverlish@gmail.com>
2021-04-10 15:41:07 +0900

    7 - 3 생성자 주입을 선택해라!

--- spring/inflearn-spring-coreprinciple-basic/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring/inflearn-spring-coreprinciple-basic/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,24 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService {
-//    @Autowired private MemberRepository memberRepository;
-//    @Autowired private DiscountPolicy discountPolicy;
-
     private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
-
-//    @Autowired(required = false)
-    @Autowired
-    public void setMemberRepository(MemberRepository memberRepository) {
-        System.out.println("memberRepository = " + memberRepository);
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired
-    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-        System.out.println("discountPolicy = " + discountPolicy);
-        this.discountPolicy = discountPolicy;
-    }
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
