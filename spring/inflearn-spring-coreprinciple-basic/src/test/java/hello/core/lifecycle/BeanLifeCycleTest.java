8b2469ff8c179c07b32655ecb659b43ade8b188f
Jinho Hyeon <neverlish@gmail.com>
2021-04-10 18:25:19 +0900

    8 - 3 빈 등록 초기화, 소멸 메서드

--- spring/inflearn-spring-coreprinciple-basic/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ spring/inflearn-spring-coreprinciple-basic/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -16,7 +16,7 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig {
-        @Bean
+        @Bean(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
--- spring/inflearn-spring-coreprinciple-basic/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring/inflearn-spring-coreprinciple-basic/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -28,15 +25,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
