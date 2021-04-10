3d37102e9f5c7bc535a1a45510d56d6184a3e156
Jinho Hyeon <neverlish@gmail.com>
2021-04-10 16:38:55 +0900

    7 - 7 애노테이션 직접 만들기

--- spring/inflearn-spring-coreprinciple-basic/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ spring/inflearn-spring-coreprinciple-basic/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -7,8 +8,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Qualifier("mainDiscountPolicy")
-@Primary
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
--- spring/inflearn-spring-coreprinciple-basic/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring/inflearn-spring-coreprinciple-basic/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -13,20 +14,8 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = rateDiscountPolicy;
-//    }
-
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
-
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
