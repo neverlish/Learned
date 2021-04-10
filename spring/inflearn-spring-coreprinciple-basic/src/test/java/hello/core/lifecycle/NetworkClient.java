f7b0694c8a347ce11f8abbefc00d59629e6bdb0a
Jinho Hyeon <neverlish@gmail.com>
2021-04-10 17:06:28 +0900

    8 빈 생명주기 콜백 - 1 빈 생명주기 콜백 시작

---
+++ spring/inflearn-spring-coreprinciple-basic/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,29 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+}
