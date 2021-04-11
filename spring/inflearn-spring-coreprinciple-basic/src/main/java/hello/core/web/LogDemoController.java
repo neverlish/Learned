9476e56caa577ab7815fae5376b7651e966abfcd
Jinho Hyeon <neverlish@gmail.com>
2021-04-11 14:40:08 +0900

    9 - 8 스코프와 프록시

--- spring/inflearn-spring-coreprinciple-basic/src/main/java/hello/core/web/LogDemoController.java
+++ spring/inflearn-spring-coreprinciple-basic/src/main/java/hello/core/web/LogDemoController.java
@@ -13,14 +13,13 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
 
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
