a592c4c13e31f8af1b7b587dca7ac52d7535e6fd
Jinho Hyeon <neverlish@gmail.com>
2021-05-09 13:18:33 +0900

    5 Spring Boot Validation - 1 JSR-380 Bean Validation

--- spring/inflearn-steven-kotlin-springboot-webmvc/mvc/src/main/kotlin/com/example/mvc/controller/put/PutApiController.kt
+++ spring/inflearn-steven-kotlin-springboot-webmvc/mvc/src/main/kotlin/com/example/mvc/controller/put/PutApiController.kt
@@ -3,7 +3,12 @@ package com.example.mvc.controller.put
 import com.example.mvc.model.http.Result
 import com.example.mvc.model.http.UserRequest
 import com.example.mvc.model.http.UserResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.*
+import java.lang.StringBuilder
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api")
@@ -20,34 +25,47 @@ class PutApiController {
     }
 
     @PutMapping(path = ["/put-mapping/object"])
-    fun putMappingObject(@RequestBody userRequest: UserRequest): UserResponse {
+    fun putMappingObject(@Valid @RequestBody userRequest: UserRequest, bindingResult: BindingResult): ResponseEntity<String> {
 
-        return UserResponse().apply {
-            this.result = Result().apply {
-                this.resultCode = "OK"
-                this.resultMessage = "성공"
+        if (bindingResult.hasErrors()) {
+            val msg = StringBuilder()
+            bindingResult.allErrors.forEach {
+                val field = it as FieldError
+                val message = it.defaultMessage
+                msg.append(field.field + " : " + message + "\n")
             }
-        }.apply {
-            this.description = "~~~~~~"
-        }.apply {
-            val userList = mutableListOf<UserRequest>()
-            userList.add(userRequest)
-            userList.add(UserRequest().apply {
-                this.name = "a"
-                this.age = 10
-                this.email = "a@email.com"
-                this.address = "a address"
-                this.phoneNumber = "010-1111-aaaa"
-            })
-            userList.add(UserRequest().apply {
-                this.name = "b"
-                this.age = 120
-                this.email = "b@email.com"
-                this.address = "b address"
-                this.phoneNumber = "010-1111-bbbb"
-            })
 
-            this.userRequest = userList
+            return ResponseEntity.badRequest().body(msg.toString())
         }
+
+        return ResponseEntity.ok("")
+
+//        return UserResponse().apply {
+//            this.result = Result().apply {
+//                this.resultCode = "OK"
+//                this.resultMessage = "성공"
+//            }
+//        }.apply {
+//            this.description = "~~~~~~"
+//        }.apply {
+//            val userList = mutableListOf<UserRequest>()
+//            userList.add(userRequest)
+//            userList.add(UserRequest().apply {
+//                this.name = "a"
+//                this.age = 10
+//                this.email = "a@email.com"
+//                this.address = "a address"
+//                this.phoneNumber = "010-1111-aaaa"
+//            })
+//            userList.add(UserRequest().apply {
+//                this.name = "b"
+//                this.age = 120
+//                this.email = "b@email.com"
+//                this.address = "b address"
+//                this.phoneNumber = "010-1111-bbbb"
+//            })
+//
+//            this.userRequest = userList
+//        }
     }
 }
