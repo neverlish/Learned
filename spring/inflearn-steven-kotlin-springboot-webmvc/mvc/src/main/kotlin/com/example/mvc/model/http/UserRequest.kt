dd15524eb91636c5a5c899b91e80d95abd3ae05f
Jinho Hyeon <neverlish@gmail.com>
2021-05-10 09:34:19 +0900

    5 - 2 Custom Validation Annotation 만들어서 사용하기

--- spring/inflearn-steven-kotlin-springboot-webmvc/mvc/src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ spring/inflearn-steven-kotlin-springboot-webmvc/mvc/src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,10 +1,6 @@
 package com.example.mvc.model.http
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
-import com.fasterxml.jackson.databind.annotation.JsonNaming
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import com.example.mvc.annotation.StringFormatDateTime
 import javax.validation.constraints.*
 
 data class UserRequest(
@@ -24,15 +20,6 @@ data class UserRequest(
     @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$")
     var phoneNumber: String? = null,
 
+    @field:StringFormatDateTime(pattern = "yyyy-MM-dd HH:mm:ss", message = "패턴이 올바르지 않습니다.")
     var createdAt: String? = null,
-) {
-    @AssertTrue(message = "생성일자의 패턴은 yyyy-MM-dd HH:mm:ss 여야 합니다")
-    private fun isValidCreatedAt(): Boolean {
-        return try {
-            LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-            true
-        } catch (e: Exception) {
-            false
-        }
-    }
-}
+)
---
+++ spring/inflearn-steven-kotlin-springboot-webmvc/mvc/src/main/kotlin/com/example/mvc/validator/StringFormayDateTimeValidator.kt
@@ -0,0 +1,25 @@
+package com.example.mvc.validator
+
+import com.example.mvc.annotation.StringFormatDateTime
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import javax.validation.ConstraintValidator
+import javax.validation.ConstraintValidatorContext
+
+class StringFormayDateTimeValidator: ConstraintValidator<StringFormatDateTime, String> {
+
+    private var pattern: String? = null
+
+    override fun initialize(constraintAnnotation: StringFormatDateTime?) {
+        this.pattern = constraintAnnotation?.pattern
+    }
+
+    override fun isValid(value: String?, context: ConstraintValidatorContext?): Boolean {
+        return try {
+            LocalDateTime.parse(value, DateTimeFormatter.ofPattern(pattern))
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
