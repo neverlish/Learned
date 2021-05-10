27945ab415acc297fad8a4007bfa0a6750b05d2e
Jinho Hyeon <neverlish@gmail.com>
2021-05-10 09:55:43 +0900

    6 - 2 ExceptionHandler를 활용하여 자세한 에러 정보 내려주기

--- spring/inflearn-steven-kotlin-springboot-webmvc/mvc/src/main/kotlin/com/example/mvc/advice/GlobalControllerAdvice.kt
+++ spring/inflearn-steven-kotlin-springboot-webmvc/mvc/src/main/kotlin/com/example/mvc/advice/GlobalControllerAdvice.kt
@@ -1,14 +1,12 @@
 package com.example.mvc.advice
 
-import com.example.mvc.controller.exception.ExceptionApiController
-import com.example.mvc.controller.put.PutApiController
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import java.lang.RuntimeException
 
-@RestControllerAdvice(basePackageClasses = [PutApiController::class])
+//@RestControllerAdvice
 class GlobalControllerAdvice {
     @ExceptionHandler(value = [RuntimeException::class])
     fun exception(e: RuntimeException): String {
