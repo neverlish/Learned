72d175566a42e6549c72a69fcd35f1ab39732f70
Jinho Hyeon <neverlish@gmail.com>
2021-05-16 15:28:22 +0900

    2 Security 설정, Data 설정

---
+++ spring/inflearn-silvernine-springboot-jwt/jwttutorial/src/main/java/me/silvernine/tutorial/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package me.silvernine.tutorial.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**",
+                        "favicon.ico"
+                );
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+    }
+}
