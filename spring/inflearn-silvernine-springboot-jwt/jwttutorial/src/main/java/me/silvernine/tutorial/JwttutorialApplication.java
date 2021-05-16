c5db7f749b3c138b7efdbfb5dd94b4adb3f2b4a3
Jinho Hyeon <neverlish@gmail.com>
2021-05-16 15:23:36 +0900

    1 JWT 소개, 프로젝트 생성

---
+++ spring/inflearn-silvernine-springboot-jwt/jwttutorial/src/main/java/me/silvernine/tutorial/JwttutorialApplication.java
@@ -0,0 +1,13 @@
+package me.silvernine.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwttutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwttutorialApplication.class, args);
+    }
+
+}
---
+++ spring/inflearn-silvernine-springboot-jwt/jwttutorial/src/test/java/me/silvernine/tutorial/JwttutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package me.silvernine.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwttutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
