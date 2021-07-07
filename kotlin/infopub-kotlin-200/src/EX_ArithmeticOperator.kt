5aa2719af534150b3ea2a7fc92e7ba03e80dada2
Jinho Hyeon <neverlish@gmail.com>
2021-07-07 09:58:11 +0900

    1 - 005 산술 연산자(Arithmetic Operator) + - * / %

---
+++ kotlin/infopub-kotlin-200/src/EX_ArithmeticOperator.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>): Unit {
+    val num: Int = 15 - 4 * 3
+    val num2: Int = 65 % 7
+    val num3: Double = 7.5 / 5 + 22.25
+    val num4: Double = num / num2 + 0.7
+
+    println(num)
+    println(num2)
+    println(num3)
+    println(num4)
+}
