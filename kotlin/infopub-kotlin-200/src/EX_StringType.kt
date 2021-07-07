0925f944919846fbd8df3d46523a3304526fd55e
Jinho Hyeon <neverlish@gmail.com>
2021-07-07 10:40:30 +0900

    1 - 011 문자열(String)

---
+++ kotlin/infopub-kotlin-200/src/EX_StringType.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>): Unit {
+    var str: String = "Hello"
+    println(str)
+
+    str = str + "\nKotlin!"
+    println(str)
+
+    println(str[8])
+
+    val num = 10 * 5 + 3
+    println(str + num)
+}
