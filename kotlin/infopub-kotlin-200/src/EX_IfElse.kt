fa0ddbfa7296c1d58c0a35afd9fc13e8473dcc05
Jinho Hyeon <neverlish@gmail.com>
2021-07-07 11:41:24 +0900

    1 - 020 흐름 제어-조건문 if-else

---
+++ kotlin/infopub-kotlin-200/src/EX_IfElse.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>): Unit {
+    val a = 10
+    val b = 5
+
+    if (a < b)
+        println("if")
+    else
+        println("else")
+
+    if (a > b)
+        println("a가 크다.")
+    else
+        println("b는 a 이상이다.")
+}
