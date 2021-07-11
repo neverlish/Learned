295fb4bc5620c360b950c0f14a7366cea035e4a4
Jinho Hyeon <neverlish@gmail.com>
2021-07-11 14:46:56 +0900

    3 - 113 클래스와 인터페이스에서 제네릭 사용하기

---
+++ kotlin/infopub-kotlin-200/src/ex_generic_to_class_and_interface/Main.kt
@@ -0,0 +1,7 @@
+package ex_generic_to_class_and_interface
+
+fun main(args: Array<String>) {
+    val pair: Pair<Int, Double>
+    pair = Pair<Int, Double>(15, 9.12)
+    println(pair.toString())
+}
