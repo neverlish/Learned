90b627fae2f0140178fd71ecce6171c712f26968
Jinho Hyeon <neverlish@gmail.com>
2021-07-11 23:10:54 +0900

    4 - 170 abs 함수: 절대값 구하기

---
+++ kotlin/infopub-kotlin-200/src/ex_abs_function/Main.kt
@@ -0,0 +1,10 @@
+package ex_abs_function
+
+import kotlin.math.abs
+
+fun main(args: Array<String>) {
+    print("${abs(-17)} ")
+    print("${abs(62)} ")
+    print("${abs(-6.34)} ")
+    print("${abs(1.12)} ")
+}
