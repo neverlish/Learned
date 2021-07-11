55f71624607b8fe6d41f831ef42073a5a4fa2b87
Jinho Hyeon <neverlish@gmail.com>
2021-07-11 23:14:20 +0900

    4 - 173 지수함수

---
+++ kotlin/infopub-kotlin-200/src/ex_exponential_function/Main.kt
@@ -0,0 +1,13 @@
+package ex_exponential_function
+
+import kotlin.math.exp
+import kotlin.math.expm1
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+fun main(args: Array<String>) {
+    println(sqrt(2.0))
+    println(2.0.pow(0.5))
+    println(exp(2.0))
+    println(expm1(2.0))
+}
