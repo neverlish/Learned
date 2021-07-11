cb8ac83a9b7850f16e4e4d42eb9e9e43b4a413cc
Jinho Hyeon <neverlish@gmail.com>
2021-07-11 23:09:43 +0900

    4 - 169 실수 값 올림, 내림, 반올림하기

---
+++ kotlin/infopub-kotlin-200/src/ex_round_functions/Main.kt
@@ -0,0 +1,14 @@
+package ex_round_functions
+
+import kotlin.math.*
+
+fun main(args: Array<String>) {
+    print("${ceil(3.2)} ")
+    print("${ceil(3.7)} ")
+    print("${floor(3.2)} ")
+    print("${floor(3.7)} ")
+    print("${round(3.2)} ")
+    print("${round(3.7)} ")
+    print("${15.83.roundToInt()} ")
+    print("${37842.45.roundToLong()} ")
+}
