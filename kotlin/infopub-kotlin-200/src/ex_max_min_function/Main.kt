8581d557289101023a23fa6e7288a9dd96bda74d
Jinho Hyeon <neverlish@gmail.com>
2021-07-11 23:08:12 +0900

    4 - 168 최대값, 최소값 구하기

---
+++ kotlin/infopub-kotlin-200/src/ex_max_min_function/Main.kt
@@ -0,0 +1,11 @@
+package ex_max_min_function
+
+import kotlin.math.max
+import kotlin.math.min
+
+fun main(args: Array<String>) {
+    println(max(3, 7))
+    println(max(10.53, 1.15))
+    println(min(18, 13))
+    println(min(7.6, 20.01))
+}
