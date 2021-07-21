ae4b7c308b740bae67e54e7b0ce55b98ed13774b
Jinho Hyeon <neverlish@gmail.com>
2021-07-22 08:46:13 +0900

    1 - 01 - 14 조건문 - if 문(만약에... 라면)

---
+++ java/fastcampus-java-spring/part1/Chapter1/src/ch01/IfElseTest.java
@@ -0,0 +1,19 @@
+package ch01;
+
+public class IfElseTest {
+
+	public static void main(String[] args) {
+		int age = 7;
+		
+		if (age >= 8) {
+			System.out.println("학교에 다닙니다.");
+		}
+		else {
+			System.out.println("학교에 다니지 않습니다.");
+		}
+		
+		System.out.println("노는게 젤 좋아");
+
+	}
+
+}
