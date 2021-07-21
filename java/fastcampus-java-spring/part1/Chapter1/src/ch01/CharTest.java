33ce985a4ee6dca92bc932f33e5935173c2a55c9
Jinho Hyeon <neverlish@gmail.com>
2021-07-21 21:53:07 +0900

    1 - 01 - 08 자료형(data type)- 자료형(data type)- 문자는 프로그램에서 어떻게 표현하여 사용하나요

---
+++ java/fastcampus-java-spring/part1/Chapter1/src/ch01/CharTest.java
@@ -0,0 +1,25 @@
+package ch01;
+
+public class CharTest {
+
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1);
+		System.out.println((int)ch1);
+		
+		char ch2 = 66;
+		System.out.println(ch2);
+		System.out.println((char)ch2);
+
+		char ch3 = 67;
+		System.out.println(ch3);
+		System.out.println((char)ch3);
+		
+		char han = '한';
+		char ch = '\uD55C';
+		
+		System.out.println(han);
+		System.out.println(ch);
+	}
+
+}
