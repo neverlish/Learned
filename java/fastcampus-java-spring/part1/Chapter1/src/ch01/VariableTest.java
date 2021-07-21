0b8f766a4d6985a3984927c408d6f25f641e0ec6
Jinho Hyeon <neverlish@gmail.com>
2021-07-21 21:19:54 +0900

    1 - 01 - 06 정수는 프로그램에서 어떻게 표현하여 사용하나요

---
+++ java/fastcampus-java-spring/part1/Chapter1/src/ch01/VariableTest.java
@@ -0,0 +1,14 @@
+package ch01;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		byte bnum = -127;
+		System.out.println(bnum);
+		
+//		int num = 12345678900;
+		long lNum = 12345678900L;
+		System.out.println(lNum);
+	}
+
+}
