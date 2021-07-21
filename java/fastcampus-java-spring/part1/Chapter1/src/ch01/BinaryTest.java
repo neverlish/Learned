e7f7c5aeee3909c4f76b30d9f3d0ae25df61f518
Jinho Hyeon <neverlish@gmail.com>
2021-07-21 20:38:33 +0900

    1 - 01 - 04 컴퓨터에서 자료 표현하기

---
+++ java/fastcampus-java-spring/part1/Chapter1/src/ch01/BinaryTest.java
@@ -0,0 +1,18 @@
+package ch01;
+
+public class BinaryTest {
+
+	public static void main(String[] args) {
+		int num = 10;
+		int bNum = 0B1010;
+		int oNum = 012;
+		int xNum = 0XA;
+		
+		System.out.println(num);
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(xNum);
+
+	}
+
+}
