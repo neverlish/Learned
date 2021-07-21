5874912bd85d1c5c557efe0069bb73d49e855983
Jinho Hyeon <neverlish@gmail.com>
2021-07-21 21:37:51 +0900

    1 - 01 - 07 자료형(data type)- 실수는 어떻게 표현하여 사용하나요

---
+++ java/fastcampus-java-spring/part1/Chapter1/src/ch01/DoubleTest.java
@@ -0,0 +1,14 @@
+package ch01;
+
+public class DoubleTest {
+
+	public static void main(String[] args) {
+		double dnum = 3.14;
+		float fnum = 3.14f;
+		
+		System.out.println(dnum);
+		System.out.println(fnum);
+
+	}
+
+}
