fd284ff512e52191a7008731ae034ed782da133c
Jinho Hyeon <neverlish@gmail.com>
2021-07-22 16:14:04 +0900

    2 - 02 - 03 함수와 메서드

---
+++ java/fastcampus-java-spring/part2/Chapter2/src/ch03/FunctionTest.java
@@ -0,0 +1,40 @@
+package ch03;
+
+public class FunctionTest {
+
+	public static int addNum(int num1, int num2) {
+		int result;
+		result = num1 + num2;
+		return result;
+	}
+	
+	public static void sayHello(String greeting) {
+		System.out.println(greeting);
+	}
+	
+	public static int calcSum() {
+		int sum = 0;
+		int i;
+		
+		for (i = 0; i <= 100; i++) {
+			sum += i;
+		}
+		
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		int n1 = 10;
+		int n2 = 20;
+		
+		int total = addNum(n1, n2);
+		
+		System.out.println(total);
+		
+		sayHello("안녕하세요.");
+		
+		total = calcSum();
+		System.out.println(total);
+	}
+
+}
