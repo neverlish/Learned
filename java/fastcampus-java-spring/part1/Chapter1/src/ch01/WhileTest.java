807f50ca55b45ffd233dffc276770c3265e0a84e
Jinho Hyeon <neverlish@gmail.com>
2021-07-22 09:40:13 +0900

    1 - 01 - 17 반복문 - while문

---
+++ java/fastcampus-java-spring/part1/Chapter1/src/ch01/WhileTest.java
@@ -0,0 +1,20 @@
+package ch01;
+
+public class WhileTest {
+
+	public static void main(String[] args) {
+		
+		int num = 1;
+		int sum = 0;
+		
+		while (num <= 10) {
+			sum += num;
+			num++;
+		}
+		
+		System.out.println(sum);
+		System.out.println(num);
+
+	}
+
+}
