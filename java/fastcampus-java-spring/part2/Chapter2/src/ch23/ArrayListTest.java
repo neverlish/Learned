c42e6d3c4ba84badcfd4b64ffef871fcbd1c2371
Jinho Hyeon <neverlish@gmail.com>
2021-07-23 11:54:33 +0900

    2 - 02 - 23 객체 배열을 구현한 클래스 ArrayList

---
+++ java/fastcampus-java-spring/part2/Chapter2/src/ch23/ArrayListTest.java
@@ -0,0 +1,24 @@
+package ch23;
+
+import java.util.ArrayList;
+
+import ch21.Book;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+		ArrayList<Book> library = new ArrayList();
+		
+		library.add(new Book("태백산맥1", "조정래"));
+		library.add(new Book("태백산맥2", "조정래"));
+		library.add(new Book("태백산맥3", "조정래"));
+		library.add(new Book("태백산맥4", "조정래"));
+		library.add(new Book("태백산맥5", "조정래"));
+		
+		for (int i = 0; i < library.size(); i++) {
+			library.get(i).showInfo();
+		}
+
+	}
+
+}
