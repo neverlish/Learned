c2429af06fd9d4052a97713599afcf7de3635aaf
Jinho Hyeon <neverlish@gmail.com>
2021-07-23 10:15:27 +0900

    2 - 02 - 18 static 응용 - 싱글톤 패턴 (singleton pattern)

---
+++ java/fastcampus-java-spring/part2/Chapter2/src/ch18/Company.java
@@ -0,0 +1,19 @@
+package ch18;
+
+public class Company {
+	
+	private static Company instance = new Company();
+	
+	private Company() {
+		
+	}
+	
+	public static Company getInstance() {
+		if (instance == null)
+			instance = new Company();
+		
+		return instance;
+	}
+	
+	
+}
