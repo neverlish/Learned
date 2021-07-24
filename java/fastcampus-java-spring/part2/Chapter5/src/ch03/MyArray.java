201096259b53e2951bcedb53628f7ddc52ffdf15
Jinho Hyeon <neverlish@gmail.com>
2021-07-24 19:03:00 +0900

    2 - 05 자바와 자료구조 - 03 배열(Array) 구현하기

---
+++ java/fastcampus-java-spring/part2/Chapter5/src/ch03/MyArray.java
@@ -0,0 +1,88 @@
+package ch03;
+
+public class MyArray {
+	int[] intArr;
+	int count;
+	
+	public int ARRAY_SIZE;
+	public static final int ERROR_NUM = -999999999;
+	
+	public MyArray() {
+		count = 0;
+		ARRAY_SIZE = 10;
+		intArr = new int[ARRAY_SIZE];
+	}
+	
+	public MyArray(int size) {
+		count = 0;
+		ARRAY_SIZE = size;
+		intArr = new int[ARRAY_SIZE];
+	}
+	
+	public void addElement(int num) {
+		if (count >= ARRAY_SIZE) {
+			System.out.println("not enough memory");
+			return;
+		}
+		intArr[count++] = num;
+	}
+	
+	public void insertElement(int position, int num) {
+		int i;
+		
+		if (position < 0 || position > count) {
+			return;
+		}
+		
+		if (count >= ARRAY_SIZE) {
+			return;
+		}
+		
+		for (i = count - 1; i >= position; i++) {
+			intArr[i+1] = intArr[i];
+		}
+		intArr[position] = num;
+		count++;
+	}
+	
+	public int removeElement(int position) {
+		 int ret = ERROR_NUM;
+		 
+		 if (isEmpty()) {
+			 System.out.println("Array is empty");
+			 return ret;
+		 }
+		 
+		 if (position < 0 || position > count - 1) {
+			 return ret;
+		 }
+		 
+		 ret = intArr[position];
+		 
+		 for (int i = position; i < count - 1; i++) {
+			 intArr[i] = intArr[i+1];
+		 }
+		 count--;
+		 
+		 return ret;
+	}
+	
+	public int getSize() {
+		return count;
+	}
+	
+	public boolean isEmpty() {
+		if (count == 0) {
+			return true;
+		}
+		else return false;
+	}
+	
+	public int getElement(int position) {
+		if (position < 0 || position > count - 1) {
+			System.out.println("검색 위치 오류. 현재 리스트의 개수는 " + count + "개 입니다.");
+			return ERROR_NUM;
+		}
+		return intArr[position];
+	}
+}
