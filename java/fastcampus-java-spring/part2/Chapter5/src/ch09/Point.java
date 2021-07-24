8f0db5dec95612209bd2b76fbff6680a5aae70d6
Jinho Hyeon <neverlish@gmail.com>
2021-07-24 20:53:21 +0900

    2 - 05 - 09 제네릭 메서드 활용하기

---
+++ java/fastcampus-java-spring/part2/Chapter5/src/ch09/Point.java
@@ -0,0 +1,21 @@
+package ch09;
+
+public class Point<T, V> {
+	
+	T x;
+	V y;
+	
+	Point(T x, V y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public  T getX() {
+			return x;
+	}
+
+	public V getY() {
+		return y;
+    }
+}
+
