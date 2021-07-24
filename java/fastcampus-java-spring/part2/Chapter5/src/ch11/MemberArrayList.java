c79d8c8317c88fb814905fe5ce1e257d9224a714
Jinho Hyeon <neverlish@gmail.com>
2021-07-24 21:30:01 +0900

    2 - 05 - 12 Collection 요소를 순회하는 Iterator

--- java/fastcampus-java-spring/part2/Chapter5/src/ch11/MemberArrayList.java
+++ java/fastcampus-java-spring/part2/Chapter5/src/ch11/MemberArrayList.java
@@ -1,6 +1,7 @@
 package ch11;
 
 import java.util.ArrayList;  // 사용할 클래스 import
+import java.util.Iterator;
 
 public class MemberArrayList {
 
@@ -16,7 +17,7 @@ public class MemberArrayList {
 	
 	public boolean removeMember(int memberId){  // 멤버 아이디를 매개변수로, 삭제 여부를 반환
 		
-		for(int i =0; i<arrayList.size(); i++){ // 해당 아이디를 가진 멤버를 ArrayList에서 찾음
+	/*	for(int i =0; i<arrayList.size(); i++){ // 해당 아이디를 가진 멤버를 ArrayList에서 찾음
 			Member member = arrayList.get(i);
 			int tempId = member.getMemberId();
 			if(tempId == memberId){            // 멤버아이디가 매개변수와 일치하면 
@@ -24,7 +25,17 @@ public class MemberArrayList {
 				return true;                   // true 반환
 			}
 		}
+		*/
 		
+		Iterator<Member> ir = arrayList.iterator();
+		while(ir.hasNext()) {
+			Member member = ir.next();
+			int tempId = member.getMemberId();
+			if(tempId == memberId){            // 멤버아이디가 매개변수와 일치하면 
+				arrayList.remove(member);           // 해당 멤버를 삭제
+				return true;                   // true 반환
+			}
+		}
 		
 		System.out.println(memberId + "가 존재하지 않습니다");  //for 가 끝날때 까지 return 이 안된경우
 		return false;                   
