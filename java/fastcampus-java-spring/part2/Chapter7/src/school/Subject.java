2e7515ec2fb1b0775fcb5a91fcda5c02a0a3731d
Jinho Hyeon <neverlish@gmail.com>
2021-07-28 10:51:51 +0900

    2 - 07 프로젝트 만들기 - 01 성적 관리 프로그램(1)

---
+++ java/fastcampus-java-spring/part2/Chapter7/src/school/Subject.java
@@ -0,0 +1,55 @@
+package school;
+
+import java.util.ArrayList;
+
+import utils.Define;
+
+public class Subject {
+	private String subjectName;
+	private int subjectId;
+	private int gradeType;
+	
+	private ArrayList<Student> studentList = new ArrayList<Student>();
+	
+	public Subject(String subjectName, int subjectId) {
+		this.subjectName = subjectName;
+		this.subjectId = subjectId;
+		this.gradeType = Define.AB_TYPE;
+	}
+
+	public String getSubjectName() {
+		return subjectName;
+	}
+
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+
+	public int getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(int subjectId) {
+		this.subjectId = subjectId;
+	}
+
+	public ArrayList<Student> getStudentList() {
+		return studentList;
+	}
+
+	public void setStudentList(ArrayList<Student> studentList) {
+		this.studentList = studentList;
+	}
+	
+	public int getGradeType() {
+		return gradeType;
+	}
+
+	public void setGradeType(int gradeType) {
+		this.gradeType = gradeType;
+	}
+	
+	public void register(Student student){  //수강신청
+		studentList.add(student);
+	}
+} 
---
+++ java/fastcampus-java-spring/part2/Chapter7/src/utils/Define.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Define {
+
+	public static final int KOREAN = 1001;  //국어
+	public static final int MATH = 2001;    //수학
+		
+	public static final int AB_TYPE = 0;    // A, B, C
+	public static final int SAB_TYPE = 1;   // S, A, B, c
+	
+}
