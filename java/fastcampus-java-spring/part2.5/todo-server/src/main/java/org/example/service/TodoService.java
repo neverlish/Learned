264308a068d7fd2f2242594fad291be00b3123b8
Jinho Hyeon <neverlish@gmail.com>
2021-07-30 09:23:39 +0900

    2.5 - 03 - 05 서비스 코드 구현

---
+++ java/fastcampus-java-spring/part2.5/todo-server/src/main/java/org/example/service/TodoService.java
@@ -0,0 +1,59 @@
+package org.example.service;
+
+import lombok.AllArgsConstructor;
+import org.example.model.TodoEntity;
+import org.example.model.TodoRequest;
+import org.example.repository.TodoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class TodoService {
+    private final TodoRepository todoRepository;
+
+    public TodoEntity add(TodoRequest request) {
+        TodoEntity todoEntity = new TodoEntity();
+        todoEntity.setTitle(request.getTitle());
+        todoEntity.setOrder(request.getOrder());
+        todoEntity.setCompleted(request.getCompleted());
+        return this.todoRepository.save(todoEntity);
+    }
+
+    public TodoEntity searchById(Long id) {
+        return this.todoRepository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    public List<TodoEntity> searchAll() {
+        return this.todoRepository.findAll();
+    }
+
+    public TodoEntity updateById(Long id, TodoRequest request) {
+        TodoEntity todoEntity = this.searchById(id);
+        if (request.getTitle() != null) {
+            todoEntity.setTitle(request.getTitle());
+        }
+
+        if (request.getOrder() != null) {
+            todoEntity.setOrder(request.getOrder());
+        }
+
+        if (request.getCompleted() != null) {
+            todoEntity.setCompleted(request.getCompleted());
+        }
+
+        return this.todoRepository.save(todoEntity);
+    }
+
+    public void deleteById(Long id) {
+        this.todoRepository.deleteById(id);
+    }
+
+    public void deleteAll() {
+        this.todoRepository.deleteAll();
+    }
+}
