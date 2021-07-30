98a32c0fe6150cda5bf9e8878c78a64af84fad7a
Jinho Hyeon <neverlish@gmail.com>
2021-07-30 09:17:12 +0900

    2.5 - 03 - 04 repository 구현

---
+++ java/fastcampus-java-spring/part2.5/todo-server/src/main/java/org/example/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package org.example.repository;
+
+import org.example.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+}
