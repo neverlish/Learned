6db129c8a3c936648ec562c02ec2b90d3da3ca5b
Jinho Hyeon <neverlish@gmail.com>
2021-07-31 15:06:46 +0900

    3 - 04 - 03 GET API(2)

--- java/fastcampus-java-spring/part3/chapter04/hello/src/main/java/com/example/hello/controller/GetApiController.java
+++ java/fastcampus-java-spring/part3/chapter04/hello/src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,6 +1,9 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.UserRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
@@ -19,5 +22,41 @@ public class GetApiController {
     public String pathVariable(@PathVariable(name = "name") String pathName) {
         System.out.println("PathVariable : " + pathName);
         return pathName;
+    }
+
+    @GetMapping(path = "query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam) {
+        StringBuilder sb = new StringBuilder();
+
+        queryParam.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
+
+            sb.append(entry.getKey() + " = " + entry.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @GetMapping("query-param02")
+    public String queryParam02(
+        @RequestParam String name,
+        @RequestParam String email,
+        @RequestParam int age
+    ) {
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+        return name + " " + email + " " + age;
+    }
+
+    @GetMapping("query-param03")
+    public String queryParam03(UserRequest userRequest) {
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
     }
 }
