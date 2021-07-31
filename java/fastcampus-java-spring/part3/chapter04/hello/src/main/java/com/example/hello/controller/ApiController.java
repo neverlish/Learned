e2d5530d96d7453792f65e3290cc5452d9d18f23
Jinho Hyeon <neverlish@gmail.com>
2021-07-31 14:25:29 +0900

    3 - 04 스프링 부트 시작하기 - 02 Hello World API를 만들어보자

---
+++ java/fastcampus-java-spring/part3/chapter04/hello/src/main/java/com/example/hello/controller/ApiController.java
@@ -0,0 +1,14 @@
+package com.example.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring boot!";
+    }
+}
