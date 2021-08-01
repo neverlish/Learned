d253138387718fb3b9650bc197f4544b2f9605c8
Jinho Hyeon <neverlish@gmail.com>
2021-08-01 11:07:36 +0900

    1 - 06 HelloWorld Bean 추가

--- spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -10,4 +10,9 @@ public class HelloWorldController {
     public String helloWorld() {
         return "Hello World";
     }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World Bean");
+    }
 }
