c41f45c96ad737a0614ed1fc8647fbbe65be318e
Jinho Hyeon <neverlish@gmail.com>
2021-08-01 10:15:26 +0900

    1 Spring Boot로 개발하는 RESTful Service - 03 Spring Boot Project 생성

---
+++ spring/inflearn-springboot-resetful-webservice/src/test/java/com/example/restfulwebservice/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
