9e685053b6b8b25b2828a514559974d472e980f1
Jinho Hyeon <neverlish@gmail.com>
2021-08-01 11:33:04 +0900

    2 - 02 사용자 목록 조회를 위한 API 구현 - GET HTTP Method

---
+++ spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -0,0 +1,26 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+}
--- spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/user/UserDaoService.java
+++ spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -1,9 +1,12 @@
 package com.example.restfulwebservice.user;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Service
 public class UserDaoService {
     private static List<User> users = new ArrayList<>();
 
