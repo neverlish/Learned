1613484ac9ec0bd669c92a159298ae5c5d0df239
Jinho Hyeon <neverlish@gmail.com>
2021-08-05 09:06:54 +0900

    3 - 03 다국어 처리를 위한 Internationalization 구현 방법

--- spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -2,6 +2,11 @@ package com.example.restfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
@@ -10,4 +15,10 @@ public class RestfulWebServiceApplication {
         SpringApplication.run(RestfulWebServiceApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
+    }
 }
