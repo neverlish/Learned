e7edac58a1582a1ad11d01dde5f511f11820b699
Jinho Hyeon <neverlish@gmail.com>
2021-08-06 09:55:44 +0900

    5 - 04 Spring Data JPA를 이용한 Entity 설정과 초기 데이터 생성

--- spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/user/User.java
+++ spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/user/User.java
@@ -7,6 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -16,7 +19,11 @@ import java.util.Date;
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
---
+++ spring/inflearn-springboot-resetful-webservice/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate(), 'User1', 'test1111', '701010-1111111');
+insert into user values(2, sysdate(), 'User2', 'test2222', '801010-2222222');
+insert into user values(3, sysdate(), 'User3', 'test3333', '901010-1111111');
