0ea3d95de3c411e564599cf90afcaac8991c88a5
Jinho Hyeon <neverlish@gmail.com>
2021-08-06 09:44:15 +0900

    5 Java Persistence API 사용 - 03 JPA를 사용을 위한 Dependency 추가와 설정

--- spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -3,10 +3,19 @@ package com.example.restfulwebservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
