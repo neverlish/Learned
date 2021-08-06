536b2e0c9e7ec2b7d08e585c50db7e2fc6b56c5f
Jinho Hyeon <neverlish@gmail.com>
2021-08-06 09:27:36 +0900

    4 - 08 Configuration 클래스를 이용한 사용자 인증 처리

---
+++ spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.restfulwebservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("kenneth")
+                .password("{noop}test1234")
+                .roles("USER");
+    }
+}
