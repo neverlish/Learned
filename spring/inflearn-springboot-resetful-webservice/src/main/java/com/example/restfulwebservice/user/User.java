03822ec07c91cf5c8de46f0cd3470b56db95e14c
Jinho Hyeon <neverlish@gmail.com>
2021-08-06 09:05:05 +0900

    4 - 04 Swagger Documentation 구현 방법

--- spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/user/User.java
+++ spring/inflearn-springboot-resetful-webservice/src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,6 +1,8 @@
 package com.example.restfulwebservice.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,14 +15,21 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
+
     @Past
+    @ApiModelProperty(notes = "사용자의 등록일을 입력해 주세요.")
     private Date joinDate;
 
+    @ApiModelProperty(notes = "사용자의 패스워드를 입력해 주세요.")
     private String password;
+
+    @ApiModelProperty(notes = "사용자의 주민번호를 입력해 주세요.")
     private String ssn;
 }
