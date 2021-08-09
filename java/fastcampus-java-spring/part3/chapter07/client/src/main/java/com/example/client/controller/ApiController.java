ecdc6076dd7f2e96738b3316fb55c17a71f82761
Jinho Hyeon <neverlish@gmail.com>
2021-08-09 09:35:47 +0900

    3 - 07 Server to Server 간의 연결 - 02 Rest Template 사용하기(1)

---
+++ java/fastcampus-java-spring/part3/chapter07/client/src/main/java/com/example/client/controller/ApiController.java
@@ -0,0 +1,23 @@
+package com.example.client.controller;
+
+import com.example.client.dto.UserResponse;
+import com.example.client.service.RestTemplateService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client")
+public class ApiController {
+
+    private final RestTemplateService restTemplateService;
+
+    public ApiController(RestTemplateService restTemplateService) {
+        this.restTemplateService = restTemplateService;
+    }
+
+    @GetMapping("/hello")
+    public UserResponse getHello() {
+        return restTemplateService.hello();
+    }
+}
---
+++ java/fastcampus-java-spring/part3/chapter07/client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -0,0 +1,36 @@
+package com.example.client.service;
+
+import com.example.client.dto.UserResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class RestTemplateService {
+
+    public UserResponse hello() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .queryParam("name", "aaaa")
+                .queryParam("age", 99)
+                .path("/api/server/hello")
+                .encode()
+                .build()
+                .toUri();
+
+        System.out.println(uri.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
+
+        System.out.println(result.getStatusCode());
+        System.out.println(result.getBody());
+
+        return result.getBody();
+    }
+
+}
