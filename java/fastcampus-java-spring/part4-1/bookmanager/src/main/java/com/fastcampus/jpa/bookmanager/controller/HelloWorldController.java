0bd7c11e7bd5fae02e2044fc9820cc2813098fcc
Jinho Hyeon <neverlish@gmail.com>
2021-08-11 13:49:00 +0900

    4-1 안정적인 서비스를 위한 스프링DATA와 Security-JPA 프로그래밍 - 01 학습 전 기초 사항 - 03 스프링 부트 프로젝트 생성

---
+++ java/fastcampus-java-spring/part4-1/bookmanager/src/main/java/com/fastcampus/jpa/bookmanager/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.fastcampus.jpa.bookmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "hello-world";
+    }
+}
---
+++ java/fastcampus-java-spring/part4-1/bookmanager/src/test/java/com/fastcampus/jpa/bookmanager/controller/HelloWorldControllerTest.java
@@ -0,0 +1,25 @@
+package com.fastcampus.jpa.bookmanager.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class HelloWorldControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void helloWorld() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello-world"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello-world"))
+                .andDo(print());
+    }
+}
