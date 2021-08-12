5dd2c6217808ccfb923d83529ed1600ff94615ad
Jinho Hyeon <neverlish@gmail.com>
2021-08-12 09:17:23 +0900

    4-1 - 02 - 02 Repository Interface 메소드 실습 (1)

--- java/fastcampus-java-spring/part4-1/bookmanager/src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
+++ java/fastcampus-java-spring/part4-1/bookmanager/src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -1,9 +1,14 @@
 package com.fastcampus.jpa.bookmanager.repository;
 
 import com.fastcampus.jpa.bookmanager.domain.User;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -13,8 +18,19 @@ class UserRepositoryTest {
 
     @Test
     void crud() {
-        userRepository.save(new User());
+//        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
+//        List<User> users = userRepository.findAllById(Lists.newArrayList(1L, 2L, 3L));
+//
+//        users.forEach(System.out::println);
+//        User user1 = new User("jack", "jack@fastcampus.com");
+//        User user2 = new User("steve", "steve@fastcampus.com");
+//
+//        userRepository.saveAll(Lists.newArrayList(user1, user2));
+//
+//        List<User> users = userRepository.findAll();
+//        users.forEach(System.out::println);
 
-        System.out.println(">>>" + userRepository.findAll());
+        User user = userRepository.findById(1L).orElse(null);
+        System.out.println(user);
     }
 }
