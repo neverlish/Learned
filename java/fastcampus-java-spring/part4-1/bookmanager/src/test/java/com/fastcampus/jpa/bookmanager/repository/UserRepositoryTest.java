ec869b82ef4ef16f7d7c4c62557a1ae53d024c8f
Jinho Hyeon <neverlish@gmail.com>
2021-08-13 10:20:46 +0900

    4-1 - 02 - 02 Repository Interface 메소드 실습 (2)

--- java/fastcampus-java-spring/part4-1/bookmanager/src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
+++ java/fastcampus-java-spring/part4-1/bookmanager/src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -5,10 +5,13 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Sort;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
-import java.util.List;
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.endsWith;
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -18,19 +21,53 @@ class UserRepositoryTest {
 
     @Test
     void crud() {
-//        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
-//        List<User> users = userRepository.findAllById(Lists.newArrayList(1L, 2L, 3L));
-//
-//        users.forEach(System.out::println);
-//        User user1 = new User("jack", "jack@fastcampus.com");
-//        User user2 = new User("steve", "steve@fastcampus.com");
-//
-//        userRepository.saveAll(Lists.newArrayList(user1, user2));
-//
-//        List<User> users = userRepository.findAll();
-//        users.forEach(System.out::println);
+//        userRepository.save(new User("new martin", "newmartin@fastcampus.com"));
+//        userRepository.flush();
 
-        User user = userRepository.findById(1L).orElse(null);
-        System.out.println(user);
+//        userRepository.saveAndFlush(new User("new martin", "newmartin@fastcampus.com"));
+//        userRepository.flush();
+//        userRepository.findAll().forEach(System.out::println);
+
+//        long count = userRepository.count();
+//        System.out.println(count);
+
+//        boolean exists = userRepository.existsById(1L);
+//        System.out.println(exists);
+
+//        userRepository.delete(userRepository.findById(1L).orElseThrow(RuntimeException::new));
+//        userRepository.deleteById(1L);
+//        userRepository.findAll().forEach(System.out::println);
+
+//        userRepository.deleteAll(userRepository.findAllById(Lists.newArrayList(1L, 3L)));
+//        userRepository.deleteInBatch(userRepository.findAllById(Lists.newArrayList(1L, 3L)));
+//        userRepository.findAll().forEach(System.out::println);
+
+//        Page<User> users = userRepository.findAll(PageRequest.of(1, 3));
+//        System.out.println("page : " + users);
+//        System.out.println("totalElements: " + users.getTotalElements());
+//        System.out.println("totalPages : " + users.getTotalPages());
+//        System.out.println("numberOfElements : " + users.getNumberOfElements());
+//        System.out.println("sort : " + users.getSort());
+//        System.out.println("size : " + users.getSize());
+//
+//        users.getContent().forEach(System.out::println);
+//        ExampleMatcher matcher = ExampleMatcher.matching()
+//                .withIgnorePaths("name")
+//                .withMatcher("email", endsWith());
+//
+//        Example<User> example = Example.of(new User("ma", "fastcampus.com"), matcher);
+//
+//        userRepository.findAll(example).forEach(System.out::println);
+
+//        Example<User> example = Example.of(new User("martin", "martin@fastcampus.com"));
+//
+//        userRepository.findAll(example).forEach(System.out::println);
+
+        User user = new User();
+        user.setEmail("slow");
+
+        ExampleMatcher matcher = ExampleMatcher.matching().withMatcher("email", contains());
+        Example<User> example = Example.of(user, matcher);
+        userRepository.findAll(example).forEach(System.out::println);
     }
 }
