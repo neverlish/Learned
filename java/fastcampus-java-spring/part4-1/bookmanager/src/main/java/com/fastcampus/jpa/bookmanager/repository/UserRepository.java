2799d923400d33843b1fb609855702416f5449f2
Jinho Hyeon <neverlish@gmail.com>
2021-08-20 09:38:28 +0900

    4-1 - 03 QueryMethod 살펴보기 - 01 쿼리메소드의 정의 및 기본실습 1

--- java/fastcampus-java-spring/part4-1/bookmanager/src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
+++ java/fastcampus-java-spring/part4-1/bookmanager/src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
@@ -3,6 +3,31 @@ package com.fastcampus.jpa.bookmanager.repository;
 import com.fastcampus.jpa.bookmanager.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.List;
+import java.util.Set;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Set<User> findByName(String name);
+
+    User findByEmail(String email);
+
+    User getByEmail(String email);
+
+    User readByEmail(String email);
+
+    User queryByEmail(String email);
+
+    User searchByEmail(String email);
+
+    User streamByEmail(String email);
+
+    User findUserByEmail(String email);
+
+    User findSomethingByEmail(String email);
+
+    List<User> findFirst2ByName(String name);
+
+    List<User> findTop2ByName(String name);
+
+    List<User> findLast1ByName(String name);
 }
