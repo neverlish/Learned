b38ce02769809cf60f36b8a84dce4dabaccad23f
Jinho Hyeon <neverlish@gmail.com>
2021-08-24 08:49:24 +0900

    1 - 4 제어의 역전(IoC) - 1 오브젝트 팩토리

---
+++ spring/toby-spring/springbook/src/springbook/user/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+package springbook.user.dao;
+
+public class DaoFactory {
+    public UserDao userDao() {
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+
+        UserDao userDao = new UserDao(connectionMaker);
+
+        return userDao;
+    }
+}
