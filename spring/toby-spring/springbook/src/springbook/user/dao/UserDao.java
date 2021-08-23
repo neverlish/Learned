92028a05f89bd6b2e2a2a0c7964c2a0370d48ce3
Jinho Hyeon <neverlish@gmail.com>
2021-08-23 20:50:59 +0900

    1 - 3 - 3 관계설정 책임의 분리

--- spring/toby-spring/springbook/src/springbook/user/dao/UserDao.java
+++ spring/toby-spring/springbook/src/springbook/user/dao/UserDao.java
@@ -7,8 +7,8 @@ import java.sql.*;
 public class UserDao {
     private ConnectionMaker connectionMaker;
 
-    public UserDao() {
-        connectionMaker = new DConnectionMaker();
+    public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
     }
 
     public void add(User user) throws SQLException {
@@ -47,24 +47,5 @@ public class UserDao {
         c.close();
 
         return user;
-    }
-
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new UserDao();
-
-        User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
-
-        dao.add(user);
-
-        System.out.println(user.getId() + " 등록 성공");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-
-        System.out.println(user2.getId() + " 조회 성공");
     }
 }
