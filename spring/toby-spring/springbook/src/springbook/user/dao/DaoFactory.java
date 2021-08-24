61da1602032cf64eeee18ac57f164db63ae69223
Jinho Hyeon <neverlish@gmail.com>
2021-08-24 18:13:25 +0900

    1 - 5 스프링의 IoC - 1 오브젝트 팩토리를 이용한 스프링 IoC

--- spring/toby-spring/springbook/src/springbook/user/dao/DaoFactory.java
+++ spring/toby-spring/springbook/src/springbook/user/dao/DaoFactory.java
@@ -1,12 +1,18 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         UserDao userDao = new UserDao(connectionMaker());
 
         return userDao;
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
--- spring/toby-spring/springbook/test/springbook/user/dao/UserDaoTest.java
+++ spring/toby-spring/springbook/test/springbook/user/dao/UserDaoTest.java
@@ -1,5 +1,7 @@
 package springbook.user.dao;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
@@ -7,7 +9,8 @@ import java.sql.SQLException;
 class UserDaoTest {
 
     public static void main(String[] args) throws SQLException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("whiteship");
