6fa8267cee127e4df05fc623326c20be70b1cb40
Jinho Hyeon <neverlish@gmail.com>
2021-08-26 22:47:28 +0900

    1 - 8 - 3 DataSource 인터페이스로 변환

--- spring/toby-spring/springbook/src/springbook/user/dao/CountingDaoFactory.java
+++ spring/toby-spring/springbook/src/springbook/user/dao/CountingDaoFactory.java
@@ -2,6 +2,9 @@ package springbook.user.dao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class CountingDaoFactory {
@@ -9,8 +12,19 @@ public class CountingDaoFactory {
     @Bean
     public UserDao userDao() {
         UserDao userDao = new UserDao();
-        userDao.setConnectionMaker(connectionMaker());
+        userDao.setDataSource(dataSource());
         return userDao;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/springbook");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+
+        return dataSource;
     }
 
     @Bean
--- spring/toby-spring/springbook/src/springbook/user/dao/UserDao.java
+++ spring/toby-spring/springbook/src/springbook/user/dao/UserDao.java
@@ -2,17 +2,18 @@ package springbook.user.dao;
 
 import springbook.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private ConnectionMaker connectionMaker;
+    private DataSource dataSource;
 
-    public void setConnectionMaker(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public void add(User user) throws SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values (?, ?, ?)"
@@ -28,7 +29,7 @@ public class UserDao {
     }
 
     public User get(String id) throws SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?"
