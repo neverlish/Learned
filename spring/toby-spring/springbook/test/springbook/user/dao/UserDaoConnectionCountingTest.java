fc3addc007afc5555d7f3512f49acbcaa7501271
Jinho Hyeon <neverlish@gmail.com>
2021-08-26 13:36:06 +0900

    1 - 7 의존관계 주입(DI) - 4 의존관계 주입의 응용

---
+++ spring/toby-spring/springbook/test/springbook/user/dao/UserDaoConnectionCountingTest.java
@@ -0,0 +1,31 @@
+package springbook.user.dao;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+public class UserDaoConnectionCountingTest {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+
+        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+        System.out.println("Connection counter : " + ccm.getCounter());
+    }
+}
