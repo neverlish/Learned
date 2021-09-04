001470e08b5e50b11d1030fa3a654dee71afa80a
Jinho Hyeon <neverlish@gmail.com>
2021-09-04 20:48:00 +0900

    2 - 4 - 2 DI와 테스트

--- spring/toby-spring/springbook/test/springbook/user/dao/UserDaoTest.java
+++ spring/toby-spring/springbook/test/springbook/user/dao/UserDaoTest.java
@@ -6,24 +6,29 @@ import org.junit.runner.JUnitCore;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="/applicationContext.xml")
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations="/test-applicationContext.xml")
+//@DirtiesContext
 public class UserDaoTest {
 //    @Autowired
 //    private ApplicationContext context;
 
-    @Autowired
+//    @Autowired
     private UserDao dao;
+
     private User user1;
     private User user2;
     private User user3;
@@ -33,6 +38,10 @@ public class UserDaoTest {
         this.user1 = new User("gyumee", "박성철", "springno1");
         this.user2 = new User("leegw700", "이길원", "springno2");
         this.user3 = new User("bumjin", "박범진", "springno3");
+
+        dao = new UserDao();
+        DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost/testdb", "root", "", true);
+        dao.setDataSource(dataSource);
     }
 
 
