c59edc8e0c524b6def9b004050f55e996d10ed07
Jinho Hyeon <neverlish@gmail.com>
2021-09-04 20:17:16 +0900

    2 - 4 스프링 테스트 적용 - 1 테스트를 위한 애플리케이션 컨텍스트 관리

--- spring/toby-spring/springbook/test/springbook/user/dao/UserDaoTest.java
+++ spring/toby-spring/springbook/test/springbook/user/dao/UserDaoTest.java
@@ -3,10 +3,12 @@ package springbook.user.dao;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
@@ -14,7 +16,13 @@ import java.sql.SQLException;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/applicationContext.xml")
 public class UserDaoTest {
+//    @Autowired
+//    private ApplicationContext context;
+
+    @Autowired
     private UserDao dao;
     private User user1;
     private User user2;
@@ -22,8 +30,6 @@ public class UserDaoTest {
 
     @Before
     public void setUp() {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        this.dao = context.getBean("userDao", UserDao.class);
         this.user1 = new User("gyumee", "박성철", "springno1");
         this.user2 = new User("leegw700", "이길원", "springno2");
         this.user3 = new User("bumjin", "박범진", "springno3");
