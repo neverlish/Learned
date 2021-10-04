cb008df76030ae6425c2876adbdaf15871b01e5a
Jinho Hyeon <neverlish@gmail.com>
2021-10-04 21:31:35 +0900

    3 - 6 - 5 재사용 가능한 콜백의 분리

--- spring/toby-spring/springbook/src/springbook/user/dao/UserDao.java
+++ spring/toby-spring/springbook/src/springbook/user/dao/UserDao.java
@@ -1,10 +1,6 @@
 package springbook.user.dao;
 
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
 
@@ -13,65 +9,43 @@ import java.sql.*;
 import java.util.List;
 
 public class UserDao {
-    private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.dataSource = dataSource;
     }
 
-    public void add(User user) throws SQLException {
+    public void add(User user) {
         this.jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?)",
                 user.getId(), user.getName(), user.getPassword());
     }
 
-    public User get(String id) throws SQLException {
+    public User get(String id) {
         return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-                new Object[]{id},
-                new RowMapper<User>() {
-                    @Override
-                    public User mapRow(ResultSet rs, int i) throws SQLException {
-                        User user = new User();
-                        user.setId(rs.getString("id"));
-                        user.setName(rs.getString("name"));
-                        user.setPassword(rs.getString("password"));
-                        return user;
-                    }
-            }
+            new Object[]{id},
+            this.userMapper
         );
     }
 
-    public void deleteAll() throws SQLException {
+    public void deleteAll() {
         this.jdbcTemplate.update("delete from users");
     }
 
-    public int getCount() throws SQLException {
+    public int getCount() {
         return this.jdbcTemplate.queryForInt("select count(*) from users");
-//        return this.jdbcTemplate.query(new PreparedStatementCreator() {
-//            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-//                return con.prepareStatement("select count(*) from users");
-//            }
-//        }, new ResultSetExtractor<Integer>() {
-//            public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-//                rs.next();
-//                return rs.getInt(1);
-//            }
-//        });
     }
 
     public List<User> getAll() {
-        return this.jdbcTemplate.query("select * from users order by id",
-            new RowMapper<User>() {
-                @Override
-                public User mapRow(ResultSet rs, int i) throws SQLException {
-                    User user = new User();
-                    user.setId(rs.getString("id"));
-                    user.setName(rs.getString("name"));
-                    user.setPassword(rs.getString("password"));
-                    return user;
-                }
-            }
-        );
+        return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
     }
+
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        public User mapRow(ResultSet rs, int i) throws SQLException {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        }
+    };
 }
