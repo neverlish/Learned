0fe398bbf047a3d4d5d590052f0a5b80be1f81c6
Jinho Hyeon <neverlish@gmail.com>
2021-10-11 23:53:25 +0900

    4-2 - 02 - 05 Basic 토큰인증

---
+++ java/fastcampus-java-spring/part4-2/server/basic-authentication-test/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.sp.fc.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "hello";
+    }
+
+    @PostMapping("/greeting")
+    public String greeting(@RequestBody String name) {
+        return "hello " + name;
+    }
+}
---
+++ java/fastcampus-java-spring/part4-2/server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTest.java
@@ -0,0 +1,68 @@
+package com.sp.fc.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BasicAuthenticationTest {
+    @LocalServerPort
+    int port;
+
+    RestTemplate client = new RestTemplate();
+
+    private String greetingUrl() {
+        return "http://localhost:" + port + "/greeting";
+    }
+
+    @DisplayName("1. 인증 실패")
+    @Test
+    void test_1() {
+        HttpClientErrorException exception = assertThrows(HttpClientErrorException.class, () -> {
+            client.getForObject(greetingUrl(), String.class);
+        });
+
+        assertEquals(401, exception.getRawStatusCode());
+    }
+
+    @DisplayName("2. 인증 성공")
+    @Test
+    void test_2() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Basic " + Base64.getEncoder().encodeToString(
+                "user1:1111".getBytes()
+        ));
+        HttpEntity entity = new HttpEntity(null, headers);
+        ResponseEntity<String> response = client.exchange(greetingUrl(), HttpMethod.GET, entity, String.class);
+        assertEquals("hello", response.getBody());
+    }
+
+    @DisplayName("3. 인증 성공2")
+    @Test
+    void test_3() {
+        TestRestTemplate testClient = new TestRestTemplate("user1", "1111");
+        String resp = testClient.getForObject(greetingUrl(), String.class);
+        assertEquals("hello", resp);
+    }
+
+    @DisplayName("4. POST 인증")
+    @Test
+    void test_4() {
+        TestRestTemplate testClient = new TestRestTemplate("user1", "1111");
+        ResponseEntity<String> resp = testClient.postForEntity(greetingUrl(), "jongwon", String.class);
+        assertEquals("hello jongwon", resp.getBody());
+    }
+}
