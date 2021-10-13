60dcdd5deab8138d3ce2db78f2af07a9049b5f89
Jinho Hyeon <neverlish@gmail.com>
2021-10-13 10:42:57 +0900

    4 예외 - 2 예외 전환 - 4 기술에 독립적인 UserDao 만들기

--- spring/toby-spring/springbook/src/springbook/user/dao/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package springbook.user.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-    public Connection makeConnection() throws SQLException;
-}
---
+++ spring/toby-spring/springbook/src/springbook/user/dao/UserDaoJdbc.java
@@ -0,0 +1,51 @@
+package springbook.user.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import springbook.user.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.List;
+
+public class UserDaoJdbc implements UserDao {
+    private JdbcTemplate jdbcTemplate;
+
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void add(User user) {
+        this.jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword());
+    }
+
+    public User get(String id) {
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
+            new Object[]{id},
+            this.userMapper
+        );
+    }
+
+    public void deleteAll() {
+        this.jdbcTemplate.update("delete from users");
+    }
+
+    public int getCount() {
+        return this.jdbcTemplate.queryForInt("select count(*) from users");
+    }
+
+    public List<User> getAll() {
+        return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
+    }
+
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        public User mapRow(ResultSet rs, int i) throws SQLException {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        }
+    };
+}
