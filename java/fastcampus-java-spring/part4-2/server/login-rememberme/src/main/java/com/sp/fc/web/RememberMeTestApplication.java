f6c3418bdc6a18e982452011e81ba497f02f29d6
Jinho Hyeon <neverlish@gmail.com>
2021-10-16 20:25:43 +0900

    4-2 - 02 - 07 로그인을 지원하기 위한 필터들

--- java/fastcampus-java-spring/part4-2/server/login-rememberme/src/main/java/com/sp/fc/web/RememberMeTestApplication.java
+++ java/fastcampus-java-spring/part4-2/server/login-rememberme/src/main/java/com/sp/fc/web/RememberMeTestApplication.java
@@ -2,17 +2,13 @@ package com.sp.fc.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
         "com.sp.fc.config",
         "com.sp.fc.web"
 })
-public class UserDetailsTestApplication {
-
+public class RememberMeTestApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserDetailsTestApplication.class, args);
+        SpringApplication.run(RememberMeTestApplication.class, args);
     }
-
 }
