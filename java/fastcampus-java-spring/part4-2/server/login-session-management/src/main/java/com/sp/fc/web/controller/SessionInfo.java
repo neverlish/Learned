03e78937d0ad62bf3efe61a45fcef65cbaacfdcc
Jinho Hyeon <neverlish@gmail.com>
2021-10-17 11:18:18 +0900

    4-2 - 02 - 08 세션관리

---
+++ java/fastcampus-java-spring/part4-2/server/login-session-management/src/main/java/com/sp/fc/web/controller/SessionInfo.java
@@ -0,0 +1,17 @@
+package com.sp.fc.web.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SessionInfo {
+    private String sessionId;
+    private Date time;
+}
