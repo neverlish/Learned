6647a2bd5db3a6095097cef9c5956972a8a77fe7
Jinho Hyeon <neverlish@gmail.com>
2021-10-19 12:45:39 +0900

    5 - 1 - 2 사용자 수정 기능 추가

--- spring/toby-spring/springbook/src/springbook/user/dao/UserDao.java
+++ spring/toby-spring/springbook/src/springbook/user/dao/UserDao.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface UserDao {
     void add(User user);
     User get(String id);
+    public void update(User user1);
     List<User> getAll();
     void deleteAll();
     int getCount();
--- spring/toby-spring/springbook/test/springbook/user/dao/UserDaoTest.java
+++ spring/toby-spring/springbook/test/springbook/user/dao/UserDaoTest.java
@@ -141,6 +141,27 @@ public class UserDaoTest {
         }
     }
 
+    @Test
+    public void update() {
+        dao.deleteAll();
+
+        dao.add(user1);
+        dao.add(user2);
+
+        user1.setName("오민규");
+        user1.setPassword("springno6");
+        user1.setLevel(Level.GOLD);
+        user1.setLogin(1000);
+        user1.setRecommend(999);
+        dao.update(user1);
+
+        User user1update = dao.get(user1.getId());
+        checkSameUser(user1, user1update);
+
+        User user2same = dao.get(user2.getId());
+        checkSameUser(user2, user2same);
+    }
+
     private void checkSameUser(User user1, User user2) {
         assertThat(user1.getId(), is(user2.getId()));
         assertThat(user1.getName(), is(user2.getName()));
