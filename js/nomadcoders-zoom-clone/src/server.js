20c5699b7709398c2255d0f840d9e6e0644d8c09
Jinho Hyeon <neverlish@gmail.com>
2021-12-17 18:20:09 +0900

    1 CHAT WITH WEBSOCKETS - 02 WebSockets in NodeJS

--- js/nomadcoders-zoom-clone/src/server.js
+++ js/nomadcoders-zoom-clone/src/server.js
@@ -1,13 +1,18 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
