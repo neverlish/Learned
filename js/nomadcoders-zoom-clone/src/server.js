4487e9bce5c3044c4ccb316ab2d51bfe3f883d58
Jinho Hyeon <neverlish@gmail.com>
2021-12-21 00:03:17 +0900

    2 - 07 Nicknames

--- js/nomadcoders-zoom-clone/src/server.js
+++ js/nomadcoders-zoom-clone/src/server.js
@@ -20,15 +20,18 @@ wsServer.on("connection", (socket) => {
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-		socket.to(roomName).emit("welcome");
+		socket.to(roomName).emit("welcome", socket.nickname);
   });
   socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname)
+    );
   });
   socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", msg);
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
     done();
   });
+	socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
 /*
