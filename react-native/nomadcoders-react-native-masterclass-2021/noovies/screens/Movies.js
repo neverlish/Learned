3fdb1a915c2bffd816789b225c08db7c00c69b72
Jinho Hyeon <neverlish@gmail.com>
2022-01-28 16:51:06 +0900

    2 - 05 Tab Navigation

---
+++ react-native/nomadcoders-react-native-masterclass-2021/noovies/screens/Movies.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Movies = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Movies</Text>
+  </View>
+);
+
+export default Movies;
---
+++ react-native/nomadcoders-react-native-masterclass-2021/noovies/screens/Tv.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Tv = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Tv</Text>
+  </View>
+);
+
+export default Tv;
