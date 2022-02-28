c4438f62bc4c7dcb7a28018ba4df6f2bd507f0a9
Jinho Hyeon <neverlish@gmail.com>
2022-02-28 22:19:44 +0900

    3 - 23 Detail Screen part Three

--- react-native/nomadcoders-react-native-masterclass-2021/noovies/screens/Movies.tsx
+++ react-native/nomadcoders-react-native-masterclass-2021/noovies/screens/Movies.tsx
@@ -1,14 +1,14 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useState } from "react";
-import { Dimensions, FlatList } from 'react-native';
-import Swiper from 'react-native-swiper';
-import { useQuery, useQueryClient } from 'react-query';
-import styled from 'styled-components/native';
-import { MovieResponse, moviesApi } from '../api';
+import { Dimensions, FlatList } from "react-native";
+import Swiper from "react-native-swiper";
+import { useQuery, useQueryClient } from "react-query";
+import styled from "styled-components/native";
+import { moviesApi } from "../api";
 import HList from "../components/HList";
-import HMedia from '../components/HMedia';
+import HMedia from "../components/HMedia";
 import Loader from "../components/Loader";
-import Slide from '../components/Slide';
+import Slide from "../components/Slide";
 
 const { height: SCREEN_HEIGHT } = Dimensions.get('window');
 
@@ -30,19 +30,25 @@ const HSeparator = styled.View`
 const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
   const queryClient = useQueryClient();
   const [refreshing, setRefreshing] = useState(false);
-  const { isLoading: nowPlayingLoading, data: nowPlayingData } =
-    useQuery<MovieResponse>(["movies", "nowPlaying"], moviesApi.nowPlaying);
-  const { isLoading: upcomingLoading, data: upcomingData } =
-    useQuery<MovieResponse>(["movies", "upcoming"], moviesApi.upcoming);
-  const { isLoading: trendingLoading, data: trendingData } =
-    useQuery<MovieResponse>(["movies", "trending"], moviesApi.trending);
+  const { isLoading: nowPlayingLoading, data: nowPlayingData } = useQuery(
+    ["movies", "nowPlaying"],
+    moviesApi.nowPlaying
+  );
+  const { isLoading: upcomingLoading, data: upcomingData } = useQuery(
+    ["movies", "upcoming"],
+    moviesApi.upcoming
+  );
+  const { isLoading: trendingLoading, data: trendingData } = useQuery(
+    ["movies", "trending"],
+    moviesApi.trending
+  );
   const onRefresh = async () => {
     setRefreshing(true);
     await queryClient.refetchQueries(["movies"]);
     setRefreshing(false);
   };
-
   const loading = nowPlayingLoading || upcomingLoading || trendingLoading;
+
   return loading ? (
     <Loader />
   ) : upcomingData ? (
@@ -60,15 +66,15 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
             showsPagination={false}
             containerStyle={{
               marginBottom: 40,
-              width: '100%',
+              width: "100%",
               height: SCREEN_HEIGHT / 4,
             }}
           >
             {nowPlayingData?.results.map((movie) => (
               <Slide
                 key={movie.id}
-                backdropPath={movie.backdrop_path || ''}
-                posterPath={movie.poster_path || ''}
+                backdropPath={movie.backdrop_path || ""}
+                posterPath={movie.poster_path || ""}
                 originalTitle={movie.original_title}
                 voteAverage={movie.vote_average}
                 overview={movie.overview}
@@ -83,11 +89,11 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
         </>
       }
       data={upcomingData.results}
-      keyExtractor={(item) => item.id + ''}
+      keyExtractor={(item) => item.id + ""}
       ItemSeparatorComponent={HSeparator}
       renderItem={({ item }) => (
         <HMedia
-          posterPath={item.poster_path || ''}
+          posterPath={item.poster_path || ""}
           originalTitle={item.original_title}
           overview={item.overview}
           releaseDate={item.release_date}
@@ -95,7 +101,7 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
         />
       )}
     />
-  ) : null
+  ) : null;
 };
 
 export default Movies;
