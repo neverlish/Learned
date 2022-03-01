b5cbe76db371161e5f81a7573c8aa4d4cc5ff468
Jinho Hyeon <neverlish@gmail.com>
2022-03-01 09:29:30 +0900

    4 - 02 Manual Animations

--- react-native/nomadcoders-react-native-masterclass-2021/langApp/App.js
+++ react-native/nomadcoders-react-native-masterclass-2021/langApp/App.js
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
 
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
 export default function App() {
-  return null;
+  const [y, setY] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+  const moveUp = () => {
+    const id = setInterval(() => setY((prev) => prev + 1), 1);
+    setIntervalId(id);
+  };
+  useEffect(() => {
+    if (y === 200) {
+      clearInterval(intervalId);
+    }
+  }, [y, intervalId]);
+  console.log("rendering");
+  return (
+    <Container>
+      <Box
+        onPress={moveUp}
+        style={{
+          transform: [{ translateY: y }],
+        }}
+      />
+    </Container>
+  );
 }
