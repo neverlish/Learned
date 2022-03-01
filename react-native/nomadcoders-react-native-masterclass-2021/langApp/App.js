bbb8789ffb03cba1b97c7ffb2b54f92d8e8b2406
Jinho Hyeon <neverlish@gmail.com>
2022-03-01 11:27:22 +0900

    4 - 05 Animation and State

--- react-native/nomadcoders-react-native-masterclass-2021/langApp/App.js
+++ react-native/nomadcoders-react-native-masterclass-2021/langApp/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Animated, TouchableOpacity } from "react-native";
+import React, { useRef, useState } from "react";
+import { Animated, Easing ,TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -15,13 +15,16 @@ const Box = styled.View`
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  const Y = new Animated.Value(0);
+  const [up, setUp] = useState(false);
+  const Y = useRef(new Animated.Value(0)).current;
+  const toggleUp = () => setUp((prev) => !prev);
   const moveUp = () => {
-    Animated.spring(Y, {
-      toValue: -200,
+    Animated.timing(Y, {
+      toValue: up ? 200 : -200,
       bounciness: 20,
       useNativeDriver: true,
-    }).start();
+      easing: Easing.circle,
+    }).start(toggleUp);
   };
   return (
     <Container>
