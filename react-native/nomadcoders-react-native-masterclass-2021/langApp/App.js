ae951994d7ef367352b0991e4b0c587478a21dfd
Jinho Hyeon <neverlish@gmail.com>
2022-03-01 11:15:25 +0900

    4 - 04 Our First Animation

--- react-native/nomadcoders-react-native-masterclass-2021/langApp/App.js
+++ react-native/nomadcoders-react-native-masterclass-2021/langApp/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Animated } from "react-native";
+import React, { useState } from "react";
+import { Animated, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -7,7 +7,7 @@ const Container = styled.View`
   justify-content: center;
   align-items: center;
 `;
-const Box = styled.TouchableOpacity`
+const Box = styled.View`
   background-color: tomato;
   width: 200px;
   height: 200px;
@@ -16,15 +16,22 @@ const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
   const Y = new Animated.Value(0);
-  const moveUp = () => {};
+  const moveUp = () => {
+    Animated.spring(Y, {
+      toValue: -200,
+      bounciness: 20,
+      useNativeDriver: true,
+    }).start();
+  };
   return (
     <Container>
-      <AnimatedBox
-        onPress={moveUp}
-        style={{
-          transform: [{ translateY: Y }],
-        }}
-      />
+      <TouchableOpacity onPress={moveUp}>
+        <AnimatedBox
+          style={{
+            transform: [{ translateY: Y }],
+          }}
+        />
+      </TouchableOpacity>
     </Container>
   );
 }
