81d01265dccd5c0e2e251fc7158cab89ada10502
Jinho Hyeon <neverlish@gmail.com>
2022-03-20 17:55:00 +0900

    5 - 03 Realm SDK

--- react-native/nomadcoders-react-native-masterclass-2021/nomad-diary/App.js
+++ react-native/nomadcoders-react-native-masterclass-2021/nomad-diary/App.js
@@ -1,8 +1,37 @@
+import Realm from "realm";
+import AppLoading from "expo-app-loading";
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
 import Navigator from "./navigator";
 
+const FeelingSchema = {
+  name: "Feeling",
+  properties: {
+    _id: "int",
+    emotion: "string",
+    message: "string",
+  },
+  primaryKey: "_id",
+};
+
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const startLoading = async() => {
+    const realm = await Realm.open({
+      path: "nomadDiaryDB",
+      schema: [FeelingSchema],
+    });
+  };
+  const onFinish = () => setReady(true);
+  if (!ready) {
+    return (
+      <AppLoading
+        onError={console.error}
+        startAsync={startLoading}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <NavigationContainer>
       <Navigator />
