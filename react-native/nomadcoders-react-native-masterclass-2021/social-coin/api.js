ffef53cf1a2263db915d86ff5876f7565cf4741a
Jinho Hyeon <neverlish@gmail.com>
2022-03-21 21:02:15 +0900

    6 - 09 Victory Charts

--- react-native/nomadcoders-react-native-masterclass-2021/social-coin/api.js
+++ react-native/nomadcoders-react-native-masterclass-2021/social-coin/api.js
@@ -5,9 +5,10 @@ export const coins = () => fetch(COINS_URL).then((response) => response.json());
 
 export const info = ({ queryKey }) =>
   fetch(`${COINS_URL}/${queryKey[1]}`).then((response) => response.json());
+
 export const history = ({ queryKey }) =>
   fetch(
     `${BASE_URL}/tickers/${queryKey[1]}/historical?start=${
       new Date().toISOString().split("T")[0]
-    }&interval=30m`
+    }&interval=2h`
   ).then((response) => response.json());
--- react-native/nomadcoders-react-native-masterclass-2021/social-coin/screens/Detail.js
+++ react-native/nomadcoders-react-native-masterclass-2021/social-coin/screens/Detail.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components/native";
 import { history, info } from "../api";
+import { BLACK_COLOR } from "../colors";
 import { Icon } from "../components/Coin";
+import { VictoryChart, VictoryLine, VictoryScatter } from "victory-native";
 
-const Container = styled.ScrollView``;
+const Container = styled.ScrollView`
+  background-color: ${BLACK_COLOR};
+`;
 
 const Detail = ({
   navigation,
@@ -31,7 +35,34 @@ const Detail = ({
     ["coinHistory", id],
     history
   );
-  console.log(infoData);
-  return <Container />;
+  const [victoryData, setVictoryData] = useState(null);
+  useEffect(() => {
+    if (historyData) {
+      setVictoryData(
+        historyData.map((price) => ({
+          x: new Date(price.timestamp).getTime(),
+          y: price.price,
+        }))
+      );
+    }
+  }, [historyData]);
+  return (
+    <Container>
+      {victoryData ? (
+        <VictoryChart height={360}>
+          <VictoryLine
+            animate
+            interpolation="monotoneX"
+            data={victoryData}
+            style={{ data: { stroke: "#1abc9c" } }}
+          />
+          <VictoryScatter
+            data={victoryData}
+            style={{ data: { fill: "#1abc9c" } }}
+          />
+        </VictoryChart>
+      ) : null}
+    </Container>
+  );
 };
 export default Detail;
