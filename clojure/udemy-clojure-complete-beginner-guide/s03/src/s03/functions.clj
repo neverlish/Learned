38ce90dfcd1ba1f74b8c847ace4789af9b71aeb4
Jinho Hyeon <neverlish@gmail.com>
2022-05-22 17:44:38 +0900

    3 - 16 Data Types

--- clojure/udemy-clojure-complete-beginner-guide/s03/src/s03/functions.clj
+++ clojure/udemy-clojure-complete-beginner-guide/s03/src/s03/functions.clj
@@ -14,3 +14,23 @@
 (defn increment_set
   [x]
   (map increment x))
+
+(defn DataTypes
+  []
+  (def a 1)
+  (def b 1.25)
+  (def c 1.25e-12)
+  (def d 0xfbfc)
+  (def e nil)
+  (def f true)
+  (def g "Hello")
+  (def h 'thanks)
+
+  (println a)
+  (println b)
+  (println c)
+  (println d)
+  (println e)
+  (println f)
+  (println g)
+  (println h))
