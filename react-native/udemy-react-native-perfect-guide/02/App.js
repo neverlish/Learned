d40372aafe5936e1ea0ed4b441910b3b4242bdde
Jinho Hyeon <neverlish@gmail.com>
2022-10-23 15:08:03 +0900

    02 - 014 핵심 컴포넌트로 작업하기

--- react-native/udemy-react-native-perfect-guide/02/App.js
+++ react-native/udemy-react-native-perfect-guide/02/App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Another piece of text!</Text>
+      </View>
+      <Text>Hello World!</Text>
+      <Button title="Tap me!" />
     </View>
   );
 }
