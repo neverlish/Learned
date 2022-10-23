6e0553906d6159de566fc548b5d66d9c647c46f8
Jinho Hyeon <neverlish@gmail.com>
2022-10-23 15:45:07 +0900

    02 - 020 플렉스 박스 - 집중 탐구

--- react-native/udemy-react-native-perfect-guide/02/App.js
+++ react-native/udemy-react-native-perfect-guide/02/App.js
@@ -1,32 +1,48 @@
-import { Button, StyleSheet, TextInput, View, Text } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!' />
-        <Button title='Add Goal' />
+    <View
+      style={{
+        padding: 50,
+        flexDirection: 'row',
+        width: '80%',
+        height: 300,
+        justifyContent: 'space-around',
+        alignItems: 'stretch',
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          flex: 2,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    padding: 50,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '80%',
-    marginRight: 8,
-    padding: 8,
-  },
-});
