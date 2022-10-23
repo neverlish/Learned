ec2aee83e86dd536bfb0531032ce3bd62086015d
Jinho Hyeon <neverlish@gmail.com>
2022-10-23 18:39:06 +0900

    02 - 034 모달 오버레이 스타일링하기

--- react-native/udemy-react-native-perfect-guide/02/components/GoalInput.js
+++ react-native/udemy-react-native-perfect-guide/02/components/GoalInput.js
@@ -14,15 +14,22 @@ function GoalInput(props) {
   }
 
   return (
-    <Modal visible={props.visible} animationType='slide'>
+    <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
-          placeholder='Your course goal!' 
+          placeholder="Your course goal!"
           onChangeText={goalInputHandler}
           value={enteredGoalText}
         />
-        <Button title='Add Goal' onPress={addGoalHandler} />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" />
+          </View>
+        </View>
       </View>
     </Modal>
   );
@@ -33,18 +40,25 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 24,
+    padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: '#cccccc',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
+    width: '100%',
     padding: 8,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8
   },
 });
