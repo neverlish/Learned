3f786e7624ecc33e738413fa6417ce3d242aad99
Jinho Hyeon <neverlish@gmail.com>
2022-10-23 18:06:47 +0900

    02 - 031 아이템 삭제할 수 있게 만들기 & ID 사용하기

--- react-native/udemy-react-native-perfect-guide/02/App.js
+++ react-native/udemy-react-native-perfect-guide/02/App.js
@@ -13,8 +13,10 @@ export default function App() {
     ]);
   };
 
-  function deleteGoalHandler() {
-    console.log('DELETE');
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    })
   }
 
   return (
@@ -29,6 +31,7 @@ export default function App() {
             return (
               <GoalItem 
                 text={itemData.item.text} 
+                id={itemData.item.id}
                 onDeleteItem={deleteGoalHandler} 
               />
             );
--- react-native/udemy-react-native-perfect-guide/02/components/GoalItem.js
+++ react-native/udemy-react-native-perfect-guide/02/components/GoalItem.js
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet, Pressable } from 'react-native';
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem}>
+    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
       <View style={styles.goalItem}>
         <Text style={styles.goalText}>{props.text}</Text>
       </View>
