8ccbe2910ee6738db553ad332a7b3cdab63df7f9
Jinho Hyeon <neverlish@gmail.com>
2022-10-23 18:13:35 +0900

    02 - 032 Android 물결 효과 추가하기 & iOS 대안

--- react-native/udemy-react-native-perfect-guide/02/components/GoalItem.js
+++ react-native/udemy-react-native-perfect-guide/02/components/GoalItem.js
@@ -2,7 +2,11 @@ import { View, Text, StyleSheet, Pressable } from 'react-native';
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+    <Pressable 
+      android_ripple={{ color: '#dddddd' }} 
+      onPress={props.onDeleteItem.bind(this, props.id)}
+      style={({ pressed }) => pressed && styles.pressedItem}
+    >
       <View style={styles.goalItem}>
         <Text style={styles.goalText}>{props.text}</Text>
       </View>
@@ -15,11 +19,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
+  pressedItem: {
+    opacity: 0.5
+  },
   goalText: {
     color: 'white',
+    padding: 8,
   },
 });
