0621ca26e5f3e2ce0de1102270cd26a9acd3af53
Jinho Hyeon <neverlish@gmail.com>
2022-10-23 15:19:35 +0900

    02 - 015 React Native 앱 스타일링하기

--- react-native/udemy-react-native-perfect-guide/02/App.js
+++ react-native/udemy-react-native-perfect-guide/02/App.js
@@ -4,9 +4,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another piece of text!</Text>
+        <Text style={styles.dummyText}>Another piece of text!</Text>
       </View>
-      <Text>Hello World!</Text>
+      <Text style={styles.dummyText}>
+        Hello World!
+      </Text>
       <Button title="Tap me!" />
     </View>
   );
@@ -18,5 +20,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  dummyText: {
+    margin: 16, 
+    borderWidth: 2, 
+    borderColor: 'red', 
+    padding: 16,
   },
 });
