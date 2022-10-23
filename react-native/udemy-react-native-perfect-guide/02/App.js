33aeed1d9378069a055e111879171b69b3f192a4
Jinho Hyeon <neverlish@gmail.com>
2022-10-23 18:52:55 +0900

    02 - 037 앱 최종 마무리

--- react-native/udemy-react-native-perfect-guide/02/App.js
+++ react-native/udemy-react-native-perfect-guide/02/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { FlatList, StyleSheet, View, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
 import GoalInput from './components/GoalInput';
 import GoalItem from './components/GoalItem';
 
@@ -30,36 +32,39 @@ export default function App() {
   }
 
   return (
-    <View style={styles.appContainer}>
-      <Button
-        title='Add New Goal'
-        color='#5e0acc'
-        onPress={startAddGoalHandler}
-      />
-      <GoalInput 
-        visible={modalIsVisible} 
-        onAddGoal={addGoalHandler}
-        onCancel={endAddGoalHandler}
-      />
-      <View style={styles.goalsContainer}>
-        <FlatList 
-          data={courseGoals}
-          renderItem={(itemData) => {
-            return (
-              <GoalItem 
-                text={itemData.item.text}  
-                id={itemData.item.id}
-                onDeleteItem={deleteGoalHandler} 
-              />
-            );
-          }}
-          keyExtractor={(item, index) => {
-            return item.id;
-          }}
-          alwaysBounceVertical={false}
+    <>
+      <StatusBar style='light' />
+      <View style={styles.appContainer}>
+        <Button
+          title='Add New Goal'
+          color='#a065ec'
+          onPress={startAddGoalHandler}
         />
+        <GoalInput 
+          visible={modalIsVisible} 
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+        />
+        <View style={styles.goalsContainer}>
+          <FlatList 
+            data={courseGoals}
+            renderItem={(itemData) => {
+              return (
+                <GoalItem 
+                  text={itemData.item.text}  
+                  id={itemData.item.id}
+                  onDeleteItem={deleteGoalHandler} 
+                />
+              );
+            }}
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+            alwaysBounceVertical={false}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
--- react-native/udemy-react-native-perfect-guide/02/components/GoalInput.js
+++ react-native/udemy-react-native-perfect-guide/02/components/GoalInput.js
@@ -28,10 +28,10 @@ function GoalInput(props) {
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} />
+            <Button title="Add Goal" onPress={addGoalHandler} color='#b180f0' />
           </View>
           <View style={styles.button}>
-            <Button title="Cancel" onPress={props.onCancel} />
+            <Button title="Cancel" onPress={props.onCancel} color='#f31282' />
           </View>
         </View>
       </View>
@@ -56,9 +56,12 @@ const styles = StyleSheet.create({
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
+    borderColor: '#e4d0ff',
+    backgroundColor: '#e4d0ff',
+    color: '#120438',
+    borderRadius: 6,
     width: '100%',
-    padding: 8,
+    padding: 16,
   },
   buttonContainer: {
     marginTop: 16,
