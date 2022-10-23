035f4109c8b63a928087b6cb1514d8785e6706a2
Jinho Hyeon <neverlish@gmail.com>
2022-10-23 15:27:16 +0900

    02 - 017 레이아웃 & 플렉스 박스 살펴보기

--- react-native/udemy-react-native-perfect-guide/02/App.js
+++ react-native/udemy-react-native-perfect-guide/02/App.js
@@ -1,30 +1,21 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { Button, StyleSheet, TextInput, View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       <View>
-        <Text style={styles.dummyText}>Another piece of text!</Text>
+        <TextInput placeholder='Your course goal!' />
+        <Button title='Add Goal' />
       </View>
-      <Text style={styles.dummyText}>
-        Hello World!
-      </Text>
-      <Button title="Tap me!" />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText: {
-    margin: 16, 
-    borderWidth: 2, 
-    borderColor: 'red', 
-    padding: 16,
-  },
+  appContainer: {
+    padding: 50,
+  }  
 });
