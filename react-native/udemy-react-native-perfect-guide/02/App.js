c9b0edb5dd12cdff8163a57c87b5f234f5de895c
Jinho Hyeon <neverlish@gmail.com>
2022-10-23 15:35:58 +0900

    02 - 019 레이아웃 생성에 플렉스 박스 사용하기

--- react-native/udemy-react-native-perfect-guide/02/App.js
+++ react-native/udemy-react-native-perfect-guide/02/App.js
@@ -3,8 +3,8 @@ import { Button, StyleSheet, TextInput, View, Text } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Your course goal!' />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' />
         <Button title='Add Goal' />
       </View>
       <View>
@@ -17,5 +17,16 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
-  }  
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8,
+  },
 });
