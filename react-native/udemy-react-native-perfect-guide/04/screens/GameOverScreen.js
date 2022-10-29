da0fa1a056880a3b11bf973e6a3052b41004d823
Jinho Hyeon <neverlish@gmail.com>
2022-10-30 00:10:57 +0900

    04 - 071 (전경) 이미지 추가하기

--- react-native/udemy-react-native-perfect-guide/04/screens/GameOverScreen.js
+++ react-native/udemy-react-native-perfect-guide/04/screens/GameOverScreen.js
@@ -1,9 +1,42 @@
-import { Text } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
 function GameOverScreen() {
   return (
-    <Text>Game is Over!</Text>
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image 
+          style={styles.image} 
+          source={require('../assets/images/success.png')} 
+        />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y.</Text>
+    </View>
   )
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
