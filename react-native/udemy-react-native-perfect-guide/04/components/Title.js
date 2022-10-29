e917f4e5ceb1b8e85856aa427ffd29f029d175b2
Jinho Hyeon <neverlish@gmail.com>
2022-10-29 21:12:40 +0900

    04 - 062 제목 컴포넌트 생성하기

---
+++ react-native/udemy-react-native-perfect-guide/04/components/Title.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text } from "react-native";
+
+function Title({ children }) {
+  return <Text style={styles.title}>{children}</Text>;
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#ddb52f',
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: '#ddb52f',
+    padding: 12,
+  },
+});
--- react-native/udemy-react-native-perfect-guide/04/screens/GameScreen.js
+++ react-native/udemy-react-native-perfect-guide/04/screens/GameScreen.js
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Title from '../components/Title';
 
 function GameScreen() {
   return (
     <View style={styles.screen}>
-      <Text>Opponent's Guess</Text>
+      <Title>Opponent's Guess</Title>      
       
       <View>
         <Text>Higher or lower?</Text>
