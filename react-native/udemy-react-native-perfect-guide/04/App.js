f4145179783b2ae2834b7ee7c7a7c632708b18c5
Jinho Hyeon <neverlish@gmail.com>
2022-10-29 19:44:37 +0900

    04 - 055 선형 그러데이션 추가하기

--- react-native/udemy-react-native-perfect-guide/04/App.js
+++ react-native/udemy-react-native-perfect-guide/04/App.js
@@ -1,18 +1,19 @@
-import { StyleSheet, View } from 'react-native';
+
+
+import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: '#ddb52f',
   },
 });
-Footer
