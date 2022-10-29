32c087b2641f2c4b857fa181df350f0a58de2ce3
Jinho Hyeon <neverlish@gmail.com>
2022-10-29 19:49:15 +0900

    04 - 056 배경 이미지 추가하기

--- react-native/udemy-react-native-perfect-guide/04/App.js
+++ react-native/udemy-react-native-perfect-guide/04/App.js
@@ -1,13 +1,20 @@
 
 
 import { LinearGradient } from 'expo-linear-gradient';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -15,5 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
