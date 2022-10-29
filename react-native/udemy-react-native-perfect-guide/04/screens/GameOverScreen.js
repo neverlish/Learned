544cbfb6936872c3ffe00de5e38bc9b7e8574a72
Jinho Hyeon <neverlish@gmail.com>
2022-10-30 00:22:00 +0900

    04 - 072 중첩 텍스트 사용 & 스타일링하기

--- react-native/udemy-react-native-perfect-guide/04/screens/GameOverScreen.js
+++ react-native/udemy-react-native-perfect-guide/04/screens/GameOverScreen.js
@@ -1,4 +1,5 @@
 import { Image, StyleSheet, Text, View } from "react-native";
+import PrimaryButton from "../components/ui/PrimaryButton";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
 
@@ -12,7 +13,11 @@ function GameOverScreen() {
           source={require('../assets/images/success.png')} 
         />
       </View>
-      <Text>Your phone needed X rounds to guess the number Y.</Text>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to guess the number{' '}
+        <Text style={styles.highlight}>Y.</Text>
+      </Text>
+      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
   )
 }
@@ -38,5 +43,15 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%',
+  },
+  summaryText: {
+    fontFamily: 'open-sans',
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: 'open-sans-bold',
+    color: Colors.primary500,
   },
 });
