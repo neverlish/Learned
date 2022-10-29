622680a775d54f790cb57cde38e673c5ac4add39
Jinho Hyeon <neverlish@gmail.com>
2022-10-29 18:47:20 +0900

    04 - 051 TextInput 필드 구성하기

--- react-native/udemy-react-native-perfect-guide/04/screens/StartGameScreen.js
+++ react-native/udemy-react-native-perfect-guide/04/screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType='number-pad'
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
       <PrimaryButton>Rest</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
