e1ab9d7c9bb32ac3b9609a8476d8f0d9ef5cd7ad
Jinho Hyeon <neverlish@gmail.com>
2022-10-29 19:32:00 +0900

    04 - 054 컴포넌트 & 앱 전반에 색상 넣기

--- react-native/udemy-react-native-perfect-guide/04/App.js
+++ react-native/udemy-react-native-perfect-guide/04/App.js
@@ -1,10 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen />
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: '#ddb52f',
+  },
+});
+Footer
