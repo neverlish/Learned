79cece00ec44ff6f1ed8f07b40005a3a94151953
Jinho Hyeon <neverlish@gmail.com>
2022-10-29 21:05:39 +0900

    04 - 060 게임 화면 작업 시작하기

--- react-native/udemy-react-native-perfect-guide/04/screens/GameScreen.js
+++ react-native/udemy-react-native-perfect-guide/04/screens/GameScreen.js
@@ -1,9 +1,24 @@
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 function GameScreen() {
-  return <Text>
-    Game Screen!
-  </Text>
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      
+      <View>
+        <Text>Higher or lower?</Text>
+        
+      </View>
+      
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
