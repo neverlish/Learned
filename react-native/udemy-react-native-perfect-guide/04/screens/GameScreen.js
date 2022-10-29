2800222f2d55cc8564d0638ee65ffd744fcd2c51
Jinho Hyeon <neverlish@gmail.com>
2022-10-29 23:51:06 +0900

    04 - 069 아이콘(버튼 아이콘)으로 작업하기

--- react-native/udemy-react-native-perfect-guide/04/screens/GameScreen.js
+++ react-native/udemy-react-native-perfect-guide/04/screens/GameScreen.js
@@ -5,6 +5,7 @@ import Card from '../components/ui/Card';
 import InstructionText from '../components/ui/InstructionText';
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
+import { Ionicons } from '@expo/vector-icons';
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -69,10 +70,14 @@ function GameScreen({ userNumber, onGameOver }) {
         <InstructionText style={styles.instructionText}>Higher or lower?</InstructionText>
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+              <Ionicons name='md-remove' size={24} color='white' />
+            </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+            <Ionicons name='md-add' size={24} color='white' />
+            </PrimaryButton>
           </View>
         </View>
       </Card>
