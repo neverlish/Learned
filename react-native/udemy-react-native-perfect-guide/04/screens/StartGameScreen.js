ebb3c8ad5b3502c98734899857638c39a14005b3
Jinho Hyeon <neverlish@gmail.com>
2022-10-30 01:45:44 +0900

    04 - 047 화면 컴포넌트 설정하기

---
+++ react-native/udemy-react-native-perfect-guide/04/screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from 'react-native';
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      BUTTONS
+    </View>
+  );
+}
+
+export default StartGameScreen;
