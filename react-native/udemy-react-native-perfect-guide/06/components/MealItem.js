b62bb1c2d5aecc372ec9dff2ba10b75035a0999e
Jinho Hyeon <neverlish@gmail.com>
2022-11-06 22:10:55 +0900

    06 - 099 음식 디스플레이하기

---
+++ react-native/udemy-react-native-perfect-guide/06/components/MealItem.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+function MealItem({ title }) {
+  return <View>
+    <Text>{title}</Text>
+  </View>
+}
+
+export default MealItem;
--- react-native/udemy-react-native-perfect-guide/06/screens/MealsOverviewScreen.js
+++ react-native/udemy-react-native-perfect-guide/06/screens/MealsOverviewScreen.js
@@ -1,11 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import MealItem from '../components/MealItem';
 import { MEALS } from '../data/dummy-data';
 
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
