6f8e74b8c13265ada5f3c6b25edc976468cd45b9
Jinho Hyeon <neverlish@gmail.com>
2022-11-06 17:55:03 +0900

    05 - 81 Dimensions API 개요

--- react-native/udemy-react-native-perfect-guide/04/components/game/NumberContainer.js
+++ react-native/udemy-react-native-perfect-guide/04/components/game/NumberContainer.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import Colors from '../../constants/colors';
 
 function NumberContainer({ children }) {
@@ -11,19 +11,21 @@ function NumberContainer({ children }) {
 
 export default NumberContainer;
 
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
   container: {
     borderWidth: 4,
     borderColor: Colors.accent500,
-    padding: 24,
-    margin: 24,
+    padding: deviceWidth < 380 ? 12 : 24,
+    margin: deviceWidth < 380 ? 12 : 24,
     borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
   },
   numberText: {
     color: Colors.accent500,
-    fontSize: 36,
+    fontSize: deviceWidth < 380 ? 28 : 36,
     fontFamily: 'open-sans-bold',
   },
 });
--- react-native/udemy-react-native-perfect-guide/04/components/ui/Card.js
+++ react-native/udemy-react-native-perfect-guide/04/components/ui/Card.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
 import Colors from "../../constants/colors";
 
 function Card({ children }) {
@@ -11,11 +11,13 @@ function Card({ children }) {
 
 export default Card;
 
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
   inputContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 36,
+    marginTop: deviceWidth < 380 ? 18 : 36,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: Colors.primary800,
