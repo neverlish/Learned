587ac1cbab1422a7a8eab1d2347d1d80c7b75a74
Jinho Hyeon <neverlish@gmail.com>
2022-11-06 22:00:03 +0900

    06 - 098 화면 간 데이터 전달을 위해 라우트 매개변수로 작업하기

--- react-native/udemy-react-native-perfect-guide/06/screens/CategoriesScreen.js
+++ react-native/udemy-react-native-perfect-guide/06/screens/CategoriesScreen.js
@@ -5,7 +5,9 @@ import { CATEGORIES } from '../data/dummy-data';
 function CategoriesScreen({ navigation }) {
   function renderCategoryItem(itemData) {
     function pressHandler() {
-      navigation.navigate('MealsOverview');
+      navigation.navigate('MealsOverview', {
+        categoryId: itemData.item.id,
+      });
     }
   
     return (
--- react-native/udemy-react-native-perfect-guide/06/screens/MealsOverviewScreen.js
+++ react-native/udemy-react-native-perfect-guide/06/screens/MealsOverviewScreen.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { MEALS } from '../data/dummy-data';
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <Text>Meals Overview Screen - {catId}</Text>
     </View>
   );
 }
