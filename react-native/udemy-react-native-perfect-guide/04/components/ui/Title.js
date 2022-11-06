b154a3d5e8db58a9d42318871b68169825473c4b
Jinho Hyeon <neverlish@gmail.com>
2022-11-06 17:51:16 +0900

    05 호환 가능한 사용자 인터페이스 구축하기 (플랫폼 & 기기 크기에 맞추기) - 80 동적 너비 설정하기

--- react-native/udemy-react-native-perfect-guide/04/components/ui/Title.js
+++ react-native/udemy-react-native-perfect-guide/04/components/ui/Title.js
@@ -15,5 +15,6 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: 'white',
     padding: 12,
+    maxWidth: '80%',
   },
 });
--- react-native/udemy-react-native-perfect-guide/04/screens/GameScreen.js
+++ react-native/udemy-react-native-perfect-guide/04/screens/GameScreen.js
@@ -111,6 +111,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 12,
+    alignItems: 'center',
   },
   instructionText: {
     marginBottom: 12,
