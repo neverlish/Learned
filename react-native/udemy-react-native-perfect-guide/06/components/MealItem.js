de85586525232eaaa4b04f146d0ac38d844e4de4
Jinho Hyeon <neverlish@gmail.com>
2022-11-06 22:15:40 +0900

    06 - 100 이미지 추가하기 & 스타일링하기

--- react-native/udemy-react-native-perfect-guide/06/components/MealItem.js
+++ react-native/udemy-react-native-perfect-guide/06/components/MealItem.js
@@ -1,9 +1,56 @@
-import { Text, View } from "react-native";
+import { Image, Platform, Pressable, StyleSheet, Text, View } from "react-native";
 
-function MealItem({ title }) {
-  return <View>
-    <Text>{title}</Text>
-  </View>
+function MealItem({ title, imageUrl, duration, complexity, affordability }) {
+  return (
+    <View style={styles.mealItem}>
+      <Pressable>
+        <View>
+          <Image source={{ uri: imageUrl }} style={styles.image} />
+          <Text style={styles.title}>{title}</Text>
+        </View>
+        <View style={styles.details}>
+          <Text style={styles.detailItem}>{duration}m</Text>
+          <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+          <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
 }
 
 export default MealItem;
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: 'hidden',
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+    backgroundColor: 'white',
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 16,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  title: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+});
--- react-native/udemy-react-native-perfect-guide/06/screens/MealsOverviewScreen.js
+++ react-native/udemy-react-native-perfect-guide/06/screens/MealsOverviewScreen.js
@@ -10,7 +10,16 @@ function MealsOverviewScreen({ route }) {
   });
 
   function renderMealItem(itemData) {
-    return <MealItem title={itemData.item.title} />
+    const item = itemData.item;
+    
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    }
+    return <MealItem {...mealItemProps} />
   }
 
   return (
