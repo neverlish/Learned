a8932b460a1db04d5d9c05f17536e2780e6c0526
Jinho Hyeon <neverlish@gmail.com>
2022-11-06 18:47:42 +0900

    05 - 88 Platform API를 통해 특정 플랫폼용 코드 작성하기

--- react-native/udemy-react-native-perfect-guide/04/components/ui/Title.android.js
+++ react-native/udemy-react-native-perfect-guide/04/components/ui/Title.android.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, Platform } from 'react-native';
 
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -12,6 +12,8 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: 'white',
     textAlign: 'center',
+    // borderWidth: Platform.OS === 'android' ? 2 : 0,
+    // borderWidth: Platform.select({ ios: 0, android: 2 }),
     borderWidth: 2,
     borderColor: 'white',
     padding: 12,
---
+++ react-native/udemy-react-native-perfect-guide/04/components/ui/Title.ios.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, Platform } from 'react-native';
+
+function Title({ children }) {
+  return <Text style={styles.title}>{children}</Text>;
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 24,
+    color: 'white',
+    textAlign: 'center',
+    // borderWidth: Platform.OS === 'android' ? 2 : 0,
+    // borderWidth: Platform.select({ ios: 0, android: 2 }),
+    borderColor: 'white',
+    padding: 12,
+    maxWidth: '80%',
+  },
+});
