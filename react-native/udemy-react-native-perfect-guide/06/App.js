659de125a4667d05bf2f12c85f4d3003b8bbc4dd
Jinho Hyeon <neverlish@gmail.com>
2022-11-06 21:28:20 +0900

    06 - 094 React Navigation 패키지 시작하기

--- react-native/udemy-react-native-perfect-guide/06/App.js
+++ react-native/udemy-react-native-perfect-guide/06/App.js
@@ -1,12 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
