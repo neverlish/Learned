173b673f0365ba1b04b16e83d7ef69edb44ec180
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 01:26:22 +0900

    06 - 106 헤더 버튼 추가하기

--- react-native/udemy-react-native-perfect-guide/06/App.js
+++ react-native/udemy-react-native-perfect-guide/06/App.js
@@ -3,8 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
-import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -30,12 +30,12 @@ export default function App() {
           <Stack.Screen
             name='MealsOverview'
             component={MealsOverviewScreen}
-            options={({ route, navigation }) => {
-              const catId = route.params.categoryId;
-              return {
-                title: catId,
-              };
-            }}
+            // options={({ route, navigation }) => {
+            //   const catId = route.params.categoryId;
+            //   return {
+            //     title: catId,
+            //   };
+            // }}
           />
           <Stack.Screen 
             name='MealDetail'
--- react-native/udemy-react-native-perfect-guide/06/screens/MealDetailScreen.js
+++ react-native/udemy-react-native-perfect-guide/06/screens/MealDetailScreen.js
@@ -1,14 +1,27 @@
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useLayoutEffect } from "react";
+import { Button, Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import List from "../components/MealDetail/List";
 import Subtitle from "../components/MealDetail/Subtitle";
 import MealDetails from "../components/MealDetails";
 
 import { MEALS } from '../data/dummy-data';
 
-function MealDetailScreen({ route }) {
+function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  function headerButtonPressHandler() {
+    console.log('Pressed!');
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return <Button title='Tap me!' onPress={headerButtonPressHandler} />
+      }
+    });
+  }, [navigation, headerButtonPressHandler]);
 
   return <ScrollView style={styles.rootContainer}>
     <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
