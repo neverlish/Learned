91a23ac95305237d99336210c42ad7f9d09a3bc4
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 01:00:46 +0900

    06 - 105 음식 상세 화면 마무리

--- react-native/udemy-react-native-perfect-guide/06/components/MealDetails.js
+++ react-native/udemy-react-native-perfect-guide/06/components/MealDetails.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
 
-function MealDetails({ duration, complexity, affordability }) {
-  return <View style={styles.details}>
-    <Text style={styles.detailItem}>{duration}m</Text>
-    <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-    <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+function MealDetails({ duration, complexity, affordability, style, textStyle }) {
+  return <View style={[styles.details, style]}>
+    <Text style={[styles.detailItem, textStyle]}>{duration}m</Text>
+    <Text style={[styles.detailItem, textStyle]}>{complexity.toUpperCase()}</Text>
+    <Text style={[styles.detailItem, textStyle]}>{affordability.toUpperCase()}</Text>
   </View>
 }
 
--- react-native/udemy-react-native-perfect-guide/06/screens/MealDetailScreen.js
+++ react-native/udemy-react-native-perfect-guide/06/screens/MealDetailScreen.js
@@ -1,4 +1,6 @@
-import { Image, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import List from "../components/MealDetail/List";
+import Subtitle from "../components/MealDetail/Subtitle";
 import MealDetails from "../components/MealDetails";
 
 import { MEALS } from '../data/dummy-data';
@@ -8,23 +10,50 @@ function MealDetailScreen({ route }) {
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  return <View>
-    <Image source={{ uri: selectedMeal.imageUrl }} />
-    <Text>{selectedMeal.title}</Text>
+  return <ScrollView style={styles.rootContainer}>
+    <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+    <Text style={styles.title}>{selectedMeal.title}</Text>
     <MealDetails
       duration={selectedMeal.duration}
       affordability={selectedMeal.affordability}
-      complexity={selectedMeal.complexity} 
+      complexity={selectedMeal.complexity}
+      textStyle={styles.detailText}
     />
-    <Text>Ingredients</Text>
-    {selectedMeal.ingredients.map((ingredient) => (
-      <Text key={ingredient}>{ingredient}</Text>
-    ))}
-    <Text>Steps</Text>
-    {selectedMeal.steps.map((step) => (
-      <Text key={step}>{step}</Text>
-    ))}
-  </View>
+    <View style={styles.listOuterContainer}>
+      <View style={styles.listContainer}>
+        <Subtitle>Ingredients</Subtitle>
+        <List data={selectedMeal.ingredients} />
+        <Subtitle>Steps</Subtitle>
+        <List data={selectedMeal.steps} />
+      </View>
+    </View>
+  </ScrollView>
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32,
+  },
+  image: {
+    width: '100%',
+    height: 350,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    margin: 8,
+    textAlign: 'center',
+    color: 'white',
+  },
+  detailText: {
+    color: 'white',
+  },
+  listOuterContainer: {
+    alignItems: 'center',
+  },
+  listContainer: {
+    width: '80%',
+  },
+});
