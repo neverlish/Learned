1fa0d2bc543e94b33b1a146a537deb33e81b3fb2
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 08:56:37 +0900

    06 - 109 드로어 내비게이터 & 드로어 구성하기

--- react-native/udemy-react-native-perfect-guide/06-2/App.js
+++ react-native/udemy-react-native-perfect-guide/06-2/App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
@@ -8,9 +9,20 @@ const Drawer = createDrawerNavigator();
 
 export default function App() {
   return <NavigationContainer>
-    <Drawer.Navigator initialRouteName='User'>
-      <Drawer.Screen name='Welcome' component={WelcomeScreen} />
-      <Drawer.Screen name='User' component={UserScreen} />
+    <Drawer.Navigator screenOptions={{
+        headerStyle: { backgroundColor: '#3c0a6b' },
+        headerTintColor: 'white',  
+        drawerActiveBackgroundColor: '#f0e1ff',
+        drawerActiveTintColor: '#3c0a6b',
+        // drawerStyle: { backgroundColor: '#ccc' },
+      }} >
+      <Drawer.Screen name='Welcome' component={WelcomeScreen} options={{
+        drawerLabel: 'Welcome Screen',
+        drawerIcon: ({ color, size }) => <Ionicons name='home' color={color} size={size} />
+      }}/>
+      <Drawer.Screen name='User' component={UserScreen} options={{
+        drawerIcon: ({ color, size }) => <Ionicons name='person' color={color} size={size} />
+      }}/>
     </Drawer.Navigator>
   </NavigationContainer>;
 }
--- react-native/udemy-react-native-perfect-guide/06-2/screens/UserScreen.js
+++ react-native/udemy-react-native-perfect-guide/06-2/screens/UserScreen.js
@@ -1,11 +1,16 @@
 import { View, Text, Button, StyleSheet } from 'react-native';
 
-function UserScreen() {
+function UserScreen({ navigation }) {
+  function openDrawerHandler() {
+    navigation.toggleDrawer();
+  }
+
   return (
     <View style={styles.rootContainer}>
       <Text>
         This is the <Text style={styles.highlight}>"User"</Text> screen!
       </Text>
+      <Button title='Open Drawer' onPress={openDrawerHandler} />
     </View>
   );
 }
