1dc1ea4e85acdd13ed00f406866b449acb8d77c5
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 11:03:30 +0900

    07 - 116 콘텍스트로 앱 전반에 걸친 상태 관리하기

--- react-native/udemy-react-native-perfect-guide/06/store/context/favorites-context.js
+++ react-native/udemy-react-native-perfect-guide/06/store/context/favorites-context.js
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const FavoritesContext = createContext({
   ids: [],
@@ -7,7 +7,25 @@ export const FavoritesContext = createContext({
 });
 
 function FavoritesContextProvider({ children }) {
-  return <FavoritesContext.Provider>
+  const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+
+  function addFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  }
+
+  function removeFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => 
+      currentFavIds.filter(mealId => mealId !== id)
+    );
+  }
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite,
+    removeFavorite,
+  };
+
+  return <FavoritesContext.Provider value={value}>
     {children}
   </FavoritesContext.Provider>
 }
