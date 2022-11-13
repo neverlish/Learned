aa7649557563387af0d8421ed083f96ab4706ac0
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 13:27:51 +0900

    07 - 119 Redux & Redux 툴킷 시작하기

--- react-native/udemy-react-native-perfect-guide/06/App.js
+++ react-native/udemy-react-native-perfect-guide/06/App.js
@@ -4,11 +4,12 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { Provider } from 'react-redux';
 import CategoriesScreen from './screens/CategoriesScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
-import FavoritesContextProvider from './store/context/favorites-context';
+import { store } from './store/redux/store';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -39,7 +40,7 @@ export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <FavoritesContextProvider>
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -75,7 +76,7 @@ export default function App() {
             />
           </Stack.Navigator> 
         </NavigationContainer>
-      </FavoritesContextProvider>
+      </Provider>
     </>
   ); 
 }
