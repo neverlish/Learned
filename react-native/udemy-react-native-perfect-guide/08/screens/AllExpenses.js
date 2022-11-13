5d970672ab1ea6f2579d321ba06e8c2873d0ec5a
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 15:54:02 +0900

    08 - 130 더미 지출 데이터 추가하기

--- react-native/udemy-react-native-perfect-guide/08/screens/AllExpenses.js
+++ react-native/udemy-react-native-perfect-guide/08/screens/AllExpenses.js
@@ -1,7 +1,7 @@
-import { Text } from 'react-native';
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 
 function AllExpenses() {
-  return <Text>AllExpenses Screen</Text>;
+  return <ExpensesOutput expensesPeriod='Total' />;
 }
 
 export default AllExpenses;
--- react-native/udemy-react-native-perfect-guide/08/screens/RecentExpenses.js
+++ react-native/udemy-react-native-perfect-guide/08/screens/RecentExpenses.js
@@ -1,7 +1,7 @@
-import { Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 function RecentExpenses() {
-  return <Text>RecentExpenses Screen</Text>;
+  return <ExpensesOutput expensesPeriod='Last 7 Days' />;
 }
 
 export default RecentExpenses;
