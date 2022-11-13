f6a478ada4f5ec89c5b6a377a848a1a8e743ab8c
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 13:31:11 +0900

    07 - 120 Redux Slices로 작업하기

--- react-native/udemy-react-native-perfect-guide/06/store/redux/store.js
+++ react-native/udemy-react-native-perfect-guide/06/store/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import favoritesReducer from './favorites';
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    favoriteMeals: favoritesReducer,
+  },
 });
