56dd3b0bd59faa283076d34b6f9a75b4964eda69
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 17:24:26 +0900

    08 - 140 커스텀 버튼 추가하기

--- react-native/udemy-react-native-perfect-guide/08/screens/ManageExpense.js
+++ react-native/udemy-react-native-perfect-guide/08/screens/ManageExpense.js
@@ -1,5 +1,6 @@
 import { useLayoutEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
+import Button from '../components/UI/Button';
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
 
@@ -17,7 +18,23 @@ function ManageExpense({ route, navigation }) {
 
   }
 
+  function cancelHandler() {
+
+  }
+
+  function confirmHandler() {
+
+  }
+
   return <View style={styles.container}>
+    <View style={styles.buttons}>
+      <Button style={styles.button} mode='flat' onPress={cancelHandler}>
+        Cancel
+      </Button>
+      <Button style={styles.button} onPress={confirmHandler}>
+        {isEditing ? 'Update' : 'Add'}
+      </Button>
+    </View>
     {isEditing && (
       <View style={styles.deleteContainer}>
         <IconButton
@@ -37,6 +54,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
