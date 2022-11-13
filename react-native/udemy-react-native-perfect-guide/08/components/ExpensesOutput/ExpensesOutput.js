a0268e24e655425fe1f93e9726701574d2f48689
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 15:45:04 +0900

    08 - 129 지출에 관련된 컴포넌트 작업 계속해서 이어가기

--- react-native/udemy-react-native-perfect-guide/08/components/ExpensesOutput/ExpensesOutput.js
+++ react-native/udemy-react-native-perfect-guide/08/components/ExpensesOutput/ExpensesOutput.js
@@ -2,9 +2,9 @@ import { View } from "react-native";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 
-function ExpensesOutput({ expenses }) {
+function ExpensesOutput({ expenses, expensesPeriod }) {
   return <View>
-    <ExpensesSummary />
+    <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
     <ExpensesList />
   </View>
 }
--- react-native/udemy-react-native-perfect-guide/08/components/ExpensesOutput/ExpensesSummary.js
+++ react-native/udemy-react-native-perfect-guide/08/components/ExpensesOutput/ExpensesSummary.js
@@ -1,10 +1,14 @@
 import { Text, View } from "react-native";
 
-function ExpensesSummary() {
+function ExpensesSummary({ expenses, periodName }) {
+  const expensesSum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount;
+  }, 0);
+
   return (
     <View>
-      <Text>Last 7 Days</Text>
-      <Text>$177.95</Text>
+      <Text>{periodName}</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>
     </View>
   );
 }
