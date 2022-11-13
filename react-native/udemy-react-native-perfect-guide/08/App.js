ba85b7622ab85206b96d5c7f0444f5ad0006b3a6
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 14:17:55 +0900

    08 - 126 (React Navigation으로) 내비게이션 추가하기 & 내비게이션 구성하기

--- react-native/udemy-react-native-perfect-guide/08/App.js
+++ react-native/udemy-react-native-perfect-guide/08/App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import ManageExpense from './screens/ManageExpense';
+import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return <BottomTabs.Navigator>
+    <BottomTabs.Screen name='RecentExpenses' component={RecentExpenses} />
+    <BottomTabs.Screen name='AllExpenses' component={AllExpenses} />
+  </BottomTabs.Navigator>
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='ExpensesOverview' component={ExpensesOverview} />
+          <Stack.Screen name='ManageExpense' component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
