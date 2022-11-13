15675d201d25073280123007122b351fb4a86a51
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 13:11:01 +0900

    07 - 117 useContext로 생성된 콘텍스트 사용하기

--- react-native/udemy-react-native-perfect-guide/06/screens/MealDetailScreen.js
+++ react-native/udemy-react-native-perfect-guide/06/screens/MealDetailScreen.js
@@ -1,4 +1,4 @@
-import { useLayoutEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import IconButton from "../components/IconButton";
 import List from "../components/MealDetail/List";
@@ -6,27 +6,36 @@ import Subtitle from "../components/MealDetail/Subtitle";
 import MealDetails from "../components/MealDetails";
 
 import { MEALS } from '../data/dummy-data';
+import { FavoritesContext } from "../store/context/favorites-context";
 
 function MealDetailScreen({ route, navigation }) {
+  const favoriteMealsCtx = useContext(FavoritesContext);
+
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  function headerButtonPressHandler() {
-    console.log('Pressed!');
+  const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+
+  function changeFavoriteStatusHandler() {
+    if (mealIsFavorite) {
+      favoriteMealsCtx.removeFavorite(mealId);
+    } else {
+      favoriteMealsCtx.addFavorite(mealId);
+    }
   }
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
         return <IconButton
-          icon='star'
+          icon={mealIsFavorite ? 'star' : 'star-outline'}
           color='white'
-          onPress={headerButtonPressHandler} 
+          onPress={changeFavoriteStatusHandler} 
         />;
       }
     });
-  }, [navigation, headerButtonPressHandler]);
+  }, [navigation, changeFavoriteStatusHandler]);
 
   return <ScrollView style={styles.rootContainer}>
     <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
