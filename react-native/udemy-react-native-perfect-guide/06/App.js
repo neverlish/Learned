9413a8be81f832459b2d8df3ecbdb1e657170662
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 10:48:21 +0900

    06 - 112 하단 탭 & 앱 최종 마무리

--- react-native/udemy-react-native-perfect-guide/06/App.js
+++ react-native/udemy-react-native-perfect-guide/06/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import CategoriesScreen from './screens/CategoriesScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
@@ -15,11 +16,20 @@ function DrawerNavigator() {
   return <Drawer.Navigator screenOptions={{
     headerStyle: { backgroundColor: '#351401' },
     headerTintColor: 'white',
-    sceneContainerStyle: { backgroundColor: '#3f2f25'}
+    sceneContainerStyle: { backgroundColor: '#3f2f25' },
+    drawerContentStyle: { backgroundColor: '#351401' },
+    drawerInactiveTintColor: 'white',
+    drawerActiveTintColor: '#351401',
+    drawerActiveBackgroundColor: '#e4baa1',
   }}>
-    <Drawer.Screen name='Categories' component={CategoriesScreen} options={{
-      title: 'All Categories'
-    }} />
+    <Drawer.Screen 
+      name='Categories' 
+      component={CategoriesScreen} 
+      options={{
+        title: 'All Categories',
+        drawerIcon: ({ color, size }) => <Ionicons name='star' color={color} size={size} />
+      }}
+    />
     <Drawer.Screen name='Favorites' component={FavoritesScreen} />
   </Drawer.Navigator>;
 }
