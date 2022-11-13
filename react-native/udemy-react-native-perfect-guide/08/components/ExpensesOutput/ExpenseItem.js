6354471b84fc86c648fe4af121a9238a6d95e644
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 16:37:50 +0900

    08 - 134 날짜 형식 지정하기

--- react-native/udemy-react-native-perfect-guide/08/components/ExpensesOutput/ExpenseItem.js
+++ react-native/udemy-react-native-perfect-guide/08/components/ExpensesOutput/ExpenseItem.js
@@ -1,5 +1,6 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
+import { getFormattedDate } from "../../util/date";
 
 function ExpenseItem({ description, amount, date }) {
   return (
@@ -7,10 +8,10 @@ function ExpenseItem({ description, amount, date }) {
       <View style={styles.expenseItem}>
         <View>
           <Text style={[styles.textBase, styles.description]}>{description}</Text>
-          <Text style={styles.textBase}>{date.toString()}</Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
-          <Text style={styles.amount}>{amount}</Text>
+          <Text style={styles.amount}>{amount.toFixed(2)}</Text>
         </View>
       </View>
     </Pressable>
