cf5610d34b72f4263569ef8ac57a016070f301eb
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 17:22:56 +0900

    08 - 139 “삭제” 버튼 추가하기

--- react-native/udemy-react-native-perfect-guide/08/screens/ManageExpense.js
+++ react-native/udemy-react-native-perfect-guide/08/screens/ManageExpense.js
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from '../constants/styles';
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -11,7 +13,36 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  return <Text>ManageExpense Screen</Text>;
+  function deleteExpenseHandler() {
+
+  }
+
+  return <View style={styles.container}>
+    {isEditing && (
+      <View style={styles.deleteContainer}>
+        <IconButton
+          icon='trash'
+          color={GlobalStyles.colors.error500}
+          size={36} onPress={deleteExpenseHandler} 
+        />
+      </View>
+    )}
+  </View>;
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+});
