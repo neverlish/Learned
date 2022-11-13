e4fd63562b75c1e2b2391a0b71169038ba0ed676
Jinho Hyeon <neverlish@gmail.com>
2022-11-13 16:22:39 +0900

    08 - 133 지출 리스트 아이템

--- react-native/udemy-react-native-perfect-guide/08/components/ExpensesOutput/ExpensesList.js
+++ react-native/udemy-react-native-perfect-guide/08/components/ExpensesOutput/ExpensesList.js
@@ -1,7 +1,8 @@
 import { FlatList, Text } from "react-native";
+import ExpenseItem from "./ExpenseItem";
 
 function renderExpenseItem(itemData) {
-  return <Text>{itemData.item.description}</Text>
+  return <ExpenseItem {...itemData.item} />
 }
 
 function ExpensesList({ expenses }) {
--- react-native/udemy-react-native-perfect-guide/08/components/ExpensesOutput/ExpensesOutput.js
+++ react-native/udemy-react-native-perfect-guide/08/components/ExpensesOutput/ExpensesOutput.js
@@ -48,7 +48,9 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700,
   },
 });
