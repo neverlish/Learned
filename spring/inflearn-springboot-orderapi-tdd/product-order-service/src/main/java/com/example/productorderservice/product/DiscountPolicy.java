33949fb4a7fded8cd8b3601cee969b0f503cd7bf
Jinho Hyeon <neverlish@gmail.com>
2023-04-11 20:36:31 +0900

    5 상품 주문 API 개발 - 1 POJO 상품 주문 API 개발

--- spring/inflearn-springboot-orderapi-tdd/product-order-service/src/main/java/com/example/productorderservice/product/DiscountPolicy.java
+++ spring/inflearn-springboot-orderapi-tdd/product-order-service/src/main/java/com/example/productorderservice/product/DiscountPolicy.java
@@ -1,5 +1,5 @@
 package com.example.productorderservice.product;
 
-enum DiscountPolicy {
+public enum DiscountPolicy {
     NONE,
 }
--- spring/inflearn-springboot-orderapi-tdd/product-order-service/src/main/java/com/example/productorderservice/product/ProductRepository.java
+++ spring/inflearn-springboot-orderapi-tdd/product-order-service/src/main/java/com/example/productorderservice/product/ProductRepository.java
@@ -2,5 +2,5 @@ package com.example.productorderservice.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
