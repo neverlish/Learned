eedd9e317e3a1212433e7bdc1f1932014de318b7
Jinho Hyeon <neverlish@gmail.com>
2023-04-11 21:09:11 +0900

    5 - 3 API 테스트로 전환하기

--- spring/inflearn-springboot-orderapi-tdd/product-order-service/src/main/java/com/example/productorderservice/order/OrderService.java
+++ spring/inflearn-springboot-orderapi-tdd/product-order-service/src/main/java/com/example/productorderservice/order/OrderService.java
@@ -1,9 +1,16 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/orders")
 class OrderService {
     private final OrderPort orderPort;
 
@@ -11,11 +18,14 @@ class OrderService {
         this.orderPort = orderPort;
     }
 
-    public void createOrder(CreateOrderRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> createOrder(@RequestBody CreateOrderRequest request) {
         final Product product = orderPort.getProductById(request.productId());
 
         final Order order = new Order(product, request.quantity());
 
         orderPort.save(order);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
