dc9be2cd2ef1f86213117040e6ba4628f5ad9a34
Jinho Hyeon <neverlish@gmail.com>
2023-04-11 17:07:50 +0900

    2 - 5 JPA 적용하기

--- spring/inflearn-springboot-orderapi-tdd/product-order-service/src/main/java/com/example/productorderservice/product/ProductRepository.java
+++ spring/inflearn-springboot-orderapi-tdd/product-order-service/src/main/java/com/example/productorderservice/product/ProductRepository.java
@@ -1,18 +1,6 @@
 package com.example.productorderservice.product;
 
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Component
-class ProductRepository {
-    private Map<Long, Product> persistence = new HashMap<>();
-    private Long sequence = 0L;
-
-
-    public void save(final Product product) {
-        product.assignId(++sequence);
-        persistence.put(product.getId(), product);
-    }
+interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- spring/inflearn-springboot-orderapi-tdd/product-order-service/src/main/java/com/example/productorderservice/product/ProductService.java
+++ spring/inflearn-springboot-orderapi-tdd/product-order-service/src/main/java/com/example/productorderservice/product/ProductService.java
@@ -2,7 +2,7 @@ package com.example.productorderservice.product;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +18,7 @@ class ProductService {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
         productPort.save(product);
