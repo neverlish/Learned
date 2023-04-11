37de1162d425019c4c84ede7fec139376ed718c7
Jinho Hyeon <neverlish@gmail.com>
2023-04-11 21:19:12 +0900

    6 주문 결제 API 개발 - 1 POJO 주문 결제 기능 구현하기

---
+++ spring/inflearn-springboot-orderapi-tdd/product-order-service/src/test/java/com/example/productorderservice/payment/PaymentServiceTest.java
@@ -0,0 +1,10 @@
+package com.example.productorderservice.payment;
+
+import org.junit.jupiter.api.Test;
+
+public class PaymentServiceTest {
+    @Test
+    void 상품주문() {
+        paymentService.payment(request);
+    }
+}
