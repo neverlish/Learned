722165810f542377778d742427d7fc83922ce89f
Jinho Hyeon <neverlish@gmail.com>
2023-04-14 15:22:51 +0900

    02 - 02 Hello 서블릿

--- spring/inflearn-kimyounghan-springmvc1/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ spring/inflearn-kimyounghan-springmvc1/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
