e5a5686a1e296c186853005d764687d2a49b3339
Jinho Hyeon <neverlish@gmail.com>
2023-04-28 11:23:00 +0900

    2 - 03 Collection For

---
+++ dart/nomadcoders-start-dart/2-03.dart
@@ -0,0 +1,10 @@
+void main() {
+  var oldFriends = ['nico', 'lynn'];
+  var newFriends = [
+    'lewis',
+    'ralph',
+    'darren',
+    for (var friend in oldFriends) "💖 $friend",
+  ];
+  print(newFriends);
+}
