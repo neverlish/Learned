02b21691f83577e40845827fa67bc0b1a6160ca1
Jinho Hyeon <neverlish@gmail.com>
2023-05-01 10:51:18 +0900

    3 - 07 Icons and Transforms

--- flutter/nomadcoders-flutter-webtoon/ui/lib/main.dart
+++ flutter/nomadcoders-flutter-webtoon/ui/lib/main.dart
@@ -114,6 +114,7 @@ class App extends StatelessWidget {
                 height: 20,
               ),
               Container(
+                clipBehavior: Clip.hardEdge,
                 decoration: BoxDecoration(
                   color: const Color(0xFF1F2123),
                   borderRadius: BorderRadius.circular(25),
@@ -121,6 +122,7 @@ class App extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.all(30),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -158,6 +160,17 @@ class App extends StatelessWidget {
                             ],
                           ),
                         ],
+                      ),
+                      Transform.scale(
+                        scale: 2.2,
+                        child: Transform.translate(
+                          offset: const Offset(8, 15),
+                          child: const Icon(
+                            Icons.euro_rounded,
+                            color: Colors.white,
+                            size: 98,
+                          ),
+                        ),
                       ),
                     ],
                   ),
