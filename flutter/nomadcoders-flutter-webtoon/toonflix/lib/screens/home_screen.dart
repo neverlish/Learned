5026fdc4c336dbe274812fcc32a0d152d4f186c6
Jinho Hyeon <neverlish@gmail.com>
2023-05-01 15:14:56 +0900

    6 - 01 AppBar

---
+++ flutter/nomadcoders-flutter-webtoon/toonflix/lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          "어늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
