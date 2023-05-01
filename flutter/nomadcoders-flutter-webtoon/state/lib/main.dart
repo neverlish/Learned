96a54be7604fa1ce2b2c99415cc8357860c1b087
Jinho Hyeon <neverlish@gmail.com>
2023-05-01 11:48:24 +0900

    4 - 02 Recap

--- flutter/nomadcoders-flutter-webtoon/state/lib/main.dart
+++ flutter/nomadcoders-flutter-webtoon/state/lib/main.dart
@@ -12,11 +12,11 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
+  List<int> numbers = [];
 
   void onClicked() {
     setState(() {
-      counter = counter + 1;
+      numbers.add(numbers.length);
     });
   }
 
@@ -35,12 +35,7 @@ class _AppState extends State<App> {
                   fontSize: 30,
                 ),
               ),
-              Text(
-                '$counter',
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-              ),
+              for (var n in numbers) Text('$n'),
               IconButton(
                 onPressed: onClicked,
                 icon: const Icon(Icons.add_box_rounded),
