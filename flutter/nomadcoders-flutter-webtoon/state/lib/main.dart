f5fc6140d12177a1a62bb750f76524c554f95f5b
Jinho Hyeon <neverlish@gmail.com>
2023-05-01 11:36:20 +0900

    4 STATEFUL WIDGETS - 00 State

---
+++ flutter/nomadcoders-flutter-webtoon/state/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    counter = counter + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              IconButton(
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+                iconSize: 40,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
