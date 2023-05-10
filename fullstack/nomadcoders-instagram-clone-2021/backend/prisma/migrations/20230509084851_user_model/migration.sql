f6191d8fb2019bcceea5cc638dd0cd4015df6025
Jinho Hyeon <neverlish@gmail.com>
2023-05-10 16:48:26 +0900

    04 - 14 File Upload part One

--- fullstack/nomadcoders-instagram-clone-2021/backend/prisma/migrations/20230509084851_user_model/migration.sql
+++ fullstack/nomadcoders-instagram-clone-2021/backend/prisma/migrations/20230509084851_user_model/migration.sql
@@ -4,9 +4,6 @@
   - You are about to drop the `Movie` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropTable
-DROP TABLE "Movie";
-
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
--- fullstack/nomadcoders-instagram-clone-2021/backend/schema.js
+++ fullstack/nomadcoders-instagram-clone-2021/backend/schema.js
@@ -1,13 +1,8 @@
-import { makeExecutableSchema } from '@graphql-tools/schema'
-import { loadFilesSync } from '@graphql-tools/load-files'
-import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge'
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
 
-const typeDefs = mergeTypeDefs(loadedTypes);
-const resolvers = mergeResolvers(loadedResolvers);
-
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-export default schema;
+export const typeDefs = mergeTypeDefs(loadedTypes);
+export const resolvers = mergeResolvers(loadedResolvers);
