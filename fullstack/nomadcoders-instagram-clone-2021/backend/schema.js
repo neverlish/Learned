b916ea290ae16e6bc1bec9ce55392a3232624ff6
Jinho Hyeon <neverlish@gmail.com>
2023-05-10 15:46:52 +0900

    04 - 06 Divide and Conquer

--- fullstack/nomadcoders-instagram-clone-2021/backend/schema.js
+++ fullstack/nomadcoders-instagram-clone-2021/backend/schema.js
@@ -3,9 +3,7 @@ import { loadFilesSync } from '@graphql-tools/load-files'
 import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge'
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadedResolvers = loadFilesSync(
-  `${__dirname}/**/*.{queries,mutations}.js`
-);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
 
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadedResolvers);
