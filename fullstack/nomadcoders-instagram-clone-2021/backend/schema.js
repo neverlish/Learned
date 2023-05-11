4abb368aa091e66efa635db39ee6478bcbc1856b
Jinho Hyeon <neverlish@gmail.com>
2023-05-11 16:51:19 +0900

    05 TS SETUP - 00 TS Setup part One

--- fullstack/nomadcoders-instagram-clone-2021/backend/schema.js
+++
@@ -1,8 +0,0 @@
-import { loadFilesSync } from '@graphql-tools/load-files';
-import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
-
-const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
-
-export const typeDefs = mergeTypeDefs(loadedTypes);
-export const resolvers = mergeResolvers(loadedResolvers);
--- fullstack/nomadcoders-instagram-clone-2021/backend/src/users/users.utils.ts
+++ fullstack/nomadcoders-instagram-clone-2021/backend/src/users/users.utils.ts
@@ -6,13 +6,15 @@ export const getUser = async (token) => {
     if (!token) {
       return null;
     }
-    const { id } = await jwt.verify(token, process.env.SECRET_KEY);
-    const user = await client.user.findUnique({ where: { id } });
-    if (user) {
-      return user;
-    } else {
-      return null;
+    const verifiedToken: any = await jwt.verify(token, process.env.SECRET_KEY);
+    if ("id" in verifiedToken) {
+      const user = await client.user.findUnique({ where: { id: verifiedToken["id"] } });
+      if (user) {
+        return user;  
+      }
     }
+    
+    return null;
   } catch {
     return null;
   }
