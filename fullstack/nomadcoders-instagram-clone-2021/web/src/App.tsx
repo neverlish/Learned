06e935dbf144c9228fba864b4d529329e2955b32
Jinho Hyeon <neverlish@gmail.com>
2023-05-15 22:33:08 +0900

    09 - 04 Router Setup part Two

--- fullstack/nomadcoders-instagram-clone-2021/web/src/App.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/web/src/App.tsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+
 function App() {
+  const isLoggedIn = true;
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          {isLoggedIn ? <Home /> : <Login />}
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
