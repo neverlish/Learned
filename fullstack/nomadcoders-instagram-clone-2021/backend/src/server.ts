38cd367b009a21cbcfa4b4c7d8517aca2b6d6428
Jinho Hyeon <neverlish@gmail.com>
2023-05-15 17:35:10 +0900

    07 - 08 Subscriptions Setup part Two

--- fullstack/nomadcoders-instagram-clone-2021/backend/src/server.ts
+++ fullstack/nomadcoders-instagram-clone-2021/backend/src/server.ts
@@ -1,29 +1,32 @@
 require("dotenv").config();
+import http from "http";
 import express from "express";
 import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
 import client from "./client";
-import pubsub from "./pubsub";
 
 const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
-    return {
-      loggedInUser: await getUser(req.headers.token),
-      client,
-    };
+    if (req) {
+      return {
+        loggedInUser: await getUser(req.headers.token),
+        client,
+      };
+    }
   },
 });
 
 const app = express();
-apollo.installSubscriptionHandlers(app);
 app.use(logger("tiny"));
 apollo.applyMiddleware({ app });
 app.use("/static", express.static("uploads"));
-app.listen({ port: PORT }, () => {
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+httpServer.listen(PORT, () => {
   console.log(`🚀Server is running on http://localhost:${PORT} ✅`);
 });
