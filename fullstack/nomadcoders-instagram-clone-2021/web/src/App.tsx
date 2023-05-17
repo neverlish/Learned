0e9954a1e6de5544e5a06f90308dd63d1af5d486
Jinho Hyeon <neverlish@gmail.com>
2023-05-17 15:18:17 +0900

    12 FEED - 00 Header and Layout

--- fullstack/nomadcoders-instagram-clone-2021/web/src/App.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/web/src/App.tsx
@@ -9,6 +9,7 @@ import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 import SignUp from "./screens/SignUp";
 import routes from "./routes";
 import { HelmetProvider } from "react-helmet-async";
+import Layout from "./components/Layout";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
@@ -21,7 +22,13 @@ function App() {
           <Router>
             <Switch>
               <Route path={routes.home} exact>
-                {isLoggedIn ? <Home /> : <Login />}
+                {isLoggedIn ? (
+                  <Layout>
+                    <Home />
+                  </Layout>
+                ) : (
+                  <Login />
+                )}
               </Route>
               {!isLoggedIn ? (
                 <Route path={routes.signUp}>
