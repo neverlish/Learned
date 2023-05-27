054ade85005eb67f0524a1db024d3dc34e98cb5a
Jinho Hyeon <neverlish@gmail.com>
2023-05-27 12:28:05 +0900

    12 - 13 Parsing Hashtags

--- fullstack/nomadcoders-instagram-clone-2021/web/src/components/feed/Comment.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/web/src/components/feed/Comment.tsx
@@ -1,16 +1,35 @@
+import sanitizeHtml from "sanitize-html";
 import styled from "styled-components";
 import { FatText } from "../shared";
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 function Comment({ author, payload }: { author: string, payload: string }) {
+  const cleanedPayload = sanitizeHtml(
+    payload.replace(/#[\w]+/g, "<mark>$&</mark>"),
+    {
+      allowedTags: ["mark"],
+    }
+  );
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload,
+        }}
+      />
     </CommentContainer>
   );
 }
