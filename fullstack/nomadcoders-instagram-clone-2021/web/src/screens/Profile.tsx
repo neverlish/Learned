cf41ba4233e7b4543077b8f50382ea1f8af2896a
Jinho Hyeon <neverlish@gmail.com>
2023-05-28 12:14:12 +0900

    13 - 04 Follow Unfollow part Two

--- fullstack/nomadcoders-instagram-clone-2021/web/src/screens/Profile.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/web/src/screens/Profile.tsx
@@ -1,6 +1,29 @@
 import { gql, useQuery } from "@apollo/client";
+import { faHeart, faComment } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import Button from "../components/auth/Button";
+import PageTitle from "../components/PageTitle";
+import { FatText } from "../components/shared";
 import { PHOTO_FRAGMENT } from "../fragments";
+import { seeProfile, seeProfile_seeProfile } from "../__generated/seeProfile";
+
+const FOLLOW_USER_MUTATION = gql`
+  mutation followUser($username: String!) {
+    followUser(username: $username) {
+      ok
+    }
+  }
+`;
+
+const UNFOLLOW_USER_MUTATION = gql`
+  mutation unfollowUser($username: String!) {
+    unfollowUser(username: $username) {
+      ok
+    }
+  }
+`;
 
 const SEE_PROFILE_QUERY = gql`
   query seeProfile($username: String!) {
@@ -21,16 +44,160 @@ const SEE_PROFILE_QUERY = gql`
   }
   ${PHOTO_FRAGMENT}
 `;
+const Header = styled.div`
+  display: flex;
+`;
+const Avatar = styled.img`
+  margin-left: 50px;
+  height: 160px;
+  width: 160px;
+  border-radius: 50%;
+  margin-right: 150px;
+  background-color: #2c2c2c;
+`;
+const Column = styled.div``;
+const Username = styled.h3`
+  font-size: 28px;
+  font-weight: 400;
+`;
+const Row = styled.div`
+  margin-bottom: 20px;
+  font-size: 16px;
+`;
+const List = styled.ul`
+  display: flex;
+`;
+const Item = styled.li`
+  margin-right: 20px;
+`;
+const Value = styled(FatText)`
+  font-size: 18px;
+`;
+const Name = styled(FatText)`
+  font-size: 20px;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-auto-rows: 290px;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 30px;
+  margin-top: 50px;
+`;
+
+const Photo = styled.div<{ bg: string }>`
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  position: relative;
+`;
+
+const Icons = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  opacity: 0;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const Icon = styled.span`
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  margin: 0px 5px;
+  svg {
+    font-size: 14px;
+    margin-right: 5px;
+  }
+`;
+
+const ProfileBtn = styled(Button).attrs({
+  as: "span",
+})`
+  margin-left: 10px;
+  margin-top: 0px;
+`;
 
 function Profile() {
   const { username } = useParams<{ username: string }>();
-  const { data } = useQuery(SEE_PROFILE_QUERY, {
+  const { data, loading } = useQuery<seeProfile>(SEE_PROFILE_QUERY, {
     variables: {
       username,
     },
   });
-  console.log(data);
-  return "Profile";
+  const getButton = (seeProfile: seeProfile_seeProfile) => {
+    const { isMe, isFollowing } = seeProfile;
+    if (isMe) {
+      return <ProfileBtn>Edit Profile</ProfileBtn>;
+    }
+    if (isFollowing) {
+      return <ProfileBtn>Unfollow</ProfileBtn>;
+    } else {
+      return <ProfileBtn>Follow</ProfileBtn>;
+    }
+  };
+  return (
+    <div>
+      <PageTitle
+        title={
+          loading ? "Loading..." : `${data?.seeProfile?.username}'s Profile`
+        }
+      />
+      <Header>
+        <Avatar src={data?.seeProfile?.avatar} />
+        <Column>
+          <Row>
+            <Username>{data?.seeProfile?.username}</Username>
+            {data?.seeProfile ? getButton(data.seeProfile) : null}
+          </Row>
+          <Row>
+            <List>
+              <Item>
+                <span>
+                  <Value>{data?.seeProfile?.totalFollowers}</Value> followers
+                </span>
+              </Item>
+              <Item>
+                <span>
+                  <Value>{data?.seeProfile?.totalFollowing}</Value> following
+                </span>
+              </Item>
+            </List>
+          </Row>
+          <Row>
+            <Name>
+              {data?.seeProfile?.firstName}
+              {"  "}
+              {data?.seeProfile?.lastName}
+            </Name>
+          </Row>
+          <Row>{data?.seeProfile?.bio}</Row>
+        </Column>
+      </Header>
+      <Grid>
+        {data?.seeProfile?.photos?.map((photo) => (
+          <Photo key={photo!.id} bg={photo!.file}>
+            <Icons>
+              <Icon>
+                <FontAwesomeIcon icon={faHeart} />
+                {photo!.likes}
+              </Icon>
+              <Icon>
+                <FontAwesomeIcon icon={faComment} />
+                {photo!.commentNumber}
+              </Icon>
+            </Icons>
+          </Photo>
+        ))}
+      </Grid>
+    </div>
+  );
 }
 
 export default Profile;
