60b4c46e2e6dcc59c72fb3ab44f6b767f54cb58f
Jinho Hyeon <neverlish@gmail.com>
2023-05-29 23:58:43 +0900

    14 - 06 AppLoading

--- fullstack/nomadcoders-instagram-clone-2021/app/App.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/app/App.tsx
@@ -1,7 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = async () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    await Promise.all(fontPromises);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
