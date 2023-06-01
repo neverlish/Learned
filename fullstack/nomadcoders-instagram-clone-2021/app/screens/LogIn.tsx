67b93b211e6ff60f3217cfe4e936c876572f1a80
Jinho Hyeon <neverlish@gmail.com>
2023-06-01 23:08:52 +0900

    15 - 01 Moving Through Screens

--- fullstack/nomadcoders-instagram-clone-2021/app/screens/LogIn.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/app/screens/LogIn.tsx
@@ -1,10 +1,14 @@
+import { NavigationProp } from "@react-navigation/native";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export default function Login() {
+export default function Login({ navigation }: { navigation: NavigationProp<{ CreateAccount: undefined }> }) {
   return (
     <View>
       <Text>Login</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
+        <Text>Go to Create Account</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- fullstack/nomadcoders-instagram-clone-2021/app/screens/Welcome.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/app/screens/Welcome.tsx
@@ -1,10 +1,21 @@
+import { NavigationProp } from "@react-navigation/native";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export default function Welcome() {
+export default function Welcome({ navigation }: { navigation: NavigationProp<{ CreateAccount: undefined, LogIn: undefined }>} ) {
   return (
     <View>
       <Text>Welcome</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
+        <View>
+          <Text>Go to Create Account</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("LogIn")}>
+        <View>
+          <Text>Go to Log In</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
