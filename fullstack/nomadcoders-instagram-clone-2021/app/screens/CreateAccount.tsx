6ed06f85f4ace67207797ff63b471be21b18c683
Jinho Hyeon <neverlish@gmail.com>
2023-06-01 23:55:06 +0900

    15 - 07 Natural Forms part One

--- fullstack/nomadcoders-instagram-clone-2021/app/screens/CreateAccount.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/app/screens/CreateAccount.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { RefObject, useRef } from "react";
 import { TextInput } from "react-native";
 import AuthButton from "../components/auth/AuthButton";
 import AuthLayout from "../components/auth/AuthLayout";
 
 export default function CreateAccount() {
+  const lastNameRef = useRef(null);
+  const usernameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const onNext = (nextOne: RefObject<TextInput>) => {
+    nextOne?.current?.focus();
+  };
+  const onDone = () => {
+    alert("done!");
+  };
   return (
     <AuthLayout>
       <TextInput
@@ -11,32 +22,41 @@ export default function CreateAccount() {
         placeholderTextColor="gray"
         returnKeyType="next"
         style={{ backgroundColor: "white", width: "100%" }}
+        onSubmitEditing={() => onNext(lastNameRef)}
       />
       <TextInput
+        ref={lastNameRef}
         placeholder="Last Name"
         placeholderTextColor="gray"
         returnKeyType="next"
         style={{ backgroundColor: "white", width: "100%" }}
+        onSubmitEditing={() => onNext(usernameRef)}
       />
       <TextInput
+        ref={usernameRef}
         placeholder="Username"
         placeholderTextColor="gray"
         returnKeyType="next"
         style={{ backgroundColor: "white", width: "100%" }}
+        onSubmitEditing={() => onNext(emailRef)}
       />
       <TextInput
+        ref={emailRef}
         placeholder="Email"
         placeholderTextColor="gray"
         keyboardType="email-address"
         returnKeyType="next"
         style={{ backgroundColor: "white", width: "100%" }}
+        onSubmitEditing={() => onNext(passwordRef)}
       />
       <TextInput
+        ref={passwordRef}
         placeholder="Password"
         placeholderTextColor="gray"
         secureTextEntry
         returnKeyType="done"
         style={{ backgroundColor: "white", width: "100%" }}
+        onSubmitEditing={onDone}
       />
       <AuthButton text="Create Account" disabled={true} onPress={() => null} />
     </AuthLayout>
