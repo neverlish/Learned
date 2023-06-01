e9abac2d2bb3601c3ad253c5476e0878dc7d5db1
Jinho Hyeon <neverlish@gmail.com>
2023-06-01 22:30:40 +0900

    15 AUTHENTICATION - 00 Your First Navigation

--- fullstack/nomadcoders-instagram-clone-2021/app/App.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/app/App.tsx
@@ -1,10 +1,10 @@
 import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
 import AppLoading from "expo-app-loading";
-import * as Font from "expo-font";
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
 import { Asset } from "expo-asset";
-import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import React, { useState } from "react";
+import LoggedOutNav from "./navigators/LoggedOutNav";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -29,18 +29,8 @@ export default function App() {
     );
   }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <LoggedOutNav />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
