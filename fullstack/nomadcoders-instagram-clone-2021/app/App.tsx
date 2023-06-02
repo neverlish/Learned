1a23fbe6776df74d8e45a78e276169362e7fc342
Jinho Hyeon <neverlish@gmail.com>
2023-06-02 12:49:16 +0900

    15 - 15 AsyncStorage part Two

--- fullstack/nomadcoders-instagram-clone-2021/app/App.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/app/App.tsx
@@ -1,11 +1,12 @@
 import { ApolloProvider, useReactiveVar } from "@apollo/client";
 import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationContainer } from "@react-navigation/native";
 import AppLoading from "expo-app-loading";
 import { Asset } from "expo-asset";
 import * as Font from "expo-font";
 import React, { useState } from "react";
-import client, { isLoggedInVar } from "./apollo";
+import client, { isLoggedInVar, tokenVar } from "./apollo";
 import LoggedInNav from "./navigators/LoggedInNav";
 import LoggedOutNav from "./navigators/LoggedOutNav";
 
@@ -13,15 +14,22 @@ export default function App() {
   const [loading, setLoading] = useState(true);
   const onFinish = () => setLoading(false);
   const isLoggedIn = useReactiveVar(isLoggedInVar);
-  const preload = async () => {
+  const preloadAssets = async () => {
     const fontsToLoad = [Ionicons.font];
     const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
     const imagesToLoad = [
       require("./assets/logo.png"),
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
     ];
     const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
     await Promise.all([...fontPromises, ...imagePromises]);
+  };
+  const preload = async () => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      isLoggedInVar(true);
+      tokenVar(token);
+    }
+    return preloadAssets();
   };
   if (loading) {
     return (
