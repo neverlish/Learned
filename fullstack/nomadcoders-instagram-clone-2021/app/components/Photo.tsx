6d67a2938d59c6e30c4b310c1d8211ee5c4e30ff
Jinho Hyeon <neverlish@gmail.com>
2023-06-02 15:59:40 +0900

    16 - 07 Photo part Two

--- fullstack/nomadcoders-instagram-clone-2021/app/components/Photo.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/app/components/Photo.tsx
@@ -1,13 +1,27 @@
-import React from "react";
-import { useWindowDimensions } from "react-native";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { Image, useWindowDimensions } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import styled from "styled-components/native";
-import { seeFeed_seeFeed } from "../__generated/seeFeed";
 
 const Container = styled.View``;
-const Header = styled.View``;
-const UserAvatar = styled.Image``;
+const Header = styled.TouchableOpacity`
+  padding: 10px;
+  flex-direction: row;
+  align-items: center;
+`;
+const UserAvatar = styled.Image`
+  margin-right: 10px;
+  width: 25px;
+  height: 25px;
+  border-radius: 12.5;
+`;
+
+import { seeFeed_seeFeed } from "../__generated/seeFeed";
+
 const Username = styled.Text`
   color: white;
+  font-weight: 600;
 `;
 const File = styled.Image``;
 const Actions = styled.View``;
@@ -21,17 +35,25 @@ const Likes = styled.Text`
 `;
 
 function Photo({ id, user, caption, file, isLiked, likes }: seeFeed_seeFeed) {
+  const navigation = useNavigation<NavigationProp<{ Profile: undefined }>>();
   const { width, height } = useWindowDimensions();
+  const [imageHeight, setImageHeight] = useState(height - 450);
+  useEffect(() => {
+    Image.getSize(file, (width, height) => {
+      setImageHeight(height / 3);
+    });
+  }, [file]);
   return (
     <Container>
-      <Header>
-        <UserAvatar source={"" as any} />
+      <Header onPress={() => navigation.navigate("Profile")}>
+        <UserAvatar resizeMode="cover" source={{ uri: user.avatar }} />
         <Username>{user.username}</Username>
       </Header>
       <File
+        resizeMode="cover"
         style={{
           width,
-          height: height - 500,
+          height: imageHeight,
         }}
         source={{ uri: file }}
       />
@@ -41,7 +63,9 @@ function Photo({ id, user, caption, file, isLiked, likes }: seeFeed_seeFeed) {
       </Actions>
       <Likes>{likes === 1 ? "1 like" : `${likes} likes`}</Likes>
       <Caption>
-        <Username>{user.username}</Username>
+        <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+          <Username>{user.username}</Username>
+        </TouchableOpacity>
         <CaptionText>{caption}</CaptionText>
       </Caption>
     </Container>
