c9cba9b0355ae13bb18dfbd3baa50d26b7bf2858
Jinho Hyeon <neverlish@gmail.com>
2023-06-03 23:36:33 +0900

    15 - 11 Infinite Scrolling part One

--- fullstack/nomadcoders-instagram-clone-2021/app/apollo.ts
+++ fullstack/nomadcoders-instagram-clone-2021/app/apollo.ts
@@ -5,6 +5,7 @@ import {
   makeVar,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { offsetLimitPagination } from "@apollo/client/utilities";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const isLoggedInVar = makeVar(false);
@@ -39,6 +40,14 @@ const authLink = setContext((_, { headers }) => {
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          seeFeed: offsetLimitPagination(),
+        },
+      },
+    },
+  }),
 });
 export default client;
