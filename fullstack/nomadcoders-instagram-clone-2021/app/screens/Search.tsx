bca8a06b7c4c5bebe5905d87f632176a7fce7e25
Jinho Hyeon <neverlish@gmail.com>
2023-06-04 10:31:30 +0900

    17 - 05 Search part Three

--- fullstack/nomadcoders-instagram-clone-2021/app/screens/Search.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/app/screens/Search.tsx
@@ -1,11 +1,11 @@
 import { gql, useLazyQuery } from "@apollo/client";
 import React, { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { Text, TextInput, View } from "react-native";
+import { FieldValues, useForm } from "react-hook-form";
+import { ActivityIndicator, Text, TextInput, View } from "react-native";
 import styled from "styled-components/native";
 import DismissKeyboard from "../components/DismissKeyboard";
 import { NavigationProp } from "@react-navigation/native";
-import { searchPhotos } from "../__generated/searchPhotos";
+import { searchPhotos, searchPhotosVariables } from "../__generated/searchPhotos";
 
 const SEARCH_PHOTOS = gql`
   query searchPhotos($keyword: String!) {
@@ -16,11 +16,29 @@ const SEARCH_PHOTOS = gql`
   }
 `;
 
+const MessageContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+const MessageText = styled.Text`
+  margin-top: 15px;
+  color: white;
+  font-weight: 600;
+`;
+
 const Input = styled.TextInput``;
 
 export default function Search({ navigation }: { navigation: NavigationProp<any> }) {
-  const { setValue, register } = useForm();
-  const [startQueryFn, { loading, data }] = useLazyQuery<searchPhotos>(SEARCH_PHOTOS);
+  const { setValue, register, watch, handleSubmit } = useForm();
+  const [startQueryFn, { loading, data, called }] = useLazyQuery<searchPhotos, searchPhotosVariables>(SEARCH_PHOTOS);
+  const onValid = ({ keyword }: FieldValues) => {
+    startQueryFn({
+      variables: {
+        keyword,
+      },
+    });
+  };
   const SearchBox = () => (
     <TextInput
       style={{ backgroundColor: "white" }}
@@ -31,25 +49,39 @@ export default function Search({ navigation }: { navigation: NavigationProp<any>
       returnKeyType="search"
       autoCorrect={false}
       onChangeText={(text) => setValue("keyword", text)}
+      onSubmitEditing={handleSubmit(onValid)}
     />
   );
   useEffect(() => {
     navigation.setOptions({
       headerTitle: SearchBox,
     });
-    register("keyword");
+    register("keyword", {
+      required: true,
+      minLength: 3,
+    });
   }, []);
+  console.log(data);
   return (
     <DismissKeyboard>
-      <View
-        style={{
-          backgroundColor: "black",
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Text style={{ color: "white" }}>Photo</Text>
+      <View style={{ flex: 1, backgroundColor: "black" }}>
+        {loading ? (
+          <MessageContainer>
+            <ActivityIndicator size="large" />
+            <MessageText>Searching...</MessageText>
+          </MessageContainer>
+        ) : null}
+        {!called ? (
+          <MessageContainer>
+            <MessageText>Search by keyword</MessageText>
+          </MessageContainer>
+        ) : null}
+        {data?.searchPhotos !== undefined &&
+        data?.searchPhotos?.length === 0 ? (
+          <MessageContainer>
+            <MessageText>Could not find anything.</MessageText>
+          </MessageContainer>
+        ) : null}
       </View>
     </DismissKeyboard>
   );
