802e4f37695bd9e8ea4d1bb102a03c5b15a5d5a6
Jinho Hyeon <neverlish@gmail.com>
2023-06-08 22:51:23 +0900

    18 - 07 Take Photo part Two

--- fullstack/nomadcoders-instagram-clone-2021/app/screens/TakePhoto.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/app/screens/TakePhoto.tsx
@@ -1,8 +1,9 @@
-import { Camera, CameraType } from "expo-camera";
+import { Camera, CameraType, FlashMode } from "expo-camera";
 import React, { useEffect, useState } from "react";
-
+import { Ionicons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
+import Slider from "@react-native-community/slider";
 
 const Container = styled.View`
   flex: 1;
@@ -11,8 +12,15 @@ const Container = styled.View`
 
 const Actions = styled.View`
   flex: 0.35;
+  padding: 0px 50px;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const ButtonsContainer = styled.View`
+  width: 100%;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 `;
 
@@ -24,8 +32,12 @@ const TakePhotoBtn = styled.TouchableOpacity`
   border-radius: 50px;
 `;
 
+const SliderContainer = styled.View``;
+
 export default function TakePhoto() {
   const [ok, setOk] = useState(false);
+  const [flashMode, setFlashMode] = useState(FlashMode.off);
+  const [zoom, setZoom] = useState(0);
   const [cameraType, setCameraType] = useState(CameraType.back);
   const getPermissions = async () => {
     const { granted } = await Camera.requestPermissionsAsync();
@@ -34,12 +46,44 @@ export default function TakePhoto() {
   useEffect(() => {
     getPermissions();
   }, []);
+  const onCameraSwitch = () => {
+    if (cameraType === CameraType.front) {
+      setCameraType(CameraType.back);
+    } else {
+      setCameraType(CameraType.front);
+    }
+  };
+  const onZoomValueChange = (e: number) => {
+    setZoom(e);
+  };
   return (
     <Container>
-      <Camera type={cameraType} style={{ flex: 1 }} />
+      <Camera type={cameraType} style={{ flex: 1 }} zoom={zoom} />
       <Actions>
-        <TakePhotoBtn></TakePhotoBtn>
-        <TouchableOpacity></TouchableOpacity>
+        <SliderContainer>
+          <Slider
+            style={{ width: 200, height: 20 }}
+            minimumValue={0}
+            maximumValue={1}
+            minimumTrackTintColor="#FFFFFF"
+            maximumTrackTintColor="rgba(255, 255, 255, 0.5)"
+            onValueChange={onZoomValueChange}
+          />
+        </SliderContainer>
+        <ButtonsContainer>
+          <TakePhotoBtn />
+          <TouchableOpacity onPress={onCameraSwitch}>
+            <Ionicons
+              size={30}
+              color="white"
+              name={
+                cameraType === CameraType.front
+                  ? "camera-reverse"
+                  : "camera"
+              }
+            />
+          </TouchableOpacity>
+        </ButtonsContainer>
       </Actions>
     </Container>
   );
