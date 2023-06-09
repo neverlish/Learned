15914878138b2c4aec8d5eac935bed6fa209323d
Jinho Hyeon <neverlish@gmail.com>
2023-06-10 08:28:46 +0900

    19 - 03 Room Screen part One

---
+++ fullstack/nomadcoders-instagram-clone-2021/app/components/rooms/RoomItem.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { colors } from "../../colors";
+import styled from "styled-components/native";
+import useMe from "../../hooks/useMe";
+import { NavigationProp, useNavigation } from "@react-navigation/core";
+import { RoomParts, RoomParts_users } from "../../__generated/RoomParts";
+
+const RoomContainer = styled.TouchableOpacity`
+  width: 100%;
+  padding: 15px 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Column = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Avatar = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  margin-right: 20px;
+`;
+const Data = styled.View``;
+const UnreadDot = styled.View`
+  width: 10px;
+  border-radius: 5px;
+  height: 10px;
+  background-color: ${colors.blue};
+`;
+const Username = styled.Text`
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+`;
+const UnreadText = styled.Text`
+  color: white;
+  margin-top: 2px;
+  font-weight: 500;
+`;
+
+export default function RoomItem({ users, unreadTotal, id }: RoomParts) {
+  const { data: meData } = useMe();
+  const navigation = useNavigation<NavigationProp<{Room: {id: number, talkingTo?: RoomParts_users | null}}>>();
+  const talkingTo = users?.find(
+    (user) => user?.username !== meData?.me?.username
+  );
+  const goToRoom = () =>
+    navigation.navigate("Room", {
+      id,
+      talkingTo,
+    });
+  return (
+    <RoomContainer onPress={goToRoom}>
+      <Column>
+        <Avatar source={{ uri: talkingTo?.avatar }} />
+        <Data>
+          <Username>{talkingTo?.username}</Username>
+          <UnreadText>
+            {unreadTotal} unread {unreadTotal === 1 ? "message" : "messages"}
+          </UnreadText>
+        </Data>
+      </Column>
+      <Column>{unreadTotal !== 0 ? <UnreadDot /> : null}</Column>
+    </RoomContainer>
+  );
+}
--- fullstack/nomadcoders-instagram-clone-2021/app/screens/Room.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/app/screens/Room.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { NavigationProp, RouteProp } from "@react-navigation/native";
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 
-export default function Room() {
+const ROOM_QUERY = gql`
+  query seeRoom($id: Int!) {
+    seeRoom(id: $id) {
+      messages {
+        id
+        payload
+        user {
+          username
+          avatar
+        }
+        read
+      }
+    }
+  }
+`;
+
+export default function Room({ route, navigation }: { route: RouteProp<any>, navigation: NavigationProp<any> }) {
+  const { data } = useQuery(ROOM_QUERY, {
+    variables: {
+      id: route?.params?.id,
+    },
+  });
+  useEffect(() => {
+    navigation.setOptions({
+      title: `${route?.params?.talkingTo?.username}`,
+    });
+  }, []);
   return (
     <View>
       <Text>Messages List</Text>
