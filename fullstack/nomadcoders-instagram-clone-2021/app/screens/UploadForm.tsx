3e50c20c3ebc716020acccde4e2272b58fb3be32
Jinho Hyeon <neverlish@gmail.com>
2023-06-09 23:44:07 +0900

    18 - 14 Upload Screen part Three

--- fullstack/nomadcoders-instagram-clone-2021/app/screens/UploadForm.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/app/screens/UploadForm.tsx
@@ -1,3 +1,5 @@
+import { gql, useMutation } from "@apollo/client";
+import { ReactNativeFile } from "apollo-upload-client";
 import React, { useEffect } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import { ActivityIndicator, Text, View } from "react-native";
@@ -6,6 +8,17 @@ import styled from "styled-components/native";
 import { colors } from "../colors";
 import DismissKeyboard from "../components/DismissKeyboard";
 import { NavigationProp, RouteProp } from "@react-navigation/native";
+import { FEED_PHOTO } from "../fragments";
+import { uploadPhoto, uploadPhotoVariables } from "../__generated/uploadPhoto";
+
+const UPLOAD_PHOTO_MUTATION = gql`
+  mutation uploadPhoto($file: Upload!, $caption: String) {
+    uploadPhoto(file: $file, caption: $caption) {
+      ...FeedPhoto
+    }
+  }
+  ${FEED_PHOTO}
+`;
 
 const Container = styled.View`
   flex: 1;
@@ -33,14 +46,11 @@ const HeaderRightText = styled.Text`
 `;
 
 export default function UploadForm({ route, navigation }: { route: RouteProp<any>, navigation: NavigationProp<any> }) {
+  const [uploadPhotoMutation, { loading, error }] = useMutation<uploadPhoto, uploadPhotoVariables>(
+    UPLOAD_PHOTO_MUTATION
+  );
   const HeaderRight = () => (
-    <TouchableOpacity
-      onPress={() =>
-        navigation.navigate("UploadForm", {
-          file: chosenPhoto,
-        })
-      }
-    >
+    <TouchableOpacity onPress={handleSubmit(onValid)}>
       <HeaderRightText>Next</HeaderRightText>
     </TouchableOpacity>
   );
@@ -53,11 +63,24 @@ export default function UploadForm({ route, navigation }: { route: RouteProp<any
   }, [register]);
   useEffect(() => {
     navigation.setOptions({
-      headerRight: HeaderRightLoading,
-      headerLeft: () => null,
+      headerRight: loading ? HeaderRightLoading : HeaderRight,
+      ...(loading && { headerLeft: () => null }),
     });
-  }, []);
-  const onValid = ({ caption }: FieldValues) => {};
+  }, [loading]);
+  const onValid = ({ caption }: FieldValues) => {
+    const file = new ReactNativeFile({
+      uri: route.params?.file,
+      name: `1.jpg`,
+      type: "image/jpeg",
+    });
+    uploadPhotoMutation({
+      variables: {
+        caption,
+        file,
+      },
+    });
+  };
+  console.log(error);
   return (
     <DismissKeyboard>
       <Container>
