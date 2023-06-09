ac57d1df7e4bd91d1d14a3530517122583409ad7
Jinho Hyeon <neverlish@gmail.com>
2023-06-10 08:07:43 +0900

    19 - 01 Rooms Screen part One

--- fullstack/nomadcoders-instagram-clone-2021/app/navigators/MessagesNav.tsx
+++ fullstack/nomadcoders-instagram-clone-2021/app/navigators/MessagesNav.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Room from "../screens/Room";
 import Rooms from "../screens/Rooms";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 
 export default function MessagesNav() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: "white",
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: "black",
+        },
+        headerBackImage: ({ tintColor }) => (
+          <Ionicons color={tintColor} name="chevron-down" size={28} />
+        ),
+      }}
+    >
       <Stack.Screen name="Rooms" component={Rooms} />
       <Stack.Screen name="Room" component={Room} />
     </Stack.Navigator>
