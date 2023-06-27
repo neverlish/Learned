887c08ad1748a0cb66def80938706a10e656b819
Jinho Hyeon <neverlish@gmail.com>
2023-06-27 16:42:21 +0900

    05 - 04 PUT API 만들기

---
+++ spring/wikibooks-springboot-core/src/main/java/com/springboot/hello/controller/PutController.java
@@ -0,0 +1,41 @@
+package com.springboot.hello.controller;
+
+import com.springboot.hello.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+    @PutMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PutMapping(value = "/member1")
+    public String postMemberDto1(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+
+    @PutMapping(value = "/member2")
+    public MemberDto postMemberDto2(@RequestBody MemberDto memberDto) {
+        return memberDto;
+    }
+
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto> postMember3(@RequestBody MemberDto memberDto) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDto);
+    }
+}
