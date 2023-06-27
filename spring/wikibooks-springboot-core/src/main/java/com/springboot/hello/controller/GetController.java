7d95133149c626a23381fb4a6479c95022f1c18e
Jinho Hyeon <neverlish@gmail.com>
2023-06-27 17:05:53 +0900

    05 - 07 로깅 라이브러리 - Logback

--- spring/wikibooks-springboot-core/src/main/java/com/springboot/hello/controller/GetController.java
+++ spring/wikibooks-springboot-core/src/main/java/com/springboot/hello/controller/GetController.java
@@ -1,6 +1,9 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.dto.MemberDto;
+import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -8,18 +11,24 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class GetController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     public String getHello() {
+        LOGGER.info("getHello 메서드가 호출되었습니다.");
         return "Hello World";
     }
 
     @GetMapping(value = "/name")
     public String getName() {
+        LOGGER.info("getName 메서드가 호출되었습니다.");
         return "Flature";
     }
 
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable) {
+        LOGGER.info("@PathVariable을 통해 들어온 값 : {}", variable);
         return variable;
     }
 
