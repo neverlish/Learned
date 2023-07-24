ad3cca9a05b4ccd5a327f1921c3864a0a6363fd6
Jinho Hyeon <neverlish@gmail.com>
2023-07-24 16:57:20 +0900

    2 - 장바구니 보여주기

--- spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HomeController.java
+++ spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HomeController.java
@@ -2,12 +2,26 @@ package com.greglturnquist.hackingspringbootreactive;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
 @Controller
 public class HomeController {
+
+    private ItemRepository itemRepository;
+    private CartRepository cartRepository;
+
+    public HomeController(ItemRepository itemRepository, CartRepository cartRepository) {
+        this.itemRepository = itemRepository;
+        this.cartRepository = cartRepository;
+    }
+
     @GetMapping
-    Mono<String> home() {
-        return Mono.just("home");
+    Mono<Rendering> home() {
+        return Mono.just(Rendering.view("home.html")
+                .modelAttribute("items", this.itemRepository.findAll())
+                .modelAttribute("cart", this.cartRepository.findById("My Cart")
+                        .defaultIfEmpty(new Cart("My Cart")))
+                .build());
     }
 }
