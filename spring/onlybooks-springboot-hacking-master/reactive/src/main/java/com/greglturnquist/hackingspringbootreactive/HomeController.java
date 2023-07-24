a7b5cfede635f1fca1ea7759764e0411125e5416
Jinho Hyeon <neverlish@gmail.com>
2023-07-24 15:50:18 +0900

    1 - 템플릿 적용

---
+++ spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HomeController.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.hackingspringbootreactive;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Mono;
+
+@Controller
+public class HomeController {
+    @GetMapping
+    Mono<String> home() {
+        return Mono.just("home");
+    }
+}
