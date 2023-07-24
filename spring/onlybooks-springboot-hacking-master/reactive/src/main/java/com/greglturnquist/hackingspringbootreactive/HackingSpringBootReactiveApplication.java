88b7fd919d7ff0e6482ba112d399d1feb6b74682
Jinho Hyeon <neverlish@gmail.com>
2023-07-24 15:28:18 +0900

    1 스프링 부트 웹 애플리케이션 만들기 - 스프링 부트로 이커머스 플랫폼 만들기

---
+++ spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HackingSpringBootReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.hackingspringbootreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackingSpringBootReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackingSpringBootReactiveApplication.class, args);
+	}
+
+}
---
+++ spring/onlybooks-springboot-hacking-master/reactive/src/test/java/com/greglturnquist/hackingspringbootreactive/HackingSpringBootReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.hackingspringbootreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackingSpringBootReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
