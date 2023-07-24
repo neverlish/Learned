d735c9eb56f5de5819aae14e2b4074c824ae3b90
Jinho Hyeon <neverlish@gmail.com>
2023-07-24 17:23:57 +0900

    2 - 쿼리문 자동 생성 메서드로 충분하지 않을 때

--- spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/ItemRepository.java
+++ spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/ItemRepository.java
@@ -1,8 +1,15 @@
 package com.greglturnquist.hackingspringbootreactive;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
 public interface ItemRepository extends ReactiveCrudRepository<Item, String> {
     Flux<Item> findByNameContaining(String partialName);
+
+    @Query("{'name': ?0, 'age': ?1}")
+    Flux<Item> findItemsForCustomerMonthlyReport(String name, int age);
+
+    @Query(sort = "{'age': -1}")
+    Flux<Item> findSortedStuffForWeeklyReport();
 }
