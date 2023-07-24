edc9bb1a8b7153dedce3b2746f8d0e50d993553a
Jinho Hyeon <neverlish@gmail.com>
2023-07-24 17:49:03 +0900

    2 - 평문형 연산

--- spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/InventoryService.java
+++ spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/InventoryService.java
@@ -2,15 +2,22 @@ package com.greglturnquist.hackingspringbootreactive;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.mongodb.core.ReactiveFluentMongoOperations;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import static org.springframework.data.mongodb.core.query.Criteria.byExample;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
 @Service
 public class InventoryService {
-    private  ItemRepository repository;
+    private ItemRepository repository;
+    private ReactiveFluentMongoOperations fluentOperations;
 
-    InventoryService(ItemRepository repository) {
+    InventoryService(ItemRepository repository, ReactiveFluentMongoOperations fluentOperations) {
         this.repository = repository;
+        this.fluentOperations = fluentOperations;
     }
 
     Flux<Item> search(String partialName, String partialDescription, boolean useAnd) {
@@ -49,5 +56,28 @@ public class InventoryService {
         Example<Item> probe = Example.of(item, matcher);
 
         return repository.findAll(probe);
+    }
+
+    Flux<Item> searchByFluentExample(String name, String description) {
+        return fluentOperations.query(Item.class)
+                .matching(query(where("TV tray").is(name).and("Smurf").is(description)))
+                .all();
+    }
+    // end::code-4[]
+
+    // tag::code-5[]
+    Flux<Item> searchByFluentExample(String name, String description, boolean useAnd) {
+        Item item = new Item(name, description, 0.0);
+
+        ExampleMatcher matcher = (useAnd //
+                ? ExampleMatcher.matchingAll() //
+                : ExampleMatcher.matchingAny()) //
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING) //
+                .withIgnoreCase() //
+                .withIgnorePaths("price");
+
+        return fluentOperations.query(Item.class) //
+                .matching(query(byExample(Example.of(item, matcher)))) //
+                .all();
     }
 }
