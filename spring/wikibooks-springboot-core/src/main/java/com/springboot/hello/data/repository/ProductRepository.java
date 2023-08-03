fa56b2c334e7db050664e7a8903e94c45b0f7cca
Jinho Hyeon <neverlish@gmail.com>
2023-08-03 15:55:32 +0900

    08 - 05 @Query 어노테이션 적용하기

--- spring/wikibooks-springboot-core/src/main/java/com/springboot/hello/data/repository/ProductRepository.java
+++ spring/wikibooks-springboot-core/src/main/java/com/springboot/hello/data/repository/ProductRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -59,4 +61,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByName(String name, Sort sort);
 
     Page<Product> findByName(String name, Pageable pageable);
+
+    @Query("SELECT p from Product as p WHERE p.name = ?1")
+    List<Product> findByName(String name);
+
+    @Query("SELECT p from Product as p WHERE p.name = :name")
+    List<Product> findByNameParam(@Param("name") String name);
+
+    @Query("SELECT p.name, p.price, p.stock from Product as p WHERE p.name = :name")
+    List<Product> findByNameParam2(@Param("name") String name);
 }
