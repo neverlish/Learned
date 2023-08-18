6dbcb92dbadbb33d07880fdaab16f4b92cac35c5
Jinho Hyeon <neverlish@gmail.com>
2023-08-18 14:26:01 +0900

    3 - 블록하운드를 사용한 블로킹 코드 검출

--- spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HackingSpringBootReactiveApplication.java
+++ spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HackingSpringBootReactiveApplication.java
@@ -2,11 +2,20 @@ package com.greglturnquist.hackingspringbootreactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class HackingSpringBootReactiveApplication {
 
 	public static void main(String[] args) {
+		BlockHound
+				.builder()
+				.allowBlockingCallsInside(
+						TemplateEngine.class.getCanonicalName(), "process"
+				)
+				.install();
+
 		SpringApplication.run(HackingSpringBootReactiveApplication.class, args);
 	}
 
