d0100a5c22c345730702886177d30c9251d12bc6
Jinho Hyeon <neverlish@gmail.com>
2023-08-20 08:50:41 +0900

    0 - 영화 모델 만들고 더미 데이터 생성하기

--- flutter/inflearn-flutter-firebase-netflix/netflix_clone/lib/screen/home_screen.dart
+++ flutter/inflearn-flutter-firebase-netflix/netflix_clone/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/model/model_movie.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap(
+      {
+        'title': '사랑의 불시착',
+        'keyword': '사랑/로맨스/판타지',
+        'poster': 'test_movie_1.png',
+        'like': false
+      },
+    )
+  ];
+
   @override
   void initState() {
     super.initState();
