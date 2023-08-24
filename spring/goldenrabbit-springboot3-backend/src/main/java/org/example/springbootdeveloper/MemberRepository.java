cd95112b73c05b4e2e44be7bd289a82322751988
Jinho Hyeon <neverlish@gmail.com>
2023-08-24 16:56:58 +0900

    06 블로그 기획하기 API 만들기

--- spring/goldenrabbit-springboot3-backend/src/main/java/org/example/springbootdeveloper/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.springbootdeveloper;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
