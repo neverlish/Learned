956094ab440837534c4a712501369860f66f54c7
Jinho Hyeon <neverlish@gmail.com>
2023-08-24 15:40:38 +0900

    00 - 2 스프링 부트 3 프로젝트 만들기

---
+++ spring/goldenrabbit-springboot3-backend/src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,11 @@
+package org.example.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
