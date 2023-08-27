07849850f09f5eb6c9f0b2f296dc333ff56dc3a4
Jinho Hyeon <neverlish@gmail.com>
2023-08-27 14:44:14 +0900

    6 - API 포털 생성

--- spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HackingSpringBootReactiveApplication.java
+++ spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HackingSpringBootReactiveApplication.java
@@ -32,44 +32,41 @@ public class HackingSpringBootReactiveApplication {
 //		return new InMemoryHttpTraceRepository(); // <3>
 //	}
 
-	@Bean
-	HttpTraceRepository springDataTraceRepository(HttpTraceWrapperRepository repository) {
-		return new SpringDataHttpTraceRepository(repository);
-	}
-
-	static Converter<Document, HttpTraceWrapper> CONVERTER =
-			new Converter<Document, HttpTraceWrapper>() {
-				@Override
-				public HttpTraceWrapper convert(Document document) {
-					Document httpTrace = document.get("httpTrace", Document.class);
-					Document request = httpTrace.get("request", Document.class);
-					Document response = httpTrace.get("response", Document.class);
-
-					return new HttpTraceWrapper(new HttpTrace(
-							new HttpTrace.Request(
-									request.getString("method"),
-									URI.create(request.getString("uri")),
-									request.get("headers", Map.class),
-									null
-							),
-							new HttpTrace.Response(
-									response.getInteger("status"),
-									response.get("headers", Map.class)
-							),
-							httpTrace.getDate("timestamp").toInstant(),
-							null,
-							null,
-							httpTrace.getLong("timeTaken")
-						));
-				}
-			};
-
-	@Bean
-	public MappingMongoConverter mappingMongoConverter(MongoMappingContext context) {
-		MappingMongoConverter mappingConverter = new MappingMongoConverter(NoOpDbRefResolver.INSTANCE, context); 
-
-		mappingConverter.setCustomConversions(new MongoCustomConversions(Collections.singletonList(CONVERTER)));
-
-		return mappingConverter;
-	}
+//	@Bean
+//	HttpTraceRepository springDataTraceRepository(HttpTraceWrapperRepository repository) {
+//		return new SpringDataHttpTraceRepository(repository);
+//	}
+//
+//	static Converter<Document, HttpTraceWrapper> CONVERTER = //
+//			new Converter<Document, HttpTraceWrapper>() { //
+//				@Override
+//				public HttpTraceWrapper convert(Document document) {
+//					Document httpTrace = document.get("httpTrace", Document.class);
+//					Document request = httpTrace.get("request", Document.class);
+//					Document response = httpTrace.get("response", Document.class);
+//
+//					return new HttpTraceWrapper(new HttpTrace( //
+//							new HttpTrace.Request( //
+//									request.getString("method"), //
+//									URI.create(request.getString("uri")), //
+//									request.get("headers", Map.class), //
+//									null),
+//							new HttpTrace.Response( //
+//									response.getInteger("status"), //
+//									response.get("headers", Map.class)),
+//							httpTrace.getDate("timestamp").toInstant(), //
+//							null, //
+//							null, //
+//							httpTrace.getLong("timeTaken")));
+//				}
+//			};
+//
+//	@Bean
+//	public MappingMongoConverter mappingMongoConverter(MongoMappingContext context) {
+//		MappingMongoConverter mappingConverter = new MappingMongoConverter(NoOpDbRefResolver.INSTANCE, context);
+//
+//		mappingConverter.setCustomConversions(new MongoCustomConversions(Collections.singletonList(CONVERTER)));
+//
+//		return mappingConverter;
+//	}
 }
