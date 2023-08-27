92dc08ff94ea07d23b1cb419391ba9e478cf8779
Jinho Hyeon <neverlish@gmail.com>
2023-08-27 15:26:46 +0900

    6 - 하이퍼미디어의 가치

--- spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HypermediaItemController.java
+++ spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HypermediaItemController.java
@@ -1,10 +1,16 @@
 package com.greglturnquist.hackingspringbootreactive;
 
 import org.springframework.hateoas.*;
+import org.springframework.hateoas.mediatype.alps.Alps;
+import org.springframework.hateoas.mediatype.alps.Type;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.mediatype.alps.Alps.*;
 import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.*;
@@ -39,5 +45,21 @@ public class HypermediaItemController {
 
         return Mono.zip(repository.findById(id), selfLink, aggregateLink)
                 .map(o -> EntityModel.of(o.getT1(), Links.of(o.getT2(), o.getT3())));
+    }
+
+    @GetMapping(value = "/hypermedia/items/profile", produces = MediaTypes.ALPS_JSON_VALUE)
+    public Alps profile() {
+        return alps()
+                .descriptor(Collections.singletonList(descriptor()
+                        .id(Item.class.getSimpleName() + "-repr")
+                        .descriptor(Arrays.stream(
+                                        Item.class.getDeclaredFields())
+                                .map(field -> descriptor()
+                                        .name(field.getName())
+                                        .type(Type.SEMANTIC)
+                                        .build())
+                                .collect(Collectors.toList()))
+                        .build()))
+                .build();
     }
 }
