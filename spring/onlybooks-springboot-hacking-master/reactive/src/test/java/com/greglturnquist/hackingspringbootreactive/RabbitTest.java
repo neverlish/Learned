988430eedf135813253829afb552d93fd484e54c
Jinho Hyeon <neverlish@gmail.com>
2023-08-27 16:13:29 +0900

    7 - 테스트 케이스 구성

--- spring/onlybooks-springboot-hacking-master/reactive/src/test/java/com/greglturnquist/hackingspringbootreactive/RabbitTest.java
+++ spring/onlybooks-springboot-hacking-master/reactive/src/test/java/com/greglturnquist/hackingspringbootreactive/RabbitTest.java
@@ -1,5 +1,6 @@
 package com.greglturnquist.hackingspringbootreactive;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,10 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
@@ -26,5 +31,41 @@ public class RabbitTest {
     static void configure(DynamicPropertyRegistry registry) {
         registry.add("spring.rabbitmq.host", container::getContainerIpAddress);
         registry.add("spring.rabbitmq.port", container::getAmqpPort);
+    }
+
+    @Test
+    void verifyingMessagingThroughAmqp() throws InterruptedException {
+        this.webTestClient.post().uri("/items")
+                .bodyValue(new Item("Alf alarm clock", "nothing important", 19.99))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody();
+
+        Thread.sleep(1500L);
+
+        this.webTestClient.post().uri("/items")
+                .bodyValue(new Item("Smurf TV tray", "nothing important", 29.99))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody();
+
+        Thread.sleep(2000L);
+
+        this.repository.findAll()
+                .as(StepVerifier::create)
+                .expectNextMatches(item -> {
+                    assertThat(item.getName()).isEqualTo("Alf alarm clock");
+                    assertThat(item.getDescription()).isEqualTo("nothing important");
+                    assertThat(item.getPrice()).isEqualTo(19.99);
+                    return true;
+                })
+                .expectNextMatches(item -> {
+                    assertThat(item.getName()).isEqualTo("Smurf TV tray");
+                    assertThat(item.getDescription()).isEqualTo("nothing important");
+                    assertThat(item.getPrice()).isEqualTo(29.99);
+                    return true;
+                })
+                .verifyComplete();
+
     }
 }
