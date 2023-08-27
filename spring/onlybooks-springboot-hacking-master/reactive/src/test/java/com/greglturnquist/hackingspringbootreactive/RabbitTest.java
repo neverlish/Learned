ec6161efa7bee8cd1077cb572720fe017cd68426
Jinho Hyeon <neverlish@gmail.com>
2023-08-27 15:47:14 +0900

    7 스프링 부트 메시징 - 손쉬운 테스트

---
+++ spring/onlybooks-springboot-hacking-master/reactive/src/test/java/com/greglturnquist/hackingspringbootreactive/RabbitTest.java
@@ -0,0 +1,30 @@
+package com.greglturnquist.hackingspringbootreactive;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+@Testcontainers
+@ContextConfiguration
+public class RabbitTest {
+    @Container static RabbitMQContainer container = new RabbitMQContainer("rabbitmq:3.7.25-management-alpine");
+
+    @Autowired WebTestClient webTestClient;
+
+    @Autowired ItemRepository repository;
+
+    @DynamicPropertySource
+    static void configure(DynamicPropertyRegistry registry) {
+        registry.add("spring.rabbitmq.host", container::getContainerIpAddress);
+        registry.add("spring.rabbitmq.port", container::getAmqpPort);
+    }
+}
