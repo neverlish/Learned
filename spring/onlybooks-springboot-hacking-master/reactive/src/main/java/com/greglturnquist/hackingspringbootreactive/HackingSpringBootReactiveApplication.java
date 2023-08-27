d2e23c82cf9c922fe2f046b7f8d574d6f66615a8
Jinho Hyeon <neverlish@gmail.com>
2023-08-27 13:50:32 +0900

    5 스프링 부트 운영 - 애플리케이션 배포

--- spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HackingSpringBootReactiveApplication.java
+++ spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HackingSpringBootReactiveApplication.java
@@ -2,20 +2,11 @@ package com.greglturnquist.hackingspringbootreactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.thymeleaf.TemplateEngine;
-import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class HackingSpringBootReactiveApplication {
 
 	public static void main(String[] args) {
-		BlockHound
-				.builder()
-				.allowBlockingCallsInside(
-						TemplateEngine.class.getCanonicalName(), "process"
-				)
-				.install();
-
 		SpringApplication.run(HackingSpringBootReactiveApplication.class, args);
 	}
 
