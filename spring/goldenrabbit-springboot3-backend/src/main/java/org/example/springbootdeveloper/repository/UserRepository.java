667c6224a821b553dae63dbba5e37c8257115bc1
Jinho Hyeon <neverlish@gmail.com>
2023-08-29 14:20:11 +0900

    08 스프링 시큐리티로 로그인/로그아웃, 회원 가입 구현하기 - 2 회원 도메인 만들기

---
+++ spring/goldenrabbit-springboot3-backend/src/main/java/org/example/springbootdeveloper/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.example.springbootdeveloper.repository;
+
+import org.example.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
