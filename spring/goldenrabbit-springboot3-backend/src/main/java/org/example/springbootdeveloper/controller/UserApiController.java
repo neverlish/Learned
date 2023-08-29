a4db0faa490c8dc80447a6e8d35a6b31efd8be7e
Jinho Hyeon <neverlish@gmail.com>
2023-08-29 15:10:43 +0900

    08 - 6 로그아웃 구현하기

--- spring/goldenrabbit-springboot3-backend/src/main/java/org/example/springbootdeveloper/controller/UserApiController.java
+++ spring/goldenrabbit-springboot3-backend/src/main/java/org/example/springbootdeveloper/controller/UserApiController.java
@@ -1,9 +1,14 @@
 package org.example.springbootdeveloper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.example.springbootdeveloper.dto.AddUserRequest;
 import org.example.springbootdeveloper.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -14,6 +19,13 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+
         return "redirect:/login";
     }
 }
