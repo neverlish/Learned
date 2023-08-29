21176b9510761819541909eee3ef8936e239f8c7
Jinho Hyeon <neverlish@gmail.com>
2023-08-29 14:06:22 +0900

    07 - 5 수정/생성 기능 추가하기

--- spring/goldenrabbit-springboot3-backend/src/main/resources/static/js/article.js
+++ spring/goldenrabbit-springboot3-backend/src/main/resources/static/js/article.js
@@ -12,3 +12,28 @@ if (deleteButton) {
        })
     });
 }
+
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton) {
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value,
+            })
+
+        })
+        .then(() => {
+            alert('수정이 완료되었습니다.');
+            location.replace(`/articles/${id}`);
+       })
+    })
+}
