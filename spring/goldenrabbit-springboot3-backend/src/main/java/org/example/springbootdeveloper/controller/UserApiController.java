03e25dd56b6902b4ba4c48ee75401f66ceacbac7
Jinho Hyeon <neverlish@gmail.com>
2023-08-29 14:52:34 +0900

    08 - 4 회원 가입 구현하기

---
+++ spring/goldenrabbit-springboot3-backend/src/main/java/org/example/springbootdeveloper/controller/UserApiController.java
@@ -0,0 +1,19 @@
+package org.example.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.dto.AddUserRequest;
+import org.example.springbootdeveloper.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserApiController {
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public String signup(AddUserRequest request) {
+        userService.save(request);
+        return "redirect:/login";
+    }
+}
---
+++ spring/goldenrabbit-springboot3-backend/src/main/java/org/example/springbootdeveloper/service/UserService.java
@@ -0,0 +1,22 @@
+package org.example.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.domain.User;
+import org.example.springbootdeveloper.dto.AddUserRequest;
+import org.example.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
