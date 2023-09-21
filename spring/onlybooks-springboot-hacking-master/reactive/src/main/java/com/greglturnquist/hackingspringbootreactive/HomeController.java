00647ca2d80e2c356ee67a09240537e2e03a1dcf
Jinho Hyeon <neverlish@gmail.com>
2023-09-21 17:06:50 +0900

    9 - 사용자 컨텍스트 접근

--- spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HomeController.java
+++ spring/onlybooks-springboot-hacking-master/reactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HomeController.java
@@ -1,5 +1,6 @@
 package com.greglturnquist.hackingspringbootreactive;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.result.view.Rendering;
@@ -17,24 +18,25 @@ public class HomeController {
 
     // tag::2[]
     @GetMapping
-    Mono<Rendering> home() { // <1>
+    Mono<Rendering> home(Authentication auth) { // <1>
         return Mono.just(Rendering.view("home.html") // <2>
                 .modelAttribute("items", this.inventoryService.getInventory()) // <3>
-                .modelAttribute("cart", this.inventoryService.getCart("My Cart") // <4>
-                        .defaultIfEmpty(new Cart("My Cart")))
+                .modelAttribute("cart", this.inventoryService.getCart(cartName(auth)) // <4>
+                        .defaultIfEmpty(new Cart(cartName(auth))))
+                .modelAttribute("auth", auth)
                 .build());
     }
     // end::2[]
 
     @PostMapping("/add/{id}")
-    Mono<String> addToCart(@PathVariable String id) {
-        return this.inventoryService.addItemToCart("My Cart", id)
+    Mono<String> addToCart(Authentication auth, @PathVariable String id) {
+        return this.inventoryService.addItemToCart(cartName(auth), id)
                 .thenReturn("redirect:/");
     }
 
     @DeleteMapping("/remove/{id}")
-    Mono<String> removeFromCart(@PathVariable String id) {
-        return this.inventoryService.removeOneFromCart("My Cart", id)
+    Mono<String> removeFromCart(Authentication auth, @PathVariable String id) {
+        return this.inventoryService.removeOneFromCart(cartName(auth), id)
                 .thenReturn("redirect:/");
     }
 
@@ -48,5 +50,9 @@ public class HomeController {
     Mono<String> deleteItem(@PathVariable String id) {
         return this.inventoryService.deleteItem(id) //
                 .thenReturn("redirect:/");
+    }
+
+    private static String cartName(Authentication auth) {
+        return auth.getName() + "'s Cart";
     }
 }
