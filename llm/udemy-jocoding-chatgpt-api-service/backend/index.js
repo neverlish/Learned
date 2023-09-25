539947e115bf30e1fe8f98f232cf3c31a7cee9af
Jinho Hyeon <neverlish@gmail.com>
2023-09-25 20:59:29 +0900

    1 시작하기 - 08 개발 환경 세팅 (2) - 폴더 구조 생성 및 OpenAI API 설정

---
+++ llm/udemy-jocoding-chatgpt-api-service/backend/index.js
@@ -0,0 +1,16 @@
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration);
+
+async function apiCall() {
+  const completion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: 'Hello world'
+  })
+  console.log(completion.data.choices[0].text)
+}
+apiCall()
