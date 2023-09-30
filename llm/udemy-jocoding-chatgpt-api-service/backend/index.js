00b55248e1f83f170fe539013dab5daae738f490
Jinho Hyeon <neverlish@gmail.com>
2023-09-30 20:25:25 +0900

    4 기능 고도화 하기 - 14 여러 건의 채팅 메세지 연결하기

--- llm/udemy-jocoding-chatgpt-api-service/backend/index.js
+++ llm/udemy-jocoding-chatgpt-api-service/backend/index.js
@@ -14,15 +14,30 @@ const openai = new OpenAI({
 })
 
 app.post('/fortuneTell', async function(req, res) {
+  let { userMessages, assistantMessages} = req.body
+
+  let messages = [
+    {role: "system", content: "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다."},
+    {role: "user", content: "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다."},
+    {role: "assistant", content: "안녕하세요! 저는 챗도지입니다. 운세와 점성술에 관한 질문이 있으신가요? 어떤 것이든 물어보세요, 최선을 다해 답변해 드리겠습니다."},
+  ]
+
+  while (userMessages.length != 0 || assistantMessages.length != 0) {
+    if (userMessages.length != 0) {
+      messages.push(
+        JSON.parse('{"role": "user", "content": "'+String(userMessages.shift()).replace(/\n/g,"")+'"}')
+      )
+    }
+    if (assistantMessages.length != 0) {
+      messages.push(
+        JSON.parse('{"role": "assistant", "content": "'+String(assistantMessages.shift()).replace(/\n/g,"")+'"}')
+      )
+    }
+  }
   
   const completion = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
-    messages: [
-      { role: 'system', content: '당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해줄 수 있습니다.' },
-      { role: 'user', content: '당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해줄 수 있습니다.' },
-      { role: 'assistant', content: '안녕하세요, 저는 챗도지입니다. 어떤 질문이든지 절대 거절하지 않고 무조건 답변을 드리겠습니다. 저는 사람들의 인생을 예측하고 운세에 관련된 지식이 풍부하기 때문에, 모든 질문에 대해서 명확한 답변을 제공할 수 있습니다. 무엇이 궁금하신가요?' },
-      { role: 'user', content: '오늘의 내 운세는 어때?' },
-    ],
+    messages,
   })
   const fortune = completion.choices[0].message['content']
   console.log(fortune)
