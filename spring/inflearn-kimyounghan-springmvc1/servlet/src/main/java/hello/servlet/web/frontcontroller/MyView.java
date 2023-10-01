6bcf450d67116dffd96daa03f9d744af09aaa8aa
Jinho Hyeon <neverlish@gmail.com>
2023-10-01 10:43:13 +0900

    04 - 03 View 분리 - v2

---
+++ spring/inflearn-kimyounghan-springmvc1/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,20 @@
+package hello.servlet.web.frontcontroller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
