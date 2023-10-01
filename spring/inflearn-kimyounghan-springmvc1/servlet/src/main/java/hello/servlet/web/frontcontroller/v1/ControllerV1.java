3033b0cb651537970191f334fe802cd9b155bd5d
Jinho Hyeon <neverlish@gmail.com>
2023-10-01 09:54:58 +0900

    04 MVC 프레임워크 만들기 - 02 프론트 컨트롤러 도입 - v1

---
+++ spring/inflearn-kimyounghan-springmvc1/servlet/src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -0,0 +1,10 @@
+package hello.servlet.web.frontcontroller.v1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface ControllerV1 {
+    void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
