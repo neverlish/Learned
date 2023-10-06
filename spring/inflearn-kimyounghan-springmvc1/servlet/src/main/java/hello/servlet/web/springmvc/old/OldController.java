a44bd04fa1313b59548ce306aa29f302f6e29f75
Jinho Hyeon <neverlish@gmail.com>
2023-10-07 01:34:28 +0900

    05 - 03 뷰 리졸버

--- spring/inflearn-kimyounghan-springmvc1/servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ spring/inflearn-kimyounghan-springmvc1/servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -12,6 +12,6 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form");
     }
 }
