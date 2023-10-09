d0b567a5a0f0fbbd2a65c79de2b32e5c1a02a041
Jinho Hyeon <neverlish@gmail.com>
2023-10-09 21:29:44 +0900

    07 - 09 상품 수정

--- spring/inflearn-kimyounghan-springmvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring/inflearn-kimyounghan-springmvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -77,6 +77,19 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
 
     @PostConstruct
     public void init() {
