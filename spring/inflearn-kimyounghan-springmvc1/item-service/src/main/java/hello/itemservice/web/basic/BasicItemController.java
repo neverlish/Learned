d6125ee1aa8370da34440f617ac042a8f5fea8f4
Jinho Hyeon <neverlish@gmail.com>
2023-10-09 11:26:40 +0900

    07 - 06 상품 상세

--- spring/inflearn-kimyounghan-springmvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring/inflearn-kimyounghan-springmvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -23,6 +24,13 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 
     @PostConstruct
