a8b7816aff75ea8ab9e9fb1d14b4d263cae72499
Jinho Hyeon <neverlish@gmail.com>
2023-10-20 08:38:37 +0900

    3 - 22 Creating a New Deck

--- go/udemy-go-complete-developers-guide/cards/deck.go
+++ go/udemy-go-complete-developers-guide/cards/deck.go
@@ -4,6 +4,19 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suite := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suite)
+		}
+	}
+	return cards
+}
+
 func (cards deck) print() {
 	for i, card := range cards {
 		fmt.Println(i, card)
