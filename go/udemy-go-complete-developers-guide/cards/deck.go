072cdb74c87e288ddda43bffdb30a9a4e8755a48
Jinho Hyeon <neverlish@gmail.com>
2023-10-24 23:33:24 +0900

    3 - 29 Reading From the Hard Drive

--- go/udemy-go-complete-developers-guide/cards/deck.go
+++ go/udemy-go-complete-developers-guide/cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -37,4 +38,12 @@ func (d deck) toString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(0)
+	}
 }
