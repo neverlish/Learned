b938fc6e8844a35006898aadac5a5c3aa8f71926
Jinho Hyeon <neverlish@gmail.com>
2023-10-28 08:30:14 +0900

    3 - 37 Asserting Elements in a Slice

--- go/udemy-go-complete-developers-guide/cards/deck_test.go
+++ go/udemy-go-complete-developers-guide/cards/deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
+	}
 }
