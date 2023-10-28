0ec92a3fa367ba72b04a81f636684d7c7bb0cb91
Jinho Hyeon <neverlish@gmail.com>
2023-10-28 23:38:06 +0900

    4 - 46 Embedding Structs

--- go/udemy-go-complete-developers-guide/structs/main.go
+++ go/udemy-go-complete-developers-guide/structs/main.go
@@ -2,17 +2,25 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	var alex person
-
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	fmt.Printf("%+v", jim)
 }
