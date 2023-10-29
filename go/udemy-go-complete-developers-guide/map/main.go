9144f473d06088c7cb8d10ee3cba7172b6f4dd58
Jinho Hyeon <neverlish@gmail.com>
2023-10-29 19:36:30 +0900

    5 - 55 Manipulating Maps

--- go/udemy-go-complete-developers-guide/map/main.go
+++ go/udemy-go-complete-developers-guide/map/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
+	// var colors map[string]string
+
+	colors := make(map[int]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	colors[10] = "#ffffff"
+
+	delete(colors, 10)
 
 	fmt.Println(colors)
 
