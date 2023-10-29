fe78abd143e2ee4908246dc478d2a41a6998b605
Jinho Hyeon <neverlish@gmail.com>
2023-10-29 19:45:46 +0900

    5 - 56 Iterating Over Maps

--- go/udemy-go-complete-developers-guide/map/main.go
+++ go/udemy-go-complete-developers-guide/map/main.go
@@ -3,19 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	// var colors map[string]string
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	colors := make(map[int]string)
+	printMap(colors)
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+}
 
-	colors[10] = "#ffffff"
-
-	delete(colors, 10)
-
-	fmt.Println(colors)
-
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
