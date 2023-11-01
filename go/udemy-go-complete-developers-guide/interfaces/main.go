889946ce8e4d16a04824583a8f1e2b4dd738426a
Jinho Hyeon <neverlish@gmail.com>
2023-11-02 08:48:45 +0900

    6 - 60 Interfaces in Practice

--- go/udemy-go-complete-developers-guide/interfaces/main.go
+++ go/udemy-go-complete-developers-guide/interfaces/main.go
@@ -2,31 +2,31 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
 	eb := englishBot{}
-	// sb := spanishBot{}
+	sb := spanishBot{}
 
 	printGreeting(eb)
-	// printGreeting(sb)
+	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
-// func printGreeting(sb spanishBot) {
-// 	fmt.Println(sb.getGreeting())
-// }
-
-func (eb englishBot) getGreeting() string {
+func (englishBot) getGreeting() string {
 	// VERY custom logic for generating an english greeting
 	return "Hi there!"
 }
 
-func (sb spanishBot) getGreeting() string {
+func (spanishBot) getGreeting() string {
 	// VERY custom logic for generating an spanish greeting
 	return "Hola!"
 }
