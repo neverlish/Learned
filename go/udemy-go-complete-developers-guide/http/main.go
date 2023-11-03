f7cccb25e83d624a8f6b76da2d2243ce24d1e936
Jinho Hyeon <neverlish@gmail.com>
2023-11-03 23:16:34 +0900

    6 - 73 A Custom Writer

--- go/udemy-go-complete-developers-guide/http/main.go
+++ go/udemy-go-complete-developers-guide/http/main.go
@@ -7,11 +7,20 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
