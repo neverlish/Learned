a0bb0d7261e9a65a158344030159da791a6ad133
Jinho Hyeon <neverlish@gmail.com>
2023-11-04 10:43:22 +0900

    7 - 84 Channel Implementation

--- go/udemy-go-complete-developers-guide/channels/main.go
+++ go/udemy-go-complete-developers-guide/channels/main.go
@@ -14,16 +14,22 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		return
 	}
 	fmt.Println(link, "is up!")
+	c <- "Yep its up"
 }
