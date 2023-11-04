0f8e633b9fea78ae3e2572f0cf325768b64bf73e
Jinho Hyeon <neverlish@gmail.com>
2023-11-04 11:09:00 +0900

    7 - 85 Blocking Channels

--- go/udemy-go-complete-developers-guide/channels/main.go
+++ go/udemy-go-complete-developers-guide/channels/main.go
@@ -21,6 +21,12 @@ func main() {
 	}
 
 	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+
+	fmt.Println(<-c)
 }
 
 func checkLink(link string, c chan string) {
