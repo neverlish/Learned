59db3f31167d7a3c64cdb08e5ff3b8d4d20d7ac3
Jinho Hyeon <neverlish@gmail.com>
2023-11-06 00:41:31 +0900

    03 - 13 Linkedin 데이터 프로세싱 4부: 사용자 지정 에이전트 구현 & 테스트

--- llm/udemy-langchain-llm-based-application/ice_breaker/ice_breaker.py
+++ llm/udemy-langchain-llm-based-application/ice_breaker/ice_breaker.py
@@ -2,10 +2,13 @@ from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 
 if __name__ == "__main__":
     print("hello Langchain!")
+
+    linkedin_profie_url = linkedin_lookup_agent(name="Eden Marco")
 
     summary_template = """
         given the information {information} about a person from I want you to create:
@@ -18,7 +21,7 @@ if __name__ == "__main__":
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    
-    linkedin_data = scrape_linkedin_profile(linkedin_profie_url="https://www.linkedin.com/in/harrison-chase-961287118/")
+
+    linkedin_data = scrape_linkedin_profile(linkedin_profie_url=linkedin_profie_url)
 
     print(chain.run(information=linkedin_data))
