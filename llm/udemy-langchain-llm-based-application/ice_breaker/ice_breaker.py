cd9e2806bed7a023830a9174f13a8247fa3ca6ee
Jinho Hyeon <neverlish@gmail.com>
2023-11-30 23:13:45 +0900

    03 - 16 아웃풋 파서 (Output Parsers) - 프론트엔드 작업 준비하기

--- llm/udemy-langchain-llm-based-application/ice_breaker/ice_breaker.py
+++ llm/udemy-langchain-llm-based-application/ice_breaker/ice_breaker.py
@@ -7,13 +7,15 @@ from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
 
-if __name__ == "__main__":
-    print("hello Langchain!")
+from output_parsers import person_intel_parser, PersonIntel
 
-    linkedin_profie_url = linkedin_lookup_agent(name="Eden Marco")
+name = "Eden Marco"
+
+def ice_break(name: str) -> PersonIntel:
+    linkedin_profie_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profie_url=linkedin_profie_url)
 
-    twitter_username = twitter_lookup_agent(name="Eden Marco")
+    twitter_username = twitter_lookup_agent(name=name)
     tweets = scrape_user_tweets(username=twitter_username)
 
     summary_template = """
@@ -22,14 +24,24 @@ if __name__ == "__main__":
         2. two interesting facts about them
         3. A topic that may interest them
         4. 2 creative Ice breakers to open a conversation with them
+        \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["linkedin_information", "twitter_information"], template=summary_template
+        input_variables=["linkedin_information", "twitter_information"], 
+        template=summary_template,
+        partial_variables={"format_instructions": person_intel_parser.get_format_instructions()}
     )
     
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(linkedin_information=linkedin_data, twitter_information=tweets))
+    result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
+    print(result)
+    return person_intel_parser.parse(result)
+
+if __name__ == "__main__":
+    print("hello Langchain!")
+    result = ice_break(name=name)
+    
