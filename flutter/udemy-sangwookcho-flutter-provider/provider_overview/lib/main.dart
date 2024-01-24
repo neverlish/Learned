3df7f857c9c4c3dc5b3963f216731881944697dd
Jinho Hyeon <neverlish@gmail.com>
2024-01-24 22:55:38 +0900

    2 - 011 read, watch, select extension methods

--- flutter/udemy-sangwookcho-flutter-provider/provider_overview/lib/main.dart
+++ flutter/udemy-sangwookcho-flutter-provider/provider_overview/lib/main.dart
@@ -14,9 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<Dog>(
-      create: (context) => Dog(name: 'dog04', breed: 'breed04'),
+      create: (context) => Dog(name: 'dog05', breed: 'breed05', age: 3),
       child: MaterialApp(
-        title: 'Provider 04',
+        title: 'Provider 05',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Provider 04'),
+        title: const Text('Provider 05'),
       ),
       body: Center(
         child: Column(
@@ -47,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              '- name: ${Provider.of<Dog>(context).name}',
+              '- name: ${context.watch<Dog>().name}',
               style: const TextStyle(fontSize: 20.0),
             ),
             const SizedBox(height: 10.0),
@@ -69,7 +69,7 @@ class BreedAndAge extends StatelessWidget {
     return Column(
       children: [
         Text(
-          '- breed: ${Provider.of<Dog>(context).breed}',
+          '- breed: ${context.select<Dog, String>((Dog dog) => dog.breed)}',
           style: const TextStyle(fontSize: 20.0),
         ),
         const SizedBox(height: 10.0),
@@ -89,12 +89,12 @@ class Age extends StatelessWidget {
     return Column(
       children: [
         Text(
-          '- age: ${Provider.of<Dog>(context).age}',
+          '- age: ${context.select<Dog, int>((Dog dog) => dog.age)}',
           style: const TextStyle(fontSize: 20.0),
         ),
         const SizedBox(height: 20.0),
         ElevatedButton(
-          onPressed: () => Provider.of<Dog>(context, listen: false).grow(),
+          onPressed: () => context.read<Dog>().grow(),
           child: const Text(
             'Grow',
             style: TextStyle(fontSize: 20.0),
