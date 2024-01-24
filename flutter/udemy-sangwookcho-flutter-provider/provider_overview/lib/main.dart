d101b847f4e02717497ac9beeffeb3af9b0ec62d
Jinho Hyeon <neverlish@gmail.com>
2024-01-24 23:31:09 +0900

    2 - 016 Consumer, builder, ProviderNotFoundException

--- flutter/udemy-sangwookcho-flutter-provider/provider_overview/lib/main.dart
+++ flutter/udemy-sangwookcho-flutter-provider/provider_overview/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'models/dog.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -12,112 +10,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<Dog>(
-      create: (context) => Dog(name: 'dog08', breed: 'breed08', age: 3),
-      child: MaterialApp(
-        title: 'Provider 08',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(),
+    return MaterialApp(
+      title: 'Provider 09',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class Foo with ChangeNotifier {
+  String value = 'Foo';
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  void changeValue() {
+    value = value == 'Foo' ? 'Bar' : 'Foo';
+    notifyListeners();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Provider 08'),
+        title: const Text('Provider 09'),
       ),
-      body: Consumer<Dog>(
-        builder: (BuildContext context, Dog dog, Widget? child) {
+      body: ChangeNotifierProvider<Foo>(
+        create: (_) => Foo(),
+        child:
+            Consumer<Foo>(builder: (BuildContext context, Foo foo, Widget? _) {
           return Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
               children: [
-                child!,
-                const SizedBox(height: 10.0),
                 Text(
-                  '- name: ${dog.name}',
-                  style: const TextStyle(fontSize: 20.0),
+                  foo.value,
+                  style: const TextStyle(fontSize: 40),
                 ),
-                const SizedBox(height: 10.0),
-                const BreedAndAge(),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () => foo.changeValue(),
+                  child: const Text(
+                    'Change Value',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
               ],
             ),
           );
-        },
-        child: const Text(
-          'I like dogs very much',
-          style: TextStyle(fontSize: 20.0),
-        ),
+        }),
       ),
-    );
-  }
-}
-
-class BreedAndAge extends StatelessWidget {
-  const BreedAndAge({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<Dog>(
-      builder: (_, Dog dog, __) {
-        return Column(
-          children: [
-            Text(
-              '- breed: ${dog.breed}',
-              style: const TextStyle(fontSize: 20.0),
-            ),
-            const SizedBox(height: 10.0),
-            const Age(),
-          ],
-        );
-      },
-    );
-  }
-}
-
-class Age extends StatelessWidget {
-  const Age({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<Dog>(
-      builder: (_, dog, __) {
-        return Column(
-          children: [
-            Text(
-              '- age: ${dog.age}',
-              style: const TextStyle(fontSize: 20.0),
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () => dog.grow(),
-              child: const Text(
-                'Grow',
-                style: TextStyle(fontSize: 20.0),
-              ),
-            ),
-          ],
-        );
-      },
     );
   }
 }
