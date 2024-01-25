e76095837748f3787dfc2d5162c9fd157f2bd6b1
Jinho Hyeon <neverlish@gmail.com>
2024-01-25 19:56:49 +0900

    3 - 042 TodoSearchState and TodoSearch ChangeNotifier

---
+++ flutter/udemy-sangwookcho-flutter-provider/todo_provider/lib/providers/todo_search.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+class TodoSearchState extends Equatable {
+  final String searchTerm;
+
+  const TodoSearchState({
+    required this.searchTerm,
+  });
+
+  factory TodoSearchState.initial() {
+    return const TodoSearchState(searchTerm: '');
+  }
+
+  @override
+  List<Object> get props => [searchTerm];
+
+  @override
+  bool get stringify => true;
+
+  TodoSearchState copyWith({
+    String? searchTerm,
+  }) {
+    return TodoSearchState(
+      searchTerm: searchTerm ?? this.searchTerm,
+    );
+  }
+}
+
+class TodoSearch with ChangeNotifier {
+  TodoSearchState _state = TodoSearchState.initial();
+
+  TodoSearchState get state => _state;
+
+  void updateSearchTerm(String searchTerm) {
+    _state = _state.copyWith(searchTerm: searchTerm);
+    notifyListeners();
+  }
+}
