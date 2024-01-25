1e37303d6e610d7ab865956391540a5da14b6700
Jinho Hyeon <neverlish@gmail.com>
2024-01-25 20:42:46 +0900

    3 - 047 TodoHeader widget

--- flutter/udemy-sangwookcho-flutter-provider/todo_provider/lib/pages/todos_page.dart
+++ flutter/udemy-sangwookcho-flutter-provider/todo_provider/lib/pages/todos_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_provider/providers/active_todo_count.dart';
 
 class TodosPage extends StatefulWidget {
   const TodosPage({Key? key}) : super(key: key);
@@ -10,10 +12,41 @@ class TodosPage extends StatefulWidget {
 class _TodosPageState extends State<TodosPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('TODOS'),
+    return const SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+            child: Column(
+              children: [TodoHeader()],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class TodoHeader extends StatelessWidget {
+  const TodoHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          'TODO',
+          style: TextStyle(fontSize: 40.0),
+        ),
+        Text(
+          '${context.watch<ActiveTodoCount>().state.activeTodoCount} items left',
+          style: const TextStyle(
+            fontSize: 20.0,
+            color: Colors.redAccent,
+          ),
+        )
+      ],
     );
   }
 }
