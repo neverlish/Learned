94c9b997dff21e99360994b290667186e80a6c51
Jinho Hyeon <neverlish@gmail.com>
2024-01-26 21:36:21 +0900

    4 Weather App (OpenWeather) - 066 Packages, app folder 구조, app page skeleton

---
+++ flutter/udemy-sangwookcho-flutter-provider/open_weather_provider/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'pages/home_page.dart';
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
