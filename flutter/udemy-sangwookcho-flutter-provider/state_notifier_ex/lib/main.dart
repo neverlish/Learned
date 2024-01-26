825de4f749b28bd16fafe86014d0452865e85880
Jinho Hyeon <neverlish@gmail.com>
2024-01-26 09:11:30 +0900

    3 - 060 StateNotifier & StateNotifierProvider example 4

--- flutter/udemy-sangwookcho-flutter-provider/state_notifier_ex/lib/main.dart
+++ flutter/udemy-sangwookcho-flutter-provider/state_notifier_ex/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:provider/provider.dart';
+import 'package:state_notifier_ex/providers/bg_color.dart';
+import 'package:state_notifier_ex/providers/counter.dart';
+import 'package:state_notifier_ex/providers/customer_level.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +14,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'StateNotifier',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        StateNotifierProvider<BgColor, BgColorState>(
+          create: (context) => BgColor(),
+        ),
+        StateNotifierProvider<Counter, CounterState>(
+          create: (context) => Counter(),
+        ),
+        StateNotifierProvider<CustomerLevel, Level>(
+          create: (context) => CustomerLevel(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'StateNotifier',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
@@ -25,15 +43,45 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colorState = context.watch<BgColorState>();
+    final counterState = context.watch<CounterState>();
+    final levelState = context.watch<Level>();
+
     return Scaffold(
+      backgroundColor: levelState == Level.bronze
+          ? Colors.white
+          : levelState == Level.silver
+              ? Colors.grey
+              : Colors.yellow,
       appBar: AppBar(
+        backgroundColor: colorState.color,
         title: const Text('StateNotifier'),
       ),
       body: Center(
         child: Text(
-          '0',
+          '${counterState.counter}',
           style: Theme.of(context).textTheme.displayMedium,
         ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+            onPressed: () {
+              context.read<Counter>().increment();
+            },
+          ),
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            tooltip: 'Change color',
+            child: const Icon(Icons.color_lens_outlined),
+            onPressed: () {
+              context.read<BgColor>().changeColor();
+            },
+          ),
+        ],
       ),
     );
   }
