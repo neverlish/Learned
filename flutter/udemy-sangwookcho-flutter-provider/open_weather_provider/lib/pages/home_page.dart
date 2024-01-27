9893a5a1ff008139cd9b600478dcfb0ac763c055
Jinho Hyeon <neverlish@gmail.com>
2024-01-27 23:56:17 +0900

    4 - 074 HomePage widget 1 - initial, loading, error

--- flutter/udemy-sangwookcho-flutter-provider/open_weather_provider/lib/pages/home_page.dart
+++ flutter/udemy-sangwookcho-flutter-provider/open_weather_provider/lib/pages/home_page.dart
@@ -13,6 +13,71 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String? _city;
 
+  late final WeatherProvider _weatherProv;
+
+  @override
+  void initState() {
+    super.initState();
+    _weatherProv = context.read<WeatherProvider>();
+    _weatherProv.addListener(_registerListener);
+  }
+
+  @override
+  void dispose() {
+    _weatherProv.removeListener(_registerListener);
+    super.dispose();
+  }
+
+  void _registerListener() {
+    final WeatherState ws = context.read<WeatherProvider>().state;
+
+    if (ws.status == WeatherStatus.error) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(ws.error.errMsg),
+          );
+        },
+      );
+    }
+  }
+
+  Widget _showWeather() {
+    final state = context.watch<WeatherProvider>().state;
+
+    if (state.status == WeatherStatus.initial) {
+      return const Center(
+        child: Text(
+          'Search a city',
+          style: TextStyle(fontSize: 20.0),
+        ),
+      );
+    }
+
+    if (state.status == WeatherStatus.loading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (state.status == WeatherStatus.error && state.weather.name == '') {
+      return const Center(
+        child: Text(
+          'Search a city',
+          style: TextStyle(fontSize: 20.0),
+        ),
+      );
+    }
+
+    return Center(
+      child: Text(
+        state.weather.name,
+        style: const TextStyle(fontSize: 18.0),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,9 +99,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Home'),
-      ),
+      body: _showWeather(),
     );
   }
 }
