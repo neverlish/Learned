d35fdadf94e2a422f6467fbf699e2a50f5050161
Jinho Hyeon <neverlish@gmail.com>
2024-02-04 16:55:14 +0900

    06 - 28 Container, SizedBox 위젯 사용법

--- flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
+++ flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
@@ -14,15 +14,15 @@ class _MainScreenState extends State<MainScreen> {
       appBar: AppBar(
         title: const Text('메인 화면'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('반갑습니다.'),
-            Text('저는 유데미 강의를 듣고있는 학생입니다.'),
-            Text('저는 홍길동입니다.'),
-            Row(
+            const Text('반갑습니다.'),
+            const Text('저는 유데미 강의를 듣고있는 학생입니다.'),
+            const Text('저는 홍길동입니다.'),
+            const Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Text('안녕'),
@@ -30,13 +30,29 @@ class _MainScreenState extends State<MainScreen> {
                 Text('이것은 가로 방향으로 쌓는 위젯이야.'),
               ],
             ),
-            Row(
+            const Row(
               children: [
                 Expanded(flex: 2, child: Text('홍길동')),
                 Expanded(child: Text('홍길동')),
                 Expanded(child: Text('홍길동')),
               ],
             ),
+            Container(
+              width: 300,
+              height: 100,
+              margin: const EdgeInsets.all(32),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Text('홍드로이드'),
+            ),
+            const SizedBox(
+              width: 100,
+              height: 300,
+              child: Text('홍드로이드2'),
+            )
           ],
         ),
       ),
