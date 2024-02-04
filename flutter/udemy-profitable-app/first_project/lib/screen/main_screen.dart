3eb49a5fc4f9b93415ad1dd89a88db7b2f2f7191
Jinho Hyeon <neverlish@gmail.com>
2024-02-04 17:26:34 +0900

    08 - 34 ValueNotifier로 상태 관리 해보기

--- flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
+++ flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
@@ -10,6 +10,7 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   TextEditingController idController = TextEditingController();
   String msg = '이 곳에 입력 값이 업데이트 됩니다!';
+  ValueNotifier<int> counter = ValueNotifier<int>(0);
 
   @override
   Widget build(BuildContext context) {
@@ -28,11 +29,18 @@ class _MainScreenState extends State<MainScreen> {
           ),
           ElevatedButton(
             onPressed: () {
-              setState(() {
-                msg = idController.text;
-              });
+              // setState(() {
+              //   msg = idController.text;
+              // });
+              counter.value = 30;
             },
             child: const Text('아이디 입력 값 가져오기 !'),
+          ),
+          ValueListenableBuilder<int>(
+            valueListenable: counter,
+            builder: (context, value, child) {
+              return Text('Count : $value');
+            },
           ),
           Text(
             msg,
