d48159c68af01ca9f38db0d3f427ec79764e8b4d
Jinho Hyeon <neverlish@gmail.com>
2024-02-04 17:19:05 +0900

    07 - 32 TextField 입력필드 위젯 사용법

--- flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
+++ flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
@@ -9,6 +9,7 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   List lstHello = ['홍드로이드', '안녕하세요', '반갑습니다', '즐거운 식사시간 되세요'];
+  TextEditingController idController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +17,22 @@ class _MainScreenState extends State<MainScreen> {
       appBar: AppBar(
         title: const Text('메인 화면'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text("${lstHello[index]}"),
-            subtitle: const Text('서브타이틀'),
-          );
-        },
-        itemCount: lstHello.length,
+      body: Column(
+        children: [
+          TextField(
+            controller: idController,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: '아이디를 입력해주세요.',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print(idController.text);
+            },
+            child: const Text('아이디 입력 값 가져오기 !'),
+          ),
+        ],
       ),
     );
   }
