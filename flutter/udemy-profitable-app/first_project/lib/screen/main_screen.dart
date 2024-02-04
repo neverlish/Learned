522cff7577e2b392c84d08b5c4b85283d76d4616
Jinho Hyeon <neverlish@gmail.com>
2024-02-04 16:54:56 +0900

    06 레이아웃과 디자인 - 27 Column, Row, Expanded 위젯 사용법

--- flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
+++ flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
@@ -10,9 +10,35 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('메인 화면 입니다.'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('메인 화면'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('반갑습니다.'),
+            Text('저는 유데미 강의를 듣고있는 학생입니다.'),
+            Text('저는 홍길동입니다.'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text('안녕'),
+                Text('반가워'),
+                Text('이것은 가로 방향으로 쌓는 위젯이야.'),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(flex: 2, child: Text('홍길동')),
+                Expanded(child: Text('홍길동')),
+                Expanded(child: Text('홍길동')),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
