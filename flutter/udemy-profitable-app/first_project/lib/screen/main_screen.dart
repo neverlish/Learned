e39b033fc7b18d40c9f265d1c5a89c49faca68f9
Jinho Hyeon <neverlish@gmail.com>
2024-02-04 17:15:28 +0900

    07 - 31 ListView 위젯 사용법

--- flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
+++ flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
@@ -8,31 +8,22 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  List lstHello = ['홍드로이드', '안녕하세요', '반갑습니다', '즐거운 식사시간 되세요'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('메인 화면'),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(32),
-            height: 70,
-            width: 200,
-            child: ElevatedButton(
-              onPressed: () {
-                print("버튼이 클릭되었습니다.");
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-                foregroundColor: Colors.yellow,
-                elevation: 0,
-              ),
-              child: const Text('눌러보세요!'),
-            ),
-          )
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text("${lstHello[index]}"),
+            subtitle: const Text('서브타이틀'),
+          );
+        },
+        itemCount: lstHello.length,
       ),
     );
   }
