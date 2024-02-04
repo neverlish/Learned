f7dec365b913c95a400b0ac045c00260b62b65b1
Jinho Hyeon <neverlish@gmail.com>
2024-02-04 16:36:50 +0900

    05 - 26 route의 활용

--- flutter/udemy-profitable-app/first_project/lib/main.dart
+++ flutter/udemy-profitable-app/first_project/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:first_project/screen/main_screen.dart';
+import 'package:first_project/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,62 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MainScreen2(),
-    );
-  }
-}
-
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("나의 첫 앱"),
-      ),
-      body: const Text("안녕하세요!"),
-    );
-  }
-}
-
-class MainScreen2 extends StatefulWidget {
-  const MainScreen2({super.key});
-
-  @override
-  State<MainScreen2> createState() => _MainScreen2State();
-}
-
-class _MainScreen2State extends State<MainScreen2> {
-  String msg = '홍드로이드';
-
-  @override
-  void initState() {
-    super.initState();
-
-    Future.delayed(const Duration(seconds: 3), () {
-      setState(() {
-        msg = "홍길동";
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("나의 첫 앱"),
-      ),
-      body: Text(msg),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/main': (context) => const MainScreen(),
+      },
     );
   }
 }
---
+++ flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('메인 화면 입니다.'),
+      ),
+    );
+  }
+}
