d65e95f4743d78e0d5541782ca82e72bc73f4bbe
Jinho Hyeon <neverlish@gmail.com>
2024-02-04 17:11:55 +0900

    07 사용자 입력과 상호작용 - 30 ElevatedButton 위젯 사용법

--- flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
+++ flutter/udemy-profitable-app/first_project/lib/screen/main_screen.dart
@@ -14,63 +14,25 @@ class _MainScreenState extends State<MainScreen> {
       appBar: AppBar(
         title: const Text('메인 화면'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('반갑습니다.'),
-            const Text('저는 유데미 강의를 듣고있는 학생입니다.'),
-            const Text('저는 홍길동입니다.'),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text('안녕'),
-                Text('반가워'),
-                Text('이것은 가로 방향으로 쌓는 위젯이야.'),
-              ],
-            ),
-            const Row(
-              children: [
-                Expanded(flex: 2, child: Text('홍길동')),
-                Expanded(child: Text('홍길동')),
-                Expanded(child: Text('홍길동')),
-              ],
-            ),
-            Container(
-              width: 300,
-              height: 100,
-              margin: const EdgeInsets.all(32),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(10),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(32),
+            height: 70,
+            width: 200,
+            child: ElevatedButton(
+              onPressed: () {
+                print("버튼이 클릭되었습니다.");
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.yellow,
+                elevation: 0,
               ),
-              child: const Text('홍드로이드'),
+              child: const Text('눌러보세요!'),
             ),
-            const Text(
-              '홍드로이드2',
-              style: TextStyle(
-                color: Colors.green,
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-              ),
-            ),
-            Row(
-              children: [
-                Image.asset(
-                  "assets/car.png",
-                  width: 100,
-                  height: 100,
-                ),
-                const Icon(
-                  Icons.home_filled,
-                  size: 100,
-                ),
-              ],
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
