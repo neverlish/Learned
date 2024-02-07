873cbbfc26032f934bb30b517821cbddb0cb8fd0
Jinho Hyeon <neverlish@gmail.com>
2024-02-07 11:29:53 +0900

    12 - 40 appbar 위젯 구현

--- flutter/udemy-profitable-app/business_card/lib/screens/main_screen.dart
+++ flutter/udemy-profitable-app/business_card/lib/screens/main_screen.dart
@@ -10,6 +10,24 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: const Icon(
+          Icons.accessibility_new,
+          color: Colors.black,
+          size: 32,
+        ),
+        title: const Text(
+          '발전하는 개발자 홍드로이드를 소개 합니다.',
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
   }
 }
