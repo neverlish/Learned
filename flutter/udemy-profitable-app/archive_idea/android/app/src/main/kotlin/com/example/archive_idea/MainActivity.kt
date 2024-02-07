970194479e6d311ff6df581b12a4e90b57d287ce
Jinho Hyeon <neverlish@gmail.com>
2024-02-07 12:41:08 +0900

    13 최종 프로젝트 #2 아이디어를 기록하는 노트 앱 만들기 - 44 프로젝트 준비 및 시작 화면 구현

---
+++ flutter/udemy-profitable-app/archive_idea/android/app/src/main/kotlin/com/example/archive_idea/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.archive_idea
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/udemy-profitable-app/archive_idea/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:archive_idea/screen/main_screen.dart';
+import 'package:archive_idea/screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Business Card',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/main': (context) => const MainScreen(),
+      },
+    );
+  }
+}
