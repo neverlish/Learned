4fdc67571c52abe338d2699e53fb92b8ba8e0503
Jinho Hyeon <neverlish@gmail.com>
2024-02-09 09:02:47 +0900

    14 - 54 News API를 활용하여 데이터 수신

--- flutter/udemy-profitable-app/daily_news/lib/screen/main_screen.dart
+++ flutter/udemy-profitable-app/daily_news/lib/screen/main_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,6 +11,34 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  List<dynamic> lstNewsInfo = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getNewsInfo();
+  }
+
+  Future getNewsInfo() async {
+    const apiKey = 'YOUR_API_KEY';
+    const apiUrl =
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=$apiKey";
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> responseData = json.decode(response.body);
+        lstNewsInfo = responseData['articles'];
+        for (var element in lstNewsInfo) {
+          print(element['title']);
+        }
+      } else {
+        throw Exception('Failed to load news');
+      }
+    } catch (e) {
+      print('Failed to load news');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
