1904ba876c436c32dbfb61aeb066ff9fcbfb8f00
Jinho Hyeon <neverlish@gmail.com>
2024-02-23 01:00:59 +0900

    07 - 38 CSV 에이전트

--- llm/udemy-langchain-llm-based-application/code-interpreter/main.py
+++ llm/udemy-langchain-llm-based-application/code-interpreter/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from langchain.agents import AgentType
 from langchain_experimental.agents.agent_toolkits.python.base import create_python_agent
+from langchain_experimental.agents.agent_toolkits.csv.base import create_csv_agent
 from langchain_community.chat_models import ChatOpenAI
 from langchain_experimental.tools import PythonREPLTool
 
@@ -16,10 +17,20 @@ def main():
         agent_executor_kwargs={"handle_parsing_errors": True},
     )
 
-    python_agent_executor.run(
-        """generate and save in current working directory 15 QRcodes
-                                that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
+    # python_agent_executor.run(
+    #     """generate and save in current working directory 15 QRcodes
+    #                             that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
+    # )
+
+    csv_agent = create_csv_agent(
+        llm=ChatOpenAI(temperature=0, model="gpt-4"),
+        path="episode_info.csv",
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=True,
     )
+
+    # csv_agent.run("how many columns are there in fils episode_info.csv")
+    csv_agent.run("which writer wrote the most episodes? how many episodes did he write?")
 
 
 if __name__ == "__main__":
