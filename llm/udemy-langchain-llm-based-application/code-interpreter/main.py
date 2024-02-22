e99b6b03246db361d19fb4f2fc05f07815ff8c22
Jinho Hyeon <neverlish@gmail.com>
2024-02-23 00:46:54 +0900

    07 - 37 파이썬 에이전트

--- llm/udemy-langchain-llm-based-application/code-interpreter/main.py
+++ llm/udemy-langchain-llm-based-application/code-interpreter/main.py
@@ -1,9 +1,26 @@
 from dotenv import load_dotenv
+from langchain.agents import AgentType
+from langchain_experimental.agents.agent_toolkits.python.base import create_python_agent
+from langchain_community.chat_models import ChatOpenAI
+from langchain_experimental.tools import PythonREPLTool
 
 load_dotenv()
 
 def main():
     print("Start...")
+    python_agent_executor = create_python_agent(
+        llm=ChatOpenAI(temperature=0, model="gpt-4"),
+        tool=PythonREPLTool(),
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=True,
+        agent_executor_kwargs={"handle_parsing_errors": True},
+    )
+
+    python_agent_executor.run(
+        """generate and save in current working directory 15 QRcodes
+                                that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
+    )
+
 
 if __name__ == "__main__":
     main()
