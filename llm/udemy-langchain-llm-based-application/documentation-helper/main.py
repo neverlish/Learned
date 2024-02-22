2d7b3f0dda615d83cd832947b3536a035779e846
Jinho Hyeon <neverlish@gmail.com>
2024-02-22 23:15:46 +0900

    06 - 34 AI 랭체인 대화 어시스턴트 구축하기 - 메모리 및 ConversationalRetrievalChain

--- llm/udemy-langchain-llm-based-application/documentation-helper/main.py
+++ llm/udemy-langchain-llm-based-application/documentation-helper/main.py
@@ -15,6 +15,10 @@ if "user_prompt_history" not in st.session_state:
 if "chat_answers_history" not in st.session_state:
     st.session_state["chat_answers_history"] = []
 
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
+
 def create_sources_string(source_urls: Set[str]) -> str:
     if not source_urls:
         return ""
@@ -28,7 +32,9 @@ def create_sources_string(source_urls: Set[str]) -> str:
 
 if prompt:
     with st.spinner("Generating response.."):
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(
+            query=prompt, chat_history=st.session_state["chat_history"]
+        )
         sources = set(
             [doc.metadata["source"] for doc in generated_response["source_documents"]]
         )
@@ -39,6 +45,7 @@ if prompt:
 
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answers_history"].append(formatted_response)
+        st.session_state["chat_history"].append((prompt, generated_response["answer"]))
 
 if st.session_state["chat_answers_history"]:
     for generated_response, user_query in zip(
