288b06e00dd21826028666061e18eacf0dbab3c5
Jinho Hyeon <neverlish@gmail.com>
2024-02-24 00:10:47 +0900

    07 - 39 모든 것을 종합하기: 라우터 에이전트 + OpenAI 함수

--- llm/udemy-langchain-llm-based-application/code-interpreter/main.py
+++ llm/udemy-langchain-llm-based-application/code-interpreter/main.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
 from langchain.agents import AgentType
+from langchain.agents.initialize import initialize_agent
 from langchain_experimental.agents.agent_toolkits.python.base import create_python_agent
 from langchain_experimental.agents.agent_toolkits.csv.base import create_csv_agent
 from langchain_community.chat_models import ChatOpenAI
 from langchain_experimental.tools import PythonREPLTool
+from langchain.tools import Tool
 
 load_dotenv()
 
@@ -30,7 +32,35 @@ def main():
     )
 
     # csv_agent.run("how many columns are there in fils episode_info.csv")
-    csv_agent.run("which writer wrote the most episodes? how many episodes did he write?")
+    # csv_agent.run("which writer wrote the most episodes? how many episodes did he write?")
+
+    grand_agent = initialize_agent(
+        tools=[
+            Tool(
+                name="PythonAgent",
+                func=python_agent_executor.run,
+                description="""useful when you need to transform natural language and write from it python and execute the python code,
+                              returning the results of the code execution,
+                            DO NOT SEND PYTHON CODE TO THIS TOOL""",
+            ),
+            Tool(
+                name="CSVAgent",
+                func=csv_agent.run,
+                description="""useful when you need to answer question over episode_info.csv file,
+                             takes an input the entire question and returns the answer after running pandas calculations""",
+            ),
+        ],
+        llm=ChatOpenAI(temperature=0, model="gpt-4"),
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        verbose=True,
+    )
+
+    grand_agent.run(
+        """generate and save in current working directory 15 QRcodes
+                                that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
+    )
+
+    grand_agent.run("print seasons ascending order of the number of episodes they have")
 
 
 if __name__ == "__main__":
