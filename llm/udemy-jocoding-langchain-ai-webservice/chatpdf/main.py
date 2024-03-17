966e28c471da9757359853767429d0372c675775
Jinho Hyeon <neverlish@gmail.com>
2024-03-17 18:21:41 +0900

    5 - 18 ChatPDF - Split 구현하기

--- llm/udemy-jocoding-langchain-ai-webservice/chatpdf/main.py
+++ llm/udemy-jocoding-langchain-ai-webservice/chatpdf/main.py
@@ -3,8 +3,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 
 loader = PyPDFLoader("unsu.pdf")
 pages = loader.load_and_split()
 
-print(pages[0])
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 300,
+    chunk_overlap = 20,
+    length_function = len,
+    is_separator_regex = False,
+)
+
+texts = text_splitter.split_documents(pages)
+print(texts[0])
