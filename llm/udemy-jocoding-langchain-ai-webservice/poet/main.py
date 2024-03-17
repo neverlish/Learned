dcbd38f9b201ad27792fb3565aed80be31eb15df
Jinho Hyeon <neverlish@gmail.com>
2024-03-17 16:36:35 +0900

    3 - 10 인공지능 시인 - Streamlit 프론트엔드 구현하기 (1)

--- llm/udemy-jocoding-langchain-ai-webservice/poet/main.py
+++ llm/udemy-jocoding-langchain-ai-webservice/poet/main.py
@@ -3,10 +3,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_openai import ChatOpenAI
+import streamlit as st
 
 chat_model = ChatOpenAI()
 
-content = "주제"
+st.title('인공지능 시인')
+
+content = st.text_input('시의 주제를 제시해주세요.')
 
 result = chat_model.invoke(content + "에 대한 시를 써줘")
 print(result)
