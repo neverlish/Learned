d1c2dd2e8df32794cff55b4926eea73f22ee3c0f
Jinho Hyeon <neverlish@gmail.com>
2024-03-17 16:19:39 +0900

    2 - 08 LangChain 일단 시작하기

--- llm/udemy-jocoding-langchain-ai-webservice/poet/main.py
+++ llm/udemy-jocoding-langchain-ai-webservice/poet/main.py
@@ -1,2 +1,13 @@
 from dotenv import load_dotenv
+
 load_dotenv()
+
+from langchain_openai import ChatOpenAI, OpenAI
+
+# llm = OpenAI()
+# result = llm.invoke("내가 좋아하는 동물은 ")
+# print(result)
+
+chat_model = ChatOpenAI()
+result = chat_model.invoke("hi!")
+print(result)
