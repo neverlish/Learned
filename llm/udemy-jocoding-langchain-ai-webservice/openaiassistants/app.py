0a05c5fd58865a224a010358811b3c0be27eedb4
Jinho Hyeon <neverlish@gmail.com>
2024-03-20 00:10:38 +0900

    8 - 36 현진건 작가님 봇 만들기 - 기능 구현하기

--- llm/udemy-jocoding-langchain-ai-webservice/openaiassistants/app.py
+++ llm/udemy-jocoding-langchain-ai-webservice/openaiassistants/app.py
@@ -23,3 +23,32 @@ client = OpenAI(api_key=API_KEY)
 #   file_ids=["file-IuysuZnmEyx1e3rGVh3aRawf"]
 # )
 # print(my_assistants)
+
+# thread_yeZ2zXWzAW14JfA8DAFZLJVm
+# thread = client.beta.threads.create()
+# print(thread)
+
+# message = client.beta.threads.messages.create(
+#   thread_id="thread_yeZ2zXWzAW14JfA8DAFZLJVm",
+#   role="user",
+#   content="아내가 먹고 싶어 한 음식이 뭐야?"
+# )
+# print(message)
+
+# run_zmo7FNVIQZgIqwSKv1hw2w4q
+# run = client.beta.threads.runs.create(
+#   thread_id="thread_yeZ2zXWzAW14JfA8DAFZLJVm",
+#   assistant_id="asst_t0peUD7J0mUwJa4MmmAoGfpc",
+# )
+# print(run)
+
+# run = client.beta.threads.runs.retrieve(
+#   thread_id="thread_yeZ2zXWzAW14JfA8DAFZLJVm",
+#   run_id="run_zmo7FNVIQZgIqwSKv1hw2w4q"
+# )
+# print(run)
+
+messages = client.beta.threads.messages.list(
+  thread_id="thread_yeZ2zXWzAW14JfA8DAFZLJVm"
+)
+print(messages.data[0].content[0].text.value)
