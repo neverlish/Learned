5bc0f2c56d7e91cb2384953a9bd7b9e39799973a
Jinho Hyeon <neverlish@gmail.com>
2024-04-03 00:32:41 +0900

    2 - 011 Bloc을 사용하는 첫번째 App

---
+++ flutter/udemy-sangwookcho-flutter-bloc/my_counter_bloc/lib/blocs/counter/counter_state.dart
@@ -0,0 +1,26 @@
+part of 'counter_bloc.dart';
+
+final class CounterState extends Equatable {
+  final int counter;
+  const CounterState({
+    required this.counter,
+  });
+
+  factory CounterState.initial() {
+    return const CounterState(counter: 0);
+  }
+
+  @override
+  List<Object> get props => [counter];
+
+  @override
+  String toString() => 'CounterState(counter: $counter)';
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
+}
