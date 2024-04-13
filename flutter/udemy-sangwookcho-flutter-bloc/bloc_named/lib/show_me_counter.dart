2b8ee9d79c9436be35ab512314136c061edd5b25
Jinho Hyeon <neverlish@gmail.com>
2024-04-13 15:58:53 +0900

    2 - 022 Bloc Access: Named Route Access

---
+++ flutter/udemy-sangwookcho-flutter-bloc/bloc_named/lib/show_me_counter.dart
@@ -0,0 +1,26 @@
+import 'package:bloc_named/cubits/counter/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ShowMeCounter extends StatelessWidget {
+  const ShowMeCounter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Center(
+        child: BlocBuilder<CounterCubit, CounterState>(
+          builder: (context, state) {
+            return Text(
+              '${state.counter}',
+              style: const TextStyle(fontSize: 52.0),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
