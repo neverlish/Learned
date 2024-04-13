45c910859b444919ecfb0a599cafe4d1276a2fa9
Jinho Hyeon <neverlish@gmail.com>
2024-04-13 15:45:16 +0900

    2 - 021 Bloc Access: Anonymous Route Access

---
+++ flutter/udemy-sangwookcho-flutter-bloc/bloc_anonymous/android/app/src/main/kotlin/com/example/bloc_anonymous/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bloc_anonymous
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/udemy-sangwookcho-flutter-bloc/bloc_anonymous/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:bloc_anonymous/cubits/counter/counter_cubit.dart';
+import 'package:bloc_anonymous/show_me_counter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Anonymous Route',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BlocProvider<CounterCubit>(
+        create: (context) => CounterCubit(),
+        child: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (counterContext) {
+                    return BlocProvider.value(
+                      value: context.read<CounterCubit>(),
+                      child: const ShowMeCounter(),
+                    );
+                  }),
+                );
+              },
+              child: const Text(
+                'Show Me Counter',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                BlocProvider.of<CounterCubit>(context).increment();
+              },
+              child: const Text(
+                'Increment Counter',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
