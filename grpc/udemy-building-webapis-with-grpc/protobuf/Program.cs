b00b07da0de5c70edc07e28d6733358d2d3ce330
Jinho Hyeon <neverlish@gmail.com>
2024-04-14 14:49:58 +0900

    07 - 33 Developing with Protobuf

--- grpc/udemy-building-webapis-with-grpc/protobuf/Program.cs
+++ grpc/udemy-building-webapis-with-grpc/protobuf/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Course.Protobuf.Test;
+
+Console.WriteLine("Welcome to Protobuf test!");
+
+var emp = new Employee();
+emp.FirstName = "Memi";
+emp.LastName = "Lavi";
+emp.IsRetired = false;
+
+var birthdate = new DateTime(1976, 7, 9);
+birthdate = DateTime.SpecifyKind(birthdate, DateTimeKind.Utc);
+emp.BirthDate = Timestamp.FromDateTime(birthdate);
+emp.MaritalStatus = Employee.Types.MaritalStatus.Married;
+emp.PreviousEmployers.Add("Microsoft");
+emp.PreviousEmployers.Add("HP");
+
+using (var output = File.Create("emp.dat"))
+{
+  emp.WriteTo(output);
+}
+
+Employee empFromFile;
+
+using (var input = File.OpenRead("emp.dat"))
+{
+  empFromFile = Employee.Parser.ParseFrom(input);
+}
+
+Console.WriteLine("Protobuf test complete :-)");
