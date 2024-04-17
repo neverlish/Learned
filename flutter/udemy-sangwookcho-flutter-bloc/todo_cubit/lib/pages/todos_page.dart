c6c173b881c69e7bcaa743d4d004cb72a5df141f
Jinho Hyeon <neverlish@gmail.com>
2024-04-18 07:30:39 +0900

    3 TODO App - 036 packages, folders, app skeleton

---
+++ flutter/udemy-sangwookcho-flutter-bloc/todo_cubit/lib/pages/todos_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TodosPage extends StatelessWidget {
+  const TodosPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('TODOS'),
+      ),
+    );
+  }
+}
