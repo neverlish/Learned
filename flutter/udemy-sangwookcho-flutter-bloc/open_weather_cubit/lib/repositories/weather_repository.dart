b7e9ff8dfa42255ef11e326d3d624f61abf6df20
Jinho Hyeon <neverlish@gmail.com>
2024-04-28 20:46:33 +0900

    4 - 065 WeatherRepository

---
+++ flutter/udemy-sangwookcho-flutter-bloc/open_weather_cubit/lib/repositories/weather_repository.dart
@@ -0,0 +1,37 @@
+import '../exceptions/weather_exception.dart';
+import '../models/custom_error.dart';
+import '../models/direct_geocoding.dart';
+import '../models/weather.dart';
+import '../services/weather_api_services.dart';
+
+class WeatherRepository {
+  const WeatherRepository({
+    required this.weatherApiServices,
+  });
+
+  final WeatherApiServices weatherApiServices;
+
+  Future<Weather> fetchWeather(String city) async {
+    try {
+      final DirectGeocoding directGeocoding =
+          await weatherApiServices.getDirectGeocoding(city);
+      print('directGeocoding: $directGeocoding');
+
+      final Weather tempWeather =
+          await weatherApiServices.getWeather(directGeocoding);
+
+      final Weather weather = tempWeather.copyWith(
+        name: directGeocoding.name,
+        country: directGeocoding.country,
+      );
+
+      print('weather: $weather');
+
+      return weather;
+    } on WeatherException catch (e) {
+      throw CustomError(errMsg: e.message);
+    } catch (e) {
+      throw CustomError(errMsg: e.toString());
+    }
+  }
+}
