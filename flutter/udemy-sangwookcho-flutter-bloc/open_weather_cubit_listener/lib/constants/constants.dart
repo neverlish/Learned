47d66ad1e1e44d2d31f02a88fbf603d21d60e919
Jinho Hyeon <neverlish@gmail.com>
2024-04-28 21:41:14 +0900

    4 - 073 BlocListener를 이용한 open_weather_cubit refactoring

---
+++ flutter/udemy-sangwookcho-flutter-bloc/open_weather_cubit_listener/lib/constants/constants.dart
@@ -0,0 +1,5 @@
+const String kApiHost = 'api.openweathermap.org';
+const String kIconHost = 'www.openweathermap.org';
+const String kUnit = 'metric';
+const String kLimit = '1';
+const int kWarmOrNot = 20;
