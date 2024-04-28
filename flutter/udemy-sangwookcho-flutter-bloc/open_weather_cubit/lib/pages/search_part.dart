324f6c6839c29a2194fe04bc0f556893b40d770b
Jinho Hyeon <neverlish@gmail.com>
2024-04-28 20:34:53 +0900

    4 Weather App (OpenWeather) - 061 Packages, app folder 구조, app page skeleton

---
+++ flutter/udemy-sangwookcho-flutter-bloc/open_weather_cubit/lib/pages/search_part.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search'),
+      ),
+      body: const Center(
+        child: Text('Search'),
+      ),
+    );
+  }
+}
