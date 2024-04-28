d308c92fca576fa63f8e893f9fc7dacca3c57e31
Jinho Hyeon <neverlish@gmail.com>
2024-04-28 21:10:02 +0900

    4 - 067 SearchPage widget - Form

--- flutter/udemy-sangwookcho-flutter-bloc/open_weather_cubit/lib/pages/home_page.dart
+++ flutter/udemy-sangwookcho-flutter-bloc/open_weather_cubit/lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubits/weather/weather_cubit.dart';
+import 'search_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +12,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? _city;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              // to avoid async gap warning
+              final weatherCubit = context.read<WeatherCubit>();
+
+              _city = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return const SearchPage();
+                }),
+              );
+              print('city: $_city');
+              if (_city != null) {
+                weatherCubit.fetchWeather(_city!);
+              }
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Home'),
