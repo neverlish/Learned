42709d0341c5783d4e82077f4ac7062b13c12fc5
Jinho Hyeon <neverlish@gmail.com>
2024-05-01 01:00:06 +0900

    5 - 088 HomePage widget

--- flutter/udemy-sangwookcho-flutter-bloc/fb_auth_bloc/lib/pages/home_page.dart
+++ flutter/udemy-sangwookcho-flutter-bloc/fb_auth_bloc/lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../blocs/auth/auth_bloc.dart';
+import 'profile_page.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = '/home';
@@ -11,11 +15,48 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const PopScope(
+    return PopScope(
       canPop: false,
       child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('Home'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return const ProfilePage();
+                  }),
+                );
+              },
+              icon: const Icon(Icons.account_circle),
+            ),
+            IconButton(
+              onPressed: () {
+                context.read<AuthBloc>().add(SignoutRequestedEvent());
+              },
+              icon: const Icon(Icons.exit_to_app),
+            ),
+          ],
+        ),
         body: Center(
-          child: Text('Home'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/bloc_logo_full.png',
+                width: MediaQuery.of(context).size.width * 0.8,
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                'Bloc is an awesome\nstate management library\nfor flutter!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
