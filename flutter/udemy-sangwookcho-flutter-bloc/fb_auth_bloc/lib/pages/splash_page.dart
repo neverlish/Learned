66ff474669c0e37a14d1a4554e7344f11957d44f
Jinho Hyeon <neverlish@gmail.com>
2024-05-01 01:10:51 +0900

    5 - 090 ProfilePage

--- flutter/udemy-sangwookcho-flutter-bloc/fb_auth_bloc/lib/pages/splash_page.dart
+++ flutter/udemy-sangwookcho-flutter-bloc/fb_auth_bloc/lib/pages/splash_page.dart
@@ -13,16 +13,25 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<AuthBloc, AuthState>(
       listener: (context, state) {
-        print('listener: $state');
         if (state.authStatus == AuthStatus.unauthenticated) {
-          Navigator.pushNamed(context, SigninPage.routeName);
+          Navigator.pushNamedAndRemoveUntil(
+            context,
+            SigninPage.routeName,
+            (route) {
+              print('route.settings.name: ${route.settings.name}');
+              print('ModalRoute: ${ModalRoute.of(context)!.settings.name}');
+
+              return route.settings.name ==
+                      ModalRoute.of(context)!.settings.name
+                  ? true
+                  : false;
+            },
+          );
         } else if (state.authStatus == AuthStatus.authenticated) {
           Navigator.pushNamed(context, HomePage.routeName);
         }
       },
       builder: (context, state) {
-        print('builder: $state');
-
         return const Scaffold(
           body: Center(
             child: CircularProgressIndicator(),
