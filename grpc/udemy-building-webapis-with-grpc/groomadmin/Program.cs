e6858d3068c3c479195ca8ad192242ee56a158b0
Jinho Hyeon <neverlish@gmail.com>
2024-05-10 19:32:22 +0900

    11 - 52 Implementing the Client

---
+++ grpc/udemy-building-webapis-with-grpc/groomadmin/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using gRoom.gRPC.Messages;
+using Google.Protobuf.WellKnownTypes;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5288");
+var client = new Groom.GroomClient(channel);
+
+Console.WriteLine("*** Admin Console started ***");
+Console.WriteLine("Listening...");
+
+using var call = client.StartMonitoring(new Empty());
+
+var cts = new CancellationTokenSource();
+
+while (await call.ResponseStream.MoveNext(cts.Token))
+{
+  var msg = call.ResponseStream.Current;
+  Console.WriteLine($"New message: {msg.Contents}, user: {msg.User}, at: {msg.MsgTime}");
+}
