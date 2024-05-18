4dd938a3e0d3a5d5019a45473ad55aff0611c908
Jinho Hyeon <neverlish@gmail.com>
2024-05-18 23:56:46 +0900

    11 관계형 데이터베이스 - 086 Go와 SQL – 설치

---
+++ go/udemy-golang-web-dev/032_rdbms/01_connect/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+var err error
+
+func main() {
+	db, err = sql.Open("mysql", "root@tcp(localhost:3306)/")
+	check(err)
+	defer db.Close()
+
+	err = db.Ping()
+	check(err)
+
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	err := http.ListenAndServe(":8080", nil)
+	check(err)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	_, err = io.WriteString(w, "Successfully completed.")
+	check(err)
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
