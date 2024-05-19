db8d05813d615d4e4d4f9ffc74eb116f6ad8771c
Jinho Hyeon <neverlish@gmail.com>
2024-05-19 19:58:19 +0900

    15 - 115 MVC 디자인 패턴 – 모델 뷰 컨트롤러

---
+++ go/udemy-golang-web-dev/042_mongodb/04_controllers/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"main/controllers"
+	"net/http"
+
+	httprouter "github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController()
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+}
