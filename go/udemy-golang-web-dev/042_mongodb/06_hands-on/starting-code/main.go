a61c2129fe542f56b5c4dcab3d3f0004fdd1eedb
Jinho Hyeon <neverlish@gmail.com>
2024-05-19 22:40:37 +0900

    15 - 119 실전 연습 및 해답

---
+++ go/udemy-golang-web-dev/042_mongodb/06_hands-on/starting-code/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	httprouter "github.com/julienschmidt/httprouter"
+
+	"net/http"
+
+	mgo "gopkg.in/mgo.v2"
+)
+
+func main() {
+	r := httprouter.New()
+	// Get a UserController instance
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+}
+
+func getSession() *mgo.Session {
+	// Connect to our local mongo
+	s, err := mgo.Dial("mongodb://localhost")
+
+	// Check if connection error, is mongo running?
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
---
+++ go/udemy-golang-web-dev/042_mongodb/07_solution/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	httprouter "github.com/julienschmidt/httprouter"
+
+	"net/http"
+)
+
+func main() {
+	r := httprouter.New()
+	// Get a UserController instance
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+}
+
+func getSession() map[string]models.User {
+	return make(map[string]models.User)
+}
