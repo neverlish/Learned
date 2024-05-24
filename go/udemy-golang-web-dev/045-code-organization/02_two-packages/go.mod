45cd1ac0fb3c6d907a9fe7d6df014998ed7f7de7
Jinho Hyeon <neverlish@gmail.com>
2024-05-25 01:14:56 +0900

    15 - 154 코드 정리

---
+++ go/udemy-golang-web-dev/045-code-organization/02_two-packages/go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.22.2
+
+replace github.com/models => ./models
+
+require github.com/models v0.0.0-00010101000000-000000000000
+
+require github.com/lib/pq v1.10.9 // indirect
---
+++ go/udemy-golang-web-dev/045-code-organization/02_two-packages/models/db.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "postgres://postgres@localhost/bookstore?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+	fmt.Println("You connected to your database.")
+}
