923bd6040eb06122cda1d64f85b3236c3fc5d78b
Jinho Hyeon <neverlish@gmail.com>
2024-05-25 15:15:09 +0900

    03 Dart Data Class Generator extension과 Freezed package - 006 Dart Data Class Generator - Introduction

---
+++ flutter/udemy-sangwookcho-flutter-riverpod/dart_data_class_generator/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'pages/person_page.dart';
+import 'widgets/custom_button.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dart Data Class Generator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(20),
+          children: const [
+            CustomButton(
+              title: 'Person',
+              child: PersonPage(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
