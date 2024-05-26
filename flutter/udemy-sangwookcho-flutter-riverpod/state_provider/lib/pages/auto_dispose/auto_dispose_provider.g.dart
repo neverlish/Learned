d041662a835f319baadb16bf4846fd3f7f8d9c2b
Jinho Hyeon <neverlish@gmail.com>
2024-05-27 00:02:35 +0900

    04 - 024 StateProvider - autoDispose

---
+++ flutter/udemy-sangwookcho-flutter-riverpod/state_provider/lib/pages/auto_dispose/auto_dispose_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auto_dispose_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$autoDisposeAgeHash() => r'47b6983db3fa16f27b197da71835e9f694a3a1cf';
+
+/// See also [autoDisposeAge].
+@ProviderFor(autoDisposeAge)
+final autoDisposeAgeProvider = AutoDisposeProvider<String>.internal(
+  autoDisposeAge,
+  name: r'autoDisposeAgeProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$autoDisposeAgeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AutoDisposeAgeRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
