c0c69a4ead2619ab6e95600445f17b900816dfc1
Jinho Hyeon <neverlish@gmail.com>
2024-05-31 22:19:02 +0900

    03 - 13 Word Count 애플리케이션 토폴로지

--- kafka/udemy-apache-kafka-streams-data-processing/word-count/src/main/java/com/neverlish/Main.java
+++ kafka/udemy-apache-kafka-streams-data-processing/word-count/src/main/java/com/neverlish/Main.java
@@ -3,7 +3,11 @@ package com.neverlish;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.kstream.KTable;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 public class Main {
@@ -15,5 +19,16 @@ public class Main {
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
+        KStreamBuilder builder = new KStreamBuilder();
+        KStream<String, String> wordCountInput = builder.stream("word-count-input");
+
+        KTable<String, Long> wordCounts = wordCountInput
+            .mapValues(value -> value.toLowerCase())
+            .flatMapValues(lowerCasedText -> Arrays.asList(lowerCasedText.split(" ")))
+            .selectKey((ignoredKey, word) -> word)
+            .groupByKey()
+            .count("Counts");
+
+        wordCounts.to(Serdes.String(), Serdes.Long(), "word-count-output");
     }
 }
