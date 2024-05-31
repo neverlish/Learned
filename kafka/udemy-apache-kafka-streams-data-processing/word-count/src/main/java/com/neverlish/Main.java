21c6f9721a012946e59e4b44e6d0efd6952b0142
Jinho Hyeon <neverlish@gmail.com>
2024-05-31 22:05:49 +0900

    03 - 11 Kafka Streams 애플리케이션 속성

--- kafka/udemy-apache-kafka-streams-data-processing/word-count/src/main/java/com/neverlish/Main.java
+++ kafka/udemy-apache-kafka-streams-data-processing/word-count/src/main/java/com/neverlish/Main.java
@@ -1,7 +1,19 @@
 package com.neverlish;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-starter-app");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
     }
 }
