5c78fdd67f73240e357791c0d7488ce5d0b26f7c
Jinho Hyeon <neverlish@gmail.com>
2024-06-02 15:46:25 +0900

    10 - 034 Custom Production Error Handler

--- kafka/udemy-kafka-streams-api-for-developers-using-java-springboot/explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/launcher/GreetingsStreamApp.java
+++ kafka/udemy-kafka-streams-api-for-developers-using-java-springboot/explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/launcher/GreetingsStreamApp.java
@@ -26,6 +26,7 @@ public class GreetingsStreamApp {
         properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         properties.put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG, StreamsDeserializationExceptionHandler.class);
+        properties.put(StreamsConfig.DEFAULT_PRODUCTION_EXCEPTION_HANDLER_CLASS_CONFIG, StreamsProcessorCustomErrorHandler.class);
 
         properties.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, "2");
 
