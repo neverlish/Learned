867dbe118eb04bf8edc10ce994915ea8a32fc132
Jinho Hyeon <neverlish@gmail.com>
2024-06-02 18:43:10 +0900

    11 KTable & Global KTable - 037 Build a topology for KTable

---
+++ kafka/udemy-kafka-streams-api-for-developers-using-java-springboot/explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/ExploreKTableTopology.java
@@ -0,0 +1,27 @@
+package com.learnkafkastreams.topology;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Printed;
+
+public class ExploreKTableTopology {
+    public static String WORDS = "words";
+
+    public static Topology build() {
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        var wordsTable = streamsBuilder.table(WORDS,
+                Consumed.with(Serdes.String(), Serdes.String()), Materialized.as("words-store"));
+
+        wordsTable
+                .filter((key, value) -> value.length() > 2)
+                .toStream()
+                .peek((key, value) -> log.info("Key: {}, value: {}", key, value))
+                .print(Printed.<String, String>toSysOut().withLabel("words-ktable"));
+
+        return streamsBuilder.build();
+    }
+}
