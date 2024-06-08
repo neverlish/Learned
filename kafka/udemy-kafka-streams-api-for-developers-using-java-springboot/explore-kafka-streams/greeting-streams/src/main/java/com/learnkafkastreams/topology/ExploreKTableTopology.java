0fbcde7c0def3fb5f07b5e2bc12e2598f26c5a3d
Jinho Hyeon <neverlish@gmail.com>
2024-06-08 14:34:12 +0900

    28 Testing Kafka Streams Using TopologyTestDriver & JUnit5 - 098 Unit Testing Greetings App - Writing Data to a Output Topic

--- kafka/udemy-kafka-streams-api-for-developers-using-java-springboot/explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/ExploreKTableTopology.java
+++ kafka/udemy-kafka-streams-api-for-developers-using-java-springboot/explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/ExploreKTableTopology.java
@@ -1,5 +1,6 @@
 package com.learnkafkastreams.topology;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
@@ -7,6 +8,7 @@ import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Printed;
 
+@Slf4j
 public class ExploreKTableTopology {
     public static String WORDS = "words";
 
