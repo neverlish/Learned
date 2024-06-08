f251763296def971f7ea8f65e2ac2efea209e3ba
Jinho Hyeon <neverlish@gmail.com>
2024-06-08 11:38:14 +0900

    26 Interactive Queries - Querying State Stores using RESTFUL APIs - 087 Build a GET Endpoint to retrieve the OrderCount by OrderType - Part 1

--- kafka/udemy-kafka-streams-api-for-developers-using-java-springboot/orders-management-streams/orders-streams-app/src/main/java/com/learnkafkastreams/service/OrderStoreService.java
+++ kafka/udemy-kafka-streams-api-for-developers-using-java-springboot/orders-management-streams/orders-streams-app/src/main/java/com/learnkafkastreams/service/OrderStoreService.java
@@ -3,12 +3,24 @@ package com.learnkafkastreams.service;
 import org.apache.kafka.streams.StoreQueryParameters;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
-import org.apache.kafka.streams.state.ReadOnlyWindowStore;
 import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderStoreService {
+    StreamsBuilderFactoryBean streamsBuilderFactoryBean;
 
+    public OrderStoreService(StreamsBuilderFactoryBean streamsBuilderFactoryBean) {
+        this.streamsBuilderFactoryBean = streamsBuilderFactoryBean;
+    }
+
+    public ReadOnlyKeyValueStore<String, Long> ordersCountStore(String storeName) {
+        return streamsBuilderFactoryBean
+                .getKafkaStreams()
+                .store(StoreQueryParameters.fromNameAndType(
+                        storeName,
+                        QueryableStoreTypes.keyValueStore()
+                ));
+    }
 
 }
