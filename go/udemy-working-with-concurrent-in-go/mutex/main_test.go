d26cd5ced4b8aa8127362a43101728bf25bf095d
Jinho Hyeon <neverlish@gmail.com>
2024-06-09 01:03:51 +0900

    03 - 19 Writing a test for our weekly income project

---
+++ go/udemy-working-with-concurrent-in-go/mutex/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+func Test_main(t *testing.T) {
+	stdOut := os.Stdout
+	r, w, _ := os.Pipe()
+
+	os.Stdout = w
+	
+	main()
+
+	_ = w.Close()
+
+	result, _ := io.ReadAll(r)
+	output := string(result)
+
+	os.Stdout = stdOut
+
+	if !strings.Contains(output, "$34320.00") {
+		t.Error("wrong balance returned")
+	}
+	
+}
