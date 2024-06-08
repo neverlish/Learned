7fbc43e3f0dad64062b878991bd58d023f0d2615
Jinho Hyeon <neverlish@gmail.com>
2024-06-08 23:47:27 +0900

    02 - 10 WaitGroups to the rescue

--- go/udemy-working-with-concurrent-in-go/first-example/main.go
+++ go/udemy-working-with-concurrent-in-go/first-example/main.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println(s)
 }
 
 func main() {
-	go printSomething("This is the first thing to be printed!")
+	var wg sync.WaitGroup
+	words := []string{
+		"alpha",
+		"beta",
+		"delta",
+		"gamma",
+		"pi",
+		"zeta",
+		"eta",
+		"theta",
+		"epsilon",
+	}
 
-	time.Sleep(1 * time.Second)
+	wg.Add(len(words))
+
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+
+	wg.Wait()
 	
-	printSomething("This is the second thing to be printed!")
+	wg.Add(1)
+	printSomething("This is the second thing to be printed!", &wg)
 }
