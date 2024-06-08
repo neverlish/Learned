c5c8c3315fc532bc5a603ec3fc966ac190617721
Jinho Hyeon <neverlish@gmail.com>
2024-06-09 00:26:27 +0900

    03 - 16 Adding sync.Mutex to our code

--- go/udemy-working-with-concurrent-in-go/mutex/main.go
+++ go/udemy-working-with-concurrent-in-go/mutex/main.go
@@ -8,17 +8,21 @@ import (
 var msg string
 var wg sync.WaitGroup
 
-func updateMessage(s string) {
+func updateMessage(s string, m *sync.Mutex) {
 	defer wg.Done()
+	m.Lock()
 	msg = s
+	m.Unlock()
 }
 
 func main() {
 	msg = "Hello, world!"
 
+	var mutex sync.Mutex
+
 	wg.Add(2)
-	go updateMessage("Hello, universe!")
-	go updateMessage("Hello, cosmos!")
+	go updateMessage("Hello, universe!", &mutex)
+	go updateMessage("Hello, cosmos!", &mutex)
 	wg.Wait()
 
 	fmt.Println(msg)
