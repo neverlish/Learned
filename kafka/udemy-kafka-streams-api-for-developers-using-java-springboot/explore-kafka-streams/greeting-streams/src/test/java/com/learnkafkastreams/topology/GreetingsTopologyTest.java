1ffc6495c9ac8d6dd253a9ee7fc88c9ff401066d
Jinho Hyeon <neverlish@gmail.com>
2024-06-08 14:38:30 +0900

    28 - 099 Unit Testing Greetings App - Testing Multiple Messages

--- kafka/udemy-kafka-streams-api-for-developers-using-java-springboot/explore-kafka-streams/greeting-streams/src/test/java/com/learnkafkastreams/topology/GreetingsTopologyTest.java
+++ kafka/udemy-kafka-streams-api-for-developers-using-java-springboot/explore-kafka-streams/greeting-streams/src/test/java/com/learnkafkastreams/topology/GreetingsTopologyTest.java
@@ -3,6 +3,7 @@ package com.learnkafkastreams.topology;
 import com.learnkafkastreams.domain.Greeting;
 import com.learnkafkastreams.serdes.SerdesFactory;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.TestInputTopic;
 import org.apache.kafka.streams.TestOutputTopic;
 import org.apache.kafka.streams.TopologyTestDriver;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -44,5 +46,25 @@ class GreetingsTopologyTest {
         assertEquals("GOOD MORNING!", outputValue.value.message());
         assertNotNull(outputValue.value.timestamp());
 
+    }
+
+    @Test
+    void buildTopology_multipleInput() {
+        var greeting1 = KeyValue.pair("GM", new Greeting("Good Morning!", LocalDateTime.now()));
+        var greeting2 = KeyValue.pair("GN", new Greeting("Good Night!", LocalDateTime.now()));
+        inputTopic.pipeKeyValueList(List.of(greeting1, greeting2));
+
+        var count = outputTopic.getQueueSize();
+        assertEquals(2, count);
+
+        var outputValues = outputTopic.readKeyValuesToList();
+
+        var outputValue1 = outputValues.get(0);
+        assertEquals("GOOD MORNING!", outputValue1.value.message());
+        assertNotNull(outputValue1.value.timestamp());
+
+        var outputValue2 = outputValues.get(1);
+        assertEquals("GOOD NIGHT!", outputValue2.value.message());
+        assertNotNull(outputValue2.value.timestamp());
     }
 }
