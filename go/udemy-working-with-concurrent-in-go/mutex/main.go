ca769e62fd04a5d058b8c4feeb5a1b3371486884
Jinho Hyeon <neverlish@gmail.com>
2024-06-09 01:00:44 +0900

    03 - 18 A more complex example

--- go/udemy-working-with-concurrent-in-go/mutex/main.go
+++ go/udemy-working-with-concurrent-in-go/mutex/main.go
@@ -5,22 +5,47 @@ import (
 	"sync"
 )
 
-var msg string
 var wg sync.WaitGroup
 
-func updateMessage(s string) {
-	defer wg.Done()
-	msg = s
+type Income struct {
+	Source string
+	Amount int
 }
 
 func main() {
-	msg = "Hello, world!"
+	var bankBalance int
+	var balance sync.Mutex
 
-	wg.Add(2)
-	go updateMessage("Hello, universe!")
-	go updateMessage("Hello, cosmos!")
+	fmt.Printf("Initial account balance: $%d.00", bankBalance)
+	fmt.Println()
+
+	incomes := []Income{
+		{ Source: "Main job", Amount: 500 },
+		{ Source: "Gifts", Amount: 10 },
+		{ Source: "Part time job", Amount: 50 },
+		{ Source: "Investments", Amount: 100 },
+	}
+
+	wg.Add(len(incomes))
+
+	for i, income := range incomes {
+		go func(i int, income Income) {
+			defer wg.Done()
+			for week := 1; week <= 52; week++ {
+				balance.Lock()
+				temp := bankBalance
+				temp += income.Amount
+				bankBalance = temp
+				balance.Unlock()
+				
+				fmt.Printf("On week %d, you earned $%d.00 from %s\n", week, income.Amount, income.Source)
+			}
+		}(i, income)
+
+		
+	}
 	wg.Wait()
 
-	fmt.Println(msg)
-	
+	fmt.Printf("Final bank balance: $%d.00", bankBalance)
+	fmt.Println()
 }
