60bcc59e9c1385621d58ef4181d1a5b04996b543
Jinho Hyeon <neverlish@gmail.com>
2024-06-10 08:40:00 +0900

    06 - 45 Adding postgres

--- go/udemy-working-with-concurrent-in-go/final-project/cmd/web/main.go
+++ go/udemy-working-with-concurrent-in-go/final-project/cmd/web/main.go
@@ -1,7 +1,68 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
 const webPort = "80"
 
 func main() {
-	
+	db := initDB()
+
+	db.Ping()
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+
+	if conn == nil {
+		log.Panic("can't connect to database")
+	}
+
+}
+
+func connectToDB() *sql.DB {
+	counts := 0
+
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("postgres not yet ready...")
+		} else {
+			log.Print("connected to database!")
+			return connection
+		}
+
+		if counts > 10 {
+			return nil
+		}
+
+		log.Print("Backing off for 1 seconds")
+		time.Sleep(1 * time.Second)
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping();
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
