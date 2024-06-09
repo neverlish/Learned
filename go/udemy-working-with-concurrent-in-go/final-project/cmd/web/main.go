3a2f324076ffe4b90883276e5ac20dfd4b70e232
Jinho Hyeon <neverlish@gmail.com>
2024-06-10 08:50:33 +0900

    06 - 47 Adding sessions & Redis

--- go/udemy-working-with-concurrent-in-go/final-project/cmd/web/main.go
+++ go/udemy-working-with-concurrent-in-go/final-project/cmd/web/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
 	"os"
 	"time"
+
+	redisstore "github.com/alexedwards/scs/redisstore"
+	scs "github.com/alexedwards/scs/v2"
+	redis "github.com/gomodule/redigo/redis"
 
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
@@ -17,6 +22,8 @@ func main() {
 	db := initDB()
 
 	db.Ping()
+
+	session := initSession()
 }
 
 func initDB() *sql.DB {
@@ -66,4 +73,25 @@ func openDB(dsn string) (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+func initSession() *scs.SessionManager {
+	session := scs.New()
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+	return session
+}
+
+func initRedis() *redis.Pool {
+	redisPool := &redis.Pool{
+		MaxIdle: 10,
+		Dial: func() (redis.Conn,error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+
+	return redisPool
 }
