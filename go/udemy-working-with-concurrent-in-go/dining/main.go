3b40fbd7346b6109b62a0bc96f5d7de6b4b26c63
Jinho Hyeon <neverlish@gmail.com>
2024-06-10 00:27:58 +0900

    04 - 30 Solution to challenge

--- go/udemy-working-with-concurrent-in-go/dining/main.go
+++ go/udemy-working-with-concurrent-in-go/dining/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"sync"
 	"time"
 )
@@ -25,15 +26,24 @@ var eatTime = 1 * time.Second
 var thinkTime = 3 * time.Second
 var sleepTime = 1 * time.Second
 
+var orderMutex sync.Mutex
+var orderFinished []string
+
 
 func main() {
 	fmt.Println("Dining Philosophers Problem")
 	fmt.Println("---------------------------")	
 	fmt.Println("The table is empty.")
 
+	time.Sleep(sleepTime)
+
 	dine()
 
 	fmt.Println("The table is empty.")
+
+
+	time.Sleep(sleepTime)
+	fmt.Printf("Order finished: %s.\n", strings.Join(orderFinished, ", "))
 
 }
 
@@ -96,5 +106,8 @@ func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*s
 
 	fmt.Println(philosopher.name, "is satisfied.")
 	fmt.Println(philosopher.name, "left the table.")
+	orderMutex.Lock()
+	orderFinished = append(orderFinished, philosopher.name)
+	orderMutex.Unlock()
 
 }
