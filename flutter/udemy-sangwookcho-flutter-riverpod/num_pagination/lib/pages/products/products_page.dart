21e9af09e777307da89a558296d04e909c9d0bee
Jinho Hyeon <neverlish@gmail.com>
2024-06-13 22:51:52 +0900

    10 - 083 Numbered Pagination: ProductsPage

--- flutter/udemy-sangwookcho-flutter-riverpod/num_pagination/lib/pages/products/products_page.dart
+++ flutter/udemy-sangwookcho-flutter-riverpod/num_pagination/lib/pages/products/products_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:num_pagination/pages/products/products_providers.dart';
+import 'package:num_pagination/repositories/product_repository.dart';
+
+import 'package:number_paginator/number_paginator.dart';
 
 class ProductsPage extends ConsumerStatefulWidget {
   const ProductsPage({super.key});
@@ -9,11 +13,55 @@ class ProductsPage extends ConsumerStatefulWidget {
 }
 
 class _ProductsPageState extends ConsumerState<ProductsPage> {
+  int page = 1;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Product List'),
+    final productList = ref.watch(getProductsProvider(page));
+
+    return SafeArea(
+      child: Scaffold(
+        body: productList.when(
+          data: (products) {
+            return ListView.separated(
+              itemCount: products.length,
+              separatorBuilder: (BuildContext context, int index) {
+                return const Divider();
+              },
+              itemBuilder: (BuildContext context, int index) {
+                final product = products[index];
+                return ListTile(
+                  title: Text(product.title),
+                  subtitle: Text(product.description),
+                );
+              },
+            );
+          },
+          error: (e, st) => Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text(
+              e.toString(),
+              style: Theme.of(context).textTheme.headlineMedium,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          loading: () => const Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+        bottomNavigationBar: totalProducts == 0 && totalPages == 1
+            ? const SizedBox.shrink()
+            : Card(
+                margin: EdgeInsets.zero,
+                elevation: 4,
+                child: NumberPaginator(
+                  numberPages: totalPages,
+                  onPageChange: (int index) {
+                    setState(() {
+                      page = index + 1;
+                    });
+                  },
+                ),
+              ),
       ),
     );
   }
