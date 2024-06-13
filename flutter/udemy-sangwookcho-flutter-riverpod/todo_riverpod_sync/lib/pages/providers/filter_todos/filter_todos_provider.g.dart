c6ee912c120ee5491216fa4150ad51d422c5128b
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 00:26:51 +0900

    12 - 103 filteredTodosProvider

---
+++ flutter/udemy-sangwookcho-flutter-riverpod/todo_riverpod_sync/lib/pages/providers/filter_todos/filter_todos_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'filter_todos_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$filteredTodosHash() => r'90f0d92c32219e35e759d43711d7bb21fb4491a3';
+
+/// See also [filteredTodos].
+@ProviderFor(filteredTodos)
+final filteredTodosProvider = AutoDisposeProvider<List<Todo>>.internal(
+  filteredTodos,
+  name: r'filteredTodosProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$filteredTodosHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FilteredTodosRef = AutoDisposeProviderRef<List<Todo>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
