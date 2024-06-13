1fe329c4d91ed079521c0879c2a8e6f0879afba7
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 00:14:04 +0900

    12 Todo App - 097 packages, folders, app skeleton

---
+++ flutter/udemy-sangwookcho-flutter-riverpod/todo_riverpod_sync/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'pages/todos_page.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todos',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const TodosPage(),
+    );
+  }
+}
---
+++ flutter/udemy-sangwookcho-flutter-riverpod/todo_riverpod_sync/lib/pages/todos_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TodosPage extends StatelessWidget {
+  const TodosPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('TODOS'),
+      ),
+    );
+  }
+}
