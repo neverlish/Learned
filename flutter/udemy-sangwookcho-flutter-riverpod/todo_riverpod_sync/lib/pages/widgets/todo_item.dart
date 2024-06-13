cc752afbeefda307b019d81fc80e6a7825366209
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 01:03:09 +0900

    12 - 110 TodoItem widget - 2

--- flutter/udemy-sangwookcho-flutter-riverpod/todo_riverpod_sync/lib/pages/widgets/todo_item.dart
+++ flutter/udemy-sangwookcho-flutter-riverpod/todo_riverpod_sync/lib/pages/widgets/todo_item.dart
@@ -10,6 +10,14 @@ class TodoItem extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ListTile(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return ConfirmEditDialog(todo: todo);
+          },
+        );
+      },
       leading: Checkbox(
         value: todo.complete,
         onChanged: (value) {
@@ -45,6 +53,66 @@ class TodoItem extends ConsumerWidget {
         },
         icon: const Icon(Icons.delete),
       ),
+    );
+  }
+}
+
+class ConfirmEditDialog extends ConsumerStatefulWidget {
+  final Todo todo;
+  const ConfirmEditDialog({super.key, required this.todo});
+
+  @override
+  _ConfirmEditDialogState createState() => _ConfirmEditDialogState();
+}
+
+class _ConfirmEditDialogState extends ConsumerState<ConfirmEditDialog> {
+  late final TextEditingController textController;
+  bool error = false;
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController(text: widget.todo.desc);
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Edit Todo'),
+      content: TextField(
+        controller: textController,
+        autofocus: true,
+        decoration: InputDecoration(
+          errorText: error ? 'Value cannot be empty' : null,
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text('CANCEL'),
+        ),
+        TextButton(
+          onPressed: () {
+            error = textController.text.isEmpty ? true : false;
+            if (error) {
+              setState(() {});
+            } else {
+              ref.read(todoListProvider.notifier).editTodo(
+                    widget.todo.id,
+                    textController.text,
+                  );
+              Navigator.of(context).pop();
+            }
+          },
+          child: const Text('EDIT'),
+        ),
+      ],
     );
   }
 }
