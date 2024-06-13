b19194c4f2b483bd6bcf5cd91ecca474233dea9e
Jinho Hyeon <neverlish@gmail.com>
2024-06-13 19:02:57 +0900

    06 - 062 Customized Cache - Sync Operation

--- flutter/udemy-sangwookcho-flutter-riverpod/provider_lifecycle/lib/pages/sync_keep_alive/sync_keep_alive_page.dart
+++ flutter/udemy-sangwookcho-flutter-riverpod/provider_lifecycle/lib/pages/sync_keep_alive/sync_keep_alive_page.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:provider_lifecycle/pages/sync_keep_alive/providers.dart';
 
 class SyncKeepAlivePage extends ConsumerWidget {
   const SyncKeepAlivePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(syncKeepAliveCounterProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('SyncKeepAlive'),
       ),
-      body: const Center(
-        child: Text('SyncKeepAlive'),
+      body: Center(
+        child: Text('$counter'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(syncKeepAliveCounterProvider.notifier).increment();
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
