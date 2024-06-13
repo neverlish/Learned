15d4a89bdf0c1e41c5ee0a63ef9c2809bdbc70d8
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 00:19:19 +0900

    12 - 100 todoSearchProvider

--- flutter/udemy-sangwookcho-flutter-riverpod/todo_riverpod_sync/lib/pages/providers/todo_search/todo_search_provider.g.dart
+++ flutter/udemy-sangwookcho-flutter-riverpod/todo_riverpod_sync/lib/pages/providers/todo_search/todo_search_provider.g.dart
@@ -1,25 +1,25 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'todo_filter_provider.dart';
+part of 'todo_search_provider.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$todoFilterHash() => r'a3f1574576b8274665b7ca9810e851b62679bcfa';
+String _$todoSearchHash() => r'0960009807ae7f9542208599e857604ce7b22cab';
 
-/// See also [TodoFilter].
-@ProviderFor(TodoFilter)
-final todoFilterProvider =
-    AutoDisposeNotifierProvider<TodoFilter, Filter>.internal(
-  TodoFilter.new,
-  name: r'todoFilterProvider',
+/// See also [TodoSearch].
+@ProviderFor(TodoSearch)
+final todoSearchProvider =
+    AutoDisposeNotifierProvider<TodoSearch, String>.internal(
+  TodoSearch.new,
+  name: r'todoSearchProvider',
   debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$todoFilterHash,
+      const bool.fromEnvironment('dart.vm.product') ? null : _$todoSearchHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$TodoFilter = AutoDisposeNotifier<Filter>;
+typedef _$TodoSearch = AutoDisposeNotifier<String>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
