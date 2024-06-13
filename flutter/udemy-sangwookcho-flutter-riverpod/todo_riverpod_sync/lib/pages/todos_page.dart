b7bfb9cf26d5e0eeed07cf280b69e768eccab21e
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 00:30:52 +0900

    12 - 104 TodoHeader widget

--- flutter/udemy-sangwookcho-flutter-riverpod/todo_riverpod_sync/lib/pages/todos_page.dart
+++ flutter/udemy-sangwookcho-flutter-riverpod/todo_riverpod_sync/lib/pages/todos_page.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:todo_riverpod_sync/pages/widgets/todo_header.dart';
 
 class TodosPage extends StatelessWidget {
   const TodosPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('TODOS'),
+    return const SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              TodoHeader(),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ flutter/udemy-sangwookcho-flutter-riverpod/todo_riverpod_sync/lib/pages/widgets/todo_header.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_riverpod_sync/pages/providers/active_todo_count/active_todo_count_provider.dart';
+import 'package:todo_riverpod_sync/pages/providers/todo_list/todo_list_provider.dart';
+
+class TodoHeader extends ConsumerWidget {
+  const TodoHeader({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final activeTodoCount = ref.watch(activeTodoCountProvider);
+    final todos = ref.watch(todoListProvider);
+    return Row(
+      children: [
+        const Text(
+          'TODO',
+          style: TextStyle(fontSize: 36.0),
+        ),
+        const SizedBox(width: 10),
+        Text(
+          '($activeTodoCount/${todos.length} item${activeTodoCount != 1 ? 's' : ''} left)',
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.blue[900],
+          ),
+        )
+      ],
+    );
+  }
+}
