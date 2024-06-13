848f63e4697448e1a1fa4fd644af931924d45678
Jinho Hyeon <neverlish@gmail.com>
2024-06-13 18:53:50 +0900

    06 - 061 Lifecycle of keepAlive provider

--- flutter/udemy-sangwookcho-flutter-riverpod/provider_lifecycle/lib/pages/keep_alive/keep_alive_page.dart
+++ flutter/udemy-sangwookcho-flutter-riverpod/provider_lifecycle/lib/pages/keep_alive/keep_alive_page.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:provider_lifecycle/pages/keep_alive/providers.dart';
 
 class KeepAlivePage extends ConsumerWidget {
   const KeepAlivePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(keepAliveCounterProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('KeepAlive'),
       ),
-      body: const Center(
-        child: Text('KeepAlive'),
+      body: Center(
+        child: Text('$counter'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(keepAliveCounterProvider.notifier).increment();
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ flutter/udemy-sangwookcho-flutter-riverpod/provider_lifecycle/lib/pages/keep_alive/providers.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$keepAliveCounterHash() => r'3ec7c2b7efb07bfff47f31b6587278e91a63cfe3';
+
+/// See also [KeepAliveCounter].
+@ProviderFor(KeepAliveCounter)
+final keepAliveCounterProvider =
+    NotifierProvider<KeepAliveCounter, int>.internal(
+  KeepAliveCounter.new,
+  name: r'keepAliveCounterProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$keepAliveCounterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$KeepAliveCounter = Notifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
