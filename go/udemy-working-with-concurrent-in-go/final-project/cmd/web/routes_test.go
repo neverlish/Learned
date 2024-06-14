3b8ebd68099cd1a8102f11ba4bd6b8f940ff68c3
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 18:38:05 +0900

    10 - 80 Testing Routes

---
+++ go/udemy-working-with-concurrent-in-go/final-project/cmd/web/routes_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+)
+
+var routes = []string {
+	"/",
+	"/login",
+	"/logout",
+	"/register",
+	"/activate",
+	"/members/plans",
+	"/members/subscribe",
+}
+
+func Test_Routes_Exists(t *testing.T) {
+	testRoutes := testApp.routes()
+	
+	chiRoutes := testRoutes.(chi.Router)
+
+	for _, route := range routes {
+		routeExists(t, chiRoutes, route)
+	}
+}
+
+func routeExists(t *testing.T, routes chi.Router, route string) {
+	found := false
+
+	_ = chi.Walk(routes, func (method string, foundRoute string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		if route == foundRoute {
+			found = true
+		}
+		return nil
+	})
+
+	if !found {
+		t.Errorf("did not find %s in the registered routes", route)
+	}
+}
