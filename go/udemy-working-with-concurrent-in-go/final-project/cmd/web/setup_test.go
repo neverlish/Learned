dbf1aeb64366c01503e32d2f1bec37b5794669d5
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 18:33:24 +0900

    10 Testing - 79 Setting up our tests

---
+++ go/udemy-working-with-concurrent-in-go/final-project/cmd/web/setup_test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/gob"
+	"final-project/data"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	scs "github.com/alexedwards/scs/v2"
+)
+
+var testApp Config
+
+func TestMain(m *testing.M) {
+	gob.Register(data.User{})
+
+	session := scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+
+	testApp = Config{
+		Session: session,
+		DB: nil,
+		InfoLog: 	log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+		ErrorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+		Wait: &sync.WaitGroup{},
+		ErrorChan: make(chan error),
+		ErrorChanDone: make(chan bool),
+	}
+
+	errorChan := make(chan error)
+	mailerChan := make(chan Message, 100)
+	mailerDoneChan := make(chan bool)
+
+	testApp.Mailer = Mail{
+		Wait: testApp.Wait,
+		ErrorChan: errorChan,
+		MailerChan: mailerChan,
+		DoneChan: mailerDoneChan,
+	}
+
+	go func() {
+		select {
+		case <-testApp.Mailer.MailerChan:
+		case <-testApp.Mailer.ErrorChan:
+		case <-testApp.Mailer.DoneChan:
+			return
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case err := <-testApp.ErrorChan:
+				testApp.ErrorLog.Println(err)
+			case <-testApp.ErrorChanDone:
+				return
+			}
+		}
+	}()
+
+	os.Exit(m.Run())
+}
