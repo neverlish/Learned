27fa47def41694f2aa14253e013b87d2bf8158d6
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 21:45:29 +0900

    14 - 157 Firebase setup - basic

--- flutter/udemy-sangwookcho-flutter-riverpod/fb_auth_riverpod/lib/main.dart
+++ flutter/udemy-sangwookcho-flutter-riverpod/fb_auth_riverpod/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
