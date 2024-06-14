7d6cb270a2db9bf1146a4d7885a9b3dab737b38f
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 17:28:06 +0900

    08 - 69 Giving user data to our templates

--- go/udemy-working-with-concurrent-in-go/final-project/cmd/web/render.go
+++ go/udemy-working-with-concurrent-in-go/final-project/cmd/web/render.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"final-project/data"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -19,7 +20,7 @@ type TemplateData struct {
 	Error string
 	Authenticated bool
 	Now time.Time
-	// User *data.User
+	User *data.User
 }
 
 func (app *Config) render(w http.ResponseWriter, r *http.Request, t string, td *TemplateData) {
@@ -64,6 +65,12 @@ func (app *Config) AddDefaultData(td *TemplateData, r *http.Request) *TemplateDa
 	td.Error = app.Session.PopString(r.Context(), "error")
 	if app.IsAuthenticated(r) {
 		td.Authenticated = true
+		user, ok := app.Session.Get(r.Context(), "user").(data.User)
+		if !ok {
+			app.ErrorLog.Println("can't get user from session")
+		} else {
+			td.User = &user
+		}
 	}
 
 	td.Now = time.Now()
