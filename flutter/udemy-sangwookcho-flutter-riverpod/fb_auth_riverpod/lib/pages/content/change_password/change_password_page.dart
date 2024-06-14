4dd1b46929c6ed84cda23d8ba1c6e543b5a0c5fd
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 23:04:09 +0900

    14 - 175 ChangePasswordPage

--- flutter/udemy-sangwookcho-flutter-riverpod/fb_auth_riverpod/lib/pages/content/change_password/change_password_page.dart
+++ flutter/udemy-sangwookcho-flutter-riverpod/fb_auth_riverpod/lib/pages/content/change_password/change_password_page.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../models/custom_error.dart';
+import '../../../repositories/auth_repository_provider.dart';
+import '../../../utils/error_dialog.dart';
+import '../../widgets/buttons.dart';
+import '../../widgets/form_fields.dart';
+import 'change_password_provider.dart';
+
 class ChangePasswordPage extends ConsumerStatefulWidget {
   const ChangePasswordPage({super.key});
 
@@ -10,14 +17,128 @@ class ChangePasswordPage extends ConsumerStatefulWidget {
 }
 
 class _ChangePasswordPageState extends ConsumerState<ChangePasswordPage> {
+  final _formKey = GlobalKey<FormState>();
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _submit() {
+    setState(() => _autovalidateMode = AutovalidateMode.always);
+
+    final form = _formKey.currentState;
+
+    if (form == null || !form.validate()) return;
+
+    ref.read(changePasswordProvider.notifier).changePassword(
+          _passwordController.text.trim(),
+        );
+  }
+
+  void processSuccessCase() async {
+    try {
+      await ref.read(authRepositoryProvider).signout();
+    } on CustomError catch (e) {
+      if (!mounted) return;
+      errorDialog(context, e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Change Password'),
-      ),
-      body: const Center(
-        child: Text('Change Password'),
+    ref.listen<AsyncValue<void>>(
+      changePasswordProvider,
+      (previous, next) {
+        next.whenOrNull(error: (e, st) {
+          errorDialog(context, e as CustomError);
+        }, data: (_) {
+          processSuccessCase();
+        });
+      },
+    );
+
+    final chgPwdState = ref.watch(changePasswordProvider);
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Change Password'),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Form(
+              key: _formKey,
+              autovalidateMode: _autovalidateMode,
+              child: ListView(
+                shrinkWrap: true,
+                reverse: true,
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'If you change password,',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text.rich(
+                        TextSpan(
+                          text: 'you will be ',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: 'signed out!',
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 40.0),
+                  PasswordFormField(
+                    passwordController: _passwordController,
+                    labelText: 'New password',
+                  ),
+                  const SizedBox(height: 20.0),
+                  ConfirmPasswordFormField(
+                    passwordController: _passwordController,
+                    labelText: 'Confirm new password',
+                  ),
+                  const SizedBox(height: 20.0),
+                  CustomFilledButton(
+                    onPressed: chgPwdState.maybeWhen(
+                      loading: () => null,
+                      orElse: () => _submit,
+                    ),
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                    child: Text(
+                      chgPwdState.maybeWhen(
+                        loading: () => 'Submitting...',
+                        orElse: () => 'Change Password',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                ].reversed.toList(),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
