47269d1eff86436893ee2fddab506a2854040f2e
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 22:21:48 +0900

    14 - 166 SignupPage - 1

--- flutter/udemy-sangwookcho-flutter-riverpod/fb_auth_riverpod/lib/pages/auth/signup/signup_page.dart
+++ flutter/udemy-sangwookcho-flutter-riverpod/fb_auth_riverpod/lib/pages/auth/signup/signup_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../models/custom_error.dart';
+import '../../../utils/error_dialog.dart';
+import '../../widgets/form_fields.dart';
+import 'signin_provider.dart';
+
 class SignupPage extends ConsumerStatefulWidget {
   const SignupPage({super.key});
 
@@ -9,11 +14,71 @@ class SignupPage extends ConsumerStatefulWidget {
 }
 
 class _SignupPageState extends ConsumerState<SignupPage> {
+  final _formKey = GlobalKey<FormState>();
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _submit() {}
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Sign Up'),
+    ref.listen<AsyncValue<void>>(
+      signupProvider,
+      (previous, next) {
+        next.whenOrNull(
+          error: (e, st) => errorDialog(
+            context,
+            (e as CustomError),
+          ),
+        );
+      },
+    );
+
+    final signupState = ref.watch(signupProvider);
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Form(
+              key: _formKey,
+              autovalidateMode: _autovalidateMode,
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  const FlutterLogo(size: 150),
+                  const SizedBox(height: 20.0),
+                  NameFormField(nameController: _nameController),
+                  const SizedBox(height: 20.0),
+                  EmailFormField(emailController: _emailController),
+                  const SizedBox(height: 20.0),
+                  PasswordFormField(
+                    passwordController: _passwordController,
+                    labelText: 'Password',
+                  ),
+                  const SizedBox(height: 20.0),
+                  ConfirmPasswordFormField(
+                    passwordController: _passwordController,
+                    labelText: 'Confirm password',
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
