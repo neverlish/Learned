f764843aa9c04cfff6ea64f83ee95517bae95fed
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 20:07:56 +0900

    13 - 144 SearchPage

--- flutter/udemy-sangwookcho-flutter-riverpod/weather_riverpod_asyncvalue/lib/pages/home/home_page.dart
+++ flutter/udemy-sangwookcho-flutter-riverpod/weather_riverpod_asyncvalue/lib/pages/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_riverpod_asyncvalue/extensions/async_value_xx.dart';
 import 'package:weather_riverpod_asyncvalue/pages/home/providers/weather_provider.dart';
+import 'package:weather_riverpod_asyncvalue/pages/search/search_page.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
@@ -11,6 +12,7 @@ class HomePage extends ConsumerStatefulWidget {
 }
 
 class _HomePageState extends ConsumerState<HomePage> {
+  String? city;
   @override
   Widget build(BuildContext context) {
     final weatherState = ref.watch(weatherProvider);
@@ -18,6 +20,22 @@ class _HomePageState extends ConsumerState<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              city = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const SearchPage(),
+                ),
+              );
+              print("city: $city");
+              if (city != null) {
+                ref.read(weatherProvider.notifier).fetchWeather(city!);
+              }
+            },
+            icon: const Icon(Icons.search),
+          )
+        ],
       ),
       body: const Center(
         child: Text('Home'),
