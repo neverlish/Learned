f102443b450ed11d5f80f0b794e6aa82d73988e7
Jinho Hyeon <neverlish@gmail.com>
2024-06-14 22:38:53 +0900

    14 - 169 signinProvider

---
+++ flutter/udemy-sangwookcho-flutter-riverpod/fb_auth_riverpod/lib/pages/auth/signin/signin_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'signin_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$signinHash() => r'9fd884ed49f646083f32146cce31834df0d79b61';
+
+/// See also [Signin].
+@ProviderFor(Signin)
+final signinProvider = AutoDisposeAsyncNotifierProvider<Signin, void>.internal(
+  Signin.new,
+  name: r'signinProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$signinHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Signin = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
