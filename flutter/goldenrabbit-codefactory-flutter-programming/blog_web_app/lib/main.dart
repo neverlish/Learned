80cd927be061d6d7219676287834275d16c6a919
Jinho Hyeon <neverlish@gmail.com>
2024-09-25 17:33:31 +0900

    08 - 4 구현하기

--- flutter/goldenrabbit-codefactory-flutter-programming/blog_web_app/lib/main.dart
+++ flutter/goldenrabbit-codefactory-flutter-programming/blog_web_app/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:blog_web_app/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  
   runApp(MaterialApp(
     home: HomeSecreen(),
   ));
--- flutter/goldenrabbit-codefactory-flutter-programming/blog_web_app/lib/screen/home_screen.dart
+++ flutter/goldenrabbit-codefactory-flutter-programming/blog_web_app/lib/screen/home_screen.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeSecreen extends StatelessWidget {
-  const HomeSecreen({super.key});
+  WebViewController webViewController = WebViewController()
+    ..loadRequest(Uri.parse('https://blog.codefactory.ai/'))
+    ..setJavaScriptMode(JavaScriptMode.unrestricted);
+
+  HomeSecreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Code Factory'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              webViewController
+                  .loadRequest(Uri.parse('https://blog.codefactory.ai/'));
+            },
+            icon: Icon(Icons.home),
+          )
+        ],
+      ),
+      body: WebViewWidget(
+        controller: webViewController,
+      ),
     );
   }
 }
