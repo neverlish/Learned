7c35445f145c66a8d875c3894e89a5c8037ff09a
Jinho Hyeon <neverlish@gmail.com>
2024-09-25 20:08:10 +0900

    11 디지털 주사위 : 가속도계, 자이로스코프, Sensor_plus - 2 사전 준비

---
+++ flutter/goldenrabbit-codefactory-flutter-programming/random_dice/lib/const/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color(0xFF0E0E0E);
+const primaryColor = Colors.white;
+final secondaryColor = Colors.grey[600];
---
+++ flutter/goldenrabbit-codefactory-flutter-programming/random_dice/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:random_dice/const/colors.dart';
+import 'package:random_dice/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor,
+        sliderTheme: SliderThemeData(
+          thumbColor: primaryColor,
+          activeTrackColor: primaryColor,
+          inactiveTrackColor: primaryColor.withOpacity(0.3),
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            selectedItemColor: primaryColor,
+            unselectedItemColor: secondaryColor,
+            backgroundColor: backgroundColor),
+      ),
+      home: HomeScreen(),
+    ),
+  );
+}
