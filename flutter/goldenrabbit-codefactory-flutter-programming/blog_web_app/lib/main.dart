5c2f27e69a04a71450d843e66abe9369b233c347
Jinho Hyeon <neverlish@gmail.com>
2024-09-25 17:08:12 +0900

    08 블로그 웹 앱 : 콜백 함수, 웹뷰, 네이티브 설정 - 2 사전 준비

---
+++ flutter/goldenrabbit-codefactory-flutter-programming/blog_web_app/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:blog_web_app/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeSecreen(),
+  ));
+}
---
+++ flutter/goldenrabbit-codefactory-flutter-programming/blog_web_app/lib/screen/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeSecreen extends StatelessWidget {
+  const HomeSecreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Home Screen'),
+    );
+  }
+}
