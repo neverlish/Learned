d5aa08ef4de6b01da1bb7b2932e8db6612eb9f91
Jinho Hyeon <neverlish@gmail.com>
2024-09-28 00:31:26 +0900

    1 - 09 - 함수 정의와 호출

---
+++ rust/wikibooks-rust-learning-by-making/ch1/multiplication.rs
@@ -0,0 +1,11 @@
+fn multiplication(a: i64, b: i64) -> i64 {
+  a * b
+}
+
+fn main()  {
+  let ex1 = multiplication(3, 5);
+  println!("3 * 5 = {}", ex1);
+
+  let ex2 = multiplication(8, 4);
+  println!("8 * 4 = {}", ex2);
+}
