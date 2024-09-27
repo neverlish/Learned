b1cfb821f6bcc961d7e37ed8dbca859c84083864
Jinho Hyeon <neverlish@gmail.com>
2024-09-27 23:56:19 +0900

    1 - 07 변수 정의와 피보나치 수열 - 피보나치 수열이란

---
+++ rust/wikibooks-rust-learning-by-making/ch1/fib.rs
@@ -0,0 +1,14 @@
+fn main() {
+  let mut a = 1;
+  let mut b = 1;
+
+  println!("{}", a);
+  println!("{}", b);
+
+  for _ in 0..30 {
+    println!("{}", a + b);
+    let tmp = a;
+    a = b;
+    b = tmp + b;
+  }
+}
