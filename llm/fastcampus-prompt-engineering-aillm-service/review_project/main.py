307de2f3304e336ed1cb8a51e41ed35f2c6a14d2
Jinho Hyeon <neverlish@gmail.com>
2025-02-01 02:20:30 +0900

    06 - 04 - 04 API로 만들기

--- llm/fastcampus-prompt-engineering-aillm-service/review_project/main.py
+++ llm/fastcampus-prompt-engineering-aillm-service/review_project/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
+from typing import List
 from ch2.inference import inference_json
 from ch3.inference import inference_function_calling
+from ch4.inference import inference_langchain
 
 app = FastAPI()
 
@@ -15,3 +17,10 @@ async def evaluate_review(body: RequestBody = Body()):
 @app.post("/extract/")
 async def extract_review(body: RequestBody = Body()):
     return inference_function_calling(body.review)
+
+class RequestBody2(BaseModel):
+    reviews: List[str]
+
+@app.post("/summary")
+async def summary_reviews(body: RequestBody2 = Body()):
+    return inference_langchain(body.reviews)
