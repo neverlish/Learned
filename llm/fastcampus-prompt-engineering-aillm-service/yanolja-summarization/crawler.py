77d77deeb761c837cb598ea27992de29bdf5ee02
Jinho Hyeon <neverlish@gmail.com>
2025-01-31 11:30:05 +0900

    04 야놀자 리뷰 요약 - 03 데이터 확보 실습 - 02 데이터 크롤링 실습

---
+++ llm/fastcampus-prompt-engineering-aillm-service/yanolja-summarization/crawler.py
@@ -0,0 +1,50 @@
+import json
+import time
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+URL = 'https://www.yanolja.com/reviews/domestic/1000102261?sort=HOST_CHOICE'
+
+def crawl_yanolja_reviews():
+  review_list = []
+  driver = webdriver.Chrome()
+  driver.get(URL)
+
+  time.sleep(3)
+
+  scroll_count = 10
+
+  for i in range(scroll_count):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+
+  html = driver.page_source
+  soup = BeautifulSoup(html, 'html.parser')
+
+  review_containers = soup.select('#__next > section > div > div.css-1js0bc8 > div > div > div')
+  review_date = soup.select('#__next > section > div > div.css-1js0bc8 > div > div > div div.css-1ivchjf > p')
+
+  for i in range(len(review_containers)):
+    review_text = review_containers[i].find('p', class_='content-text').text
+    # fill이 currentColor 이고 fill-rule이 evenodd 가 아닌 경우
+    review_stars = review_containers[i].find_all('path', {'fill': 'currentColor'})
+    exclude_review_stars = review_containers[i].find_all('path', {'fill': 'currentColor', 'fill-rule': 'evenodd'})
+    star_cnt = len(review_stars) - len(exclude_review_stars)
+
+    date = review_date[i].text
+
+    review_dict = {
+      'review': review_text,
+      'stars': star_cnt,
+      'date': date
+    }
+
+    review_list.append(review_dict)
+
+  with open('./res/reviews.json', 'w') as f:
+    json.dump(review_list, f, indent=4, ensure_ascii=False)
+  
+
+if __name__ == "__main__":
+  crawl_yanolja_reviews()
