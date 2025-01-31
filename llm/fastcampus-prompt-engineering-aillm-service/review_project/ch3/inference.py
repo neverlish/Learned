b69c84122592ef3a9f3a51bb24a042286b1fa58e
Jinho Hyeon <neverlish@gmail.com>
2025-02-01 01:35:48 +0900

    06 - 03 리뷰 중요 키워드 추출 - 01 프롬프트 작성 및 테스트

---
+++ llm/fastcampus-prompt-engineering-aillm-service/review_project/ch3/inference.py
@@ -0,0 +1,25 @@
+from openai import Client
+
+import os
+from ch3.prompt_template import prompt_template
+
+client = Client(api_key=os.environ["OPENAI_API_KEY"])
+
+def inference(review):
+  prompt = prompt_template.format(review=review)
+
+  response = client.chat.completions.create(
+    model='gpt-3.5-turbo',
+    messages=[
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "user", "content": prompt}
+    ],
+    temperature=0,
+  )
+
+  output = response.choices[0].message.content
+
+  return output
+
+if __name__ == "__main__":
+  print(inference("내 인생 영화"))
