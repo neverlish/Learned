2a1be9f7f1378449fa5e9629b13f2d238bc03563
Jinho Hyeon <neverlish@gmail.com>
2025-02-01 00:11:33 +0900

    06 - 02 - 02 JSON 응답 프롬프트 추가

--- llm/fastcampus-prompt-engineering-aillm-service/review_project/ch2/inference.py
+++ llm/fastcampus-prompt-engineering-aillm-service/review_project/ch2/inference.py
@@ -2,7 +2,7 @@ from openai import Client
 
 import os
 
-from prompt_template import prompt_template
+from prompt_template import prompt_template, prompt_template_json
 
 client = Client(api_key=os.environ["OPENAI_API_KEY"])
 
@@ -22,5 +22,21 @@ def inference(review):
 
   return output
 
+def inference_json(review):
+  prompt = prompt_template_json.format(review=review)
+
+  response = client.chat.completions.create(
+    model='gpt-3.5-turbo',
+    messages=[
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "user", "content": prompt}
+    ],
+    temperature=0,
+  )
+
+  output = response.choices[0].message.content
+
+  return output
+
 if __name__ == "__main__":
-  print(inference("내 인생 영화"))
+  print(inference_json("내 인생 영화"))
--- llm/fastcampus-prompt-engineering-aillm-service/review_project/ch2/prompt_template.py
+++ llm/fastcampus-prompt-engineering-aillm-service/review_project/ch2/prompt_template.py
@@ -3,3 +3,15 @@ prompt_template = """다음은 영화에 대한 리뷰입니다. 영화에 대�
 ```review
 {review}
 ```"""
+
+prompt_template_json = """다음은 영화에 대한 리뷰입니다. 영화에 대해 긍정적이면 1, 부정적이면 0으로 평가해주세요.
+
+아래 json 양식처럼 답변해주세요.
+
+{{
+  "score": 0 or 1
+}}
+
+```review
+{review}
+```"""
