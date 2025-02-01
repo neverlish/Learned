0499294ad93bce6689efccc3c4e147f31aa5035e
Jinho Hyeon <neverlish@gmail.com>
2025-02-01 14:19:54 +0900

    06 - 05 - 02 JSON 응답 프롬프트 추가

--- llm/fastcampus-prompt-engineering-aillm-service/review_project/ch5/inference.py
+++ llm/fastcampus-prompt-engineering-aillm-service/review_project/ch5/inference.py
@@ -2,17 +2,22 @@
 from openai import Client
 import os
 
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser, PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
-from ch5.prompt_template import prompt_template, prompt_template_langchain
+from pydantic import BaseModel
+from typing import List
+
+import json
+
+from ch5.prompt_template import prompt_template, prompt_template_langchain, json_schema, prompt_template_function_calling
 
 client = Client(api_key=os.environ["OPENAI_API_KEY"])
 
 def inference_all(reviews):
   reviews = "\n".join([f"review_no: {review['id']}\tcontent: {review['document']}" for review in reviews])
-  prompt = prompt_template.format(reviews=reviews)
+  prompt = prompt_template.format(reviews=reviews, json_schema=json_schema)
   response = client.chat.completions.create(
     model='gpt-3.5-turbo',
     messages=[
@@ -20,31 +25,74 @@ def inference_all(reviews):
       {"role": "user", "content": prompt}
     ],
     temperature=0,
+    response_format={'type': 'json_object'}
   )
 
   output = response.choices[0].message.content
-  return output
+  output_json = json.loads(output)
+  return output_json
+
+class Review(BaseModel):
+  review_no: int
+  score: int
+  positive_keywords: List[str]
+  negative_keywords: List[str]
+
+class Output(BaseModel):
+  reviews: List[Review]
+  summary: str
+
+output_parser = PydanticOutputParser(pydantic_object=Output)
 
 prompt = PromptTemplate(
   template=prompt_template_langchain,
-  input_variables=["reviews"]
+  input_variables=["reviews"],
+  partial_variables={"format_instructions": output_parser.get_format_instructions()},
 )
 
 model = ChatOpenAI(
   temperature=0.0,
   openai_api_key=os.environ["OPENAI_API_KEY"],
   model_name="gpt-3.5-turbo",
+  model_kwargs={"response_format": {"type": "json_object"}},
 )
-
-output_parser = StrOutputParser()
 
 chain = (prompt | model | output_parser)
 
 def inference_all_langchain(reviews):
   reviews = "\n".join([f"review_no: {review['id']}\tcontent: {review['document']}" for review in reviews])
+  prompt_now = prompt.invoke({"reviews": reviews})
   output = chain.invoke({"reviews": reviews})
   return output
-  
+
+def inference_all_function_calling(reviews):
+  reviews = "\n".join([f"review_no: {review['id']}\tcontent: {review['document']}" for review in reviews])
+  prompt = prompt_template_function_calling.format(reviews=reviews)
+  tools = [
+    {
+      "type": "function",
+      "function": {
+        "name": "analyze_reviews",
+        "description": "analyze reviews",
+        "parameters": json_schema,
+      },
+    }
+  ]
+  response = client.chat.completions.create(
+    model='gpt-3.5-turbo',
+    messages=[
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "user", "content": prompt}
+    ],
+    temperature=0,
+    response_format={'type': 'json_object'},
+    tools=tools,
+    tool_choice={"type": "function", "function": {"name": "analyze_reviews"}}
+  )
+
+  output = response.choices[0].message.tool_calls[0].function.arguments
+  output_json = json.loads(output)
+  return output_json
 
 if __name__ == "__main__":
   print(inference_all_langchain([
--- llm/fastcampus-prompt-engineering-aillm-service/review_project/ch5/prompt_template.py
+++ llm/fastcampus-prompt-engineering-aillm-service/review_project/ch5/prompt_template.py
@@ -4,15 +4,66 @@ prompt_template = """다음은 영화에 대한 리뷰들입니다.
 2. 리뷰에서 긍정적인 키워드, 부정적인 키워드를 추출해주세요.
 3. 리뷰 내용들을 종합적으로 요약해주세요.
 
+아래 json schema를 참고하여 json 응답을 생성해주세요.
+{json_schema}
+
 ```reviews
 {reviews}
 ```"""
+
+json_schema = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "properties": {
+    "reviews": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "review_no": {
+            "type": "integer"
+          },
+          "score": {
+            "type": "integer",
+            "enum": [0, 1]
+          },
+          "positive_keywords": {
+            "type": "array",
+            "items": {
+              "type": "string"
+            }
+          },
+          "negative_keywords": {
+            "type": "array",
+            "items": {
+              "type": "string"
+            }
+          },
+        }
+      }
+    }
+  }
+}
 
 prompt_template_langchain = """다음은 영화에 대한 리뷰들입니다.
 
 1. 영화에 대해 긍정적이면 1, 부정적이면 0으로 평가해주세요.
 2. 리뷰에서 긍정적인 키워드, 부정적인 키워드를 추출해주세요.
 3. 리뷰 내용들을 종합적으로 요약해주세요.
+
+{format_instructions}
+
+```reviews
+{reviews}
+```"""
+
+prompt_template_function_calling = """다음은 영화에 대한 리뷰들입니다.
+
+1. 영화에 대해 긍정적이면 1, 부정적이면 0으로 평가해주세요.
+2. 리뷰에서 긍정적인 키워드, 부정적인 키워드를 추출해주세요.
+3. 리뷰 내용들을 종합적으로 요약해주세요.
+
+json으로 응답해주세요.
 
 ```reviews
 {reviews}
