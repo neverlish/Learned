5b81d783a0ea1e9898ac20c09338c8ac64da4c92
Jinho Hyeon <neverlish@gmail.com>
2025-02-02 14:15:40 +0900

    09 - 01 - 03 JSON 응답 프롬프트 추가

--- llm/fastcampus-prompt-engineering-aillm-service/ai_faq/ch1/prompt_template.py
+++ llm/fastcampus-prompt-engineering-aillm-service/ai_faq/ch1/prompt_template.py
@@ -2,3 +2,12 @@ prompt_template = """다음 내용을 읽고 FAQ를 만들어주세요.
 
 {product_detail}
 """
+
+prompt_template_json = """다음 내용을 읽고 FAQ를 만들어주세요.
+
+{format_instructions}
+
+출력 형식은 다음과 같습니다.
+
+{product_detail}
+"""
