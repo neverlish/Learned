0be3622dc450b087819b6f44462281ece6298236
Jinho Hyeon <neverlish@gmail.com>
2025-02-12 00:01:22 +0900

    09 - 03 - 03 답변 생성하기

---
+++ llm/fastcampus-prompt-engineering-aillm-service/ai_faq/prompt_template.py
@@ -0,0 +1,8 @@
+prompt_template = """Answer the question based only on the following context:
+
+{context}
+
+Question: {question}
+
+Answer in the following language: korean
+"""
--- llm/fastcampus-prompt-engineering-aillm-service/ai_faq/search_and_answer.py
+++ llm/fastcampus-prompt-engineering-aillm-service/ai_faq/search_and_answer.py
@@ -1,9 +1,11 @@
 import os
 from langchain.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
+from prompt_template import prompt_template
+from openai import Client
 
 api_key = os.getenv('OPENAI_API_KEY')
-
+client = Client(api_key=api_key)
 
 def search(question):
   db = FAISS.load_local(
@@ -15,9 +17,34 @@ def search(question):
   result = db.search(question, search_type='similarity')
 
   return result[0].metadata
+
+
+def generate_answer(context, question):
+  context_join = f"""Q: {context['question']}
+A: {context['answer']}"""
+
+  prompt = prompt_template.format(context=context_join, question=question)
+
+  response = client.chat.completions.create(
+    model='gpt-4o-mini',
+    messages=[
+      {'role': 'system', 'content': 'You are a helpful assistant.'},
+      {'role': 'user', 'content': prompt}
+    ],
+    temperature=0,
+  )
+
+  output = response.choices[0].message.content
+
+  return output
+
   
 if __name__ == '__main__':
   question = '실무에서 LLM 기반 서비스 개발할 때 문제점?'
   qa = search(question)
   print(qa['question'])
   print(qa['answer'])
+  print()
+  print(question)
+  
+  print(generate_answer(qa, question))
