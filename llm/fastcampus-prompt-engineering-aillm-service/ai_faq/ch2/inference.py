9655f2ec32318c6d411c343bae91fe8cb34e0405
Jinho Hyeon <neverlish@gmail.com>
2025-02-11 23:42:50 +0900

    09 - 02 - 03 JSON 응답 프롬프트 추가

--- llm/fastcampus-prompt-engineering-aillm-service/ai_faq/ch2/inference.py
+++ llm/fastcampus-prompt-engineering-aillm-service/ai_faq/ch2/inference.py
@@ -1,8 +1,11 @@
 from openai import Client
 import os
-
+import json
+from pydantic import BaseModel
+from typing import List
+from langchain_core.output_parsers import PydanticOutputParser
 from ch2.download_data import get_data
-
+from ch2.prompt_template import prompt_template
 client = Client(api_key=os.getenv('OPENAI_API_KEY'))
 
 
@@ -40,6 +43,43 @@ def inference_many(url_list):
   output = response.choices[0].message.content
   return output
 
+class QA(BaseModel):
+  question: str
+  answer: str
+
+class Output(BaseModel):
+  qa_list: List[QA]
+
+output_parser = PydanticOutputParser(pydantic_object=Output)
+
+def inference_many_json(url_list):
+  prompt = prompt_template.format(format_instructions=output_parser.get_format_instructions())
+  content = [
+    {"type": "text", "text": prompt}
+  ]
+  for url in url_list[:5]:
+    content.append({
+      "type": "image_url",
+      "image_url": {
+        "url": url
+      }
+    })
+  response = client.chat.completions.create(
+    model='gpt-4o-mini',
+    messages=[
+      {
+        "role": "user",
+        "content": content
+      }
+    ],
+    max_tokens=1000,
+    response_format={"type": "json_object"}
+  )
+  output = response.choices[0].message.content
+  output = json.loads(output)
+  return output
+
 if __name__ == '__main__':
   url_list = get_data()
-  print(inference_many(url_list))
+  result = inference_many_json(url_list)
+  print(json.dumps(result, indent=2, ensure_ascii=False))
---
+++ llm/fastcampus-prompt-engineering-aillm-service/ai_faq/ch2/prompt_template.py
@@ -0,0 +1,4 @@
+prompt_template = """다음 사진들의 내용을 읽고 FAQ를 한국어로 만들어주세요.
+
+{format_instructions}
+"""
