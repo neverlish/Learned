d0f95072efdf8739f1b016c67f71dff76dbcdbb3
Jinho Hyeon <neverlish@gmail.com>
2025-02-11 23:53:53 +0900

    09 - 03 Q&A 자동응대 - 01 VectorDB에 저장하기

--- llm/fastcampus-prompt-engineering-aillm-service/ai_faq/ch2/download_data.py
+++ llm/fastcampus-prompt-engineering-aillm-service/ai_faq/ch2/download_data.py
@@ -10,7 +10,7 @@ def get_data():
 
   soup = BeautifulSoup(response.text, 'html.parser')
   img_list = soup.find_all('img')
-  url_list = [tag.get('src') for tag in img_list if tag.get('src')]
+  url_list = [tag.get('src') for tag in img_list if tag.get('src') and not tag.get('src').endswith('.svg')]
   return url_list
 
 
