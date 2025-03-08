990e0ce17e11480ca7f7c604764ce081f75ea906
Jinho Hyeon <neverlish@gmail.com>
2025-03-08 10:09:16 +0900

    13 - 04 - 07 BM25를 결합한 하이브리드 서치 구현

---
+++ llm/fastcampus-prompt-engineering-aillm-service/investment-assistant/project_3/06_ensemble_search.py
@@ -0,0 +1,38 @@
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.retrievers import BM25Retriever
+from langchain_community.vectorstores import FAISS
+from langchain.retrievers import EnsembleRetriever
+
+doc_list = [
+    "우리나라는 2022년에 코로나가 유행했다.",
+    "우리나라 2024년 GDP 전망은 3.0%이다.",
+    "우리나라는 2022년 국내총생산 중 연구개발 예산은 약 5%이다.",
+]
+
+bm25_retriever = BM25Retriever.from_texts(
+    doc_list,
+    metadatas=[{"source": 1}] * len(doc_list),
+)
+
+bm25_retriever.k = 1
+
+embedding = OpenAIEmbeddings(model="text-embedding-3-small")
+
+faiss_vectorstore = FAISS.from_texts(
+    doc_list,
+    embedding,
+    metadatas=[{"source": 1}] * len(doc_list),
+)
+
+faiss_retriever = faiss_vectorstore.as_retriever(search_kwargs={"k": 1})
+
+query = "2022년 우리나라 GDP 대비 R&D 규모는?"
+
+ensemble_retriever = EnsembleRetriever(
+    retrievers=[bm25_retriever, faiss_retriever],
+    weights=[0.5, 0.5],
+)
+
+ensemble_docs = ensemble_retriever.invoke(query)
+
+print(ensemble_docs)
