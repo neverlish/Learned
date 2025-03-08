3bbf9afb61f747cc8f9824bfe2aa83db77565226
Jinho Hyeon <neverlish@gmail.com>
2025-03-08 22:48:29 +0900

    13 - 06 온프레미스 RAG 구현하기 - 02 SentenceTransformers 사용하기

---
+++ llm/fastcampus-prompt-engineering-aillm-service/investment-assistant/project_3/11_sentence_transformers.py
@@ -0,0 +1,24 @@
+from sentence_transformers import SentenceTransformer
+
+# 1. Load a pretrained Sentence Transformer model
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# The sentences to encode
+sentences = [
+    "The weather is lovelyt today!",
+    "It's so sunny outside",
+    "He drove to the stadium.",
+]
+
+# 2. Calculate embeddings by calling model.encode()
+embeddings = model.encode(sentences)
+print(embeddings.shape)
+# [3, 384]
+# print(embeddings[0])
+
+# 3. Calculate the embedding similarities
+similarities = model.similarity(embeddings[0], embeddings[1])
+print(similarities)
+# tensor([[1.0000, 0.6660, 0.1046],
+#         [0.6660, 1.0000, 0.1411],
+#         [0.1046, 0.1411, 1.0000]])
