6a99822176e1c5adf55db8e7ae349f1e3bb36b94
Jinho Hyeon <neverlish@gmail.com>
2025-03-09 00:32:30 +0900

    13 - 06 - 06 Huggingface Transformers 라이브러리 설명

--- llm/fastcampus-prompt-engineering-aillm-service/investment-assistant/project_3/13_transformers_sllm.py
+++ llm/fastcampus-prompt-engineering-aillm-service/investment-assistant/project_3/13_transformers_sllm.py
@@ -1,41 +1,18 @@
 import transformers
 import torch
 
-model_id = "MLP-KTLim/llama-3-Korean-Bllossom-8B"
+model_id = "42dot/42dot_LLM-SFT-1.3B"
 
 pipeline = transformers.pipeline(
-    "text-generation",
-    model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16},
-    device_map="auto",
+    "text-generation", model=model_id, model_kwargs={"torch_dtype": torch.float16}
 )
 
 pipeline.model.eval()
 
-PROMPT = """You are a helpful AI assistant. Please answer the user's questions kindly. 당신은 유능한 AI 어시스턴트 입니다. 사용자의 질문에 대해 친절하게 답변해주세요."""
-instruction = "서울의 유명한 관광 코스를 만들어줄래?"
-
-messages = [
-    {"role": "system", "content": f"{PROMPT}"},
-    {"role": "user", "content": f"{instruction}"},
-]
-
-prompt = pipeline.tokenizer.apply_chat_template(
-    messages, tokenize=False, add_generation_prompt=True
-)
-
-terminators = [
-    pipeline.tokenizer.eos_token_id,
-    pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>"),
-]
+prompt = "삼성전자에 대해 알려줘."
 
 outputs = pipeline(
-    prompt,
-    max_new_tokens=2048,
-    eos_token_id=terminators,
-    do_sample=True,
-    temperature=0.6,
-    top_p=0.9,
+    prompt, max_new_tokens=100, do_sample=True, temperature=0.3, top_p=0.9
 )
 
 print(outputs[0]["generated_text"][len(prompt) :])
