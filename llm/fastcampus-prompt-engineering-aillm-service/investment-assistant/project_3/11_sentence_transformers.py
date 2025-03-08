50f972bf68088178ebdb376c66b28eb1f559c3c2
Jinho Hyeon <neverlish@gmail.com>
2025-03-08 22:55:01 +0900

    13 - 06 - 03 SBERT 모델 선정하기

--- llm/fastcampus-prompt-engineering-aillm-service/investment-assistant/project_3/11_sentence_transformers.py
+++ llm/fastcampus-prompt-engineering-aillm-service/investment-assistant/project_3/11_sentence_transformers.py
@@ -1,13 +1,11 @@
 from sentence_transformers import SentenceTransformer
 
 # 1. Load a pretrained Sentence Transformer model
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("skt/kobert-base-v1")
 
 # The sentences to encode
 sentences = [
-    "The weather is lovelyt today!",
-    "It's so sunny outside",
-    "He drove to the stadium.",
+    "나는 학교에 가서 공부를 한다",
 ]
 
 # 2. Calculate embeddings by calling model.encode()
@@ -17,8 +15,8 @@ print(embeddings.shape)
 # print(embeddings[0])
 
 # 3. Calculate the embedding similarities
-similarities = model.similarity(embeddings[0], embeddings[1])
-print(similarities)
+# similarities = model.similarity(embeddings[0], embeddings[1])
+# print(similarities)
 # tensor([[1.0000, 0.6660, 0.1046],
 #         [0.6660, 1.0000, 0.1411],
 #         [0.1046, 0.1411, 1.0000]])
