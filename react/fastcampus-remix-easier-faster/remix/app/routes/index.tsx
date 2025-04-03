9176d386787e6c4334e3694e6466b35472127a3a
Jinho Hyeon <neverlish@gmail.com>
2025-04-03 15:14:00 +0900

    2 - 1 - 3 Route Module 을 이용한 웹 페이지 구성 1

--- react/fastcampus-remix-easier-faster/remix/app/routes/index.tsx
+++ react/fastcampus-remix-easier-faster/remix/app/routes/index.tsx
@@ -1,32 +1,59 @@
+import { HeadersFunction, LinksFunction, MetaFunction } from "@remix-run/node";
+
+import Stylesheet from "~/styles/test.css";
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "icon", href: "/favicon.ico", type: "image/png" },
+
+    {
+      rel: "stylesheet",
+      href: "https://example-css-six.vercel.app/example.css",
+    },
+
+    {
+      rel: "stylesheet",
+      href: Stylesheet
+    },
+
+    {
+      rel: "prefetch",
+      as: "image",
+      href: "/img/bunny.jpg",
+    },
+  ]
+}
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  "og:title": "OG TITLE 입니다",
+  "og:description": "OG DESCRIPTION 입니다",
+  title: "나는 제목입니다.",
+
+  description: "나는 설명입니다.",
+  viewport: "width=device-width, initial-scale=1",
+
+  refresh: {
+    httpEquiv: "refresh",
+    content: "test",
+  }
+})
+
+export const headers: HeadersFunction = ({
+  actionHeaders,
+  loaderHeaders,
+  parentHeaders,
+}) => ({
+  "Cache-Control": "max-age=369, s-maxage=3636",
+})
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Index</h1>
+      <p>Some content</p>
+      <input type="text" />
+      안녕하세요
     </div>
   );
 }
