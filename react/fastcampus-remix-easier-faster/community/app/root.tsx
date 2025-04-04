4512d9b2a346aa9a3092ab163d7e5a907db3f4aa
Jinho Hyeon <neverlish@gmail.com>
2025-04-05 02:19:03 +0900

    5 도전: 결제 기능 포함 커뮤니티 만들기 - 2 기초 작업 - 1 프로젝트 구축

---
+++ react/fastcampus-remix-easier-faster/community/app/root.tsx
@@ -0,0 +1,39 @@
+import { createEmotionCache, MantineProvider } from "@mantine/core";
+import { StylesPlaceholder } from "@mantine/remix";
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+createEmotionCache({ key: "mantine" });
+
+export default function App() {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <html lang="en">
+        <head>
+          <StylesPlaceholder />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    </MantineProvider>
+  );
+}
