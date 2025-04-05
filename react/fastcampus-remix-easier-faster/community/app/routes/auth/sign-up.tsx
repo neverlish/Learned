f4aecc9280297295416a6b991fedf04af4ae39bf
Jinho Hyeon <neverlish@gmail.com>
2025-04-05 15:11:55 +0900

    5 - 4 - 3 로그인, 회원가입 기능 구현 - 1

--- react/fastcampus-remix-easier-faster/community/app/routes/auth/sign-up.tsx
+++ react/fastcampus-remix-easier-faster/community/app/routes/auth/sign-up.tsx
@@ -1,5 +1,44 @@
 import { Box, Button, Card, Divider, Input, Space, Text } from "@mantine/core";
+import { ActionFunction, json, redirect } from "@remix-run/node";
 import { Form, Link, useNavigation } from "@remix-run/react";
+import qs from "qs";
+import supabase from "~/models/supabase";
+import { createUser } from "~/models/user.service";
+
+interface InputData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const inputData = qs.parse(await request.text()) as unknown as InputData;
+
+  const { data, error } = await supabase.auth.signUp({
+    ...inputData,
+    options: {
+      data: {
+        name: inputData.name,
+      },
+    },
+  });
+
+  console.log(data, error);
+
+  if (error) {
+    return json({
+      error: true,
+      message: {
+        title: "회원가입 실패",
+        message: error.message,
+        color: "red",
+      },
+    });
+  }
+	await createUser(data.user?.id as string, inputData.name);
+
+  return redirect("/auth/sign-in");
+};
 
 export default function SignUp() {
   const navigation = useNavigation();
