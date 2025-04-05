b9ae33966f4f3e85e2063679ee1e4af289d24f0f
Jinho Hyeon <neverlish@gmail.com>
2025-04-05 16:26:35 +0900

    5 - 4 - 7 글 작성 기능 구현

--- react/fastcampus-remix-easier-faster/community/app/routes/post.create.tsx
+++ react/fastcampus-remix-easier-faster/community/app/routes/post.create.tsx
@@ -1,16 +1,82 @@
 import { ActionIcon, Box, Button, Divider, Select, Space, TextInput, Title } from "@mantine/core";
-import { json, LoaderFunction } from "@remix-run/node";
-import { Form, Link } from "@remix-run/react";
+import { ActionFunction, json, LoaderFunction, redirect } from "@remix-run/node";
+import { Form, Link, useActionData, useLoaderData } from "@remix-run/react";
 import { IconChevronLeft } from "@tabler/icons-react";
-import { authenticate } from "~/auth.server";
+import { authenticate, getUser } from "~/auth.server";
 import PostUpload from "~/components/Post/Upload";
+import { createPost } from "~/models/post.service";
+import qs from "qs";
+import { getBoards, TBoard } from "~/models/board.service";
+import { useEffect, useState } from "react";
+import { showNotification } from "@mantine/notifications";
+
+interface ILoaderData {
+  boards: TBoard[];
+}
+
+interface InputData {
+  title: string;
+  content: string;
+  board_id: number;
+  writer: string;
+}
+
+interface IActionData {
+  message: TMessage;
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
-  await authenticate(request); // Access Token 을 발급할 수 없으면 /auth/sign-in 으로 리다이렉트됨
-  return {};
+  await authenticate(request);
+  const boards = await getBoards();
+  return json<ILoaderData>({ boards: boards.data as TBoard[] });
+}; // 로그인 여부 검증 및 작성할 게시판 선택 시 필요한 게시판 리스트 반환
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const { headers } = await authenticate(request);
+  const user = await getUser(request);
+  const data = qs.parse(await request.text()) as unknown as InputData;
+
+  if (data.board_id && data.title && data.content) {
+    const post = await createPost(
+      data.title,
+      data.content,
+      data.board_id,
+      user.user.id
+    );
+    return redirect(`/`, {
+      headers,
+    });
+  }
+
+  return json<IActionData>({
+    message: {
+      title: "글 작성 실패",
+      message: "제목과 내용을 모두 입력해주세요.",
+      color: "red",
+    },
+  });
 };
 
 export default function PostCreate() {
+  const { boards } = useLoaderData<ILoaderData>();
+  const actionData = useActionData<IActionData>();
+  const [message, setMessage] = useState<IActionData>();
+
+  useEffect(() => {
+    if (actionData) {
+      setMessage(actionData);
+    }
+  }, [actionData]);
+
+  useEffect(() => {
+    if (message) {
+      showNotification({
+        title: message.message.title,
+        message: message.message.message,
+        color: message.message.color,
+      });
+    }
+  }, [message]);
 
   return (
     <Box
@@ -38,13 +104,10 @@ export default function PostCreate() {
             name="board_id"
             size="xl"
             placeholder="게시판"
-            data={[{
-              value: '1',
-              label: '공지사항'
-            }, {
-              value: '2',
-              label: '자유게시판'
-            }]}
+            data={boards.map((board) => ({
+              value: board.id.toString(),
+              label: board.name,
+            }))}    
           />
           <Space w="md" />
           <TextInput
