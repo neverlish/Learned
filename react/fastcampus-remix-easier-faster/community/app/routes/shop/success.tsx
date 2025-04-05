1bd9033ad2143eda82bbc8885efb82ba4e7b3bf3
Jinho Hyeon <neverlish@gmail.com>
2025-04-05 12:55:10 +0900

    5 - 3 - 11 결제 컴포넌트 구현하기 - 2

--- react/fastcampus-remix-easier-faster/community/app/routes/shop/success.tsx
+++ react/fastcampus-remix-easier-faster/community/app/routes/shop/success.tsx
@@ -1,14 +1,22 @@
-import { Box } from "@mantine/core";
+import { Alert, Box, Space, Title } from "@mantine/core";
+import { IconCheck } from "@tabler/icons-react";
 
 export default function Shop() {
   return (
-    <Box sx={{
-      padding: '0 50px',
-      paddingTop: '50px',
-      width: 'calc(100% - 100px)',
-      maxWidth: '1100px',
-      margin: '0 auto',
-    }}>
+    <Box
+      sx={{
+        padding: "0 50px",
+        paddingTop: "50px",
+        width: "calc(100% - 100px)",
+        maxWidth: "1100px",
+        margin: "0 auto",
+      }}
+    >
+      <Title>포인트 구매완료</Title>
+      <Space h="xl" />
+      <Alert icon={<IconCheck size="1rem" />} title="결제 완료">
+        포인트 구매가 완료되었습니다.
+      </Alert>
     </Box>
-  )
+  );
 }
