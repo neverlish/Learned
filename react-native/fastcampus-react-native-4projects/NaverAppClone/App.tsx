97ea39659611e4e09221eaeab2d969f1aeb13bbb
Jinho Hyeon <neverlish@gmail.com>
2025-05-17 00:45:18 +0900

    2 - 1 - 4-1 네이버 홈 화면 구현하기 (React Native CLI) (1)

--- react-native/fastcampus-react-native-4projects/NaverAppClone/App.tsx
+++ react-native/fastcampus-react-native-4projects/NaverAppClone/App.tsx
@@ -1,20 +1,46 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import ShoppingScreen from './screens/ShoppingScreen';
 import {RootStackParamList, RouteNames} from './routes';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BrowserScreen from './screens/BrowserScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
+const HomeIcon = ({focused, color}: {focused: boolean; color: string}) => {
+  const iconName = focused ? 'home' : 'home-outline';
+  return <MaterialCommunityIcons name={iconName} color={color} size={26} />;
+};
+
+const ShoppingIcon = ({focused, color}: {focused: boolean; color: string}) => {
+  const iconName = focused ? 'shopping' : 'shopping-outline';
+  return <MaterialCommunityIcons name={iconName} color={color} size={26} />;
+};
+
 const HomeTab = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name={RouteNames.HOME} component={HomeScreen} />
-      <Tab.Screen name={RouteNames.SHOPPING} component={ShoppingScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: 'black',
+        },
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: 'white',
+      }}>
+      <Tab.Screen
+        name={RouteNames.HOME}
+        component={HomeScreen}
+        options={{tabBarLabel: '홈', tabBarIcon: HomeIcon}}
+      />
+      <Tab.Screen
+        name={RouteNames.SHOPPING}
+        component={ShoppingScreen}
+        options={{tabBarLabel: '쇼핑', tabBarIcon: ShoppingIcon}}
+      />
     </Tab.Navigator>
   );
 };
