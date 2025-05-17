bc573337ff7a39e89ec92fa6d4cbd81dcf7d25d6
Jinho Hyeon <neverlish@gmail.com>
2025-05-17 21:27:10 +0900

    2 - 1 - 13-2 브라우저 기능 구현하기 (Expo) (2)

--- react-native/fastcampus-react-native-4projects/NaverAppCloneExpo/app/browser.tsx
+++ react-native/fastcampus-react-native-4projects/NaverAppCloneExpo/app/browser.tsx
@@ -1,15 +1,17 @@
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import React, { useMemo, useRef, useState } from "react";
 import {
   Animated,
   Platform,
+  SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import WebView from "react-native-webview";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 const styles = StyleSheet.create({
   safearea: {
@@ -34,7 +36,53 @@ const styles = StyleSheet.create({
     height: "100%",
     backgroundColor: "green",
   },
+  navigator: {
+    backgroundColor: "black",
+    flexDirection: "row",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    justifyContent: "space-between",
+  },
+  button: {
+    width: 30,
+    height: 30,
+    padding: 4,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  naverIconOutline: {
+    borderWidth: 1,
+    borderColor: "white",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  naverIconText: {
+    color: "white",
+  },
 });
+
+const NavButton = ({
+  iconName,
+  disabled,
+  onPress,
+}: {
+  iconName: keyof typeof MaterialCommunityIcons.glyphMap;
+  disabled?: boolean;
+  onPress?: () => void;
+}) => {
+  const color = disabled ? "gray" : "white";
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      disabled={disabled}
+      onPress={onPress}
+    >
+      <MaterialCommunityIcons name={iconName} color={color} size={24} />
+    </TouchableOpacity>
+  );
+};
 
 const BrowserScreen = () => {
   const params = useLocalSearchParams();
@@ -46,6 +94,10 @@ const BrowserScreen = () => {
   );
 
   const progressAnim = useRef(new Animated.Value(0)).current;
+
+  const webViewRef = useRef<WebView | null>(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+  const [canGoForward, setCanGoForward] = useState(false);
 
   return (
     <SafeAreaView style={styles.safearea}>
@@ -66,9 +118,12 @@ const BrowserScreen = () => {
         />
       </View>
       <WebView
+        ref={webViewRef}
         source={{ uri: initialUrl }}
         onNavigationStateChange={(event) => {
           setUrl(event.url);
+          setCanGoBack(event.canGoBack);
+          setCanGoForward(event.canGoForward);
         }}
         onLoadProgress={(event) => {
           progressAnim.setValue(event.nativeEvent.progress);
@@ -77,6 +132,38 @@ const BrowserScreen = () => {
           progressAnim.setValue(0);
         }}
       />
+      <View style={styles.navigator}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            router.back();
+          }}
+        >
+          <View style={styles.naverIconOutline}>
+            <Text style={styles.naverIconText}>N</Text>
+          </View>
+        </TouchableOpacity>
+        <NavButton
+          iconName="arrow-left"
+          disabled={!canGoBack}
+          onPress={() => {
+            webViewRef.current?.goBack();
+          }}
+        />
+        <NavButton
+          iconName="arrow-right"
+          disabled={!canGoForward}
+          onPress={() => {
+            webViewRef.current?.goForward();
+          }}
+        />
+        <NavButton
+          iconName="refresh"
+          onPress={() => {
+            webViewRef.current?.reload();
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
