0da160efc06bbccf00eeb80fe5ecf55f7f06dcd7
Jinho Hyeon <neverlish@gmail.com>
2025-05-19 21:02:02 +0900

    3 - 1 - 9-1 유튜브 영상 로드 (Expo)_1

--- react-native/fastcampus-react-native-4projects/LoopTubeExpo/App.tsx
+++ react-native/fastcampus-react-native-4projects/LoopTubeExpo/App.tsx
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, {useState} from 'react';
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Icon from '@expo/vector-icons/MaterialIcons';
 
 const styles = StyleSheet.create({
-  container: {
+  safearea: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#242424',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  inputConatainer: {
+    backgroundColor: '#1A1A1A',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    marginHorizontal: 16,
+    borderRadius: 10,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginVertical: 16,
+  },
+  input: {
+    fontSize: 15,
+    color: '#AEAEB2',
+    flex: 1,
+    marginRight: 4,
   },
 });
+
+export default function App() {
+  const [url, setUrl] = useState('');
+
+  return (
+    <SafeAreaView style={styles.safearea}>
+      <View style={styles.inputConatainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="클릭하여 링크를 삽입하세요"
+          placeholderTextColor={'#AEAEB2'}
+          onChangeText={setUrl}
+          value={url}
+          inputMode="url"
+        />
+        <TouchableOpacity hitSlop={{top: 10, bottom: 10, left: 10, right: 10}}>
+          <Icon name="add-link" size={24} color={'#AEAEB2'} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
