8c35ddefbbfce234073ecbb4562cbec400a387b7
Jinho Hyeon <neverlish@gmail.com>
2025-05-19 21:03:45 +0900

    3 - 1 - 9-2 유튜브 영상 로드 (Expo)_2

--- react-native/fastcampus-react-native-4projects/LoopTubeExpo/App.tsx
+++ react-native/fastcampus-react-native-4projects/LoopTubeExpo/App.tsx
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import {
+  Alert,
+  Dimensions,
   Platform,
   SafeAreaView,
   StatusBar,
@@ -9,6 +11,11 @@ import {
   View,
 } from 'react-native';
 import Icon from '@expo/vector-icons/MaterialIcons';
+import queryString from 'query-string';
+import WebView from 'react-native-webview';
+
+const YT_WIDTH = Dimensions.get('window').width;
+const YT_HEIGHT = YT_WIDTH * (9 / 16);
 
 const styles = StyleSheet.create({
   safearea: {
@@ -32,10 +39,85 @@ const styles = StyleSheet.create({
     flex: 1,
     marginRight: 4,
   },
+  youtubeContainer: {
+    width: YT_WIDTH,
+    height: YT_HEIGHT,
+    backgroundColor: '#4A4A4A',
+  },
 });
 
-export default function App() {
+const App = () => {
   const [url, setUrl] = useState('');
+  const [youTubeId, setYouTubeId] = useState('');
+
+  const onPressOpenLink = useCallback(() => {
+    const {
+      query: {v: id},
+    } = queryString.parseUrl(url);
+    console.log('id', id);
+
+    if (typeof id === 'string') {
+      setYouTubeId(id);
+    } else {
+      Alert.alert('잘못된 URL입니다.');
+    }
+  }, [url]);
+
+  const source = useMemo(() => {
+    const html = `
+    <!DOCTYPE html>
+    <html>
+      <body>
+        <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
+        <div id="player"></div>
+
+        <script>
+          // 2. This code loads the IFrame Player API code asynchronously.
+          var tag = document.createElement('script');
+
+          tag.src = "https://www.youtube.com/iframe_api";
+          var firstScriptTag = document.getElementsByTagName('script')[0];
+          firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+          // 3. This function creates an <iframe> (and YouTube player)
+          //    after the API code downloads.
+          var player;
+          function onYouTubeIframeAPIReady() {
+            player = new YT.Player('player', {
+              height: '${YT_HEIGHT}',
+              width: '${YT_WIDTH}',
+              videoId: '${youTubeId}',
+              events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+              }
+            });
+          }
+
+          // 4. The API will call this function when the video player is ready.
+          function onPlayerReady(event) {
+            event.target.playVideo();
+          }
+
+          // 5. The API calls this function when the player's state changes.
+          //    The function indicates that when playing a video (state=1),
+          //    the player should play for six seconds and then stop.
+          var done = false;
+          function onPlayerStateChange(event) {
+            if (event.data == YT.PlayerState.PLAYING && !done) {
+              setTimeout(stopVideo, 6000);
+              done = true;
+            }
+          }
+          function stopVideo() {
+            player.stopVideo();
+          }
+        </script>
+      </body>
+    </html>
+    `;
+    return {html};
+  }, [youTubeId]);
 
   return (
     <SafeAreaView style={styles.safearea}>
@@ -48,10 +130,17 @@ export default function App() {
           value={url}
           inputMode="url"
         />
-        <TouchableOpacity hitSlop={{top: 10, bottom: 10, left: 10, right: 10}}>
+        <TouchableOpacity
+          onPress={onPressOpenLink}
+          hitSlop={{top: 10, bottom: 10, left: 10, right: 10}}>
           <Icon name="add-link" size={24} color={'#AEAEB2'} />
         </TouchableOpacity>
       </View>
+      <View style={styles.youtubeContainer}>
+        {youTubeId.length > 0 && <WebView source={source} />}
+      </View>
     </SafeAreaView>
   );
-}
+};
+
+export default App;
