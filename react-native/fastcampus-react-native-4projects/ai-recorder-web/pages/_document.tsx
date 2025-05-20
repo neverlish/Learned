e3beb44d4aabefcea3b160e0f9622c3dd4de5bc9
Jinho Hyeon <neverlish@gmail.com>
2025-05-20 21:22:16 +0900

    4 - 1 - 3-1 녹음 화면 구현 (웹) 1

--- react-native/fastcampus-react-native-4projects/ai-recorder-web/pages/_document.tsx
+++ react-native/fastcampus-react-native-4projects/ai-recorder-web/pages/_document.tsx
@@ -3,8 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="max-w-screen-sm mx-auto">
         <Main />
         <NextScript />
       </body>
