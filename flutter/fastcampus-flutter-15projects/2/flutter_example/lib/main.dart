0b07be7c435bec86ea4bb0e124032fbb9c48a2e3
Jinho Hyeon <neverlish@gmail.com>
2025-05-25 23:15:48 +0900

    2 - 2 - 4 (Tip) Flutter의 Constraint

--- flutter/fastcampus-flutter-15projects/2/flutter_example/lib/main.dart
+++ flutter/fastcampus-flutter-15projects/2/flutter_example/lib/main.dart
@@ -2,76 +2,103 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          
-        ),
-        body: const Body(),
+        body: HomeWidget(),
       ),
     ),
   );
 }
 
-// class Body extends StatelessWidget {
-//   const Body({super.key});
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: ConstraintsWidget(),
+      ),
+    );
+  }
+}
+
+// class ConstraintsWidget extends StatelessWidget {
+//   const ConstraintsWidget({super.key});
 
 //   @override
 //   Widget build(BuildContext context) {
-//     return Stack(
-//       children: [
-//         Container(width: 500, height: 500, color: Colors.black),
-//         Container(width: 400, height: 400, color: Colors.red),
-//         Container(width: 300, height: 300, color: Colors.blue),
-//         // Positioned(
-//         //   left: 20,
-//         //   top: 20,
-//         //   child: Container(width: 200, height: 200, color: Colors.green),
-//         // ),
-//         Align(
-//           // alignment: Alignment.topRight,
-//           alignment: const Alignment(0.5, -0.5),
-//           child: Container(width: 200, height: 200, color: Colors.green),
+//     return Container(
+//       height: 500,
+//       width: 500,
+//       color: Colors.blue,
+//       // child: SingleChi
+//       //   child: Container(
+//       //     color: Colors.red,
+//       //     height: 300,
+//       //     width: 300,
+//       //   )
+//       // child: Center(
+//       //   child: Container(
+//       //     // constraints: const BoxConstraints(
+//       //     //     minHeight: 200, minWidth: 200, maxWidth: 250, maxHeight: 250),
+//       //     constraints: BoxConstraints.tight(const Size(200, 200)),
+//       //     color: Colors.red,
+//       //     padding: const EdgeInsets.all(8),
+//       //     child: Container(
+//       //       margin: const EdgeInsets.all(20),
+//       //       width: 50,
+//       //       height: 50,
+//       //       color: Colors.green,
+//       //     ),
+//       //   ),
+//       // ),
+//       // child: Positioned(
+//       //   child: UnconstrainedBox(
+//       //     child: Container(
+//       //       width: 300,
+//       //       height: 700,
+//       //       color: Colors.green,
+//       //     ),
+//       //   ),
+//       // ),
+//       child: Center(
+//         child: OverflowBox(
+//           child: Container(
+//             width: 300,
+//             height: 700,
+//             color: Colors.green,
+//           ),
 //         ),
-//       ],
+//       ),
 //     );
 //   }
 // }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class ConstraintsWidget extends StatelessWidget {
+  const ConstraintsWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Row(
       children: [
-        Center(
+        Flexible(
           child: Container(
-            width: 300,
-            height: 300,
-            decoration: BoxDecoration(
-                color: Colors.red,
-              borderRadius: BorderRadius.circular(150)),
-          ),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: Container(
-            width: 280,
-            height: 280,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(140),
+            color: Colors.blue,
+            child: const Text(
+              "Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!",
+              style: TextStyle(
+                fontSize: 30,
+              ),
             ),
           ),
         ),
-        const Align(
-          alignment: Alignment.center,
-          child: Text(
-            'Count 0',
-            style: TextStyle(color: Colors.red, fontSize: 42),
+        Flexible(
+          child: Container(
+            color: Colors.red,
+            child: const Text("Hello world!"),
           ),
-        )
+        ),
       ],
     );
   }
