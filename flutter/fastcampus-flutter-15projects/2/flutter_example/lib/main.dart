077fe51f8d01bc7a4bb87f76a062134f63ab9fa0
Jinho Hyeon <neverlish@gmail.com>
2025-05-26 20:27:18 +0900

    2 - 6 Flutter Setting - 2 Flutter에서 외부 라이브러리를 쓰기

--- flutter/fastcampus-flutter-15projects/2/flutter_example/lib/main.dart
+++ flutter/fastcampus-flutter-15projects/2/flutter_example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,62 +16,25 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const TestWidget();
+    return const RandomWords();
   }
 }
 
-class TestWidget extends StatefulWidget {
-  const TestWidget({super.key});
-
-  @override
-  State<TestWidget> createState() => _TestWidgetState();
-}
-
-class _TestWidgetState extends State<TestWidget> {
-  int value = 0;
+class RandomWords extends StatelessWidget {
+  const RandomWords({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final wordList = generateWordPairs().take(5).toList();
+    final widgets = wordList
+        .map((word) => Text(
+              word.asCamelCase,
+              style: const TextStyle(fontSize: 32),
+            ))
+        .toList();
+
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Count : $value',
-          style: const TextStyle(fontSize: 30),
-        ),
-        TestButton(addCounter),
-      ],
-    );
-  }
-
-  void addCounter(int addValue) => setState(() => value = addValue + value);
-}
-
-class TestButton extends StatelessWidget {
-  const TestButton(this.callback, {super.key});
-
-  final Function(int) callback;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8),
-      width: double.infinity,
-      child: InkWell(
-        onTap: () => callback.call(1),
-        onDoubleTap: () => callback.call(5),
-        onLongPress: () => callback.call(10),
-        child: Center(
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            decoration: BoxDecoration(border: Border.all()),
-            child: const Text(
-              'Up Counter',
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-        ),
-      ),
+      children: widgets,
     );
   }
 }
