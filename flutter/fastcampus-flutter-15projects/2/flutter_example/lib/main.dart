eea7f0a264cc311cd4a9aea1c13d7bffe21560df
Jinho Hyeon <neverlish@gmail.com>
2025-05-26 19:46:01 +0900

    2 - 3 Flutter State Basic - 1 Stateful Widget vs Stateless Widget

--- flutter/fastcampus-flutter-15projects/2/flutter_example/lib/main.dart
+++ flutter/fastcampus-flutter-15projects/2/flutter_example/lib/main.dart
@@ -4,102 +4,84 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: HomeWidget(),
+        body: Body(),
       ),
     ),
   );
 }
 
-class HomeWidget extends StatelessWidget {
-  const HomeWidget({super.key});
+class Body extends StatelessWidget {
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(
-        body: ConstraintsWidget(),
+    return const Column(
+      children: [
+        ExampleStateless(),
+        ExampleStateful(index: 3),
+      ],
+    );
+  }
+}
+
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.red,
       ),
     );
   }
 }
 
-// class ConstraintsWidget extends StatelessWidget {
-//   const ConstraintsWidget({super.key});
+class ExampleStateful extends StatefulWidget {
+  final int index;
+  const ExampleStateful({required this.index, super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       height: 500,
-//       width: 500,
-//       color: Colors.blue,
-//       // child: SingleChi
-//       //   child: Container(
-//       //     color: Colors.red,
-//       //     height: 300,
-//       //     width: 300,
-//       //   )
-//       // child: Center(
-//       //   child: Container(
-//       //     // constraints: const BoxConstraints(
-//       //     //     minHeight: 200, minWidth: 200, maxWidth: 250, maxHeight: 250),
-//       //     constraints: BoxConstraints.tight(const Size(200, 200)),
-//       //     color: Colors.red,
-//       //     padding: const EdgeInsets.all(8),
-//       //     child: Container(
-//       //       margin: const EdgeInsets.all(20),
-//       //       width: 50,
-//       //       height: 50,
-//       //       color: Colors.green,
-//       //     ),
-//       //   ),
-//       // ),
-//       // child: Positioned(
-//       //   child: UnconstrainedBox(
-//       //     child: Container(
-//       //       width: 300,
-//       //       height: 700,
-//       //       color: Colors.green,
-//       //     ),
-//       //   ),
-//       // ),
-//       child: Center(
-//         child: OverflowBox(
-//           child: Container(
-//             width: 300,
-//             height: 700,
-//             color: Colors.green,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
 
-class ConstraintsWidget extends StatelessWidget {
-  const ConstraintsWidget({super.key});
+class _ExampleStatefulState extends State<ExampleStateful> {
+  late int _index;
+  late TextEditingController textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.index;
+    textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Flexible(
-          child: Container(
-            color: Colors.blue,
-            child: const Text(
-              "Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!",
-              style: TextStyle(
-                fontSize: 30,
-              ),
-            ),
-          ),
+    return Expanded(
+      flex: 1,
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (_index == 5) {
+              _index = 0;
+              return;
+            }
+            _index++;
+          });
+        },
+        child: Container(
+          color: Colors.blue.withOpacity(_index / 5),
+          child: Center(child: Text('$_index')),
         ),
-        Flexible(
-          child: Container(
-            color: Colors.red,
-            child: const Text("Hello world!"),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
