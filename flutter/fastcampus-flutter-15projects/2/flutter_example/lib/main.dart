f062b6302494837ee6cd0e01c586f7d7000ad494
Jinho Hyeon <neverlish@gmail.com>
2025-05-26 20:11:03 +0900

    2 - 5 Flutter Gesture - 1 Flutter의 다양한 입력들

--- flutter/fastcampus-flutter-15projects/2/flutter_example/lib/main.dart
+++ flutter/fastcampus-flutter-15projects/2/flutter_example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,71 +18,200 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        ExampleStateless(),
-        ExampleStateful(index: 3),
+        TestCheckBox(),
+        TestRadioButton(),
+        TestSlider(),
+        TestSwitch(),
+        TestPopupMenu(),
       ],
     );
   }
 }
 
-class ExampleStateless extends StatelessWidget {
-  const ExampleStateless({super.key});
+class TestCheckBox extends StatefulWidget {
+  const TestCheckBox({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: 1,
-      child: Container(
-        color: Colors.red,
-      ),
-    );
-  }
+  State<TestCheckBox> createState() => _TestCheckBoxState();
 }
 
-class ExampleStateful extends StatefulWidget {
-  final int index;
-  const ExampleStateful({required this.index, super.key});
-
-  @override
-  State<ExampleStateful> createState() => _ExampleStatefulState();
-}
-
-class _ExampleStatefulState extends State<ExampleStateful> {
-  late int _index;
-  late TextEditingController textController;
+class _TestCheckBoxState extends State<TestCheckBox> {
+  late List<bool> values;
 
   @override
   void initState() {
     super.initState();
-    _index = widget.index;
-    textController = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    textController.dispose();
-    super.dispose();
+    values = [false, false, false];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 1,
-      child: GestureDetector(
-        onTap: () {
-          setState(() {
-            if (_index == 5) {
-              _index = 0;
-              return;
-            }
-            _index++;
-          });
-        },
-        child: Container(
-          color: Colors.blue.withOpacity(_index / 5),
-          child: Center(child: Text('$_index')),
+    return Row(
+      children: [
+        Checkbox(
+          value: values[0],
+          onChanged: (value) => changeValue(0, value: value),
         ),
-      ),
+        Checkbox(
+          value: values[1],
+          onChanged: (value) => changeValue(1, value: value),
+        ),
+        Checkbox(
+          value: values[2],
+          onChanged: (value) => changeValue(2, value: value),
+        ),
+      ],
+    );
+  }
+
+  void changeValue(int index, {bool? value = false}) {
+    setState(() {
+      values[index] = value!;
+    });
+  }
+}
+
+class TestRadioButton extends StatefulWidget {
+  const TestRadioButton({super.key});
+
+  @override
+  State<TestRadioButton> createState() => _TestRadioButtonState();
+}
+
+enum TestTestValue {
+  test1,
+  test2,
+  test3,
+}
+
+class _TestRadioButtonState extends State<TestRadioButton> {
+  TestTestValue? selectValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Radio<TestTestValue>(
+            value: TestTestValue.test1,
+            groupValue: selectValue,
+            onChanged: (value) => setState(() => selectValue = value!),
+          ),
+          title: Text(TestTestValue.test1.name),
+          onTap: () => {
+            setState(() {
+              if (selectValue != TestTestValue.test1) {
+                selectValue = TestTestValue.test1;
+              }
+            }),
+          },
+        ),
+        ListTile(
+          leading: Radio<TestTestValue>(
+            value: TestTestValue.test2,
+            groupValue: selectValue,
+            onChanged: (value) => setState(() => selectValue = value!),
+          ),
+          title: Text(TestTestValue.test2.name),
+        ),
+        ListTile(
+          leading: Radio<TestTestValue>(
+            value: TestTestValue.test3,
+            groupValue: selectValue,
+            onChanged: (value) => setState(() => selectValue = value!),
+          ),
+          title: Text(TestTestValue.test3.name),
+        ),
+      ],
+    );
+  }
+}
+
+class TestSlider extends StatefulWidget {
+  const TestSlider({super.key});
+
+  @override
+  State<TestSlider> createState() => _TestSliderState();
+}
+
+class _TestSliderState extends State<TestSlider> {
+  double value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('$value'),
+        Slider(
+          value: value,
+          onChanged: (newValue) => setState(() => value = newValue),
+          divisions: 100,
+          max: 100,
+          min: 0,
+          label: value.round().toString(),
+          // activeColor: Colors.red,
+        ),
+      ],
+    );
+  }
+}
+
+class TestSwitch extends StatefulWidget {
+  const TestSwitch({super.key});
+
+  @override
+  State<TestSwitch> createState() => _TestSwitchState();
+}
+
+class _TestSwitchState extends State<TestSwitch> {
+  bool value = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Switch(
+          value: value,
+          onChanged: (newValue) => setState(() => value = newValue),
+        ),
+        CupertinoSwitch(
+          value: value,
+          onChanged: (newValue) => setState(() => value = newValue),
+        ),
+      ],
+    );
+  }
+}
+
+class TestPopupMenu extends StatefulWidget {
+  const TestPopupMenu({super.key});
+
+  @override
+  State<TestPopupMenu> createState() => _TestPopupMenuState();
+}
+
+class _TestPopupMenuState extends State<TestPopupMenu> {
+  TestTestValue selectValue = TestTestValue.test1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(selectValue.name),
+        PopupMenuButton(
+          itemBuilder: (context) {
+            return TestTestValue.values
+                .map(
+                  (value) => PopupMenuItem(
+                    value: value,
+                    child: Text(value.name),
+                  ),
+                )
+                .toList();
+          },
+          onSelected: (value) => setState(() => selectValue = value),
+        ),
+      ],
     );
   }
 }
