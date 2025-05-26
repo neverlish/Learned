d128e3a1ec0046abfd5c61fdfbc2e59731ea549d
Jinho Hyeon <neverlish@gmail.com>
2025-05-26 20:41:19 +0900

    2 - 7 Flutter Navigator - 1 Flutter에서 페이지에서 페이지 이동하기 1

--- flutter/fastcampus-flutter-15projects/2/flutter_example/lib/main.dart
+++ flutter/fastcampus-flutter-15projects/2/flutter_example/lib/main.dart
@@ -6,20 +6,54 @@ const bannerImage = '$assetImagePath/banner.png';
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(
-        body: Body(),
-      ),
+      home: HomeWidget()
     ),
   );
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(bannerImage),
+    return Scaffold(
+      appBar: AppBar(),
+      body: homeBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User'),
+        ],
+        currentIndex: index,
+        onTap: (newIndex) => setState(() {
+          index = newIndex;
+        }),
+      ),
     );
+  }
+
+  Widget homeBody() {
+    switch (index) {
+      case 1:
+        return const Center(child: Icon(Icons.search, size: 100));
+      case 2:
+        return const Center(child: Icon(Icons.person, size: 100));
+      default:
+        return const Center(child: Icon(Icons.home, size: 100));
+    }
   }
 }
