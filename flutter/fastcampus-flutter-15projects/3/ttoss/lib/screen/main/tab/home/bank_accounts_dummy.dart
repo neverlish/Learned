29bf63972128e9d4b0920452877ec22a91ec0b01
Jinho Hyeon <neverlish@gmail.com>
2025-05-28 16:25:18 +0900

    3 - 2 - 7 기초, List, Map 데이터 구조

--- flutter/fastcampus-flutter-15projects/3/ttoss/lib/screen/main/tab/home/bank_accounts_dummy.dart
+++ flutter/fastcampus-flutter-15projects/3/ttoss/lib/screen/main/tab/home/bank_accounts_dummy.dart
@@ -12,7 +12,24 @@ final bankAccountKakao =
     BankAccount(bankKakao, 7000000, accountTypeName: "입출금통장");
 
 main() {
-  print(bankAccounts[0].accountTypeName);
+  // print(bankAccounts[0].accountTypeName);
+
+  // for (final item in bankAccounts) {
+  //   print(item.accountTypeName);
+  // }
+  // final shinhanBank = bankMap["shinhan1"];
+  // print(shinhanBank == bankAccountShinhan1);
+  //
+  // for (final entry in bankMap.entries) {
+  //   print(
+  //       "${entry.key}:${entry.value.accountTypeName ?? entry.value.bank.name}");
+  // }
+  //
+  print(bankSet.contains(bankAccountShinhan1));
+  // print(bankSet.length);
+
+  // bankAccounts.toSet();
+  // bankSet.toList();
 }
 
 final bankAccounts = [
@@ -22,3 +39,16 @@ final bankAccounts = [
   bankAccountToss,
   bankAccountKakao
 ];
+
+final bankMap = {
+  "shinhan1": bankAccountShinhan1,
+  "shinhan2": bankAccountShinhan2,
+};
+
+final bankSet = {
+  bankAccountShinhan1,
+  bankAccountShinhan2,
+  bankAccountShinhan3,
+  bankAccountToss,
+  bankAccountKakao,
+};
