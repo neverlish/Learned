be8432521cafc4488afddceea649b366431799c3
Jinho Hyeon <neverlish@gmail.com>
2025-05-28 16:52:13 +0900

    3 - 2 - 8 Home만들기

--- flutter/fastcampus-flutter-15projects/3/ttoss/lib/screen/main/tab/home/f_home.dart
+++ flutter/fastcampus-flutter-15projects/3/ttoss/lib/screen/main/tab/home/f_home.dart
@@ -1,7 +1,10 @@
 import 'package:fast_app_base/common/common.dart';
-import 'package:fast_app_base/common/widget/round_button_theme.dart';
-import 'package:fast_app_base/common/widget/w_round_button.dart';
+import 'package:fast_app_base/common/widget/w_big_button.dart';
+import 'package:fast_app_base/common/widget/w_rounded_container.dart';
 import 'package:fast_app_base/screen/dialog/d_message.dart';
+import 'package:fast_app_base/screen/main/tab/home/bank_accounts_dummy.dart';
+import 'package:fast_app_base/screen/main/tab/home/w_bank_account.dart';
+import 'package:fast_app_base/screen/main/tab/home/w_ttoss_app_bar.dart';
 import 'package:flutter/material.dart';
 
 import '../../../dialog/d_color_bottom.dart';
@@ -15,45 +18,39 @@ class HomeFragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: context.appColors.seedColor.getMaterialColorValues[100],
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            children: [
-              IconButton(
-                onPressed: () => openDrawer(context),
-                icon: const Icon(Icons.menu),
-              )
-            ],
-          ),
-          const EmptyExpanded(),
-          RoundButton(
-            text: 'Snackbar 보이기',
-            onTap: () => showSnackbar(context),
-            theme: RoundButtonTheme.blue,
-          ),
-          const Height(20),
-          RoundButton(
-            text: 'Confirm 다이얼로그',
-            onTap: () => showConfirmDialog(context),
-            theme: RoundButtonTheme.whiteWithBlueBorder,
-          ),
-          const Height(20),
-          RoundButton(
-            text: 'Message 다이얼로그',
-            onTap: showMessageDialog,
-            theme: RoundButtonTheme.whiteWithBlueBorder,
-          ),
-          const Height(20),
-          RoundButton(
-            text: '메뉴 보기',
-            onTap: () => openDrawer(context),
-            theme: RoundButtonTheme.blink,
-          ),
-          const EmptyExpanded()
-        ],
+      color: Colors.black,
+      child: Container(
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              padding: const EdgeInsets.only(top: 60),
+              child: Column(
+                children: [
+                  BigButton(
+                    "토스뱅크",
+                    onTap: () {
+                      context.showSnackbar("토스뱅크를 눌렀어요.");
+                    },
+                  ),
+                  height10,
+                  RoundedContainer(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        "자산".text.bold.white.make(),
+                        height5,
+                        ...bankAccounts
+                            .map((e) => BankAccountWidget(e))
+                            .toList()
+                      ],
+                    ),
+                  ),
+                ],
+              ).pSymmetric(h: 20),
+            ),
+            const TtossAppBar(),
+          ],
+        ),
       ),
     );
   }
---
+++ flutter/fastcampus-flutter-15projects/3/ttoss/lib/screen/main/tab/home/w_ttoss_app_bar.dart
@@ -0,0 +1,66 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:flutter/material.dart';
+
+class TtossAppBar extends StatefulWidget {
+  const TtossAppBar({super.key});
+
+  @override
+  State<TtossAppBar> createState() => _TtossAppBarState();
+}
+
+class _TtossAppBarState extends State<TtossAppBar> {
+  bool _showRedDot = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      color: context.appColors.appBarBackground,
+      child: Row(
+        children: [
+          width10,
+          Image.asset(
+            '$basePath/icon/toss.png',
+            height: 30,
+          ),
+          emptyExpanded,
+          Image.asset(
+            '$basePath/icon/map_point.png',
+            height: 30,
+          ),
+          width10,
+          Tap(
+            onTap: () {
+              setState(() {
+                _showRedDot = !_showRedDot;
+              });
+            },
+            child: Stack(
+              children: [
+                Image.asset(
+                  '$basePath/icon/notification.png',
+                  height: 30,
+                ),
+                if (_showRedDot)
+                  Positioned.fill(
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        width: 6,
+                        height: 6,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  )
+              ],
+            ),
+          ),
+          width10,
+        ],
+      ),
+    );
+  }
+}
