244c769fc76b44f11c8ec9a13ade2ec4e3640ff4
Jinho Hyeon <neverlish@gmail.com>
2025-05-29 19:30:56 +0900

    3 - 3 - 5 스크롤에 따라 크기가 변하는 Title 구현하기

--- flutter/fastcampus-flutter-15projects/3/ttoss/lib/screen/main/tab/stock/setting/s_setting.dart
+++ flutter/fastcampus-flutter-15projects/3/ttoss/lib/screen/main/tab/stock/setting/s_setting.dart
@@ -4,6 +4,7 @@ import 'package:fast_app_base/common/data/preference/app_preferences.dart';
 import 'package:fast_app_base/common/data/preference/prefs.dart';
 import 'package:fast_app_base/common/widget/w_big_button.dart';
 import 'package:fast_app_base/screen/main/tab/stock/setting/d_number_dialog.dart';
+import 'package:fast_app_base/screen/main/tab/stock/setting/w_animated_app_bar.dart';
 import 'package:fast_app_base/screen/main/tab/stock/setting/w_switch_menu.dart';
 import 'package:fast_app_base/screen/opensource/s_opensource.dart';
 import 'package:flutter/material.dart';
@@ -17,64 +18,130 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
+  final scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: '설정'.text.make(),
-      ),
-      body: ListView(
+      body: Stack(
         children: [
-          Obx(
-            () => SwitchMenu(
-              '푸시 설정',
-              Prefs.isPushOnRx.get(),
-              onChanged: (isOn) {
-                Prefs.isPushOnRx.set(isOn);
-              },
-            ),
+          ListView(
+            controller: scrollController,
+            padding: const EdgeInsets.only(top: 150),
+            children: [
+              Obx(
+                () => SwitchMenu(
+                  '푸시 설정',
+                  Prefs.isPushOnRx.get(),
+                  onChanged: (isOn) {
+                    Prefs.isPushOnRx.set(isOn);
+                  },
+                ),
+              ),
+              Obx(
+                () => Slider(
+                  value: Prefs.sliderPosition.get(),
+                  onChanged: (value) {
+                    Prefs.sliderPosition.set(value);
+                  },
+                ),
+              ),
+              Obx(
+                () => BigButton(
+                  '날짜 ${Prefs.birthday.get() == null ? '' : Prefs.birthday.get()?.formattedDate}',
+                  onTap: () async {
+                    final date = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime.now().subtract(90.days),
+                      lastDate: DateTime.now().add(90.days),
+                    );
+                    if (date != null) {
+                      Prefs.birthday.set(date);
+                    }
+                  },
+                ),
+              ),
+              Obx(
+                () => BigButton(
+                  '저장된 숫자 ${Prefs.number.get()}',
+                  onTap: () async {
+                    final number = await NumberDialog().show();
+                    if (number != null) {
+                      Prefs.number.set(number);
+                    }
+                  },
+                ),
+              ),
+              BigButton(
+                '오픈소스 화면',
+                onTap: () async {
+                  Nav.push(const OpensourceScreen());
+                },
+              ),
+              BigButton(
+                '오픈소스 화면',
+                onTap: () async {
+                  Nav.push(const OpensourceScreen());
+                },
+              ),
+              BigButton(
+                '오픈소스 화면',
+                onTap: () async {
+                  Nav.push(const OpensourceScreen());
+                },
+              ),
+              BigButton(
+                '오픈소스 화면',
+                onTap: () async {
+                  Nav.push(const OpensourceScreen());
+                },
+              ),
+              BigButton(
+                '오픈소스 화면',
+                onTap: () async {
+                  Nav.push(const OpensourceScreen());
+                },
+              ),
+              BigButton(
+                '오픈소스 화면',
+                onTap: () async {
+                  Nav.push(const OpensourceScreen());
+                },
+              ),
+              BigButton(
+                '오픈소스 화면',
+                onTap: () async {
+                  Nav.push(const OpensourceScreen());
+                },
+              ),
+              BigButton(
+                '오픈소스 화면',
+                onTap: () async {
+                  Nav.push(const OpensourceScreen());
+                },
+              ),
+              BigButton(
+                '오픈소스 화면',
+                onTap: () async {
+                  Nav.push(const OpensourceScreen());
+                },
+              ),
+              BigButton(
+                '오픈소스 화면',
+                onTap: () async {
+                  Nav.push(const OpensourceScreen());
+                },
+              ),
+              BigButton(
+                '오픈소스 화면',
+                onTap: () async {
+                  Nav.push(const OpensourceScreen());
+                },
+              ),
+            ],
           ),
-          Obx(
-            () => Slider(
-              value: Prefs.sliderPosition.get(),
-              onChanged: (value) {
-                Prefs.sliderPosition.set(value);
-              },
-            ),
-          ),
-          Obx(
-            () => BigButton(
-              '날짜 ${Prefs.birthday.get() == null ? '' : Prefs.birthday.get()?.formattedDate}',
-              onTap: () async {
-                final date = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime.now().subtract(90.days),
-                  lastDate: DateTime.now().add(90.days),
-                );
-                if (date != null) {
-                  Prefs.birthday.set(date);
-                }
-              },
-            ),
-          ),
-          Obx(
-            () => BigButton(
-              '저장된 숫자 ${Prefs.number.get()}',
-              onTap: () async {
-                final number = await NumberDialog().show();
-                if (number != null) {
-                  Prefs.number.set(number);
-                }
-              },
-            ),
-          ),
-          BigButton(
-            '오픈소스 화면',
-            onTap: () async {
-              Nav.push(const OpensourceScreen());
-            },
-          )
+          AnimatedAppBar('설정', controller: scrollController),
         ],
       ),
     );
---
+++ flutter/fastcampus-flutter-15projects/3/ttoss/lib/screen/main/tab/stock/setting/w_animated_app_bar.dart
@@ -0,0 +1,75 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/common/widget/w_arrow.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+class AnimatedAppBar extends StatefulWidget {
+  final String title;
+  final ScrollController controller;
+  const AnimatedAppBar(this.title, {super.key, required this.controller});
+
+  @override
+  State<AnimatedAppBar> createState() => _AnimatedAppBarState();
+}
+
+class _AnimatedAppBarState extends State<AnimatedAppBar> {
+  Duration get duration => 10.ms;
+  double scrollPosition = 0;
+
+  @override
+  void initState() {
+    widget.controller.addListener(() {
+      setState(() {
+        scrollPosition = widget.controller.position.pixels;
+      });
+    });
+    super.initState();
+  }
+
+  bool get isTriggered => scrollPosition > 80;
+
+  double getValue(double initial, double target) {
+    if (isTriggered) {
+      return target;
+    }
+
+    double fraction = scrollPosition / 80;
+    return initial + (target - initial) * fraction;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: context.backgroundColor,
+      child: SafeArea(
+        child: Stack(
+          children: [
+            Tap(
+              onTap: () {
+                Nav.pop(context);
+              },
+              child: const Arrow(
+                direction: AxisDirection.left,
+              ).p20(),
+            ),
+            AnimatedContainer(
+              duration: duration,
+              padding: EdgeInsets.only(
+                  left: getValue(20, 50), top: getValue(50, 15)),
+              child: AnimatedDefaultTextStyle(
+                duration: duration,
+                style: TextStyle(
+                  fontSize: getValue(30, 18),
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                child: widget.title.text.make(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
