9dedd0e27de7ebe8e92b6c016ac725227c5e01a1
Jinho Hyeon <neverlish@gmail.com>
2025-06-08 09:38:03 +0900

    3 - 12 당근마켓 - FCM Push - 3 firebase_messaging 연동하기 -1

--- flutter/fastcampus-flutter-15projects/3/daangn/lib/main.dart
+++ flutter/fastcampus-flutter-15projects/3/daangn/lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:timeago/timeago.dart';
 
 import 'app.dart';
 import 'common/data/preference/app_preferences.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await AppPreferences.init();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   setLocaleMessages('ko', KoMessages());
 
