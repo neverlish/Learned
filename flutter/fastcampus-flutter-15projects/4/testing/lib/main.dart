e17a9a7a0b6d32c1e8f564d9ed126b6f0e915e98
Jinho Hyeon <neverlish@gmail.com>
2025-06-08 23:11:43 +0900

    4 - 8 Testing - 2 Unit Tests

---
+++ flutter/fastcampus-flutter-15projects/4/testing/lib/main.dart
@@ -0,0 +1,51 @@
+// Copyright 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:testing_app/models/favorites.dart';
+import 'package:testing_app/screens/favorites.dart';
+import 'package:testing_app/screens/home.dart';
+
+void main() {
+  runApp(const TestingApp());
+}
+
+GoRouter router() {
+  return GoRouter(
+    routes: [
+      GoRoute(
+        path: HomePage.routeName,
+        builder: (context, state) => const HomePage(),
+        routes: [
+          GoRoute(
+            path: FavoritesPage.routeName,
+            builder: (context, state) => const FavoritesPage(),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+class TestingApp extends StatelessWidget {
+  const TestingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: MaterialApp.router(
+        title: 'Testing Sample',
+        theme: ThemeData(
+          colorSchemeSeed: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          useMaterial3: true,
+        ),
+        routerConfig: router(),
+      ),
+    );
+  }
+}
