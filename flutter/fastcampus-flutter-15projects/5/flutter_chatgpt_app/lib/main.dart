edb2047b76e47c51f4b0eb0e9bc0cd7b47c9e0d5
Jinho Hyeon <neverlish@gmail.com>
2025-06-15 02:38:24 +0900

    5 - 3 - 7 홈 화면 구현

--- flutter/fastcampus-flutter-15projects/5/flutter_chatgpt_app/lib/main.dart
+++ flutter/fastcampus-flutter-15projects/5/flutter_chatgpt_app/lib/main.dart
@@ -30,40 +30,107 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController messageTextController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  static const String _kStrings = "Fastcampus Flutter ChatGPT";
+  String get _currentString => _kStrings;
+
+  @override
+  void dispose() {
+    messageTextController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    //
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.centerRight,
+                child: Card(
+                  child: PopupMenuButton(
+                    itemBuilder: (context) {
+                      return [
+                        const PopupMenuItem(
+                          child: ListTile(
+                            title: Text('히스토리'),
+                          ),
+                        ),
+                        const PopupMenuItem(
+                          child: ListTile(
+                            title: Text('설정'),
+                          ),
+                        ),
+                        const PopupMenuItem(
+                          child: ListTile(
+                            title: Text('새로운 채팅'),
+                          ),
+                        ),
+                      ];
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  child: const Center(child: Text(_kStrings)),
+                ),
+              ),
+              Dismissible(
+                key: const Key('chat-bar'),
+                direction: DismissDirection.startToEnd,
+                onDismissed: (d) {
+                  if (d == DismissDirection.startToEnd) {
+                    //
+                  }
+                },
+                background: const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('New Chat'),
+                  ],
+                ),
+                confirmDismiss: (d) async {
+                  if (d == DismissDirection.startToEnd) {
+                    //
+                  }
+                  return null;
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(32),
+                          border: Border.all(),
+                        ),
+                        child: TextField(
+                          controller: messageTextController,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Message',
+                          ),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      iconSize: 42,
+                      onPressed: () {},
+                      icon: const Icon(Icons.arrow_circle_up),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
