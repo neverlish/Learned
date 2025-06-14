ee361b2127198ee6c3e4e432967f6ded07686aa6
Jinho Hyeon <neverlish@gmail.com>
2025-06-14 20:57:47 +0900

    5 - 2 - 16 (Web) 퀴즈 출제자 앱 구현 - 퀴즈 시작

--- flutter/fastcampus-flutter-15projects/5/fastcampus_realtime_quiz_app/lib/web/quiz_manager_page.dart
+++ flutter/fastcampus-flutter-15projects/5/fastcampus_realtime_quiz_app/lib/web/quiz_manager_page.dart
@@ -89,6 +89,64 @@ class _QuizManagerPageState extends State<QuizManagerPage> {
     });
   }
 
+  startQuiz(Quiz item) async {
+    final ref =
+        await database?.ref("quiz_state/${item.quizDetailRef}/state").get();
+    final currentState = ref?.value as bool;
+    if (!currentState) {
+      final quizDetailRef =
+          await database?.ref("quiz_detail/${item.quizDetailRef}").get();
+
+      final problemCount =
+          quizDetailRef?.child("/problems").children.length ?? 0;
+
+      DateTime nowDatetime = DateTime.now();
+      List<Map> triggerTimes = [];
+      int solveTime = 5;
+      for (var i = 0; i < problemCount; i++) {
+        final startTime = nowDatetime.add(Duration(
+          seconds: 5 + (i * solveTime),
+        ));
+        final endTime = startTime.add(const Duration(seconds: 5));
+        triggerTimes.add({
+          "start": startTime.millisecondsSinceEpoch,
+          "end": endTime.millisecondsSinceEpoch,
+        });
+        nowDatetime = endTime;
+      }
+
+      if (context.mounted) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: const Text("퀴즈를 시작할까요? "),
+            title: const Text("안내"),
+            actions: [
+              TextButton(
+                onPressed: () async {
+                  await database
+                      ?.ref("quiz_state/${item.quizDetailRef}")
+                      .update(
+                    {
+                      "state": true,
+                      "current": 0,
+                      "triggers": triggerTimes,
+                    },
+                  );
+
+                  if (context.mounted) {
+                    Navigator.of(context).pop();
+                  }
+                },
+                child: const Text("네"),
+              )
+            ],
+          ),
+        );
+      }
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -166,7 +224,7 @@ class _QuizManagerPageState extends State<QuizManagerPage> {
                           title: Text("code: ${item.code}"),
                           subtitle: Text("${item.quizDetailRef}"),
                           onTap: () {
-                            //
+                            startQuiz(item);
                           },
                         );
                       }),
