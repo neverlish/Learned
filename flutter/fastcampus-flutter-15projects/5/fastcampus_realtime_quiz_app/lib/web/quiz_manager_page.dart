2321c4eaa168690c0510ddbc3b068eef22446a37
Jinho Hyeon <neverlish@gmail.com>
2025-06-14 20:25:15 +0900

    5 - 2 - 14 (Web) 퀴즈 출제자 앱 구현 - 퀴즈 출제 4

--- flutter/fastcampus-flutter-15projects/5/fastcampus_realtime_quiz_app/lib/web/quiz_manager_page.dart
+++ flutter/fastcampus-flutter-15projects/5/fastcampus_realtime_quiz_app/lib/web/quiz_manager_page.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
+import 'package:fastcampus_realtime_quiz_app/main.dart';
 import 'package:fastcampus_realtime_quiz_app/model/quiz.dart';
+import 'package:fastcampus_realtime_quiz_app/web/quiz_bottom_sheet_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class QuizManagerPage extends StatefulWidget {
@@ -12,9 +17,62 @@ class _QuizManagerPageState extends State<QuizManagerPage> {
   String? uid;
   List<QuizManager> quizItems = [];
 
+  signInAnonymously() {
+    FirebaseAuth.instance.signInAnonymously().then((value) => {
+          setState(() {
+            uid = value.user?.uid;
+          })
+        });
+  }
+
+  generateQuiz() async {
+    if (quizItems.isEmpty) {
+      return;
+    }
+    final pinCode = Random().nextInt(999999).toString().padLeft(6);
+    final quizRef = database!.ref("quiz");
+    final quizDetailRef = database!.ref("quiz_detail");
+    final quizStateRef = database!.ref("quiz_state");
+
+    final newQuizDetailRef = quizDetailRef.push();
+    newQuizDetailRef.set({
+      "code": pinCode,
+      "problems": quizItems
+          .map(
+            (e) => {
+              "title": e.title,
+              "options": e.problems
+                  ?.map((e2) => e2.textEditingController.text)
+                  .toList(),
+              "answerIndex": e.answer?.index,
+              "answer": e.answer?.textEditingController.text
+            },
+          )
+          .toList(),
+    });
+
+    await quizStateRef.child("${newQuizDetailRef.key}").set({
+      "quizDetailRef": newQuizDetailRef.key,
+      "user": [],
+      "state": false,
+      "score": [],
+      "solve": [{}],
+    });
+
+    final newQuizRef = quizRef.push();
+    await newQuizRef.set({
+      "code": pinCode,
+      "uid": uid,
+      "generateTime": DateTime.now().toString(),
+      "timestamp": DateTime.now().millisecondsSinceEpoch,
+      "quizDetailRef": newQuizDetailRef.key,
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    signInAnonymously();
   }
 
   @override
@@ -68,7 +126,7 @@ class _QuizManagerPageState extends State<QuizManagerPage> {
                         height: 72,
                         color: Colors.indigo,
                         onPressed: () {
-                          //
+                          generateQuiz();
                         },
                         child: const Text(
                           '제출 및 핀코드 생성',
@@ -89,7 +147,14 @@ class _QuizManagerPageState extends State<QuizManagerPage> {
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () async {
-          //
+          final quiz = await showModalBottomSheet(
+            context: context,
+            builder: (context) => const QuizBottomSheetWidget(),
+          );
+
+          setState(() {
+            quizItems.add(quiz);
+          });
         },
       ),
     );
