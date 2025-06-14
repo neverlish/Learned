fcb046d43c0d842aceb18a88c0f4180846fdc702
Jinho Hyeon <neverlish@gmail.com>
2025-06-15 01:09:02 +0900

    5 - 2 - 24 (Mobile) 실시간 퀴즈 앱 구현 - 퀴즈 풀이 화면 2

--- flutter/fastcampus-flutter-15projects/5/fastcampus_realtime_quiz_app/lib/quiz_app/quiz_app.dart
+++ flutter/fastcampus-flutter-15projects/5/fastcampus_realtime_quiz_app/lib/quiz_app/quiz_app.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:fastcampus_realtime_quiz_app/main.dart';
@@ -175,7 +176,16 @@ class _QuizPageState extends State<QuizPage> {
                     if (currentIndex < problemsSets.length) {
                       return Container(
                         color: Colors.white,
-                        child: Container(),
+                        child: _ProblemSolveWidget(
+                          index: currentIndex,
+                          ref: quizStateRef!,
+                          problems: problemsSets[currentIndex],
+                          startTime:
+                              problemTriggers[currentIndex]["start"] ?? 0,
+                          endTime: problemTriggers[currentIndex]["end"] ?? 0,
+                          uid: widget.uid,
+                          name: widget.name,
+                        ),
                       );
                     } else {}
                   }
@@ -191,5 +201,170 @@ class _QuizPageState extends State<QuizPage> {
         ],
       ),
     );
+  }
+}
+
+class _ProblemSolveWidget extends StatefulWidget {
+  final DatabaseReference ref;
+  final Problems problems;
+  final String uid;
+  final String name;
+  final int startTime;
+  final int endTime;
+  final int index;
+
+  const _ProblemSolveWidget({
+    super.key,
+    required this.index,
+    required this.ref,
+    required this.problems,
+    required this.startTime,
+    required this.endTime,
+    required this.name,
+    required this.uid,
+  });
+
+  @override
+  State<_ProblemSolveWidget> createState() => _ProblemSolveWidgetState();
+}
+
+class _ProblemSolveWidgetState extends State<_ProblemSolveWidget> {
+  Timer? timer;
+
+  int leftTime = 0;
+  int readyTime = 0;
+
+  bool isStart = false;
+  bool isSubmit = false;
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  refresh() {
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future periodicTask() async {
+    final startTime = DateTime.fromMillisecondsSinceEpoch(widget.startTime);
+    final endTime = DateTime.fromMillisecondsSinceEpoch(widget.endTime);
+
+    timer ??= Timer.periodic(const Duration(seconds: 1), (t) {
+      DateTime nowDatetime = DateTime.now();
+      final sDiff = nowDatetime.difference(startTime);
+      final eDiff = endTime.difference(nowDatetime);
+
+      readyTime = sDiff.inSeconds;
+      leftTime = eDiff.inSeconds;
+
+      if (sDiff.inSeconds >= 0) {
+        isStart = true;
+      }
+
+      if (eDiff.inSeconds <= 0) {
+        int nextIndex = widget.index + 1;
+        widget.ref.child("current").set(nextIndex);
+        timer?.cancel();
+        timer = null;
+        isStart = false;
+        isSubmit = false;
+      }
+      refresh();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    periodicTask();
+    return switch (isStart) {
+      true => Column(
+          children: [
+            Text(
+              "문제",
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            Text(
+              "${widget.problems.title}",
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            Expanded(
+              child: isSubmit
+                  ? Text(
+                      "제출 완료",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    )
+                  : GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                      ),
+                      itemCount: widget.problems.options?.length,
+                      itemBuilder: (context, index) {
+                        final item = widget.problems.options?[index];
+                        return GestureDetector(
+                          onTap: () async {
+                           
+                          },
+                          child: Card(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "${index + 1} 번",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 24,
+                                  ),
+                                ),
+                                Text(
+                                  "$item",
+                                  style: const TextStyle(fontSize: 24),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+            ),
+            Text(
+              "$leftTime초",
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 42,
+              ),
+            )
+          ],
+        ),
+      false => Container(
+          child: widget.index > 0
+              ? Column(
+                  children: [
+                    const Text("곧 다음 퀴즈가 시작됩니다."),
+                    Expanded(
+                        child: Container()),
+                    Text(
+                      "${readyTime * -1}",
+                      style: Theme.of(context).textTheme.displayLarge,
+                    )
+                  ],
+                )
+              : Column(
+                  children: [
+                    const Text("대기중\n곧 퀴즈가 시작됩니다. "),
+                    Expanded(child: Container()),
+                    Text(
+                      "${readyTime * -1}",
+                      style: Theme.of(context).textTheme.displayLarge,
+                    )
+                  ],
+                ),
+        ),
+    };
   }
 }
