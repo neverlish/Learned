0dbcf0cb4007da58c44f4928ee4b86ef5c130413
Jinho Hyeon <neverlish@gmail.com>
2025-06-15 15:50:08 +0900

    5 - 5 ML 통합 앱 - 바코드 스캔 (Barcode Scanner) - 3 ML 통합 앱 - 바코드 스캔 (Barcode Scanner)

---
+++ flutter/fastcampus-flutter-15projects/5/mlkit_barcode_scanner_starter/android/app/src/main/kotlin/com/example/mlkit_barcode_scanner_starter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mlkit_barcode_scanner_starter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter/fastcampus-flutter-15projects/5/mlkit_barcode_scanner_starter/lib/barcode_painter.dart
@@ -0,0 +1,28 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
+
+class BarcodeDetectorPainter extends CustomPainter {
+  BarcodeDetectorPainter(
+    this.barcodes,
+    this.imageSize,
+    this.rotation,
+    this.cameraLensDirection,
+  );
+
+  final List<Barcode> barcodes;
+  final Size imageSize;
+  final InputImageRotation rotation;
+  final CameraLensDirection cameraLensDirection;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    
+  }
+
+  @override
+  bool shouldRepaint(BarcodeDetectorPainter oldDelegate) {
+    return oldDelegate.imageSize != imageSize ||
+        oldDelegate.barcodes != barcodes;
+  }
+}
