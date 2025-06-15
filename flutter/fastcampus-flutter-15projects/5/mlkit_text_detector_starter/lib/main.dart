03afd6cc8cc4c51db12db504f5b2fe9f28645cf1
Jinho Hyeon <neverlish@gmail.com>
2025-06-15 16:56:11 +0900

    5 - 6 ML 통합 앱 - 텍스트 인식 (Text recognition) - 3 텍스트 및 영수증 인식 화면 구현

---
+++ flutter/fastcampus-flutter-15projects/5/mlkit_text_detector_starter/lib/main.dart
@@ -0,0 +1,166 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? _image;
+  bool _canProcess = true;
+  bool _isBusy = false;
+  String _text = "";
+  CustomPaint? _customPaint;
+  final _cameraLensDirection = CameraLensDirection.back;
+
+  TextRecognitionScript _script = TextRecognitionScript.korean;
+  TextRecognizer textRecognizer =
+      TextRecognizer(script: TextRecognitionScript.korean);
+  final ImagePicker _imagePicker = ImagePicker();
+
+  @override
+  void dispose() {
+    _canProcess = false;
+    textRecognizer.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          DropdownButton(
+            items: TextRecognitionScript.values
+                .map(
+                  (e) => DropdownMenuItem(value: e, child: Text(e.name)),
+                )
+                .toList(),
+            value: _script,
+            onChanged: (script) {
+              setState(() {
+                if (script != null) {
+                  _script = script;
+                  textRecognizer.close();
+                  textRecognizer = TextRecognizer(script: _script);
+                }
+              });
+            },
+          ),
+          _image != null
+              ? SizedBox(
+                  height: 320,
+                  width: 320,
+                  child: Image.file(
+                    _image!,
+                  ))
+              : Container(
+                  height: 240,
+                  width: 240,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                  ),
+                ),
+          ElevatedButton(
+            onPressed: () => _getImage(ImageSource.gallery),
+            child: Text('갤러리 이미지 가져오기'),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Text(_text),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future _getImage(ImageSource source) async {
+    setState(() {
+      _image = null;
+    });
+
+    final pickedFile = await _imagePicker.pickImage(source: source);
+    if (pickedFile != null) {
+      _processFile(pickedFile.path);
+    }
+  }
+
+  Future _processFile(String path) async {
+    setState(() {
+      _image = File(path);
+    });
+
+    final inputImage = InputImage.fromFilePath(path);
+    _processImage(inputImage);
+  }
+
+  Future<void> _processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+
+    final recognizedText = await textRecognizer.processImage(inputImage);
+
+    if (inputImage.metadata?.size != null &&
+        inputImage.metadata?.rotation != null) {
+    } else {
+      _text = "recognized Text:  \n\n";
+      for (var block in recognizedText.blocks) {
+        _text += "Block: ${block.text} \n";
+        for (var line in block.lines) {
+          _text += "Line: ${line.text} \n\n";
+          for (var element in line.elements) {
+            _text += "Element: ${element.text} \n\n";
+          }
+        }
+        _text += "\n\n";
+      }
+      setState(() {});
+      _customPaint = null;
+    }
+
+    _isBusy = false;
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+}
---
+++ flutter/fastcampus-flutter-15projects/5/mlkit_text_detector_starter/lib/text_detector_painter.dart
@@ -0,0 +1,27 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+
+class TextRecognizerPainter extends CustomPainter {
+  TextRecognizerPainter(
+    this.recognizedText,
+    this.imageSize,
+    this.rotation,
+    this.cameraLensDirection,
+  );
+
+  final RecognizedText recognizedText;
+  final Size imageSize;
+  final InputImageRotation rotation;
+  final CameraLensDirection cameraLensDirection;
+
+  @override
+  void paint(Canvas canvas, Size canvasSize) {
+    
+  }
+
+  @override
+  bool shouldRepaint(TextRecognizerPainter oldDelegate) {
+    return oldDelegate.recognizedText != recognizedText;
+  }
+}
