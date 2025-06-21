539973962dd0321d40f65ac2847a2be215ef74a0
Jinho Hyeon <neverlish@gmail.com>
2025-06-21 20:13:08 +0900

    6 - 11 - 14 QA 시나리오 대응하기 - PrimaryScrollController

--- flutter/fastcampus-flutter-15projects/6/e_commerce_app/lib/presentation/pages/home/component/view_module_list/view_module_list.dart
+++ flutter/fastcampus-flutter-15projects/6/e_commerce_app/lib/presentation/pages/home/component/view_module_list/view_module_list.dart
@@ -16,55 +16,38 @@ class ViewModuleList extends StatefulWidget {
 }
 
 class _ViewModuleListState extends State<ViewModuleList> {
-  final ScrollController scrollController = ScrollController();
-
-  @override
-  void initState() {
-    super.initState();
-    scrollController.addListener(_onScroll);
-  }
-
-  @override
-  void dispose() {
-    scrollController.dispose();
-    super.dispose();
-  }
-
-  void _onScroll() {
-    if (_isEnd) {
-      context.read<ViewModuleBloc>().add(ViewModuleFetched());
-    }
-  }
-
-  bool get _isEnd {
-    if (!scrollController.hasClients) return false;
-
-    final maxScroll = scrollController.position.maxScrollExtent;
-    final curScroll = scrollController.offset;
-
-    return curScroll >= maxScroll * 0.9;
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
     return RefreshIndicator(
-      child: BlocBuilder<ViewModuleBloc, ViewModuleState>(
-        builder: (_, state) {
-          return (state.status.isInitial || state.viewModules.isEmpty)
-              ? HomePlaceholder()
-              : ListView.builder(
-                  controller: scrollController,
-                  itemBuilder: (_, index) {
-                    return Column(
-                      children: [
-                        ...state.viewModules,
-                        if (state.status.isLoading) LoadingWidget(),
-                        Footer(),
-                      ],
-                    );
-                  },
-                  itemCount: state.viewModules.length,
-                );
+      child: NotificationListener(
+        child: BlocBuilder<ViewModuleBloc, ViewModuleState>(
+          builder: (_, state) {
+            return (state.status.isInitial || state.viewModules.isEmpty)
+                ? HomePlaceholder()
+                : ListView.builder(
+                    itemBuilder: (_, index) {
+                      return Column(
+                        children: [
+                          ...state.viewModules,
+                          if (state.status.isLoading) LoadingWidget(),
+                          Footer(),
+                        ],
+                      );
+                    },
+                    itemCount: state.viewModules.length,
+                  );
+          },
+        ),
+        onNotification: (ScrollNotification notification) {
+          final maxScroll = notification.metrics.maxScrollExtent;
+          final curScroll = notification.metrics.pixels;
+
+          if (curScroll >= maxScroll * 0.9) {
+            context.read<ViewModuleBloc>().add(ViewModuleFetched());
+          }
+          return false;
         },
       ),
       onRefresh: () async => context.read<ViewModuleBloc>().add(
