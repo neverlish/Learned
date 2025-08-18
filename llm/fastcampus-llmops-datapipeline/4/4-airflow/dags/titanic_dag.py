a79f9b2c1231d2cbe8a55c0a2135d0fc897824ed
Jinho Hyeon <neverlish@gmail.com>
2025-08-18 15:04:23 +0900

    4 - 4 - 7 워크플로우 작성 실습 심화 - Airflow + MySQL 연동 (2)

--- llm/fastcampus-llmops-datapipeline/4/4-airflow/dags/titanic_dag.py
+++ llm/fastcampus-llmops-datapipeline/4/4-airflow/dags/titanic_dag.py
@@ -1,4 +1,4 @@
-import pendulum
+from datetime import datetime
 
 from airflow.decorators import dag, task
 from airflow.operators.empty import EmptyOperator
@@ -8,7 +8,7 @@ from airflow.providers.mysql.hooks.mysql import MySqlHook
 @dag(
     dag_id="titanic_dag",
     schedule=None,
-    start_date=pendulum.datetime(2025, 2, 8),
+    start_date=datetime(2025, 2, 8),
     catchup=False,
     tags=["example"],
 )
@@ -40,10 +40,37 @@ def titanic_dag():
     def get_row_count():
         mysql_hook = MySqlHook(mysql_conn_id='mysql_conn')
         cur = mysql_hook.get_cursor()
-        print(f"row_count: {cur.rowcount}")
         return cur.rowcount
 
-    start >> create_table() >> get_row_count()
+    @task
+    def insert_csv_into_table(row_count=None):
+        if row_count > 0:
+            return
+
+        mysql_hook = MySqlHook(mysql_conn_id="mysql_conn")
+        sql = """
+        LOAD DATA INFILE '/var/lib/mysql-files/titanic_data.csv' 
+        INTO TABLE titanic_data 
+        FIELDS TERMINATED BY ',' 
+        OPTIONALLY ENCLOSED BY '\"' 
+        LINES TERMINATED BY '\n' 
+        IGNORE 1 ROWS;
+        """
+        mysql_hook.run(sql)
+
+    @task
+    def get_stats():
+        mysql_hook = MySqlHook(mysql_conn_id="mysql_conn")
+        sql = """
+        SELECT survived, sex, avg(age) 
+        FROM titanic_data 
+        GROUP BY survived, sex;
+        """
+        mysql_hook.run(sql)
+
+    row_count = get_row_count()
+
+    (start >> create_table() >> insert_csv_into_table(row_count) >> get_stats())
 
 
 titanic_dag()
