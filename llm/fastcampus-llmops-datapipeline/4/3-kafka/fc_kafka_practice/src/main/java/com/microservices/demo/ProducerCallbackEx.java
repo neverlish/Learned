a5dfe75a5820929bbfb18b977c10719935fb1e9c
Jinho Hyeon <neverlish@gmail.com>
2025-08-18 10:49:43 +0900

    4 - 3 - 14 Producer callback - Java 실습

---
+++ llm/fastcampus-llmops-datapipeline/4/3-kafka/fc_kafka_practice/src/main/java/com/microservices/demo/ProducerCallbackEx.java
@@ -0,0 +1,81 @@
+package com.microservices.demo;
+
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerCallbackEx {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerCallbackEx.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer!");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+
+        // connect to Localhost
+        properties.setProperty("bootstrap.servers", "127.0.0.1:29092");
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        properties.setProperty("batch.size", "400");
+
+//        properties.setProperty("partitioner.class", RoundRobinPartitioner.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+
+        for (int j=0; j<10; j++){
+
+            for (int i=0; i<30; i++){
+
+                // create a Producer Record
+                ProducerRecord<String, String> producerRecord =
+                        new ProducerRecord<>("demo_java", "hello world " + i);
+
+                // send data
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception e) {
+                        // executes every time a record successfully sent or an exception is thrown
+                        if (e == null) {
+                            // the record was successfully sent
+                            log.info("Received new metadata \n" +
+                                    "Topic: " + metadata.topic() + "\n" +
+                                    "Partition: " + metadata.partition() + "\n" +
+                                    "Offset: " + metadata.offset() + "\n" +
+                                    "Timestamp: " + metadata.timestamp());
+                        } else {
+                            log.error("Error while producing", e);
+                        }
+                    }
+                });
+            }
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        // tell the producer to send all data and block until done -- synchronous
+        producer.flush();
+
+        // flush and close the producer
+        producer.close();
+    }
+}
