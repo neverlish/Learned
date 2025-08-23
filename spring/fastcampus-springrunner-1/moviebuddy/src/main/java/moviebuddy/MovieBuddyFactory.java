cea461bec719982aa5f91dd3aeef12b63d9c8aab
Jinho Hyeon <neverlish@gmail.com>
2025-08-23 20:17:04 +0900

    7 - 2 객체 타입으로 자동 의존관계 주입하기

--- spring/fastcampus-springrunner-1/moviebuddy/src/main/java/moviebuddy/MovieBuddyFactory.java
+++ spring/fastcampus-springrunner-1/moviebuddy/src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -1,8 +1,5 @@
 package moviebuddy;
 
-import moviebuddy.domain.MovieFinder;
-import moviebuddy.domain.MovieReader;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -13,10 +10,7 @@ import org.springframework.context.annotation.Import;
 public class MovieBuddyFactory {
     @Configuration
     static class DomainModuleConfig {
-        @Bean
-        public MovieFinder movieFinder(MovieReader movieReader) {
-            return new MovieFinder(movieReader);
-        }
+
     }
 
     @Configuration
--- spring/fastcampus-springrunner-1/moviebuddy/src/main/java/moviebuddy/domain/MovieFinder.java
+++ spring/fastcampus-springrunner-1/moviebuddy/src/main/java/moviebuddy/domain/MovieFinder.java
@@ -1,12 +1,17 @@
 package moviebuddy.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@Service
 public class MovieFinder {
     private MovieReader movieReader;
 
+//    @Autowired
     public MovieFinder(MovieReader movieReader) {
         this.movieReader = Objects.requireNonNull(movieReader);
     }
