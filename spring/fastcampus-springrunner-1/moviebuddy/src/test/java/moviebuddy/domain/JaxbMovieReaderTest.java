fa74eecc7d82e76f641ae08d770fedac1b6593b3
Jinho Hyeon <neverlish@gmail.com>
2025-08-23 20:51:15 +0900

    8 - 2 영화 검색 컴포넌트 테스트를 스프링 통합 테스트로 변경하기

--- spring/fastcampus-springrunner-1/moviebuddy/src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
+++ spring/fastcampus-springrunner-1/moviebuddy/src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -2,15 +2,19 @@ package moviebuddy.domain;
 
 import java.util.List;
 
+import moviebuddy.MovieBuddyFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+@SpringJUnitConfig(MovieBuddyFactory.class)
 public class JaxbMovieReaderTest {
+    @Autowired
+    JaxbMovieReader movieReader;
 
     @Test
     void NotEmpty_LoadedMovies() {
-        JaxbMovieReader movieReader = new JaxbMovieReader();
-
         List<Movie> movies = movieReader.loadMovies();
         Assertions.assertEquals(1375, movies.size());
     }
--- spring/fastcampus-springrunner-1/moviebuddy/src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ spring/fastcampus-springrunner-1/moviebuddy/src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -3,18 +3,33 @@ package moviebuddy.domain;
 import moviebuddy.MovieBuddyFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 
 /**
  * @author springrunner.kr@gmail.com
  */
+@SpringJUnitConfig(MovieBuddyFactory.class)
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = MovieBuddyFactory.class)
 public class MovieFinderTest {
 
-	final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
-	final MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
+//	MovieFinder movieFinder;
+
+//	@Autowired
+//	public MovieFinderTest(MovieFinder movieFinder) {
+//		this.movieFinder = movieFinder;
+//	}
+
+//	@Autowired
+//	void setMovieFinder(MovieFinder movieFinder) {
+//		this.movieFinder = movieFinder;
+//	}
+
+	@Autowired
+	MovieFinder movieFinder;
 
 
 	@Test
