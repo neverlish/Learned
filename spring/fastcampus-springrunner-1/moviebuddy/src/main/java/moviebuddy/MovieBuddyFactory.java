fb8a23439cb9454e8be9a2d093928cfdeedb951a
Jinho Hyeon <neverlish@gmail.com>
2025-08-23 20:12:18 +0900

    6 - 3 자바 기반 빈 구성정보 조합하기

--- spring/fastcampus-springrunner-1/moviebuddy/src/main/java/moviebuddy/MovieBuddyFactory.java
+++ spring/fastcampus-springrunner-1/moviebuddy/src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -5,18 +5,25 @@ import moviebuddy.domain.MovieFinder;
 import moviebuddy.domain.MovieReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({ MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class })
+//@ImportResource("xml file")
 public class MovieBuddyFactory {
-
-    @Bean
-    public MovieReader movieReader() {
-        return new CsvMovieReader();
+    @Configuration
+    static class DomainModuleConfig {
+        @Bean
+        public MovieFinder movieFinder(MovieReader movieReader) {
+            return new MovieFinder(movieReader);
+        }
     }
 
-    @Bean
-//    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public MovieFinder movieFinder(MovieReader movieReader) {
-        return new MovieFinder(movieReader);
+    @Configuration
+    static class DataSourceModuleConfig {
+        @Bean
+        public MovieReader movieReader() {
+            return new CsvMovieReader();
+        }
     }
 }
