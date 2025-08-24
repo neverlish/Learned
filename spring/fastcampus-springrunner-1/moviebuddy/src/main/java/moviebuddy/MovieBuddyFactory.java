f8a8b980910122c2d267aede3775b2907e2fa438
Jinho Hyeon <neverlish@gmail.com>
2025-08-24 12:58:43 +0900

    17 - 3 스프링 AOP 모듈을 사용해 프락시 다루기

--- spring/fastcampus-springrunner-1/moviebuddy/src/main/java/moviebuddy/MovieBuddyFactory.java
+++ spring/fastcampus-springrunner-1/moviebuddy/src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -2,12 +2,15 @@ package moviebuddy;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import moviebuddy.cache.CachingAdvice;
 import moviebuddy.data.CachingMovieReader;
 import moviebuddy.data.CsvMovieReader;
 import moviebuddy.domain.Movie;
 import moviebuddy.domain.MovieReader;
+import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
@@ -43,8 +46,16 @@ public class MovieBuddyFactory {
     static class DataSourceModuleConfig {
         @Primary
         @Bean
-        public MovieReader cachingMovieReader(CacheManager cacheManager, MovieReader target) {
-            return new CachingMovieReader(cacheManager, target);
+        public ProxyFactoryBean cachingMovieReader(ApplicationContext applicationContext) {
+            MovieReader target = applicationContext.getBean(MovieReader.class);
+            CacheManager cacheManager = applicationContext.getBean(CacheManager.class);
+
+            ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+            proxyFactoryBean.setTarget(target);
+//            proxyFactoryBean.setProxyTargetClass(true);
+            proxyFactoryBean.addAdvice(new CachingAdvice(cacheManager));
+
+            return proxyFactoryBean;
         }
     }
 }
