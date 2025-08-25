d714f846cd1bdaac1807bc34f7efc1111a5c7cf5
Jinho Hyeon <neverlish@gmail.com>
2025-08-25 15:48:06 +0900

    2 - 4 더 간결하게 모델과 뷰를 다루는 방법들

--- spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/web/TodoController.java
+++ spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/web/TodoController.java
@@ -1,36 +1,27 @@
 package todoapp.web;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import todoapp.web.model.SiteProperties;
 
 @Controller
 public class TodoController {
+    private final SiteProperties siteProperties;
 
-    private Environment environment;
-    private String siteAuthor;
-    private SiteProperties siteProperties;
-
-    public TodoController(Environment environment, @Value("${site.author}") String siteAuthor, SiteProperties siteProperties) {
-        this.environment = environment;
-        this.siteAuthor = siteAuthor;
+    public TodoController(SiteProperties siteProperties) {
         this.siteProperties = siteProperties;
     }
 
+    @ModelAttribute("site")
+    public SiteProperties siteProperties() {
+        return siteProperties;
+    }
+
     @RequestMapping("/todos")
-    public ModelAndView todos() throws Exception {
-//        SiteProperties site = new SiteProperties(
-////                environment.getProperty("site.author"),
-//                siteAuthor,
-//                "스프링 MVC 할 일 관리 앱");
-
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("site", siteProperties);
-        mav.setViewName("todos");
-
-        return mav;
+    public void todos() throws Exception {
+        
     }
 }
