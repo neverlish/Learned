bfc8afedfa188733dec1964a0f25fe3457d6ba2b
Jinho Hyeon <neverlish@gmail.com>
2025-08-26 20:34:06 +0900

    3 - 4 범용적인 뷰 연결하기

--- spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/web/WebMvcConfiguration.java
+++ spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/web/WebMvcConfiguration.java
@@ -2,19 +2,23 @@ package todoapp.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.ObjectToStringHttpMessageConverter;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 import todoapp.commons.web.view.CommaSeparatedValuesView;
 import todoapp.core.todo.domain.Todo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -43,7 +47,8 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
   @Override
   public void configureViewResolvers(ViewResolverRegistry registry) {
     // registry.enableContentNegotiation();
-    registry.viewResolver(new TodoController.TodoCsvViewResolver());
+//    registry.viewResolver(new TodoController.TodoCsvViewResolver());
+//    registry.enableContentNegotiation(new CommaSeparatedValuesView());
     // 위와 같이 직접 설정하면, 스프링부 트가 구성한 ContentNegotiatingViewResolver 전략이 무시된다.
   }
 
@@ -59,18 +64,24 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     converters.add(new ObjectToStringHttpMessageConverter(conversionService));
   }
 
-  @Bean(name = "todos")
-  public CommaSeparatedValuesView todoCsvView() {
-    return new CommaSeparatedValuesView();
-  }
+//  @Bean(name = "todos")
+//  public CommaSeparatedValuesView todoCsvView() {
+//    return new CommaSeparatedValuesView();
+//  }
 
   /**
    * 스프링부트가 생성한 ContentNegotiatingViewResolver를 조작할 목적으로 작성된 설정 정보이다.
    */
+  @Configuration
   public static class ContentNegotiationCustomizer {
 
+    @Autowired
     public void configurer(ContentNegotiatingViewResolver viewResolver) {
-      // TODO ContentNegotiatingViewResolver 조작하기
+      List<View> defaultViews = new ArrayList<>(viewResolver.getDefaultViews());
+      defaultViews.add(new CommaSeparatedValuesView());
+      defaultViews.add(new MappingJackson2JsonView());
+
+      viewResolver.setDefaultViews(defaultViews);
     }
 
   }
