c146706ba9a5e8f176efe2c049d39de682e28329
Jinho Hyeon <neverlish@gmail.com>
2025-08-26 10:54:12 +0900

    2 - 8 할 일 등록 API 제공하기 (하)

--- spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/web/TodoRestController.java
+++ spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/web/TodoRestController.java
@@ -1,20 +1,26 @@
 package todoapp.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import todoapp.core.todo.application.TodoFind;
+import todoapp.core.todo.application.TodoRegistry;
 import todoapp.core.todo.domain.Todo;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 //@Controller
 public class TodoRestController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private final TodoFind finder;
+    private final TodoRegistry editor;
 
-    public TodoRestController(TodoFind finder) {
+    public TodoRestController(TodoFind finder, TodoRegistry editor) {
         this.finder = finder;
+        this.editor = editor;
     }
 
 //    @RequestMapping(path = "/api/todos", method = RequestMethod.GET)
@@ -26,7 +32,28 @@ public class TodoRestController {
 
 //    @RequestMapping(path = "/api/todos", method = RequestMethod.POST)
     @PostMapping("/api/todos")
-    public void create() {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody CreateTodoCommand command) {
+        logger.debug("request command : {}", command);
 
+        editor.register(command.getTitle());
+    }
+
+    static class CreateTodoCommand {
+        private String title;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("CreateTodoCommand [title=%s]", title);
+
+        }
     }
 }
