1649820765f0cb6b429f51c90ac875d1eb141408
Jinho Hyeon <neverlish@gmail.com>
2025-08-26 10:38:16 +0900

    2 - 6 할 일 목록 조회 API 제공하기 (하)

--- spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/data/TodosDataInitializer.java
+++ spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/data/TodosDataInitializer.java
@@ -1,12 +1,17 @@
 package todoapp.data;
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import todoapp.core.todo.domain.Todo;
 import todoapp.core.todo.domain.TodoRepository;
 
 @Component
-public class TodosDataInitializer implements InitializingBean {
+@ConditionalOnProperty(name = "todos.data.initialize", havingValue = "true")
+public class TodosDataInitializer implements InitializingBean, ApplicationRunner, CommandLineRunner {
 
     private final TodoRepository todoRepository;
 
@@ -17,5 +22,15 @@ public class TodosDataInitializer implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         todoRepository.save(Todo.create("Task One"));
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        todoRepository.save(Todo.create("Task two"));
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        todoRepository.save(Todo.create("Task three"));
     }
 }
