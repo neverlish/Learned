664393ec3410eb3adf3a2a3bf1ebf0881fa2cf4b
Jinho Hyeon <neverlish@gmail.com>
2025-08-26 20:20:57 +0900

    3 - 2 CSV 파일 다운로드 뷰 구현하기

--- spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/web/TodoController.java
+++ spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/web/TodoController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,17 +13,23 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.AbstractView;
+import todoapp.core.todo.application.TodoFind;
+import todoapp.core.todo.domain.Todo;
 import todoapp.web.model.SiteProperties;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 @Controller
 public class TodoController {
     private final SiteProperties siteProperties;
+    private final TodoFind finder;
 
-    public TodoController(SiteProperties siteProperties) {
+    public TodoController(SiteProperties siteProperties, TodoFind finder) {
         this.siteProperties = siteProperties;
+        this.finder = finder;
     }
 
     @ModelAttribute("site")
@@ -36,8 +43,8 @@ public class TodoController {
     }
 
     @RequestMapping(path = "/todos", produces = "text/csv")
-    public void downloadTodos() {
-
+    public void downloadTodos(Model model) {
+        model.addAttribute("todos", finder.all());
     }
 
     public static class TodoCsvViewResolver implements ViewResolver {
@@ -57,8 +64,23 @@ public class TodoController {
         }
 
         @Override
+        protected boolean generatesDownloadContent() {
+            return true;
+        }
+
+        @Override
         protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
             logger.info("render model as csv content");
+            response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"todos.csv\"");
+            response.getWriter().println("id,title,completed");
+
+            List<Todo> todos = (List<Todo>) model.getOrDefault("todos", Collections.emptyList());
+
+            for (Todo todo: todos) {
+                String line = String.format("%d,%s,%s", todo.getId(), todo.getTitle(), todo.isCompleted());
+                response.getWriter().println(line);
+            }
+            response.flushBuffer();
         }
     }
 }
