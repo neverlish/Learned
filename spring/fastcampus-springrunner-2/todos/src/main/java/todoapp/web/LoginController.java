c2f86fdabd77967bc278010d1e8005f0f064c6fa
Jinho Hyeon <neverlish@gmail.com>
2025-08-29 01:02:47 +0900

    6 - 3 사용자 로그인 로직 작성하기

--- spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/web/LoginController.java
+++ spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/web/LoginController.java
@@ -7,10 +7,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import todoapp.core.user.application.UserPasswordVerifier;
+import todoapp.core.user.application.UserRegistration;
+import todoapp.core.user.domain.UserEntityNotFoundException;
 
 @Controller
 public class LoginController {
+    private final UserPasswordVerifier userPasswordVerifier;
+    private final UserRegistration userRegistration;
     private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public LoginController(UserPasswordVerifier userPasswordVerifier, UserRegistration userRegistration) {
+        this.userPasswordVerifier = userPasswordVerifier;
+        this.userRegistration = userRegistration;
+    }
 
     @GetMapping("login")
     public void loginForm() {
@@ -18,7 +28,7 @@ public class LoginController {
     }
 
     @PostMapping("login")
-    public void loginProcess(
+    public String loginProcess(
 //        @RequestParam("username") String username,
 //        @RequestParam String username,
 //        String username,
@@ -27,6 +37,14 @@ public class LoginController {
     ) {
 
         logger.debug("login command: {}", command);
+
+        try {
+            userPasswordVerifier.verify(command.getUsername(), command.getPassword());
+        } catch (UserEntityNotFoundException error) {
+            userRegistration.join(command.getUsername(), command.password);
+        }
+
+        return "redirect:/todos";
     }
 
     static class LoginCommand {
