6f2b0ff389ee1270363aee488d24d5be96d44902
Jinho Hyeon <neverlish@gmail.com>
2025-08-30 01:42:14 +0900

    9 - 2 스프링 테스트 컨텍스트 프레임워크 적용하기

--- spring/fastcampus-springrunner-2/todos/src/test/java/todoapp/web/LoginControllerTests.java
+++ spring/fastcampus-springrunner-2/todos/src/test/java/todoapp/web/LoginControllerTests.java
@@ -1,35 +1,27 @@
 package todoapp.web;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.context.web.WebAppConfiguration;
 import todoapp.TodosApplication;
 import todoapp.core.user.domain.User;
 import todoapp.security.UserSession;
 import todoapp.security.UserSessionRepository;
 
-import java.util.Objects;
-
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = TodosApplication.class)
+//@WebAppConfiguration
+//@SpringJUnitConfig(TodosApplication.class)
+@SpringJUnitWebConfig(TodosApplication.class)
 public class LoginControllerTests {
-    private AnnotationConfigServletWebServerApplicationContext applicationContext;
+    @Autowired ApplicationContext applicationContext;
+    @Autowired UserSessionRepository userSessionRepository;
+    @Autowired LoginController controller;
 
-    @BeforeEach
-    void setUp() throws Exception {
-        applicationContext = new AnnotationConfigServletWebServerApplicationContext(TodosApplication.class);
-    }
-
-    @AfterEach
-    void tearDown() throws Exception {
-        if (Objects.nonNull(applicationContext)) {
-            applicationContext.close();
-        }
-    }
     @Test
     void 스프링컨테이너는_자동클래스탐지로_로그인컨트롤러를_찾아_등록해요() {
         Assertions.assertTrue(applicationContext.containsBean("loginController"));
@@ -37,30 +29,13 @@ public class LoginControllerTests {
 
     @Test
     void 인증되지않은_사용자가_로그인화면에_접근하면_로그인화면을_보여준다() {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        MockHttpServletResponse response = new MockHttpServletResponse();
-
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request, response));
-
-        LoginController controller = applicationContext.getBean(LoginController.class);
         Assertions.assertEquals("login", controller.loginForm());
-
-        RequestContextHolder.resetRequestAttributes();
     }
 
     @Test
     void 인증된_사용자가_로그인화면에_접근하면_할일관리화면으로_전환시킨다() {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        MockHttpServletResponse response = new MockHttpServletResponse();
-
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request, response));
-
-        UserSessionRepository userSessionRepository = applicationContext.getBean(UserSessionRepository.class);
         userSessionRepository.set(new UserSession(new User("tester", "")));
 
-        LoginController controller = applicationContext.getBean(LoginController.class);
         Assertions.assertEquals("redirect:/todos", controller.loginForm());
-
-        RequestContextHolder.resetRequestAttributes();
     }
 }
