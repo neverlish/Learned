a7463baeba643877ba1b87bbd6cf7529274e8f8e
Jinho Hyeon <neverlish@gmail.com>
2025-08-29 23:31:13 +0900

    6 - 17 로그인한 사용자만 프로필 조회 API 접근 허용하기

--- spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/web/UserRestController.java
+++ spring/fastcampus-springrunner-2/todos/src/main/java/todoapp/web/UserRestController.java
@@ -1,26 +1,16 @@
 package todoapp.web;
 
-import jakarta.servlet.http.HttpSession;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import todoapp.core.user.domain.User;
 import todoapp.security.UserSession;
-import todoapp.security.UserSessionRepository;
 import todoapp.web.model.UserProfile;
-
-import java.util.Objects;
 
 @RestController
 public class UserRestController {
     @GetMapping("/api/user/profile")
-    public ResponseEntity<UserProfile> userProfile(UserSession userSession) {
-        if (Objects.nonNull(userSession)) {
-            return ResponseEntity.ok(new UserProfile(userSession.getUser()));
-        }
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    @RolesAllowed("ROLE_USER")
+    public UserProfile userProfile(UserSession userSession) {
+        return new UserProfile(userSession.getUser());
     }
 }
