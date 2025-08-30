82cb29e1891d922792a89b08f89708ca6fa1a6c7
Jinho Hyeon <neverlish@gmail.com>
2025-08-30 17:12:31 +0900

    3 - 4 애플리케이션 구성파일 작성하기

--- spring/fastcampus-springrunner-3/divelog/src/main/java/com/fastcampus/springrunner/divelog/config/AppConfiguration.java
+++ spring/fastcampus-springrunner-3/divelog/src/main/java/com/fastcampus/springrunner/divelog/config/AppConfiguration.java
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({ MyServiceProperties.class })
+@EnableConfigurationProperties({ MyServiceProperties.class, SiteProperties.class })
 public class AppConfiguration {
 }
