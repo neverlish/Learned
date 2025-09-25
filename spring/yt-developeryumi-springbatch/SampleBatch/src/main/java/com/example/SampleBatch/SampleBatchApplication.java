043b2869f057a28c5713913201024f30ac0072b5
Jinho Hyeon <neverlish@gmail.com>
2025-09-26 01:30:34 +0900

    3 프로젝트 생성

---
+++ spring/yt-developeryumi-springbatch/SampleBatch/src/main/java/com/example/SampleBatch/SampleBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.SampleBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleBatchApplication.class, args);
+	}
+
+}
