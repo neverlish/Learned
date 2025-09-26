525e80d171228a13ea6bcaedbbeb08100eb37503
Jinho Hyeon <neverlish@gmail.com>
2025-09-26 17:24:07 +0900

    3 프로젝트 시작 의존성 추가

---
+++ spring/yt-developeryumi-resilience4j/SpringResilience/src/main/java/com/example/SpringResilience/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.example.SpringResilience.controller;
+
+import com.example.SpringResilience.component.Rest1Comp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MainController {
+
+    private Rest1Comp rest1Comp;
+
+    @Autowired
+    public MainController(Rest1Comp rest1Comp) {
+
+        this.rest1Comp = rest1Comp;
+    }
+
+    @GetMapping("/")
+    public String mainP() {
+
+        return rest1Comp.restTemplate1().getForObject("/data", String.class);
+    }
+}
---
+++ spring/yt-developeryumi-resilience4j/SpringResilience/src/test/java/com/example/SpringResilience/SpringResilienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringResilience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResilienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
