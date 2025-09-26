8034f80dc3b5c3ccb7d970491c52af5f0d9fdc23
Jinho Hyeon <neverlish@gmail.com>
2025-09-26 17:56:37 +0900

    8 bulk head 구현 실습

--- spring/yt-developeryumi-resilience4j/SpringResilience/src/main/java/com/example/SpringResilience/controller/MainController.java
+++ spring/yt-developeryumi-resilience4j/SpringResilience/src/main/java/com/example/SpringResilience/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.example.SpringResilience.controller;
 
 import com.example.SpringResilience.component.Rest1Comp;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +22,9 @@ public class MainController {
     }
 
 //    @CircuitBreaker(name = "MainControllerMethod1", fallbackMethod = "실패시수행할메소드이름")
-    @Retry(name = "MainControllerMethod1", fallbackMethod = "실패시수행할메소드이름")
+//    @Retry(name = "MainControllerMethod1", fallbackMethod = "실패시수행할메소드이름")
+//    @Bulkhead(name = "특정할이름", type = Bulkhead.Type.THREADPOOL, fallbackMethod = "실패시수행할메소드이름")
+    @Bulkhead(name = "MainControllerMethod1", type = Bulkhead.Type.SEMAPHORE, fallbackMethod = "실패시수행할메소드이름")
     @GetMapping("/")
     public String mainP() {
         return rest1Comp.restTemplate1().getForObject("/data", String.class);
