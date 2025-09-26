1d4d9d8c75751fafaf49bfba4ccd30c058ae06d7
Jinho Hyeon <neverlish@gmail.com>
2025-09-26 17:27:18 +0900

    4 호출용 API 서버

---
+++ spring/yt-developeryumi-resilience4j/APIServer/src/test/java/com/example/APIServer/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
