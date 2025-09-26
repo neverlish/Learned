1c8dbf03cc5a8ecb7b7e43c9d829163a9750c5a0
Jinho Hyeon <neverlish@gmail.com>
2025-09-26 21:17:07 +0900

    9 세션 사용자 아이디 정보

--- spring/yt-developeryumi-springsecurity/TestSecurity/src/main/java/com/example/TestSecurity/controller/MainController.java
+++ spring/yt-developeryumi-springsecurity/TestSecurity/src/main/java/com/example/TestSecurity/controller/MainController.java
@@ -1,13 +1,30 @@
 package com.example.TestSecurity.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String mainP() {
+    public String mainP(Model model) {
+        String id = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        model.addAttribute("id", id);
+        model.addAttribute("role", role);
 
         return "main";
     }
