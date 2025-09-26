119c20df5afcd4a8ca8098a754af660dd6d31c53
Jinho Hyeon <neverlish@gmail.com>
2025-09-26 21:35:26 +0900

    13 http basic 방식 로그인

--- spring/yt-developeryumi-springsecurity/TestSecurity/src/main/java/com/example/TestSecurity/config/SecurityConfig.java
+++ spring/yt-developeryumi-springsecurity/TestSecurity/src/main/java/com/example/TestSecurity/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.TestSecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -63,6 +64,9 @@ public class SecurityConfig {
         http
                 .logout((auth) -> auth.logoutUrl("/logout")
                         .logoutSuccessUrl("/"));
+
+        http
+                .httpBasic(Customizer.withDefaults());
 
 
 
