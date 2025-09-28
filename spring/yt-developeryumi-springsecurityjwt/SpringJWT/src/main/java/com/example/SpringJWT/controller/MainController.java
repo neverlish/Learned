aec9f9d415ad705bfc7b6ce679de4c6d030bf13d
Jinho Hyeon <neverlish@gmail.com>
2025-09-28 22:22:14 +0900

    12 세션 정보

--- spring/yt-developeryumi-springsecurityjwt/SpringJWT/src/main/java/com/example/SpringJWT/controller/MainController.java
+++ spring/yt-developeryumi-springsecurityjwt/SpringJWT/src/main/java/com/example/SpringJWT/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.example.SpringJWT.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
@@ -11,6 +17,15 @@ public class MainController {
     @GetMapping("/")
     public String mainP() {
 
-        return "main Controller";
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller : "+name + role;
     }
 }
+
