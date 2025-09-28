9093f8b3037ae77b47678bbb585a0e79b962ac22
Jinho Hyeon <neverlish@gmail.com>
2025-09-28 21:45:58 +0900

    8 DB기반 로그인 검증 로직

---
+++ spring/yt-developeryumi-springsecurityjwt/SpringJWT/src/main/java/com/example/SpringJWT/dto/CustomUserDetails.java
@@ -0,0 +1,72 @@
+package com.example.SpringJWT.dto;
+
+import com.example.SpringJWT.entity.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final UserEntity userEntity;
+
+    public CustomUserDetails(UserEntity userEntity) {
+
+        this.userEntity = userEntity;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+
+            @Override
+            public String getAuthority() {
+
+                return userEntity.getRole();
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+
+        return userEntity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+
+        return userEntity.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+
+        return true;
+    }
+}
--- spring/yt-developeryumi-springsecurityjwt/SpringJWT/src/main/java/com/example/SpringJWT/repository/UserRepository.java
+++ spring/yt-developeryumi-springsecurityjwt/SpringJWT/src/main/java/com/example/SpringJWT/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Boolean existsByUsername(String username);
+    UserEntity findByUsername(String username);
 }
