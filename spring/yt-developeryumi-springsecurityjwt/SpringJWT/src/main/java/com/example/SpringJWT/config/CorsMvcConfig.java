8edb07222c269284f20be1bafc4907d00da1f394
Jinho Hyeon <neverlish@gmail.com>
2025-09-28 22:25:14 +0900

    13 CORS 설정

---
+++ spring/yt-developeryumi-springsecurityjwt/SpringJWT/src/main/java/com/example/SpringJWT/config/CorsMvcConfig.java
@@ -0,0 +1,16 @@
+package com.example.SpringJWT.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
--- spring/yt-developeryumi-springsecurityjwt/SpringJWT/src/main/java/com/example/SpringJWT/config/SecurityConfig.java
+++ spring/yt-developeryumi-springsecurityjwt/SpringJWT/src/main/java/com/example/SpringJWT/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.SpringJWT.config;
 import com.example.SpringJWT.filter.JWTFilter;
 import com.example.SpringJWT.filter.LoginFilter;
 import com.example.SpringJWT.util.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,6 +14,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -41,6 +46,27 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })));
 
         http
                 .csrf((auth) -> auth.disable());
