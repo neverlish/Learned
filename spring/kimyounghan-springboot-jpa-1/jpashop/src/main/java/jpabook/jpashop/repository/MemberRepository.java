ee4efcaa6687cd56b8876f22886c2a2db6b74ad1
Jinho Hyeon <neverlish@gmail.com>
2025-10-04 23:48:23 +0900

    5 - 17 회원 서비스 개발

--- spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,15 +1,15 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
