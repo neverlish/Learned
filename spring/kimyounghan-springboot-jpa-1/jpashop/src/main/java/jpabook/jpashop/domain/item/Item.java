6eee1ed1dae2258c128e3446e2927b5e531a5c85
Jinho Hyeon <neverlish@gmail.com>
2025-10-05 00:15:46 +0900

    6 상품 도메인 개발 - 19 상품 엔티티 개발(비즈니스 로직 추가)

--- spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,4 +1,5 @@
 package jpabook.jpashop.domain.item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 import jpabook.jpashop.domain.Category;
@@ -23,4 +24,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
