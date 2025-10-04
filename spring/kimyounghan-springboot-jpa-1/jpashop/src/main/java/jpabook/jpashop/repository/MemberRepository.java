f2f89c48727ed6b5bb3336e84d6652f2d28e0e73
Jinho Hyeon <neverlish@gmail.com>
2025-10-04 23:23:27 +0900

    5 회원 도메인 개발 - 16 회원 리포지토리 개발

---
+++ spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
