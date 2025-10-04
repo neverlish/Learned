0ca4f2e31f34c06fe352f83156bc2e4ae777482d
Jinho Hyeon <neverlish@gmail.com>
2025-10-05 00:23:45 +0900

    7 주문 도메인 개발 - 22 주문, 주문상품 엔티티 개발

--- spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -42,4 +42,38 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /** 주문 취소 */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /** 전체 주문 가격 조회 */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
--- spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -21,4 +21,26 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    /** 주문 취소 */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /** 주문상품 전체 가격 조회 */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
