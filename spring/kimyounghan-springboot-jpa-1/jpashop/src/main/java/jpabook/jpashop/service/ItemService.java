1fdcf16af8fed20e8efe92c654a7be90d711463f
Jinho Hyeon <neverlish@gmail.com>
2025-10-05 18:12:38 +0900

    8 - 33 변경 감지와 병합(merge)

--- spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -16,6 +16,17 @@ public class ItemService {
     public void saveItem(Item item) {
         itemRepository.save(item);
     }
+    
+    /**
+     * 영속성 컨텍스트가 자동 변경 */
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity)
+    {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
 
     public List<Item> findItems() {
         return itemRepository.findAll();
