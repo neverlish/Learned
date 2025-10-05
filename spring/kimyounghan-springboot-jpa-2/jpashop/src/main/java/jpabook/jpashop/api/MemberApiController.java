eea6fb255657e013c8d228bdfc565272528dfca2
Jinho Hyeon <neverlish@gmail.com>
2025-10-05 18:58:27 +0900

    2 - 6 회원 수정 API

--- spring/kimyounghan-springboot-jpa-2/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ spring/kimyounghan-springboot-jpa-2/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -54,4 +54,25 @@ public class MemberApiController {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+
+    /**
+     * 수정 API */
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
 }
--- spring/kimyounghan-springboot-jpa-2/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ spring/kimyounghan-springboot-jpa-2/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -39,4 +39,12 @@ public class MemberService {
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
+
+    /**
+     * 회원 수정 */
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 }
