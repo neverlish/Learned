e1009db87564fedcc660a455ca2352033264cb25
Jinho Hyeon <neverlish@gmail.com>
2025-10-05 14:34:45 +0900

    8 - 29 회원 목록 조회

--- spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/web/MemberController.java
+++ spring/kimyounghan-springboot-jpa-1/jpashop/src/main/java/jpabook/jpashop/web/MemberController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -36,5 +38,11 @@ public class MemberController {
         member.setAddress(address);
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/members") public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
