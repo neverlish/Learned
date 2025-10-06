0ce2033e4bb2e8e84f0722b9584a8c5711089987
Jinho Hyeon <neverlish@gmail.com>
2025-10-06 21:57:30 +0900

    4 - 11 간단한 주문 조회 V2: 엔티티를 DTO로 변환

--- spring/kimyounghan-springboot-jpa-2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ spring/kimyounghan-springboot-jpa-2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -37,4 +37,32 @@ public class OrderSimpleApiController {
         }
         return all;
     }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X) * - 단점: 지연로딩으로 쿼리 N번 호출
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate; //주문시간
+        private OrderStatus orderStatus;
+        private Address address;
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
 }
