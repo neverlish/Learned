f8bc5dce0975db102cefad54cae310fafde7499a
Jinho Hyeon <neverlish@gmail.com>
2025-10-06 21:51:29 +0900

    4 API 개발 고급 - 지연 로딩과 조회 성능 최적화 - 10 간단한 주문 조회 V1: 엔티티를 직접 노출

--- spring/kimyounghan-springboot-jpa-2/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ spring/kimyounghan-springboot-jpa-2/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -11,6 +13,14 @@ public class JpashopApplication {
 		String data = hello.getData();
 
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	@Bean
+	Hibernate5JakartaModule hibernate5Module() {
+		Hibernate5JakartaModule hibernate5Module = new Hibernate5JakartaModule();
+		//강제 지연 로딩 설정
+ 		//hibernate5Module.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;
 	}
 
 }
---
+++ spring/kimyounghan-springboot-jpa-2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+
+/** *
+ * xToOne(ManyToOne, OneToOne) 관계 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ *
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+    private final OrderRepository orderRepository;
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리 * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAll(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기화
+        }
+        return all;
+    }
+}
