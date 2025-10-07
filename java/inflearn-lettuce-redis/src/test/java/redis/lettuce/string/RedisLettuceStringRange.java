419744f0284e7d2e3521ba70d5289808652c5314
Jinho Hyeon <neverlish@gmail.com>
2025-10-07 21:59:47 +0900

    3 - 11 string 문자열 데이터 다루기

--- java/inflearn-lettuce-redis/src/test/java/redis/lettuce/string/RedisLettuceStringRange.java
+++ java/inflearn-lettuce-redis/src/test/java/redis/lettuce/string/RedisLettuceStringRange.java
@@ -1,5 +1,6 @@
 package redis.lettuce.string;
 
+import io.lettuce.core.KeyValue;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
@@ -7,6 +8,10 @@ import io.lettuce.core.api.sync.RedisCommands;
 import org.junit.jupiter.api.Test;
 import redis.lettuce.CommandAction;
 import redis.lettuce.CommandTemplate;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RedisLettuceStringRange {
     @Test
@@ -19,6 +24,30 @@ public class RedisLettuceStringRange {
 
             String get = redisCommands.get(key);
             System.out.println("get = " + get);
+
+            redisCommands.append(key, "_addr");
+            System.out.println("get = " + redisCommands.get(key));
+
+            Long strlen = redisCommands.strlen(key);
+            System.out.println("strlen = " + strlen);
+
+            String getrange = redisCommands.getrange(key, 0, 4);
+            System.out.println("getrange = " + getrange);
+
+            redisCommands.setrange(key, 6, "updateStr");
+            System.out.println("get = " + redisCommands.get(key));
+
+            Map<String, String> map = new LinkedHashMap<>();
+            for (int i = 0; i < 5; i++) {
+                map.put(key+i, value+i);
+            }
+            redisCommands.mset(map);
+            List<KeyValue<String, String>> mget = redisCommands.mget(map.keySet().toArray(new String[0]));
+            
+            mget.stream().forEach(m -> {
+                System.out.println("key = " + m.getKey() + ",value = " + m.getValue());
+            });
+
         });
 
         CommandTemplate.commandAction(action);
