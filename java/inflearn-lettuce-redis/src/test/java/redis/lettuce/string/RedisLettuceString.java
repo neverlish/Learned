561435029fc7ea1ddf998a512062a5c9e6b5ba04
Jinho Hyeon <neverlish@gmail.com>
2025-10-07 20:01:56 +0900

    3 - 8 만기 시간(ttl)과 string 저장 옵션 알아보기

--- java/inflearn-lettuce-redis/src/test/java/redis/lettuce/string/RedisLettuceString.java
+++ java/inflearn-lettuce-redis/src/test/java/redis/lettuce/string/RedisLettuceString.java
@@ -1,10 +1,14 @@
 package redis.lettuce.string;
 
+import io.lettuce.core.GetExArgs;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
+import io.lettuce.core.SetArgs;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 public class RedisLettuceString {
     @Test
@@ -25,5 +29,29 @@ public class RedisLettuceString {
         redisCommands.set(key, value);
         String get = redisCommands.get(key);
         System.out.println("get = " + get);
+
+        Long ttl = redisCommands.ttl(key);
+        System.out.println("ttl = " + ttl);
+
+        redisCommands.expire(key, Duration.ofMinutes(1));
+        System.out.println("ttl = " + redisCommands.ttl(key));
+
+        SetArgs setArgs = SetArgs.Builder
+//                .ex(90);
+            .keepttl();
+        redisCommands.set(key, value + "_new", setArgs);
+        System.out.println("ttl = " + redisCommands.ttl(key));
+
+        String getdel = redisCommands.getdel(key);
+        System.out.println("getdel = " + getdel);
+        System.out.println("ttl = " + redisCommands.ttl(key));
+
+        GetExArgs getExArgs = GetExArgs.Builder.ex(120);
+        String getex = redisCommands.getex(key, getExArgs);
+        System.out.println("getex = " + getex);
+        System.out.println("ttl = " + redisCommands.ttl(key));
+
+        connection.close();
+        redisClient.shutdown();
     }
 }
