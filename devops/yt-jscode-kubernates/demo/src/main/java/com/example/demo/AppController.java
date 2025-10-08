f54fa9d278390805a2e42147f2ecfe4afba49f7c
Jinho Hyeon <neverlish@gmail.com>
2025-10-09 02:41:53 +0900

    2 - 4 [예제] 백엔드(Spring Boot) 서버를 파드(Pod)로 띄워보기

---
+++ devops/yt-jscode-kubernates/demo/src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!";
+    }
+}
--- devops/yt-jscode-kubernates/note.md
+++ devops/yt-jscode-kubernates/note.md
@@ -9,3 +9,9 @@
   - root@nginx-pod:/# curl localhost:80
 - sudo kubectl port-forward pod/nginx-pod 80:80
 - kubectl delete pod nginx-pod
+
+# 2.4 [예제] 백엔드(Spring Boot) 서버를 파드(Pod)로 띄워보기
+
+- ./gradlew clean build
+- docker build -t spring-server .
+- kubectl apply -f spring-pod.yaml
