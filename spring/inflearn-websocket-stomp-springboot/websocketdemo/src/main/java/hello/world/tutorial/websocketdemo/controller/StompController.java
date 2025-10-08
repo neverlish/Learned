ff9eaea837e93d3540544e4d8bd083471c779d84
Jinho Hyeon <neverlish@gmail.com>
2025-10-08 15:18:24 +0900

    4 - 10 @SendToUser 살펴보기

--- spring/inflearn-websocket-stomp-springboot/websocketdemo/src/main/java/hello/world/tutorial/websocketdemo/controller/StompController.java
+++ spring/inflearn-websocket-stomp-springboot/websocketdemo/src/main/java/hello/world/tutorial/websocketdemo/controller/StompController.java
@@ -2,21 +2,31 @@ package hello.world.tutorial.websocketdemo.controller;
 
 import hello.world.tutorial.websocketdemo.dto.ReqDto;
 import hello.world.tutorial.websocketdemo.dto.ResDto;
+import hello.world.tutorial.websocketdemo.dto.ResSessionsDto;
+import hello.world.tutorial.websocketdemo.listener.StompEventListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Controller
 @Slf4j
 public class StompController {
-//    @GetMapping("/aaa")
+    private final StompEventListener eventListener;
+
+    public StompController(StompEventListener eventListener) {
+        this.eventListener = eventListener;
+    }
+
+    //    @GetMapping("/aaa")
     @MessageMapping("/hello")
     @SendTo("/topic/hello")
     public ResDto basic(ReqDto reqDto) {
@@ -50,6 +60,15 @@ public class StompController {
         log.info("request: {}", request);
 
         return new ResDto("[" + detail + "]_" + request.getMessage().toUpperCase(), LocalDateTime.now());
+    }
+
+    @MessageMapping("/sessions")
+    @SendToUser("/queue/sessions")
+    public ResSessionsDto sessions(ReqDto request, MessageHeaders headers) {
+        log.info("request: {}", request);
+        Set<String> sessions = eventListener.getSessions();
+        String sourceSessionId = headers.get("simpSessionId").toString();
+        return new ResSessionsDto(sessions.size(), sessions.stream().toList(), sourceSessionId, LocalDateTime.now());
     }
 
 }
