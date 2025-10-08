1d56c4c349e8d81e098c1bc7f09a6615dfd38a89
Jinho Hyeon <neverlish@gmail.com>
2025-10-08 15:09:10 +0900

    4 - 8 @MessageMapping 살펴보기2

--- spring/inflearn-websocket-stomp-springboot/websocketdemo/src/main/java/hello/world/tutorial/websocketdemo/controller/StompController.java
+++ spring/inflearn-websocket-stomp-springboot/websocketdemo/src/main/java/hello/world/tutorial/websocketdemo/controller/StompController.java
@@ -3,6 +3,9 @@ package hello.world.tutorial.websocketdemo.controller;
 import hello.world.tutorial.websocketdemo.dto.ReqDto;
 import hello.world.tutorial.websocketdemo.dto.ResDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -21,4 +24,32 @@ public class StompController {
 
         return new ResDto(reqDto.getMessage().toUpperCase(), LocalDateTime.now());
     }
+
+    @MessageMapping("/multi")
+    @SendTo({"/topic/hello", "/topic/hello2"})
+    public ResDto multi(ReqDto request) {
+        log.info("request: {}", request);
+
+        return new ResDto(request.getMessage().toUpperCase(), LocalDateTime.now());
+    }
+
+    @MessageMapping("/hello1")
+    @SendTo("/topic/hello")
+    public ResDto annotations(Message<ReqDto> message, MessageHeaders headers, ReqDto request) {
+        log.info("message: {}", message);
+        log.info("headers: {}", headers);
+        log.info("request: {}", request);
+
+        return new ResDto(request.getMessage().toUpperCase(), LocalDateTime.now());
+    }
+
+    @MessageMapping("/hello/{detail}")
+    @SendTo("/topic/hello")
+    public ResDto detail(@DestinationVariable("detail") String detail, ReqDto request) {
+        log.info("detail: {}", detail);
+        log.info("request: {}", request);
+
+        return new ResDto("[" + detail + "]_" + request.getMessage().toUpperCase(), LocalDateTime.now());
+    }
+
 }
