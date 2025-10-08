2f468f2ad3f2a682dc6b37f47f40f498508c49cd
Jinho Hyeon <neverlish@gmail.com>
2025-10-08 13:17:57 +0900

    1 project 생성 및 기본 설정 - 1 project 생성

---
+++ spring/inflearn-websocket-stomp-springboot/websocketdemo/src/main/java/hello/world/tutorial/websocketdemo/WebsocketdemoApplication.java
@@ -0,0 +1,13 @@
+package hello.world.tutorial.websocketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketdemoApplication.class, args);
+	}
+
+}
