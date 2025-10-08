778a3ef1b1a76f81a4080a665a46651f52f520bd
Jinho Hyeon <neverlish@gmail.com>
2025-10-08 17:39:29 +0900

    5 - 14 virtual thread in task scheduling

---
+++ spring/inflearn-virtualthread-1hour/virtualthread/src/main/java/hello/world/virtualthread/scheduler/VirtualScheduler.java
@@ -0,0 +1,16 @@
+package hello.world.virtualthread.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class VirtualScheduler {
+
+
+    @Scheduled(fixedRate = 5000)
+    public void fixedRate() {
+        log.info("fixedRate. thread: {}", Thread.currentThread());
+    }
+}
