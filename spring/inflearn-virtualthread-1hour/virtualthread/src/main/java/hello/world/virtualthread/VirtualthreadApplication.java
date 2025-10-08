9eb46b8af679c7067c5644a1d3e6bfc68ab0b582
Jinho Hyeon <neverlish@gmail.com>
2025-10-08 16:06:52 +0900

    2 개발 환경 설정 - 3 개발 환경 설정

---
+++ spring/inflearn-virtualthread-1hour/virtualthread/src/main/java/hello/world/virtualthread/VirtualthreadApplication.java
@@ -0,0 +1,13 @@
+package hello.world.virtualthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualthreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualthreadApplication.class, args);
+	}
+
+}
