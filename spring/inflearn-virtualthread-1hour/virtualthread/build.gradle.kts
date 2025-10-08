8fc81348eeee30c6952a194f950641c12d6462e8
Jinho Hyeon <neverlish@gmail.com>
2025-10-08 17:37:18 +0900

    5 spring boot 에서의 virtual thread - 12 virtual thread in spring rest controller

--- spring/inflearn-virtualthread-1hour/virtualthread/build.gradle.kts
+++ spring/inflearn-virtualthread-1hour/virtualthread/build.gradle.kts
@@ -27,6 +27,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ spring/inflearn-virtualthread-1hour/virtualthread/src/main/java/hello/world/virtualthread/controller/VirtualController.java
@@ -0,0 +1,22 @@
+package hello.world.virtualthread.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class VirtualController {
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/sleep")
+    public void sleep() throws InterruptedException {
+        log.info("1) counter: {}, thread: {}", counter.incrementAndGet(), Thread.currentThread());
+        Thread.sleep(5000);
+        log.info("2) thread: {}", Thread.currentThread());
+    }
+}
