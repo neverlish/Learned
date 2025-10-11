6a2d23cdbd253cb939d650809d44d34d0104bd71
Jinho Hyeon <neverlish@gmail.com>
2025-10-11 14:15:59 +0900

    3 - 1  [실습] Spring Boot로 Kafka에서 처리에 실패한 메시지를 재시도(Retry)하도록 만들기

--- kafka/yt-jscode-kafka/email-send-consumer/src/main/java/com/example/emailsendconsumer/EmailSendConsumer.java
+++ kafka/yt-jscode-kafka/email-send-consumer/src/main/java/com/example/emailsendconsumer/EmailSendConsumer.java
@@ -1,6 +1,8 @@
 package com.example.emailsendconsumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,10 +12,19 @@ public class EmailSendConsumer {
         topics = "email.send",
         groupId = "email-send-group"
     )
+    @RetryableTopic(
+        attempts = "5",
+        backoff = @Backoff(delay = 1000, multiplier = 2)
+    )
     public void consume(String message) {
         System.out.println("Kafka로부터 받아온 메시지 : " + message);
 
         EmailSendMessage emailSendMessage = EmailSendMessage.fromJson(message);
+
+        if (emailSendMessage.getTo().equals("fail@naver.com")) {
+            System.out.println("잘못된 이메일 주소로 인해 발송 실패");
+            throw new RuntimeException("잘못된 이메일 주소로 인해 발송 실패");
+        }
 
         try {
             Thread.sleep(3000);
