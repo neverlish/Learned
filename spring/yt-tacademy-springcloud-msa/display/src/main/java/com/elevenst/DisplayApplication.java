f697e7396215cea3d9887ad0879798d6ea2fd0ee
Jinho Hyeon <neverlish@gmail.com>
2025-10-20 00:33:14 +0900

    4 Circuit Breaker - Hystrix

--- spring/yt-tacademy-springcloud-msa/display/src/main/java/com/elevenst/DisplayApplication.java
+++ spring/yt-tacademy-springcloud-msa/display/src/main/java/com/elevenst/DisplayApplication.java
@@ -2,9 +2,11 @@ package com.elevenst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
 @SpringBootApplication
 public class DisplayApplication {
     @Bean
--- spring/yt-tacademy-springcloud-msa/display/src/main/java/com/elevenst/service/ProductRemoteServiceImpl.java
+++ spring/yt-tacademy-springcloud-msa/display/src/main/java/com/elevenst/service/ProductRemoteServiceImpl.java
@@ -1,5 +1,6 @@
 package com.elevenst.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,7 +13,13 @@ public class ProductRemoteServiceImpl implements ProductRemoteService {
     private RestTemplate restTemplate;
 
     @Override
+    @HystrixCommand(fallbackMethod = "getProductInfoFallback")
     public String getProductInfo(String productId) {
         return restTemplate.getForObject(URL + productId, String.class);
+    }
+
+    public String getProductInfoFallback(String productId, Throwable t) {
+        System.out.println("t = " + t);
+        return "[This Product is sold out]";
     }
 }
