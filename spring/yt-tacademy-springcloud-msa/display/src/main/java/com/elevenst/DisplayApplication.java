afefdb405c3d6c2db4671b529a797f4881e549ff
Jinho Hyeon <neverlish@gmail.com>
2025-10-20 10:12:32 +0900

    7 Declarative Http Client - Feign

--- spring/yt-tacademy-springcloud-msa/display/src/main/java/com/elevenst/DisplayApplication.java
+++ spring/yt-tacademy-springcloud-msa/display/src/main/java/com/elevenst/DisplayApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableCircuitBreaker
 @EnableEurekaClient
+@EnableFeignClients
 @SpringBootApplication
 public class DisplayApplication {
 
