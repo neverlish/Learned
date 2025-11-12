3374aca37a9c9c577374fd247fde2772b97b039f
neverlish <neverlish@gmail.com>
2025-11-12 11:27:29 +0900

    2 스레딩 기초 - 스레드 생성 - 4 스레드 생성 - 1부. 스레드의 기능과 디버깅

---
+++ java/udemy-java-multithreading/src/main/java/thread/creation/example/Main1.java
@@ -0,0 +1,27 @@
+package thread.creation.example;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main1 {
+    static void main() throws InterruptedException {
+        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
+        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("We are now in thread " + Thread.currentThread().getName());
+                System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
+            }
+        });
+
+        thread.setName("New Worker Thread");
+
+        thread.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
+        thread.start();
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
+
+        Thread.sleep(10000);
+    }
+}
