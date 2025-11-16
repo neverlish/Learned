bd44d3eed34136cdbb7da58fe99f6d04ce65af46
neverlish <neverlish@gmail.com>
2025-11-17 00:46:20 +0900

    6 - 22 락킹 기법과 데드락

---
+++ java/udemy-java-multithreading/src/main/java/thread/deadlock/Main.java
@@ -0,0 +1,92 @@
+package thread.deadlock;
+
+import java.util.Random;
+
+public class Main {
+    static void main() {
+        Intersection intersection = new Intersection();
+        Thread trainAThread = new Thread(new TrainA(intersection));
+        Thread trainBThread = new Thread(new TrainB(intersection));
+
+        trainAThread.start();
+        trainBThread.start();
+    }
+    public static class TrainA implements Runnable {
+        private Intersection intersection;
+        private Random random = new Random();
+
+        public TrainA(Intersection intersection) {
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                long sleepTime = random.nextInt(5);
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+
+                }
+                intersection.takeRoadA();
+            }
+        }
+    }
+
+    public  static class TrainB implements Runnable {
+        private Intersection intersection;
+        private Random random = new Random();
+
+        public TrainB(Intersection intersection) {
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                long sleepTime = random.nextInt(5);
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+
+                }
+                intersection.takeRoadB();
+            }
+        }
+    }
+
+    public static class Intersection {
+        private Object roadA = new Object();
+        private Object roadB = new Object();
+
+        public void takeRoadA() {
+            synchronized (roadA) {
+                System.out.println("Road A is locked by thread " + Thread.currentThread().getName());
+
+                synchronized (roadB) {
+                    System.out.println("Train is passing through road A");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+            }
+        }
+
+        public void takeRoadB() {
+            synchronized (roadB) {
+                System.out.println("Road B is locked by thead " + Thread.currentThread().getName());
+
+                synchronized (roadA) {
+                    System.out.println("Train is passing through road B");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+            }
+        }
+    }
+}
