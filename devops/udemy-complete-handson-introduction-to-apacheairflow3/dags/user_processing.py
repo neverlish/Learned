c88a822c6adb3b3562632748d0d69efee97f507e
neverlish <neverlish@gmail.com>
2025-11-21 16:24:15 +0900

    4 - 30 Processing users

--- devops/udemy-complete-handson-introduction-to-apacheairflow3/dags/user_processing.py
+++ devops/udemy-complete-handson-introduction-to-apacheairflow3/dags/user_processing.py
@@ -42,7 +42,23 @@ def user_processing():
             'email': fake_user['personalInfo']['email'],
         }
 
+    @task
+    def process_user(user_info):
+        import csv
+        user_info = {
+            'id': "123",
+            'firstname': "John",
+            'lastname': "Doe",
+            'email': "john.doe@example.com",
+        }
+
+        with open("/tmp/user_info.csv", "a") as f:
+            writer = csv.DictWriter(f, fieldnames=user_info.keys())
+            writer.writeheader()
+            writer.writerow(user_info)
+
     fake_user = is_api_available()
     user_info = extract_user(fake_user)
+    process_user(user_info)
     
 user_processing()
