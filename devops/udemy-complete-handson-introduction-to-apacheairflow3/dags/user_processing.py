59408520ebc0523de0772b88366a7357cd3f589a
neverlish <neverlish@gmail.com>
2025-11-21 09:09:59 +0900

    4 - 22 Create a Table

--- devops/udemy-complete-handson-introduction-to-apacheairflow3/dags/user_processing.py
+++ devops/udemy-complete-handson-introduction-to-apacheairflow3/dags/user_processing.py
@@ -1,7 +1,21 @@
 from airflow.sdk import dag
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 @dag
 def user_processing():
-    pass
-
+    
+    create_table = SQLExecuteQueryOperator(
+        task_id="create_table",
+        conn_id="postgres",
+        sql="""
+        CREATE TABLE IF NOT EXISTS users (
+            id INT PRIMARY KEY,
+            firstname VARCHAR(255),
+            lastname VARCHAR(255),
+            email VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        """
+    )
+    
 user_processing()
