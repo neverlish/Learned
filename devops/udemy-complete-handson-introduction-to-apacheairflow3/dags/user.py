8e1d199676da2afb2a7fdf47fd524579561e2f73
neverlish <neverlish@gmail.com>
2025-11-22 10:27:54 +0900

    5 - 38 Define dependencies between Assets

--- devops/udemy-complete-handson-introduction-to-apacheairflow3/dags/user.py
+++ devops/udemy-complete-handson-introduction-to-apacheairflow3/dags/user.py
@@ -1,4 +1,4 @@
-from airflow.sdk import asset
+from airflow.sdk import asset, Asset, Context
 
 @asset(
     schedule='@daily',
@@ -9,3 +9,15 @@ def user(self) -> dict[str]:
 
     r = requests.get(self.uri)
     return r.json()
+
+@asset(
+    schedule=user
+)
+def user_location(user: Asset, context: Context) -> dict[str]:
+    user_data = context['ti'].xcom_pull(
+        dag_id=user.name,
+        task_ids=user.name,
+        include_prior_dates=True
+    )
+
+    return user_data['results'][0]['location']
