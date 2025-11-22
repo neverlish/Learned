f53a1368701472acb910ac0d76143e0728a3a086
neverlish <neverlish@gmail.com>
2025-11-22 10:30:31 +0900

    5 - 39 Monitor your Assets

--- devops/udemy-complete-handson-introduction-to-apacheairflow3/dags/user.py
+++ devops/udemy-complete-handson-introduction-to-apacheairflow3/dags/user.py
@@ -21,3 +21,14 @@ def user_location(user: Asset, context: Context) -> dict[str]:
     )
 
     return user_data['results'][0]['location']
+
+@asset(
+    schedule=user
+)
+def user_login(user: Asset, context: Context) -> dict[str]:
+    user_data = context['ti'].xcom_pull(
+        dag_id=user.name,
+        task_ids=user.name,
+        include_prior_dates=True
+    )
+    return user_data['results'][0]['login']
