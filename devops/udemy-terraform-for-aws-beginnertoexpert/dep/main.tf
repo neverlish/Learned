44041e9ab672285e5c0883bfde140e3c98c46399
neverlish <neverlish@gmail.com>
2025-11-23 00:16:23 +0900

    12 - 71 Data Sources

--- devops/udemy-terraform-for-aws-beginnertoexpert/dep/main.tf
+++ devops/udemy-terraform-for-aws-beginnertoexpert/dep/main.tf
@@ -5,11 +5,30 @@ provider "aws" {
 resource "aws_instance" "db" {
   ami = "ami-04fcc2023d6e37430"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "DB Server"
+  }
 }
 
 resource "aws_instance" "web" {
   ami = "ami-04fcc2023d6e37430"
   instance_type = "t2.micro"
 
+  tags = {
+    Name = "Web Server"
+  }
+
   depends_on = [aws_instance.db]
+}
+
+data "aws_instance" "dbsearch" {
+  filter {
+    name = "tag:Name"
+    values = ["DB Server"]
+  }
+}
+
+output "dbservers" {
+  value = data.aws_instance.dbsearch
 }
