a90c6f436de68fc057265d2f57106079aeb6d952
neverlish <neverlish@gmail.com>
2025-11-22 11:50:12 +0900

    6 - 51 Smart distribution of your tasks

--- devops/udemy-complete-handson-introduction-to-apacheairflow3/dags/celery_dag.py
+++ devops/udemy-complete-handson-introduction-to-apacheairflow3/dags/celery_dag.py
@@ -7,15 +7,21 @@ def celery_dag():
     def a():
         sleep(5)
 
-    @task
+    @task(
+        queue="high_cpu"
+    )
     def b():
         sleep(5)
 
-    @task
+    @task(
+        queue="high_cpu"
+    )
     def c():
         sleep(5)
 
-    @task
+    @task(
+        queue="high_cpu"
+    )
     def d():
         sleep(5)
 
