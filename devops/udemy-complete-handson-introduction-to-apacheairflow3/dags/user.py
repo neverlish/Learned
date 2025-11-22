03584ea636e61bc382089bf23a101aa9fb04cce1
neverlish <neverlish@gmail.com>
2025-11-22 10:36:07 +0900

    5 - 40 Create multiple Assets at once

--- devops/udemy-complete-handson-introduction-to-apacheairflow3/dags/user.py
+++ devops/udemy-complete-handson-introduction-to-apacheairflow3/dags/user.py
@@ -10,25 +10,20 @@ def user(self) -> dict[str]:
     r = requests.get(self.uri)
     return r.json()
 
-@asset(
-    schedule=user
+@asset.multi(
+    schedule=user,
+    outlets=[
+        Asset(name='user_location'),
+        Asset(name='user_login'),
+    ]
 )
-def user_location(user: Asset, context: Context) -> dict[str]:
+def user_info(user: Asset, context: Context) -> list[dict[str]]:
     user_data = context['ti'].xcom_pull(
         dag_id=user.name,
         task_ids=user.name,
         include_prior_dates=True
     )
-
-    return user_data['results'][0]['location']
-
-@asset(
-    schedule=user
-)
-def user_login(user: Asset, context: Context) -> dict[str]:
-    user_data = context['ti'].xcom_pull(
-        dag_id=user.name,
-        task_ids=user.name,
-        include_prior_dates=True
-    )
-    return user_data['results'][0]['login']
+    return [
+        user_data['results'][0]['location'],
+        user_data['results'][0]['login']
+    ]
