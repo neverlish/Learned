a47f1073a0a94ab8d7b78a957a6269b6e4bc872c
neverlish <neverlish@gmail.com>
2025-11-22 16:20:29 +0900

    7 - 49 Security Groups

---
+++ devops/udemy-terraform-for-aws-beginnertoexpert/sg/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-04fcc2023d6e37430"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.webtraffic.name]
+}
+
+resource "aws_security_group" "webtraffic" {
+  name = "Allow HTTPS"
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
