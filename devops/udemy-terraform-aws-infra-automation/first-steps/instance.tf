f4b4816b3c2a6087c3258db9d9630a06fc0a47fc
neverlish <neverlish@gmail.com>
2025-11-27 20:47:19 +0900

    3 - 10 테라폼의 첫 단계 - 이전 예시 개선하기

--- devops/udemy-terraform-aws-infra-automation/first-steps/instance.tf
+++ devops/udemy-terraform-aws-infra-automation/first-steps/instance.tf
@@ -2,8 +2,26 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-04fcc2023d6e37430"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  tags = {
+    Name = "example"
+  }
 }
 
