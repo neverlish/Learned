3f241bfeb3bffa1de3874fa5958940f768d8b3e8
neverlish <neverlish@gmail.com>
2025-11-30 13:46:19 +0900

    7 - 69 패커와 젠킨스를 이용한 테라폼 2부

---
+++ devops/udemy-terraform-aws-infra-automation/jenkins-packer-demo/scripts/jenkins-init.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# volume setup
+vgchange -ay
+
+DEVICE_FS=`blkid -o value -s TYPE ${DEVICE}`
+if [ "`echo -n $DEVICE_FS`" == "" ] ; then 
+  # wait for the device to be attached
+  DEVICENAME=`echo "${DEVICE}" | awk -F '/' '{print $3}'`
+  DEVICEEXISTS=''
+  while [[ -z $DEVICEEXISTS ]]; do
+    echo "checking $DEVICENAME"
+    DEVICEEXISTS=`lsblk |grep "$DEVICENAME" |wc -l`
+    if [[ $DEVICEEXISTS != "1" ]]; then
+      sleep 15
+    fi
+  done
+  pvcreate ${DEVICE}
+  vgcreate data ${DEVICE}
+  lvcreate --name volume1 -l 100%FREE data
+  mkfs.ext4 /dev/data/volume1
+fi
+mkdir -p /var/lib/jenkins
+echo '/dev/data/volume1 /var/lib/jenkins ext4 defaults 0 0' >> /etc/fstab
+mount /var/lib/jenkins
+
+# jenkins repository
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+echo "deb http://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
+apt-get update
+
+# install dependencies
+apt-get install -y python3 openjdk-17-jdk awscli
+# install jenkins
+# Add Jenkins repository and key
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update package list and install Jenkins
+apt-get update
+apt-get install -y jenkins=${JENKINS_VERSION} unzip
+
+# install terraform
+wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# install packer
+cd /usr/local/bin
+wget -q https://releases.hashicorp.com/packer/0.10.2/packer_0.10.2_linux_amd64.zip
+unzip packer_0.10.2_linux_amd64.zip
+# clean up
+apt-get clean
+rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+rm packer_0.10.2_linux_amd64.zip
