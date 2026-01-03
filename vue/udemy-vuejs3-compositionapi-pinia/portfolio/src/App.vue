6318379fbe39710ff6d8db6dddd7ac819210347d
neverlish <neverlish@gmail.com>
2026-01-03 20:30:41 +0900

    2 - 26 프로젝트 셋업-Vite

---
+++ vue/udemy-vuejs3-compositionapi-pinia/portfolio/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="parent">
+    <h1 class="display-1">App</h1>
+    <button class="btn btn-primary">button</button>
+  </div>
+  <h1>heading 1</h1>
+</template>
+
+<style scoped lang="scss">
+.parent {
+  h1 { color: red; }
+  button { color: yellow; }
+}
+</style>
---
+++ vue/udemy-vuejs3-compositionapi-pinia/portfolio/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+createApp(App).mount('#app')
