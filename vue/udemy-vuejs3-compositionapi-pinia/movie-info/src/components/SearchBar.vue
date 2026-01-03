a4e173f779ad917e4b6d5fb722915fff3a308967
neverlish <neverlish@gmail.com>
2026-01-03 14:31:08 +0900

    1 - 16 검색창 입력값 검사하기

--- vue/udemy-vuejs3-compositionapi-pinia/movie-info/src/components/SearchBar.vue
+++ vue/udemy-vuejs3-compositionapi-pinia/movie-info/src/components/SearchBar.vue
@@ -2,7 +2,7 @@
   <div class="search-box">
     <input 
       type="search" 
-      v-model="inputText"
+      @change="inputText = $event.target.value"
       placeholder="검색어 입력"
     >
     <button>검색</button>
@@ -16,6 +16,14 @@ export default {
   data() {
     return {
       inputText: '',
+    }
+  },
+  watch: {
+    inputText(name) {
+      // 입력한 영화제목이 데이터에 있는지 확인
+      if(name !== "노량") {
+        alert('해당하는 영화가 없습니다');
+      }
     }
   }
 }
