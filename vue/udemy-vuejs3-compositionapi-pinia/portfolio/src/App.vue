c296170301a98ad05272e3187840f26aacc4ff0f
neverlish <neverlish@gmail.com>
2026-01-03 21:13:48 +0900

    2 - 27 홈 타이틀 영역 디자인

--- vue/udemy-vuejs3-compositionapi-pinia/portfolio/src/App.vue
+++ vue/udemy-vuejs3-compositionapi-pinia/portfolio/src/App.vue
@@ -3,16 +3,71 @@
 </script>
 
 <template>
-  <div class="parent">
-    <h1 class="display-1">App</h1>
-    <button class="btn btn-primary">button</button>
-  </div>
-  <h1>heading 1</h1>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top" data-bs-theme="dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Doolee</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <section id="title">
+    <div class="container">
+      <div class="detail row">
+        <figure class="col-md-6">
+          <img src="./assets/images/title.png" alt="title">
+        </figure>
+        <div class="text col-md-6">
+          <h2>Hello,<br>I'm Doolee</h2>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
-.parent {
-  h1 { color: red; }
-  button { color: yellow; }
-}
-</style>
+#title {
+
+  .container {
+    $navbar_height: 56px; // navbar 높이값
+    height: 100vh;
+    padding: $navbar_height;
+
+    .row {
+      height: 100%;
+      flex-direction: row-reverse;
+    }
+  }
+
+  figure {
+    img {
+      width: 100%;
+    }
+    display: grid;
+    place-items: center;
+  }
+
+  .text {
+    h2 {
+      font-size: 4em;
+    }
+
+    display: grid;
+    place-items: center;
+  }
+}</style>
