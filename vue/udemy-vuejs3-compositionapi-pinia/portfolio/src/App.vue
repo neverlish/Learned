71fe7756bbca615f7fb16c174afe9904dd90d498
neverlish <neverlish@gmail.com>
2026-01-03 21:16:56 +0900

    2 - 28 타이틀 영역 컴포넌트로 만들기

--- vue/udemy-vuejs3-compositionapi-pinia/portfolio/src/App.vue
+++ vue/udemy-vuejs3-compositionapi-pinia/portfolio/src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import Title from './components/Title.vue';
 
 </script>
 
@@ -25,49 +26,8 @@
       </div>
     </div>
   </nav>
-
-  <section id="title">
-    <div class="container">
-      <div class="detail row">
-        <figure class="col-md-6">
-          <img src="./assets/images/title.png" alt="title">
-        </figure>
-        <div class="text col-md-6">
-          <h2>Hello,<br>I'm Doolee</h2>
-        </div>
-      </div>
-    </div>
-  </section>
+  <Title />
 </template>
 
 <style scoped lang="scss">
-#title {
-
-  .container {
-    $navbar_height: 56px; // navbar 높이값
-    height: 100vh;
-    padding: $navbar_height;
-
-    .row {
-      height: 100%;
-      flex-direction: row-reverse;
-    }
-  }
-
-  figure {
-    img {
-      width: 100%;
-    }
-    display: grid;
-    place-items: center;
-  }
-
-  .text {
-    h2 {
-      font-size: 4em;
-    }
-
-    display: grid;
-    place-items: center;
-  }
-}</style>
+</style>
---
+++ vue/udemy-vuejs3-compositionapi-pinia/portfolio/src/components/Title.vue
@@ -0,0 +1,52 @@
+
+<template>
+  <section id="title">
+    <div class="container">
+      <div class="detail row">
+        <figure class="col-md-6">
+          <img src="../assets/images/title.png" alt="title">
+        </figure>
+        <div class="text col-md-6">
+          <h2 v-html="title"></h2>
+        </div>
+      </div>
+    </div>
+  </section>    
+</template>
+  
+<script setup>
+  const title = "Hello,<br> I'm Michael Kwon"; 
+
+</script>
+  
+<style lang="scss">
+#title {
+  .container {
+    $navbar_height: 56px; // navbar 높이값
+    height: 100vh;
+    padding: $navbar_height;
+
+    .row {
+      height: 100%;
+      flex-direction: row-reverse;
+    }
+  }
+
+  figure {
+    img {
+      width: 100%;
+    }
+    display: grid;
+    place-items: center;
+  }
+
+  .text {
+    h2 {
+      font-size: 4em;
+    }
+
+    display: grid;
+    place-items: center;
+  }
+}
+</style>
