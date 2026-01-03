3da0f0d2f11d4984ce2704a26d34c081c4ca6795
neverlish <neverlish@gmail.com>
2026-01-03 22:00:14 +0900

    3 - 38 Navbar 영역 및 아이콘 추가

--- vue/udemy-vuejs3-compositionapi-pinia/vue-weather-app/src/App.vue
+++ vue/udemy-vuejs3-compositionapi-pinia/vue-weather-app/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+  import Navbar from './components/Navbar.vue';
 
 </script>
 
 <template>
-  <h1>App</h1>
+  <Navbar/>
+  <font-awesome-icon :icon="['fas', 'location-dot']" />
 </template>
 
 <style scoped lang="scss">
--- vue/udemy-vuejs3-compositionapi-pinia/vue-weather-app/src/main.js
+++ vue/udemy-vuejs3-compositionapi-pinia/vue-weather-app/src/main.js
@@ -2,4 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// 사용할 아이콘 불러오기
+import { faBarsStaggered, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
+// 아이콘 등록하기
+library.add(faBarsStaggered, faLocationDot);
+
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app')
