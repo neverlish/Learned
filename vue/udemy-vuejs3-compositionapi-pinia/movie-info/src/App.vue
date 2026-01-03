892ab88a69c9f414932ba03aef21484549e8024b
neverlish <neverlish@gmail.com>
2026-01-03 11:56:48 +0900

    1 - 9 모달창 내용 동적으로 보여주기

--- vue/udemy-vuejs3-compositionapi-pinia/movie-info/src/App.vue
+++ vue/udemy-vuejs3-compositionapi-pinia/movie-info/src/App.vue
@@ -11,13 +11,13 @@
       <button @click="increaseLike(i)">좋아요</button>
       <span>{{ movie.like }}</span>
       <p>
-        <button @click="isModal=true">상세보기</button>
+        <button @click="isModal=true; selectedMovie=i">상세보기</button>
       </p>
     </div>
   </div>
   <div class="modal" v-if="isModal">
     <div class="inner">
-      <h3>Detail</h3>
+      <h3>{{data[selectedMovie].title}}</h3>
       <p>영화 상세정보</p>
       <button @click="isModal=false">닫기</button>
     </div>
@@ -34,6 +34,7 @@ export default {
     return {
       isModal: false,
       data: data,
+      selectedMovie: 0
     }
   },
   methods: {
