489307b9bbde2ad5f66b11ed19c753bdc98d40a0
neverlish <neverlish@gmail.com>
2026-01-03 11:42:15 +0900

    1 - 6 click 이벤트로 좋아요 버튼 만들기

--- vue/udemy-vuejs3-compositionapi-pinia/movie-info/src/App.vue
+++ vue/udemy-vuejs3-compositionapi-pinia/movie-info/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <h1>영화정보</h1>
   <div v-for="(movie, i) in data" :key="i">
-    <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
+    <h3 class="bg-yellow" :style="movie.textRed">{{ movie.title }}</h3>
     <p>개봉: {{ movie.year }}</p>
     <p>장르: {{ movie.category }}</p>
+    <button @click="increaseLike(i)">좋아요</button><span>{{ movie.like }}</span>
   </div>
 </template>
 
@@ -17,19 +18,27 @@ export default {
           title: '노량',
           year: 2023,
           category: '액션',
-          textRed: 'color: red;'
+          textRed: 'color: red;',
+          like: 0,
         },
         {
           title: '아쿠아맨과 로스트 킹덤',
           year: 2023,
           category: '액션, 판타지, 어드벤처',
+          like: 0,
         },
         {
           title: '3월의 휴가',
           year: 2023,
           category: '판타지, 드라마',
+          like: 0,
         },
       ]
+    }
+  },
+  methods: {
+    increaseLike(i) {
+      this.data[i].like++;
     }
   }
 }
