08d338509970e9b884422b1a07a8df067ed5cf1b
neverlish <neverlish@gmail.com>
2026-01-03 11:36:19 +0900

    1 - 4 문서에 데이터 표시하기

--- vue/udemy-vuejs3-compositionapi-pinia/movie-info/src/App.vue
+++ vue/udemy-vuejs3-compositionapi-pinia/movie-info/src/App.vue
@@ -1,11 +1,28 @@
 <template>
-  <h1>Hello Vue</h1>
+  <h1>영화정보</h1>
+  <div>
+    <h3 class="bg-yellow" :style="textRed">{{ title }}</h3>
+    <p>개봉: {{ year }}</p>
+    <p>장르: {{ category }}</p>
+  </div>
 </template>
 
 <script>
-
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: '노량',
+      year: 2023,
+      category: '액션',
+      textRed: 'color: red;'
+    }
+  }
+}
 </script>
 
 <style>
-
+.bg-yellow {
+  background-color: yellow;
+}
 </style>
