b0b8282114c5ba272fc6c9a4c37ce5348c2d97c3
neverlish <neverlish@gmail.com>
2026-01-04 15:12:58 +0900

    2 - 14 [NEW] Defining our LangGraph Graph

--- llm/udemy-langgraph-develop-llmpowered-aiagents/reflection-agent/main.py
+++ llm/udemy-langgraph-develop-llmpowered-aiagents/reflection-agent/main.py
@@ -1,30 +1,34 @@
-from typing import List, Sequence
+from typing import TypedDict, Annotated
 
 from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_core.messages import BaseMessage, HumanMessage
-from langgraph.graph import END, MessageGraph
+from langgraph.graph import END, StateGraph
+from langgraph.graph.message import add_messages
 
-from chains import generate_chain, reflection_prompt
+from chains import generate_chain, reflect_chain
+
+class MessageGraph(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
 
 REFLECT = "reflect"
 GENERATE = "generate"
 
-def generation_node(state: Sequence[BaseMessage]):
-    return generate_chain.invoke({"messages": state})
+def generation_node(state: MessageGraph):
+    return {"messages": [generate_chain.invoke({"messages": state["messages"]})]}
 
-def reflection_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
-    res = reflection_prompt.invoke({"messages": messages})
-    return [HumanMessage(content=res.content)]
+def reflection_node(state: MessageGraph):
+    res = reflect_chain.invoke({"messages": state["messages"]})
+    return {"messages": [HumanMessage(content=res.content)]}
 
-builder = MessageGraph()
+builder = StateGraph(state_schema=MessageGraph)
 builder.add_node(GENERATE, generation_node)
 builder.add_node(REFLECT, reflection_node)
 builder.set_entry_point(GENERATE)
 
-def should_continue(state: List[BaseMessage]):
-    if len(state) > 6:
+def should_continue(state: MessageGraph):
+    if len(state["messages"]) > 6:
         return END
     return REFLECT
 
@@ -33,7 +37,6 @@ builder.add_edge(REFLECT, GENERATE)
 
 graph = builder.compile()
 print(graph.get_graph().draw_mermaid())
-# graph.get_graph().print_ascii()
 
 if __name__ == "__main__":
     print("Hello LangGraph")
