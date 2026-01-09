5d49805afe18a5b29ca920e89cb1439d2f6f11c6
neverlish <neverlish@gmail.com>
2026-01-10 08:37:48 +0900

    7 - 53 Parallel node fan-out and fan-in with extra steps

--- llm/udemy-langgraph-develop-llmpowered-aiagents/human-in-the-loop-memory/async.py
+++ llm/udemy-langgraph-develop-llmpowered-aiagents/human-in-the-loop-memory/async.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -20,24 +21,27 @@ class ReturnNodeValue:
 
     def __call__(self, state: State) -> Any:
         import time
+
         time.sleep(1)
         print(f"Adding {self._value} to {state['aggregate']}")
         return {"aggregate": [self._value]}
+
 
 builder = StateGraph(State)
 builder.add_node("a", ReturnNodeValue("I'm A"))
 builder.add_edge(START, "a")
 builder.add_node("b", ReturnNodeValue("I'm B"))
+builder.add_node("b2", ReturnNodeValue("I'm B2"))
 builder.add_node("c", ReturnNodeValue("I'm C"))
 builder.add_node("d", ReturnNodeValue("I'm D"))
 builder.add_edge("a", "b")
 builder.add_edge("a", "c")
-builder.add_edge("b", "d")
-builder.add_edge("c", "d")
+builder.add_edge("b", "b2")
+builder.add_edge(["b2", "c"], "d")
 builder.add_edge("d", END)
 graph = builder.compile()
 graph.get_graph().draw_mermaid_png(output_file_path="async.png")
 
 if __name__ == "__main__":
-    print("Hello Async Graph")
+    print("Helo Async Graph")
     graph.invoke({"aggregate": []}, {"configurable": {"thread_id": "foo"}})
