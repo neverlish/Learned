69f6a7d732e45e75ea1ae2d363a2daf1e42fd74d
neverlish <neverlish@gmail.com>
2026-01-10 08:35:08 +0900

    7 Run graph asynchronously - 52 Implementing Async Execution

---
+++ llm/udemy-langgraph-develop-llmpowered-aiagents/human-in-the-loop-memory/async.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
+import operator
+from typing import Annotated, Any
+
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+
+
+class State(TypedDict):
+    aggregate: Annotated[list, operator.add]
+
+
+class ReturnNodeValue:
+    def __init__(self, node_secret: str):
+        self._value = node_secret
+
+    def __call__(self, state: State) -> Any:
+        import time
+        time.sleep(1)
+        print(f"Adding {self._value} to {state['aggregate']}")
+        return {"aggregate": [self._value]}
+
+builder = StateGraph(State)
+builder.add_node("a", ReturnNodeValue("I'm A"))
+builder.add_edge(START, "a")
+builder.add_node("b", ReturnNodeValue("I'm B"))
+builder.add_node("c", ReturnNodeValue("I'm C"))
+builder.add_node("d", ReturnNodeValue("I'm D"))
+builder.add_edge("a", "b")
+builder.add_edge("a", "c")
+builder.add_edge("b", "d")
+builder.add_edge("c", "d")
+builder.add_edge("d", END)
+graph = builder.compile()
+graph.get_graph().draw_mermaid_png(output_file_path="async.png")
+
+if __name__ == "__main__":
+    print("Hello Async Graph")
+    graph.invoke({"aggregate": []}, {"configurable": {"thread_id": "foo"}})
