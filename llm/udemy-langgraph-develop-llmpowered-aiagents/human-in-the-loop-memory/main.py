cac034c4a5115dc721aca8ca323daa89b7940b93
neverlish <neverlish@gmail.com>
2026-01-09 23:25:52 +0900

    6 - 50 MemorySaver

--- llm/udemy-langgraph-develop-llmpowered-aiagents/human-in-the-loop-memory/main.py
+++ llm/udemy-langgraph-develop-llmpowered-aiagents/human-in-the-loop-memory/main.py
@@ -37,3 +37,25 @@ memory = MemorySaver()
 graph = builder.compile(checkpointer=memory, interrupt_before=["human_feedback"])
 
 graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
+
+if __name__ == "__main__":
+    thread = {"configurable": {"thread_id": "777"}}
+
+    initial_input = {"input": "hello world"}
+
+    for event in graph.stream(initial_input, thread, stream_mode="values"):
+        print(event)
+
+    print(graph.get_state(thread).next)
+
+    user_input = input("Tell me how you want to update the state: ")
+
+    graph.update_state(thread, {"user_feedback": user_input}, as_node="human_feedback")
+
+    print("--State after update--")
+    print(graph.get_state(thread))
+
+    print(graph.get_state(thread).next)
+
+    for event in graph.stream(None, thread, stream_mode="values"):
+        print(event)
