b073600574e3f65528a60babd42c31e824fa2a15
neverlish <neverlish@gmail.com>
2026-01-09 22:36:39 +0900

    5 - 44 Coding the Agent's Brain: Implementing the ReAct Runnable

---
+++ llm/udemy-langgraph-develop-llmpowered-aiagents/react-agentexecutor/react.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+@tool
+def triple(num:float) -> float:
+    """
+    param num: a number to triple
+    returns: the triple of the input number
+    """
+    return float(num) * 3
+
+tools = [TavilySearch(max_results=1), triple]
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0).bind_tools(tools)
