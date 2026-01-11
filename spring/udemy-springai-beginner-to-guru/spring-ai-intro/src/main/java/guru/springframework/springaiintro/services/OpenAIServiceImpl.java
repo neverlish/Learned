ad52d6eda9264d178f243e6d3d241e55b0616ff9
neverlish <neverlish@gmail.com>
2026-01-11 20:55:05 +0900

    3 - 23 Directing OpenAI to Respond with JSON

--- spring/udemy-springai-beginner-to-guru/spring-ai-intro/src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
+++ spring/udemy-springai-beginner-to-guru/spring-ai-intro/src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
@@ -1,5 +1,8 @@
 package guru.springframework.springaiintro.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.springaiintro.model.Answer;
 import guru.springframework.springaiintro.model.GetCapitalRequest;
 import guru.springframework.springaiintro.model.Question;
@@ -7,6 +10,7 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -27,6 +31,9 @@ public class OpenAIServiceImpl implements OpenAIService {
     @Value("classpath:templates/get-capital-with-info.st")
     private Resource getCapitalPromptWithInfo;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Override
     public Answer getCapitalWithInfo(GetCapitalRequest getCapitalRequest) {
         PromptTemplate promptTemplate = new PromptTemplate(getCapitalPromptWithInfo);
@@ -38,12 +45,22 @@ public class OpenAIServiceImpl implements OpenAIService {
 
     @Override
     public Answer getCapital(GetCapitalRequest getCapitalRequest) {
-//        PromptTemplate promptTemplate = new PromptTemplate("what is the capital of " + getCapitalRequest.stateOrCountry() + "?");
         PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
         Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
         ChatResponse response = chatModel.call(prompt);
 
-        return new Answer(response.getResult().getOutput().getText());
+        System.out.println(response.getResult().getOutput().getText());
+        String responseString;
+
+        try {
+            JsonNode jsonNode = objectMapper.readTree(response.getResult().getOutput().getText());
+            responseString = jsonNode.get("answer").asText();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Answer(responseString);
+        //return new Answer(response.getResult().getOutput().getText());
     }
 
     @Override
