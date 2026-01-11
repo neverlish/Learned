fd888fe7fe3e34ebd4eb9f41abdc0dbc97e4766f
neverlish <neverlish@gmail.com>
2026-01-11 21:04:12 +0900

    3 - 24 Binding OpenAI Responses With JSON Schema

--- spring/udemy-springai-beginner-to-guru/spring-ai-intro/src/main/java/guru/springframework/springaiintro/services/OpenAIService.java
+++ spring/udemy-springai-beginner-to-guru/spring-ai-intro/src/main/java/guru/springframework/springaiintro/services/OpenAIService.java
@@ -2,13 +2,14 @@ package guru.springframework.springaiintro.services;
 
 import guru.springframework.springaiintro.model.Answer;
 import guru.springframework.springaiintro.model.GetCapitalRequest;
+import guru.springframework.springaiintro.model.GetCapitalResponse;
 import guru.springframework.springaiintro.model.Question;
 
 public interface OpenAIService {
     Answer getAnswer(Question question);
     String getAnswer(String question);
 
-    Answer getCapital(GetCapitalRequest getCapitalRequest);
+    GetCapitalResponse getCapital(GetCapitalRequest getCapitalRequest);
 
     Answer getCapitalWithInfo(GetCapitalRequest getCapitalRequest);
 }
--- spring/udemy-springai-beginner-to-guru/spring-ai-intro/src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
+++ spring/udemy-springai-beginner-to-guru/spring-ai-intro/src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
@@ -5,17 +5,20 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.springaiintro.model.Answer;
 import guru.springframework.springaiintro.model.GetCapitalRequest;
+import guru.springframework.springaiintro.model.GetCapitalResponse;
 import guru.springframework.springaiintro.model.Question;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.BeanOutputConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
@@ -44,23 +47,19 @@ public class OpenAIServiceImpl implements OpenAIService {
     }
 
     @Override
-    public Answer getCapital(GetCapitalRequest getCapitalRequest) {
+    public GetCapitalResponse getCapital(GetCapitalRequest getCapitalRequest) {
+        BeanOutputConverter<GetCapitalResponse> converter = new BeanOutputConverter<>(GetCapitalResponse.class);
+        String format = converter.getFormat();
+        System.out.println("format: " + format);
+
         PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
-        Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
+        Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry(),
+                "format", format));
         ChatResponse response = chatModel.call(prompt);
 
         System.out.println(response.getResult().getOutput().getText());
-        String responseString;
 
-        try {
-            JsonNode jsonNode = objectMapper.readTree(response.getResult().getOutput().getText());
-            responseString = jsonNode.get("answer").asText();
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
-        return new Answer(responseString);
-        //return new Answer(response.getResult().getOutput().getText());
+        return converter.convert(Objects.requireNonNull(response.getResult().getOutput().getText()));
     }
 
     @Override
