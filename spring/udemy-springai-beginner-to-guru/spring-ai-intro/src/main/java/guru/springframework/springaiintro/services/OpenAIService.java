d3f085757106aa1fb36f67207f9fdf61cb959406
neverlish <neverlish@gmail.com>
2026-01-11 20:20:15 +0900

    3 - 21 Using Prompt Templates

--- spring/udemy-springai-beginner-to-guru/spring-ai-intro/src/main/java/guru/springframework/springaiintro/services/OpenAIService.java
+++ spring/udemy-springai-beginner-to-guru/spring-ai-intro/src/main/java/guru/springframework/springaiintro/services/OpenAIService.java
@@ -1,9 +1,12 @@
 package guru.springframework.springaiintro.services;
 
 import guru.springframework.springaiintro.model.Answer;
+import guru.springframework.springaiintro.model.GetCapitalRequest;
 import guru.springframework.springaiintro.model.Question;
 
 public interface OpenAIService {
     Answer getAnswer(Question question);
     String getAnswer(String question);
+
+    Answer getCapital(GetCapitalRequest getCapitalRequest);
 }
--- spring/udemy-springai-beginner-to-guru/spring-ai-intro/src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
+++ spring/udemy-springai-beginner-to-guru/spring-ai-intro/src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
@@ -1,12 +1,17 @@
 package guru.springframework.springaiintro.services;
 
 import guru.springframework.springaiintro.model.Answer;
+import guru.springframework.springaiintro.model.GetCapitalRequest;
 import guru.springframework.springaiintro.model.Question;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
@@ -14,6 +19,19 @@ public class OpenAIServiceImpl implements OpenAIService {
 
     public OpenAIServiceImpl(ChatModel chatModel) {
         this.chatModel = chatModel;
+    }
+
+    @Value("classpath:templates/get-capital-prompt.st")
+    private Resource getCapitalPrompt;
+
+    @Override
+    public Answer getCapital(GetCapitalRequest getCapitalRequest) {
+//        PromptTemplate promptTemplate = new PromptTemplate("what is the capital of " + getCapitalRequest.stateOrCountry() + "?");
+        PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
+        Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
+        ChatResponse response = chatModel.call(prompt);
+
+        return new Answer(response.getResult().getOutput().getText());
     }
 
     @Override
