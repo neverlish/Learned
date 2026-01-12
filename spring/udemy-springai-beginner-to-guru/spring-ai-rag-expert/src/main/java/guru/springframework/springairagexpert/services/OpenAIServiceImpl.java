655bf64d3cef39ead322e0df0549ed55610087c2
neverlish <neverlish@gmail.com>
2026-01-12 22:03:50 +0900

    6 - 51 Configure System Prompt and Test

--- spring/udemy-springai-beginner-to-guru/spring-ai-rag-expert/src/main/java/guru/springframework/springairagexpert/services/OpenAIServiceImpl.java
+++ spring/udemy-springai-beginner-to-guru/spring-ai-rag-expert/src/main/java/guru/springframework/springairagexpert/services/OpenAIServiceImpl.java
@@ -3,10 +3,12 @@ package guru.springframework.springairagexpert.services;
 import guru.springframework.springairagexpert.model.Answer;
 import guru.springframework.springairagexpert.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
@@ -27,22 +29,28 @@ public class OpenAIServiceImpl implements OpenAIService {
     final ChatModel chatModel;
     final VectorStore vectorStore;
 
-    @Value("classpath:/templates/rag-prompt-template-meta.st")
+    @Value("classpath:/templates/rag-prompt-template.st")
     private Resource ragPromptTemplate;
+
+    @Value("classpath:/templates/system-message.st")
+    private Resource systemMessageTemplate;
 
     @Override
     public Answer getAnswer(Question question) {
+        PromptTemplate systemMessagePromptTemplate = new SystemPromptTemplate(systemMessageTemplate);
+        Message systemMessage = systemMessagePromptTemplate.createMessage();
+
         List<Document> documents = vectorStore.similaritySearch(SearchRequest.builder()
                 .query(question.question()).topK(5).build());
         List<String> contentList = documents.stream().map(Document::getContent).toList();
 
         PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
-        Prompt prompt = promptTemplate.create(Map.of("input", question.question(), "documents",
+        Message userMessage = promptTemplate.createMessage(Map.of("input", question.question(), "documents",
                 String.join("\n", contentList)));
 
-        contentList.forEach(System.out::println);
+//        contentList.forEach(System.out::println);
 
-        ChatResponse response = chatModel.call(prompt);
+        ChatResponse response = chatModel.call(new Prompt(List.of(systemMessage, userMessage)));
 
         return new Answer(response.getResult().getOutput().getContent());
     }
