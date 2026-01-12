fe43f888148fedf6ee316cda5ea43f00b4260fdd
neverlish <neverlish@gmail.com>
2026-01-12 22:59:47 +0900

    7 - 57 Configure Function Callback

--- spring/udemy-springai-beginner-to-guru/spring-ai-functions/src/main/java/guru/springframework/springaifunctions/services/OpenAIServiceImpl.java
+++ spring/udemy-springai-beginner-to-guru/spring-ai-functions/src/main/java/guru/springframework/springaifunctions/services/OpenAIServiceImpl.java
@@ -1,11 +1,21 @@
 package guru.springframework.springaifunctions.services;
 
 
+import guru.springframework.springaifunctions.functions.WeatherServiceFunction;
 import guru.springframework.springaifunctions.model.Answer;
 import guru.springframework.springaifunctions.model.Question;
+import guru.springframework.springaifunctions.model.WeatherRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.model.function.FunctionCallback;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -14,10 +24,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
-    final ChatModel chatModel;
+    @Value("${sfg.aiapp.apiNinjasKey}")
+    private String apiNinjasKey;
+
+    private final OpenAiChatModel openAiChatModel;
 
     @Override
     public Answer getAnswer(Question question) {
-       return new Answer("to-do implement me!");
+        var promptOptions = OpenAiChatOptions.builder()
+                .functionCallbacks(List.of(FunctionCallback.builder()
+                        .function("CurrentWeather", new WeatherServiceFunction(apiNinjasKey))
+                        .description("Get the current weather for a location")
+                        .inputType(WeatherRequest.class)
+                        .build()))
+                .build();
+
+        Message userMessage = new PromptTemplate(question.question()).createMessage();
+
+        var response = openAiChatModel.call(new Prompt(List.of(userMessage), promptOptions));
+
+        return new Answer(response.getResult().getOutput().getContent());
     }
 }
