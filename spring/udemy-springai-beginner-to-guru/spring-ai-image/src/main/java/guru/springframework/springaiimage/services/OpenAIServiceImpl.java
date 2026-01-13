cdd9eb6b69d7b5a8add2df206117b5aedc594eaa
neverlish <neverlish@gmail.com>
2026-01-13 23:46:02 +0900

    8 - 69 Implement Vision Service

--- spring/udemy-springai-beginner-to-guru/spring-ai-image/src/main/java/guru/springframework/springaiimage/services/OpenAIServiceImpl.java
+++ spring/udemy-springai-beginner-to-guru/spring-ai-image/src/main/java/guru/springframework/springaiimage/services/OpenAIServiceImpl.java
@@ -2,23 +2,43 @@ package guru.springframework.springaiimage.services;
 
 import guru.springframework.springaiimage.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.image.ImageModel;
 import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.model.Media;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Base64;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
     private final OpenAiImageModel imageModel;
 
+    private final ChatModel chatModel;
+
     @Override
     public String getDescription(MultipartFile file) {
-        return "";
+        OpenAiChatOptions options = OpenAiChatOptions.builder()
+                .withModel(OpenAiApi.ChatModel.GPT_4_O.getValue())
+                .build();
+
+        var userMessage = new UserMessage("Explain what do you see in this picture?",
+                List.of(new Media(MimeTypeUtils.IMAGE_JPEG, file.getResource())));
+
+        ChatResponse response = chatModel.call(new Prompt(List.of(userMessage), options));
+
+        return response.getResult().getOutput().toString();
     }
 
     @Override
