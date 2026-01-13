b9a64c73e62b333d943678b207d6e6490ad265f2
neverlish <neverlish@gmail.com>
2026-01-13 23:53:15 +0900

    9 - 73 OpenAI Speech Client

--- spring/udemy-springai-beginner-to-guru/spring-ai-audio/src/main/java/guru/springframework/springaiaudio/services/OpenAIServiceImpl.java
+++ spring/udemy-springai-beginner-to-guru/spring-ai-audio/src/main/java/guru/springframework/springaiaudio/services/OpenAIServiceImpl.java
@@ -2,6 +2,11 @@ package guru.springframework.springaiaudio.services;
 
 import guru.springframework.springaiaudio.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.ai.openai.audio.speech.SpeechPrompt;
+import org.springframework.ai.openai.audio.speech.SpeechResponse;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,10 +15,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
-
+    private final OpenAiAudioSpeechModel speechModel;
 
     @Override
     public byte[] getSpeech(Question question) {
-        return new byte[0];
+        OpenAiAudioSpeechOptions speechOptions = OpenAiAudioSpeechOptions.builder()
+                .voice(OpenAiAudioApi.SpeechRequest.Voice.ALLOY)
+                .speed(1.0f)
+                .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+                .model(OpenAiAudioApi.TtsModel.TTS_1.value)
+                .build();
+
+        SpeechPrompt speechPrompt = new SpeechPrompt(question.question(), speechOptions);
+
+        SpeechResponse response = speechModel.call(speechPrompt);
+
+        return response.getResult().getOutput();
     }
 }
