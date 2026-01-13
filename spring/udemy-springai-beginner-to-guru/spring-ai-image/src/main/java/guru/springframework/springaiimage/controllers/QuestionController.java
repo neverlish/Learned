1a14c0ee73a4456428d24f1cccbea602dfc3a0c8
neverlish <neverlish@gmail.com>
2026-01-13 23:17:56 +0900

    8 - 64 Implement OpenAI Image Service

--- spring/udemy-springai-beginner-to-guru/spring-ai-image/src/main/java/guru/springframework/springaiimage/controllers/QuestionController.java
+++ spring/udemy-springai-beginner-to-guru/spring-ai-image/src/main/java/guru/springframework/springaiimage/controllers/QuestionController.java
@@ -5,6 +5,7 @@ import guru.springframework.springaiimage.services.OpenAIService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -14,7 +15,7 @@ public class QuestionController {
     private final OpenAIService openAIService;
 
     @PostMapping(value = "/image", produces = MediaType.IMAGE_PNG_VALUE)
-    public byte[] getImage(Question question) {
+    public byte[] getImage(@RequestBody Question question) {
         return openAIService.getImage(question);
     }
 }
--- spring/udemy-springai-beginner-to-guru/spring-ai-image/src/main/java/guru/springframework/springaiimage/services/OpenAIServiceImpl.java
+++ spring/udemy-springai-beginner-to-guru/spring-ai-image/src/main/java/guru/springframework/springaiimage/services/OpenAIServiceImpl.java
@@ -2,15 +2,30 @@ package guru.springframework.springaiimage.services;
 
 import guru.springframework.springaiimage.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.image.ImageModel;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
 
 @RequiredArgsConstructor
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
+    private final ImageModel imageModel;
 
     @Override
     public byte[] getImage(Question question) {
+        var options = OpenAiImageOptions.builder()
+                .withHeight(1024).withWidth(1024)
+                .withResponseFormat("b64_json")
+                .withModel("dall-e-3")
+                .build();
 
-       return null;
+        ImagePrompt imagePrompt = new ImagePrompt(question.question(), options);
+
+        var imageResponse = imageModel.call(imagePrompt);
+
+        return Base64.getDecoder().decode(imageResponse.getResult().getOutput().getB64Json());
     }
 }
