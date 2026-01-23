be97403ca50f07af72bed8d530c0a441a32475a1
neverlish <neverlish@gmail.com>
2026-01-23 12:34:11 +0900

    4 SparkSQL, DataFrames 및 DataSets - 25 [활동] SparkSQL 사용하기

---
+++ scala/udemy-apache-spark-scala/src/main/scala/com/sundogsoftware/spark/SparkSQLDataset.scala
@@ -0,0 +1,43 @@
+package com.sundogsoftware.spark
+
+import org.apache.spark.sql._
+import org.apache.log4j._
+
+object SparkSQLDataset {
+
+  case class Person(id:Int, name:String, age:Int, friends:Int)
+
+  /** Our main function where the action happens */
+  def main(args: Array[String]) {
+    
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    
+    // Use SparkSession interface
+    val spark = SparkSession
+      .builder
+      .appName("SparkSQL")
+      .master("local[*]")
+      .getOrCreate()
+
+    // Load each line of the source data into an Dataset
+    import spark.implicits._
+    val schemaPeople = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("data/fakefriends.csv")
+      .as[Person]
+
+    schemaPeople.printSchema()
+    
+    schemaPeople.createOrReplaceTempView("people")
+
+    val teenagers = spark.sql("SELECT * FROM people WHERE age >= 13 AND age <= 19")
+    
+    val results = teenagers.collect()
+    
+    results.foreach(println)
+    
+    spark.stop()
+  }
+}
