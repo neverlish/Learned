82944cff5d829c3f1e272103fdf1d9327ae76c36
neverlish <neverlish@gmail.com>
2026-02-16 08:16:24 +0900

    2 - 17 Prompt Engineering

--- llm/udemy-aiagents-workflows-practical-guide/02-ai-workflows/main.py
+++ llm/udemy-aiagents-workflows-practical-guide/02-ai-workflows/main.py
@@ -7,10 +7,23 @@ load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-def generate_x_post(usr_input: str) -> str:
+def generate_x_post(topic: str) -> str:
+    prompt = f"""
+        You are an expert social media manager, and you excel at crafting viral and highly engaging posts for X (formerly Twitter).
+
+        Your task is to generate a post that is concise, impactful, and tailored to the topic provided by the user.
+        Avoid using hashtags and lots of emojis (a few emojis are okay, but not too many).
+
+        Keep the post short and focused, structure it in a clean, readable way, using line breaks and empty lines to enhance readability.
+
+        Here's the topic provided by the user for which you need to generate a post:
+        <topic>
+        {topic}
+        </topic>
+"""
     payload = {
         "model": "gpt-4",
-        "input": "..."
+        "input": prompt
     }
     response = requests.post(
         "https://api.openai.com/v1/responses",
