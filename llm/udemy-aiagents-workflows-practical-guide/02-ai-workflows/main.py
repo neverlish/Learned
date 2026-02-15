6986bc841a4f53530c25622240762d72d9a81be7
neverlish <neverlish@gmail.com>
2026-02-16 08:21:08 +0900

    2 - 22 Using The OpenAI Python SDK

--- llm/udemy-aiagents-workflows-practical-guide/02-ai-workflows/main.py
+++ llm/udemy-aiagents-workflows-practical-guide/02-ai-workflows/main.py
@@ -1,11 +1,9 @@
-import os
-
-import requests
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client = OpenAI()
 
 def generate_x_post(topic: str) -> str:
     prompt = f"""
@@ -21,21 +19,9 @@ def generate_x_post(topic: str) -> str:
         {topic}
         </topic>
 """
-    payload = {"model": "gpt-4o", "input": prompt}
-    response = requests.post(
-        "https://api.openai.com/v1/responses",
-        json=payload,
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {OPENAI_API_KEY}",
-        },
-    )
+    response = client.responses.create(model="gpt-4o", input=prompt)
 
-    response_text = (
-        response.json().get("output", [{}])[0].get("content", [{}])[0].get("text", "")
-    )
-
-    return response_text
+    return response.output_text
 
 def main():
     print("Hello from essentials!")
