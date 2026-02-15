ab1f7a918506d3b2ed2e2b031f6b4c1392bf6cec
neverlish <neverlish@gmail.com>
2026-02-16 00:06:06 +0900

    2 - 14 Getting Started With A First Example Workflow

--- llm/udemy-aiagents-workflows-practical-guide/02-ai-workflows/main.py
+++ llm/udemy-aiagents-workflows-practical-guide/02-ai-workflows/main.py
@@ -1,5 +1,14 @@
+def generate_x_post(usr_input: str) -> str:
+    # Call AI / LLM
+    pass
+
 def main():
     print("Hello from essentials!")
+
+    usr_input = input("What should the post be about?")
+    x_post = generate_x_post(usr_input)
+    print("Generated X post")
+    print(x_post)
 
 
 if __name__ == "__main__":
