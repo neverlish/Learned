a1c5c64ca8afc9b52b7d80fda0b399d0723f7fd0
neverlish <neverlish@gmail.com>
2026-02-16 14:09:33 +0900

    4 - 56 Adding Tools To Agents

--- llm/udemy-aiagents-workflows-practical-guide/04-third-party/research_crew/src/research_crew/crew.py
+++ llm/udemy-aiagents-workflows-practical-guide/04-third-party/research_crew/src/research_crew/crew.py
@@ -2,6 +2,8 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
+from crewai_tools import BraveSearchTool
+
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
@@ -23,7 +25,8 @@ class ResearchCrew():
     def researcher(self) -> Agent:
         return Agent(
             config=self.agents_config['researcher'], # type: ignore[index]
-            verbose=True
+            verbose=True,
+            tools=[BraveSearchTool()]
         )
 
     @agent
