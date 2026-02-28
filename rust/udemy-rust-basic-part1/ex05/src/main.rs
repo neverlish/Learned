5b545a99034b1c91011e28a16a3d15a03c12a101
neverlish <neverlish@gmail.com>
2026-02-28 15:56:42 +0900

    5 개체 처리 (1)

---
+++ rust/udemy-rust-basic-part1/ex05/src/main.rs
@@ -0,0 +1,25 @@
+// fn main() {
+//     let n = 12;
+//     let n2 = 51;
+//     println!("{}", n + n2);
+// }
+
+// fn main() {
+//     let n;
+//     n = 12;
+//     println!("{}", n);
+// }
+
+// fn main() {
+//     let n1;
+//     println!("{}", n1);
+//     n1 = 10;
+// }
+
+fn main() {
+    let _n = 10;
+    let n = 20;
+    let _ = 30;
+
+    println!("{}", n);
+}
