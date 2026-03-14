973b716692f15b503b38b9e3ddd81cde0d129c1b
neverlish <neverlish@gmail.com>
2026-03-14 15:54:20 +0900

    5 - 3 동시 접속 웹소켓

--- rust/jpub-rust-axum/websocket/src/main.rs
+++ rust/jpub-rust-axum/websocket/src/main.rs
@@ -1,37 +1,83 @@
+use std::sync::Arc;
+
 use axum::{
+    body::Body,
+    extract::{
+        ws::{Message, WebSocket},
+        State, WebSocketUpgrade,
+    },
+    http::{HeaderMap, Request, StatusCode},
+    middleware::{self, Next},
+    response::{IntoResponse, Response},
     routing::get,
     Router,
-    extract::{
-        WebSocketUpgrade,
-        ws::{Message, WebSocket},
-    },
-    response::IntoResponse,
+};
+use futures_util::{
+    sink::SinkExt,
+    stream::{SplitSink, SplitStream, StreamExt},
 };
 
-use futures_util::{sink::SinkExt, stream::StreamExt};
+use tokio::sync::{
+    broadcast::{self, Receiver, Sender},
+    Mutex,
+};
+#[derive(Debug, Clone)]
+struct AppState {
+    broadcast_tx: Arc<Mutex<Sender<Message>>>,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/ws", get(websocket_handler));
+    let (tx, _) = broadcast::channel(32);
+    let app = AppState {
+        broadcast_tx: Arc::new(Mutex::new(tx)),
+    };
+    let app = Router::new()
+        .route("/ws", get(websocket_handler)).with_state(app);
     
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn websocket_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
-    ws.on_upgrade(handle_socket)
+async fn websocket_handler(ws: WebSocketUpgrade, State(app): State<AppState>) -> impl IntoResponse {
+    ws.on_upgrade(|socket| handle_socket(socket, app))
 }
 
-async fn handle_socket(ws: WebSocket) {
-    let (mut ws_tx, mut ws_rx) = ws.split();
+async fn handle_socket(ws: WebSocket, app: AppState) {
+    let (ws_tx, ws_rx) = ws.split();
+    let ws_tx = Arc::new(Mutex::new(ws_tx));
+    
+    {
+        let broadcast_rx = app.broadcast_tx.lock().await.subscribe();
+        tokio::spawn(async move {
+            recv_broadcast(ws_tx, broadcast_rx).await;
+        });
+    }
 
-    while let Some(Ok(msg)) = ws_rx.next().await {
-        ws_tx
-            .send(Message::Text(format!(
-                "Message received: {}",
-                msg.to_text().unwrap()
-            )))
-            .await
-            .unwrap();
+    recv_from_client(ws_rx, app.broadcast_tx).await;
+}
+
+async fn recv_from_client(
+    mut client_rx: SplitStream<WebSocket>,
+    broadcast_tx: Arc<Mutex<Sender<Message>>>,
+) {
+    while let Some(Ok(msg)) = client_rx.next().await {
+        if matches!(msg, Message::Close(_)) {
+            return;
+        }
+        if broadcast_tx.lock().await.send(msg).is_err() {
+            println!("Failed to broadcast a message");
+        }
+    }
+}
+
+async fn recv_broadcast(
+    client_tx: Arc<Mutex<SplitSink<WebSocket, Message>>>,
+    mut broadcast_rx: Receiver<Message>,
+) {
+    while let Ok(msg) = broadcast_rx.recv().await {
+        if client_tx.lock().await.send(msg).await.is_err() {
+            return;
+        }
     }
 }
