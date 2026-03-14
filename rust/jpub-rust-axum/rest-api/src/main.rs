373b94e5d6eeda00250a468b51a91ec4ac513cf8
neverlish <neverlish@gmail.com>
2026-03-14 11:19:07 +0900

    3 - 6 모듈화 - 1 DB 모듈 분리

--- rust/jpub-rust-axum/rest-api/src/main.rs
+++ rust/jpub-rust-axum/rest-api/src/main.rs
@@ -1,9 +1,11 @@
 mod entities;
+mod db;
+
 
 use std::collections::HashMap;
 
 use axum::{
-    extract::Query,
+    extract::{Query, State},
     routing::get,
     Json,
     Router,
@@ -12,7 +14,7 @@ use axum::{
 use sea_orm::{
     ColumnTrait,
     Condition,
-    Database,
+    DatabaseConnection,
     EntityTrait,
     QueryFilter,
 };
@@ -23,11 +25,12 @@ use entities::users::{
     Model,
 };
 
-const DATABASE_URL: &str = "postgres://axum:1234@localhost/axum";
+use db::init_db;
 
-async fn get_user(Query(params): Query<HashMap<String, String>>) -> Json<Model> {
-    let conn = Database::connect(DATABASE_URL).await.unwrap();
-
+async fn get_user(
+    State(conn): State<DatabaseConnection>,
+    Query(params): Query<HashMap<String, String>>
+) -> Json<Model> {
     let mut condition = Condition::any();
 
     if let Some(id) = params.get("id") {
@@ -50,7 +53,13 @@ async fn get_user(Query(params): Query<HashMap<String, String>>) -> Json<Model> 
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/users", get(get_user));
+    dotenvy::dotenv().ok();
+
+    let conn = init_db().await;
+    
+    let app = Router::new()
+        .route("/users", get(get_user))
+        .with_state(conn);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
