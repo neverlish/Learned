2e6dcab45c8beb0c4ec5fd2390e2d6307c1e16c0
neverlish <neverlish@gmail.com>
2026-05-22 17:54:47 +0900

    4 - 19 Creating Multiple LLM Chat Clients Configuration Example

--- llm/udemy-mastering-spring-ai/chatmodel001/src/main/java/org/example/chatmodel001/config/AiConfig.java
+++ llm/udemy-mastering-spring-ai/chatmodel001/src/main/java/org/example/chatmodel001/config/AiConfig.java
@@ -1,15 +1,28 @@
 package org.example.chatmodel001.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AiConfig {
+    @Primary
     @Bean
-    ChatClient chatClient(ChatClient.Builder builder) {
-        return builder
-                .defaultSystem("You are a coding assistant.")
-                .build();
+    public ChatClient openAIChatClient(@Qualifier("openAiChatModel") ChatModel openAiChatModel) {
+        ChatClient chatClient = ChatClient.create(openAiChatModel);
+        System.out.println("Created ChatClient:" + chatClient);
+        System.out.println("Using ChatModel: " + openAiChatModel);
+        return chatClient;
+    }
+
+    @Bean
+    public ChatClient antropicChatClient(@Qualifier("anthropicChatModel") ChatModel antropicChatModel) {
+        ChatClient chatClient = ChatClient.create(antropicChatModel);
+        System.out.println("Created ChatClient:" + chatClient);
+        System.out.println("Using ChatModel: " + antropicChatModel);
+        return chatClient;
     }
 }
--- llm/udemy-mastering-spring-ai/chatmodel001/src/main/java/org/example/chatmodel001/service/ClientClientService.java
+++ llm/udemy-mastering-spring-ai/chatmodel001/src/main/java/org/example/chatmodel001/service/ClientClientService.java
@@ -4,18 +4,15 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class ClientClientService {
-//    @Bean
-    public ChatClient openAIChatClient(ChatModel openAiChatModel) {
-        ChatClient chatClient = ChatClient.create(openAiChatModel);
-        System.out.println("Created ChatClient:" + chatClient);
-        System.out.println("Using ChatModel: " + openAiChatModel);
-        return chatClient;
-    }
+
+    @Autowired
+    private ChatClient chatClient;
 
 }
