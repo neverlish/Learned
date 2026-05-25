41f37593095947db3e598d7ebfa3db81a07887d1
neverlish <neverlish@gmail.com>
2026-05-25 15:31:34 +0900

    6 - 41 Structured Output example with Object

---
+++ llm/udemy-mastering-spring-ai/springboot-structured-output-demo/src/main/java/com/example/springboot_structured_output_demo/Movie.java
@@ -0,0 +1,4 @@
+package com.example.springboot_structured_output_demo;
+
+public record Movie(String title, String year) {
+}
--- llm/udemy-mastering-spring-ai/springboot-structured-output-demo/src/main/java/com/example/springboot_structured_output_demo/controller/AIController.java
+++ llm/udemy-mastering-spring-ai/springboot-structured-output-demo/src/main/java/com/example/springboot_structured_output_demo/controller/AIController.java
@@ -1,6 +1,8 @@
 package com.example.springboot_structured_output_demo.controller;
 
+import com.example.springboot_structured_output_demo.Movie;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.converter.ListOutputConverter;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.convert.support.DefaultConversionService;
@@ -44,5 +46,24 @@ public class AIController {
                 .entity(new ParameterizedTypeReference<Map<String, String>>() {
 
                 });
+    }
+
+    @GetMapping("/director-movie-service")
+    public List<Movie> getDirectorMovies(@RequestParam("directorName") String directorName) {
+        String template = """
+                "Generate a list of movies directed by {directorName}. If the director is unknown, return null.
+                Each movie should include a title and release year. {format}"
+                """;
+
+        List<Movie> movieList = chatClient.prompt()
+                .user(userSpec -> userSpec.text(template)
+                        .param("directorName", directorName)
+                        .param("format", "json"))
+                .call()
+                .entity(new ParameterizedTypeReference<List<Movie>>() {
+
+                });
+
+        return movieList;
     }
 }
