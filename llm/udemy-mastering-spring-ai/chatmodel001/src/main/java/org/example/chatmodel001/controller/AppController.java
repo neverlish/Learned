3744f27d140897b595b7c9098777573e53e884b7
neverlish <neverlish@gmail.com>
2026-05-25 12:57:51 +0900

    4 - 26 ChatResponse Example

--- llm/udemy-mastering-spring-ai/chatmodel001/src/main/java/org/example/chatmodel001/controller/AppController.java
+++ llm/udemy-mastering-spring-ai/chatmodel001/src/main/java/org/example/chatmodel001/controller/AppController.java
@@ -3,6 +3,7 @@ package org.example.chatmodel001.controller;
 import org.example.chatmodel001.service.ChatClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,9 +11,8 @@ public class AppController {
     @Autowired
     ChatClientService chatClientService;
 
-    @GetMapping("/ask")
-    public String ask() {
-        chatClientService.askQuestions();
-        return "Questions asked. Check console for responses.";
+    @GetMapping("/ask/{userInput}")
+    public String ask(@PathVariable String userInput) {
+        return chatClientService.askQuestion(userInput);
     }
 }
--- llm/udemy-mastering-spring-ai/chatmodel001/src/main/java/org/example/chatmodel001/service/ChatClientService.java
+++ llm/udemy-mastering-spring-ai/chatmodel001/src/main/java/org/example/chatmodel001/service/ChatClientService.java
@@ -1,8 +1,8 @@
 package org.example.chatmodel001.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 
@@ -12,24 +12,18 @@ public class ChatClientService {
     @Autowired
     private ChatClient chatClient;
 
-    @Autowired
-    @Qualifier("antropicChatClient")
-    private ChatClient anthropicChatClient;
-
-    public void askQuestions() {
-        String openAIResponse = chatClient.prompt()
-                .user("Tell me two five lines about java")
+    public String askQuestion(String userInput) {
+        System.out.println("ChatResponse ");
+        ChatResponse chatResponse = chatClient.prompt()
+                .user(userInput)
                 .call()
-                .content();
-        System.out.println("Response from OpenAI model : " + openAIResponse);
-
-        System.out.println("----------------------");
-
-        String anthropicResponse = anthropicChatClient.prompt()
-                .user("What is the capital of France?")
-                .call()
-                .content();
-        System.out.println("Response from Anthropic model : " + anthropicResponse);
+                .chatResponse();
+        System.out.println("ChatResponse: " + chatResponse);
+        String text = chatResponse.getResult().getOutput().getText();
+        System.out.println("Actual Response Text: " + text);
+        return text;
 
     }
+
+
 }
