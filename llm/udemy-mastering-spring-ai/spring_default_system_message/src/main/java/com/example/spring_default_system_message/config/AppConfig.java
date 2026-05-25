a7b8de814ff7009e2b188c43cc41f3e2749a3af4
neverlish <neverlish@gmail.com>
2026-05-25 12:39:44 +0900

    4 - 23 Default Sysmtem Message using Resource

--- llm/udemy-mastering-spring-ai/spring_default_system_message/src/main/java/com/example/spring_default_system_message/config/AppConfig.java
+++ llm/udemy-mastering-spring-ai/spring_default_system_message/src/main/java/com/example/spring_default_system_message/config/AppConfig.java
@@ -1,15 +1,20 @@
 package com.example.spring_default_system_message.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class AppConfig {
+    @Value("classpath:system_message.txt")
+    Resource resource;
+
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
         return chatClientBuilder
-                .defaultSystem("You are a helpful airline assistant.")
+                .defaultSystem(resource)
                 .build();
     }
 }
