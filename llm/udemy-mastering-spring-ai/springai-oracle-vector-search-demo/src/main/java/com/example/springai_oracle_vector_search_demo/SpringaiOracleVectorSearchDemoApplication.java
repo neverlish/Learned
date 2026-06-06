b37b20a954e52896bd08d9e1808b6bc0f4fb80f3
neverlish <neverlish@gmail.com>
2026-06-06 10:51:32 +0900

    9 - 69 Oracle Vector Search Example

---
+++ llm/udemy-mastering-spring-ai/springai-oracle-vector-search-demo/src/main/java/com/example/springai_oracle_vector_search_demo/SpringaiOracleVectorSearchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springai_oracle_vector_search_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiOracleVectorSearchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiOracleVectorSearchDemoApplication.class, args);
+	}
+
+}
---
+++ llm/udemy-mastering-spring-ai/springai-oracle-vector-search-demo/src/test/java/com/example/springai_oracle_vector_search_demo/SpringaiOracleVectorSearchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springai_oracle_vector_search_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiOracleVectorSearchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
