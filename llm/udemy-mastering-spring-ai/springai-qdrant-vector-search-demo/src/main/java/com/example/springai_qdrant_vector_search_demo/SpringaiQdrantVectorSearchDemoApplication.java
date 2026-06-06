65d3fa8c5e723d2d38b76e123407902413f7998d
neverlish <neverlish@gmail.com>
2026-06-06 11:48:47 +0900

    9 - 73 Qdrant Vector Search Example

---
+++ llm/udemy-mastering-spring-ai/springai-qdrant-vector-search-demo/src/main/java/com/example/springai_qdrant_vector_search_demo/SpringaiQdrantVectorSearchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springai_qdrant_vector_search_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiQdrantVectorSearchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiQdrantVectorSearchDemoApplication.class, args);
+	}
+
+}
---
+++ llm/udemy-mastering-spring-ai/springai-qdrant-vector-search-demo/src/test/java/com/example/springai_qdrant_vector_search_demo/SpringaiQdrantVectorSearchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springai_qdrant_vector_search_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiQdrantVectorSearchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
